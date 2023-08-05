c4e01f01429b875d74f8c5ffeeec91874611a848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:58 +0000

    Add package aiida-quantumespresso/3.0.0a4/aiida_quantumespresso-3.0.0a4.tar.gz

---
+++ packages/aiida-quantumespresso/aiida_quantumespresso-3.0.0a4.tar.gz/aiida_quantumespresso-3.0.0a4/aiida_quantumespresso/calculations/ph.py
@@ -0,0 +1,332 @@
+# -*- coding: utf-8 -*-
+"""Plugin to create a Quantum Espresso ph.x input file."""
+from __future__ import absolute_import
+
+import io
+import os
+import numpy
+import six
+
+from aiida import orm
+from aiida.common import datastructures, exceptions
+from aiida.common.lang import classproperty
+from aiida.engine import CalcJob
+
+from aiida_quantumespresso.calculations.pw import PwCalculation
+from aiida_quantumespresso.calculations import _lowercase_dict, _uppercase_dict
+from aiida_quantumespresso.utils.convert import convert_input_to_namelist_entry
+
+
+class PhCalculation(CalcJob):
+    """`CalcJob` implementation for the ph.x code of Quantum ESPRESSO."""
+
+    # Keywords that cannot be set by the user but will be set by the plugin
+    _blocked_keywords = [
+        ('INPUTPH', 'outdir'),
+        ('INPUTPH', 'iverbosity'),
+        ('INPUTPH', 'prefix'),
+        ('INPUTPH', 'fildyn'),
+        ('INPUTPH', 'ldisp'),
+        ('INPUTPH', 'nq1'),
+        ('INPUTPH', 'nq2'),
+        ('INPUTPH', 'nq3'),
+        ('INPUTPH', 'qplot')
+    ]
+
+    _use_kpoints = True
+
+    _compulsory_namelists = ['INPUTPH']
+
+    # Default input and output files
+    _PREFIX = 'aiida'
+    _DEFAULT_INPUT_FILE = 'aiida.in'
+    _DEFAULT_OUTPUT_FILE = 'aiida.out'
+    _OUTPUT_XML_TENSOR_FILE_NAME = 'tensors.xml'
+
+    # Not using symlink in pw to allow multiple nscf to run on top of the same scf
+    _default_symlink_usage = False
+
+    @classmethod
+    def define(cls, spec):
+        super(PhCalculation, cls).define(spec)
+        spec.input('metadata.options.input_filename', valid_type=six.string_types, default=cls._DEFAULT_INPUT_FILE)
+        spec.input('metadata.options.output_filename', valid_type=six.string_types, default=cls._DEFAULT_OUTPUT_FILE)
+        spec.input('metadata.options.parser_name', valid_type=six.string_types, default='quantumespresso.ph')
+        spec.input('qpoints', valid_type=orm.KpointsData, help='qpoint mesh')
+        spec.input('parameters', valid_type=orm.Dict, help='')
+        spec.input('settings', valid_type=orm.Dict, required=False, help='')
+        spec.input('parent_folder', valid_type=orm.RemoteData,
+            help='the folder of a completed `PwCalculation`')
+        spec.output('output_parameters', valid_type=orm.Dict)
+        spec.default_output_node = 'output_parameters'
+        spec.exit_code(
+            100, 'ERROR_NO_RETRIEVED_FOLDER', message='The retrieved folder data node could not be accessed.')
+        spec.exit_code(
+            110, 'ERROR_READING_OUTPUT_FILE', message='The output file could not be read from the retrieved folder.')
+
+    @classproperty
+    def _OUTPUT_SUBFOLDER(cls):
+        return './out/'
+
+    @classproperty
+    def _FOLDER_DRHO(cls):
+        return 'FILDRHO'
+
+    @classproperty
+    def _DRHO_PREFIX(cls):
+        return 'drho'
+
+    @classproperty
+    def _DRHO_STAR_EXT(cls):
+        return 'drho_rot'
+
+    @classproperty
+    def _FOLDER_DYNAMICAL_MATRIX(cls):
+        return 'DYN_MAT'
+
+    @classproperty
+    def _OUTPUT_DYNAMICAL_MATRIX_PREFIX(cls):
+        return os.path.join(cls._FOLDER_DYNAMICAL_MATRIX, 'dynamical-matrix-')
+
+    def prepare_for_submission(self, folder):
+        """Create the input files from the input nodes passed to this instance of the `CalcJob`.
+
+        :param folder: an `aiida.common.folders.Folder` to temporarily write files on disk
+        :return: `aiida.common.datastructures.CalcInfo` instance
+        """
+        local_copy_list = []
+        remote_copy_list = []
+        remote_symlink_list = []
+
+        if 'settings' in self.inputs:
+            settings = _uppercase_dict(self.inputs.settings.get_dict(), dict_name='settings')
+        else:
+            settings = {}
+
+        parent_folder = self.inputs.parent_folder
+
+        try:
+            parent_calc = parent_folder.get_incoming(node_class=orm.CalcJobNode).one().node
+        except ValueError:
+            raise exceptions.UniquenessError('Input RemoteData has multiple parents')
+
+        # If the parent calculation is a `PhCalculation` we are restarting
+        restart_flag = parent_calc.process_type == 'aiida.calculations:quantumespresso.ph'
+
+        # Also, the parent calculation must be on the same computer
+        if not self.node.computer.uuid == parent_calc.computer.uuid:
+            raise exceptions.InputValidationError(
+                'Calculation has to be launched on the same computer as that of the parent: {}'.format(
+                    parent_calc.computer.get_name()))
+
+        # put by default, default_parent_output_folder = ./out
+        try:
+            default_parent_output_folder = parent_calc.process_class._OUTPUT_SUBFOLDER
+        except AttributeError:
+            try:
+                default_parent_output_folder = parent_calc._get_output_folder()
+            except AttributeError:
+                raise exceptions.InputValidationError('parent calculation does not have a default output subfolder')
+        parent_calc_out_subfolder = settings.pop('PARENT_CALC_OUT_SUBFOLDER', default_parent_output_folder)
+
+        # I put the first-level keys as uppercase (i.e., namelist and card names) and the second-level keys as lowercase
+        parameters = _uppercase_dict(self.inputs.parameters.get_dict(), dict_name='parameters')
+        parameters = {k: _lowercase_dict(v, dict_name=k) for k, v in six.iteritems(parameters)}
+
+        prepare_for_d3 = settings.pop('PREPARE_FOR_D3', False)
+        if prepare_for_d3:
+            self._blocked_keywords += [
+                ('INPUTPH', 'fildrho'),
+                ('INPUTPH', 'drho_star%open'),
+                ('INPUTPH', 'drho_star%ext'),
+                ('INPUTPH', 'drho_star%dir')
+            ]
+
+        for namelist, flag in self._blocked_keywords:
+            if namelist in parameters:
+                if flag in parameters[namelist]:
+                    raise exceptions.InputValidationError(
+                        "Cannot specify explicitly the '{}' flag in the '{}' namelist or card.".format(flag, namelist))
+
+        if 'INPUTPH' not in parameters:
+            raise exceptions.InputValidationError('required namelist INPUTPH not specified')
+
+        parameters['INPUTPH']['outdir'] = self._OUTPUT_SUBFOLDER
+        parameters['INPUTPH']['iverbosity'] = 1
+        parameters['INPUTPH']['prefix'] = self._PREFIX
+        parameters['INPUTPH']['fildyn'] = self._OUTPUT_DYNAMICAL_MATRIX_PREFIX
+
+        if prepare_for_d3:
+            parameters['INPUTPH']['fildrho'] = self._DRHO_PREFIX
+            parameters['INPUTPH']['drho_star%open'] = True
+            parameters['INPUTPH']['drho_star%ext'] = self._DRHO_STAR_EXT
+            parameters['INPUTPH']['drho_star%dir'] = self._FOLDER_DRHO
+
+        try:
+            mesh, offset = self.inputs.qpoints.get_kpoints_mesh()
+
+            if any([i != 0. for i in offset]):
+                raise NotImplementedError(
+                    'Computation of phonons on a mesh with non zero offset is not implemented, at the level of ph.x')
+
+            parameters['INPUTPH']['ldisp'] = True
+            parameters['INPUTPH']['nq1'] = mesh[0]
+            parameters['INPUTPH']['nq2'] = mesh[1]
+            parameters['INPUTPH']['nq3'] = mesh[2]
+
+            postpend_text = None
+
+        except AttributeError:
+            # this is the case where no mesh was set. Maybe it's a list
+            try:
+                list_of_points = self.inputs.qpoints.get_kpoints(cartesian=True)
+            except AttributeError:
+                # In this case, there are no info on the qpoints at all
+                raise exceptions.InputValidationError('Input `qpoints` contains neither a mesh nor a list of points')
+
+            # change to 2pi/a coordinates
+            lattice_parameter = numpy.linalg.norm(self.inputs.qpoints.cell[0])
+            list_of_points *= lattice_parameter / (2. * numpy.pi)
+
+            # add here the list of point coordinates
+            if len(list_of_points) > 1:
+                parameters['INPUTPH']['qplot'] = True
+                parameters['INPUTPH']['ldisp'] = True
+                postpend_text = '{}\n'.format(len(list_of_points))
+                for points in list_of_points:
+                    postpend_text += '{}  {}  {}  1\n'.format(*points)
+
+                # Note: the weight is fixed to 1, because ph.x calls these
+                # things weights but they are not such. If they are going to
+                # exist with the meaning of weights, they will be supported
+            else:
+                parameters['INPUTPH']['ldisp'] = False
+                postpend_text = ''
+                for points in list_of_points:
+                    postpend_text += '{}  {}  {}\n'.format(*points)
+
+        # customized namelists, otherwise not present in the distributed ph code
+        try:
+            namelists_toprint = settings.pop('NAMELISTS')
+            if not isinstance(namelists_toprint, list):
+                raise exceptions.InputValidationError(
+                    "The 'NAMELISTS' value, if specified in the settings input "
+                    'node, must be a list of strings')
+        except KeyError:  # list of namelists not specified in the settings; do automatic detection
+            namelists_toprint = self._compulsory_namelists
+
+        # create a folder for the dynamical matrices
+        if not restart_flag:  # if it is a restart, it will be copied over
+            folder.get_subfolder(self._FOLDER_DYNAMICAL_MATRIX, create=True)
+
+        input_filename = folder.get_abs_path(self.metadata.options.input_filename)
+        with io.open(input_filename, 'w') as infile:
+            for namelist_name in namelists_toprint:
+                infile.write(u'&{0}\n'.format(namelist_name))
+                # namelist content; set to {} if not present, so that we leave an empty namelist
+                namelist = parameters.pop(namelist_name, {})
+                for key, value in sorted(six.iteritems(namelist)):
+                    infile.write(convert_input_to_namelist_entry(key, value))
+                infile.write(u'/\n')
+
+            # add list of qpoints if required
+            if postpend_text is not None:
+                infile.write(postpend_text)
+
+        if parameters:
+            raise exceptions.InputValidationError(
+                'The following namelists are specified in parameters, but are '
+                'not valid namelists for the current type of calculation: '
+                '{}'.format(','.join(list(parameters.keys()))))
+
+        # copy the parent scratch
+        symlink = settings.pop('PARENT_FOLDER_SYMLINK', self._default_symlink_usage)  # a boolean
+        if symlink:
+            # I create a symlink to each file/folder in the parent ./out
+            folder.get_subfolder(self._OUTPUT_SUBFOLDER, create=True)
+
+            remote_symlink_list.append((
+                parent_folder.computer.uuid,
+                os.path.join(parent_folder.get_remote_path(), parent_calc_out_subfolder, '*'),
+                self._OUTPUT_SUBFOLDER
+            ))
+
+            # I also create a symlink for the ./pseudo folder
+            # TODO: suppress this when the recover option of QE will be fixed
+            # (bug when trying to find pseudo file)
+            remote_symlink_list.append((
+                parent_folder.computer.uuid,
+                os.path.join(parent_folder.get_remote_path(), self._get_pseudo_folder()),
+                self._get_pseudo_folder()
+            ))
+        else:
+            # here I copy the whole folder ./out
+            remote_copy_list.append((
+                parent_folder.computer.uuid,
+                os.path.join(parent_folder.get_remote_path(), parent_calc_out_subfolder),
+                self._OUTPUT_SUBFOLDER
+            ))
+            # I also copy the ./pseudo folder
+            # TODO: suppress this when the recover option of QE will be fixed
+            # (bug when trying to find pseudo file)
+            remote_copy_list.append((
+                parent_folder.computer.uuid,
+                os.path.join(parent_folder.get_remote_path(), self._get_pseudo_folder()),
+                self._get_pseudo_folder()
+            ))
+
+        if restart_flag:  # in this case, copy in addition also the dynamical matrices
+            if symlink:
+                remote_symlink_list.append((
+                    parent_folder.computer.uuid,
+                    os.path.join(parent_folder.get_remote_path(), self._FOLDER_DYNAMICAL_MATRIX),
+                    self._FOLDER_DYNAMICAL_MATRIX
+                ))
+
+            else:
+                # copy the dynamical matrices
+                # no need to copy the _ph0, since I copied already the whole ./out folder
+                remote_copy_list.append((
+                    parent_folder.computer.uuid,
+                    os.path.join(parent_folder.get_remote_path(), self._FOLDER_DYNAMICAL_MATRIX),
+                    '.'
+                ))
+
+        # Create an `.EXIT` file if `only_initialization` flag in `settings` is set to `True`
+        if settings.pop('ONLY_INITIALIZATION', False):
+            exit_filename = folder.get_abs_path('{}.EXIT'.format(self._PREFIX))
+            with open(exit_filename, 'w') as handle:
+                handle.write('\n')
+
+        codeinfo = datastructures.CodeInfo()
+        codeinfo.cmdline_params = (list(settings.pop('CMDLINE', [])) + ['-in', self.metadata.options.input_filename])
+        codeinfo.stdout_name = self.metadata.options.output_filename
+        codeinfo.code_uuid = self.inputs.code.uuid
+
+        calcinfo = datastructures.CalcInfo()
+        calcinfo.uuid = str(self.uuid)
+        calcinfo.codes_info = [codeinfo]
+        calcinfo.local_copy_list = local_copy_list
+        calcinfo.remote_copy_list = remote_copy_list
+        calcinfo.remote_symlink_list = remote_symlink_list
+
+        # Retrieve by default the output file and the xml file
+        filepath_xml_tensor = os.path.join(self._OUTPUT_SUBFOLDER, '_ph0', '{}.phsave'.format(self._PREFIX))
+        calcinfo.retrieve_list = []
+        calcinfo.retrieve_list.append(self.metadata.options.output_filename)
+        calcinfo.retrieve_list.append(self._FOLDER_DYNAMICAL_MATRIX)
+        calcinfo.retrieve_list.append(os.path.join(filepath_xml_tensor, self._OUTPUT_XML_TENSOR_FILE_NAME))
+        calcinfo.retrieve_list += settings.pop('ADDITIONAL_RETRIEVE_LIST', [])
+
+        if settings:
+            unknown_keys = ', '.join(list(settings.keys()))
+            raise exceptions.InputValidationError('`settings` contained unknown keys: {}'.format(unknown_keys))
+
+        return calcinfo
+
+    def _get_pseudo_folder(self):
+        """
+        Get the calculation-specific pseudo folder (relative path).
+        Default given by PwCalculation._PSEUDO_SUBFOLDER
+        """
+        return PwCalculation._PSEUDO_SUBFOLDER
---
+++ packages/aiida-quantumespresso/aiida_quantumespresso-3.0.0a4.tar.gz/aiida_quantumespresso-3.0.0a4/aiida_quantumespresso/workflows/ph/base.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""Workchain to run a Quantum ESPRESSO ph.x calculation with automated error handling and restarts."""
+from __future__ import absolute_import
+
+from aiida import orm
+from aiida.common import AttributeDict
+from aiida.engine import while_
+from aiida.plugins import CalculationFactory
+
+from aiida_quantumespresso.common.workchain.utils import ErrorHandlerReport, register_error_handler
+from aiida_quantumespresso.common.workchain.base.restart import BaseRestartWorkChain
+from aiida_quantumespresso.utils.resources import get_default_options
+
+PhCalculation = CalculationFactory('quantumespresso.ph')
+PwCalculation = CalculationFactory('quantumespresso.pw')
+
+
+class PhBaseWorkChain(BaseRestartWorkChain):
+    """Workchain to run a Quantum ESPRESSO ph.x calculation with automated error handling and restarts."""
+
+    _calculation_class = PhCalculation
+
+    defaults = AttributeDict({
+        'delta_factor_max_seconds': 0.95,
+        'alpha_mix': 0.70,
+    })
+
+    @classmethod
+    def define(cls, spec):
+        # yapf: disable
+        super(PhBaseWorkChain, cls).define(spec)
+        spec.input('code', valid_type=orm.Code)
+        spec.input('qpoints', valid_type=orm.KpointsData)
+        spec.input('parent_folder', valid_type=orm.RemoteData)
+        spec.input('parameters', valid_type=orm.Dict, required=False)
+        spec.input('settings', valid_type=orm.Dict, required=False)
+        spec.input('options', valid_type=orm.Dict, required=False)
+        spec.input('only_initialization', valid_type=orm.Bool, default=orm.Bool(False))
+        spec.outline(
+            cls.setup,
+            cls.validate_inputs,
+            while_(cls.should_run_calculation)(
+                cls.prepare_calculation,
+                cls.run_calculation,
+                cls.inspect_calculation,
+            ),
+            cls.results,
+        )
+        spec.exit_code(
+            402, 'ERROR_CALCULATION_INVALID_INPUT_FILE',
+            message='the calculation failed because it had an invalid input file'
+        )
+        spec.output('output_parameters', valid_type=orm.Dict)
+        spec.output('remote_folder', valid_type=orm.RemoteData)
+        spec.output('retrieved', valid_type=orm.FolderData)
+
+    def validate_inputs(self):
+        """
+        Validate inputs that depend might depend on each other and cannot be validated by the spec. Also define
+        dictionary `inputs` in the context, that will contain the inputs for the calculation that will be launched
+        in the `run_calculation` step.
+        """
+        self.ctx.inputs = AttributeDict({
+            'code': self.inputs.code,
+            'qpoints': self.inputs.qpoints,
+            'parent_folder': self.inputs.parent_folder,
+        })
+
+        if 'parameters' in self.inputs:
+            self.ctx.inputs.parameters = self.inputs.parameters.get_dict()
+        else:
+            self.ctx.inputs.parameters = {}
+
+        if 'INPUTPH' not in self.ctx.inputs.parameters:
+            self.ctx.inputs.parameters['INPUTPH'] = {}
+
+        if 'settings' in self.inputs:
+            self.ctx.inputs.settings = self.inputs.settings.get_dict()
+        else:
+            self.ctx.inputs.settings = {}
+
+        if 'options' in self.inputs:
+            self.ctx.inputs.options = self.inputs.options.get_dict()
+        else:
+            self.ctx.inputs.options = get_default_options()
+
+        if self.inputs.only_initialization.value:
+            self.ctx.inputs.settings['ONLY_INITIALIZATION'] = True
+
+    def prepare_calculation(self):
+        """
+        Prepare the inputs for the next calculation
+        """
+        if isinstance(self.ctx.restart_calc, PhCalculation):
+            self.ctx.inputs.parameters['INPUTPH']['recover'] = True
+            self.ctx.inputs.parent_folder = self.ctx.restart_calc.outputs.remote_folder
+
+    def _prepare_process_inputs(self, process, inputs):
+        """
+        The 'max_seconds' setting in the 'INPUTPH' card of the parameters will be set to a fraction of the
+        'max_wallclock_seconds' that will be given to the job via the 'options' dictionary. This will prevent the job
+        from being prematurely terminated by the scheduler without getting the chance to exit cleanly.
+        """
+        max_wallclock_seconds = inputs.options['max_wallclock_seconds']
+        max_seconds_factor = self.defaults.delta_factor_max_seconds
+        max_seconds = max_wallclock_seconds * max_seconds_factor
+        inputs.parameters['INPUTPH']['max_seconds'] = max_seconds
+
+        return super(PhBaseWorkChain, self)._prepare_process_inputs(process, inputs)
+
+
+@register_error_handler(PhBaseWorkChain, 400)
+def _handle_fatal_error_read_namelists(self, calculation):
+    """
+    The calculation failed because it could not read the generated input file
+    """
+    if any(['reading inputph namelist' in w for w in calculation.res.warnings]):
+        self.report('PhCalculation<{}> failed because of an invalid input file'.format(calculation.pk))
+        return ErrorHandlerReport(True, True, self.exit_codes.ERROR_CALCULATION_INVALID_INPUT_FILE)
+
+
+@register_error_handler(PhBaseWorkChain, 300)
+def _handle_error_exceeded_maximum_walltime(self, calculation):
+    """
+    Calculation ended nominally but ran out of allotted wall time
+    """
+    if 'Maximum CPU time exceeded' in calculation.res.warnings:
+        self.ctx.restart_calc = calculation
+        self.report(
+            'PhCalculation<{}> terminated because maximum wall time was exceeded, restarting'.format(calculation.pk)
+        )
+        return ErrorHandlerReport(True, True)
+
+
+@register_error_handler(PhBaseWorkChain, 200)
+def _handle_fatal_error_not_converged(self, calculation):
+    """
+    The calculation failed because it could not read the generated input file
+    """
+    if 'Phonon did not reach end of self consistency' in calculation.res.warnings:
+        alpha_mix_old = calculation.inputs.parameters.get_dict()['INPUTPH'].get('alpha_mix(1)', self.defaults.alpha_mix)
+        alpha_mix_new = 0.9 * alpha_mix_old
+        self.ctx.inputs.parameters['INPUTPH']['alpha_mix(1)'] = alpha_mix_new
+        self.ctx.restart_calc = calculation
+        self.report(
+            'PhCalculation<{}> terminated without reaching convergence, '
+            'setting alpha_mix to {} and restarting'.format(calculation.pk, alpha_mix_new)
+        )
+        return ErrorHandlerReport(True, True)
+
+
+@register_error_handler(PhBaseWorkChain, 100)
+def _handle_error_premature_termination(self, calculation):
+    """
+    Calculation did not reach the end of execution, probably because it was killed by the scheduler
+    for running out of allotted walltime
+    """
+    if 'QE ph run did not reach the end of the execution.' in calculation.res.parser_warnings:
+        inputs = calculation.inputs.parameters.get_dict()
+        settings = self.ctx.inputs.settings
+
+        factor = self.defaults.delta_factor_max_seconds
+        max_seconds = settings.get('max_seconds', inputs['INPUTPH']['max_seconds'])
+        max_seconds_reduced = int(max_seconds * factor)
+        self.ctx.inputs.parameters['INPUTPH']['max_seconds'] = max_seconds_reduced
+
+        self.ctx.restart_calc = calculation
+        self.report(
+            'PwCalculation<{}> was terminated prematurely, reducing "max_seconds" from {} to {}'.format(
+                calculation.pk, max_seconds, max_seconds_reduced
+            )
+        )
+        return ErrorHandlerReport(True, True)
