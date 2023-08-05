76ebcda9498238a3c1545c94f98d6155c8b576ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:52 +0000

    Add package lazytools-sflippl/0.0.0.9000.post1/lazytools_sflippl-0.0.0.9000.post1-py3-none-any.whl

---
+++ packages/lazytools-sflippl/lazytools_sflippl-0.0.0.9000.post1-py3-none-any.whl/lazytools/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 12:35:02 2019
+
+@author: sflippl
+"""
+
+name = 'lazytools'
+
+from lazytools.spyder import *
+from lazytools.exceptions import *
+from lazytools.to_dataframe import *
+from lazytools.visualizations import *
