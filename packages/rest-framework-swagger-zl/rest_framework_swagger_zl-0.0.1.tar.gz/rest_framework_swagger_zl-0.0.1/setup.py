314f8747f056c0bb1227a5fab44662a802a43a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:27 +0000

    Add package rest-framework-swagger-zl/0.0.1/rest_framework_swagger_zl-0.0.1.tar.gz

---
+++ packages/rest-framework-swagger-zl/rest_framework_swagger_zl-0.0.1.tar.gz/rest_framework_swagger_zl-0.0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name = "rest_framework_swagger_zl",
+    version = "0.0.1",
+    keywords = ("pip", "rest_framework_swagger_zl"),
+    description = "generate api doc html",
+    long_description = "generate api doc html",
+    license = "MIT Licence",
+    url = "https://github.com/zhangliu520/rest_framework_swagger_zl.git",
+    author = "mrzl",
+    author_email = "752477168@qq.com",
+    packages = find_packages(),
+    platforms = "any",
+    install_requires = [],
+
+)
+
