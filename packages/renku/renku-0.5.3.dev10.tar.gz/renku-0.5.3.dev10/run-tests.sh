c8710d68bb8299bcea56a1d471f342721f0c2624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:37 +0000

    Add package renku/0.5.3.dev10/renku-0.5.3.dev10.tar.gz

---
+++ packages/renku/renku-0.5.3.dev10.tar.gz/renku-0.5.3.dev10/run-tests.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017-2019 - Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# quit on errors:
+set -o errexit
+
+# quit on unbound symbols:
+set -o nounset
+
+pydocstyle renku tests conftest.py docs
+isort -rc -c -df
+unify -c -r renku tests conftest.py docs
+check-manifest --ignore ".travis-*,renku/version.py"
+find . -iname \*.sh -print0 | xargs -0 shellcheck
+sphinx-build -qnNW docs docs/_build/html
+pytest -v -m "not integration"
