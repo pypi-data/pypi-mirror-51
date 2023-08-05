0f9524f13a43b53d3472c4661b75a62312c0cda9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:04 +0000

    Add package test-run/0.0.6/test-run-0.0.6.zip

---
+++ packages/test-run/test-run-0.0.6.zip/test-run-0.0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+def readme():
+	with open('README.md') as f:
+		README=f.read()
+	return README
+
+setup(
+	name="test-run",
+	version="0.0.6",
+	description="A Python package to print",
+	long_description=readme(),
+	long_description_content_type=["text/markdown"],
+	author="ANOOP SINGH RANA",
+	author_email="apours986@gmail.com",
+	license="MIT",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+	],
+	#packages="test_run", thisw was for python3 but u need each nd every directory separately
+	packages=["test_run"],
+	include_package_data=True,
+	entry_points={
+		"console_scripts":  [
+			"test-run=test_run.test1:main",
+		]
+	},
+)
