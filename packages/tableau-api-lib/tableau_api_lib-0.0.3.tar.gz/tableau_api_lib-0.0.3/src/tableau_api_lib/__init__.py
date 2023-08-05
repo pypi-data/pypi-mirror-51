599533b2400ecd07c8848f3c5c2126d7e438195d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:20 +0000

    Add package tableau-api-lib/0.0.3/tableau_api_lib-0.0.3.tar.gz

---
+++ packages/tableau-api-lib/tableau_api_lib-0.0.3.tar.gz/tableau_api_lib-0.0.3/src/tableau_api_lib/__init__.py
@@ -0,0 +1,8 @@
+from tableau_api_lib.tableau_server_connection import TableauServerConnection
+from tableau_api_lib.sample import sample_config
+from tableau_api_lib import decorators
+from tableau_api_lib import api_endpoints
+from tableau_api_lib import exceptions
+from tableau_api_lib import api_requests
+
+name = 'tableau_api_lib'
