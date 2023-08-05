ce1317b88d6a969563ba88066afd801613ba6ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:13 +0000

    Add package flashtext-mod/2.73/flashtext-mod-2.73.tar.gz

---
+++ packages/flashtext-mod/flashtext-mod-2.73.tar.gz/flashtext-mod-2.73/flashtext-mod/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+name = 'flashtext-mod'
+version = '2.73'
+
+cmdclass = {'test': PyTest}
+
+try:
+    from sphinx.setup_command import BuildDoc
+    cmdclass['build_sphinx'] = BuildDoc
+except ImportError:
+    print('WARNING: sphinx not available, not building docs')
+
+setup(
+    name=name,
+    version=version,
+    url='https://github.com/brlala/flashtext-mod',
+    author='brlala',
+    author_email='henrytlh@hotmail.com',
+    description='Extract/Replaces keywords in sentences.',
+    download_url='https://github.com/brlala/flashtext-mod/archive/v2.73.tar.gz',
+    packages=['flashtext-mod'],
+    install_requires=[],
+    platforms='any',
+    cmdclass=cmdclass,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', version)
+        }
+    }
+)
