aa601bec7afe49c06a413abe2465148f622bc84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:43:41 +0000

    Add package turkishnlp/0.0.61/turkishnlp-0.0.61.tar.gz

---
+++ packages/turkishnlp/turkishnlp-0.0.61.tar.gz/turkishnlp-0.0.61/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name="turkishnlp",
+    version="0.0.61",
+    packages=['turkishnlp'],
+    description="A python script that processes Turkish language",
+    long_description=open('README.md', encoding="utf8").read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/MeteHanC/turkishnlp",
+    author="Metehan Cetinkaya",
+    author_email="metehancet@gmail.com",
+    maintainer="Metehan Cetinkaya",
+    maintainer_email="metehancet@gmail.com",
+    keywords=['turkishnlp', 'python', 'nlp', 'language processing'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Environment :: MacOS X',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
