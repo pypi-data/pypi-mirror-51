9afaef0616c88fd84058deee4d4fe4307cad60cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:49 +0000

    Add package gui-automation/3.0.3/gui_automation-3.0.3.tar.gz

---
+++ packages/gui-automation/gui_automation-3.0.3.tar.gz/gui_automation-3.0.3/setup.py
@@ -0,0 +1,25 @@
+# Made by Marcos Boggia
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='gui_automation',
+    version='3.0.3',
+    author="Marcos Boggia",
+    author_email="marcos_boggia@hotmail.com",
+    description="Simple python library useful for automating tasks using images.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcosboggia/gui_automation",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'PyAutoGUI'
+    ],
+    classifiers=[
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: MIT License",
+     "Operating System :: OS Independent",
+    ],
+ )
