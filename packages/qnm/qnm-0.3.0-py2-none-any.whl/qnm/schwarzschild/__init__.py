b69f7e785f1a8b1c6d94f4f41b39312181d5219e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:40 +0000

    Add package qnm/0.3.0/qnm-0.3.0-py2-none-any.whl

---
+++ packages/qnm/qnm-0.3.0-py2-none-any.whl/qnm/schwarzschild/__init__.py
@@ -0,0 +1,10 @@
+""" Finding QNMs of Schwarzschild (numerically and approximately).
+
+TODO Documentation.
+"""
+
+from __future__ import print_function, division, absolute_import
+
+from . import approx
+from . import tabulated
+from . import overtonesequence
