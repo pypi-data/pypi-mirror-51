01420366f9c4b7d96635cd332477360f40763044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:07 +0000

    Add package dynaconf/2.0.4/dynaconf-2.0.4.tar.gz

---
+++ packages/dynaconf/dynaconf-2.0.4.tar.gz/dynaconf-2.0.4/dynaconf/loaders/base.py
@@ -0,0 +1,172 @@
+import io
+import os
+
+from dynaconf.utils import ensure_a_list
+from dynaconf.utils import raw_logger
+
+
+logger = raw_logger()
+
+
+class BaseLoader(object):
+    """Base loader for dynaconf source files.
+
+    :param obj: {[LazySettings]} -- [Dynaconf settings]
+    :param env: {[string]} -- [the current env to be loaded defaults to
+      [development]]
+    :param identifier: {[string]} -- [identifier ini, yaml, json, py, toml]
+    :param extensions: {[list]} -- [List of extensions with dots ['.a', '.b']]
+    :param file_reader: {[callable]} -- [reads file return dict]
+    :param string_reader: {[callable]} -- [reads string return dict]
+    """
+
+    def __init__(
+        self, obj, env, identifier, extensions, file_reader, string_reader
+    ):
+        """Instantiates a loader for different sources"""
+        self.obj = obj
+        self.env = env or obj.current_env
+        self.identifier = identifier
+        self.extensions = extensions
+        self.file_reader = file_reader
+        self.string_reader = string_reader
+
+    @staticmethod
+    def warn_not_installed(obj, identifier):  # pragma: no cover
+        if identifier not in obj._not_installed_warnings:
+            logger.warning(
+                "%(ident)s support is not installed in your environment. "
+                "`pip install dynaconf[%(ident)s]`",
+                {"ident": identifier},
+            )
+        obj._not_installed_warnings.append(identifier)
+
+    def load(self, filename=None, key=None, silent=True):
+        """
+        Reads and loads in to `self.obj` a single key or all keys from source
+
+        :param filename: Optional filename to load
+        :param key: if provided load a single key
+        :param silent: if load erros should be silenced
+        """
+        filename = filename or self.obj.get(self.identifier.upper())
+        if not filename:
+            return
+
+        if not isinstance(filename, (list, tuple)):
+            split_files = ensure_a_list(filename)
+            if all([f.endswith(self.extensions) for f in split_files]):  # noqa
+                files = split_files  # it is a ['file.ext', ...]
+            else:  # it is a single config as string
+                files = [filename]
+        else:  # it is already a list/tuple
+            files = filename
+
+        self.obj._loaded_files.extend(files)
+
+        # add the [default] env
+        env_list = [self.obj.get("DEFAULT_ENV_FOR_DYNACONF")]
+
+        # compatibility with older versions that still uses [dynaconf] as
+        # [default] env
+        global_env = self.obj.get("ENVVAR_PREFIX_FOR_DYNACONF") or "DYNACONF"
+        if global_env not in env_list:
+            env_list.append(global_env)
+
+        # add the current [env]
+        if self.env not in env_list:
+            env_list.append(self.env)
+
+        # add the [global] env
+        env_list.append("GLOBAL")
+
+        # load all envs
+        self._read(files, env_list, silent, key)
+
+    def _read(self, files, envs, silent=True, key=None):
+        for source_file in files:
+            if source_file.endswith(self.extensions):
+                try:
+                    with io.open(
+                        source_file,
+                        encoding=self.obj.get(
+                            "ENCODING_FOR_DYNACONF", "utf-8"
+                        ),
+                    ) as open_file:
+                        source_data = self.file_reader(open_file)
+                    self.obj.logger.debug(
+                        "{}_loader: {}".format(self.identifier, source_file)
+                    )
+                except IOError:
+                    self.obj.logger.debug(
+                        "{}_loader: {} (Ignored, file not Found)".format(
+                            self.identifier, source_file
+                        )
+                    )
+                    source_data = None
+            else:
+                # for tests it is possible to pass string
+                source_data = self.string_reader(source_file)
+
+            if not source_data:
+                continue
+
+            # env name is checked in lower
+            source_data = {
+                k.lower(): value for k, value in source_data.items()
+            }
+
+            for env in envs:
+
+                data = {}
+                try:
+                    data = source_data[env.lower()]
+                except KeyError:
+                    if env not in (
+                        self.obj.get("ENVVAR_PREFIX_FOR_DYNACONF")
+                        or "DYNACONF",
+                        "GLOBAL",
+                    ):
+                        message = "%s_loader: %s env not defined in %s" % (
+                            self.identifier,
+                            env,
+                            source_file,
+                        )
+                        if silent:
+                            self.obj.logger.warning(message)
+                        else:
+                            raise KeyError(message)
+                    continue
+
+                if env != self.obj.get("DEFAULT_ENV_FOR_DYNACONF"):
+                    identifier = "{0}_{1}".format(self.identifier, env.lower())
+                else:
+                    identifier = self.identifier
+
+                # data 1st level keys should be transformed to upper case.
+                data = {k.upper(): v for k, v in data.items()}
+                if key:
+                    key = key.upper()
+
+                is_secret = "secret" in source_file
+
+                self.obj.logger.debug(
+                    "{}_loader: {}[{}]{}".format(
+                        self.identifier,
+                        os.path.split(source_file)[-1],
+                        env.lower(),
+                        list(data.keys()) if is_secret else data,
+                    )
+                )
+
+                if not key:
+                    self.obj.update(
+                        data, loader_identifier=identifier, is_secret=is_secret
+                    )
+                elif key in data:
+                    self.obj.set(
+                        key,
+                        data.get(key),
+                        loader_identifier=identifier,
+                        is_secret=is_secret,
+                    )
---
+++ packages/dynaconf/dynaconf-2.0.4.tar.gz/dynaconf-2.0.4/dynaconf/loaders/vault_loader.py
@@ -0,0 +1,132 @@
+# docker run -e 'VAULT_DEV_ROOT_TOKEN_ID=myroot' -p 8200:8200 vault
+# pip install hvac
+from dynaconf.utils.parse_conf import parse_conf_data
+
+try:
+    from hvac import Client
+    from hvac.exceptions import InvalidPath
+except ImportError:
+    raise ImportError(
+        "vault package is not installed in your environment. "
+        "`pip install dynaconf[vault]` or disable the vault loader with "
+        "export VAULT_ENABLED_FOR_DYNACONF=false"
+    )
+
+
+IDENTIFIER = "vault"
+
+
+def _get_env_list(obj, env):
+    """Creates the list of environments to read
+
+    :param obj: the settings instance
+    :param env: settings env default='DYNACONF'
+    :return: a list of working environments
+    """
+    # add the [default] env
+    env_list = [obj.get("DEFAULT_ENV_FOR_DYNACONF")]
+    # compatibility with older versions that still uses [dynaconf] as
+    # [default] env
+    global_env = obj.get("ENVVAR_PREFIX_FOR_DYNACONF") or "DYNACONF"
+    if global_env not in env_list:
+        env_list.append(global_env)
+    # add the current env
+    if obj.current_env and obj.current_env not in env_list:
+        env_list.append(obj.current_env)
+    # add a manually set env
+    if env and env not in env_list:
+        env_list.append(env)
+    # add the [global] env
+    env_list.append("GLOBAL")
+    return [env.lower() for env in env_list]
+
+
+def get_client(obj):
+    client = Client(
+        **{k: v for k, v in obj.VAULT_FOR_DYNACONF.items() if v is not None}
+    )
+    if obj.VAULT_ROLE_ID_FOR_DYNACONF is not None:
+        client.auth_approle(
+            role_id=obj.VAULT_ROLE_ID_FOR_DYNACONF,
+            secret_id=obj.get("VAULT_SECRET_ID_FOR_DYNACONF"),
+        )
+    assert client.is_authenticated(), (
+        "Vault authentication error: is VAULT_TOKEN_FOR_DYNACONF or "
+        "VAULT_ROLE_ID_FOR_DYNACONF defined?"
+    )
+    return client
+
+
+def load(obj, env=None, silent=None, key=None):
+    """Reads and loads in to "settings" a single key or all keys from vault
+
+    :param obj: the settings instance
+    :param env: settings env default='DYNACONF'
+    :param silent: if errors should raise
+    :param key: if defined load a single key, else load all in env
+    :return: None
+    """
+
+    client = get_client(obj)
+    env_list = _get_env_list(obj, env)
+    for env in env_list:
+        path = "/".join([obj.VAULT_PATH_FOR_DYNACONF, env])
+        try:
+            data = client.secrets.kv.read_secret_version(path)
+        except InvalidPath:
+            # If the path doesn't exist, ignore it and set data to None
+            data = None
+        if data:
+            # There seems to be a data dict within a data dict,
+            # extract the inner data
+            data = data.get("data", {}).get("data", {})
+        try:
+            if data and key:
+                value = parse_conf_data(data.get(key), tomlfy=True)
+                if value:
+                    obj.logger.debug(
+                        "vault_loader: loading by key: %s:%s (%s:%s)",
+                        key,
+                        "****",
+                        IDENTIFIER,
+                        path,
+                    )
+                    obj.set(key, value)
+            elif data:
+                obj.logger.debug(
+                    "vault_loader: loading: %s (%s:%s)",
+                    list(data.keys()),
+                    IDENTIFIER,
+                    path,
+                )
+                obj.update(data, loader_identifier=IDENTIFIER, tomlfy=True)
+        except Exception as e:
+            if silent:
+                if hasattr(obj, "logger"):
+                    obj.logger.error(str(e))
+                return False
+            raise
+
+
+def write(obj, data=None, **kwargs):
+    """Write a value in to loader source
+
+    :param obj: settings object
+    :param data: vars to be stored
+    :param kwargs: vars to be stored
+    :return:
+    """
+    if obj.VAULT_ENABLED_FOR_DYNACONF is False:
+        raise RuntimeError(
+            "Vault is not configured \n"
+            "export VAULT_ENABLED_FOR_DYNACONF=true\n"
+            "and configure the VAULT_FOR_DYNACONF_* variables"
+        )
+    data = data or {}
+    data.update(kwargs)
+    if not data:
+        raise AttributeError("Data must be provided")
+    client = get_client(obj)
+    path = "/".join([obj.VAULT_PATH_FOR_DYNACONF, obj.current_env.lower()])
+    client.secrets.kv.create_or_update_secret(path, secret=data)
+    load(obj)
