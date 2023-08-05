13b5fb67bfaff1fcb92658bae4108509f2cda137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package bff/0.1.9/bff-0.1.9.tar.gz

---
+++ packages/bff/bff-0.1.9.tar.gz/bff-0.1.9/bff/__init__.py
@@ -0,0 +1,37 @@
+"""All of bff' functions."""
+import logging
+
+from ._version import get_versions
+
+from .fancy import (
+    cast_to_category_pd, concat_with_categories, get_peaks, idict,
+    mem_usage_pd, parse_date, plot_history, plot_predictions, plot_series,
+    plot_true_vs_pred, read_sql_by_chunks, sliding_window, value_2_list
+)
+
+from .config import FancyConfig
+
+# Public object of the module.
+__all__ = [
+    'cast_to_category_pd',
+    'concat_with_categories',
+    'get_peaks',
+    'idict',
+    'mem_usage_pd',
+    'parse_date',
+    'plot_history',
+    'plot_predictions',
+    'plot_series',
+    'plot_true_vs_pred',
+    'read_sql_by_chunks',
+    'sliding_window',
+    'value_2_list',
+    'FancyConfig',
+]
+
+# Logging configuration.
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT)
+
+__version__ = get_versions()['version']
+del get_versions
