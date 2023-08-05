4d7974969b6bfecbe4601953090cc389cdf1a02f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:52 +0000

    Add package Python-video-annotator/3.305.533/Python%20video%20annotator-3.305.533.tar.gz

---
+++ packages/Python-video-annotator/Python%20video%20annotator-3.305.533.tar.gz/Python video annotator-3.305.533/setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import re, os
+
+
+PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+
+with open(os.path.join(PACKAGE_PATH, 'pythonvideoannotator','__init__.py'), 'r') as fd:
+    content = fd.read()
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)
+
+with open(os.path.join(PACKAGE_PATH, '..','..','README.md'), 'r') as fd:
+    long_description = fd.read()
+
+
+# REQUIREMENTS BEGIN
+REQUIREMENTS = [
+    "geometry_designer==0.4.38",
+	"modular-computer-vision-api-gui==0.3.31",
+	"pyforms-gui==4.904.151",
+	"modular-computer-vision-api==0.3.28",
+	"python-video-annotator-models-gui==0.7.62",
+	"python-video-annotator-models==0.8.82",
+	"python-video-annotator-module-timeline==0.6.26",
+	"python-video-annotator-module-eventstats==0.5.15",
+	"python-video-annotator-module-virtual-object-generator==0.6.26",
+	"python-video-annotator-module-deeplab==0.902.21",
+	"python-video-annotator-module-contours-images==0.5.28",
+	"python-video-annotator-module-tracking==0.6.38",
+	"python-video-annotator-module-smooth-paths==0.5.19",
+	"python-video-annotator-module-distances==0.5.18",
+	"python-video-annotator-module-path-map==0.6.16",
+	"python-video-annotator-module-motion-counter==0.5.26",
+	"python-video-annotator-module-create-paths==0.5.15",
+	"python-video-annotator-module-regions-filter==0.5.18",
+	"python-video-annotator-module-import-export==0.5.23",
+	"python-video-annotator-module-background-finder==0.5.21",
+	"python-video-annotator-module-find-orientation==0.5.18",
+	"python-video-annotator-module-path-editor==0.5.28"
+]
+# REQUIREMENTS END
+
+setup(
+    name='Python video annotator',
+    version=version,
+    description="""""",
+    author=['Ricardo Ribeiro'],
+    author_email='ricardojvr@gmail.com',
+    url='https://bitbucket.org/fchampalimaud/pythonvideoannotator-models',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'simplejson',
+        'pypi-xmlrpc',
+        'send2trash',
+        'scipy',
+        'sklearn',
+        'confapp',
+    ] + REQUIREMENTS,
+    entry_points={
+        'console_scripts': [
+            'start-video-annotator=pythonvideoannotator.__main__:start',
+        ],
+    },
+    package_data={'pythonvideoannotator': [
+        'resources/icons/*.png',
+        'resources/themes/default/*.css',
+        ]
+    },
+)
