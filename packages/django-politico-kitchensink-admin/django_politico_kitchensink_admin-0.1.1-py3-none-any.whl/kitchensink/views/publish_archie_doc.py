ada59360e1f2e280afe416ce82ea9bc4ca197bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:01 +0000

    Add package django-politico-kitchensink-admin/0.1.1/django_politico_kitchensink_admin-0.1.1-py3-none-any.whl

---
+++ packages/django-politico-kitchensink-admin/django_politico_kitchensink_admin-0.1.1-py3-none-any.whl/kitchensink/views/publish_archie_doc.py
@@ -0,0 +1,35 @@
+import requests
+from django.http import Http404
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from kitchensink.conf import settings
+from kitchensink.models import ArchieDoc
+from kitchensink.utils.api_auth import TokenAPIAuthentication
+
+
+class PublishArchieDoc(APIView):
+    authentication_classes = (TokenAPIAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self, pk):
+        try:
+            return ArchieDoc.objects.get(pk=pk)
+        except ArchieDoc.DoesNotExist:
+            raise Http404
+
+    def post(self, request, pk, format=None):
+        doc = self.get_object(pk)
+        r = requests.post(
+            settings.API_ENDPOINT,
+            json={
+                "archieDoc": doc.doc_id,
+                "publish": True,
+                "schema": doc.validation_schema,
+            },
+        )
+        if r.status_code != requests.codes.ok:
+            return Response(r.content, status=r.status_code)
+        return Response(r.json(), status=status.HTTP_200_OK)
