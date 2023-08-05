eb43858a5c6e417ec49eaa2ab4f74086a8764fad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:24 +0000

    Add package gorpyter/0.6.0/gorpyter-0.6.0.tar.gz

---
+++ packages/gorpyter/gorpyter-0.6.0.tar.gz/gorpyter-0.6.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gorpyter",
+    version="0.6.0",
+    author="Layne Sadler",
+    author_email="layne.sadler@gmail.com",
+    description="Python wrapper for GOR's R SDK with Pandas serialization.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/gorpyter/",
+    license="WXNC",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'rpy2>=3.0.5','tzlocal>=2.0.0', 'pandas>=0.25.0', 'numpy>=1.17.0',
+    ],
+)
