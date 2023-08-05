760b91ff23bb401a61db69b59d1fc259785abf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:24 +0000

    Add package nylas/4.8.0/nylas-4.8.0-py2-none-any.whl

---
+++ packages/nylas/nylas-4.8.0-py2-none-any.whl/tests/test_events.py
@@ -0,0 +1,48 @@
+import pytest
+from urlobject import URLObject
+from requests import RequestException
+from nylas.client.restful_models import Event
+
+
+def blank_event(api_client):
+    event = api_client.events.create()
+    event.title = "Paris-Brest"
+    event.calendar_id = "calendar_id"
+    event.when = {"start_time": 1409594400, "end_time": 1409594400}
+    return event
+
+
+@pytest.mark.usefixtures("mock_event_create_response")
+def test_event_crud(api_client):
+    event1 = blank_event(api_client)
+    event1.save()
+    assert event1.id == "cv4ei7syx10uvsxbs21ccsezf"
+
+    event1.title = "blah"
+    event1.save()
+    assert event1.title == "loaded from JSON"
+    assert event1.get("ignored") is None
+
+    # Third time should fail.
+    event2 = blank_event(api_client)
+    with pytest.raises(RequestException):
+        event2.save()
+
+
+@pytest.mark.usefixtures("mock_event_create_notify_response")
+def test_event_notify(mocked_responses, api_client):
+    event1 = blank_event(api_client)
+    event1.save(notify_participants="true", other_param="1")
+    assert event1.id == "cv4ei7syx10uvsxbs21ccsezf"
+
+    url = mocked_responses.calls[-1].request.url
+    query = URLObject(url).query_dict
+    assert query["notify_participants"] == "true"
+    assert query["other_param"] == "1"
+
+
+@pytest.mark.usefixtures("mock_calendars", "mock_events")
+def test_calendar_events(api_client):
+    calendar = api_client.calendars.first()
+    assert calendar.events
+    assert all(isinstance(event, Event) for event in calendar.events)
