71bd69c773919055a14144bb93e6d5f8b651a45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:12 +0000

    Add package ONEmSDK/0.5.0/ONEmSDK-0.5.0.tar.gz

---
+++ packages/ONEmSDK/ONEmSDK-0.5.0.tar.gz/ONEmSDK-0.5.0/onemsdk/parser/node.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Dict, List, Union
+
+from pydantic import BaseModel
+
+
+class Node(BaseModel):
+    tag: str
+    attrs: Dict[str, Union[str, None]] = {}
+    children: List[Union[Node, str]] = []
+
+    def add_child(self, child: Union[Node, str]):
+        self.children.append(child)
+
+
+Node.update_forward_refs()
