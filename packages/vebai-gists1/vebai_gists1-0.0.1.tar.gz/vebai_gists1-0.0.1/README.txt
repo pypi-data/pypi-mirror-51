99722f6771c3ec0acb9b4da120d05bf054c55ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:59 +0000

    Add package vebai-gists1/0.0.1/vebai_gists1-0.0.1.tar.gz

---
+++ packages/vebai-gists1/vebai_gists1-0.0.1.tar.gz/vebai_gists1-0.0.1/README.txt
@@ -0,0 +1,6 @@
+This is a project which reads a specified users public gists
+
+Install by typing:  pip instsll gists 
+
+python -m twine upload --repository-url https://test.pypi.org/legacy/dist/*
+
---
+++ packages/vebai-gists1/vebai_gists1-0.0.1.tar.gz/vebai_gists1-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="vebai_gists1",
+
+    # Version number (initial):
+    version="0.0.1",
+
+    # Application author details:
+    author="Victor Ebai",
+    
+    description='Get gists and notify when new gists are published',
+    
+    # Packages
+    py_modules=["vebai_gists1"],
+    package_dir={'':'gists'},
+
+    # Include additional files into the package
+
+
+    
+    # license="LICENSE.txt",
+    
+
+)
