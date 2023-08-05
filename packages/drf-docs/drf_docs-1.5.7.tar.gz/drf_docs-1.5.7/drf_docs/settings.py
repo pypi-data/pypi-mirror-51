73eb963048c1a2b3e514958625fc5d7056ec21fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:13 +0000

    Add package drf-docs/1.5.7/drf_docs-1.5.7.tar.gz

---
+++ packages/drf-docs/drf_docs-1.5.7.tar.gz/drf_docs-1.5.7/drf_docs/settings.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+import importlib
+
+class DRFSettings(object):
+    def __init__(self):
+        self.drf_settings = {
+            "HIDE_DOCS": self.get_setting("HIDE_DOCS") or False,
+            "APP_NAME": self.get_setting("APP_NAME") or settings.ROOT_URLCONF.split(".")[0],
+        }
+        # 获取用户settings中的ERROR_CODE
+        error_code = self.get_setting("ERROR_CODE") or False
+        if error_code:
+            #  TODO 异常处理
+            ins = importlib.import_module(error_code)
+            # 获取翻译
+            translate = getattr(ins, "translate", None)
+            # 校验translate格式 [{"desc":xxx, "data":{"key": "value"}}]
+            for i in translate:
+                if "desc" in i and "data" in i:
+                    assert isinstance(i["data"], dict)
+                else:
+                    assert "desc" in i and "data" in i
+
+            self.drf_settings['translate'] = translate
+        else:
+            self.drf_settings['translate'] = []
+
+    def get_setting(self, name):
+        try:
+            return settings.DRF_DOCS[name]
+        except:
+            return None
+
+    @property
+    def settings(self):
+        return self.drf_settings
+
+# dev增加一行注释
