58ccab0c3f2a8933c681a38e1a1c78075332b0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:52 +0000

    Add package purrsong/0.1.3/purrsong-0.1.3-py3-none-any.whl

---
+++ packages/purrsong/purrsong-0.1.3-py3-none-any.whl/purrsong/api.py
@@ -0,0 +1,4 @@
+from purrsong.datasets import load as load_dataset
+from purrsong.datasets import list_datasets
+from purrsong.models import load as load_model
+from purrsong.models import list_models
