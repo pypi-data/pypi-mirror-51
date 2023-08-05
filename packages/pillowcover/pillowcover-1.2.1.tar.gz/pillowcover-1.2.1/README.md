d4eb32f168dc33c7b6b40ab5e7daa4b54e05ff38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:48 +0000

    Add package pillowcover/1.2.1/pillowcover-1.2.1.tar.gz

---
+++ packages/pillowcover/pillowcover-1.2.1.tar.gz/pillowcover-1.2.1/README.md
@@ -0,0 +1,85 @@
+# pillowcover
+Mass Manipulate Images Using Python: Change Brightness, Contrast, Sharpness, Resize, Compress, Crop and Convert (e.g png to jpg).
+A simple script (wrapper) using pillow.
+Licensed Under: GNU General Public License v3 or later (GPLv3+)
+For the Gui option checkout branch 'gui'
+
+## Requirements
+python >= 3.5, pillow
+
+## Installation
+Simplest option is to install with pip using sudo.
+``` bash
+sudo python3 -m pip install pillowcover --upgrade
+```
+--OR--
+Install as a user without sudo. This installs it in ~/.local/bin/ make sure this path is in your $PATH.
+``` bash
+python3 -m pip install --user pillowcover --upgrade
+```
+If not found, add the following to your .bashrc or .zshrc
+``` bash
+# Add to PATH to Install and run programs with "pip install --user"
+export PATH=$PATH:~/.local/bin
+```
+
+## Usage
+To increase brightness, contrast, sharpness by 20% and resize all images (landscape, portrait)
+to max dimension of 1920 pixels (using largest dimension. hight or width) and compress them to 60%
+in a given folder while keeping the same aspect ratio.
+
+``` bash
+pillowcover -d /home/user/Pictures/ -b 1.20 -c 1.20 -s 1.20 -r 1920 -q 60
+```
+
+To crop (640*480 from the left corner) and convert (from png to jpg) all images in a folder.
+``` bash
+plc -d /home/user/Pictures/ -k "0,0,640,480" -x jpg
+```
+
+## Usage Options
+``` bash
+usage: plc [-h] [-v] [-i IMAGE_FILE] [-d IMAGE_DIR] [-o OUTPUT_DIR]
+           [-b BRIGHTNESS] [-c CONTRAST] [-s SHARPNESS] [-r RESIZE]
+           [-R RESIZE_RATIO] [-k CROP] [-q COMPRESSION] [-x EXTENSION]
+
+PillowCover: Mass Manipulate Images Using Python: Change Brightness, Contrast,
+Sharpness, Resize, Compress, Crop and Convert. A simple script (wrapper) using
+pillow. Licensed Under: GNU General Public License v3 or later (GPLv3+)
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --version         show program\'s version number and exit
+  -i IMAGE_FILE, --img IMAGE_FILE
+                        Path to the image file to edit
+  -d IMAGE_DIR, --dir IMAGE_DIR
+                        Path to the directory containing the image files
+  -o OUTPUT_DIR, --out-dir OUTPUT_DIR
+                        Path to output directory to store edited images. by
+                        default they get stored in (parent
+                        director)/pillowcover-output
+  -b BRIGHTNESS, --brightness BRIGHTNESS
+                        Change brightness level, 1.00 is the current value
+  -c CONTRAST, --contrast CONTRAST
+                        Change contrast level, 1.00 is the current value
+  -s SHARPNESS, --sharpness SHARPNESS
+                        Change sharpness level, 1.00 is the current value
+  -r RESIZE, --resize RESIZE
+                        Resize img, keep ratio. provide the maximum
+                        length/width value as one number. e.g pillowcase.py
+                        img -r 1920
+  -R RESIZE_RATIO, --resize-ratio RESIZE_RATIO
+                        Resize img by providing new width and length e.g
+                        pillowcase.py img -R "640 480"
+  -k CROP, --crop CROP  Crop img by providing starting point then width and
+                        length from it e.g pillowcase.py img -k "0,0,640,480"
+  -q COMPRESSION, --compression-quality COMPRESSION
+                        Compression quality 1-100, 100 means no compression at
+                        all
+  -x EXTENSION, --extension EXTENSION
+                        Specifiy the output extension. Can be 'jpg', 'jpeg',
+                        'png' Default is same as the input file's. e.g
+                        pillowcase.py -d imgdir -x "jpg"
+
+
+```
---
+++ packages/pillowcover/pillowcover-1.2.1.tar.gz/pillowcover-1.2.1/pillowcover/pillowcover.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import os
+import sys
+
+from PIL import Image, ImageEnhance, ImageFilter
+
+from .__init__ import __license__, __version__
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="PillowCover: Mass Manipulate Images Using Python: Change Brightness, Contrast,"
+        " Sharpness, Resize, Compress, Crop and Convert. A simple script (wrapper) using pillow."
+        " Licensed Under: {}".format(__license__)
+    )
+    parser.add_argument("-v", "--version", action="version", version="pollowcover " + __version__)
+    parser.add_argument("-i", "--img", dest="image_file", help="Path to the image file to edit", type=str)
+    parser.add_argument(
+        "-d", "--dir", dest="image_dir", help="Path to the directory containing the image files", type=str,)  # widget="DirChooser")
+    parser.add_argument(
+        "-o",
+        "--out-dir",
+        dest="output_dir",
+        help="Path to output directory to store edited images.\
+        by default they get stored in (parent director)/pillowcover-output",
+        type=str,
+    )
+    parser.add_argument(
+        "-b",
+        "--brightness",
+        dest="brightness",
+        help="Change brightness level, 1.00 is the current value",
+        type=float,
+    )
+    parser.add_argument(
+        "-c",
+        "--contrast",
+        dest="contrast",
+        help="Change contrast level, 1.00 is the current value",
+        type=float,
+    )
+    parser.add_argument(
+        "-s",
+        "--sharpness",
+        dest="sharpness",
+        help="Change sharpness level, 1.00 is the current value",
+        type=float,
+    )
+    parser.add_argument(
+        "-r",
+        "--resize",
+        dest="resize",
+        help="Resize img, keep ratio. provide the maximum length/width\
+        value as one number. e.g pillowcase.py img -r 1920",
+        type=int,
+    )
+    parser.add_argument(
+        "-R",
+        "--resize-ratio",
+        dest="resize_ratio",
+        help='Resize img by providing new width and length\
+        e.g pillowcase.py img -R "640 480"',
+        type=str,
+    )
+    parser.add_argument(
+        "-k",
+        "--crop",
+        dest="crop",
+        help='Crop img by providing starting point then width and length from it\
+        e.g pillowcase.py img -k "0,0,640,480"',
+        type=str,
+    )
+
+    parser.add_argument(
+        "-q",
+        "--compression-quality",
+        dest="compression",
+        help="Compression quality 1-100, 100 means no compression at all",
+        type=int,
+    )
+
+    parser.add_argument(
+        "-x",
+        "--extension",
+        help="Specifiy the output extension. Can be 'jpg', 'jpeg', 'png'\
+        Default is same as the input file's. e.g pillowcase.py -d imgdir -x \"jpg\"",
+        type=str,
+    )
+
+    args = parser.parse_args()
+
+    run(
+        args.image_file,
+        args.image_dir,
+        args.output_dir,
+        args.brightness,
+        args.contrast,
+        args.sharpness,
+        args.resize,
+        args.resize_ratio,
+        args.crop,
+        args.compression,
+        args.extension,
+    )
+
+
+def adjust_brightness(image, brightness):
+    enhancer_object = ImageEnhance.Brightness(image)
+    return enhancer_object.enhance(brightness)
+
+
+def adjust_contrast(image, contrast):
+    enhancer_object = ImageEnhance.Contrast(image)
+    return enhancer_object.enhance(contrast)
+
+
+def adjust_sharpness(image, sharpness):
+    enhancer_object = ImageEnhance.Sharpness(image)
+    return enhancer_object.enhance(sharpness)
+
+
+def resize_img(image, max_dimension):
+    size = max_dimension, max_dimension  # tuple of (max_width, max_height)
+    image.thumbnail(size, Image.ANTIALIAS)
+
+
+def resize_img_ratio(image, dimensions):
+    width = int(dimensions.split()[0])
+    height = int(dimensions.split()[1])
+    resized_img = image.resize((width, height))
+    return resized_img
+
+
+def crop_img(image, dimensions):
+    dimensions_list = [int(i) for i in dimensions.split(',')]
+    dimensions_tuple = tuple(dimensions_list)
+    cropped_img = image.crop(dimensions_tuple)
+    return cropped_img
+
+
+def sharperner(input_image, output_image):
+    with Image.open(input_image) as image:
+        try:
+            image.filter(ImageFilter.SHARPEN)
+        except ValueError:
+            print("Exception while trying to sharpen img")
+            pass  # skip filtering for images which do not support it
+        image.save(output_image)
+
+
+def run(
+        image_file,
+        image_dir,
+        output_dir,
+        brightness,
+        contrast,
+        sharpness,
+        resize,
+        resize_ratio,
+        crop,
+        compression,
+        extension):
+    if not image_dir and not image_file:
+        print("directory containing images with '-d' or individual image with '-i' required")
+        sys.exit(1)
+    if image_dir:
+        if not os.path.isdir(image_dir):
+            print("directory does not exist, make sure it's the full path")
+            sys.exit(1)
+
+        # get full path, "Downloads" will turn to "/home/user/Downloads/"
+        dir_path = os.path.abspath(image_dir) + "/"  # glob needs "/"
+        extensions = ("*.png", "*.jpg", "*.jpeg", "*.PNG", "*.JPG", "*.JPEG")
+        # all_imgs = []
+        # for extension in extensions:
+        #     all_imgs.extend(glob.glob(dir + extension))   # same as following
+        all_imgs = []
+        [all_imgs.extend(glob.glob(dir_path + ext)) for ext in extensions]
+
+        if not all_imgs:
+            print("directory exists but no image was found in it")
+            sys.exit(1)
+    elif image_file:
+        if not os.path.isfile(image_file):
+            print("provided image not exist, make sure it's the full path")
+            sys.exit(1)
+        all_imgs = [image_file]
+
+    for img in all_imgs:
+        if not extension:
+            extension = os.path.splitext(os.path.basename(img))[1]
+        # remove the . in .jpg for example
+        if extension[0] == ".":
+            extension = extension[1:]
+
+        # if resizing, include resize value in img name
+        if resize:
+            img_file_name = "{}_{}.{}".format(
+                os.path.splitext(os.path.basename(img))[0],
+                str(resize),
+                extension,
+            )
+        else:
+            img_file_name = "{}.{}".format(os.path.splitext(os.path.basename(img))[0], extension)
+        print(img_file_name)
+        # if output_dir not provided, save images in (parent)/pillowcover-output folder
+        if not output_dir:
+            output_dir = os.path.join(os.path.dirname(os.path.abspath(img)), "pillowcover-output")
+
+        # create it if doesn't exist
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+
+        new_name = os.path.join(output_dir, img_file_name)
+        with Image.open(img) as image:
+            if crop:
+                image = crop_img(image, crop)
+            if brightness:
+                image = adjust_brightness(image, brightness)
+            if contrast:
+                image = adjust_contrast(image, contrast)
+            if sharpness:
+                image = adjust_sharpness(image, sharpness)
+            if resize:
+                resize_img(image, resize)
+            if resize_ratio:
+                image = resize_img_ratio(image, resize_ratio)
+
+            # converting PNG to JPG, fixes error
+            if extension.lower() == "jpg" and image.mode in ("RGBA", "P"):
+                image = image.convert("RGB")
+
+            if compression:
+                image.save(new_name, quality=compression)
+                print("saving image {}".format(new_name))
+            else:
+                image.save(new_name)
+                print("saving image {}".format(new_name))
+                # image.show()
+
+
+if __name__ == "__main__":
+    main()
