d5245e3a11169218965ae4edddcbb097c55eb774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:04 +0000

    Add package renku/0.5.3.dev6/renku-0.5.3.dev6.tar.gz

---
+++ packages/renku/renku-0.5.3.dev6.tar.gz/renku-0.5.3.dev6/renku/models/migrations/dataset.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017-2019 - Swiss Data Science Center (SDSC)
+# A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
+# Eidgenössische Technische Hochschule Zürich (ETHZ).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Migrations for dataset."""
+
+
+def migrate_dataset(data):
+    """Migrate from old dataset formats."""
+    if data.get('@type') != 'dctypes:Dataset':
+        return data
+
+    data['creator'] = data.pop('authors', {})
+
+    for file_name, file_ in data.get('files', {}).items():
+        file_['creator'] = file_.pop('authors', {})
+
+    return data
