0ddd844194ee15f04a8049c2133c3fe8a3530e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:14 +0000

    Add package polarishub-flask/0.1.5/polarishub_flask-0.1.5.tar.gz

---
+++ packages/polarishub-flask/polarishub_flask-0.1.5.tar.gz/polarishub_flask-0.1.5/polarishub_flask/server/file_handler.py
@@ -0,0 +1,59 @@
+import os, sys, json
+from flask import abort
+settings = {}
+
+def load_settings():
+    with open(os.path.join(os.getcwd(), 'server', 'settings.json')) as f:
+        return json.load(f)
+
+
+def get_settings():
+    global settings
+    if settings == {} or settings is None:
+        settings = load_settings()
+    return settings
+
+def save_settings():
+    try:
+        with open(os.path.join(os.getcwd(), 'server', 'settings.json'), 'w') as f:
+            json.dump(settings, f)
+        return True
+    except:
+        return False
+
+def get_dir(path):
+    if os.path.isdir(path):
+        path_list = os.listdir(path)
+        # print (path_list, path)
+        print(os.getcwd())
+        path_list = [(path_list[i], os.path.isfile(os.path.join(path, path_list[i])), os.path.join(path[len(os.getcwd()):], path_list[i])) for i in range(len(path_list))]
+        print ("path_list", path_list)
+        return path_list
+    else:
+        abort(404)
+
+keys = {
+    'username': lambda name:len(name)>0
+    } 
+def update_settings(new_settings):
+    global settings
+    print(new_settings)
+    # for items in new_settings:
+        # print (items)
+    for key, value in new_settings.items():
+        # print(items)
+        # key, value = items
+        print ((key, value))
+        if key in keys.keys() and keys[key](value):
+            print ("key gets:", key)
+            print (key, value)
+            if value:
+                settings[key] = value
+        else:
+            settings = load_settings()
+            return False
+
+    if save_settings():
+        return True
+    else:
+        return False
