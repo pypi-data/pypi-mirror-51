99b6777e0e2d5db7105d3c20c89b171dc84d780a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:41 +0000

    Add package nowyou-grafits-script-python/0.1/nowyou_grafits_script_python-0.1-py3-none-any.whl

---
+++ packages/nowyou-grafits-script-python/nowyou_grafits_script_python-0.1-py3-none-any.whl/ui_tools/UI_tools.py
@@ -0,0 +1,29 @@
+import pyautogui as pyautogui
+import string
+import cv2 # locate image confidece support
+from utils.utils import resource_path
+
+secs_between_keys = 0.1
+
+class Ui_tools:
+
+    @staticmethod
+    def getPosition(image: string):
+        return pyautogui.locateCenterOnScreen(image, grayscale=True, confidence=.9)
+
+    @staticmethod
+    def click(position: tuple):
+        pyautogui.click(position[0], position[1], 1, button='left')
+
+    @staticmethod
+    def click_on(ref: str):
+        position = Ui_tools.getPosition(resource_path(ref))
+        Ui_tools.click(position)
+
+    @staticmethod
+    def hot_key(*arg):
+        pyautogui.hotkey(arg)
+
+    @staticmethod
+    def type(text: string):
+        pyautogui.typewrite([text], interval=secs_between_keys)
