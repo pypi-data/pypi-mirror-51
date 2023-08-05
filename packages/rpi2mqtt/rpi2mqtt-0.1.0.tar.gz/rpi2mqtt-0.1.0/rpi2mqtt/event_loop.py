15fe6c483d8afc37aa496dc0afe7cf946109d947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:41 +0000

    Add package rpi2mqtt/0.1.0/rpi2mqtt-0.1.0.tar.gz

---
+++ packages/rpi2mqtt/rpi2mqtt-0.1.0.tar.gz/rpi2mqtt-0.1.0/rpi2mqtt/event_loop.py
@@ -0,0 +1,26 @@
+# import asyncio
+from rpi2mqtt.config import config
+import RPi.GPIO as GPIO
+from rpi2mqtt.binary import *
+from rpi2mqtt.temperature import *
+import time
+import rpi2mqtt.mqtt as mqtt
+
+
+def main():
+    sensor_list = []
+
+    for sensor in config.sensors:
+        s = DHT(sensor.pin, sensor.topic, sensor.name, 'sensor', sensor.type)
+        # s.setup()
+        sensor_list.append(s)
+
+    while True:
+        for sensor in sensor_list:
+            sensor.callback()
+
+        time.sleep(300)
+
+
+if __name__ == '__main__':
+    main()
