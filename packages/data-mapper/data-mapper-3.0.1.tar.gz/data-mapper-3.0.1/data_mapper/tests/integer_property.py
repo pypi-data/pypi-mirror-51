f299c1abf84d510b80ce2677d074b77c674f81e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package data-mapper/3.0.1/data-mapper-3.0.1.tar.gz

---
+++ packages/data-mapper/data-mapper-3.0.1.tar.gz/data-mapper-3.0.1/data_mapper/tests/integer_property.py
@@ -0,0 +1,42 @@
+from unittest import TestCase
+
+from data_mapper.properties.integer import IntegerProperty
+
+
+class IntegerPropertyTests(TestCase):
+    def _test(self, prop, data, expected):
+        result = prop.get(data)
+        self.assertEqual(expected, result)
+
+    def test__int(self):
+        self._test(
+            prop=IntegerProperty('x'),
+            data=dict(x=1),
+            expected=1,
+        )
+
+    def test__str(self):
+        self._test(
+            prop=IntegerProperty('x'),
+            data=dict(x='1'),
+            expected=1,
+        )
+
+    def test__float(self):
+        self._test(
+            prop=IntegerProperty('x'),
+            data=dict(x=1.1),
+            expected=1,
+        )
+
+    def test__none(self):
+        self._test(
+            prop=IntegerProperty('x', required=False),
+            data=dict(x=None),
+            expected=None,
+        )
+        self._test(
+            prop=IntegerProperty('x', required=False),
+            data=dict(),
+            expected=None,
+        )
