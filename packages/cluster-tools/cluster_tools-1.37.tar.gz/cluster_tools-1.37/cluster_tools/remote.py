147d4cdc09eec128f7e080ab6395e88db08ded6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package cluster-tools/1.37/cluster_tools-1.37.tar.gz

---
+++ packages/cluster-tools/cluster_tools-1.37.tar.gz/cluster_tools-1.37/cluster_tools/remote.py
@@ -0,0 +1,79 @@
+"""Tools for executing remote commands."""
+import sys
+import os
+import traceback
+from . import pickling
+import logging
+from cluster_tools.schedulers.slurm import SlurmExecutor
+from cluster_tools.schedulers.pbs import PBSExecutor
+from .file_formatters import format_infile_name, format_outfile_name
+
+def format_remote_exc():
+    typ, value, tb = sys.exc_info()
+    tb = tb.tb_next  # Remove root call to worker().
+    return "".join(traceback.format_exception(typ, value, tb))
+
+
+def worker(workerid, cfut_dir):
+    """Called to execute a job on a remote host."""
+    executor = get_executor_class()
+    try:
+        input_file_name = format_infile_name(cfut_dir, workerid)
+        print("trying to read: ", input_file_name)
+        print("working dir: ", os.getcwd())
+        with open(input_file_name, "rb") as f:
+            indata = f.read()
+        fun, args, kwargs, meta_data = pickling.loads(indata)
+        setup_logging(meta_data)
+        
+        logging.info("Job computation started (jobid={}, workerid={}).".format(executor.get_current_job_id(), workerid))
+        result = True, fun(*args, **kwargs)
+        logging.info("Job computation completed.")
+        out = pickling.dumps(result)
+
+    except Exception as e:
+        print(traceback.format_exc())
+
+        result = False, format_remote_exc()
+        logging.info("Job computation failed.")
+        out = pickling.dumps(result)
+
+    destfile = format_outfile_name(cfut_dir, workerid)
+    tempfile = destfile + ".tmp"
+    with open(tempfile, "wb") as f:
+        f.write(out)
+    logging.info("Pickle file written to {}.".format(tempfile))
+    os.rename(tempfile, destfile)
+    logging.info("Pickle file renamed to {}.".format(destfile))
+
+
+def setup_logging(meta_data):
+    logging_config = meta_data.get("logging_config", {"level": logging.DEBUG, "format": "%(asctime)s %(levelname)s %(message)s"})
+
+    # Call basicConfig which is necessary for the logging to work.
+    logging.basicConfig(**logging_config)
+
+    # It can happen that the pickled logger was already initialized. In this case,
+    # the above basicConfig call was a noop. Therefore, we have to set the level explicitly.
+    logger = logging.getLogger()
+    if "level" in logging_config:
+        logger.setLevel(logging_config["level"])
+
+    logging.info("Setting up logging.basicConfig (potentially overwriting logging configuration of the main script). Config: {}".format(logging_config))
+    logging.info("Starting job computation...")
+
+
+def get_executor_class():
+    for executor in [SlurmExecutor, PBSExecutor]:
+        if executor.get_current_job_id() is not None:
+            return executor
+
+if __name__ == "__main__":
+    worker_id = sys.argv[1]
+    cfut_dir = sys.argv[2]
+
+    job_array_index = get_executor_class().get_job_array_index()
+    if job_array_index is not None:
+        worker_id = worker_id + "_" + job_array_index
+
+    worker(worker_id, cfut_dir)
