7c1adc5071c249c51a4919f8f2d86b1ebb47bed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:45 +0000

    Add package meta-ml/0.0.14/meta-ml-0.0.14.tar.gz

---
+++ packages/meta-ml/meta-ml-0.0.14.tar.gz/meta-ml-0.0.14/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name="meta-ml",
+    version="0.0.14",
+    description="MetaRL-based Estimator using Task-encodings for AutoML",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/cosmicBboy/ml-research/tree/master/metalearn",
+    packages=[
+        "metalearn",
+        "metalearn.components",
+        "metalearn.data_environments",
+        "metalearn.data_environments.feature_maps",
+        "metalearn.inference"],
+    install_requires=[
+        "colorlover",
+        "click==6.7",
+        "dash",
+        "dash-core-components",
+        "dash-html-components",
+        "dill",
+        "floyd-cli",
+        "kaggle",
+        "matplotlib",
+        "numpy",
+        "openml==0.9.0",
+        "pandas==0.24.2",
+        "pynisher",
+        "torch==1.2.0",
+        "scikit-learn==0.21.2",
+        "scipy",
+        "yamlordereddictloader",
+    ],
+    scripts=["bin/metalearn"],
+)
