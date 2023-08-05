3d04889da5cae6cc11dde500abf8f33c61437ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package gurux-net/1.0.7/gurux_net-1.0.7.tar.gz

---
+++ packages/gurux-net/gurux_net-1.0.7.tar.gz/gurux_net-1.0.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gurux_net",
+    version="1.0.7",
+    author="Gurux Ltd",
+    author_email="gurux@gurux.org",
+    description="Gurux network media is used to commmunication with TCP/IP or UDP connections.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gurux/gurux.net.python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['gurux-common'],
+)
