b785dfdf69d5d885c94a10f1fb59e469ae402705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:02 +0000

    Add package simple-latex/0.0.6/simple_latex-0.0.6-py3-none-any.whl

---
+++ packages/simple-latex/simple_latex-0.0.6-py3-none-any.whl/simple_latex/__init__.py
@@ -0,0 +1,16 @@
+from .latex_preamble import Documentclass, Definition, Preamble, Package, RenewCommand, NewCommand
+from .latex_document import Document, BeginClass, EndClass, MathClass
+from .latex_universal import Command
+from .simple_latex_document import SimpleLatexDocument
+all = ['Documentclass',
+       'Command',
+       'Definition',
+       'Preamble',
+       'Package',
+       'RenewCommand',
+       'NewCommand',
+       'Document',
+       'BeginClass',
+       'EndClass',
+       'MathClass', 
+       'SimpleLatexDocument']
---
+++ packages/simple-latex/simple_latex-0.0.6-py3-none-any.whl/simple_latex/latex_document/__init__.py
@@ -0,0 +1,6 @@
+from .begin import BeginClass
+from .end import EndClass
+from .document import Document
+from .math import MathClass
+
+all = ['BeginClass', 'EndClass', 'Document', 'MathClass']
