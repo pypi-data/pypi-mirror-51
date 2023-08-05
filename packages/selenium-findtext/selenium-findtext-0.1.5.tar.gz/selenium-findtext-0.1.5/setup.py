e608a16f8e69d9f4f24d659450a107d825cfc8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package selenium-findtext/0.1.5/selenium-findtext-0.1.5.tar.gz

---
+++ packages/selenium-findtext/selenium-findtext-0.1.5.tar.gz/selenium-findtext-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='selenium-findtext',
+    version='0.1.5',
+    description='Text finding helpers for Selenium tests',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://github.com/peaqe/findtext',
+    author='Calvin Spealman',
+    author_email='cspealma@redhat.com',
+    license='MIT',
+    py_modules=['findtext'],
+    zip_safe=True,
+)
