73a7a83b5cf1a4ac4724e5a16cee2ccd0152f203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:10 +0000

    Add package authl/0.2.0/Authl-0.2.0.tar.gz

---
+++ packages/authl/Authl-0.2.0.tar.gz/Authl-0.2.0/authl/handlers/test_handler.py
@@ -0,0 +1,37 @@
+""" Test handler which always returns an immediate success. Not to be used
+in production. """
+
+from .. import disposition
+from . import Handler
+
+
+class TestHandler(Handler):
+    """ An Authl handler which always returns True for any URI beginning with
+    'test:'. Primarily for testing purposes. """
+
+    def handles_url(self, url):
+        if url.startswith('test:'):
+            return url
+        return None
+
+    def initiate_auth(self, id_url, callback_url):
+        return disposition.Verified(id_url)
+
+    def check_callback(self, url, get, data):
+        return disposition.Error("This shouldn't be possible")
+
+    @property
+    def cb_id(self):
+        return 'TEST_DO_NOT_USE'
+
+    @property
+    def service_name(self):
+        return 'Loopback'
+
+    @property
+    def url_schemes(self):
+        return [('test:%', 'example')]
+
+    @property
+    def description(self):
+        return """Used for testing purposes. Don't use this on a production website."""
