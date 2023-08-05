f0e3a2e9227b0eb219e8144c69011382623445bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:12 +0000

    Add package Flask-Beet/0.0.4/Flask-Beet-0.0.4.tar.gz

---
+++ packages/Flask-Beet/Flask-Beet-0.0.4.tar.gz/Flask-Beet-0.0.4/CHANGELOG.md
@@ -0,0 +1,23 @@
+# Changelog
+Note: version releases in the 0.x.y range may introduce breaking changes.
+
+## 0.0.5
+
+- patch: Ensure we use the proper blueprint in our templates
+
+## 0.0.4
+
+- patch: Ensure the MANIFEST contains the templates, scripts and styles
+
+## 0.0.3
+
+- patch: Improve Makefile to work with semver
+
+## 0.0.2
+
+- patch: Improve release script
+
+## 0.0.1
+
+- patch: Update README.md and Makefile to reflect the use of semversioner
+
---
+++ packages/Flask-Beet/Flask-Beet-0.0.4.tar.gz/Flask-Beet-0.0.4/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+__version__ = "0.0.4"
+
+setup(
+    name="Flask-Beet",
+    version=__version__,
+    url="http://bitshares.eu/",
+    license="MIT",
+    author="Fabian Schuh",
+    author_email="fabian@chainsquad.com",
+    description="A flask extension that allows login via Beet app",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["flask_beet"],
+    zip_safe=False,
+    include_package_data=True,
+    platforms="any",
+    install_requires=[open("requirements.txt").readlines()],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
