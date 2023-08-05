bbf45c32fc01ebcce3ca576b342264a66d6a9608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package shift-tool/3.1.0/shift-tool-3.1.0.tar.gz

---
+++ packages/shift-tool/shift-tool-3.1.0.tar.gz/shift-tool-3.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='shift-tool',
+    version='3.1.0',
+    packages=["shift2"],
+    description='Not an encryption tool',
+    url='https://github.com/Ewpratten/shift',
+    author='Evan Pratten',
+    author_email='ewpratten@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ),
+    entry_points={
+        'console_scripts': [
+            'shift2 = shift2.__main__:main'
+        ]
+    })
---
+++ packages/shift-tool/shift-tool-3.1.0.tar.gz/shift-tool-3.1.0/shift2/__main__.py
@@ -0,0 +1,36 @@
+from . import shcrypt
+import argparse
+import os
+import sys
+
+def main():
+    # Load args
+    parser = argparse.ArgumentParser(description="Keyed text encoding")
+    parser.add_argument("file", help="Input file")
+    parser.add_argument("key", help="encode/decode key")
+    parser.add_argument("-d", "--decode", help="switch to decoding mode", action="store_true", default=False)
+    args = parser.parse_args()
+    
+    # Convert the key to something easier to work with
+    shifts = shcrypt.key2shifts(args.key)
+
+    # Load the file, and convert to b64
+    if not os.path.exists(args.file):
+        print("File not found", file=sys.stderr)
+        exit(1)
+
+    with open(args.file, "r") as fp:
+        file = fp.read()
+        fp.close()
+
+    # Pass file through correct modifier
+    if not args.decode:
+        output = shcrypt.encode(file, shifts)
+    else:
+        output = shcrypt.decode(file, shifts)
+
+    # Print out the output for piping
+    print(output)
+
+if __name__ == "__main__":
+    main()
