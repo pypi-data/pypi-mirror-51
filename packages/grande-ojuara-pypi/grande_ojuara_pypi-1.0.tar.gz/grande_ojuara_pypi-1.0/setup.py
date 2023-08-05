a62ac7a4875ec13b58fa26716e1fe069e02ec09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:36 +0000

    Add package grande-ojuara-pypi/1.0/grande_ojuara_pypi-1.0.tar.gz

---
+++ packages/grande-ojuara-pypi/grande_ojuara_pypi-1.0.tar.gz/grande_ojuara_pypi-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='grande_ojuara_pypi',
+	version='1.0',
+    author='Daniel, William, Udglylania, Ariany e Luan',
+    author_email='ojuara@araujo.com',
+	description='Brincando com distribuição python',
+	py_modules = ['ojuara']
+)
