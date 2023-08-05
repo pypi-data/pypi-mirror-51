b84e9ee6683ca1c052a62766631301d53c322e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:40 +0000

    Add package artellapipe/0.0.11/artellapipe-0.0.11.tar.gz

---
+++ packages/artellapipe/artellapipe-0.0.11.tar.gz/artellapipe-0.0.11/source/artellapipe/core/__init__.py
@@ -0,0 +1,9 @@
+order = [
+    'artellapipe.core.abstract',
+    'artellapipe.core.artellaclasses',
+    'artellapipe.core.artellalib',
+    'artellapipe.core.assetinfo',
+    'artellapipe.core.assetfile',
+    'artellapipe.core.asset',
+    'artellapipe.core.syncdialog'
+]
