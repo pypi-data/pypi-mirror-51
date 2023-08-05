a208909dd58a1f3e38ba0a3c3633e8a984d67239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:25 +0000

    Add package aio-pika/6.0.0/aio-pika-6.0.0.tar.gz

---
+++ packages/aio-pika/aio-pika-6.0.0.tar.gz/aio-pika-6.0.0/aio_pika/tools.py
@@ -0,0 +1,65 @@
+import asyncio
+from functools import wraps
+
+
+__all__ = 'create_task', 'iscoroutinepartial', 'shield'
+
+
+def iscoroutinepartial(fn):
+    """
+    Function returns True if function it's a partial instance of coroutine.
+    See additional information here_.
+
+    :param fn: Function
+    :return: bool
+
+    .. _here: https://goo.gl/C0S4sQ
+
+    """
+
+    while True:
+        parent = fn
+
+        fn = getattr(parent, 'func', None)
+
+        if fn is None:
+            break
+
+    return asyncio.iscoroutinefunction(parent)
+
+
+def create_task(func, *args, loop=None, **kwargs):
+    loop = loop or asyncio.get_event_loop()
+
+    if iscoroutinepartial(func):
+        return loop.create_task(func(*args, **kwargs))
+
+    def run(future):
+        if future.done():
+            return
+
+        try:
+            future.set_result(func(*args, **kwargs))
+        except Exception as e:
+            future.set_exception(e)
+
+        return future
+
+    future = loop.create_future()
+    loop.call_soon(run, future)
+    return future
+
+
+def shield(func):
+    """
+    Simple and useful decorator for wrap the coroutine to `asyncio.shield`.
+    """
+
+    async def awaiter(future):
+        return await future
+
+    @wraps(func)
+    def wrap(*args, **kwargs):
+        return wraps(func)(awaiter)(asyncio.shield(func(*args, **kwargs)))
+
+    return wrap
