4bd8e0a7dc65c76c4bd8fdc1fdb00b5aa991b67a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:45:26 +0000

    Add package semvecpy/0.1.2/semvecpy-0.1.2.tar.gz

---
+++ packages/semvecpy/semvecpy-0.1.2.tar.gz/semvecpy-0.1.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="semvecpy",
+    version="0.1.2",
+    author="Semantic Vectors Authors",
+    author_email="semanticvectors@googlegroups.com",
+    description="Semantic Vectors work in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/semanticvector/semvecpy",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "bitarray",
+        "numpy",
+    ],
+)
