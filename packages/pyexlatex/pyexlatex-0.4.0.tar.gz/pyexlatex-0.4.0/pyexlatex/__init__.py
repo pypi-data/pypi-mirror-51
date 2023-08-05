fe2edbde164b4e16d1e47b41262824c863fcf9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:15 +0000

    Add package pyexlatex/0.4.0/pyexlatex-0.4.0.tar.gz

---
+++ packages/pyexlatex/pyexlatex-0.4.0.tar.gz/pyexlatex-0.4.0/pyexlatex/__init__.py
@@ -0,0 +1,26 @@
+from pyexlatex.models.document import Document
+from pyexlatex.models.equation import Equation
+from pyexlatex.models.section.paragraphs import Paragraph, SubParagraph
+from pyexlatex.models.section.sections import Section, SubSection, SubSubSection
+from pyexlatex.models.section.appendix import Appendix
+from pyexlatex.models.lists.unordered import UnorderedList
+from pyexlatex.models.lists.ordered import OrderedList
+from pyexlatex.models.format.raw import Raw
+from pyexlatex.models.format.text.text import Text
+from pyexlatex.models.ref import Ref
+from pyexlatex.models.footnote import Footnote
+from pyexlatex.models.references.bibtex.article import BibTexArticle
+from pyexlatex.models.references.bibtex.misc import BibTexMisc
+from pyexlatex.models.references.bibtex.manual import BibTexManual
+from pyexlatex.models.references.bibliography import Bibliography
+from pyexlatex.models.references.citations import Cite, CiteP, CiteT
+from pyexlatex.models.format.text.color import TextColor
+from pyexlatex.models.format.text.monospace import Monospace
+from pyexlatex.models.format.text.underline import Underline
+from pyexlatex.models.format.text.bold import Bold
+from pyexlatex.models.format.text.italics import Italics
+from pyexlatex.models.format.code.python import Python
+from pyexlatex.figure.models.graphic import Graphic
+from pyexlatex.models.format.vfill import VFill
+from pyexlatex.models.hyperlinks import Hyperlink
+from pyexlatex.models.package import Package
---
+++ packages/pyexlatex/pyexlatex-0.4.0.tar.gz/pyexlatex-0.4.0/pyexlatex/models/title/page.py
@@ -0,0 +1,26 @@
+from pyexlatex.models.documentitem import DocumentItem
+from pyexlatex.models.title.title import Title
+from pyexlatex.models.credits.author import Author
+from pyexlatex.models.date import Date
+from pyexlatex.models.section.abstract import Abstract
+from pyexlatex.texgen import _maketitle_str
+
+class TitlePage(DocumentItem):
+
+    def __init__(self, title: str = None, author: str = None, date: str = None, abstract: str = None):
+        from pyexlatex.logic.builder import _build
+
+        contents = [
+            Title(title) if title is not None else None,
+            Author(author) if author is not None else None,
+            Date(date) if date is not None else Date(),
+            _maketitle_str(),
+            Abstract(abstract) if abstract is not None else None
+        ]
+
+        self.contents = [content for content in contents if content is not None]
+
+        self._output = _build(self.contents)
+
+    def __str__(self):
+        return self._output
