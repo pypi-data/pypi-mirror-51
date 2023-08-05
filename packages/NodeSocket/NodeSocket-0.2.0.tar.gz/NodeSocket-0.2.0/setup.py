a40fd209ad92c8d6cbcd980be4db0973307f9d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package NodeSocket/0.2.0/NodeSocket-0.2.0.tar.gz

---
+++ packages/NodeSocket/NodeSocket-0.2.0.tar.gz/NodeSocket-0.2.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="NodeSocket",
+    version="0.2.0",
+    author="Jeremy Kanovsky",
+    author_email="kanovsky.jeremy@gmail.com",
+    description="A lightweight Node.js ↔ Python data socket",
+    url="https://github.com/0xJeremy/socket.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
