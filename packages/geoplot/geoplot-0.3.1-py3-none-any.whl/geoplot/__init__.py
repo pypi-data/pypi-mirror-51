73b861623d483b4e20cf85b7703fc47b963cae13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:05 +0000

    Add package geoplot/0.3.1/geoplot-0.3.1-py3-none-any.whl

---
+++ packages/geoplot/geoplot-0.3.1-py3-none-any.whl/geoplot/__init__.py
@@ -0,0 +1,15 @@
+from .geoplot import (
+    pointplot, polyplot, choropleth, cartogram, kdeplot, sankey, voronoi, quadtree, webmap,
+    __version__
+)
+from .crs import (
+    PlateCarree, LambertCylindrical, Mercator, Miller, Mollweide, Robinson, Sinusoidal,
+    InterruptedGoodeHomolosine, Geostationary, NorthPolarStereo, SouthPolarStereo, Gnomonic,
+    AlbersEqualArea, AzimuthalEquidistant, LambertConformal, Orthographic, Stereographic,
+    TransverseMercator, LambertAzimuthalEqualArea, UTM, OSGB, EuroPP, OSNI, WebMercator
+)
+from .utils import (
+    gaussian_points, gaussian_polygons, gaussian_multi_polygons, uniform_random_global_points,
+    uniform_random_global_network, classify_clusters
+)
+from .datasets import get_path
