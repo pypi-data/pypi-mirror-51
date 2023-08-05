6934809bcb9be080f00b37182e743f63b9142cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:00 +0000

    Add package bem/0.1.1/bem-0.1.1.tar.gz

---
+++ packages/bem/bem-0.1.1.tar.gz/bem-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='bem',
+      version='0.1.1',
+      description='Random forest for exoplanets',
+      url='https://github.com/soleneulmer/bem',
+      author='Solène Ulmer-Moll',
+      author_email='solene.ulmer-moll@astro.up.pt',
+      license='MIT',
+      packages=['bem'])
