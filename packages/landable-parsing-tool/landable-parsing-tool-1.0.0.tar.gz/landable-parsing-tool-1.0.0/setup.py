49f6879902fdc9f34749d56ad5d4777d94b30403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package landable-parsing-tool/1.0.0/landable-parsing-tool-1.0.0.tar.gz

---
+++ packages/landable-parsing-tool/landable-parsing-tool-1.0.0.tar.gz/landable-parsing-tool-1.0.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='landable-parsing-tool',
+    version='1.0.0',
+    description='Landable parsing tool',
+    author='Omar Diaz',
+    author_email='omar.diaz@crosslend.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+      'pony',
+      'Click',
+      'mysqlclient',
+      'dsnparse',
+      'beautifulsoup4',
+    ],
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'lendable_parse_tool=landable_parse_tool:tool',
+        ]
+    },
+)
