40fa8c1d3441345139536d31ac9490b294cbb204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:48 +0000

    Add package ofx2xlsmbr/0.99.91/ofx2xlsmbr-0.99.91.tar.gz

---
+++ packages/ofx2xlsmbr/ofx2xlsmbr-0.99.91.tar.gz/ofx2xlsmbr-0.99.91/ofx2xlsmbr/reader/OFXReaderBankStatement.py
@@ -0,0 +1,28 @@
+from .IReaderBankStatement import IReaderBankStatement
+from ofx2xlsmbr.model.BankStatement import BankStatement
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class OFXReaderBankStatement(IReaderBankStatement):
+    def read(self, factory, ofx) -> BankStatement:
+        bs = BankStatement()
+
+        logger.info(ofx)
+
+        stmts = ofx.statements
+        
+        # btmts -> bs
+        txs = stmts[0].transactions
+
+        csReader = factory.createReaderCashFlow()
+        for tx in txs:
+            cs = csReader.read(factory, tx)
+            if (cs.value >= 0.0):
+                bs.inflows.append(cs)
+            else:
+                bs.outflows.append(cs)
+
+        return bs
