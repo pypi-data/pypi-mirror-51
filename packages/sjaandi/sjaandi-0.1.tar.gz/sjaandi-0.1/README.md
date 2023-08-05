6fa382cabdb2dc6e20332bc1799cc5a3a8c58c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:41 +0000

    Add package sjaandi/0.1/sjaandi-0.1.tar.gz

---
+++ packages/sjaandi/sjaandi-0.1.tar.gz/sjaandi-0.1/README.md
@@ -0,0 +1,19 @@
+sjaandi
+==============================
+
+ Library for visualizing and understanding predictions of convolutional neural networks.
+
+
+## Getting the Data
+
+Download [Imagenette-160]() using the following shell command:
+
+```python
+python src/data/get_imagenette.py
+```
+
+The data will be available in `data/raw/imagenette-160/`.
+
+--------
+
+<p><small>Project based on the <a target="_blank" href="https://drivendata.github.io/cookiecutter-data-science/">cookiecutter data science project template</a>.</small></p>
---
+++ packages/sjaandi/sjaandi-0.1.tar.gz/sjaandi-0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='sjaandi',
+    version='0.1',
+    scripts=None,
+    author='Max Pechyonkin',
+    author_email='maxim.pechyonkin@gmail.com',
+    description='Library for visual similarity search and visualization',
+    # long_description=long_description,
+    # long_description_content_type='text/markdown',
+    url='https://github.com/pechyonkin/sjaandi',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'fastai',
+        'matplotlib',
+        'numpy',
+        'scikit-learn',
+        'python-dotenv',
+        'click',
+        'rasterfairy @ git+git://github.com/pechyonkin/RasterFairy@python3',
+    ],
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
