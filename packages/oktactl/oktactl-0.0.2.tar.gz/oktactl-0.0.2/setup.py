988cf986ad82bc8a144d53271d0a0bdaaebd9a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:44 +0000

    Add package oktactl/0.0.2/oktactl-0.0.2.tar.gz

---
+++ packages/oktactl/oktactl-0.0.2.tar.gz/oktactl-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(name='oktactl',
+      version='0.0.2',
+      description='Sample Okta CLI tool in python',
+      license='MIT',
+      author='Atulya Kumar Pandey',
+      author_email='atul3015@gmail.com',
+      url='https://github.com/atul3015kr/oktactl',
+      packages=find_packages(),
+      install_requires=[
+          'Click',
+          'requests'
+      ],
+      entry_points='''
+            [console_scripts]
+            oktactl=oktactl.okta:okta
+        ''',
+      zip_safe=True,
+      include_package_data=True
+      )
