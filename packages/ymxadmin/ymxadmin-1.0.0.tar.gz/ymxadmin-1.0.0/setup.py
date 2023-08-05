f3cb84278fe26d9f447c1b338fc56f97d8a7b143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:30 +0000

    Add package ymxadmin/1.0.0/ymxadmin-1.0.0.tar.gz

---
+++ packages/ymxadmin/ymxadmin-1.0.0.tar.gz/ymxadmin-1.0.0/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='ymxadmin',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A city selector for django xadmin',
+    long_description="A xadmin city selector",
+    url='https://www.example.com/',
+    author='wp',
+    author_email='wpsmoly@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
