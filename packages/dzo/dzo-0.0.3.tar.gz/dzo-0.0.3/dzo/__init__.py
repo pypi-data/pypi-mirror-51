3fc7de8de289c2c8f518c6ba7274b073a0e93b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:26 +0000

    Add package dzo/0.0.3/dzo-0.0.3.tar.gz

---
+++ packages/dzo/dzo-0.0.3.tar.gz/dzo-0.0.3/dzo/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""dzo -- Python implemented portable and easy-to-use search engine.
+"""
+import logging
+
+from ._meta import _AUTHOR, _VERSION
+
+
+__version__ = _VERSION
+__author__ = _AUTHOR
+
+DEFAULT_LOG_LEVEL: int = logging.DEBUG
