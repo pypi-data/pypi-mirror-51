2c434fc3674f670504740307312543ac04d61128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:54 +0000

    Add package django-apar/1.1.5.129/django-apar-1.1.5.129.tar.gz

---
+++ packages/django-apar/django-apar-1.1.5.129.tar.gz/django-apar-1.1.5.129/aparnik/packages/educations/urls/api.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+
+app_name='educations'
+
+urlpatterns = [
+    url(r'^books/', include('aparnik.packages.educations.books.api.urls', namespace='books')),
+    url(r'^publishers/', include('aparnik.packages.educations.books.api.urls-publishers', namespace='publishers')),
+    url(r'^writers-translators/', include('aparnik.packages.educations.books.api.urls-writers-translators', namespace='writers-translators')),
+    url(r'^teachers/', include('aparnik.packages.educations.teachers.api.urls', namespace='teachers')),
+    url(r'^courses/', include('aparnik.packages.educations.courses.api.urls', namespace='courses')),
+    url(r'^educations/', include('aparnik.packages.educations.educations.api.urls', namespace='educations')),
+    url(r'^progresses/', include('aparnik.packages.educations.progresses.api.urls', namespace='progresses')),
+]
