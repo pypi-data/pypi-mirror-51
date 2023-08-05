f0920d4d35ee2648c10a87278f6c0aa99e04bab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:12 +0000

    Add package cmdargs/0.1/cmdargs-0.1.tar.gz

---
+++ packages/cmdargs/cmdargs-0.1.tar.gz/cmdargs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cmdargs',
+      version='0.1',
+      description='',
+      url='https://github.com/Mister7F',
+      author='Mister7F',
+      author_email='mister7f@gmail.com',
+      license='MIT',
+      packages=['.'],
+      zip_safe=False)
