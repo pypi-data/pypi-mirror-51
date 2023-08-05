e6d52da2b7ee3ab15e2e4f7f665c946ef8b9f748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:04 +0000

    Add package buzzword/1.2.2/buzzword-1.2.2.tar.gz

---
+++ packages/buzzword/buzzword-1.2.2.tar.gz/buzzword-1.2.2/buzzword/parts/nav.py
@@ -0,0 +1,21 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from buzzword.parts import style
+
+LINKS = [
+    ("User guide", "https://buzzword.readthedocs.io/en/latest/guide/"),
+    ("Creating corpora", "https://buzzword.readthedocs.io/en/latest/building/"),
+    ("Depgrep query syntax", "https://buzzword.readthedocs.io/en/latest/depgrep/"),
+    ("About", "https://buzzword.readthedocs.io/en/latest/about/"),
+]
+
+hrefs = [html.Li([html.A(name, target="_blank", href=url)]) for name, url in LINKS]
+
+navbar = html.Div(
+    [
+        html.Img(src="../assets/bolt.jpg", height=42, width=38, style=style.NAV_HEADER),
+        dcc.Link("buzzword", href="/", style=style.NAV_HEADER),
+        html.Div(html.Ul(hrefs, className="nav navbar-nav"), className="pull-right"),
+    ],
+    className="navbar navbar-default navbar-static-top",
+)
