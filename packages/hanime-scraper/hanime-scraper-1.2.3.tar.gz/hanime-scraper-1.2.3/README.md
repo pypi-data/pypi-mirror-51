b0dcf9bbea4261809e0179756f331a428757baeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package hanime-scraper/1.2.3/hanime-scraper-1.2.3.tar.gz

---
+++ packages/hanime-scraper/hanime-scraper-1.2.3.tar.gz/hanime-scraper-1.2.3/README.md
@@ -0,0 +1,6 @@
+# hanime
+hanime image scraper
+
+this is an educational journey in making webscraping for himages.
+this is my first time using python
+this is for educational purposes only
---
+++ packages/hanime-scraper/hanime-scraper-1.2.3.tar.gz/hanime-scraper-1.2.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hanime-scraper",
+    version="1.2.3",
+    author="WorstGameDev",
+    author_email="worstgamedev@gmail.com",
+    description="A small commandline script for hanime community image scraping.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/worstgamedev/hanime-scraper",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['hanimestart=mymodule:app_requests'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free For Educational Use",
+        "Operating System :: OS Independent",
+    ],
+)
