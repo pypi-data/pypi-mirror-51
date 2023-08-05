163258b6ed4ae2c153d89d81ab265ad0a0e0d1a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:28 +0000

    Add package toolbox-az/0.2/toolbox_az-0.2.tar.gz

---
+++ packages/toolbox-az/toolbox_az-0.2.tar.gz/toolbox_az-0.2/setup.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2019 Lightricks. All rights reserved.
+from setuptools import setup
+setup(name='toolbox_az',
+      version='0.2',
+      description='Toolbox with some useful utils to be used in projects.',
+      url='https://github.com/advaza/toolbox',
+      author='Adva Zair',
+      author_email='adva@lightricks.com',
+      license='MIT',
+      packages=['toolbox_az'],
+      install_requires=[])
