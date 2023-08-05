37cf6607b1d5c408291da77afbef5d808b117543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:42 +0000

    Add package gurux-serial/1.0.8/gurux_serial-1.0.8.tar.gz

---
+++ packages/gurux-serial/gurux_serial-1.0.8.tar.gz/gurux_serial-1.0.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gurux_serial",
+    version="1.0.8",
+    author="Gurux Ltd",
+    author_email="gurux@gurux.org",
+    description="Gurux serial media is used to commmunication with serial port connections.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gurux/gurux.serial.python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['gurux-common'],
+)
