6bb67dc7eada92da1dd5bb776f5953c75b93dbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:50 +0000

    Add package lisflood-lisvap/0.4.0/lisflood_lisvap-0.4.0-py3-none-any.whl

---
+++ packages/lisflood-lisvap/lisflood_lisvap-0.4.0-py3-none-any.whl/lisvap/__init__.py
@@ -0,0 +1,7 @@
+version = (0, 4, 0)
+__authors__ = "Peter Burek, Johan van der Knijff, Ad de Roo"
+__version__ = '.'.join(list(map(str, version)))
+__date__ = "15/07/2019"
+__copyright__ = "Copyright 2019, Lisflood Open Source"
+__maintainers__ = "Domenico Nappo, Valerio Lorini, Lorenzo Mentaschi, Lorenzo Alfieri"
+__status__ = "Development"
