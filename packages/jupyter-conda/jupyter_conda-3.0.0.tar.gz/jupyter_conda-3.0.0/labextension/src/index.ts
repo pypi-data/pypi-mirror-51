97b81d3f96cf8aff7c9c6f4883eb463eb9774951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package jupyter-conda/3.0.0/jupyter_conda-3.0.0.tar.gz

---
+++ packages/jupyter-conda/jupyter_conda-3.0.0.tar.gz/jupyter_conda-3.0.0/labextension/src/index.ts
@@ -0,0 +1,98 @@
+import {
+  ILayoutRestorer,
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from "@jupyterlab/application";
+import {
+  ICommandPalette,
+  MainAreaWidget,
+  WidgetTracker
+} from "@jupyterlab/apputils";
+import { IMainMenu } from "@jupyterlab/mainmenu";
+import { classes, style } from "typestyle";
+import { GlobalStyle } from "./components/globalStyles";
+import { condaEnvId, CondaEnvWidget } from "./CondaEnvWidget";
+import { CondaEnvironments, IEnvironmentManager } from "./services";
+import { ISettingRegistry } from "@jupyterlab/coreutils";
+
+export { Conda, IEnvironmentManager } from "./services";
+
+async function activateCondaEnv(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  menu: IMainMenu,
+  restorer: ILayoutRestorer,
+  settingsRegistry: ISettingRegistry
+): Promise<IEnvironmentManager> {
+  const { commands, shell } = app;
+  const plugin_namespace = "conda-env";
+  const command: string = "condaenv:open-ui";
+
+  const settings = await settingsRegistry.load(condaEnvId);
+  const model = new CondaEnvironments(settings);
+
+  // Track and restore the widget state
+  let tracker = new WidgetTracker<MainAreaWidget<CondaEnvWidget>>({
+    namespace: plugin_namespace
+  });
+  let content: CondaEnvWidget;
+
+  commands.addCommand(command, {
+    label: "Conda Packages Manager",
+    execute: () => {
+      if (tracker.currentWidget) {
+        shell.activateById(tracker.currentWidget.id);
+        return;
+      }
+
+      content = new CondaEnvWidget(-1, -1, model);
+      content.addClass("jp-NbConda");
+      content.id = plugin_namespace;
+      content.title.label = "Packages";
+      content.title.caption = "Conda Packages Manager";
+      content.title.iconClass = Style.TabIcon;
+      const widget = new MainAreaWidget({ content });
+
+      void tracker.add(widget);
+      shell.add(widget, "main");
+    }
+  });
+
+  // Add command to command palette
+  palette.addItem({ command, category: "Settings" });
+
+  // Handle state restoration.
+  restorer.restore(tracker, {
+    command,
+    name: () => plugin_namespace
+  });
+
+  // Add command to settings menu
+  menu.settingsMenu.addGroup([{ command: command }], 999);
+
+  return model;
+}
+
+/**
+ * Initialization data for the jupyterlab_conda extension.
+ */
+const extension: JupyterFrontEndPlugin<IEnvironmentManager> = {
+  id: condaEnvId,
+  autoStart: true,
+  activate: activateCondaEnv,
+  requires: [ICommandPalette, IMainMenu, ILayoutRestorer, ISettingRegistry],
+  provides: IEnvironmentManager
+};
+
+export default extension;
+
+namespace Style {
+  export const TabIcon = classes(
+    "fa",
+    "fa-cubes",
+    style(GlobalStyle.FaIcon, {
+      lineHeight: "unset",
+      fontWeight: "normal"
+    })
+  );
+}
