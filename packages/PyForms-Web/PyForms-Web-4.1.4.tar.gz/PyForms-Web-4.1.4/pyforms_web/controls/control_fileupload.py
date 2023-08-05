9edf92d03b5f10ba15a63ba6222c54736c613044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:39 +0000

    Add package PyForms-Web/4.1.4/PyForms-Web-4.1.4.tar.gz

---
+++ packages/PyForms-Web/PyForms-Web-4.1.4.tar.gz/PyForms-Web-4.1.4/pyforms_web/controls/control_fileupload.py
@@ -0,0 +1,32 @@
+import os
+from django.conf import settings
+from pyforms_web.controls.control_base import ControlBase
+import simplejson
+
+class ControlFileUpload(ControlBase):
+
+	def init_form(self):
+		return "new ControlFileUpload('{0}', {1})".format( self._name, simplejson.dumps(self.serialize()) )
+
+	@property
+	def filepath(self):
+		if self.value:
+			return os.path.join( settings.MEDIA_ROOT, self.value[len(settings.MEDIA_URL):] )
+		else:
+			return None
+
+	def serialize(self):
+		data = super(ControlFileUpload, self).serialize()
+		if self.value:
+			try:
+				file_data = {
+					'name': os.path.basename(self.value),
+					'size': os.path.getsize(self.filepath),
+					'file': self.value,
+					 'url': self.value
+				}
+				data.update({ 'file_data':file_data })
+			except OSError:
+				pass
+
+		return data
