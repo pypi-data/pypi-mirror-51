adb701fa53318f0fc49c2995c7151c3d07050c81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:00 +0000

    Add package empire-erp/0.0.2/empire-erp-0.0.2.tar.gz

---
+++ packages/empire-erp/empire-erp-0.0.2.tar.gz/empire-erp-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="empire-erp",
+    version="0.0.2",
+    author="Vladimir Nomkhoev",
+    author_email="author@example.com",
+    description="Empire ERP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nomhoi/empire-erp",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
