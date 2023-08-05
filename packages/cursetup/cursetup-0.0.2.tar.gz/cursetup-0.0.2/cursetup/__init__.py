288827a8d57ab65ee2e4ba031061f73e05df2301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:32 +0000

    Add package cursetup/0.0.2/cursetup-0.0.2.tar.gz

---
+++ packages/cursetup/cursetup-0.0.2.tar.gz/cursetup-0.0.2/cursetup/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import curses
+import time
+def setup():
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    stdscr.keypad(True)
+    return stdscr
+
+def quit(stdscr):
+    curses.echo()
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.endwin()
+
+def centertext(text, window):
+    height,width = window.getmaxyx()
+    window.addstr(height/2, width/2, text)
+    window.refresh()
+
+def animatetext(atext, window):
+    btext=''
+    for i in range(len(atext)):
+        btext+=atext[i]
+        centertext(btext, window)
+        window.refresh()
+        time.sleep(.05)
+def setup_input(window):
+        sub = window.subwin(1, 40, 3, 2)
+        tb = curses.textpad.Textbox(sub)
+        window.refresh()
+        tb.edit()
