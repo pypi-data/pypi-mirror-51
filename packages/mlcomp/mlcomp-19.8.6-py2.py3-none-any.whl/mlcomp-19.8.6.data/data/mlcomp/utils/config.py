128898264ac5d256a7d5d07767bf05a597dd2b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:22 +0000

    Add package mlcomp/19.8.6/mlcomp-19.8.6-py2.py3-none-any.whl

---
+++ packages/mlcomp/mlcomp-19.8.6-py2.py3-none-any.whl/mlcomp-19.8.6.data/data/mlcomp/utils/config.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+from typing import List
+import os
+import json
+
+import albumentations as A
+
+from mlcomp import DATA_FOLDER
+from mlcomp.utils.io import yaml_load
+from mlcomp.utils.misc import dict_flatten, dict_unflatten
+
+
+class Config(dict):
+    @property
+    def data_folder(self):
+        return os.path.join(DATA_FOLDER, self['info']['project'])
+
+    @staticmethod
+    def from_json(config: str):
+        return Config(json.loads(config))
+
+    @staticmethod
+    def from_yaml(config: str):
+        return Config(yaml_load(config))
+
+
+def merge_dicts_smart(target: dict, source: dict, sep='/'):
+    target_flatten = dict_flatten(target)
+    mapping = defaultdict(list)
+    for k, v in target_flatten.items():
+        parts = k.split(sep)
+        for i in range(len(parts) - 1, -1, -1):
+            key = sep.join(parts[i:])
+            mapping[key].append(k)
+
+    for k, v in source.items():
+        assert len(mapping[k]) == 1, f'ambiguous mapping for {k}'
+        key = mapping[k][0]
+        target_flatten[key] = v
+
+    return dict_unflatten(target_flatten)
+
+
+def parse_albu(configs: List[dict]):
+    res = []
+    for config in configs:
+        assert 'name' in config, f'name is required in {config}'
+        config = config.copy()
+        name = config.pop('name')
+        if name == 'Compose':
+            items = config.pop('items')
+            aug = A.Compose(parse_albu(items), **config)
+        else:
+            aug = getattr(A, name)(**config)
+        res.append(aug)
+    return res
+
+
+__all__ = ['Config', 'merge_dicts_smart', 'parse_albu']
---
+++ packages/mlcomp/mlcomp-19.8.6-py2.py3-none-any.whl/mlcomp/utils/io.py
@@ -0,0 +1,29 @@
+import os
+import yaml
+
+yaml.warnings({'YAMLLoadWarning': False})
+
+
+def read_lines(file: str):
+    return [l.strip() for l in open(file)]
+
+
+def from_module_path(file: str, path: str):
+    return os.path.join(os.path.dirname(file), path)
+
+
+def yaml_load(text: str = None, file: str = None):
+    stream = text
+    if file is not None:
+        stream = open(file).read()
+    res = yaml.load(stream, Loader=yaml.FullLoader)
+    if res is None:
+        return {}
+    return res
+
+
+def yaml_dump(data):
+    return yaml.dump(data, default_flow_style=False, sort_keys=False)
+
+
+__all__ = ['read_lines', 'from_module_path', 'yaml_load', 'yaml_dump']
