fd935f02bb88ac859e6125a0e6bd2285bfdaf0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:38 +0000

    Add package acse-9-irp-wafflescore/1.0.1/acse_9_irp_wafflescore-1.0.1.tar.gz

---
+++ packages/acse-9-irp-wafflescore/acse_9_irp_wafflescore-1.0.1.tar.gz/acse_9_irp_wafflescore-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="acse_9_irp_wafflescore",
+    version="1.0.1",
+    author="Nitchakul Pipitvej",
+    author_email="np2618@ic.ac.uk",
+    description="ML for Automatic Facies Classification ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/msc-acse/acse-9-independent-research-project-wafflescore",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
