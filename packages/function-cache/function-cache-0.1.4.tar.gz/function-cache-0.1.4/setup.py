9f86d2aa8d7431f95d8b2a20b4fd4eeee69351d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:52 +0000

    Add package function-cache/0.1.4/function-cache-0.1.4.tar.gz

---
+++ packages/function-cache/function-cache-0.1.4.tar.gz/function-cache-0.1.4/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+setup(
+    name='function-cache',
+    version='0.1.4',
+    packages=['function_cache'],
+    description='A Django-based convenience decorator for caching results of functions to various backends.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Yanchuan Sim',
+    author_email='yc@botmd.io',
+    url='https://gitlab.com/fivehealth/function-cache',
+    license='Apache License 2.0',
+    install_requires=[
+        'Django>2.0',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Framework :: Django',
+    ],
+)
