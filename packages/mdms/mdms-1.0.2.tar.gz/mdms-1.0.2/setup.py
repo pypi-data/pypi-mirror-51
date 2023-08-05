72a84beacf6484d6d3f5342a3ae96c0511cd756a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:50 +0000

    Add package mdms/1.0.2/mdms-1.0.2.tar.gz

---
+++ packages/mdms/mdms-1.0.2.tar.gz/mdms-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mdms",
+    version="1.0.2",
+    author="Szymon Zaczek",
+    author_email="szymon.zaczek@edu.p.lodz.pl",
+    description='An interface to one of the most popular Molecular Dynamics codes - Amber - which aids user in '
+                     'preparing and running their own simulations.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/szymonzaczek/MDMS",
+    packages=setuptools.find_packages(),
+    scripts=['mdms/mdms_menu.py'],
+    install_requires=['numpy', 'pandas', 'biopython', 'pdb-tools'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Chemistry"
+    ])
