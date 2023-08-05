a7ac333685ac2c330883e0698788aba59d68cadd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package wordpresstools/0.0.1/wordpresstools-0.0.1.tar.gz

---
+++ packages/wordpresstools/wordpresstools-0.0.1.tar.gz/wordpresstools-0.0.1/README.md
@@ -0,0 +1,9 @@
+# Python WordPress Tool
+    A collection of tools for managing WordPress installations
+
+## PhpVariableExtractor
+
+### Extractor
+    Class to extract variables from a PHP file as key value pairs in a Python dict.
+  
+  
---
+++ packages/wordpresstools/wordpresstools-0.0.1.tar.gz/wordpresstools-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wordpresstools",
+    version="0.0.1",
+    author="Matthew Larsen",
+    author_email="matt.larsen@connorgp.com",
+    description="A small set of tools for administration of WordPress installations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matt-larsen-sld/wptools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
