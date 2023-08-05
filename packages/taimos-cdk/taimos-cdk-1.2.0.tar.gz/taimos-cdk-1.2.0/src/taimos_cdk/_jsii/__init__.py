eac2680b0880ea0f55912a6a4f409a89af3c41aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:28 +0000

    Add package taimos-cdk/1.2.0/taimos-cdk-1.2.0.tar.gz

---
+++ packages/taimos-cdk/taimos-cdk-1.2.0.tar.gz/taimos-cdk-1.2.0/src/taimos_cdk/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+from jsii.python import classproperty
+
+import aws_cdk.alexa_ask
+import aws_cdk.aws_cloudformation
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_codebuild
+import aws_cdk.aws_codecommit
+import aws_cdk.aws_codepipeline
+import aws_cdk.aws_codepipeline_actions
+import aws_cdk.aws_ecr
+import aws_cdk.aws_events_targets
+import aws_cdk.aws_iam
+import aws_cdk.aws_logs
+import aws_cdk.aws_s3_deployment
+import aws_cdk.aws_sam
+import aws_cdk.aws_secretsmanager
+import aws_cdk.aws_sns_subscriptions
+import aws_cdk.core
+__all__ = []
+
+publication.publish()
