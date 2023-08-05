d43bd2a361674fde1e38549e3f1ee678198ef59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:58 +0000

    Add package Some-Sonar/1.1/Some_Sonar-1.1.tar.gz

---
+++ packages/Some-Sonar/Some_Sonar-1.1.tar.gz/Some_Sonar-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='Some_Sonar',
+    version='1.01',
+    install_requires=[
+          'RPi.GPIO',
+      ],
+    packages=find_packages(),
+    author_email='zarubinilia@yabdex.ru',
+    include_package_data=True,
+    zip_safe=False
+)
