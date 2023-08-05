b2b72f2a2fec1ace39a26bbe8f1a746e0b1f0c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:57 +0000

    Add package poly-pack/0.25/poly_pack-0.25.tar.gz

---
+++ packages/poly-pack/poly_pack-0.25.tar.gz/poly_pack-0.25/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='poly_pack',
+      version='0.25',
+      description='Some helpful functions for polymer stuff',
+      url='https://github.com/reha54321/poly_pack',
+      author='Reha Mathur',
+      author_email='reha_mathur@horacemann.org',
+      license='MIT',
+      packages=['poly_pack'],
+      install_requires=[
+          'matplotlib',
+          'numpy',
+          'scipy',
+          'pyFAI',
+          'fabio',
+          'tifffile',
+      ],
+      include_package_data=True,
+      zip_safe=False)
