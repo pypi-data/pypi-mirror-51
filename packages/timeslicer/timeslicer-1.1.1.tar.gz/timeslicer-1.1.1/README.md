6ee23cdaf519b99b8af87e448eae25b1294013e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:45 +0000

    Add package timeslicer/1.1.1/timeslicer-1.1.1.tar.gz

---
+++ packages/timeslicer/timeslicer-1.1.1.tar.gz/timeslicer-1.1.1/README.md
@@ -0,0 +1,27 @@
+# Time Slicer
+
+A Python package for creating timeslice images from time lapses.
+
+Contains a module for importing the time slice function into other Python projects and a main 
+script for creating a timeslice from the terminal.
+
+## Usage
+
+```commandline
+pip install timeslicer==1.1.1
+```
+
+Create Timeslice from terminal:
+```commandline
+python3 -m timeslicer
+```
+
+## Sample Output Images
+
+### Standard Timeslice
+
+![standard](https://raw.githubusercontent.com/nramkissoon/Time-Slicer/master/samples/test.jpg)
+
+### Randomized Timeslice
+
+![randomized](https://raw.githubusercontent.com/nramkissoon/Time-Slicer/master/samples/test-randomized.jpg)
---
+++ packages/timeslicer/timeslicer-1.1.1.tar.gz/timeslicer-1.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="timeslicer",
+    version="1.1.1",
+    author="Nicholas Ramkissoon",
+    description="Package for creating timeslice images from time lapses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nramkissoon/Time-Slice",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
