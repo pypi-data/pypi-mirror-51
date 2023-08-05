6451e50597fb7be167bde506d58e3b7e80f712d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:31 +0000

    Add package supermath200/1.0/supermath200-1.0.tar.gz

---
+++ packages/supermath200/supermath200-1.0.tar.gz/supermath200-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='supermath200',
+    version='1.0',
+    description = '亲，这个包用于测试哦',
+    author='ping',
+    author_email='915129898@qq.com',
+    py_modules=['supermath200.demo1','supermath200.demo2']
+)
