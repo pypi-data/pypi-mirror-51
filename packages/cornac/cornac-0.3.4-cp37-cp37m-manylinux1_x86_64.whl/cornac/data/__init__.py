8a98cbe1783d52c669a457c5b07be8d9bc282174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:45 +0000

    Add package cornac/0.3.4/cornac-0.3.4-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/cornac/cornac-0.3.4-cp37-cp37m-manylinux1_x86_64.whl/cornac/data/__init__.py
@@ -0,0 +1,36 @@
+# Copyright 2018 The Cornac Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+from .modality import FeatureModality
+from .text import TextModality
+from .image import ImageModality
+from .graph import GraphModality
+from .trainset import TrainSet
+from .trainset import MatrixTrainSet
+from .trainset import MultimodalTrainSet
+from .testset import TestSet
+from .testset import MultimodalTestSet
+from .reader import Reader
+
+__all__ = ['FeatureModality',
+           'TextModality',
+           'ImageModality',
+           'GraphModality',
+           'TrainSet',
+           'MatrixTrainSet',
+           'MultimodalTrainSet',
+           'TestSet',
+           'MultimodalTestSet',
+           'Reader']
