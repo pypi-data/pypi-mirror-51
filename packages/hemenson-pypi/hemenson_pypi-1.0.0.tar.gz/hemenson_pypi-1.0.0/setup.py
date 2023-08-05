c57949fe1dd75587344e6457bec2569fa9efbb66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:46 +0000

    Add package hemenson-pypi/1.0.0/hemenson_pypi-1.0.0.tar.gz

---
+++ packages/hemenson-pypi/hemenson_pypi-1.0.0.tar.gz/hemenson_pypi-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hemenson_pypi',
+    version='1.0.0',
+    author='Hemenson David silva',
+    description='Brincando com distribuição python',
+    py_modules=[
+        'hemenson',
+    ],
+)
