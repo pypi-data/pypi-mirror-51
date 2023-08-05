c3da499da7cef1c50ba6d3f4f89d85eb63a9eed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:08 +0000

    Add package dotstrings/0.3.0/dotstrings-0.3.0-py3-none-any.whl

---
+++ packages/dotstrings/dotstrings-0.3.0-py3-none-any.whl/dotstrings/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""Utilities for dealing with .strings files"""
+
+from dotstrings.parser import load, loads
+from dotstrings.dot_strings_entry import DotStringsEntry
+from dotstrings.localized_string import LocalizedString
