06ecdc90d811292945a78cd9111b0efeb2035cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:54 +0000

    Add package omfvista/0.2.1/omfvista-0.2.1.tar.gz

---
+++ packages/omfvista/omfvista-0.2.1.tar.gz/omfvista-0.2.1/omfvista/__init__.py
@@ -0,0 +1,28 @@
+"""``omfvista``: 3D visualization for the Open Mining Format (omf)
+"""
+
+from omfvista.wrapper import wrap, project_to_vtk, load_project
+from omfvista.lineset import line_set_to_vtk
+from omfvista.pointset import point_set_to_vtk
+from omfvista.surface import surface_geom_to_vtk, surface_grid_geom_to_vtk, surface_to_vtk
+from omfvista.volume import volume_grid_geom_to_vtk, volume_to_vtk
+from omfvista.utilities import *
+
+
+# Package meta data
+__author__ = 'Bane Sullivan'
+__license__ = 'BSD-3-Clause'
+__copyright__ = '2019, Bane Sullivan'
+__version__ = '0.2.1'
+__displayname__ = 'OMF-VTK'
+__name__ = 'omfvista'
+
+
+def ignore_warnings():
+    """Sets a warning filter for pillow's annoying ``DecompressionBombWarning``
+    """
+    import warnings
+    from PIL import Image
+    warnings.simplefilter(action='ignore', category=Image.DecompressionBombWarning)
+
+ignore_warnings()
