04642fa0c27e97749f4abcc8be49ce4514d9b879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:41 +0000

    Add package liquidcli/0.0.23/liquidcli-0.0.23.tar.gz

---
+++ packages/liquidcli/liquidcli-0.0.23.tar.gz/liquidcli-0.0.23/liquidcli/data.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Execution:
+    id: int = ''
+    quantity: float = 0
+    price: float = 0
+    taker_side: str = ''
+    created_at: int = 0
+
+
+@dataclass
+class FiatAccount:
+    id: int = 0
+    currency: str = ''
+    currency_symbol: str = ''
+    balance: float = 0
+    reserved_balance: float = 0
+    pusher_channel: str = ''
+    lowest_offer_interest_rate: float = 0
+    highest_offer_interest_rate: float = 0
+    exchange_rate: float = 0
+    currency_type: str = ''
+
+
+@dataclass
+class CryptoAccount:
+    id: int = 0
+    balance: float = 0
+    reserved_balance: float = 0
+    address: str = ''
+    currency: str = ''
+    currency_symbol: str = ''
+    pusher_channel: str = ''
+    minimum_withdraw: float = 0
+    lowest_offer_interest_rate: float = 0
+    highest_offer_interest_rate: float = 0
+    currency_type: str = ''
+
+
+@dataclass
+class Page:
+    current_page: int
+    total_pages: int
+    models: List[object]
+
+
+@dataclass
+class Order:
+    id: int = 0
+    order_type: str = ''
+    margin_type: str = ''
+    quantity: float = 0
+    disc_quantity: str = ''
+    iceberg_total_quantity: float = 0
+    side: str = ''
+    filled_quantity: float = 0
+    price: float = 0
+    updated_at: int = 0
+    created_at: int = 0
+    status: str = ''
+    product_id: int = 0
+    product_code: str = ''
+    funding_currency: str = ''
+    currency_pair_code: str = ''
+    order_fee: float = 0
+    client_order_id: str = ''
+    executions: List[Execution] = None
