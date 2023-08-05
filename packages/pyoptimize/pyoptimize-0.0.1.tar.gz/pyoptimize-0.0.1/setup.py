80b7a1c6cc539f3ea5d3daba3fea32b49ded94c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:33 +0000

    Add package pyoptimize/0.0.1/pyoptimize-0.0.1.tar.gz

---
+++ packages/pyoptimize/pyoptimize-0.0.1.tar.gz/pyoptimize-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyoptimize',
+    version='0.0.1',
+    author='Andrew Barisser',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.15.0",
+        ])
