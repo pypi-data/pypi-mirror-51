85b73ba18e5591c1f600f5f6c4867212873b1e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:25 +0000

    Add package pcreode/2.2.0/pcreode-2.2.0.tar.gz

---
+++ packages/pcreode/pcreode-2.2.0.tar.gz/pcreode-2.2.0/pcreode/__init__.py
@@ -0,0 +1,7 @@
+#somehow doing the below allows you to access them just by saying 'pcreode'
+from .pcreode import *
+#from pcreode import functions
+
+#also had to remove the "from functions import *" from pcreode.py
+
+__version__ = "2.2.0"
