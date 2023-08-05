1623548b6b0f709e0506b7590ade6d67bfe14724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:29 +0000

    Add package instagrump/1.1.5/instagrump-1.1.5.tar.gz

---
+++ packages/instagrump/instagrump-1.1.5.tar.gz/instagrump-1.1.5/instagrump/test.py
@@ -0,0 +1,8 @@
+from instagrump.Profile import Profile
+from instagrump.Content import Content
+
+def content_test():
+	return Content('https://www.instagram.com/p/B09HQvnHjV_/?utm_source=ig_web_copy_link')
+
+def profile_test():
+	return Profile('anyageraldine')
