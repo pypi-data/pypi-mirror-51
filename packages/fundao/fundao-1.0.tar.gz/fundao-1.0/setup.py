02453a0cf754da287ca98ac29eabad09b7ea6578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package fundao/1.0/fundao-1.0.tar.gz

---
+++ packages/fundao/fundao-1.0.tar.gz/fundao-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='fundao',
+	version='1.0',
+    author='Fundao',
+    author_email='ojuara@araujo.com',
+    description='Brincando com distribuição python',
+    py_modules = ['ojuara']
+)
