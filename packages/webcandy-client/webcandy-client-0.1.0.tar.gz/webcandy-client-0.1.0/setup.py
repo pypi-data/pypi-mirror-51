72ea6a8e995a29dccf278f7cde8ba7062801cce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package webcandy-client/0.1.0/webcandy-client-0.1.0.tar.gz

---
+++ packages/webcandy-client/webcandy-client-0.1.0.tar.gz/webcandy-client-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setup(
+    name='webcandy-client',
+    version='0.1.0',
+    author='Graham Preston',
+    author_email='graham.preston@gmail.com',
+    description='Client-side code for communicating with a Webcandy server.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/gcpreston/webcandy-client',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'opclib',
+        'requests',
+        'websockets'
+    ],
+    entry_points={
+        'console_scripts': [
+            'wc-client = webcandy_client.client:main',
+            'wc-controller = webcandy_client.controller:main'
+        ]
+    }
+)
