d11c8d074dedc993122d3e67fc00faa5e00aca12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:53 +0000

    Add package browseapi/0.11.2/browseapi-0.11.2.tar.gz

---
+++ packages/browseapi/browseapi-0.11.2.tar.gz/browseapi-0.11.2/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='browseapi',
+    packages=['browseapi'],
+    version='0.11.2',
+    license='MIT',
+    description='eBay Browse API Python client',
+    long_description=long_description,
+    author='Andrii Matiiash',
+    author_email='andmati743@gmail.com',
+    url='https://github.com/AverHLV/browseapi',
+    download_url='https://github.com/AverHLV/browseapi/archive/0.11.2.tar.gz',
+    keywords=['ASYNC', 'BROWSE API', 'CLIENT'],
+
+    install_requires=[
+        'aiohttp',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
