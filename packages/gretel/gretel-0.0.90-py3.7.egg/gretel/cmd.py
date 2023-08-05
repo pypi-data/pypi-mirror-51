b524ef5087740d45e8eda0b5bd0dbd8c81c57dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:02 +0000

    Add package gretel/0.0.90/gretel-0.0.90-py3.7.egg

---
+++ packages/gretel/gretel-0.0.90-py3.7.egg/gretel/cmd.py
@@ -0,0 +1,256 @@
+import argparse
+import numpy as np
+import sys
+import os
+
+from . import gretel
+from . import util
+
+def main():
+    """Gretel: A metagenomic haplotyper."""
+    parser = argparse.ArgumentParser(description="Gretel: A metagenomic haplotyper.")
+    parser.add_argument("bam")
+    parser.add_argument("vcf")
+    parser.add_argument("contig")
+    parser.add_argument("-s", "--start", type=int, default=1, help="1-indexed start base position [default: 1]")
+    parser.add_argument("-e", "--end", type=int, default=-1, help="1-indexed end base position [default: reference length]")
+
+    parser.add_argument("-l", "--lorder", type=int, default=0, help="Order of markov chain to predict next nucleotide [default calculated from read data]")
+    parser.add_argument("-p", "--paths", type=int, default=100, help="Maximum number of paths to generate [default:100]")
+
+    parser.add_argument("--master", default=None, help="Master sequence (will be used to fill in homogeneous gaps in haplotypes, otherwise --gapchar)") #TODO Use something other than N? Should probably be a valid IUPAC
+    parser.add_argument("--gapchar", default="N", help="Character to fill homogeneous gaps in haplotypes if no --master [default N]")
+    parser.add_argument("--delchar", default="", help="Character to output in haplotype for deletion (eg. -) [default is blank]")
+
+    parser.add_argument("--quiet", default=False, action='store_true', help="Don't output anything other than a single summary line.")
+    parser.add_argument("--sentinels", default=False, action='store_true', help="Add additional sentinels for read ends [default:False][EXPERIMENTAL]")
+    parser.add_argument("-o", "--out", default=".", help="Output directory [default .]")
+    parser.add_argument("-@", "--threads", type=int, default=1, help="Number of BAM iterators [default 1]")
+
+    parser.add_argument("--debugreads", type=str, default="", help="A newline delimited list of read names to output debug data when parsing the BAM")
+    parser.add_argument("--debugpos", type=str, default="", help="A newline delimited list of 1-indexed genomic positions to output debug data when parsing the BAM")
+
+    parser.add_argument("--dumpmatrix", type=str, default=None, help="Location to dump the Hansel matrix to disk")
+    parser.add_argument("--dumpsnps", type=str, default=None, help="Location to dump the SNP positions to disk")
+
+    ARGS = parser.parse_args()
+
+    if ARGS.end == -1:
+        ARGS.end = util.get_ref_len_from_bam(ARGS.bam, ARGS.contig)
+        sys.stderr.write("[NOTE] Setting end_pos to %d" % ARGS.end)
+
+    debug_reads = set([])
+    if ARGS.debugreads:
+        debug_fofn = open(ARGS.debugreads)
+        for line in debug_fofn:
+            debug_reads.add(line.strip())
+
+    debug_pos = set([])
+    if ARGS.debugpos:
+        debug_fofn = open(ARGS.debugpos)
+        for line in debug_fofn:
+            debug_pos.add(int(line.strip()))
+
+    VCF_h = gretel.process_vcf(ARGS.vcf, ARGS.contig, ARGS.start, ARGS.end)
+    if ARGS.dumpsnps:
+        snp_fh = open(ARGS.dumpsnps, 'w')
+        for k in sorted(VCF_h["snp_fwd"].keys()):
+            snp_fh.write("%d\t%d\t%d\n" % (VCF_h["snp_fwd"][k]+1, k, k-ARGS.start+1))
+        snp_fh.close()
+
+    BAM_h = gretel.process_bam(VCF_h, ARGS.bam, ARGS.contig, ARGS.start, ARGS.end, ARGS.lorder, ARGS.sentinels, ARGS.threads, debug_reads=debug_reads, debug_pos=debug_pos)
+
+    if ARGS.dumpmatrix:
+        #sys.stderr.write("[NOTE] Dumping Hansel matrix to %s\n" % ARGS.dumpmatrix)
+        #np.save(ARGS.dumpmatrix, BAM_h["read_support"])
+
+
+        #TODO Stolen from util, they should be shared.
+        symbols = ['A', 'C', 'G', 'T', 'N', '-', '_']
+        symbols_d = {symbol: i for i, symbol in enumerate(symbols)}
+        def __symbol_num(symbol):
+            #TODO Catch potential KeyError
+            #TODO Generic mechanism for casing (considering non-alphabetically named states, too...)
+            return symbols_d[symbol]
+
+        z = ['A', 'C', 'G', 'T']
+        for a in z:
+            for b in z:
+                p = os.path.join(ARGS.dumpmatrix + '.' + a + '-' + b + '.txt')
+                sys.stderr.write("[NOTE] Dumping (%s>%s) Hansel matrix to %s\n" % (a,b,p))
+                np.savetxt(p, BAM_h["read_support"][__symbol_num(a), __symbol_num(b)], delimiter=',')
+
+
+    # Check if there is a gap in the matrix
+    # TODO(samstudio8) Ideally we would do this IN the bam worker threads such
+    #                  that a thread could raise an error and halt the whole
+    #                  process if we already know we'll yield a matrix we cannot
+    #                  actually work with, but this will do for now...
+    for i in range(0, VCF_h["N"]+1):
+        marginal = BAM_h["read_support"].get_counts_at(i)
+
+        if i > 0:
+            snp_rev = VCF_h["snp_rev"][i-1]
+        else:
+            snp_rev = 0
+        if marginal.get("total", 0) == 0:
+            sys.stderr.write('''[FAIL] Unable to recover pairwise evidence concerning SNP #%d at position %d
+       Gretel needs every SNP to appear on a read with at least one other SNP, at least once.
+       There is no read in your data set that bridges SNP #%d with any of its neighbours.
+
+       * If you are trying to run Gretel along an entire contig or genome, please note that
+       this is not the recommended usage for Gretel, as it was intended to uncover the
+       variation in a metahaplome: the set of haplotypes for a specific gene.
+           See our pre-print https://doi.org/10.1101/223404 for more information
+
+       Consider running a prediction tool such as `prokka` on your assembly or reference
+       and using the CDS regions in the GFF for corresponding genes of interest to
+       uncover haplotypes with Gretel instead.
+
+       * If you are already doing this, consider calling for SNPs more aggressively.
+       We use `snpper` (https://github.com/SamStudio8/gretel-test/blob/master/snpper.py)
+       to determine any site in a BAM that has at least one read in disagreement with
+       the reference as a SNP. Although this introduces noise from alignment and sequence
+       error, Gretel is fairly robust. Importantly, this naive calling method will
+       likely close gaps between SNPs and permit recovery.
+
+       * Finally, consider that the gaps are indicative that your reads do not support
+       one or more parts of your assembly or reference. You could try and find or construct
+       a more suitable reference, or reduce the size of the recovery window.
+
+       Sorry :(\n''' % (i, snp_rev, i))
+            sys.exit(1)
+
+
+    #print "[META] #CONTIG", ARGS.contig
+    #print "[META] #SNPS", VCF_h["N"]
+    #print "[META] #READS", BAM_h["N"]
+
+    PATHS = []
+    PATH_PROBS = []
+    PATH_PROBS_UW = []
+    PATH_FALLS = []
+
+    # Spew out exciting information about the SNPs
+    all_marginals = {
+        "A": [],
+        "C": [],
+        "G": [],
+        "T": [],
+        "N": [],
+        "-": [],
+        "_": [],
+        "total": [],
+    }
+    if not ARGS.quiet:
+        print ("i\tpos\tgap\tA\tC\tG\tT\tN\t-\t_\ttot")
+        last_rev = 0
+        for i in range(0, VCF_h["N"]+1):
+            marginal = BAM_h["read_support"].get_counts_at(i)
+            snp_rev = 0
+            if i > 0:
+                snp_rev = VCF_h["snp_rev"][i-1]
+            print ("%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d" % (
+                i,
+                snp_rev,
+                snp_rev - last_rev,
+                marginal.get("A", 0),
+                marginal.get("C", 0),
+                marginal.get("G", 0),
+                marginal.get("T", 0),
+                marginal.get("N", 0),
+                marginal.get("-", 0),
+                marginal.get("_", 0),
+                marginal.get("total", 0),
+            ))
+            all_marginals["A"].append(marginal.get("A", 0))
+            all_marginals["C"].append(marginal.get("C", 0))
+            all_marginals["G"].append(marginal.get("G", 0))
+            all_marginals["T"].append(marginal.get("T", 0))
+            all_marginals["N"].append(marginal.get("N", 0))
+            all_marginals["-"].append(marginal.get("-", 0))
+            all_marginals["_"].append(marginal.get("_", 0))
+            all_marginals["total"].append(
+                marginal.get("total", 0)
+            )
+            last_rev = snp_rev
+
+
+    # Make some genes
+    SPINS = ARGS.paths
+    ongoing_mag = 0
+    for i in range(0, SPINS):
+        init_path, init_prob, init_min = gretel.generate_path(VCF_h["N"], BAM_h["read_support"], BAM_h["read_support_o"])
+        if init_path == None:
+            break
+        current_path = init_path
+
+        MIN_REMOVE = 0.01 # 1%
+        if init_min < MIN_REMOVE:
+            sys.stderr.write("[RWGT] Ratio %.10f too small, adjusting to %.3f\n" % (init_min, MIN_REMOVE))
+            init_min = MIN_REMOVE
+        rw_magnitude = gretel.reweight_hansel_from_path(BAM_h["read_support"], init_path, init_min)
+
+        #TODO Horribly inefficient.
+        if current_path in PATHS:
+            continue
+        else:
+            ongoing_mag += rw_magnitude
+            PATHS.append(current_path)
+            PATH_PROBS.append(init_prob["weighted"])
+            PATH_PROBS_UW.append(init_prob["unweighted"])
+            PATH_FALLS.append(ongoing_mag)
+            ongoing_mag = 0
+        ongoing_mag += rw_magnitude
+
+
+    # Make some pretty pictures
+    dirn = ARGS.out + "/"
+    fasta_out_fh = open(dirn+"out.fasta", "w")
+    if ARGS.master:
+        master_fa = util.load_fasta(ARGS.master)
+        master_seq = master_fa.fetch(master_fa.references[0])
+    else:
+        master_seq = [' '] * ARGS.end
+
+    for i, path in enumerate(PATHS):
+        seq = list(master_seq[:])
+        for j, mallele in enumerate(path[1:]):
+            snp_pos_on_master = VCF_h["snp_rev"][j]
+            try:
+                if mallele == "-":
+                    # It's a deletion, don't print a SNP
+                    seq[snp_pos_on_master-1] = ARGS.delchar
+                else:
+                    seq[snp_pos_on_master-1] = mallele
+            except IndexError:
+                print (path, len(seq), snp_pos_on_master-1)
+                sys.exit(1)
+
+        to_write = "".join(seq[ARGS.start-1 : ARGS.end])
+        if not ARGS.master:
+            to_write = to_write.replace(' ', ARGS.gapchar)
+
+        fasta_out_fh.write(">%d__%.2f\n" % (i, PATH_PROBS[i]))
+        fasta_out_fh.write("%s\n" % to_write)
+    fasta_out_fh.close()
+
+    #TODO datetime, n_obs, n_slices, avg_obs_len, L, n_paths, n_avg_loglik
+    crumb_file = open(dirn+"gretel.crumbs", "w")
+    crumb_file.write("# %d\t%d\t%d\t%.2f\t%d\t%.2f\t%.2f\t%.2f\n" % (
+        VCF_h["N"],
+        BAM_h["read_support"].n_crumbs,
+        BAM_h["read_support"].n_slices,
+        BAM_h["meta"]["L"],
+        BAM_h["read_support"].L,
+        np.mean(PATH_PROBS),
+        np.mean(PATH_PROBS_UW),
+        np.mean(PATH_FALLS),
+    ))
+    for p in range(len(PATHS)):
+        crumb_file.write("%d\t%.2f\t%.2f\t%.2f\n" % (
+                p,
+                PATH_PROBS[p],
+                PATH_PROBS_UW[p],
+                PATH_FALLS[p]
+        ))
---
+++ packages/gretel/gretel-0.0.90-py3.7.egg/gretel/gretel.py
@@ -0,0 +1,303 @@
+import sys
+from math import log,log10,exp
+import random
+
+import vcf
+import numpy as np
+
+from hansel import Hansel
+from . import util
+
+#TODO Should the denom of the conditional use the unique variants at i-l or i?
+#TODO Util to parse known input and return SNP seq
+
+def reweight_hansel_from_path(hansel, path, ratio):
+    #TODO Reweight only the L pairs?
+    """
+    Given a completed path, reweight the applicable pairwise observations in the Hansel structure.
+
+    Parameters
+    ----------
+    hansel : :py:class:`hansel.hansel.Hansel`
+        The Hansel structure currently being explored by Gretel.
+
+    path : list{str}
+        The ordered sequence of selected variants.
+
+    ratio : float
+        The proportion of evidence to remove from each paired observation that
+        was considered to recover the provided path.
+
+        It is recommended this be the smallest marginal distribution observed across selected variants.
+
+        *i.e.* For each selected variant in the path, note the value of the
+        marginal distribution for the probability of observing that particular
+        variant at that genomic position. Parameterise the minimum value of
+        those marginals.
+
+    Returns
+    -------
+    Spent Observations : float
+        The sum of removed observations from the Hansel structure.
+    """
+
+    size = 0
+    for i in range(0, len(path)):
+        for j in range(0, i+1+1):
+            # Reduce read supports
+            if i >= len(path)-1:
+                size += hansel.reweight_observation(path[i], path[j], i, i+1, ratio)
+                break #???
+            else:
+                if j < i:
+                    # This isn't just a case of j < i, but means that we are looking
+                    # at the two SNPs the wrong way around, we must switch them before
+                    # handing them over to reweight_observation
+                    t_i = j
+                    t_j = i
+                else:
+                    t_i = i
+                    t_j = j
+                size += hansel.reweight_observation(path[t_i], path[t_j], t_i, t_j, ratio)
+    sys.stderr.write("[RWGT] Ratio %.3f, Removed %.1f\n" % (ratio, size))
+    return size
+
+
+## INPUT OUTPUT ###############################################################
+def process_vcf(vcf_path, contig_name, start_pos, end_pos):
+    """
+    Parse a VCF to extract the genomic positions of called variants.
+
+    Parameters
+    ----------
+    vcf_path : str
+        Path to the VCF file.
+
+    contig_name : str
+        Name of the target contig on which variants were called.
+
+    start_pos : int
+        The 1-indexed genomic position from which to begin considering variants.
+
+    end_pos : int
+        The 1-indexed genomic position at which to stop considering variants.
+
+    Returns
+    -------
+    Gretel Metastructure : dict
+        A collection of structures used for the execution of Gretel.
+        The currently used keys are:
+            N : int
+                The number of observed SNPs
+            snp_fwd : dict{int, int}
+                A reverse lookup from the n'th variant, to its genomic position on the contig
+            snp_rev : dict{int, int}
+                A forward lookup to translate the n'th genomic position to its i'th SNP rank
+            region : list{int}
+                A masked representation of the target contig, positive values are variant positions
+    """
+
+    # Open the VCF
+    vcf_records = vcf.Reader(filename=vcf_path)
+    n_snps = 0
+    snp_reverse = {}
+    snp_forward = {}
+    region = np.zeros(end_pos + 1, dtype=int)
+    i = 0
+    for record in vcf_records.fetch(contig_name, 0, end_pos):
+        if record.POS < start_pos:
+            continue
+        if record.POS > end_pos:
+            continue
+
+        n_snps += 1
+        region[record.POS] = 1
+        snp_reverse[i] = record.POS
+        snp_forward[record.POS] = i
+        i += 1
+
+    return {
+        "N": n_snps,
+        "snp_fwd": snp_forward,
+        "snp_rev": snp_reverse,
+        "region": region,
+    }
+
+def process_bam(vcf_handler, bam_path, contig_name, start_pos, end_pos, L, use_end_sentinels, n_threads, debug_reads=False, debug_pos=False):
+    """
+    Initialise a Hansel structure and load variants from a BAM.
+
+    Parameters
+    ----------
+    vcf_handler : dict{str, any}
+        Variant metadata, as provided by :py:func:`gretel.gretel.process_vcf`.
+
+    bam_path : str
+        Path to the alignment BAM.
+
+    contig_name : str
+        The name of the contig for which to recover haplotypes.
+
+    start_pos : int
+        The 1-indexed genomic position from which to begin considering variants.
+
+    end_pos : int
+        The 1-indexed genomic position at which to stop considering variants.
+
+    L : int
+        The Gretel `L-parameter`, controlling the number of positions back
+        from the head of the current path (including the head) to consider
+        when calculating conditional probabilities.
+
+    use_end_sentinels : boolean
+        Whether or not to append an additional pairwise observation between
+        the final variant on a read towards a sentinel.
+
+    n_threads : int
+        Number of threads to spawn for reading the BAM
+
+    Returns
+    -------
+    Gretel Metastructure : dict
+        A collection of structures used for the execution of Gretel.
+        The currently used keys are:
+            read_support : :py:class:`hansel.hansel.Hansel`
+                The Hansel structure.
+            read_support_o : :py:class:`hansel.hansel.Hansel`
+                A copy of the Hansel structure stored with the intention of not reweighting its observations.
+            meta : dict{str, any}
+                A dictionary of metadata returned from the BAM parsing, such as
+                a list of the number of variants that each read spans.
+    """
+
+    #NOTE(samstudio8)
+    # Could we optimise for lower triangle by collapsing one of the dimensions
+    # such that Z[m][n][i][j] == Z[m][n][i + ((j-1)*(j))/2]
+
+
+    hansel, meta = util.load_from_bam(bam_path, contig_name, start_pos, end_pos, vcf_handler, use_end_sentinels, n_threads, debug_reads=debug_reads, debug_pos=debug_pos)
+
+    if hansel.L == 0:
+        hansel.L = meta["L"]
+        sys.stderr.write("[NOTE] Setting Gretel.L to %d\n" % hansel.L)
+
+    return {
+        "read_support": hansel,
+        "read_support_o": hansel.copy(),
+        "meta": meta,
+    }
+
+## PATH GENERATION ############################################################
+
+def append_path(path, next_m, next_v):
+    """
+    Append a selected variant to a given path.
+    .. deprecated:: 1.0
+        This method is somewhat of a stub.
+        It is likely to be deprecated at no notice in future.
+
+    Parameters
+    ----------
+    path : list{str}
+        The current sequence of variants representing a path (haplotype) in progress.
+
+    next_m : str
+        The symbol to append to the path.
+
+    next_v : float
+        The marginal probability of `next_m` at the current position.
+
+    Raises
+    ------
+    Exception
+        Raised if `next_m` is None.
+
+    """
+    #TODO(samstudio8) This is somewhat of a pointless stub, now.
+    #TODO(samstudio8) Probably a bit gross as it has side effects on path...
+    #TODO(samstudio8) Could probably raise an Exception for any next_m not in hansel.symbols?
+    if next_m is not None:
+        path.append(next_m)
+    else:
+        raise Exception("Cowardly refusing to append None as a nucleotide. Cheerio.")
+
+def generate_path(n_snps, hansel, original_hansel):
+    """
+    Explore and generate the most likely path (haplotype) through the observed Hansel structure.
+
+    Parameters
+    ----------
+    n_snps : int
+        The number of variants.
+
+    hansel : :py:class:`hansel.hansel.Hansel`
+        The Hansel structure currently being explored by Gretel.
+
+    original_hansel : :py:class:`hansel.hansel.Hansel`
+        A copy of the Hansel structure created by Gretel, before any reweighting.
+
+    Returns
+    -------
+    Path : list{str} or None
+        The sequence of variants that represent the completed path (or haplotype), or None
+        if one could not be successfully constructed.
+
+    Path Probabilities : dict{str, float}
+        The `unweighted` (orignal Hansel) and `weighted` (current Hansel) joint
+        probabilities of the variants in the returned path occurring together
+        in the given order.
+
+    Minimum Marginal : float
+        The smallest marginal distribution observed across selected variants.
+    """
+
+    # Cross the metahaplome in a greedily, naive fashion to establish a base path
+    # This seeds the rest of the path generation (we might want to just select
+    #   a random path here in future)
+
+    running_prob = 0.0
+    running_prob_uw = 0.0
+    current_path = ['_'] # start with the dummy
+    marginals = []
+
+    # Find path
+    sys.stderr.write("*** ESTABLISH ***\n")
+    for snp in range(1, n_snps+1):
+        #sys.stderr.write("\t*** ***\n")
+        #sys.stderr.write("\t[SNP_] SNP %d\n" % snp)
+
+        # Get marginal and calculate branch probabilities for each available
+        # mallele, given the current path seen so far
+        # Select the next branch and append it to the path
+        curr_branches = hansel.get_edge_weights_at(snp, current_path)
+        #sys.stderr.write("\t[TREE] %s\n" % curr_branches)
+        # Return the symbol and probability of the next base to add to the
+        # current path based on the best marginal
+        next_v = 0.0
+        next_m = None
+
+        for symbol in curr_branches:
+            if symbol == "total":
+                continue
+            if next_m is None:
+                next_v = curr_branches[symbol]
+                next_m = symbol
+            elif curr_branches[symbol] > next_v:
+                next_v = curr_branches[symbol]
+                next_m = symbol
+
+        if next_m == None:
+            sys.stderr.write('''[NOTE] Unable to select next branch from SNP %d to %d
+       By design, Gretel will attempt to recover haplotypes until a hole in the graph has been found.
+       Recovery will intentionally terminate now.\n''' % (snp-1, snp))
+            return None, None, None
+
+        selected_edge_weight = hansel.get_marginal_of_at(next_m, snp)
+        marginals.append(selected_edge_weight) #NOTE This isn't a log, as it is used as a ratio later
+
+        running_prob += log10(selected_edge_weight)
+        running_prob_uw += log10(original_hansel.get_marginal_of_at(next_m, snp))
+        append_path(current_path, next_m, next_v)
+
+    return current_path, {"unweighted": running_prob_uw, "weighted": running_prob}, min(marginals)
+
