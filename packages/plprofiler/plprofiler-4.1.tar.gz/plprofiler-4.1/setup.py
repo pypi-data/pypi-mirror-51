78ee3079b0b381c6bb67497acdd31be156d0586c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:52 +0000

    Add package plprofiler/4.1/plprofiler-4.1.tar.gz

---
+++ packages/plprofiler/plprofiler-4.1.tar.gz/plprofiler-4.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name = 'plprofiler',
+    description = 'PL/pgSQL Profiler module and command line tool',
+    version = '4.1',
+    author = 'Jan Wieck',
+    author_email = 'jan@wi3ck.info',
+    url = 'https://bitbucket.org/openscg/plprofiler/overview',
+    license = 'Artistic License and CDDL',
+    packages = ['plprofiler', ],
+    package_data = {
+        'plprofiler': [
+            'lib/FlameGraph/README',
+            'lib/FlameGraph/README-plprofiler',
+            'lib/FlameGraph/flamegraph.pl',
+        ],
+    },
+    install_requires = [
+        'psycopg2-binary',
+        'configparser',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'plprofiler = plprofiler:main',
+        ]
+    },
+)
