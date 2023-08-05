a7f45780556826fed7199d7fdefa087b32591dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:35 +0000

    Add package miniimagenettools/0.2.1/miniimagenettools-0.2.1.tar.gz

---
+++ packages/miniimagenettools/miniimagenettools-0.2.1.tar.gz/miniimagenettools-0.2.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='miniimagenettools',
+    version='0.2.1',
+    author='yaoyaoliu',
+    author_email='yaoyaoliu@outlook.com',
+    url='https://yyliu.net',
+    description=u'Tools for generating mini-ImageNet dataset and processing batches',
+    packages=['miniimagenettools'],
+    install_requires=[
+   'opencv-python',
+   'numpy',
+   'pillow',
+   'tqdm'
+]
+)
