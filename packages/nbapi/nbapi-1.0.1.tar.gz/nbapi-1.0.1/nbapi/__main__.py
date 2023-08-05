3b361bd926bab4e481c971d31ea927c86ad86fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:28 +0000

    Add package nbapi/1.0.1/nbapi-1.0.1.tar.gz

---
+++ packages/nbapi/nbapi-1.0.1.tar.gz/nbapi-1.0.1/nbapi/__main__.py
@@ -0,0 +1,19 @@
+import urllib
+class random():
+    def anime():
+        for line in urllib.request.urlopen('http://neko-bot.net/info/totalanime.txt'):
+                   
+            nekomax=line
+
+            nekomin=6
+        total=randint(int(nekomin),int(nekomax))
+        return 'http://neko-bot.net/anime/anime'+str(total)+'.png'
+
+    def neko():
+        for line in urllib.request.urlopen('http://neko-bot.net/info/totalnekos.txt'):
+                   
+            nekomax=line
+
+            nekomin=6
+        total=randint(int(nekomin),int(nekomax))
+        return 'http://neko-bot.net/nekos/neko'+str(total)+'.png'
