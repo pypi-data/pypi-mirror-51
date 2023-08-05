5f75b308cb6e14bcde01bbd7e6e079dda941d8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:26 +0000

    Add package gerby/0.1.0/gerby-0.1.0-py3-none-any.whl

---
+++ packages/gerby/gerby-0.1.0-py3-none-any.whl/gerby/clients/__init__.py
@@ -0,0 +1,5 @@
+from .forecast_client import ForecastClient
+from .glass_frog_client import GlassFrogClient
+from .guru_client import GuruClient
+from .harvest_client import HarvestClient
+from .slack_client import SlackClient
