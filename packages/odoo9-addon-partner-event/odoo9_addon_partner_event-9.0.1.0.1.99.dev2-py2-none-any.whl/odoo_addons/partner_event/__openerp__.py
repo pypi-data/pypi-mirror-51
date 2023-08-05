0a695c2c1c8a6e44af59241c19478b70c8d12e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:39 +0000

    Add package odoo9-addon-partner-event/9.0.1.0.1.99.dev2/odoo9_addon_partner_event-9.0.1.0.1.99.dev2-py2-none-any.whl

---
+++ packages/odoo9-addon-partner-event/odoo9_addon_partner_event-9.0.1.0.1.99.dev2-py2-none-any.whl/odoo_addons/partner_event/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2014 Tecnativa S.L. - Pedro M. Baeza
+# © 2015 Tecnativa S.L. - Javier Iniesta
+# © 2016 Tecnativa S.L. - Antonio Espinosa
+# © 2016 Tecnativa S.L. - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Link partner to events',
+    'version': '9.0.1.0.1',
+    'category': 'Marketing',
+    'author': 'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.tecnativa.com',
+    'depends': [
+        'event',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/res_partner_view.xml',
+        'views/event_event_view.xml',
+        'views/event_registration_view.xml',
+        'wizard/res_partner_register_event_view.xml',
+    ],
+    'installable': True,
+}
