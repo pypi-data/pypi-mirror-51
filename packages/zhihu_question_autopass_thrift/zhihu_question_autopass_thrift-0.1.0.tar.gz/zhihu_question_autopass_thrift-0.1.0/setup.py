b611935904f2fc3f460db47b1d9b79d5d85dd947
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:10 +0000

    Add package zhihu_question_autopass_thrift/0.1.0/zhihu_question_autopass_thrift-0.1.0.tar.gz

---
+++ packages/zhihu_question_autopass_thrift/zhihu_question_autopass_thrift-0.1.0.tar.gz/zhihu_question_autopass_thrift-0.1.0/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zhihu_question_autopass_thrift',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['thrift']
+)
