d5f8b042c91f63541fb938bf47980bc266578738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:31 +0000

    Add package hackingtools/0.9.939.482/hackingtools-0.9.939.482-py3-none-any.whl

---
+++ packages/hackingtools/hackingtools-0.9.939.482-py3-none-any.whl/hackingtools/modules/crackers/unzip/ht_unzip.py
@@ -0,0 +1,27 @@
+from hackingtools.core import Logger, Utils, Config
+import hackingtools as ht
+import os
+
+from zipfile import ZipFile
+
+config = Config.getConfig(parentKey='modules', key='ht_unzip')
+output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'output'))
+
+class StartModule():
+
+	def __init__(self):
+		Utils.emptyDirectory(output_dir)
+		pass
+
+	def help(self):
+		Logger.printMessage(message=ht.getFunctionsNamesFromModule('ht_unzip'))
+
+	def extractFile(self, zipPathName, password):
+		#ZipFile only works with 7z with ZypCrypto encryption for setting the password
+		try:
+			Logger.printMessage(message="extractFile", description='ZIP - {pwd}'.format(pwd=password), debug_core=True)
+			with ZipFile(zipPathName) as zf:
+				zf.extractall(pwd=str.encode(password))
+			return password
+		except Exception as e:
+			return None
