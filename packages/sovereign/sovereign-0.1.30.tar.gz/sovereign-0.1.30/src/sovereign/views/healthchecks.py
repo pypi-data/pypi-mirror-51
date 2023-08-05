31f7a34f7aa198e0322176c6389b435c0edc0cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:05 +0000

    Add package sovereign/0.1.30/sovereign-0.1.30.tar.gz

---
+++ packages/sovereign/sovereign-0.1.30.tar.gz/sovereign-0.1.30/src/sovereign/views/healthchecks.py
@@ -0,0 +1,35 @@
+import random
+from quart import Blueprint
+from sovereign import XDS_TEMPLATES, __version__
+from sovereign import discovery
+from sovereign.sources import load_sources
+from sovereign.utils.mock import mock_discovery_request
+
+
+blueprint = Blueprint('healthchecks', __name__)
+
+xds_version = list(XDS_TEMPLATES.keys())[-1]
+
+
+@blueprint.route('/healthcheck')
+def health_check():
+    return 'OK'
+
+
+@blueprint.route('/deepcheck')
+def deep_check():
+    template = random.choice(
+        list(XDS_TEMPLATES[xds_version].keys())
+    )
+    discovery.response(
+        mock_discovery_request(),
+        xds=template,
+        debug=True
+    )
+    load_sources(service_cluster='', debug=True)
+    return 'OK'
+
+
+@blueprint.route('/version')
+def version_check():
+    return __version__
