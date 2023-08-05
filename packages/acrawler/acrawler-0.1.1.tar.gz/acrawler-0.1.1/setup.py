65918b6ac471118a607d38687f0ceecd41d87375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:03 +0000

    Add package acrawler/0.1.1/acrawler-0.1.1.tar.gz

---
+++ packages/acrawler/acrawler-0.1.1.tar.gz/acrawler-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+NAME = "acrawler"
+DESCRIPTION = "A simple web-crawling framework, based on aiohttp."
+URL = "https://github.com/wooddance/aCrawler"
+EMAIL = "zireael.me@gmail.com"
+AUTHOR = "wooddance"
+VERSION = "0.1.1"
+REQUIRED = ["aiohttp", "parsel", "cchardet"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=open("README.rst").read(),
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    python_requires=">=3.6.0",
+    install_requires=REQUIRED,
+    py_modules=["acrawler"],
+)
