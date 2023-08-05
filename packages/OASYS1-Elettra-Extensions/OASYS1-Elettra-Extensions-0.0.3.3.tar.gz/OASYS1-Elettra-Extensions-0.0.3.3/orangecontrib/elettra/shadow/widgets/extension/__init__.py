cb3a15e316dea63bef607a6ee556c461c54797ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:48 +0000

    Add package OASYS1-Elettra-Extensions/0.0.3.3/OASYS1-Elettra-Extensions-0.0.3.3.tar.gz

---
+++ packages/OASYS1-Elettra-Extensions/OASYS1-Elettra-Extensions-0.0.3.3.tar.gz/OASYS1-Elettra-Extensions-0.0.3.3/orangecontrib/elettra/shadow/widgets/extension/__init__.py
@@ -0,0 +1,11 @@
+# Category description for the widget registry
+
+NAME = "ShadowOui Elettra Extension"
+
+DESCRIPTION = "Widgets for ShadowOui"
+
+BACKGROUND = "#0099cc"
+
+ICON = "icons/LogoElettra_transparent.png"
+
+PRIORITY = 130
