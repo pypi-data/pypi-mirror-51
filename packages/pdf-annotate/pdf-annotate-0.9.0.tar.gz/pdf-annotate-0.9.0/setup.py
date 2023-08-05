42899a33f0bdd7f73ef06bacec751c86732a9e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:26 +0000

    Add package pdf-annotate/0.9.0/pdf-annotate-0.9.0.tar.gz

---
+++ packages/pdf-annotate/pdf-annotate-0.9.0.tar.gz/pdf-annotate-0.9.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='pdf-annotate',
+    version='0.9.0',
+    description='Add annotations to PDFs',
+    author='Michael Bryant',
+    author_email='smart-recordset@plangrid.com',
+    url='https://github.com/plangrid/pdf-annotate',
+    packages=find_packages('.', exclude=['tests*']),
+    include_package_data=True,
+    install_requires=[
+        'attrs>=18.1.0',  # this could probably be lower, but it's not tested
+        'pdfrw>=0.4',
+        'pillow>=5.2.0',  # this could probably be lower, but it's not tested
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
