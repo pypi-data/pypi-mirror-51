3c52bfbc088102b045dbaab5241af1ecbd83f132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:45:50 +0000

    Add package django-base-publications/0.0.3/django-base-publications-0.0.3.tar.gz

---
+++ packages/django-base-publications/django-base-publications-0.0.3.tar.gz/django-base-publications-0.0.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['django_base_publications',
+ 'django_base_publications.migrations',
+ 'django_base_publications.models']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['django-autoslug>=1.9,<2.0',
+ 'django-taggit>=1.1,<2.0',
+ 'django>=1.11,<3.0',
+ 'funcy>=1.13,<2.0',
+ 'ipython>=7.7,<8.0']
+
+setup_kwargs = {
+    'name': 'django-base-publications',
+    'version': '0.0.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Дмитрий',
+    'author_email': 'acrius@mail.ru',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
