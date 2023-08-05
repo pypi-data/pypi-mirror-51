2a028431e24aa5cc2be19758ae3197253f86c40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:15 +0000

    Add package lanelet2-parser/0.1.1/lanelet2_parser-0.1.1-py3-none-any.whl

---
+++ packages/lanelet2-parser/lanelet2_parser-0.1.1-py3-none-any.whl/ll2_parser/__init__.py
@@ -0,0 +1,10 @@
+from parser import *
+
+__copyright__    = 'Copyright (C) 2019 Tier IV, Inc.'
+__version__      = '1.0.0'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Yuki Iida'
+__author_email__ = 'yuki.iida@tier4.jp'
+__url__          = 'https://github.com/tier4/ll2-parser'
+
+__all__ = ['lanelet2_parser']
---
+++ packages/lanelet2-parser/lanelet2_parser-0.1.1-py3-none-any.whl/ll2_parser/exceptions.py
@@ -0,0 +1,17 @@
+class AppException(Exception):
+    error_code = ''
+
+    def __init__(self, message=None):
+        if message is not None:
+            self.message = message
+
+    def to_dict(self):
+        return {
+            'message': self.message
+        }
+
+class InvalidFileFormatError(AppException):
+    message = 'OSM file does not have the correct format.'
+
+class MissingResourceError(AppException):
+    message = 'File not found.'
