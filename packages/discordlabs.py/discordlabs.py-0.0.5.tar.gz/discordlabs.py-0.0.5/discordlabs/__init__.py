ed5505a8d1d0ae1c024379e87d9fe63d7e36265c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:58 +0000

    Add package discordlabs.py/0.0.5/discordlabs.py-0.0.5.tar.gz

---
+++ packages/discordlabs.py/discordlabs.py-0.0.5.tar.gz/discordlabs.py-0.0.5/discordlabs/__init__.py
@@ -0,0 +1,13 @@
+__title__ = 'discordlabs.py'
+__author__ = 'Anish Anne'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Anish Anne'
+__version__ = '0.0.5'
+
+name="discordlabs"
+from .client import Client
+from collections import namedtuple
+
+VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
+
+version_info = VersionInfo(major=0, minor=0, micro=5, releaselevel='final', serial=0)
