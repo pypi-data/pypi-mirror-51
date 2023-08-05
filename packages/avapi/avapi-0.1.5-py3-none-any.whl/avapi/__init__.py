7b2c7af91a0cc6d3aae69ff4c8dfdf494a80a740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:07 +0000

    Add package avapi/0.1.5/avapi-0.1.5-py3-none-any.whl

---
+++ packages/avapi/avapi-0.1.5-py3-none-any.whl/avapi/__init__.py
@@ -0,0 +1,10 @@
+'''Package for getting and manipulating data from Alpha Vantage
+
+Functions: get_data(), response().
+'''
+
+__authors__ = "Ole Olaussen, Xuan Ling"
+__email__ = "ole.olaussen@ekkobit.com, xuan.ling@ekkobit.com"
+
+
+from .data import get_data, to_df, response
