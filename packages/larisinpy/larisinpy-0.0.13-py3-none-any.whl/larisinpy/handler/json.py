003345d0d2d5f56e24d21e5bbdd92d6c775c37b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:58 +0000

    Add package larisinpy/0.0.13/larisinpy-0.0.13-py3-none-any.whl

---
+++ packages/larisinpy/larisinpy-0.0.13-py3-none-any.whl/larisinpy/handler/json.py
@@ -0,0 +1,20 @@
+
+class JsonUtils(object):
+
+    def __init__(self):
+        pass
+
+    def checkProperty(self, json_file, property):
+
+        if isinstance(property, list):
+            try:
+                value = json_file[property[0]][property[1]]
+            except:
+                value = "ERROR404"
+        else:
+            try:
+                value = json_file[propertys]
+            except:
+                value = "ERROR404"
+
+        return value
