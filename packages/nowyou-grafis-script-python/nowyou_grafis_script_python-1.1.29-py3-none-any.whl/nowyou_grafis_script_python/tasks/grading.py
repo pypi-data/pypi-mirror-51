4ab2050eb88c8d3953551c671ee7d411249472b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:23 +0000

    Add package nowyou-grafis-script-python/1.1.29/nowyou_grafis_script_python-1.1.29-py3-none-any.whl

---
+++ packages/nowyou-grafis-script-python/nowyou_grafis_script_python-1.1.29-py3-none-any.whl/nowyou_grafis_script_python/tasks/grading.py
@@ -0,0 +1,36 @@
+from ..ui_tools.UI_tools import Ui_tools
+from ..utils.utils import resource_path
+
+class Grading:
+
+    @staticmethod
+    def run():
+        Grading.create_size()
+
+    # 3a
+    @staticmethod
+    def create_size():
+        Ui_tools.click_on('\static\\3ai.png')
+        Ui_tools.click_on('\static\\3aii.png')
+        Ui_tools.click_on('\static\\3aiii.png')
+        Ui_tools.click_on('\static\\3aiv.png')
+        Ui_tools.click_on('\static\\3av.png')
+
+        # TODO make as input parameter
+        Ui_tools.type('Test-name')
+
+        Ui_tools.hot_key('enter')
+
+        Ui_tools.click_on('\static\\3aviii.png')
+
+        position = Ui_tools.getPosition(resource_path('\static\\3aix.png'))
+        Ui_tools.click((230 + position[0], position[1]))
+
+        # TODO make as input parameter
+        Ui_tools.type('610')
+
+        Ui_tools.hot_key('esc')
+        Ui_tools.hot_key('ctrl', 's')
+        Ui_tools.click_on('\static\\3axii.png')
+        Ui_tools.hot_key('esc')
+        Ui_tools.hot_key('esc')
