8a14027bc2f4377f8ac49817ca556ba65ae4ae47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:29 +0000

    Add package libdiatonic/0.2/libdiatonic-0.2-py3-none-any.whl

---
+++ packages/libdiatonic/libdiatonic-0.2-py3-none-any.whl/libdiatonic/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+# flatten the structure here for ease of use
+
+from . import pitch
+from .pitch.chord import *
+from .pitch.interval import *
+from .pitch.key import *
+from .pitch.mode import *
+from .pitch.note import *
+
+from . import meter
+from .meter.duration import *
+from .meter.measure import *
