4a2b83fbf04780cd0ddb2d70c5074a9fa6fce52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:10 +0000

    Add package shapeletpy/0.9.2/shapeletpy-0.9.2.tar.gz

---
+++ packages/shapeletpy/shapeletpy-0.9.2.tar.gz/shapeletpy-0.9.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='shapeletpy',
+      version='0.9.2',
+      description='Time series classification Using Shapelets',
+      url='https://github.com/rohitvincent/shapeletpy',
+      author='Rohit Vincent',
+      author_email='rohit.vincent@ucdconnect.ie',
+      license='MIT',
+      packages=['shapeletpy'],
+      zip_safe=False)
