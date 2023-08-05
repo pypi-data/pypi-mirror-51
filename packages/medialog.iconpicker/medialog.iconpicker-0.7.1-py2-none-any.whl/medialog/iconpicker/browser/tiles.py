c6b98bb3588a41c195389d2dfd09891ccb2d0778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:29 +0000

    Add package medialog.iconpicker/0.7.1/medialog.iconpicker-0.7.1-py2-none-any.whl

---
+++ packages/medialog.iconpicker/medialog.iconpicker-0.7.1-py2-none-any.whl/medialog/iconpicker/browser/tiles.py
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+from plone.app.tiles.browser.add import DefaultAddForm
+from plone.app.tiles.browser.add import DefaultAddView
+from plone.app.tiles.browser.edit import DefaultEditForm
+from plone.app.tiles.browser.edit import DefaultEditView
+from plone.memoize.view import memoize
+from plone.supermodel import model
+from plone.tiles import Tile
+from plone.tiles.data import TransientTileDataManager
+from plone.tiles.interfaces import ITileDataManager
+from zope import schema
+from zope.i18nmessageid import MessageFactory
+#from zope.interface import provider
+
+from plone.autoform.directives import widget
+
+#????
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.publisher.browser import BrowserView
+from zope.schema import getFields
+from plone.tiles.interfaces import ITileType
+
+
+from collective.z3cform.datagridfield import DataGridFieldFactory
+from collective.z3cform.datagridfield import DictRow
+
+
+_ = MessageFactory('medialog.iconpicker')
+
+
+from medialog.iconpicker.widgets.widget import IconPickerFieldWidget
+from medialog.iconpicker.widgets.widget import ColorPickerFieldWidget
+from medialog.iconpicker.interfaces import IIconPickerSettings
+
+
+class IIconTile(model.Schema):
+    iconfield = schema.TextLine(
+        title = _("icon", default=u"Icon"),
+        required = False,
+        description = _("help_icon",
+                      default="Choose Icon"),
+    )
+
+    widget(
+            iconfield=IconPickerFieldWidget,
+    )
+
+
+    color = schema.TextLine(
+        title = _("color", default=u"Color"),
+        required = False,
+        description = _("help_color",
+                      default="Choose Color"),
+    )
+
+    widget(
+            color=ColorPickerFieldWidget,
+    )
+
+    title =schema.TextLine(
+        title = _("title", default=u"Title"),
+        required = False,
+        description = _("help_tittel",
+                      default="Title"),
+    )
+
+    text =schema.TextLine(
+        title = _("text", default=u"Text"),
+        required = False,
+        description = _("help_text",
+                      default="Text"),
+    )
+
+    link = schema.URI(title=u"Link",
+      required = False,
+    )
+
+
+    css_class =schema.TextLine(
+        title = _("css class", default=u"CSS class"),
+        required = False,
+        description = _("help_css_class",
+                      default="CSS Class"),
+    )
+
+
+
+class IconTile(Tile):
+    """A tile that displays icon and some text"""
+
+    def __init__(self, context, request):
+        super(IconTile, self).__init__(context, request)
+
+    @property
+    def data(self):
+        data = super(IconTile, self).data
+        return data
+
+    @property
+    def family_css(self):
+        #return css_family_class, like fa, wi
+        iconset = self.iconset()
+        if iconset == 'glyphicon':
+             return 'glyphicon'
+        if iconset == 'mapicon':
+            return 'map-icons'
+        if iconset == 'typicon':
+            return 'typcn'
+        if iconset == 'ionicon':
+            return 'ionicons'
+        if iconset == 'weathericon':
+            return 'wi'
+        if iconset == 'octicon' :
+            return 'octicon'
+        if iconset == 'elusiveicon':
+            return 'el-icon'
+        if iconset == 'medialogfont':
+            return 'medialogfont'
+        if iconset == 'iconpickerfont':
+            return 'iconpickerfont'
+        if iconset == 'lineawsome':
+            return 'linewsome'
+        return 'fa'
+
+    def iconset(self):
+        """Returns current iconset name This is also used for loading the resources below"""
+        return api.portal.get_registry_record('medialog.iconpicker.interfaces.IIconPickerSettings.iconset')
+
+
+class IPair(model.Schema):
+    iconfield = schema.TextLine(
+        title = _("icon", default=u"Icon"),
+        required = False,
+        description = _("help_icon",
+                      default="Choose Icon"),
+    )
+
+    widget(
+            iconfield=IconPickerFieldWidget,
+    )
+
+
+    title =schema.TextLine(
+        title = _("title", default=u"Title"),
+        required = False,
+        description = _("help_tittel",
+                      default="Title"),
+    )
+
+    text =schema.TextLine(
+        title = _("text", default=u"Text"),
+        required = False,
+        description = _("help_text",
+                      default="Text"),
+    )
+
+
+class IMultiIconTile(model.Schema):
+
+    color = schema.TextLine(
+        title = _("color", default=u"Color"),
+        required = False,
+        description = _("help_color",
+                      default="Choose Color"),
+    )
+
+    widget(
+            color=ColorPickerFieldWidget,
+    )
+
+
+    css_class =schema.TextLine(
+        title = _("css class", default=u"CSS class"),
+        required = False,
+        description = _("help_css_class",
+                      default="CSS Class"),
+    )
+
+    widget(iconpairs=DataGridFieldFactory)
+    iconpairs = schema.List(
+        title = _(u"icon text_pairs",
+            default=u"Icon Text pairs"),
+        value_type= DictRow(schema=IPair),
+        required=False
+    )
---
+++ packages/medialog.iconpicker/medialog.iconpicker-0.7.1-py2-none-any.whl/medialog/iconpicker/interfaces.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# Default ENCODING = 'UTF-8'
+
+####################
+from z3c.form import interfaces
+from zope import schema
+#from zope.interface import Interface
+from zope.interface import alsoProvides
+from plone.supermodel import model
+
+from medialog.controlpanel.interfaces import IMedialogControlpanelSettingsProvider
+
+from zope.i18nmessageid import MessageFactory
+
+_ = MessageFactory('medialog.iconpicker')
+
+class IIconPickerSettings(model.Schema):
+    """Adds settings to medialog.controlpanel
+    https://erikflowers.github.io/weather-icons/
+	http://www.typicons.com/
+	https://octicons.github.com/
+	http://ionicons.com/
+	http://elusiveicons.com/cheatsheet/
+	https://fortawesome.github.io/Font-Awesome/icons/
+	http://map-icons.com/
+    """
+
+    model.fieldset(
+        'iconpicker',
+        label=_(u'Iconpicker settings'),
+        fields=[
+             'iconset',
+             'cols',
+             'rows',
+             'placement',
+             'loadbootstrap',
+        ],
+     )
+
+    iconset = schema.Choice(
+        title=_(u"label_iconset", default=u"Iconset"),
+        description=_(u"help_iconset",
+        default=u"""Choose iconset to be used for iconpicker.Some links:
+        <a href="https://erikflowers.github.io/weather-icons/">Weather-icons</a> •
+        <a href="http://www.typicons.com">Typicons</a> •
+        <a href="https://octicons.github.com/">Octicons</a> •
+        <a href="http://ionicons.com/">Ionicons</a>  •
+        <a href="">http://elusiveicons.com/cheatsheet/</a>  •
+        <a href="https://fortawesome.github.io/Font-Awesome/icons/">Font Awsome</a>  •
+        <a href="http://map-icons.com/">map-icons</a>
+        """),
+        values=[('glyphicon'), ('ionicon'), ('fontawesome'), ('weathericon'), ('mapicon'), ('octicon'), ('typicon'), ('elusiveicon'), ('medialogfont'), ('iconpickerfont'), ('lineawesome'),]
+    )
+
+    loadbootstrap = schema.Bool(
+     	title=_(u"label_loadbootstrap", default=u"Load Bootstrap"),
+        description=_(u"help_loadbootstrap",
+            default=u"""Loads ++resource++collective.js.bootstrap/js/bootstrap.min.js. <br/>
+            You probably want to do this in your diazo theme instead.""")
+    )
+
+    cols = schema.Int (
+    	title=_(u"label_columns", default=u"Columns"),
+    )
+
+    rows = schema.Int (
+    	title=_(u"label_rows", default=u"Rows"),
+    )
+
+    placement = schema.Choice(
+        title=_(u"label_placement", default=u"Placement"),
+        values=[('left'), ('top'), ('bottom'), ('right'),]
+    )
+
+
+alsoProvides(IIconPickerSettings, IMedialogControlpanelSettingsProvider)
