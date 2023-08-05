730fd8dcf89f2a42740c3ea196d012fdbb73c085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:53 +0000

    Add package reconocimiento/1.1.1/reconocimiento-1.1.1.tar.gz

---
+++ packages/reconocimiento/reconocimiento-1.1.1.tar.gz/reconocimiento-1.1.1/README.md
@@ -0,0 +1,7 @@
+#Paquete para Reconocer la placa de un automovil
+#Y registrar en un archivo txt el nombre del cliente
+#el numero de placa diciendo de que tipo es
+
+#Pack to recognize the license plate of a car
+#And register the client's name in a txt file
+#the plate number saying what type it is
---
+++ packages/reconocimiento/reconocimiento-1.1.1.tar.gz/reconocimiento-1.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "reconocimiento",
+    packages = ["Package"],
+    version = "1.1.1",
+    description = "Este paquete nos permite reconocer y registrar           placas",
+    author = "Sebastian Zambrano",
+    author_email = "Sebastianrza@gmail.com",
+    url = "",
+    keyword =["Placas","automovil","Reconocimiento"]
+)
