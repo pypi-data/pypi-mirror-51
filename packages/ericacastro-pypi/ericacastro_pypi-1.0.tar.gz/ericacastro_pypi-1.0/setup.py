7068f0b44b1cfd026114f6e6749fa91cb915fdd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:57 +0000

    Add package ericacastro-pypi/1.0/ericacastro_pypi-1.0.tar.gz

---
+++ packages/ericacastro-pypi/ericacastro_pypi-1.0.tar.gz/ericacastro_pypi-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    	name='ericacastro_pypi',
+		version='1.0',
+    	author='Castro',
+    	author_email='castro@araujo.com',
+    	description='Brincando com distribuição python',
+    	py_modules = ['castro']
+	)
