8cfc3a874af6a1bce78dd6a23547be9f837f3a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:33 +0000

    Add package qcrit/0.0.12/qcrit-0.0.12.tar.gz

---
+++ packages/qcrit/qcrit-0.0.12.tar.gz/qcrit-0.0.12/setup.py
@@ -0,0 +1,25 @@
+'''
+Setup
+'''
+import setuptools
+
+with open('README.md', 'r') as fh:
+	LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+	name='qcrit',
+	version='0.0.12',
+	author='Tim Gianitsos',
+	author_email='contact@qcrit.org',
+	description='Quantitative Criticism Lab',
+	long_description=LONG_DESCRIPTION,
+	long_description_content_type='text/markdown',
+	url='https://www.qcrit.org',
+	project_urls={'Source Code': 'https://github.com/QuantitativeCriticismLab/qcrit'},
+	packages=setuptools.find_packages(),
+	classifiers=[
+		'Programming Language :: Python :: 3.6',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+)
