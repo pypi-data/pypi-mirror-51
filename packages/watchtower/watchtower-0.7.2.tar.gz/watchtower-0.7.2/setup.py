e239092f487345dd59f728d42c0161e7938fe0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:47 +0000

    Add package watchtower/0.7.2/watchtower-0.7.2.tar.gz

---
+++ packages/watchtower/watchtower-0.7.2.tar.gz/watchtower-0.7.2/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
+tests_require = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "test-requirements.txt"))]
+
+setup(
+    name="watchtower",
+    version="0.7.2",
+    url="https://github.com/kislyuk/watchtower",
+    license="Apache Software License",
+    author="Andrey Kislyuk",
+    author_email="kislyuk@gmail.com",
+    description="Python CloudWatch Logging",
+    long_description=open("README.rst").read(),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    packages=find_packages(exclude=["test"]),
+    platforms=["MacOS X", "Posix"],
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
