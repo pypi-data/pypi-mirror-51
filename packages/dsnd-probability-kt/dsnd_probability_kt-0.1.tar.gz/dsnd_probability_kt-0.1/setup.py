de003b5cfa16d0f8ddbbe5756d71e0e995476bea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package dsnd-probability-kt/0.1/dsnd_probability_kt-0.1.tar.gz

---
+++ packages/dsnd-probability-kt/dsnd_probability_kt-0.1.tar.gz/dsnd_probability_kt-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_kt',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dsnd_probability_kt'],
+      author = 'Kayleigh Torok',
+      zip_safe=False)
