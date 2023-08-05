2f4cf08316e312c00f2dcea0ce86e3c03b8b07e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:41 +0000

    Add package mldatautils/0.0.4/mldatautils-0.0.4.tar.gz

---
+++ packages/mldatautils/mldatautils-0.0.4.tar.gz/mldatautils-0.0.4/mldatautils/db_utils.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from configparser import ConfigParser
+from sqlalchemy import create_engine
+
+def _config_parse(config_file):
+    if config_file is not None:
+        try:
+            configs = ConfigParse()
+            configs.read(config_file)
+            dwh_schema = dict(configs.items('dwh_schema'))
+        except FileNotFoundError:
+            logger.error(f'{config_file} does not exist.')
+            sys.exit(1)
+    else:
+        dwh_config = {
+            'username': os.getenv('DB_USERNAME'),
+            'password': os.getenv('DB_PASSWORD'),
+            'hostname': os.getenv('DB_HOSTNAME'),
+            'port': os.getenv('DB_PORT'),
+            'database': os.getenv('DATABASE'),
+        }
+    return config_file
+
+def create_engine(config_file=None):
+    dwh_config = _config_parse(config_file)
+    url = 'postgres://{user}:{password}@{host}:{port}/{database}'.format(**dwh_config)
+    return create_engine(url)
+
