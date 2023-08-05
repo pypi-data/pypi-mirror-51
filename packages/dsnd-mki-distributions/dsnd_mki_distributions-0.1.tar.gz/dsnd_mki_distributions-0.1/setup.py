7754707a9b891c01dfbd56b582c04cafb09b789e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:28 +0000

    Add package dsnd-mki-distributions/0.1/dsnd_mki_distributions-0.1.tar.gz

---
+++ packages/dsnd-mki-distributions/dsnd_mki_distributions-0.1.tar.gz/dsnd_mki_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_mki_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_mki_distributions'],
+      zip_safe=False)
