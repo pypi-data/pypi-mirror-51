1fb41c0e8d70fdbe59d368dfbfb5f47f5c75a5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:58 +0000

    Add package mkdocs-multilang/0.1.1/mkdocs-multilang-0.1.1.tar.gz

---
+++ packages/mkdocs-multilang/mkdocs-multilang-0.1.1.tar.gz/mkdocs-multilang-0.1.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mkdocs-multilang',
+    version='0.1.1',
+    description='A MkDocs plugin',
+    long_description='',
+    keywords='mkdocs',
+    url='',
+    author='Avente LLC',
+    author_email='support@avente.com',
+    license='MIT',
+    python_requires='>=2.7',
+    install_requires=[
+        'mkdocs>=1.0.4'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'mkdocs.plugins': [
+            'multilang = mkdocs_multilang.plugin:MultiLang'
+        ]
+    }
+)
