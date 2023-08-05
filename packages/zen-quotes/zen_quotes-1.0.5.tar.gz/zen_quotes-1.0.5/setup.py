3affc7dfe31f46bbd0049c5ec264e0a373946fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:46 +0000

    Add package zen-quotes/1.0.5/zen_quotes-1.0.5.tar.gz

---
+++ packages/zen-quotes/zen_quotes-1.0.5.tar.gz/zen_quotes-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='zen_quotes',
+    packages = ['zen_quotes'],
+    version='1.0.5',
+    description='A sample lib Zen of Python',
+    author='Yuri Mussi',
+    author_email='ymussi@gmail.com',
+    license='BSD',
+    url='https://github.com/ymussi/zen_quotes',
+    keywords = [],
+    classifiers = [],
+    install_requires = ['requests']
+    )
