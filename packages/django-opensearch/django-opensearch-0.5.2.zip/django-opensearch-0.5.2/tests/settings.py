edbdd260177d7f21a3ee79189733a478fadd54b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:30 +0000

    Add package django-opensearch/0.5.2/django-opensearch-0.5.2.zip

---
+++ packages/django-opensearch/django-opensearch-0.5.2.zip/django-opensearch-0.5.2/tests/settings.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+# django-opensearch
+# settings/settings.py
+
+
+import pathlib
+import sys
+
+
+# black magic to use imports from library code
+sys.path.insert(0, str(pathlib.Path(__file__).absolute().parent.parent.parent))
+
+# secret key
+SECRET_KEY = "django-opensearch-test-key"  # type: str
+
+# security settings
+ALLOWED_HOSTS = ["example.com"]
+
+# configure databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "django-opensearch-tests.sqlite3",
+    }
+}  # type: dict
+
+# configure templates
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {},
+    }
+]  # type: list
+
+
+# add nose test runner application and django-opensearch
+INSTALLED_APPS = ["django_nose", "opensearch"]  # type: list
+
+# add nose test runner
+TEST_RUNNER = "django_nose.NoseTestSuiteRunner"  # type: str
+
+# configure nose test runner
+NOSE_ARGS = [
+    "--rednose",
+    "--force-color",
+    "--with-timer",
+    "--with-doctest",
+    "--with-coverage",
+    "--cover-inclusive",
+    "--cover-erase",
+    "--cover-package=opensearch",
+    "--logging-clear-handlers",
+]  # type: list
+
+# configure urls
+ROOT_URLCONF = "opensearch.urls"  # type: str
+
+# opensearch settings
+OPENSEARCH_CONTACT_EMAIL = "test@example.com"  # type: str
+OPENSEARCH_SHORT_NAME = "opensearch"  # type: str
+OPENSEARCH_DESCRIPTION = "Search engine human-readable text description"  # type: str
+OPENSEARCH_FAVICON_WIDTH = 16  # type: int
+OPENSEARCH_FAVICON_HEIGHT = 16  # type: int
+OPENSEARCH_FAVICON_TYPE = "image/x-icon"  # type str
+OPENSEARCH_FAVICON_FILE = "favicon.ico"  # type: str
+OPENSEARCH_SEARCH_URL = "search"  # type: str
+OPENSEARCH_SEARCH_QUERYSTRING = "q="  # type: str
+OPENSEARCH_INPUT_ENCODING = "UTF-8"  # type: str
