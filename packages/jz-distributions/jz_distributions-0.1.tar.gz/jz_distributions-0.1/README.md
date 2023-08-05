978467fda91c4de626d3f3793c83da01a2550d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:10 +0000

    Add package jz-distributions/0.1/jz_distributions-0.1.tar.gz

---
+++ packages/jz-distributions/jz_distributions-0.1.tar.gz/jz_distributions-0.1/README.md
@@ -0,0 +1,1 @@
+This package contains some distribution classes.
---
+++ packages/jz-distributions/jz_distributions-0.1.tar.gz/jz_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jz_distributions',
+      version='0.1',
+      description='Gaussian distribution and binomial distribution',
+      packages=['jz_distributions'],
+      zip_safe=False)
