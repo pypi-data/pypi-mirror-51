fb0c1d52732bd59ca9e95d922fe27c59abd25d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:43:43 +0000

    Add package XTCryptoSignals/0.1.7/XTCryptoSignals-0.1.7-py3-none-any.whl

---
+++ packages/XTCryptoSignals/XTCryptoSignals-0.1.7-py3-none-any.whl/xtcryptosignals/settings_local.py
@@ -0,0 +1,15 @@
+__author__ = "Paulo Antunes"
+__copyright__ = "Copyright 2018, XTCryptoSignals"
+__credits__ = ["Paulo Antunes", ]
+__license__ = "GPL"
+__maintainer__ = "Paulo Antunes"
+__email__ = "pjmlantunes@gmail.com"
+
+
+BINANCE_API_KEY = ''  # Add Personal Binance API KEY
+BINANCE_API_SECRET = ''  # Add Personal Binance API SECRET
+
+CORS_ALLOWED_ORIGINS = (
+    'http://127.0.0.1:8000',
+    'https://xtcryptosignals.com',
+)
