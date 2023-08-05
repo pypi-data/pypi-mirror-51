03e7637c65b09e1b9d414233e609f2c58112f534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:39 +0000

    Add package DaPy/1.10.3/DaPy-1.10.3-py2.py3-none-any.whl

---
+++ packages/DaPy/DaPy-1.10.3-py2.py3-none-any.whl/DaPy/methods/__init__.py
@@ -0,0 +1,3 @@
+from .classifiers import MLPClassifier, DecisionTreeClassifier, LogistClassifier
+from .statistic import ANOVA, DiscriminantAnalysis, RunTest, CoxStautTest, SignTest, WicoxonTest, IndependentTTest
+from .evaluator import Performance
