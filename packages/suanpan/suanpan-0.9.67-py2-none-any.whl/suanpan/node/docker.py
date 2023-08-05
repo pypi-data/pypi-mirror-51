f213e2a81615c095c45fa468d1d17af49c8194a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:47 +0000

    Add package suanpan/0.9.67/suanpan-0.9.67-py2-none-any.whl

---
+++ packages/suanpan/suanpan-0.9.67-py2-none-any.whl/suanpan/node/docker.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from lostc import collection as lcc
+from suanpan.node.base import BaseNode, Param, Port
+
+
+class DockerNode(BaseNode):
+    pass
