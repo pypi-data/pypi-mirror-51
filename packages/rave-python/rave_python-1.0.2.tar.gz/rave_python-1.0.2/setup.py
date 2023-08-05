1f486beab763603b8cd0c157e841bb461082c6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:36 +0000

    Add package rave-python/1.0.2/rave_python-1.0.2.tar.gz

---
+++ packages/rave-python/rave_python-1.0.2.tar.gz/rave_python-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rave_python",
+    version="1.0.2",
+    author="Flutterwave",
+    author_email="developers@flutterwavego.com",
+    description="Official Rave Python Wrapper By Flutterwave",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Flutterwave/rave-python",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires = [
+        'pycryptodome',
+        'requests'
+    ]
+)
