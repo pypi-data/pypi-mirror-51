6cf84f434efa3e359384070abac1bb36aecac83b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:04 +0000

    Add package pipgen/0.2.2/pipgen-0.2.2-py3-none-any.whl

---
+++ packages/pipgen/pipgen-0.2.2-py3-none-any.whl/pipgen-0.2.2.data/scripts/depipify.sh
@@ -0,0 +1,20 @@
+#this is an 'undo' tool, meant only for testing
+
+if [ $# -lt 1 ]; then
+  printf "\nPACKAGE FOLDER PATH REQUIRED\n"
+  exit
+fi
+cd $1
+
+if [ ! -f "setup.py" ]; then
+  printf "\nPROJECT ALREADY DE-PIPIFIED\n"
+  exit
+fi
+
+PKG_NAME=${PWD##*/}
+for DIR in ./*/ ./*/**/; do
+  rm "$DIR"/__init__.py 2>/dev/null || true
+done
+mv */* . 2>/dev/null || true
+rm setup.py
+rm -rf "$PKG_NAME" 2>/dev/null || true
