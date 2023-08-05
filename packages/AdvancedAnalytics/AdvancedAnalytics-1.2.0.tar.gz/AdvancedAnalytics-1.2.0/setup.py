76a9ba0dd5dcccc9bb080f07b48f84f187d8fcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:36 +0000

    Add package AdvancedAnalytics/1.2.0/AdvancedAnalytics-1.2.0.tar.gz

---
+++ packages/AdvancedAnalytics/AdvancedAnalytics-1.2.0.tar.gz/AdvancedAnalytics-1.2.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: EJones
+"""
+import setuptools
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="AdvancedAnalytics", 
+    version="1.2.0", 
+    author="Edward R Jones", 
+    author_email="ejones@tamu.edu", 
+    url="https://github.com/tandonneur/AdvancedAnalytics", 
+    description="Python support for 'The Art and Science of Data Analytics'",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent"],
+    keywords=["Analytics", "data map", "preprocessing", "pre-processing", 
+              "postprocessing", "post-processing", "NLTK", "Sci-Learn", 
+              "sklearn", "StatsModels", "web scraping", "word cloud",
+              "regression", "decision trees", "random forest", 
+              "neural network", "cross validation", "topic analysis",
+              "sentiment analytic", "natural language processing", "NLP"],
+    zip_safe=True,
+    install_requires = [
+            "scikit-learn",
+            "scikit-image",
+            "statsmodels",
+            "nltk",
+            "pydotplus",
+            "python-graphviz",
+            "wordcloud",
+            "tinysegmenter",
+            "newspaper3k"]
+)
+# The package "newsapi-python" is not accepted in required list
+# Needs to be install separately pip or conda install newsapi-python
