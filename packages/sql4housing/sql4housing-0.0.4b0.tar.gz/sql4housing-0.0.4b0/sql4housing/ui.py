a2bbe0ba02568d82111d4086d75b567ad3d62c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package sql4housing/0.0.4b0/sql4housing-0.0.4b0.tar.gz

---
+++ packages/sql4housing/sql4housing-0.0.4b0.tar.gz/sql4housing-0.0.4b0/sql4housing/ui.py
@@ -0,0 +1,9 @@
+def header(header_str, color=''):
+    print('\n\033[1m%s%s\033[0m' % (color, header_str))
+
+
+def item(item_str):
+    try:
+        print('  ▶ %s' % item_str)
+    except UnicodeEncodeError:
+        print('  - %s' % item_str)
