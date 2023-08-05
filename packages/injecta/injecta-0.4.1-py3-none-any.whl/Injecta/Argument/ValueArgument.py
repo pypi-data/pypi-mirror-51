1d746688250f32d14f47d426a452ad1fc4750b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:13 +0000

    Add package injecta/0.4.1/injecta-0.4.1-py3-none-any.whl

---
+++ packages/injecta/injecta-0.4.1-py3-none-any.whl/Injecta/Argument/ValueArgument.py
@@ -0,0 +1,15 @@
+from Injecta.Argument.ArgumentInterface import ArgumentInterface
+
+class ValueArgument(ArgumentInterface):
+
+    def __init__(self, value: str):
+        self.__value = value
+
+    def getValue(self):
+        if isinstance(self.__value, str):
+            return '\'' + self.__value + '\''
+
+        if isinstance(self.__value, bool):
+            return 'True' if self.__value is True else 'False'
+
+        return self.__value
