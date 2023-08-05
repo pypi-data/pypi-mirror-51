f813f178a815fbcf589cbc8aaf996afa36d3ccee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:34 +0000

    Add package usagelogger/0.2.0/usagelogger-0.2.0.tar.gz

---
+++ packages/usagelogger/usagelogger-0.2.0.tar.gz/usagelogger-0.2.0/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup
+
+setup(
+    name='usagelogger',
+    version='0.2.0',
+    description='Logging usage of Python-based services, with user privacy by design.',
+    long_description="""Logging usage of Python-based services, with user privacy by design.
+
+## Middleware Integrations
+
+These integrations allow usage logging from existing applications with only minor configuration changes.
+
+* Django - coming soon!
+
+## Logging With API
+
+Loggers can be directly integrated into your application via our logging
+<a href="https://github.com/resurfaceio/logger-python/blob/master/API.md">API</a>.
+This requires more effort than middleware integrations, but also offers the greatest flexibility and control.
+
+## Protecting User Privacy
+
+Loggers always have an active set of <a href="https://resurface.io/rules.html">rules</a> that control what data is logged
+and how sensitive data is masked. By default, loggers apply a strict set of predefined rules, but logging rules are easily 
+customized to meet the needs of any application.
+
+---
+<small>&copy; 2016-2019 <a href="https://resurface.io">Resurface Labs Inc.</a></small>
+""",
+    long_description_content_type='text/markdown',
+    url='https://github.com/resurfaceio/logger-python',
+    author='Resurface Labs',
+    author_email='admin@resurface.io',
+    license="Apache License 2.0",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Topic :: Internet :: Log Analysis',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    keywords='logging resurface',
+    packages=['usagelogger'],
+    python_requires='>=3.6, <4',
+    install_requires=[],
+    tests_require=['pytest'],
+    project_urls={
+        'Bug Reports': 'https://github.com/resurfaceio/logger-python/issues',
+        'Source': 'https://github.com/resurfaceio/logger-python'
+    }
+)
