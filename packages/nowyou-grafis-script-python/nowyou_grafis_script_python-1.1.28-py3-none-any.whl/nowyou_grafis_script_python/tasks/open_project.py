55f191f10496ccf177e4725e1bd83b4e44350ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:23 +0000

    Add package nowyou-grafis-script-python/1.1.28/nowyou_grafis_script_python-1.1.28-py3-none-any.whl

---
+++ packages/nowyou-grafis-script-python/nowyou_grafis_script_python-1.1.28-py3-none-any.whl/nowyou_grafis_script_python/tasks/open_project.py
@@ -0,0 +1,42 @@
+from ..ui_tools.UI_tools import Ui_tools
+
+
+class OpenProject:
+
+    @staticmethod
+    def run():
+        OpenProject.open_data()
+        OpenProject.open_file()
+        OpenProject.select_path()
+        OpenProject.input_file()
+        OpenProject.confirm()
+        OpenProject.close_dialog()
+
+    # 2a
+    @staticmethod
+    def open_data():
+        Ui_tools.click_on('\static\\2a.png')
+
+    # 2b
+    @staticmethod
+    def open_file():
+        Ui_tools.hot_key('ctrl', 'shift', 'o')
+
+    # 2c
+    @staticmethod
+    def select_path():
+        Ui_tools.hot_key('delete')
+
+    # 2d
+    @staticmethod
+    def input_file():
+        # TODO make as input parameter
+        Ui_tools.type("nowyou-test-grafis")
+
+    @staticmethod
+    def confirm():
+        Ui_tools.hot_key('enter')
+
+    @staticmethod
+    def close_dialog():
+        Ui_tools.hot_key('enter')
