3599ee7ae6636a71d45a6fd55aa295d9b15edbf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:36 +0000

    Add package sota-extractor/0.0.7/sota_extractor-0.0.7-py3-none-any.whl

---
+++ packages/sota-extractor/sota_extractor-0.0.7-py3-none-any.whl/sota_extractor/scrapers/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["eff", "snli", "squad", "reddit"]
+
+
+from sota_extractor.scrapers.eff import eff
+from sota_extractor.scrapers.snli import snli
+from sota_extractor.scrapers.squad import squad
+from sota_extractor.scrapers.reddit import reddit
