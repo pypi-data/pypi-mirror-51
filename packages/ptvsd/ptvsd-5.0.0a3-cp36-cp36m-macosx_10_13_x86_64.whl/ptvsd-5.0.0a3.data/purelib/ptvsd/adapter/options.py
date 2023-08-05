f18c5d6650adc124f2d6c3839060ee5f560de446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package ptvsd/5.0.0a3/ptvsd-5.0.0a3-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/ptvsd/ptvsd-5.0.0a3-cp36-cp36m-macosx_10_13_x86_64.whl/ptvsd-5.0.0a3.data/purelib/ptvsd/adapter/options.py
@@ -0,0 +1,16 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See LICENSE in the project root
+# for license information.
+
+from __future__ import absolute_import, print_function, unicode_literals
+
+"""Global adapter options that are set via command line, environment variables,
+or configuartion files.
+"""
+
+
+host = "127.0.0.1"
+"""Default host name for the debug server"""
+
+port = 5678
+"""Default port for the debug server"""
