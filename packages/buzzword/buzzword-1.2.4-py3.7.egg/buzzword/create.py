ee76ddb9ef9dd90811d5cd4792fa034747328121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:59 +0000

    Add package buzzword/1.2.4/buzzword-1.2.4-py3.7.egg

---
+++ packages/buzzword/buzzword-1.2.4-py3.7.egg/buzzword/create.py
@@ -0,0 +1,80 @@
+"""
+buzzword: make a workspace for buzzword in the current directory
+
+Usage: python -m buzzword.create <name>
+
+Once a workspace is defined, you can cd in there and run buzzword.
+
+You can modify the various config and data files as you see fit.
+"""
+
+import os
+import sys
+
+from buzz.corpus import Corpus
+
+NAME = sys.argv[-1]
+
+FULLNAME = os.path.abspath(NAME)
+CORPUS_PATH = os.path.join(NAME, "example")
+
+print("Making a new workspace at {}".format(FULLNAME))
+
+ENV = """
+# .env example for deploying buzzword
+# comment out keys you are not using
+BUZZWORD_CORPORA_FILE=corpora.json
+BUZZWORD_ROOT={}
+BUZZWORD_LOAD=true
+BUZZWORD_TITLE=buzzword
+BUZZWORD_DEBUG=false
+# BUZZWORD_MAX_DATASET_ROWS=999999
+BUZZWORD_DROP_COLUMNS=parse,text
+BUZZWORD_PAGE_SIZE=25
+BUZZWORD_TABLE_SIZE=2000,200
+BUZZWORD_ADD_GOVERNOR=false
+""".format(
+    FULLNAME
+)
+
+CORPORA = """
+{{
+  "Example corpus: joke": {{
+    "slug": "jokes",
+    "path": "{}",
+    "desc": "Sample corpus with speaker names and metadata",
+    "len": 29,
+    "drop_columns": ["text"],
+    "disable": false,
+    "date": "2019",
+    "url": "https://en.wikipedia.org/wiki/Joke"
+    }}
+}}
+""".format(
+    os.path.abspath(CORPUS_PATH + "-parsed")
+)
+
+CORPUS = """
+<meta doc-type="joke" rating=6.50 speaker="NARRATOR"/>
+<meta being="animal">A lion</meta> and <meta being="animal">a cheetah</meta> decide to race. <meta move="setup" dialog=false punchline=false some-schema=9 />
+The cheetah crosses the finish line first. <meta move="setup" dialog=false punchline=false />
+CHEETAH: I win! <meta move="middle" dialog=true some-schema=2 />
+LION: You're a <meta play-on="cheater">cheetah</meta>! <meta move="punchline" funny=true dialog=true some-schema=3 />
+CHEETAH: You're <meta play-on="lying">lion</meta>! <meta move="punchline" funny=true dialog=true some-schema=4 rating=7.8 />
+"""
+
+os.makedirs(FULLNAME)
+
+for folder in ["csv", "uploads", "example"]:
+    os.makedirs(os.path.join(FULLNAME, folder))
+with open(os.path.join(FULLNAME, ".env"), "w") as fo:
+    fo.write(ENV.strip() + "\n")
+with open(os.path.join(FULLNAME, "corpora.json"), "w") as fo:
+    fo.write(CORPORA.strip() + "\n")
+with open(os.path.join(CORPUS_PATH, "001-joke-lion-pun.txt"), "w") as fo:
+    fo.write(CORPUS.strip() + "\n")
+print("Testing parser: {}->{}-parsed ...".format(CORPUS_PATH, CORPUS_PATH))
+parsed = Corpus(CORPUS_PATH).parse(cons_parser=None)
+
+print("Workspace made in {}".format(FULLNAME))
+print("Run 'cd {} && python -m buzzword' to start.".format(NAME))
---
+++ packages/buzzword/buzzword-1.2.4-py3.7.egg/buzzword/parts/main.py
@@ -0,0 +1,88 @@
+"""
+buzzword: run on startup, corpus loading and app initialisation
+"""
+
+import dash
+from buzz.corpus import Corpus
+import json
+import os
+from buzzword.parts.helpers import _preprocess_corpus
+from buzzword.parts.configure import _configure_buzzword
+from buzzword.parts.strings import _slug_from_name
+
+external_stylesheets = [
+    "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+    "https://codepen.io/chriddyp/pen/bWLwgP.css",
+]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.config.suppress_callback_exceptions = True
+app.title = "buzzword"
+server = app.server
+
+GLOBAL_CONFIG = _configure_buzzword(__name__)
+
+ROOT = GLOBAL_CONFIG["root"]
+
+
+def _get_corpus_config(local_conf, global_conf, name):
+    """
+    Return global conf plus individual settings for corpus
+    """
+    conf = {**global_conf}
+    settings = {"max_dataset_rows", "drop_columns", "add_governor", "load", "slug"}
+    for setting in settings:
+        loc = local_conf.get(setting)
+        if loc is not None:
+            conf[setting] = loc
+        else:
+            if setting == "slug":
+                conf[setting] = _slug_from_name(name)
+    conf["corpus_name"] = name
+    return conf
+
+
+def _get_corpora(corpus_meta):
+    """
+    Load in all available corpora and make their initial tables
+
+    This is run when the app starts up
+    """
+    corpora = dict()
+    tables = dict()
+    corpora_config = dict()
+    for i, (corpus_name, metadata) in enumerate(corpus_meta.items(), start=1):
+        if metadata.get("disabled"):
+            print("Skipping corpus because it is disabled: {}".format(corpus_name))
+            continue
+        slug = metadata.get("slug", _slug_from_name(corpus_name))
+        corpus = Corpus(metadata["path"])
+        conf = _get_corpus_config(metadata, GLOBAL_CONFIG, corpus_name)
+        if conf["load"]:
+            print("Loading corpus into memory: {} ...".format(corpus_name))
+            corpus = corpus.load(add_governor=conf["add_governor"])
+            corpus = _preprocess_corpus(corpus, **conf)
+        else:
+            print("NOT loading corpus into memory: {} ...".format(corpus_name))
+        initial_table = corpus.table(show="p", subcorpora="file")
+        corpora[slug] = corpus
+        tables[slug] = initial_table
+        corpora_config[slug] = conf
+    return corpora, tables, corpora_config
+
+
+def _get_corpora_meta(corpora_file):
+    """
+    Get the contents of corpora.json, or an empty dict
+    """
+    exists = os.path.isfile(corpora_file)
+    if not exists:
+        print("Corpora file not found at {}!".format(corpora_file))
+        return dict()
+    with open(corpora_file, "r") as fo:
+        return json.loads(fo.read())
+
+
+CORPUS_META = _get_corpora_meta(GLOBAL_CONFIG.get("corpora_file"))
+
+CORPORA, INITIAL_TABLES, CORPORA_CONFIGS = _get_corpora(CORPUS_META)
