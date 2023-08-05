d5717bb22e7937745f0ac588be9cba5fe7c6cf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:30 +0000

    Add package pyxel-lander/1.1.0/pyxel-lander-1.1.0.tar.gz

---
+++ packages/pyxel-lander/pyxel-lander-1.1.0.tar.gz/pyxel-lander-1.1.0/pyxel_lander/__init__.py
@@ -0,0 +1,11 @@
+from .constants import AUTHOR, EMAIL, VERSION
+
+__author__ = AUTHOR
+__email__ = EMAIL
+__version__ = VERSION
+
+__all__ = [
+    '__author__',
+    '__email__',
+    '__version__',
+]
