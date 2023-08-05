cbf9eff9bf7010850b9f45639303424a0f70cda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package gurux-common/1.0.9/gurux_common-1.0.9.tar.gz

---
+++ packages/gurux-common/gurux_common-1.0.9.tar.gz/gurux_common-1.0.9/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gurux_common",
+    version="1.0.9",
+    author="Gurux Ltd",
+    author_email="gurux@gurux.org",
+    description="Gurux common helpers for device commmunication.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gurux/gurux.common.python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+)
