ac8d304cb1c2b58af65ade5d3c953828e9f5adfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:08 +0000

    Add package daemons/1.3.1/daemons-1.3.1.tar.gz

---
+++ packages/daemons/daemons-1.3.1.tar.gz/daemons-1.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+with open("README.rst") as f:
+    readme = f.read()
+
+setup(
+    name="daemons",
+    version="1.3.1",
+    url="https://github.com/kevinconway/daemons",
+    license="'Apache License 2.0",
+    description="Well behaved unix daemons for every occasion.",
+    author="Kevin Conway",
+    author_email="kevinjacobconway@gmail.com",
+    long_description=readme,
+    classifiers=[],
+    packages=find_packages(exclude=["tests", "build", "dist", "docs"]),
+    install_requires=[],
+)
