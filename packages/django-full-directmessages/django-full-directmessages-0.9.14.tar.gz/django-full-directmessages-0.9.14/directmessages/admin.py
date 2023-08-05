448ea4db5ae7b20dd0c89d12f1e9c41028bc44dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:40 +0000

    Add package django-full-directmessages/0.9.14/django-full-directmessages-0.9.14.tar.gz

---
+++ packages/django-full-directmessages/django-full-directmessages-0.9.14.tar.gz/django-full-directmessages-0.9.14/directmessages/admin.py
@@ -0,0 +1,10 @@
+from .models import Message
+from django.contrib import admin
+
+
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+    list_display = ('id', 'sender', 'content', )
+
+
+admin.site.register(Message, MessageAdmin)
