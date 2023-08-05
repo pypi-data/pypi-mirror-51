ae89aa86a17202c077d5ffe9223e7055041ad06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:29 +0000

    Add package ForesightPy/1.0.1/foresightpy-1.0.1.tar.gz

---
+++ packages/ForesightPy/foresightpy-1.0.1.tar.gz/foresightpy-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='foresightpy',
+     version='1.0.1',
+     author="Oliver Boom",
+     author_email="ob3618@ic.ac.uk",
+     license='MIT',
+     description="A package for price forecasting using LSTM networks",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/msc-acse/acse-9-independent-research-project-OliverJBoom",
+     download_url = "https://github.com/msc-acse/acse-9-independent-research-project-OliverJBoom/archive/1.0.1.tar.gz",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
