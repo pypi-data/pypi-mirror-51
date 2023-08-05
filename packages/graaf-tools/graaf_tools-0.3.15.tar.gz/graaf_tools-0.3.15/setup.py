35a972d80dcdfc554892d8c1800dda75b3365a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:52 +0000

    Add package graaf-tools/0.3.15/graaf_tools-0.3.15.tar.gz

---
+++ packages/graaf-tools/graaf_tools-0.3.15.tar.gz/graaf_tools-0.3.15/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'graaf_tools',
+  packages = find_packages(),
+  version = '0.3.15',
+  license='MIT',
+  description = 'tools and code for research',
+  author = 'Rafael Van Belle',
+  author_email = 'rafael.vanbelle@gmail.com',
+  url = 'https://github.com/raftie/graaf_tools',
+  download_url = 'https://github.com/Raftie/graaf_tools/archive/v0.3.15.tar.gz',
+  keywords = ['network'],
+  install_requires=[
+          'tqdm',
+          'gensim',
+          'networkx',
+      ],
+  classifiers=[
+  ],
+)
