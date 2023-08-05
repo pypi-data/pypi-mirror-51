1f8914f204d3ea68887b98a084bbaad51f3c44d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:38 +0000

    Add package ktis-parser/0.2.4/ktis-parser-0.2.4.tar.gz

---
+++ packages/ktis-parser/ktis-parser-0.2.4.tar.gz/ktis-parser-0.2.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ktis-parser",
+    version="0.2.4",
+    author="zaeval",
+    author_email="zaeval@kookmin.ac.kr",
+    description="ktis_parser default information parser",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zaeval/ktis_parser",
+    packages=setuptools.find_packages(),
+    license="GPL",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ),
+)
