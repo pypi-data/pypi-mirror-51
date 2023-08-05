2ebca7b3603237c31313916e3bf1d0334e3ee865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:30 +0000

    Add package fbchat-asyncio/0.2.3/fbchat_asyncio-0.2.3-py3-none-any.whl

---
+++ packages/fbchat-asyncio/fbchat_asyncio-0.2.3-py3-none-any.whl/fbchat/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""Facebook Chat (Messenger) for Python
+
+:copyright: (c) 2015 - 2019 by Taehoon Kim
+:license: BSD 3-Clause, see LICENSE for more details.
+"""
+from __future__ import unicode_literals
+
+# These imports are far too general, but they're needed for backwards compatbility.
+from .models import *
+
+from ._client import Client
+
+__title__ = "fbchat-asyncio"
+__version__ = "0.2.3"
+__description__ = "Facebook Messenger library for Python/Asyncio."
+
+__copyright__ = "Copyright 2015 - 2019 by Taehoon Kim"
+__license__ = "BSD 3-Clause"
+
+__author__ = "Taehoon Kim; Moreels Pieter-Jan; Mads Marquart"
+__email__ = "carpedm20@gmail.com"
+
+__all__ = ["Client"]
