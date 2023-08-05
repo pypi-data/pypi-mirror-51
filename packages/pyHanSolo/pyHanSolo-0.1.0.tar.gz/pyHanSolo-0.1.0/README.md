9c5d029ce718a04c31484c8b80d99f4e2670b560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:01 +0000

    Add package pyHanSolo/0.1.0/pyHanSolo-0.1.0.tar.gz

---
+++ packages/pyHanSolo/pyHanSolo-0.1.0.tar.gz/pyHanSolo-0.1.0/README.md
@@ -0,0 +1,1 @@
+# pyHanSolo [![Build Status](https://travis-ci.org/Danielhiversen/pyHanSolo.svg?branch=master)](https://travis-ci.org/Danielhiversen/pyHanSolo)
---
+++ packages/pyHanSolo/pyHanSolo-0.1.0.tar.gz/pyHanSolo-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='pyHanSolo',
+    packages=['han_solo'],
+    install_requires=['crcmod', 'aiohttp'],
+    version='0.1.0',
+    description='A python3 library to communicate with HanSolo',
+    python_requires='>=3.5.3',
+    author='Daniel Hoyer Iversen',
+    author_email='mail@dahoiv.net',
+    url='https://github.com/Danielhiversen/pyHanSolo',
+    license="MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
