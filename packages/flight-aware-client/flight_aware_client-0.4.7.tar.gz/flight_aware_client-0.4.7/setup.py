4cf1ebbbd26c56d09d82bdd04d64047a0ce7b48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:18 +0000

    Add package flight-aware-client/0.4.7/flight_aware_client-0.4.7.tar.gz

---
+++ packages/flight-aware-client/flight_aware_client-0.4.7.tar.gz/flight_aware_client-0.4.7/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='flight_aware_client',
+    version='0.4.7',
+    packages=['FlightAwareClient',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license'
+)
