a2157675ee8ab8ba6e7191a4f7196020154ea9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:40 +0000

    Add package Toaster-Secure-Scaffold-RC/0.8.2/Toaster%20Secure%20Scaffold%20RC-0.8.2.tar.gz

---
+++ packages/Toaster-Secure-Scaffold-RC/Toaster%20Secure%20Scaffold%20RC-0.8.2.tar.gz/Toaster Secure Scaffold RC-0.8.2/secure_scaffold/tests/test_factories.py
@@ -0,0 +1,46 @@
+import json
+
+from flask import Flask
+
+from secure_scaffold import factories, settings
+
+
+def test_app_factory_returns_an_app():
+    app = factories.AppFactory().generate()
+
+    assert isinstance(app, Flask)
+
+
+def test_app_factory_adds_report_to_headers():
+    app = factories.AppFactory().generate()
+    client = app.test_client()
+
+    resp = client.get('/')
+
+    expected_headers = json.dumps(settings.REPORT_TO_HEADER)
+
+    assert resp.headers['Report-To'] == expected_headers
+
+
+def test_app_factory_adds_csp_headers():
+    app = factories.AppFactory().generate()
+    client = app.test_client()
+
+    resp = client.get('/')
+
+    expected_headers = '; '.join(
+        f'{key} {value}'
+        for key, value in settings.CSP_CONFIG.items()
+    )
+
+    assert resp.headers['Content-Security-Policy'] == expected_headers
+
+
+def test_extra_flask_args():
+    app = factories.AppFactory(static_url_path='/static').generate()
+    assert app.static_url_path == '/static'
+
+
+def test_given_flask_name():
+    app = factories.AppFactory(name='tester').generate()
+    assert app.name == 'tester'
