9cbb310cd32efea46478ec7293c761f674592540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:18 +0000

    Add package batchglm/0.6.4/batchglm-0.6.4.tar.gz

---
+++ packages/batchglm/batchglm-0.6.4.tar.gz/batchglm-0.6.4/batchglm/api/data.py
@@ -0,0 +1,3 @@
+from batchglm.data import design_matrix
+from batchglm.data import constraint_matrix_from_dict, constraint_matrix_from_string, string_constraints_from_dict
+from batchglm.data import view_coef_names, preview_coef_names
