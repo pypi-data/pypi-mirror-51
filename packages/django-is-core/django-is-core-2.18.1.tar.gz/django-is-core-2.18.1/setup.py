91768a3e78bcdbe1982f7734d3b4f841bf44db99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:25 +0000

    Add package django-is-core/2.18.1/django-is-core-2.18.1.tar.gz

---
+++ packages/django-is-core/django-is-core-2.18.1.tar.gz/django-is-core-2.18.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+from is_core.version import get_version
+
+setup(
+    name='django-is-core',
+    version=get_version(),
+    description="Information systems core.",
+    keywords='django, admin, information systems, REST',
+    author='Lubos Matl',
+    author_email='matllubos@gmail.com',
+    url='https://github.com/matllubos/django-is-core',
+    license='BSD',
+    package_dir={'is_core': 'is_core'},
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    install_requires=[
+        'django>=1.10',
+        'django-pyston~=2.9.16',
+        'django-block-snippets==2.0.1',
+        'django-chamber~=0.5.2',
+        'python-dateutil~=2.2',
+        'pytz',
+        'Unidecode>=0.04.16',
+        'python-mimeparse==0.1.4',
+        'django-ipware~=1.0.0'
+    ],
+    zip_safe=False
+)
