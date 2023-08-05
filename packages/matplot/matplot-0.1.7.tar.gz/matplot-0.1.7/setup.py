8c5d1099f3300821fc49eebe577f645a8629f7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:16 +0000

    Add package matplot/0.1.7/matplot-0.1.7.tar.gz

---
+++ packages/matplot/matplot-0.1.7.tar.gz/matplot-0.1.7/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"pyloco task setup script"
+
+if __name__ == "__main__":
+
+    from setuptools import setup, find_packages
+
+    __taskname__ = "matplot"
+
+    setup(
+        name="matplot",
+        version="0.1.7",
+        packages=find_packages(),
+        package_data={},
+        install_requires=["pyloco>=0.0.134", "matplotlib>=3.1.1"],
+        entry_points = {"pyloco.task": ["{taskname} = {taskname}:entry_task".format(taskname=__taskname__)]},
+    )
