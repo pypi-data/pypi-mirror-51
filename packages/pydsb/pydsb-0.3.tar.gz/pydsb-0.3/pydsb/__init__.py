b6daf52ffc3917f26c7ffc0e9e3ee3fc0cfe3a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:49 +0000

    Add package pydsb/0.3/pydsb-0.3.tar.gz

---
+++ packages/pydsb/pydsb-0.3.tar.gz/pydsb-0.3/pydsb/__init__.py
@@ -0,0 +1,35 @@
+import requests
+
+
+class PyDSB:
+    URL = "https://iphone.dsbcontrol.de/iPhoneService.svc/DSB"
+
+    def __init__(self, username=None, password=None):
+        self.username = username
+        self.password = password
+        self.token = None
+
+    def login(self):
+        req = requests.get(f"{self.URL}/authid/{self.username}/{self.password}")
+        token = req.text.strip("\"")
+
+        if token == "00000000-0000-0000-0000-000000000000":
+            raise LoginError("Wrong username or password")
+
+        self.token = token
+
+    def get_plans(self):
+        req = requests.get(f"{self.URL}/timetables/{self.token}")
+        items = req.json()
+
+        return [
+            {"is_html": item["ishtml"],
+             "uploaded_date": item["timetabledate"],
+             "group": item["timetablegroupname"],
+             "title": item["timetabletitle"],
+             "url": item["timetableurl"]} for item in items
+        ]
+
+
+class LoginError(Exception):
+    pass
