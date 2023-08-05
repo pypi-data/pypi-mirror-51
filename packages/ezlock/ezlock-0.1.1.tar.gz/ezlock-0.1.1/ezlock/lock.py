75c8fb69f2b38a7856395682bfb770ef9172e444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:02 +0000

    Add package ezlock/0.1.1/ezlock-0.1.1.tar.gz

---
+++ packages/ezlock/ezlock-0.1.1.tar.gz/ezlock-0.1.1/ezlock/lock.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+import os
+import time
+
+class LockError(Exception):
+    pass
+
+class Lock:
+
+    def __init__(self, path='.lock'):
+        """
+        Lock object that keeps track of a file found at `self.path`. If there is a file found at `self.path`, the lock is considered... locked!
+        
+        Parameters
+        ==========
+        path : str, Path
+            path to write the lock file to (will be converted to `pathlib.Path`). Defaults to '.lock'.
+        """
+        self.path = Path(path)
+
+    @property
+    def name(self):
+        """
+        name written to lock to prove ownership
+        """
+        return 'pid:{}, obj:{}'.format(os.getpid(), id(self))
+
+    @property
+    def locked(self):
+        """
+        Does the lock-file at `self.path` exist?
+        """
+        return self.path.exists()
+
+    @property
+    def mine(self):
+        """
+        Was the lock created by this object?
+        """
+        try:
+            return self.path.read_text() == self.name
+        except FileNotFoundError:
+            raise LockError("Attempted to check ownership on lock that doesn't exist")
+
+    def acquire(self):
+        """
+        Create the lock-file, and stamp on it that it was made by me!
+        """
+        if self.locked:
+            raise LockError("Attempted to acquire on already locked lock!")
+        self.path.write_text(self.name)
+
+    def release(self, force=False, rerelease=True):
+        """
+        Release the lock.
+        
+        Will get upset if the lock isn't `self.mine` but can override by setting `force=True`.
+        
+        Arguments
+        =========
+        force : bool
+            force releasing the lock, even if not `self.mine`. (default `False`)
+        rerelease : 
+            when `True` will not complain if attempting to release and already released lock. (default `True`)
+        """
+        if not self.locked:
+            if not rerelease:
+                raise LockError("Attempted to release an already released lock")
+            return None
+        if not self.mine and not force:
+            raise LockError("Attempted to release a lock that wasn't mine, can set `force=True`")
+        pid = self.path.read_text()
+
+    def wait(self, dt=0.01):
+        """
+        Wait until lock is released.
+        
+        Arguments
+        =========
+        dt : float
+            how long to wait between checking for `self.locked`
+        """
+        while self.locked:
+              time.sleep(dt)
+
+    def __enter__(self):
+        self.acquire()
+
+    def __exit__(self):
+        self.release()
+
+    def __bool__(self):
+        return self.locked
---
+++ packages/ezlock/ezlock-0.1.1.tar.gz/ezlock-0.1.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['ezlock']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ezlock',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': '0Hughman0',
+    'author_email': 'rammers2@hotmail.co.uk',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
