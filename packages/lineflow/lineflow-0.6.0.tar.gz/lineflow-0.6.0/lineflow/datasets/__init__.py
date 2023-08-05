198854c86b98505fcb7e390f1d4de03968cd694f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:38 +0000

    Add package lineflow/0.6.0/lineflow-0.6.0.tar.gz

---
+++ packages/lineflow/lineflow-0.6.0.tar.gz/lineflow-0.6.0/lineflow/datasets/__init__.py
@@ -0,0 +1,8 @@
+from .cnn_dailymail import CnnDailymail, get_cnn_dailymail  # NOQA
+from .imdb import Imdb, get_imdb  # NOQA
+from .msr_paraphrase import MsrParaphrase, get_msr_paraphrase  # NOQA
+from .penn_treebank import PennTreebank, get_penn_treebank  # NOQA
+from .small_parallel_enja import SmallParallelEnJa, get_small_parallel_enja  # NOQA
+from .squad import Squad, get_squad  # NOQA
+from .text_classification import TextClassification, get_text_classification_dataset  # NOQA
+from .wikitext import WikiText2, WikiText103, get_wikitext  # NOQA
