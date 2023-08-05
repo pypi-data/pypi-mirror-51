08b9af09ec2a55aa2ba20cc2b0029a5965b2f4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:52 +0000

    Add package hip-data-tools/0.0.0/hip_data_tools-0.0.0.tar.gz

---
+++ packages/hip-data-tools/hip_data_tools-0.0.0.tar.gz/hip_data_tools-0.0.0/setup.py
@@ -0,0 +1,18 @@
+"""
+Setup and release this package for wider consumption using setup tools
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(include=["hip_data_tools", "hip_data_tools.*"]),
+    zip_safe=False,
+    install_requires=[
+        "boto3==1.9.206"
+    ],
+    test_suite="tests",
+    tests_require=[
+    ],
+    python_requires='~=3.6',
+    pbr=True
+)
