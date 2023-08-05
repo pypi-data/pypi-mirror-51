8fc8327581c99f3a836870bb0ae170e0961e5e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:48 +0000

    Add package isicarchive/0.3.5/isicarchive-0.3.5-py3-none-any.whl

---
+++ packages/isicarchive/isicarchive-0.3.5-py3-none-any.whl/isicarchive/vars.py
@@ -0,0 +1,14 @@
+"""
+isicarchive variables
+
+constants
+---------
+ISIC_API_URI : str
+    current API URI
+ISIC_BASE_URL : str
+    hostname of ISIC Archive, including https:// protocol id
+"""
+
+__version__ = '0.3.5'
+ISIC_API_URI = '/api/v1'
+ISIC_BASE_URL = 'https://isic-archive.com'
