481ced4fcae86cb1aee13ea2280358ccaed8aea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package Python-video-annotator-models/0.8.82/Python%20video%20annotator%20-%20models-0.8.82.tar.gz

---
+++ packages/Python-video-annotator-models/Python%20video%20annotator%20-%20models-0.8.82.tar.gz/Python video annotator - models-0.8.82/pythonvideoannotator_models/models/project_io.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python2
+# -*- coding: utf-8 -*-
+import os, simplejson as json
+from confapp import conf
+from send2trash import send2trash
+from pythonvideoannotator_models.utils import tools
+from pythonvideoannotator_models.models.project_base import ProjectBase
+
+class ProjectIO(ProjectBase):
+
+	######################################################################################
+	#### IO FUNCTIONS ####################################################################
+	######################################################################################
+	
+	def save(self, data={}, project_path=None):
+
+		project_path = str(project_path) if project_path is not None else self.directory
+		if project_path is None: raise Exception('The project path is not defined')
+
+		self._directory = str(project_path)
+
+		videos_path = os.path.join(project_path, 'videos')
+		if not os.path.exists(videos_path): os.makedirs(videos_path)
+
+		data['videos'] = []
+
+		## Save videos #################################
+		videos_paths = []
+		for video in self.videos:
+			data['videos'].append(video.save({}, videos_path))
+			videos_paths.append(video.directory)
+
+		## Check if there are videos in the videos folder that should be removed		
+		for video_path in tools.list_folders_in_path(videos_path):
+			if video_path not in videos_paths: send2trash(video_path)
+
+		
+
+		#Save the project file ######################################
+		project_filename = os.path.join(self.directory, 'project.json')
+		with open(project_filename, 'w') as outfile: 
+			json.dump(data, outfile)
+
+		return data
+
+
+
+
+	def load(self, data, project_path=None):
+
+		project_path = str(project_path) if project_path is not None else self.directory
+
+		if project_path is None:
+			raise Exception('The project path was not defined')
+
+		self._directory = os.path.abspath(project_path)
+
+		project_filename = os.path.join(str(project_path), 'project.json')
+		with open(project_filename, 'r') as outfile:
+			data.update( json.load(outfile) )
+	
+		videos_path  = os.path.join(project_path, 'videos')		
+		videos_paths = tools.list_folders_in_path(videos_path)
+
+		for video_path in videos_paths:
+			video = self.create_video()
+			video.load(data, video_path)
+
+		
+		
+		return data
---
+++ packages/Python-video-annotator-models/Python%20video%20annotator%20-%20models-0.8.82.tar.gz/Python video annotator - models-0.8.82/pythonvideoannotator_models/models/video/objects/object2d/datasets/value/value_base.py
@@ -0,0 +1,54 @@
+from pythonvideoannotator_models.models.video.objects.object2d.datasets.dataset import Dataset
+
+class ValueBase(Dataset):
+
+    def __init__(self, object2d):
+        super(ValueBase, self).__init__(object2d)
+        self.filepath = None # used for lazy load
+        self.name     = object2d.generate_child_name('Value ')
+        self._values  = []
+
+        self._iterator_index = 0
+        
+    ######################################################################
+    ### CLASS FUNCTIONS ##################################################
+    ######################################################################
+
+    def __len__(self):                   return len(self._values)
+    def __getitem__(self, index):        return self._values[index] if index<len(self) else None
+    def __setitem__(self, index, value): self.set_value(index, value)
+    def __str__(self):                   return self.name
+
+    def __iter__(self):
+        self._iterator_index = 0
+        return self
+
+    def __next__(self):
+        if self._iterator_index >= len(self._values):
+            raise StopIteration
+        res = self._values[self._iterator_index]
+        self._iterator_index += 1
+        return res
+
+    ######################################################################
+    ### DATA MODIFICATION AND ACCESS #####################################
+    ######################################################################
+
+    def get_value(self, index):
+        if index<0 or index>=len(self): return None
+        return self[index] if self[index] is not None else None
+
+    def set_value(self, index, val):
+        # add positions in case they do not exists
+        if index >= len(self):
+            for i in range(len(self), index + 1): self._values.append(None)
+
+        # create a new moment in case it does not exists
+        self._values[index] = val
+
+    @property
+    def values(self):
+        return self._values
+    @values.setter
+    def values(self, values):
+        self._values = values
