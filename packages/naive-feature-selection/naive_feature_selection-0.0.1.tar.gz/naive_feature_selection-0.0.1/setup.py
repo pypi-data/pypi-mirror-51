8c84f45c6816e28c2ffd65568d73332170b92543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:03 +0000

    Add package naive-feature-selection/0.0.1/naive_feature_selection-0.0.1.tar.gz

---
+++ packages/naive-feature-selection/naive_feature_selection-0.0.1.tar.gz/naive_feature_selection-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="naive_feature_selection",
+    version="0.0.1",
+    author="Armin Askari, Alexandre d'Aspremont, Laurent El Ghaoui",
+    author_email="aspremon@ens.fr",
+    packages=find_packages(),
+    python_requires=">=3.5.0",
+    install_requires=["scikit-learn"],
+    description="Naive Feature Selection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aspremon/NaiveFeatureSelection",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
