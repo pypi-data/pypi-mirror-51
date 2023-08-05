3d15df50e8f370416fd4ceb53c6fe26f058401d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:39 +0000

    Add package NodeSocket/0.3.0/NodeSocket-0.3.0.tar.gz

---
+++ packages/NodeSocket/NodeSocket-0.3.0.tar.gz/NodeSocket-0.3.0/NodeSocket/NodeSocket.py
@@ -0,0 +1,55 @@
+import socket
+from threading import Thread
+from json import dumps as dictToJson
+from json import loads as jsonToDict
+
+class NodeSocket(Thread):
+	def __init__(self, path='/tmp/node-python-sock', openSocket=True):
+		super(NodeSocket, self).__init__()
+		self.path = path
+		self.data = ''
+		self.channels = {}
+		if openSocket:
+			self.open()
+		self.start()
+
+	def open(self):
+		while True:
+			try:
+				self.client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+				self.client.connect(self.path)
+				self.socketOpen = True
+				return
+			except: continue
+
+	def run(self, size=256):
+		if self.socketOpen:
+			tmp = ''
+			while True:
+				tmp += self.client.recv(size).decode().encode('utf-8')
+				try:
+					msg = jsonToDict(tmp)
+					if msg['type'] not in self.channels.keys():
+						self.registerChannel(msg['type'])
+					self.channels[msg['type']] = msg['data']
+					tmp = ''
+				except: continue
+
+	def registerChannel(self, channel):
+		self.channels[channel] = None
+
+	def get(self, channel):
+		if channel in self.channels.keys():
+			return self.channels[channel]
+		return None
+
+	def write(self, channel, data):
+		msg = {'type': channel, 'data': data}
+		self.client.send(dictToJson(msg).encode())
+
+	def close(self):
+		self.client.close()
+		self.socketOpen = False
+
+	def isOpen(self):
+		return self.socketOpen
