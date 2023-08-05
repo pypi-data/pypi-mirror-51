018d398eed948ecfc371fcca78a5407600b35cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:43:05 +0000

    Add package TUPA/1.4.1/TUPA-1.4.1-py3-none-any.whl

---
+++ packages/TUPA/TUPA-1.4.1-py3-none-any.whl/tupa/__version__.py
@@ -0,0 +1,7 @@
+VERSION = "1.4.1"
+# noinspection PyBroadException
+try:
+    from subprocess import check_output, DEVNULL
+    GIT_VERSION = check_output(["git", "describe", "--tags", "--always"], stderr=DEVNULL).decode().strip().lstrip("v")
+except:
+    GIT_VERSION = VERSION
