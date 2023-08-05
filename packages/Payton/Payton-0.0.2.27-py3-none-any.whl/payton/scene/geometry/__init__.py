3bce5cbf5ce97d0e493f31f528e6021ff8b2edfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:30 +0000

    Add package Payton/0.0.2.27/Payton-0.0.2.27-py3-none-any.whl

---
+++ packages/Payton/Payton-0.0.2.27-py3-none-any.whl/payton/scene/geometry/__init__.py
@@ -0,0 +1,21 @@
+# pylama:ignore=W
+"""
+Payton main geometry module
+
+Geometry module holds the basic geometry shapes. They are all inherited
+from `payton.scene.geometry.base.Object` class. They are as simple as possible.
+
+Their face informations are generated at the initialization but vertex array
+object is not generated until it arrives in render pipeline.
+
+* Object
+  * Material (`payton.scene.material`)
+    * Shader (`payton.scene.shader`)
+"""
+from payton.scene.geometry.base import *
+from payton.scene.geometry.mesh import *
+from payton.scene.geometry.cylinder import *
+from payton.scene.geometry.cube import *
+from payton.scene.geometry.plane import *
+from payton.scene.geometry.sphere import *
+from payton.scene.geometry.point import *
