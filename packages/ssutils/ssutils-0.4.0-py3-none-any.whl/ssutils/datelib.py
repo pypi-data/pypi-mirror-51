c433bf076b2afd0aabfb564010ef53bf2399cfa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:36 +0000

    Add package ssutils/0.4.0/ssutils-0.4.0-py3-none-any.whl

---
+++ packages/ssutils/ssutils-0.4.0-py3-none-any.whl/ssutils/datelib.py
@@ -0,0 +1,56 @@
+from datetime import date
+from datetime import timedelta
+import datetime
+
+class Date:
+
+	def __init__(self):
+		self.today = date.today()
+		
+	def get_quarter (self, datein=str(date.today())):
+		sd = str(datein)
+		sdin = int(sd[5:][:2])
+		if sdin in (1, 2, 3):
+			return 'Q1'
+		elif sdin in (4, 5, 6):
+			return 'Q2'
+		elif sdin in (7, 8, 9):
+			return 'Q3'
+		elif sdin in (10, 11, 12):
+			return 'Q4'
+		return 'Q?'
+
+	def get_month (self, datein=str(date.today())):
+		sd = str(datein)
+		sdin = int(sd[5:][:2])
+		if sdin == 1:
+			return 'January'
+		elif sdin == 2:
+			return 'February'
+		elif sdin == 3:
+			return 'March'
+		elif sdin == 4:
+			return 'April'
+		elif sdin == 5:
+			return 'May'
+		elif sdin == 6:
+			return 'June'
+		elif sdin == 7:
+			return 'July'
+		elif sdin == 8:
+			return 'August'
+		elif sdin == 9:
+			return 'September'
+		elif sdin == 10:
+			return 'October'
+		elif sdin == 11:
+			return 'November'
+		elif sdin == 12:
+			return 'December'
+		return datein
+
+	def get_year_start (self, datein=str(date.today())):
+		return str(datein)[:4] + '-01-01'
+
+	def get_year_end (self, datein=str(date.today())):
+		return str(datein)[:4] + '-12-31'
