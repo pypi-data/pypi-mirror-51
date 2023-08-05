c23e7018d53f8779c78b1ac7280286f5cf5fdd6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:01 +0000

    Add package data-distributions/0.1/data_distributions-0.1.tar.gz

---
+++ packages/data-distributions/data_distributions-0.1.tar.gz/data_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='data_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['data_distributions'],
+      zip_safe=False)
