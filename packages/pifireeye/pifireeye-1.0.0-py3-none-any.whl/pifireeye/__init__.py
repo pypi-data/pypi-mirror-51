9cc967d489c17728bb5db247f7f3e40321b9c41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package pifireeye/1.0.0/pifireeye-1.0.0-py3-none-any.whl

---
+++ packages/pifireeye/pifireeye-1.0.0-py3-none-any.whl/pifireeye/__init__.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+# import apis into sdk package
+from .api.category_api import CategoryApi
+from .api.device_command_api import DeviceCommandApi
+from .api.device_signal_api import DeviceSignalApi
+from .api.device_status_api import DeviceStatusApi
+from .api.error_info_api import ErrorInfoApi
+from .api.image_api import ImageApi
+from .api.order_api import OrderApi
+
+# import ApiClient
+from .api_client import ApiClient
+from .configuration import Configuration
+# import models into sdk package
+from .models.device_command import DeviceCommand
+from .models.device_signal import DeviceSignal
+from .models.device_status import DeviceStatus
+from .models.error_info import ErrorInfo
+from .models.image import Image
+from .models.image_category import ImageCategory
+from .models.order import Order
+from .models.order_category import OrderCategory
+from .models.signal import Signal
---
+++ packages/pifireeye/pifireeye-1.0.0-py3-none-any.whl/pifireeye/api/__init__.py
@@ -0,0 +1,11 @@
+
+# flake8: noqa
+
+# import apis into api package
+from .category_api import CategoryApi
+from .device_command_api import DeviceCommandApi
+from .device_signal_api import DeviceSignalApi
+from .device_status_api import DeviceStatusApi
+from .error_info_api import ErrorInfoApi
+from .image_api import ImageApi
+from .order_api import OrderApi
