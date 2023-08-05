e83b733de37b3dc68245867ca6dad6dac06e1cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:26 +0000

    Add package cache-function-deepakmishra/0.0.1/cache-function-deepakmishra-0.0.1.tar.gz

---
+++ packages/cache-function-deepakmishra/cache-function-deepakmishra-0.0.1.tar.gz/cache-function-deepakmishra-0.0.1/README.md
@@ -0,0 +1,5 @@
+# Cache Function
+
+You can use this python decorator
+[Github-flavored Markdown](https://github.com/deepakmishra/cache_function/)
+to cache a result of a function.
---
+++ packages/cache-function-deepakmishra/cache-function-deepakmishra-0.0.1.tar.gz/cache-function-deepakmishra-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cache-function-deepakmishra",
+    version="0.0.1",
+    author="Deepak Mishra",
+    author_email="deepakmishra117@gmail.com",
+    description="A small decorator which helps cache a result of a function",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepakmishra/cache_function",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: Freeware",
+        "Operating System :: OS Independent",
+    ],
+)
