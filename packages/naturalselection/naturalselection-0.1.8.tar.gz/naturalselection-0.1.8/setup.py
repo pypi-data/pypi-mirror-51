24b237e2f85758b5dea845b27830194e5fccacb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:28 +0000

    Add package naturalselection/0.1.8/naturalselection-0.1.8.tar.gz

---
+++ packages/naturalselection/naturalselection-0.1.8.tar.gz/naturalselection-0.1.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as file_in:
+    long_description = file_in.read()
+
+setuptools.setup(
+    name = 'naturalselection',  
+    entry_points = {'console_scripts' : [
+        'naturalselection = naturalselection:main', 
+        ]},
+    install_requires = ['numpy','matplotlib','tqdm','tensorflow','sklearn'],
+    version = "0.1.8",
+    author = "Dan Saattrup Nielsen",
+    author_email = "saattrupdan@gmail.com",
+    description = "An all-purpose pythonic genetic algorithm",
+    keywords = "genetic algorithm neural network",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/saattrupdan/naturalselection",
+    packages = setuptools.find_packages(),
+    classifiers = [
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: Python Software Foundation License",
+     "Operating System :: OS Independent",
+    ],
+    )
