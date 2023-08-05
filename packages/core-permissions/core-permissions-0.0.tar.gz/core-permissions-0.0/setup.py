9a5b6dac0f2d82b916b783d6a984870db11807d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:40 +0000

    Add package core-permissions/0.0/core-permissions-0.0.tar.gz

---
+++ packages/core-permissions/core-permissions-0.0.tar.gz/core-permissions-0.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+from setuptools import setup, find_packages
+
+version, license = None, None
+with open('permissions/__init__.py', 'r') as fd:
+    content = fd.read()
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)
+    license = re.search(r'^__license__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)
+if version is None: raise RuntimeError('Cannot find version information')
+if license is None: raise RuntimeError('Cannot find license information')
+
+with open('README.md', 'r') as fd:
+    long_description = fd.read()
+
+setup(
+    name='core-permissions',
+    version=version,
+    description='Research CORE ERM - permissions module',
+    author='Ricardo Ribeiro, Hugo Cachitas',
+    author_email='ricardojvr@gmail.com, hugo.cachitas@research.fchampalimaud.org',
+    url='https://github.com/research-core/core-permissions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    license=license,
+)
