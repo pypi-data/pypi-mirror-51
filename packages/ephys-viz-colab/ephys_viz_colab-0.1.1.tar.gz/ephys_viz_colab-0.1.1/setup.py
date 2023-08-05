558304164680993d30bbd783a5e51be7c26cbf43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:12 +0000

    Add package ephys-viz-colab/0.1.1/ephys_viz_colab-0.1.1.tar.gz

---
+++ packages/ephys-viz-colab/ephys_viz_colab-0.1.1.tar.gz/ephys_viz_colab-0.1.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+pkg_name = "ephys_viz_colab"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.1.1",
+    author="Jeremy Magland",
+    description="Neurophysiology visualization widgets",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    scripts=[],
+    install_requires=[
+        'jupyter',
+        'numpy',
+        'mountaintools',
+        'spikeforest',
+        'scipy'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
