2a5ab08d61c3099a71c159c6ef0f90dc5c05626b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:09 +0000

    Add package probdistributions0816/0.1/probdistributions0816-0.1.tar.gz

---
+++ packages/probdistributions0816/probdistributions0816-0.1.tar.gz/probdistributions0816-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='probdistributions0816',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probdistributions0816'],
+      zip_safe=False)
