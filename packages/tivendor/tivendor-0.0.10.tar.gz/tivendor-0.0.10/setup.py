609783c7e84d3258f34fc63bc7b5bbbf044b6b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:05 +0000

    Add package tivendor/0.0.10/tivendor-0.0.10.tar.gz

---
+++ packages/tivendor/tivendor-0.0.10.tar.gz/tivendor-0.0.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+try:
+    long_description = open('README.md', encoding='utf8').read()
+except Exception as e:
+    long_description = ''
+
+
+setup(
+    name='tivendor',
+    version='0.0.10',
+    description='vendor request of taiqiyun',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.22.0',
+        'kafka-python>=1.4.6',
+    ],
+    py_modules=['tivendor'],
+    include_package_data=True,
+)
