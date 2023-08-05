118fb6c5239066abee6ae935c9b42e5e980507af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:37 +0000

    Add package okta-custom-cli/0.0.1/okta-custom-cli-0.0.1.tar.gz

---
+++ packages/okta-custom-cli/okta-custom-cli-0.0.1.tar.gz/okta-custom-cli-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(name='okta-custom-cli',
+      version='0.0.1',
+      description='Sample Okta CLI tool in python',
+      license='MIT',
+      author='Atulya Kumar Pandey',
+      author_email='atul3015@gmail.com',
+      packages=find_packages(),
+      install_requires=[
+          'Click',
+          'pyfiglet',
+          'requests'
+      ],
+      entry_points='''
+            [console_scripts]
+            okta-custom-cli=okta_custom_cli.okta:okta
+        ''',
+      zip_safe=True,
+      include_package_data=True
+      )
