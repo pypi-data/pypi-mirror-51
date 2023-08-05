4ec58db5b0461124484ab3ebab7e4689d6e1d0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:17 +0000

    Add package ariadne-extensions/0.1.3/ariadne-extensions-0.1.3.tar.gz

---
+++ packages/ariadne-extensions/ariadne-extensions-0.1.3.tar.gz/ariadne-extensions-0.1.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from os import path
+
+from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ariadne-extensions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.1.3',
+    url='https://github.com/aleszoulek/ariadne-extensions',
+    author='Ales Zoulek',
+    author_email='ales.zoulek@gmail.com',
+    packages=['ariadne_extensions'],
+    tests_require=['pytest'],
+    setup_requires=['pytest-runner'],
+    install_requires=[
+    ],
+)
