02a8414c3247979da8d3b885d37f45d76932603c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:10 +0000

    Add package pifetcher/0.0.2.6/pifetcher-0.0.2.6.tar.gz

---
+++ packages/pifetcher/pifetcher-0.0.2.6.tar.gz/pifetcher-0.0.2.6/src/pifetcher/work_queue/base_work_queue.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from pifetcher.core import Config
+class BaseWorkQueue(ABC):
+    def __init__(self, queue_name):
+        self.num_work_per_time = Config.queue["num_works_per_time"]
+        pass
+    @abstractmethod
+    def get_work(self):
+        raise NotADirectoryError()
+    @abstractmethod
+    def add_work(self, message):
+        raise NotADirectoryError()
+    @abstractmethod
+    def delete_work(self, id):
+        raise NotADirectoryError()
