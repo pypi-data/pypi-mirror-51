c98c957379fab9e1acc74eb1d31bbfd6c34b0050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:39 +0000

    Add package fitsbits/0.1.1/fitsbits-0.1.1-py3-none-any.whl

---
+++ packages/fitsbits/fitsbits-0.1.1-py3-none-any.whl/fitsbits/_modtemplate.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+'''
+
+#############
+## LOGGING ##
+#############
+
+import logging
+from fitsbits import log_sub, log_fmt, log_date_fmt
+
+DEBUG = False
+if DEBUG:
+    level = logging.DEBUG
+else:
+    level = logging.INFO
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(
+    level=level,
+    style=log_sub,
+    format=log_fmt,
+    datefmt=log_date_fmt,
+)
+
+LOGDEBUG = LOGGER.debug
+LOGINFO = LOGGER.info
+LOGWARNING = LOGGER.warning
+LOGERROR = LOGGER.error
+LOGEXCEPTION = LOGGER.exception
+
+
+#############
+## IMPORTS ##
+#############
