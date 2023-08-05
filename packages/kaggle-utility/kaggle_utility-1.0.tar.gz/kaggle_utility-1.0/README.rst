a582a872ee64d8eb5e7dd839823f1836359ec140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:21 +0000

    Add package kaggle-utility/1.0/kaggle_utility-1.0.tar.gz

---
+++ packages/kaggle-utility/kaggle_utility-1.0.tar.gz/kaggle_utility-1.0/README.rst
@@ -0,0 +1,4 @@
+Kaggle Utility
+=========================
+
+This package will contain all the utility functions for machine learning project.
---
+++ packages/kaggle-utility/kaggle_utility-1.0.tar.gz/kaggle_utility-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kaggle_utility',
+    version='1.0',
+    author='Abdur Rehman Nadeem',
+    description='A utility for kaggle machine learning projects',
+    license='MIT',
+    url='https://github.com/abdurrehman11/kaggle_utility',
+    packages=find_packages(),
+    install_requires=[
+        'scikit-learn==0.20.3',
+        'pandas==0.24.2',
+        'numpy==1.16.3',
+        'xgboost==0.90',
+        ],
+)
