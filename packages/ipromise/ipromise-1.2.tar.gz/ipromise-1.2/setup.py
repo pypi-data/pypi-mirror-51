986e620bd130ed05a53cd60f6a94d53e9110ec24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:44 +0000

    Add package ipromise/1.2/ipromise-1.2.tar.gz

---
+++ packages/ipromise/ipromise-1.2.tar.gz/ipromise-1.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+setup(
+    name='ipromise',
+    version='1.2',
+    description=('A Python base class that provides various decorators for '
+                 'specifying promises relating to inheritance.'),
+    long_description_content_type='text/x-rst',
+    author='Neil Girdhar',
+    author_email='mistersheik@gmail.com',
+    url='https://github.com/NeilGirdhar/ipromise',
+    download_url='https://github.com/neilgirdhar/ipromise/archive/1.2.tar.gz',
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords=['testing', 'logging', 'example'],
+    python_requires='>=3.6',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
