cc56ddf2fbb4c678d80e18ac02eb84434c886814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:15 +0000

    Add package yubikey-manager/3.1.0/yubikey-manager-3.1.0.tar.gz

---
+++ packages/yubikey-manager/yubikey-manager-3.1.0.tar.gz/yubikey-manager-3.1.0/test/util.py
@@ -0,0 +1,23 @@
+from click.testing import CliRunner
+from ykman.cli.__main__ import cli
+import os
+
+
+PKG_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def open_file(*relative_path):
+    return open(os.path.join(PKG_DIR, 'files', *relative_path), 'rb')
+
+
+def ykman_cli(*argv, **kwargs):
+    result = ykman_cli_raw(*argv, **kwargs)
+    if result.exit_code != 0:
+        raise result.exception
+    return result.output
+
+
+def ykman_cli_raw(*argv, **kwargs):
+    runner = CliRunner()
+    result = runner.invoke(cli, list(argv), obj={}, **kwargs)
+    return result
