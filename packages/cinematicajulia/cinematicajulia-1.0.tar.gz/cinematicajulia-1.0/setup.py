b8fe6a60084f11785b6e6fb7e420d887ebd81182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:24 +0000

    Add package cinematicajulia/1.0/cinematicajulia-1.0.tar.gz

---
+++ packages/cinematicajulia/cinematicajulia-1.0.tar.gz/cinematicajulia-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='cinematicajulia',
+    version='1.0',
+    author='Júlia Valentim',
+    author_email='juliavalentimop@gmail.com',
+    description='Equações da cinemática',
+    py_modules = ['julia']
+)
