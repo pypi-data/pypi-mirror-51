35fa8530b1cf7d7cf158bdda0fcf3b7bf0189c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package attrs-mate/0.0.5/attrs_mate-0.0.5-py2.py3-none-any.whl

---
+++ packages/attrs-mate/attrs_mate-0.0.5-py2.py3-none-any.whl/attrs_mate/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.5"
+__short_description__ = "A plugin extends power of attrs library."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__maintainer__ = "Sanhe Hu"
+__maintainer_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
+
+
+try:
+    from .mate import attr, AttrsClass, LazyClass
+except ImportError:  # pragma: no cover
+    pass
