4b4a75f40c9b754becd8ff7918d4dcc008127d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:04 +0000

    Add package django-squirrell-materialize/1.3/django_squirrell_materialize-1.3.tar.gz

---
+++ packages/django-squirrell-materialize/django_squirrell_materialize-1.3.tar.gz/django_squirrell_materialize-1.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='django_squirrell_materialize',
+     version='1.3',
+     scripts=['ardilla'] ,
+     author="ESCO",
+     author_email="franjcesco@gmail.com",
+     description="A Docker and AWS utility package",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/esco/django_ardilla_materialize",
+     packages=setuptools.find_packages(),
+     include_package_data=True,
+     zip_safe=True,
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
