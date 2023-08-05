5be36cb342d70fde5f25b7d086afe48c374966b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package mth/1.0/mth-1.0.tar.gz

---
+++ packages/mth/mth-1.0.tar.gz/mth-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mth',
+    version='1.0',
+    author='Ojuara Abaporujucaíba',
+    author_email='ojuara@araujo.com',
+    description='Brincando com distribuição python',
+    py_modules = ['ojuara']
+)
