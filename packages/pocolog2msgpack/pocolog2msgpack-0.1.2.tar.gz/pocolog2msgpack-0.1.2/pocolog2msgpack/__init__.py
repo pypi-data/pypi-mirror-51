03afc4fc453559df9eaa88017e441ac488bd7533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:23 +0000

    Add package pocolog2msgpack/0.1.2/pocolog2msgpack-0.1.2.tar.gz

---
+++ packages/pocolog2msgpack/pocolog2msgpack-0.1.2.tar.gz/pocolog2msgpack-0.1.2/pocolog2msgpack/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+from .converter import Converter
+from .x_2_y_file import X2YFile
+
+# Version
+__version__ = "0.1.2"
---
+++ packages/pocolog2msgpack/pocolog2msgpack-0.1.2.tar.gz/pocolog2msgpack-0.1.2/setup.py
@@ -0,0 +1,40 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pocolog2msgpack",
+    version="0.1.2",
+    description="""Python wrapper around procolog2msgpack,
+    to convert rock-log- to msgpack-files""",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Johann Hemmann",
+    author_email="johann.hemmann@dfki.de",
+    project_urls={
+            "Source Code": "https://github.com/Urhengulas/python-procolog2msgpack",
+    },
+    license="LGPL-3.0",
+    classifiers=[
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(
+        exclude=("convert_log_files")
+    ),
+    include_package_data=True,
+    install_requires=[
+        "msgpack",
+        "docker",
+    ],
+    python_reqires=">=3.5"
+)
