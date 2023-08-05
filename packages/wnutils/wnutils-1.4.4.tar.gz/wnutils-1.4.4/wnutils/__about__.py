eae5a72a5e0d2a12a807c293876ba298c32c8d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:59 +0000

    Add package wnutils/1.4.4/wnutils-1.4.4.tar.gz

---
+++ packages/wnutils/wnutils-1.4.4.tar.gz/wnutils-1.4.4/wnutils/__about__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+__title__ = "wnutils"
+__summary__ = "Python project to read and plot webnucleo files"
+__version__ = "1.4.4"
+__author__ = "Clemson University"
+__copyright__ = "Clemson University, 2018-2019"
