5e9fb7809f1df8813ed26ef000f69904c647918f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:45:17 +0000

    Add package nkn-sdk/0.1.0/nkn-sdk-0.1.0.tar.gz

---
+++ packages/nkn-sdk/nkn-sdk-0.1.0.tar.gz/nkn-sdk-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nkn-sdk',
+    version='0.1.0',
+    author='heron',
+    author_email='peng.liu@nkn.org',
+    description='NKN client and wallet SDK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/nknorg/nkn-sdk-py3',
+    packages=setuptools.find_packages(),
+    install_requires=['pynacl', 'pycrypto', 'base58', 'requests', 'protobuf'],
+    python_requires='>=3.3',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
