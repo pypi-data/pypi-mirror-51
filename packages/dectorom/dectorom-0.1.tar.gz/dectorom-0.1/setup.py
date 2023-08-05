47776b08fcc58a42803153435ecf42a2ac68adef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:55 +0000

    Add package dectorom/0.1/dectorom-0.1.tar.gz

---
+++ packages/dectorom/dectorom-0.1.tar.gz/dectorom-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='dectorom',
+  packages=['dectorom'],
+  version='0.1',
+  description='Module for converting numbers from decimal to roman',
+  author='Murad Murtuzaliev',
+  author_email='mur.murtuzaliev@yandex.ru',
+  url='https://github.com/muradmurtuzaliev/dectorom',
+  download_url='https://github.com/muradmurtuzaliev/dectorom',
+  keywords=['decimal', 'roman', 'convert'],
+  classifiers=[],
+)
