cfe940124d2ba3f0fa45d1e7eb33dc6b4695b665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:44 +0000

    Add package abito/0.0.10/abito-0.0.10-py3-none-any.whl

---
+++ packages/abito/abito-0.0.10-py3-none-any.whl/abito/__init__.py
@@ -0,0 +1,11 @@
+from .stats import LinearSample, RatioSample, mann_whitney_u_test_from_stats, t_test_from_stats
+
+
+__version__ = '0.0.10'
+
+__all__ = [
+    'LinearSample',
+    'RatioSample',
+    'mann_whitney_u_test_from_stats',
+    't_test_from_stats',
+]
