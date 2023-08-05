17ac8103b73dedecff2d7b2b886663dfa3fd5afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:48 +0000

    Add package drdigit-brezniczky/0.0.3/drdigit_brezniczky-0.0.3-py3-none-any.whl

---
+++ packages/drdigit-brezniczky/drdigit_brezniczky-0.0.3-py3-none-any.whl/drdigit/twin_digits.py
@@ -0,0 +1,17 @@
+from scipy.stats import binom
+import numpy as np
+
+
+def prob_of_twins(x):
+    """ Return the probability of at least this many repeat pairs (adjacent
+        digits being equal) in the given sequence of digits.
+        In a sequence of 1, 1, 1, 2, 3, 3, 4 there will be 3 pairs considered.
+
+        x: list-like of digits
+    """
+    if len(x) <= 1:
+        # nothing to see here, not enough information
+        return 1
+    x = np.array(x)
+    count = sum(x[:-1] == x[1:])
+    return 1 - binom(len(x) - 1, 0.1).cdf(count - 1)
