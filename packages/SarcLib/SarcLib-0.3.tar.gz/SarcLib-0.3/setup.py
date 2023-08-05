ffe3e3ba7ccb6a007c74207f57f5c2423deda5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:23 +0000

    Add package SarcLib/0.3/SarcLib-0.3.tar.gz

---
+++ packages/SarcLib/SarcLib-0.3.tar.gz/SarcLib-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'SarcLib',
+    packages = ['SarcLib'],
+    version = '0.3',
+    description = 'A library for handling the Nintendo SARC archive format',
+    author = 'AboodXD',
+    author_email = 'aboood40091@gmail.com',
+    url = 'https://github.com/aboood40091/SarcLib',
+    download_url = 'https://github.com/aboood40091/SarcLib/archive/v0.3.tar.gz',
+    keywords = ['arc', 'sarc', 'szs', 'yaz0'],
+    license = 'GNU General Public License v3 (GPLv3)',
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Archiving',
+    ],
+)
