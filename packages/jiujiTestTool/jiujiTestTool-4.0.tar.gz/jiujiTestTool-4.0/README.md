c99d117c9264abdc9a51ec84c98bcaf793e2543e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:18 +0000

    Add package jiujiTestTool/4.0/jiujiTestTool-4.0.tar.gz

---
+++ packages/jiujiTestTool/jiujiTestTool-4.0.tar.gz/jiujiTestTool-4.0/README.md
@@ -0,0 +1,2 @@
+# jiujiTool
+test
---
+++ packages/jiujiTestTool/jiujiTestTool-4.0.tar.gz/jiujiTestTool-4.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: lol666666
+# Description:
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'jiujiTestTool',
+    version = '4.0',
+    keywords='jiujiTestTool',
+    description = 'a library for jiujiTool Developer',
+    license = 'MIT License',
+    author = 'lol666666',
+    url = 'https://github.com/lol666666/jiujiTool.git',
+    author_email = 'loveworkyeah@163.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [
+
+        ],
+
+     
+)
