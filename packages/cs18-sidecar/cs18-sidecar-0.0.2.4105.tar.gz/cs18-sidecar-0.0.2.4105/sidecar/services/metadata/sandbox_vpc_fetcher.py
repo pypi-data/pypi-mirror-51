b8097a5b4ab5e78bd29bd986b244b44e28424e93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:45:15 +0000

    Add package cs18-sidecar/0.0.2.4105/cs18-sidecar-0.0.2.4105.tar.gz

---
+++ packages/cs18-sidecar/cs18-sidecar-0.0.2.4105.tar.gz/cs18-sidecar-0.0.2.4105/sidecar/services/metadata/sandbox_vpc_fetcher.py
@@ -0,0 +1,10 @@
+from sidecar.model.objects import AwsSidecarConfiguration
+from sidecar.services.metadata.sandbox_public_address_fetcher import SandboxMetadataFetcher
+
+
+class SandboxVpcFetcher(SandboxMetadataFetcher):
+    def __init__(self, config: AwsSidecarConfiguration):
+        self.config = config
+
+    def get_value(self) -> str:
+        return self.config.virtual_network_id
