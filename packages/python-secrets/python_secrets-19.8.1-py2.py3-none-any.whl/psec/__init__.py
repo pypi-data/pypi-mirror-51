9505fe622146e2e0e65df605f26739f4c8b28d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:47 +0000

    Add package python-secrets/19.8.1/python_secrets-19.8.1-py2.py3-none-any.whl

---
+++ packages/python-secrets/python_secrets-19.8.1-py2.py3-none-any.whl/psec/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import pbr.version
+
+version_info = pbr.version.VersionInfo('psec')
+try:
+    __version__ = version_info.cached_version_string()
+except Exception:
+    __version__ = '19.8.0'
+
+try:
+    __release__ = version_info.release_string()
+except Exception:
+    __release__ = '19.8.0'
+
+__author__ = 'Dave Dittrich'
+__email__ = 'dave.dittrich@gmail.com'
+
+__all__ = ['__author__', '__email__', '__version__', '__release__']
+
+# vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
