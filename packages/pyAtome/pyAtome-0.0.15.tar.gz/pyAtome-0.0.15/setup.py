db005b81512fc5d657861268152cf157d467d080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:58 +0000

    Add package pyAtome/0.0.15/pyAtome-0.0.15.tar.gz

---
+++ packages/pyAtome/pyAtome-0.0.15.tar.gz/pyAtome-0.0.15/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyAtome',
+    version="0.0.15",
+    license='Apache Software License',
+    author='Pierre Ourdouille',
+    author_email='baqs@users.github.com',
+    description='Get your energy consumption from Atome Linky device',
+    long_description=long_description,
+    url='http://github.com/baqs/pyAtome/',
+    packages=setuptools.find_packages(include=['pyatome']),
+    setup_requires=[
+        'requests',
+        'setuptools'
+    ],
+    install_requires=[
+        'requests',
+        'fake_useragent',
+        'simplejson'
+    ],
+    entry_points={
+    'console_scripts': [
+        'pyatome = pyatome.__main__:main'
+    ]
+    }
+)
