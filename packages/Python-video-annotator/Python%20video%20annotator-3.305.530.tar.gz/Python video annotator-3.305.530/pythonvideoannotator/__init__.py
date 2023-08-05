0434543d01e66d417021a3e4f43e81798a21aab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:32 +0000

    Add package Python-video-annotator/3.305.530/Python%20video%20annotator-3.305.530.tar.gz

---
+++ packages/Python-video-annotator/Python%20video%20annotator-3.305.530.tar.gz/Python video annotator-3.305.530/pythonvideoannotator/__init__.py
@@ -0,0 +1,27 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+__version__ = "3.305.530"
+__author__ 		= ["Ricardo Ribeiro", "Carlos Mao de Ferro", "Hugo Cachitas"]
+__credits__ 	= ["Ricardo Ribeiro", "Carlos Mao de Ferro", "Hugo Cachitas"]
+__license__ 	= "Attribution-NonCommercial-ShareAlike 4.0 International"
+__maintainer__ 	= ["Ricardo Ribeiro", "Carlos Mao de Ferro"]
+__email__ 		= ["ricardojvr at gmail.com", "cajomferro at gmail.com"]
+__status__ 		= "Development"
+
+from confapp import conf; conf += 'pythonvideoannotator.settings'
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(conf.APP_LOG_HANDLER_LEVEL)
+
+if conf.APP_LOG_HANDLER_FILE:
+    logger = logging.getLogger()
+    loggers_formatter = logging.Formatter(conf.PYFORMS_LOG_FORMAT)
+
+    fh = logging.FileHandler(conf.APP_LOG_HANDLER_FILE)
+    fh.setLevel(conf.APP_LOG_HANDLER_FILE_LEVEL)
+    fh.setFormatter(loggers_formatter)
+    logger.addHandler(fh)
