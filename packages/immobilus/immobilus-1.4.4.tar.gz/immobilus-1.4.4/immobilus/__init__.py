fc701cbae6351cb7d4ed1544f287dcc7e4e704c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:15 +0000

    Add package immobilus/1.4.4/immobilus-1.4.4.tar.gz

---
+++ packages/immobilus/immobilus-1.4.4.tar.gz/immobilus-1.4.4/immobilus/__init__.py
@@ -0,0 +1,4 @@
+from immobilus.logic import immobilus
+
+
+__all__ = ['immobilus']
---
+++ packages/immobilus/immobilus-1.4.4.tar.gz/immobilus-1.4.4/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='immobilus',
+    version='1.4.4',
+    description='Say `Immobilus!` to freeze your tests',
+    long_description=long_description,
+    author='Eugene Pokidov',
+    author_email='pokidovea@gmail.com',
+    url='https://github.com/pokidovea/immobilus',
+    packages=['immobilus'],
+    install_requires=['python-dateutil'],
+    include_package_data=True,
+    license='Apache 2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
