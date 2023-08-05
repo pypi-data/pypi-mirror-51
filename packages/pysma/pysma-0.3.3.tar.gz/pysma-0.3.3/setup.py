0ba4ade366971332390bea12b0f7b99a87f08630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:37 +0000

    Add package pysma/0.3.3/pysma-0.3.3.tar.gz

---
+++ packages/pysma/pysma-0.3.3.tar.gz/pysma-0.3.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""pysma library setup."""
+from setuptools import setup
+
+VERSION = "0.3.3"
+URL = "https://github.com/kellerza/pysma"
+
+setup(
+    name="pysma",
+    version=VERSION,
+    description="Library to interface an SMA Solar WebConnect module",
+    url=URL,
+    download_url="{}/tarball/{}".format(URL, VERSION),
+    author="Johann Kellerman",
+    author_email="kellerza@gmail.com",
+    license="MIT",
+    packages=["pysma"],
+    install_requires=["aiohttp>3,<4", "async_timeout>3,<4", "attrs>18", "jmespath<2"],
+    zip_safe=True,
+)
