957fc10bfb961bd5073a56fd49a9f19a4e5d862a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:58 +0000

    Add package owldata/0.0.14/owldata-0.0.14.tar.gz

---
+++ packages/owldata/owldata-0.0.14.tar.gz/owldata-0.0.14/owldata/__init__.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# =====================================================================
+# Copyright (C) 2018-2019 by Owl Data
+# author: Danny, Destiny
+
+# =====================================================================
+
+from .api import OwlData
+from .__version__ import __version__
+
+__docformat__ = 'restructuredtext'
+__doc__ = '''
+OwlData 數據貓頭鷹 API
+==============================
+## 數據貓頭鷹官方網站: https://owl.cmoney.com.tw/Owl/
+
+**owldata** 提供一個穩定且快速的介面獲取數據貓頭鷹任何資料
+
+'''
