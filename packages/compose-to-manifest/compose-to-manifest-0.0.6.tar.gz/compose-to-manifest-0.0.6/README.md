60736f79c9acf6ec3d9c049cdd14b90f6005c563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:38 +0000

    Add package compose-to-manifest/0.0.6/compose-to-manifest-0.0.6.tar.gz

---
+++ packages/compose-to-manifest/compose-to-manifest-0.0.6.tar.gz/compose-to-manifest-0.0.6/README.md
@@ -0,0 +1,30 @@
+# compose-to-manifest
+Convert Docker Compose project to Azure IoT Edge deployment manifest
+
+## Requirement
+1. Make sure docker is running on your system before using this tool.
+2. Python version >= 3.6.0
+## Installation
+```bash
+pip install compose-to-manifest
+```
+
+
+## Usage
+```
+python3 convertor.py [-h] -t [file|project] -i docker_compose_file_path -o output_path [-r your_docker_container_registry_address]
+```
+
+## Example
+  ```bash
+  git clone https://github.com/VSChina/compose-to-manifest
+  ```
+In compose-to-manifest folder  
+1. Convert single file
+    ```bash
+    compose-to-manifest -t file -i example/flask-redis/docker-compose.yml -o example/flask-redis/deployment.template.json
+    ```
+2. Convert project
+    ```bash
+    compose-to-manifest -t project -i example/flask-redis/docker-compose.yml -o example/flask-redis-edge
+    ```
---
+++ packages/compose-to-manifest/compose-to-manifest-0.0.6.tar.gz/compose-to-manifest-0.0.6/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="compose-to-manifest",
+    version="0.0.6",
+    author="typeli",
+    author_email="juncli@outlook.com",
+    description="Convert Docker Compose project to Azure IoT Edge deployment manifest",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://https://github.com/VSChina/compose-to-manifest",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+        "console_scripts": ["compose-to-manifest=convertor.convertor:main"],
+    },
+    install_requires=[
+        "docker-compose",
+    ],
+)
+
+# python setup.py sdist bdist_wheel
+
+# test
+# python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# python3.6 -m pip install --user --index-url https://test.pypi.org/simple/  compose-to-manifest --upgrade
+
+# upload to pypi
+# python -m twine upload dist/*
+# python3.6 -m pip install compose-to-manifest
