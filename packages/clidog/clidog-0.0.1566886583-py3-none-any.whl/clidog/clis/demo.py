ec8d6a2d4e9b3fe496a977d38993fa9efdd259ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:48 +0000

    Add package clidog/0.0.1566886583/clidog-0.0.1566886583-py3-none-any.whl

---
+++ packages/clidog/clidog-0.0.1566886583-py3-none-any.whl/clidog/clis/demo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : tql-CLI.
+# @File         : demo
+# @Time         : 2019-07-16 20:38
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+class A:
+
+    def __init__(self, d={'x': 1, 'y': 2}):
+        print(d)
+        for k, v in d.items():
+            setattr(self, k, v)
+        print(self.x)
+
+    def f(self, x, y):
+        return x+1
+
+
+if __name__ == '__main__':
+    A()
