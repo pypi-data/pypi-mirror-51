757b0470c4fd5c722c64ab448aaed63b3c746d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:06 +0000

    Add package qcelemental/0.7.0/qcelemental-0.7.0-py3-none-any.whl

---
+++ packages/qcelemental/qcelemental-0.7.0-py3-none-any.whl/qcelemental/data/__init__.py
@@ -0,0 +1,7 @@
+import json
+import os
+import pathlib
+
+from .alvarez_2008_covalent_radii import alvarez_2008_covalent_radii
+from .nist_2011_atomic_weights import nist_2011_atomic_weights
+from .nist_2014_codata import nist_2014_codata
