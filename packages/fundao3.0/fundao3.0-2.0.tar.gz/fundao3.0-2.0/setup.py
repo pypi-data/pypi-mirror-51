cb73435efce646d3d2fdec555ad2b846b5c8ce3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:16 +0000

    Add package fundao3.0/2.0/fundao3.0-2.0.tar.gz

---
+++ packages/fundao3.0/fundao3.0-2.0.tar.gz/fundao3.0-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='fundao3.0',
+    version='2.0',
+    author='Ojuara Abaporujucaíba',
+    author_email='ojuara@araujo.com',
+    description='Brincando com distribuição python',
+    py_modules = ['ojuara']
+)
