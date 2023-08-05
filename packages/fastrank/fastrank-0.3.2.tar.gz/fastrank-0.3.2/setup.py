fbb2c643fd3b9629f3bfa0901c6e2349365de30d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package fastrank/0.3.2/fastrank-0.3.2.tar.gz

---
+++ packages/fastrank/fastrank-0.3.2.tar.gz/fastrank-0.3.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+pyfastrank_version = '0.3.2'
+cfastrank_version = '0.3.2'
+
+setup(
+    name='fastrank',
+    version=pyfastrank_version,
+    author="John Foley",
+    author_email="jjfoley@smith.edu",
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    description="A set of learning-to-rank algorithms.",
+    url="https://github.com/jjfiv/fastrank",
+    packages=find_packages(),
+    install_requires=['cfastrank=={0}'.format(cfastrank_version), "attrs", "cffi", "numpy", "scikit-learn", "ujson"],
+    platforms='any',
+)
