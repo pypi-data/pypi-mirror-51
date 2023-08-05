c58626245cdda74ed2a459dea30a17e1e282b96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:48 +0000

    Add package rio-alpha/1.0.1/rio_alpha-1.0.1-py3-none-any.whl

---
+++ packages/rio-alpha/rio_alpha-1.0.1-py3-none-any.whl/rio_alpha/alpha.py
@@ -0,0 +1,97 @@
+"""Alpha masking worker and concurrent processor."""
+
+import numpy as np
+import rasterio
+import riomucho
+from rio_alpha.alpha_mask import mask_exact
+
+
+def alpha_worker(open_file, window, ij, g_args):
+    """rio mucho worker for alpha. It reads input
+    files and perform alpha calculations on each window.
+
+    Parameters
+    ------------
+    open_files: list of rasterio open files
+    window: Window object
+            A window is a view onto a rectangular subset of a
+            raster dataset.
+    g_args: dictionary
+
+    Returns
+    ---------
+    rgba: ndarray
+          ndarray with original RGB bands of shape (3, rows, cols)
+          and a mask of shape (rows, cols) where
+          opaque == 0 and transparent == max of dtype
+    """
+    src = open_file[0]
+
+    arr = src.read(window=window)
+
+    # Determine Alpha Band
+    if g_args["ndv"]:
+        # User-supplied nodata value
+        alpha = mask_exact(arr, g_args["ndv"])
+    else:
+        # Let rasterio decide
+        alpha = src.dataset_mask(window=window)
+
+    # Replace or Add alpha band to input data
+    if arr.shape[0] == 4:
+        # replace band 4 with new alpha mask
+        # (likely the same but let's not make that assumption)
+        rgba = arr.copy()
+        rgba[3] = alpha
+    elif arr.shape[0] == 3:
+        # stack the alpha mask to add band 4
+        rgba = np.append(arr, alpha[np.newaxis, :, :], axis=0)
+    else:
+        raise ValueError("Array must have 3 or 4 bands (RGB or RGBA)")
+
+    return rgba
+
+
+_alpha_worker = alpha_worker
+
+
+def add_alpha(src_path, dst_path, ndv, creation_options, processes):
+    """
+    Parameters
+    ------------
+    src_paths: list of strings
+    dst_path: string
+    ndv: list
+         a list of floats where the
+         length of the list = band count
+    creation_options: dict
+    processes: integer
+
+
+    Returns
+    ---------
+    None
+        Output is written to dst_path
+    """
+
+    with rasterio.open(src_path) as src:
+        dst_profile = src.profile
+
+    dst_profile.update(**creation_options)
+
+    dst_profile.pop("photometric", None)
+
+    dst_profile.update(count=4, nodata=None)
+
+    global_args = {"src_nodata": 0, "dst_dtype": dst_profile["dtype"], "ndv": ndv}
+
+    with riomucho.RioMucho(
+        [src_path],
+        dst_path,
+        _alpha_worker,
+        options=dst_profile,
+        global_args=global_args,
+        mode="manual_read",
+    ) as rm:
+
+        rm.run(processes)
---
+++ packages/rio-alpha/rio_alpha-1.0.1-py3-none-any.whl/rio_alpha/findnodata.py
@@ -0,0 +1,144 @@
+"""Find nodata value of datasets."""
+
+import click
+import numpy as np
+import rasterio
+from scipy.stats import mode
+
+from rio_alpha.utils import (
+    _convert_rgb,
+    _compute_continuous,
+    _debug_mode,
+    _search_image_edge,
+    _evaluate_count,
+)
+
+
+def discover_ndv(rgb_orig, debug, verbose):
+    """Returns nodata value by calculating mode of RGB array
+
+    Parameters
+    ----------
+    rgb_orig: ndarray
+              array of input pixels of shape (rows, cols, depth)
+    debug: Boolean
+           Enables matplotlib & printing of figures
+    verbose: Boolean
+             Prints extra information, like competing candidate values
+
+
+    Returns
+    -------
+    list of nodata value candidates or empty string if none found
+
+    """
+    rgb_mod, rgb_mod_flat = _convert_rgb(rgb_orig)
+    # Full image mode bincount
+    mode_vals = mode(rgb_mod_flat)
+    candidate_original = [int((mode_vals[0])[0, i]) for i in range(3)]
+
+    # Find continuous values in RGB array
+    candidate_continuous, arr = _compute_continuous(rgb_mod, 1)
+
+    # If debug mode, print histograms & be verbose
+    if debug:
+        click.echo("Original image ndv candidate: %s" % (str(candidate_original)))
+        click.echo("Filtered image ndv candidate: %s" % (str(candidate_continuous)))
+        outplot = "/tmp/hist_plot.png"
+        _debug_mode(rgb_mod_flat, arr, outplot)
+
+    # Compare ndv candidates from full & squished image
+    candidate_list = [
+        i for i, j in zip(candidate_original, candidate_continuous) if i == j
+    ]
+
+    # If candidates from original & filtered images match exactly,
+    # print value & exit
+    if len(candidate_list) == 3:
+        return candidate_list
+
+    # If candidates do not match exactly, continue vetting process
+    # by searching image edge for frequency of each candidate
+    elif len(candidate_list) < 3:
+        if verbose:
+            click.echo(
+                "Competing ndv candidates...searching "
+                "image collar for value frequency. "
+                "Candidate list: %s" % str(candidate_list)
+            )
+
+        count_img_edge_full, count_img_edge_continuous = _search_image_edge(
+            rgb_mod, candidate_original, candidate_continuous
+        )
+
+        if verbose:
+            for candidate in (candidate_original, candidate_continuous):
+                click.echo(
+                    "Candidate value: %s "
+                    "Candidate count: %s "
+                    "Continuous count: %s"
+                    % (
+                        str(candidate),
+                        str(count_img_edge_full),
+                        str(count_img_edge_continuous),
+                    )
+                )
+
+        output = _evaluate_count(
+            count_img_edge_full, count_img_edge_continuous, verbose
+        )
+
+        return output
+
+    else:
+        raise ValueError("Invalid candidate list {!r}".format(candidate_list))
+
+
+def determine_nodata(src_path, user_nodata, discovery, debug, verbose):
+    """Worker function for determining nodata
+
+    Parameters
+    ----------
+    src_path: string
+    user_nodata: string/integer
+                 User supplies the nodata value,
+                 input a single value or a string of list
+    discovery: Boolean
+               determines nodata if alpha channel does not exist
+               or internal ndv does not exist
+    debug: Boolean
+           Enables matplotlib & printing of figures
+    verbose: Boolean
+             Prints extra information, like competing candidate values
+
+
+    Returns
+    -------
+    nodata value: string
+                  string(int) or stringified array of values of
+                  len == the number of bands.
+                  For example, string([int(ndv), int(ndv), int(ndv)])
+    """
+
+    with rasterio.open(src_path, "r") as src:
+        count = src.count
+        data = np.rollaxis(src.read(), 0, 3)
+
+    if user_nodata:
+        return user_nodata
+
+    if count == 4:
+        return "alpha"
+    else:
+        nodata = src.nodata
+        if nodata is None:
+            if discovery:
+                candidates = discover_ndv(data, debug, verbose)
+                if len(candidates) != 3:
+                    return ""
+                else:
+                    return "[{}, {}, {}]".format(*candidates)
+            else:
+                return ""
+        else:
+            return "%s" % (str(int(nodata)))
