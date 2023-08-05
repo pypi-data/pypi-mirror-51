a46bae0a4678f90470d4c993c3d9c8872d7ab547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:44 +0000

    Add package zhlite/1.6.1/zhlite-1.6.1.tar.gz

---
+++ packages/zhlite/zhlite-1.6.1.tar.gz/zhlite-1.6.1/zhlite/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from zhlite.zhlite import *
+
+__author__ = "zhangbo"
+__email__ = "deplives@deplives.com"
+
+__license__ = "MIT"
+
+__version__ = "1.6.1"
+__date__ = "2019-08-24"
+__release__ = "更新：减少了请求次数，降低账号被限制的风险，加快了数据获取速度"
+
+
+class Version(object):
+
+    def __init__(self):
+        self.info = {
+            "version": __version__,
+            "date": __date__,
+            "release": __release__
+        }
+
+    def __str__(self):
+        return " ".join([f"{k}: {v}" for k, v in self.info.items()])
+
+
+version = Version()
+
+__all__ = ["Auth", "User", "Question", "Answer", "Article"]
