fbf3f6eabdfdc1aea1c7e88313f42207f5351f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:36 +0000

    Add package catt/0.10.0/catt-0.10.0.tar.gz

---
+++ packages/catt/catt-0.10.0.tar.gz/catt-0.10.0/catt/util.py
@@ -0,0 +1,85 @@
+import ipaddress
+import json
+import tempfile
+import time
+from pathlib import Path
+
+import click
+import ifaddr
+
+
+def warning(msg):
+    click.secho("Warning: ", fg="red", nl=False, err=True)
+    click.echo("{}.".format(msg), err=True)
+
+
+def echo_json(data_dict):
+    click.echo(json.dumps(data_dict, indent=4, default=str))
+
+
+def guess_mime(path):
+    # source: https://developers.google.com/cast/docs/media
+    extension = Path(path).suffix.lower()
+    extensions = {
+        ".mp4": "video/mp4",
+        ".m4a": "audio/mp4",
+        ".mp3": "audio/mp3",
+        ".mpa": "audio/mpeg",
+        ".webm": "video/webm",
+        ".mkv": "video/x-matroska",
+        ".bmp": "image/bmp",
+        ".jpg": "image/jpeg",
+        ".gif": "image/gif",
+        ".png": "image/png",
+        ".webp": "image/web",
+    }
+    return extensions.get(extension, "video/mp4")
+
+
+def hunt_subtitles(video):
+    """Searches for subtitles in the current folder"""
+
+    video_path = Path(video)
+    video_path_stem_lower = video_path.stem.lower()
+    for entry_path in video_path.parent.iterdir():
+        if entry_path.is_dir():
+            continue
+        if entry_path.stem.lower().startswith(video_path_stem_lower) and entry_path.suffix.lower() in [".vtt", ".srt"]:
+            return str(entry_path.resolve())
+    return None
+
+
+def create_temp_file(content):
+    with tempfile.NamedTemporaryFile(mode="w+b", suffix=".vtt", delete=False) as tfile:
+        tfile.write(content.encode())
+        return tfile.name
+
+
+def human_time(seconds: int):
+    return time.strftime("%H:%M:%S", time.gmtime(seconds))
+
+
+def get_local_ip(host):
+    for adapter in ifaddr.get_adapters():
+        for adapter_ip in adapter.ips:
+            aip = adapter_ip.ip[0] if isinstance(adapter_ip.ip, tuple) else adapter_ip.ip
+            try:
+                if not isinstance(ipaddress.ip_address(host), type(ipaddress.ip_address(aip))):
+                    raise ValueError
+            except ValueError:
+                continue
+            ipt = [(ip, adapter_ip.network_prefix) for ip in (aip, host)]
+            catt_net, cc_net = [ipaddress.ip_network("{0}/{1}".format(*ip), strict=False) for ip in ipt]
+            if catt_net == cc_net:
+                return aip
+            else:
+                continue
+
+
+def is_ipaddress(device):
+    try:
+        ipaddress.ip_address(device)
+    except ValueError:
+        return False
+    else:
+        return True
---
+++ packages/catt/catt-0.10.0.tar.gz/catt-0.10.0/tests/test_catt.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from catt.controllers import Cache
+from catt.stream_info import StreamInfo
+
+
+class TestThings(unittest.TestCase):
+    def test_stream_info_youtube_video(self):
+        stream = StreamInfo("https://www.youtube.com/watch?v=VZMfhtKa-wo")
+        self.assertIn("https://", stream.video_url)
+        self.assertEqual(stream.video_id, "VZMfhtKa-wo")
+        self.assertTrue(stream.is_remote_file)
+        self.assertEqual(stream.extractor, "youtube")
+
+    def test_stream_info_youtube_playlist(self):
+        stream = StreamInfo("https://www.youtube.com/playlist?list=PL9Z0stL3aRykWNoVQW96JFIkelka_93Sc")
+        self.assertIsNone(stream.video_url)
+        self.assertEqual(stream.playlist_id, "PL9Z0stL3aRykWNoVQW96JFIkelka_93Sc")
+        self.assertTrue(stream.is_playlist)
+        self.assertEqual(stream.extractor, "youtube")
+
+    def test_stream_info_other_video(self):
+        stream = StreamInfo("https://www.twitch.tv/buddha/clip/OutstandingSquareMallardBudStar")
+        self.assertIn("https://", stream.video_url)
+        self.assertEqual(stream.video_id, "471296669")
+        self.assertTrue(stream.is_remote_file)
+        self.assertEqual(stream.extractor, "twitch")
+
+    def test_cache(self):
+        cache = Cache()
+        cache.set_data("name", "ip", "port")
+        self.assertEqual(cache.get_data("name"), ("ip", "port"))
+
+        cache.clear()
+        cache = Cache()
+        self.assertEqual(cache.get_data("name"), (None, None))
+        cache.clear()
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(unittest.main())
