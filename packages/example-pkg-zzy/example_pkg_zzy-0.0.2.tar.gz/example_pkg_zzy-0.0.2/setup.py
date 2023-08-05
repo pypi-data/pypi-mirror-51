171de9539d9c130b51663b221aaa72aed6dafd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:14 +0000

    Add package example-pkg-zzy/0.0.2/example_pkg_zzy-0.0.2.tar.gz

---
+++ packages/example-pkg-zzy/example_pkg_zzy-0.0.2.tar.gz/example_pkg_zzy-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    desc = f.read()
+
+setuptools.setup(
+    name="example_pkg_zzy",
+    version='0.0.2',
+    author='824zzy',
+    keywards=['pip', 'test'],
+    author_email='zhuzhengyuan824@gmail.com',
+    description='Template_package',
+    long_desc = desc,
+    long_desc_content_type='text/markdown',
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",],
+    install_requires=['tqdm',],
+    
+)
