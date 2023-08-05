4e0d4939f1ec19be5ff0d39db7cafa7b4d630250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package hanime-scraper/1.5.0/hanime-scraper-1.5.0.tar.gz

---
+++ packages/hanime-scraper/hanime-scraper-1.5.0.tar.gz/hanime-scraper-1.5.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import sys
+import hanime
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hanime-scraper",
+    version="1.5.0",
+    author="WorstGameDev",
+    author_email="worstgamedev@gmail.com",
+    description="A small commandline script for hanime community image scraping.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/worstgamedev/hanime-scraper",
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['hanime = hanime.appstart:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free For Educational Use",
+        "Operating System :: OS Independent",
+    ],
+)
