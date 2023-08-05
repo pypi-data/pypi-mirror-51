6a5aeabfadcbf6232406a89e98333f6c45089380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:31 +0000

    Add package rest-framework-swagger-zl/0.0.3/rest_framework_swagger_zl-0.0.3.tar.gz

---
+++ packages/rest-framework-swagger-zl/rest_framework_swagger_zl-0.0.3.tar.gz/rest_framework_swagger_zl-0.0.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name = "rest_framework_swagger_zl",
+    version = "0.0.3",
+    keywords = ("pip", "rest_framework_swagger_zl"),
+    description = "generate api doc html,Swagger UI for Django REST Framework 3.5+",
+    long_description = "generate api doc html,Swagger UI for Django REST Framework 3.5+",
+    license='FreeBSD License',
+    url = "https://github.com/zhangliu520/rest_framework_swagger_zl.git",
+    author = "mrzl",
+    author_email = "752477168@qq.com",
+    packages = find_packages(),
+include_package_data=True,
+    platforms = "any",
+    install_requires = [],
+
+)
+
