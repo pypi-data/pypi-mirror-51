e332d15c175680d3ea5b7304c4c6309041535815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:04 +0000

    Add package pymq/0.1.0.dev1/pymq-0.1.0.dev1-py3-none-any.whl

---
+++ packages/pymq/pymq-0.1.0.dev1-py3-none-any.whl/eventbus/__init__.py
@@ -0,0 +1,31 @@
+import atexit
+
+from eventbus.core import init, shutdown, publish, add_listener, remove_listener, listener, Queue, Empty, \
+    queue, rpc, expose, remote, RpcRequest, RpcResponse
+
+name = "eventbus"
+
+__all__ = [
+    # server
+    'init',
+    'shutdown',
+    # pubsub
+    'publish',
+    'add_listener',
+    'remove_listener',
+    'listener',
+    # queue
+    'Queue',
+    'Empty',
+    'queue',
+    # rpc
+    'rpc',
+    'expose',
+    'remote',
+    'RpcResponse',
+    'RpcRequest',
+    # provider
+    'provider'
+]
+
+atexit.register(shutdown)
