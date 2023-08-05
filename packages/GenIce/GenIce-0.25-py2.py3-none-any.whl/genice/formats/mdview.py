8800f9245cd4f64228e73fd12c521798c8f013fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:59 +0000

    Add package GenIce/0.25/GenIce-0.25-py2.py3-none-any.whl

---
+++ packages/GenIce/GenIce-0.25-py2.py3-none-any.whl/genice/formats/mdview.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+"""
+MDView file format
+
+Usage:
+    genice III -f mdview > 3.rings.mdv
+    mdview -format- rings.mdv
+"""
+
+import numpy as np
+
+au = 0.052917721092 # nm
+AA = 0.1 # nm
+
+def hook7(lattice):
+    lattice.logger.info("Hook7: Output in MDView format.")
+    lattice.logger.info("  Total number of atoms: {0}".format(len(lattice.atoms)))
+    if __name__[-6:] == 'mdv_au':
+        conv = 1.0 / au
+    else:
+        conv = 10.0
+    s = ""
+    s += "-center 0 0 0\n"
+    s += "-fold\n"
+    s += "{0}\n".format(len(lattice.atoms))
+    for atom in lattice.atoms:
+        molorder, resname, atomname, position, order = atom
+        s += "{0:5} {1:9.4f} {2:9.4f} {3:9.4f}\n".format(atomname,*(position[:3]*conv))
+    s = '#' + "\n#".join(lattice.doc) + "\n" + s
+    print(s,end="")
+    lattice.logger.info("Hook7: end.")
+
+
+hooks = {7:hook7}
---
+++ packages/GenIce/GenIce-0.25-py2.py3-none-any.whl/genice/formats/y.py
@@ -0,0 +1,103 @@
+# coding: utf-8
+"""
+Yaplot format.
+defined in https://github.com/vitroid/Yaplot
+"""
+
+from collections import defaultdict
+import numpy as np
+import yaplotlib as yp
+
+def hook1(lattice):
+    lattice.logger.info("Hook1: Draw the cell in Yaplot format.")
+    s = yp.Layer(2)
+    x,y,z = lattice.repcell.mat
+    for p,q,r in ((x,y,z),(y,z,x),(z,x,y)):
+        for a in (np.zeros(3), p, q, p+q):
+            s += yp.Line(a,a+r)
+    print(s,end="")
+    lattice.logger.info("Hook1: end.")
+
+
+def hook6(lattice):
+    global nwateratoms
+    lattice.logger.info("Hook6: Output water molecules in Yaplot format.")
+    lattice.logger.info("  Total number of atoms: {0}".format(len(lattice.atoms)))
+    # prepare the reverse dict
+    waters = defaultdict(dict)
+    for atom in lattice.atoms:
+        resno, resname, atomname, position, order = atom
+        if "O" in atomname:
+            waters[order]["O"] = position
+        elif "H" in atomname:
+            if "H0" not in waters[order]:
+                waters[order]["H0"] = position
+            else:
+                waters[order]["H1"] = position
+    s = ""
+    s += yp.Color(3)
+    for order, water in waters.items():
+        O = water["O"]        
+        H0 = water["H0"]        
+        H1 = water["H1"]        
+        s += yp.Layer(4)
+        s += yp.Color(3)
+        s += yp.Size(0.03)
+        s += yp.Circle(O)
+        s += yp.Line(O,H0)
+        s += yp.Line(O,H1)
+        s += yp.Size(0.01)
+        s += yp.Circle(H0)
+        s += yp.Circle(H1)
+        s += yp.Color(2)
+        s += yp.Layer(1)
+        s += yp.Text(O, "{0}".format(order))
+    s += yp.Layer(3)
+    s += yp.Color(4)
+    s += yp.ArrowType(1)
+    s += yp.Size(0.03)
+    for i,j in lattice.spacegraph.edges(data=False):
+        if i in waters and j in waters:  # edge may connect to the dopant
+            O = waters[j]["O"]
+            H0 = waters[i]["H0"]
+            H1 = waters[i]["H1"]
+            d0 = H0 - O
+            d1 = H1 - O
+            rr0 = np.dot(d0,d0)
+            rr1 = np.dot(d1,d1)
+            if rr0 < rr1 and rr0 < 0.245**2:
+                s += yp.Arrow(H0,O)
+            if rr1 < rr0 and rr1 < 0.245**2:
+                s += yp.Arrow(H1,O)
+    print(s, end="")
+    nwateratoms = len(lattice.atoms)
+    lattice.logger.info("Hook6: end.")
+
+
+def hook7(lattice):
+    global nwateratoms
+    lattice.logger.info("Hook7: Output water molecules in Yaplot format.")
+    lattice.logger.info("  Total number of atoms: {0}".format(len(lattice.atoms)))
+    gatoms = lattice.atoms[nwateratoms:]
+    palettes = dict()
+    s = ""
+    s += yp.Layer(4)
+    s += yp.ArrowType(1)
+    H = []
+    O  = ""
+    for atom in gatoms:
+        resno, resname, atomname, position, order = atom
+        if atomname in palettes:
+            pal = palettes[atomname]
+        else:
+            pal = 4 + len(palettes)
+            palettes[atomname] = pal
+        s += yp.Color(pal)
+        s += yp.Size(0.04)
+        s += yp.Circle(position)
+    s = '#' + "\n#".join(lattice.doc) + "\n" + s
+    print(s)
+    lattice.logger.info("Hook7: end.")
+    
+
+hooks = {7:hook7, 6:hook6, 1:hook1}
