e2a6690b885cb9dba8c2128a8d69fcd1d3f4f9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package gfycat/0.1.3/gfycat-0.1.3-py3-none-any.whl

---
+++ packages/gfycat/gfycat-0.1.3-py3-none-any.whl/gfycat/constants.py
@@ -0,0 +1,31 @@
+# Gfycat does not yet have a full fledged API with
+# access credentials and advanced access control.
+# These parameters are explicitly mentioned at
+# http://gfycat.com/api
+
+# Fetching from a URL
+FETCH_URL_ENDPOINT = 'http://upload.gfycat.com/transcode'
+FETCH_URL_LAZY_ENDPOINT = 'http://upload.gfycat.com/transcodeRelease/'
+FETCH_URL_STATUS_ENDPOINT = 'https://upload.gfycat.com/status/'
+
+# File Upload
+FILE_UPLOAD_ENDPOINT = 'https://gifaffe.s3.amazonaws.com/'
+FILE_UPLOAD_STATUS_ENDPOINT = 'https://upload.gfycat.com/transcode/'
+ACL = 'private'
+AWS_ACCESS_KEY_ID = 'AKIAIT4VU4B7G2LQYKZQ'
+POLICY = "eyAiZXhwaXJhdGlvbiI6ICIyMDIwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICAgICAgICAgICAiY29uZGl0aW9ucyI6IFsKICAgICAgICAgICAgeyJidWNrZXQiOiAiZ2lmYWZmZSJ9LAogICAgICAgICAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAiIl0sCiAgICAgICAgICAgIHsiYWNsIjogInByaXZhdGUifSwKCSAgICB7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6ICIyMDAifSwKICAgICAgICAgICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLAogICAgICAgICAgICBbImNvbnRlbnQtbGVuZ3RoLXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogICAgICAgICAgICBdCiAgICAgICAgICB9"
+SUCCESS_ACTION_STATUS = 200
+SIGNATURE = 'mk9t/U/wRN4/uU01mXfeTe2Kcoc='
+CONTENT_TYPE = 'image/gif'
+
+# Query
+QUERY_ENDPOINT = 'https://api.gfycat.com/v1/gfycats/'
+
+# Check Link
+CHECK_LINK_ENDPOINT = 'http://gfycat.com/cajax/checkUrl/'
+
+# OAuth
+OAUTH_ENDPOINT = 'https://api.gfycat.com/v1/oauth/token'
+
+# Error
+ERROR_KEY = 'errorMessage'
