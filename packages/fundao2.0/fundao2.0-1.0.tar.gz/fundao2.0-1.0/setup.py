df8bcf28f60fc0b5e6635dbd3081fe60d257674d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:14 +0000

    Add package fundao2.0/1.0/fundao2.0-1.0.tar.gz

---
+++ packages/fundao2.0/fundao2.0-1.0.tar.gz/fundao2.0-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='fundao2.0',
+    version='1.0',
+    author='Ojuara Abaporujucaíba',
+    author_email='ojuara@araujo.com',
+    description='Brincando com distribuição python',
+    py_modules = ['ojuara']
+)
