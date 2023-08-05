47d561d4dc85928d27b3e64ff714a073032796ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package orbis-plugin-scoring-nel-scorer/2.2/orbis_plugin_scoring_nel_scorer-2.2.tar.gz

---
+++ packages/orbis-plugin-scoring-nel-scorer/orbis_plugin_scoring_nel_scorer-2.2.tar.gz/orbis_plugin_scoring_nel_scorer-2.2/orbis_plugin_scoring_nel_scorer/conditions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+conditions = {
+    "simple": [
+        "same_url",
+        "same_type",
+        "same_surfaceForm"
+    ],
+    "strict": [
+        "same_start",
+        "same_end",
+        "same_url",
+        "same_type",
+        "same_surfaceForm"
+    ],
+    "overlap": [
+        "overlap",
+        "same_url",
+        "same_type"
+    ]
+}
