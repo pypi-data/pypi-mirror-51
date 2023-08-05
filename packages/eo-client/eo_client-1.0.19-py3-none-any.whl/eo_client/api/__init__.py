1cb5ad9a356e48eee4ee41a80cd4007ef9467b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:55 +0000

    Add package eo-client/1.0.19/eo_client-1.0.19-py3-none-any.whl

---
+++ packages/eo-client/eo_client-1.0.19-py3-none-any.whl/eo_client/api/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from eo_client.api.atomic_projects_api import AtomicProjectsApi
+from eo_client.api.complex_projects_api import ComplexProjectsApi
+from eo_client.api.organization_teams_api import OrganizationTeamsApi
+from eo_client.api.organizational_roles_api import OrganizationalRolesApi
+from eo_client.api.organizations_api import OrganizationsApi
+from eo_client.api.people_api import PeopleApi
+from eo_client.api.project_teams_api import ProjectTeamsApi
+from eo_client.api.projects_api import ProjectsApi
+from eo_client.api.team_members_api import TeamMembersApi
+from eo_client.api.team_memberships_api import TeamMembershipsApi
+from eo_client.api.team_purposes_api import TeamPurposesApi
+from eo_client.api.teams_api import TeamsApi
