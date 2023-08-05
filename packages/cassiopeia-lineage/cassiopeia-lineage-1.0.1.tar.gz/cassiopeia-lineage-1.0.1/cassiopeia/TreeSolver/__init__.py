a4d86d9aaba16de4fd0751d6e9112d6e9288c7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:31 +0000

    Add package cassiopeia-lineage/1.0.1/cassiopeia-lineage-1.0.1.tar.gz

---
+++ packages/cassiopeia-lineage/cassiopeia-lineage-1.0.1.tar.gz/cassiopeia-lineage-1.0.1/cassiopeia/TreeSolver/__init__.py
@@ -0,0 +1,9 @@
+"""Top level for Tree Solver development."""
+
+from cassiopeia.TreeSolver.Node import Node
+from cassiopeia.TreeSolver.Cassiopeia_Tree import Cassiopeia_Tree
+
+__author__ = "Matt Jones, Alex Khodaverdian"
+__email__ = "mattjones315@berkeley.edu"
+__version__ = '0.0.1'
+
