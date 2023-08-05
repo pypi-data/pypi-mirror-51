b41667e0a1fa42914727e2afee3984a8e5817709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:49 +0000

    Add package fengkaibin/1.0/fengkaibin-1.0.tar.gz

---
+++ packages/fengkaibin/fengkaibin-1.0.tar.gz/fengkaibin-1.0/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 12:39:32 2019
+
+@author: fengkaibin
+
+"""
+
+from distutils.core import setup
+
+setup(name='fengkaibin',
+    version='1.0',
+    description = "fengkaibin",
+    url='https://github.com',
+    author = "myself and I",
+    author_email = "fengkaibin@hotmail.com",
+    license='MIT', 
+    packages=[''],
+    )
+ 
---
+++ packages/fengkaibin/fengkaibin-1.0.tar.gz/fengkaibin-1.0/test_pkg.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 12:36:28 2019
+
+@author: fengkaibin
+
+"""
+
+import os
+
+def test():
+    
+    a = os.getcwd()
+    return a
+
+
+def test2():
+    
+    a = os.getcwd()
+    return a
+
+
+def test3():
+    
+    a = os.getcwd()
+    return a
