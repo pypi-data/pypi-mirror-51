ed51bf58ffc1269ec0830f9f5181aa91d8cf4cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:03 +0000

    Add package keyprotect/1.0.1/keyprotect-1.0.1.tar.gz

---
+++ packages/keyprotect/keyprotect-1.0.1.tar.gz/keyprotect-1.0.1/keyprotect/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from keyprotect.api.keys_api import KeysApi
+from keyprotect.api.policies_api import PoliciesApi
