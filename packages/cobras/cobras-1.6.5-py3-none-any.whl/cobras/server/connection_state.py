53b7023fddd2673ec2ad0a86daeb346d28596418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:41 +0000

    Add package cobras/1.6.5/cobras-1.6.5-py3-none-any.whl

---
+++ packages/cobras/cobras-1.6.5-py3-none-any.whl/cobras/server/connection_state.py
@@ -0,0 +1,47 @@
+'''Small class to record data attached to a connection
+
+FIXME: use this to enforce publish+subscribe permissions.
+
+Copyright (c) 2018-2019 Machine Zone, Inc. All rights reserved.
+'''
+
+import logging
+import uuid
+import json
+
+import websockets
+
+
+class ConnectionState:
+    def __init__(self, appkey):
+        self.appkey = appkey
+
+        self.connection_id = uuid.uuid4().hex[:8]
+        self.subscriptions = {}
+        self.ok = True
+        self.role = 'na'
+        self.authenticated = False
+        self.permissions = []
+
+        self.nonce = None
+        self.error = 'na'
+
+        self.path = f'/tmp/log_{self.connection_id}'
+        self.fileLogging = False
+
+    def log(self, msg):
+        log = f"[{self.connection_id}::{self.role}] {msg}"
+        logging.info(log)
+
+        if self.fileLogging:
+            with open(self.path, 'a') as f:
+                f.write(log + '\n')
+
+    async def respond(self, ws, data):
+        response = json.dumps(data)
+        logging.info(f"> {response}")
+
+        try:
+            await ws.send(response)
+        except websockets.exceptions.ConnectionClosed as e:
+            logging.warning(f'Trying to write in a closed connection: {e}')
