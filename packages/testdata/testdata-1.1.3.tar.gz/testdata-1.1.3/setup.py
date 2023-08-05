58cb5cd0bd6d4c13f4cfa3300288e8d5a61f724c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:35 +0000

    Add package testdata/1.1.3/testdata-1.1.3.tar.gz

---
+++ packages/testdata/testdata-1.1.3.tar.gz/testdata-1.1.3/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# http://docs.python.org/distutils/setupscript.html
+# http://docs.python.org/2/distutils/examples.html
+from setuptools import setup, find_packages
+import re
+import os
+from codecs import open
+
+
+name = 'testdata'
+with open(os.path.join(name, "__init__.py")) as f:
+    version = re.search("^__version__\s*=\s*[\'\"]([^\'\"]+)", f.read(), flags=re.I | re.M).group(1)
+
+long_description = ""
+if os.path.isfile('README.rst'):
+    with open('README.rst', encoding='utf-8') as f:
+        long_description = f.read()
+
+setup(
+    name=name,
+    version=version,
+    description='Easily generate random unicode test data among other things',
+    long_description=long_description,
+    author='Jay Marcyes',
+    author_email='jay@marcyes.com',
+    url='http://github.com/Jaymon/{}'.format(name),
+    #py_modules=[name],
+    packages=find_packages(),
+    package_data={name: ['data/*']},
+    license="MIT",
+    #tests_require=[],
+    classifiers=[ # https://pypi.python.org/pypi?:action=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite = "{}_test".format(name),
+)
+
