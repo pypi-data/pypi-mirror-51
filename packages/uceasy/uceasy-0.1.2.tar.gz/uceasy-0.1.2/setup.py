ec5a098799fabd7ba66e28eafdb22794a6455266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:10 +0000

    Add package uceasy/0.1.2/uceasy-0.1.2.tar.gz

---
+++ packages/uceasy/uceasy-0.1.2.tar.gz/uceasy-0.1.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['uceasy',
+ 'uceasy.adapters',
+ 'uceasy.cli',
+ 'uceasy.controller',
+ 'uceasy.use_cases']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0', 'pandas>=0.25.0,<0.26.0']
+
+entry_points = \
+{'console_scripts': ['uceasy = uceasy.cli.uceasy_cli:uceasy']}
+
+setup_kwargs = {
+    'name': 'uceasy',
+    'version': '0.1.2',
+    'description': 'Wrapper for the Phyluce phylogenomics software package',
+    'long_description': None,
+    'author': 'Caio Raposo',
+    'author_email': 'caioraposo@protonmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
