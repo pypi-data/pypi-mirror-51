382859b0f00f4f107a81b8b7d11221a70acd1c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:51 +0000

    Add package analyzere-extras/0.2.0/analyzere_extras-0.2.0.tar.gz

---
+++ packages/analyzere-extras/analyzere_extras-0.2.0.tar.gz/analyzere_extras-0.2.0/setup.py
@@ -0,0 +1,38 @@
+from codecs import open
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    readme = f.read()
+
+with open(path.join(here, 'requirements', 'install.txt'),
+          encoding='utf-8') as f:
+    install_requires = f.read().splitlines()
+
+setup(
+    name='analyzere_extras',
+    version='0.2.0',
+    description='Python extras to support visualization',
+    long_description=readme,
+    url='https://github.com/analyzere/analyzere-python-extras',
+    author='Analyze Re',
+    author_email='support@analyzere.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[
+        'analyzere_extras',
+    ],
+    install_requires=install_requires
+)
