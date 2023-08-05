254344403fbd41690500d6a63a569733c49f1170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:51 +0000

    Add package yahoo-finance-hdd/0.2.2/yahoo_finance_hdd-0.2.2.tar.gz

---
+++ packages/yahoo-finance-hdd/yahoo_finance_hdd-0.2.2.tar.gz/yahoo_finance_hdd-0.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+  name = 'yahoo_finance_hdd',         
+  packages = ['yahoo_finance_hdd'],   
+  version = '0.2.2',      
+  license='MIT',        
+  description = 'Download historical financial data from yahoo finance',   
+  author = 'Serkan Dizbay',                   
+  url = 'https://github.com/sdiz/yahoo_finance_hdd.git',   
+  download_url = 'https://github.com/sdiz/yahoo_finance_hdd/archive/v_0.2.2.tar.gz',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  keywords = ['financial data', 'yahoo finance'],  
+  install_requires=[            
+          'numpy',
+          'pandas',
+          'requests',
+          'datetime',
+          'pandas_market_calendars',
+      ],
+  classifiers=[
+    'License :: OSI Approved :: MIT License',
+    "Programming Language :: Python :: 3",
+  ],
+)
