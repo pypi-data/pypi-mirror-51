d03da3978848151b98b4996c868f8e24fda7745b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:35 +0000

    Add package SegSRGAN/2.1.3/SegSRGAN-2.1.3-py3-none-any.whl

---
+++ packages/SegSRGAN/SegSRGAN-2.1.3-py3-none-any.whl/SegSRGAN/utils/download.py
@@ -0,0 +1,20 @@
+import os
+import wget
+import stat
+import requests
+
+# Download precalculated weight files used for the machine learning algorithm
+# and available from the repository.
+def download_weights( weights_path ):
+    # We get the content of the weights folder.
+    z = requests.get('https://api.github.com/repos/koopa31/SegSRGAN/contents/data/weights?ref=develop')
+    contents = z.json()
+    # Creation of weights folder if it does not already exist.
+    if os.path.isdir(weights_path) is False:
+        print(weights_path)
+        os.mkdir(weights_path, mode=0o777)
+    # Downloading of the files.
+    for content in contents:
+        if os.path.isfile(os.path.join(weights_path, content['name'])) is False:
+            print("Processing %s" % content['path'])
+            wget.download(content['download_url'], out=weights_path)
