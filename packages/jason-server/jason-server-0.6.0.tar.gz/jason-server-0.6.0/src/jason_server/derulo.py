07cc968898c8cc2313b1e3067d3df9b6ae371374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:06 +0000

    Add package jason-server/0.6.0/jason-server-0.6.0.tar.gz

---
+++ packages/jason-server/jason-server-0.6.0.tar.gz/jason-server-0.6.0/src/jason_server/derulo.py
@@ -0,0 +1,180 @@
+from bottle import Bottle, template, request, response
+from jason_server.database import Database
+from jason_server.utils import chunk_list
+
+# --------------------------------------------------------------------------- #
+
+INDEX_TEMPLATE = """
+<html>
+    <head>
+        <title>Jason Server</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+            <h1>Jason Server</h1>
+            </div>
+        </div>
+        <div class="container">
+            <h2>Resources</h2>
+            <ul>
+                % for table in tables:
+                <li>
+                    <a href="{{base_url}}/{{table}}">{{table}}</a>
+                </li>
+                % end
+            </ul>
+            <h2>HTTP Method supported:</h2>
+            <p><span class="badge badge-secondary">GET</span></p>
+        </div>
+    </body>
+</html>
+"""
+
+# --------------------------------------------------------------------------- #
+
+app = Bottle()
+db = None
+# --------------------------------------------------------------------------- #
+
+def verify_query_sort(query):
+    sort, order = (None, "asc")
+    if query and '_sort' in query:
+        sort = query['_sort']
+        if '_order' in query:
+            order = query['_order']
+    return sort, order
+
+def verify_query_paginate(query):
+    page, limit = (None, 10)
+    if query and '_page' in query:
+        page = int(query['_page'])
+        if '_limit' in query:
+            limit = int(query['_limit'])
+            limit = limit if limit < 10 else 10
+
+    return page, limit
+
+
+def build_link_header(request, page, total):
+
+    REL_FIRST = '<{url}?_page={first}>; rel="first"'
+    REL_PREV = '<{url}?_page={prev}>; rel="prev"'
+    REL_NEXT = '<{url}?_page={next}>; rel="next"'
+    REL_LAST = '<{url}?_page={last}>; rel="last"'
+
+    scheme, netloc, path = request.urlparts[0:3]
+    url = "{scheme}://{netloc}{path}".format(scheme=scheme, netloc=netloc, path=path)
+    links = None
+
+    if total == 1:
+        return ""
+
+    if page == 1:
+        links = [
+            REL_FIRST.format(url=url, first=1),
+            REL_NEXT.format(url=url, next=2),
+            REL_LAST.format(url=url, last=total)
+        ]
+    elif page == total:
+        links = [
+            REL_FIRST.format(url=url, first=1),
+            REL_PREV.format(url=url, prev=total-1),
+            REL_LAST.format(url=url, last=total)
+        ]
+        return ",".join(links)
+    else:
+        links = [
+            REL_FIRST.format(url=url, first=1),
+            REL_PREV.format(url=url, prev=page-1),
+            REL_NEXT.format(url=url, next=page+1),
+            REL_LAST.format(url=url, last=total)
+        ]
+
+    return ",".join(links)
+
+
+# --------------------------------------------------------------------------- #
+
+@app.hook('after_request')
+def set_default_headers():
+    response.set_header("X-Powerded-By", "Bottle")
+
+@app.route('/')
+def bottle_world():
+
+    def build_url(host, port):
+        return "http://{}:{}".format(host, port)
+
+    host = app.config.get('host')
+    port = app.config.get('port')
+    resources = {
+        "base_url": build_url(host, port),
+        "tables": db.endpoints
+    }
+    return template(INDEX_TEMPLATE, resources)
+
+
+@app.route('/db')
+def db():
+    return db.json
+
+
+@app.route('/<endpoint>', method='GET')
+def get(endpoint):
+    table = db.resource(endpoint)
+    elements = table.all()
+
+    if not request.query:
+        return dict(data=elements)
+
+    results = elements
+
+    sort, order = verify_query_sort(request.query)
+    if sort:
+        if order == 'asc':
+            results = sorted(results, key = lambda i: i[sort])
+        else:
+            results = sorted(results, key = lambda i: i[sort], reverse=True)
+
+    page, limit = verify_query_paginate(request.query)
+    if page:
+        chunk_data = list(chunk_list(results, limit))
+        results = chunk_data[page-1]
+        link_header = build_link_header(request, page, len(chunk_data))
+        response.set_header("Link", link_header)
+
+    return dict(data=results)
+
+# --------------------------------------------------------------------------- #
+
+
+def print_message(database, tables, host, port):
+    BOLD_BLUE = '\033[1;34m'
+    RESET = '\033[0m'
+
+    print("\n    {}Database{}".format(BOLD_BLUE, RESET))
+    print("    {}".format(database))
+
+    print("\n    {}Resources{}".format(BOLD_BLUE, RESET))
+    base_url = "http://{}:{}".format(host, port)
+    for table in tables:
+        print("    {}/{}".format(base_url, table))
+
+    print("\n    {}Home{}".format(BOLD_BLUE, RESET))
+    print("    {}/".format(base_url))
+
+
+def run(options, database):
+    host = options['host']
+    port = options['port']
+    quiet = options['quiet']
+
+    app.config.setdefault('host', host)
+    app.config.setdefault('port', port)
+    global db
+    db = Database(database)
+    if not quiet:
+        print_message(database, db.endpoints, host, port)
+    app.run(host=host, port=port, quiet=True)
---
+++ packages/jason-server/jason-server-0.6.0.tar.gz/jason-server-0.6.0/tests/integration/test_app.py
@@ -0,0 +1,116 @@
+import pytest
+
+import requests
+import time
+
+
+def describe_endpoint():
+
+    @pytest.fixture()
+    def s():
+        # Wait for jason_server
+        time.sleep(2)
+
+        s = requests.Session()
+        yield s
+
+    def get(s):
+        home_url = "http://127.0.0.1:8100"
+        persons_url = "http://127.0.0.1:8100/persons"
+
+        r = s.get(home_url)
+        assert 200 == r.status_code
+
+        r = s.get(persons_url)
+        assert 200 == r.status_code
+        assert 'application/json' == r.headers['content-type']
+        data = r.json()
+        assert 13 == len(data['data'])
+
+    def get_with_paginate(s):
+        persons_url_page1 = "http://127.0.0.1:8100/persons?_page=1"
+        persons_url_page2 = "http://127.0.0.1:8100/persons?_page=2"
+
+        r = s.get(persons_url_page1)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 10 == len(data['data'])
+
+        r = s.get(persons_url_page2)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 3 == len(data['data'])
+
+    def get_with_paginate_and_limit(s):
+        persons_url_page1_limit3 = "http://127.0.0.1:8100/persons?_page=1&_limit=3"
+        persons_url_page2_limit3 = "http://127.0.0.1:8100/persons?_page=2&_limit=3"
+        persons_url_page3_limit3 = "http://127.0.0.1:8100/persons?_page=3&_limit=3"
+        persons_url_page4_limit3 = "http://127.0.0.1:8100/persons?_page=4&_limit=3"
+        persons_url_page5_limit3 = "http://127.0.0.1:8100/persons?_page=5&_limit=3"
+
+        r = s.get(persons_url_page1_limit3)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 3 == len(data['data'])
+
+        r = s.get(persons_url_page2_limit3)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 3 == len(data['data'])
+
+        r = s.get(persons_url_page3_limit3)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 3 == len(data['data'])
+
+        r = s.get(persons_url_page4_limit3)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 3 == len(data['data'])
+
+        r = s.get(persons_url_page5_limit3)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 1 == len(data['data'])
+
+    def get_with_sorting(s):
+        persons_url_sort_by_age = "http://127.0.0.1:8100/persons?_sort=age"
+        persons_url_sort_by_name = "http://127.0.0.1:8100/persons?_sort=name"
+
+        r = s.get(persons_url_sort_by_age)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 21 ==data['data'][0]['age']
+        assert "Bell Hinton" == data['data'][0]['name']
+
+        r = s.get(persons_url_sort_by_name)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 30 == data['data'][0]['age']
+        assert "Anastasia Coffey" == data['data'][0]['name']
+
+    def get_with_sorting_and_pagination(s):
+        persons_url_sort_age_paginate = "http://127.0.0.1:8100/persons?_sort=age&_page=1"
+        persons_url_sort_name_paginate = "http://127.0.0.1:8100/persons?_sort=name&_page=2"
+        persons_url_sort_desc_age_paginate = "http://127.0.0.1:8100/persons?_sort=age&_order=desc&_page=1"
+        persons_url_sort_name_paginate_with_limit = "http://127.0.0.1:8100/persons?_sort=name&_page=2&_limit=4"
+
+        r = s.get(persons_url_sort_age_paginate)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 23 ==data['data'][1]['age']
+
+        r = s.get(persons_url_sort_name_paginate)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 36 ==data['data'][1]['age']
+
+        r = s.get(persons_url_sort_desc_age_paginate)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 37 ==data['data'][1]['age']
+
+        r = s.get(persons_url_sort_name_paginate_with_limit)
+        assert 200 == r.status_code
+        data = r.json()
+        assert 23 ==data['data'][0]['age']
