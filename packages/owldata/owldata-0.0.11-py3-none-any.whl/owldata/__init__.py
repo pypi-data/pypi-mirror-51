927138f943c4200ea9e5919618441f0c3e80e52d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package owldata/0.0.11/owldata-0.0.11-py3-none-any.whl

---
+++ packages/owldata/owldata-0.0.11-py3-none-any.whl/owldata/__init__.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# =====================================================================
+# Copyright (C) 2018-2019 by Owl Data
+# author: Danny, Destiny
+
+# =====================================================================
+
+from .api import OwlData
+from .__version__ import __version__
+
+__docformat__ = 'restructuredtext'
+__doc__ = '''
+owldata: 協助大家研究分析得好夥伴
+==============================
+
+**owldata** 提供一個穩定且快速的介面獲取數據貓頭鷹任何資料
+
+'''
