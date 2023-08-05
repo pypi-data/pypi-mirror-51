f6f5fa6c0cb32308272004d95447b5f3a76a2e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package qcloud-python-sts/3.0.2/qcloud-python-sts-3.0.2.tar.gz

---
+++ packages/qcloud-python-sts/qcloud-python-sts-3.0.2.tar.gz/qcloud-python-sts-3.0.2/setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from setuptools import setup, find_packages
+setup(
+    name='qcloud-python-sts',
+    version='3.0.2',
+    description='this is sts for python on v3',
+    url='https://github.com/tencentyun/qcloud-cos-sts-sdk',
+    author='qcloudterminal',
+    author_email='qcloudterminal@gmail.com',
+    license='MIT',
+    packages=['sts'],
+    install_requires=['requests']
+)
