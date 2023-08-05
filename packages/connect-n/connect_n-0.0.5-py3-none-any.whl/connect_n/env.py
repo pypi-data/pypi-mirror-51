ee9513fd2224aea9e1c1c641e3c05a02e8169362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:37 +0000

    Add package connect-n/0.0.5/connect_n-0.0.5-py3-none-any.whl

---
+++ packages/connect-n/connect_n-0.0.5-py3-none-any.whl/connect_n/env.py
@@ -0,0 +1,13 @@
+"""Default and environment variables for this project."""
+
+N = 4
+ROWS = 6
+COLUMNS = 7
+
+OFFSET = -2 * (10 ** 3)
+UNIT_SCORE = 10 ** 3
+
+LARGE_VALUE = 10 ** 12
+SMALL_VALUE = -10 ** 12
+
+NEURAL_NET_param = {"name": "Neural Network parameters"}
