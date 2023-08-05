385a50c14ee01035504a551b26de1ed6a774e441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package ftw.colorbox/1.4.0/ftw.colorbox-1.4.0.tar.gz

---
+++ packages/ftw.colorbox/ftw.colorbox-1.4.0.tar.gz/ftw.colorbox-1.4.0/ftw/colorbox/browser/initalize_colorbox.py
@@ -0,0 +1,36 @@
+from ftw.colorbox.interfaces import IColorboxSettings
+from Products.Five.browser import BrowserView
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+
+class InitColorBoxView(BrowserView):
+
+    def __call__(self):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IColorboxSettings)
+        exclude_expression = settings.colorbox_exclude
+        return """
+            var ftwColorboxInitialize = function() {
+                var staticOptions = {
+                    'title': function(){
+                        var link = $(this);
+                        var title = link.attr('title');
+
+                        // Use the value from the attrib "data-caption" if there is one.
+                        var caption = link.data('caption');
+                        if (caption) {
+                            title = caption;
+                        }
+
+                        return title;
+                    }
+                }
+                var options = {%s}  // The options stored in the Plone registry.
+                $.extend(options, staticOptions)
+                $('a.colorboxLink').not(function(){%s}).colorbox(options);
+            }
+            jQuery(function($) {
+                ftwColorboxInitialize();
+            });
+        """ % (','.join(settings.colorbox_config), exclude_expression)
