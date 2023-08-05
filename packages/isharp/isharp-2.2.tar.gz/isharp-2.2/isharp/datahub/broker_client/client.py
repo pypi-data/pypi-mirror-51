fccbc0c1612359d44767b7eef2f3d5fb0830f03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:16 +0000

    Add package isharp/2.2/isharp-2.2.tar.gz

---
+++ packages/isharp/isharp-2.2.tar.gz/isharp-2.2/isharp/datahub/broker_client/client.py
@@ -0,0 +1,32 @@
+
+from isharp.datahub.broker_client.remote_proxy import BrokerConnectionPool
+from  isharp.datahub.broker_client.client_utils import mtx_headers_as_dataframe as to_df
+import logging
+logging.basicConfig()
+logger = logging.getLogger()
+
+
+# create console handler and set level to info
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+logger = logging.getLogger(__name__)
+
+
+logger.info("hello from logging")
+
+
+with BrokerConnectionPool() as broker:
+    mtx = broker.checkout("file://localhost:5672/file_name_1.csv?format=CSV")
+    print(mtx.content)
+    broker.release(mtx)
+    mtx = broker.checkout("file://localhost:5672/file_name_1.csv?format=CSV")
+    broker.release(mtx)
+    print (to_df(broker.list("localhost:5672")))
+
+print("end")
+
+
