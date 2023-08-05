9c94ac2894eeb37b8accfe79d3754f62c3e5d804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:43 +0000

    Add package pyspark-stubs/2.4.0.post3/pyspark-stubs-2.4.0.post3.tar.gz

---
+++ packages/pyspark-stubs/pyspark-stubs-2.4.0.post3.tar.gz/pyspark-stubs-2.4.0.post3/third_party/3/pyspark/sql/_typing.pyi
@@ -0,0 +1,25 @@
+from typing import Any, List, Optional, TypeVar, Union
+from typing_extensions import Protocol
+import datetime
+import decimal
+
+import pyspark.sql.column
+import pyspark.sql.types
+
+ColumnOrName = Union[pyspark.sql.column.Column, str]
+DecimalLiteral = decimal.Decimal
+DateTimeLiteral = Union[datetime.datetime, datetime.date]
+LiteralType = Union[bool, int, float, str]
+DataTypeOrString = Union[pyspark.sql.types.DataType, str]
+
+class SupportsOpen(Protocol):
+    def open(self, partition_id: int, epoch_id: int) -> bool:
+        ...
+
+class SupportsProcess(Protocol):
+    def process(self, row: pyspark.sql.types.Row) -> None:
+        ...
+
+class SupportsClose(Protocol):
+    def close(self, error: Exception) -> None:
+        ...
