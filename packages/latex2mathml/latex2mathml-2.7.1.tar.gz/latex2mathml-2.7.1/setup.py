eed58e6bc0db215addb4816f8f821275b8fbb192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:22 +0000

    Add package latex2mathml/2.7.1/latex2mathml-2.7.1.tar.gz

---
+++ packages/latex2mathml/latex2mathml-2.7.1.tar.gz/latex2mathml-2.7.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+VERSION = '2.7.1'
+
+setup(
+    name='latex2mathml',
+    version=VERSION,
+    packages=['latex2mathml'],
+    url='https://github.com/roniemartinez/latex2mathml',
+    download_url='https://github.com/roniemartinez/latex2mathml/tarball/{}'.format(VERSION),
+    license='MIT',
+    author='Ronie Martinez',
+    author_email='ronmarti18@gmail.com',
+    description='Pure Python library for LaTeX to MathML conversion',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=[],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 'Topic :: Text Processing :: Markup :: HTML',
+                 'Topic :: Text Processing :: Markup :: LaTeX'],
+    package_data={'latex2mathml': ['unimathsymbols.txt']}
+)
