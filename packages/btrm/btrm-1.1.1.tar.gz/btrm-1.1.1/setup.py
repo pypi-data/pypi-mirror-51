059868d372c13a775bbe7d20199b8023b58fc953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:27 +0000

    Add package btrm/1.1.1/btrm-1.1.1.tar.gz

---
+++ packages/btrm/btrm-1.1.1.tar.gz/btrm-1.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='btrm',
+    version='1.1.1',
+    author='Linh Nguyen',
+    author_email='linh1612340@gmail.com',
+    url='https://github.com/nobabykill/btrm',
+    description='Alternative tool for rm command in linux using python',
+    platforms=['window', 'linux', 'macos'],
+    keywords=['btrm', 'backup than remove'],
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'argparse',
+    ],
+    entry_points={
+            'console_scripts': [
+                'btrm = sources.btrm:main'
+            ]},
+    include_package_data=True,
+)
