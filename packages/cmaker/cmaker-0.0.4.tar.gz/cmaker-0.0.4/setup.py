3b7452540d7e20565bcd943db69f953279369d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:47 +0000

    Add package cmaker/0.0.4/cmaker-0.0.4.tar.gz

---
+++ packages/cmaker/cmaker-0.0.4.tar.gz/cmaker-0.0.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="cmaker",
+    version="0.0.4",
+    description="Recursive make-operation for sprawling C++ projects.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ychnlgy/Cmaker",
+    author="Yuchen Li",
+    author_email="ychnlgy@gmail.com",
+    license="MIT",
+    scripts=["makerc.py"],
+    packages=setuptools.find_packages(exclude=["tests"]),
+    include_package_data=True
+)
