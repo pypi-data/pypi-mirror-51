01c6068ff4e9da55154607d2bfbd23d12c7d9b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:53 +0000

    Add package CMT/0.3.0.dev2/CMT-0.3.0.dev2-py37-none-any.whl

---
+++ packages/CMT/CMT-0.3.0.dev2-py37-none-any.whl/cmt/converter/ecmap_v2.py
@@ -0,0 +1,23 @@
+from cmt.a_converter import AConverter
+from cmt.ecmap.v1 import *
+from cmt.ecmap.v2 import *
+
+
+class Converter(AConverter):
+    @staticmethod
+    def convert(source: ECMap_2) -> 'CMap_2':
+        raise ValueError(
+            f"Converting {source.identifier.name} {source.format_version} to CMap {source.format_version} is not supported.")
+
+    @staticmethod
+    def downgrade(source: ECMap_2) -> ECMap_1:
+        res = ECMap_1()
+        res.cmap = source.cmap
+        return res
+
+    @staticmethod
+    def upgrade(source: ECMap_2) -> 'ECMap_3':
+        raise ValueError(
+            f"Upgrading {source.identifier.name} {source.format_version} to"
+            f" {source.identifier.name} {source.format_version + 1} is not supported."
+        )
