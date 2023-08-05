4ab19cb540bbb500e9bfc6254f93daea39b4d6eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package rabbitmqX/1.2.0/rabbitmqX-1.2.0-py3-none-any.whl

---
+++ packages/rabbitmqX/rabbitmqX-1.2.0-py3-none-any.whl/rabbitmqX/patterns/server/topic_server.py
@@ -0,0 +1,45 @@
+class Topic_Server():
+
+     def __init__(self, binding_keys, integration_service):
+        
+        self.url = os.environ.get('CLOUDAMQP_URL', 'amqp://tvmjkfee:0BCkrC2idZZJcrCSCXDsoVpd1_VWisUh@emu.rmq.cloudamqp.com/tvmjkfee')
+        self.params = pika.URLParameters(self.url)
+        self.connection = pika.BlockingConnection(self.params)
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange='topic_seon', exchange_type='topic')
+        self.result = self.channel.queue_declare('', exclusive=True)
+        self.queue_name = self.result.method.queue
+        self.integration_service = integration_service
+        self.binding_keys = binding_keys
+        
+        for binding_key in binding_keys:
+            channel.queue_bind(exchange='topic_seon', queue=queue_name, routing_key=binding_key)
+                
+    def callback(self,ch, method, properties, body):
+    
+        data = json.loads(body)
+
+        response = self.integration_service.do(data)
+    
+        
+    def start(self):
+
+        channel.basic_consume(
+            queue=queue_name, on_message_callback=self.callback, auto_ack=True)
+
+        channel.start_consuming()
+
+        
+
+        
+
+        
+
+        
+
+
+
+
+
+
+
