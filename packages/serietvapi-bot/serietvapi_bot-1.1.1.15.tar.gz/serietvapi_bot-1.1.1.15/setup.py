03586c17620605b66fca8e6c71d6221c03c52c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:29 +0000

    Add package serietvapi-bot/1.1.1.15/serietvapi_bot-1.1.1.15.tar.gz

---
+++ packages/serietvapi-bot/serietvapi_bot-1.1.1.15.tar.gz/serietvapi_bot-1.1.1.15/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="serietvapi_bot",
+    version="1.1.1.15",
+    author="KingKaitoKid",
+    author_email="tvditaly@altervista.org",
+    description="Un semplice tool Python che si integra con le API del Bot Telegram @SerieTvItaly_bot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KingKaitoKid/serietvapi_bot",
+    packages=setuptools.find_packages(),
+    install_requires=["youtube-dl", "telethon", "requests"],
+    entry_points={
+    'console_scripts': [
+        'serietvapi_bot=serietvapi_bot.API_main:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
