4bbf4de76ce0052329ebb1b2e62d8c7dfb8aaa17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:43:35 +0000

    Add package titus-optimize/0.5.1/titus_optimize-0.5.1.tar.gz

---
+++ packages/titus-optimize/titus_optimize-0.5.1.tar.gz/titus_optimize-0.5.1/setup.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+VERSION = "0.5.1"
+
+def readme():
+    """print long description"""
+    with open('README.rst') as f:
+        return f.read()
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(tag, VERSION)
+            sys.exit(info)
+
+setup(
+    name='titus_optimize',
+    author='titus',
+    author_email='titus-ops@netflix.com',
+    description="A package for optimizing placements of containers.",
+    long_description=readme(),
+    keywords='titus',
+    url='https://github.com/Netflix-Skunkworks/titus-optimize',
+    version=VERSION,
+    setup_requires=['setupmeta'],
+    python_requires='>=3.5',
+    install_requires=[],
+    extras_require={
+        'test': ['tox'],
+    },
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
+)
