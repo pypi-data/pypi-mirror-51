9b268c508ddf8642361f03fccb505aebf5672f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:06 +0000

    Add package messente-api/1.1.0/messente-api-1.1.0.tar.gz

---
+++ packages/messente-api/messente-api-1.1.0.tar.gz/messente-api-1.1.0/messente_api/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from messente_api.api.blacklist_api import BlacklistApi
+from messente_api.api.contacts_api import ContactsApi
+from messente_api.api.delivery_report_api import DeliveryReportApi
+from messente_api.api.groups_api import GroupsApi
+from messente_api.api.omnimessage_api import OmnimessageApi
