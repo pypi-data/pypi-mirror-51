7f0ecbeec9db3bdb28726d9eec11977882119549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:07 +0000

    Add package avapi/0.1.5/avapi-0.1.5.tar.gz

---
+++ packages/avapi/avapi-0.1.5.tar.gz/avapi-0.1.5/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def license():
+    with open('LICENSE') as f:
+        return f.read()
+
+
+setuptools.setup(name='avapi',
+                 version='0.1.5',
+                 description='Get data from Alpha Vantage into python.',
+                 long_description=readme(),
+                 long_description_content_type="text/markdown",
+                 classifiers=[
+                        'Development Status :: 3 - Alpha',
+                        'License :: OSI Approved :: MIT License',
+                        'Programming Language :: Python :: 3',
+                        'Topic :: Office/Business :: Financial :: Investment',
+                        ],
+                 url='http://github.com/olemola/vantage',
+                 author='Ole Olaussen',
+                 author_email='ole@ekkobit.com',
+                 license='MIT',
+                 license_file=license(),
+                 packages=setuptools.find_packages(),
+                 test_suite='nose.collector',
+                 tests_require=['nose'],
+                 install_requires=[
+                        'pandas', 'requests',
+                        ],
+                 include_package_data=True,
+                 )
