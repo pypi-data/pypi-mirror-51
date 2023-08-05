2cb17637c08f18cfbf4c1058e4745750621bf4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:49 +0000

    Add package monitoring2/1.1.1/monitoring2-1.1.1.tar.gz

---
+++ packages/monitoring2/monitoring2-1.1.1.tar.gz/monitoring2-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='monitoring2',
+    version='1.1.1',
+    packages=find_packages(),
+    author='Dmitry Dimonoff',
+    author_email='mr.dimonoff@gmail.com',
+    url='https://pypi.org/manage/project/monitoring2/release/',
+    )
