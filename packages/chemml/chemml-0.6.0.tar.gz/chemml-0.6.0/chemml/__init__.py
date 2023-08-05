8d92dee8f2404a844c8018446df098f4538328ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:52 +0000

    Add package chemml/0.6.0/chemml-0.6.0.tar.gz

---
+++ packages/chemml/chemml-0.6.0.tar.gz/chemml-0.6.0/chemml/__init__.py
@@ -0,0 +1,7 @@
+# __name__ = "chemml"
+__version__ = "0.6.0"
+__author__ = ["Mojtaba Haghighatlari (mojtabah@buffalo.edu)", "Johannes Hachmann (hachmann@buffalo.edu)"]
+
+
+# import sys
+# sys.dont_write_bytecode = True
