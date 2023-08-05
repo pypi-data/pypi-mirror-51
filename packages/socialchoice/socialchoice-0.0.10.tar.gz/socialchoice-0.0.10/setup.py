24dff6e377949f5162e1d64ccd16aa38f6c8dfdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:20:08 +0000

    Add package socialchoice/0.0.10/socialchoice-0.0.10.tar.gz

---
+++ packages/socialchoice/socialchoice-0.0.10.tar.gz/socialchoice-0.0.10/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+try:
+    with open("../README.md") as readme_fd:
+        readme = readme_fd.read()
+except FileNotFoundError:
+    with open("README.md") as readme_fd:
+        readme = readme_fd.read()
+
+setup(
+    name="socialchoice",
+    version="0.0.10",
+    url="https://github.com/julian-zucker/socialchoice",
+    license="Apache 2.0",
+    packages=find_packages(),
+    author="Julian Zucker",
+    author_email="julian.zucker@gmail.com",
+    description="Social Choice Theory in Python",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=["networkx", "pytest", "scipy"],
+)
