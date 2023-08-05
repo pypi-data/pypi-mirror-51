def124cf2b2120667eb846d5eb24d7645a8ae05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:21 +0000

    Add package pyexlatex/0.4.1/pyexlatex-0.4.1.tar.gz

---
+++ packages/pyexlatex/pyexlatex-0.4.1.tar.gz/pyexlatex-0.4.1/pyexlatex/models/package.py
@@ -0,0 +1,27 @@
+from pyexlatex.texgen import _usepackage_str
+
+
+class Package:
+    r"""
+    Represents LaTeX \usepackage{}, pass to Document if any custom LaTeX packages are needed.
+    """
+
+    def __init__(self, name, modifier_str=None):
+        self.name = name
+        self.modifier_str = modifier_str
+
+    def __repr__(self):
+        return f'<Package(name={self.name})>'
+
+    def __str__(self):
+        return _usepackage_str(self.name, self.modifier_str)
+
+    def __eq__(self, other):
+        return (self.name == other.name) and (self.modifier_str == other.modifier_str)
+
+    def __hash__(self):
+        return hash((self.name, self.modifier_str))
+
+    def matches_name(self, other):
+        return self.name == other
+
