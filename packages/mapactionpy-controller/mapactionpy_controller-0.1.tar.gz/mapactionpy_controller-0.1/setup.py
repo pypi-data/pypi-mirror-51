d529601f7500503d1a56bae6b51c111ffaabd3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:15 +0000

    Add package mapactionpy-controller/0.1/mapactionpy_controller-0.1.tar.gz

---
+++ packages/mapactionpy-controller/mapactionpy_controller-0.1.tar.gz/mapactionpy_controller-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='mapactionpy_controller',
+      version='0.1',
+      description='Controls the workflow of map and infographic production',
+      url='http://github.com/mapaction/mapactionpy_controller',
+      author='MapAction',
+      author_email='github@mapaction.com',
+      license='GPL3',
+      packages=['mapactionpy_controller'],
+      test_suite='unittest',
+      tests_require=['unittest'],
+      zip_safe=False)
