e0e5bb4459fd8c6931bfb43713d4a2c66c2f723b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:57 +0000

    Add package chess-annotator/1.1.4/chess-annotator-1.1.4.tar.gz

---
+++ packages/chess-annotator/chess-annotator-1.1.4.tar.gz/chess-annotator-1.1.4/setup.py
@@ -0,0 +1,112 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='chess-annotator',
+
+    description='adds comments and analysis to chess games',
+    long_description=long_description,
+
+    version='1.1.4',
+
+    # The project's main homepage.
+    url='https://github.com/rpdelaney/python-chess-annotator',
+
+    # Author details
+    author='Ryan Delaney',
+    author_email='ryan.delaney@gmail.com',
+
+    # Choose your license
+    license='GPL3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        # Software topics
+        'Topic :: Games/Entertainment :: Board Games',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: End Users/Desktop',
+
+        # What kind of environment the application is intended for
+        'Environment :: Console',
+
+        # Supported operating systems
+        'Operating System :: OS Independent',
+
+        # Supported languages
+        'Natural Language :: English',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: '
+        'GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    # What does your project relate to?
+    keywords='chess',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['python-chess==0.27.3'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        # 'dev': ['black'],
+        'test': ['pytest', 'pytest-cov'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # package_data={
+    #     'eco': ['annotator/eco/eco.json'],
+    # },
+    include_package_data=True,
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'annotator=annotator.__main__:main',
+        ],
+    },
+)
