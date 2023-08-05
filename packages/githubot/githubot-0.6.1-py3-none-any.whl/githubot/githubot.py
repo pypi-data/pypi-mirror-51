6fe2f7cba0f5d845d69b7840cc2623e373ef9d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:49 +0000

    Add package githubot/0.6.1/githubot-0.6.1-py3-none-any.whl

---
+++ packages/githubot/githubot-0.6.1-py3-none-any.whl/githubot/githubot.py
@@ -0,0 +1,38 @@
+'''
+Command line tool to use Github's API for automation.
+
+Usage: githubot [--version] <command> [<args>...]
+
+options:
+    -h --help   Show this message and exit.
+    -v --version   Show version.
+
+Subcommand:
+
+release     Releases management.
+file        Files management.
+'''
+
+import runpy
+
+from docopt import docopt
+
+from githubot import __version__
+from githubot import release
+
+
+def main():
+    args = docopt(__doc__, version=__version__, options_first=True)
+
+    if args['<command>'] == 'release':
+        from githubot import release
+        release.main()
+    elif args['<command>'] == 'file':
+        from githubot import file
+        file.main()
+    else:
+        pass
+
+
+if __name__ == '__main__':
+    main()
