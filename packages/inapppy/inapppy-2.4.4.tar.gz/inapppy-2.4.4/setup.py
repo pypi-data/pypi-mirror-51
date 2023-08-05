6b89287a2c0d858c5fc30d2197bb0d88070c6a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:18 +0000

    Add package inapppy/2.4.4/inapppy-2.4.4.tar.gz

---
+++ packages/inapppy/inapppy-2.4.4.tar.gz/inapppy-2.4.4/setup.py
@@ -0,0 +1,22 @@
+# encoding: UTF-8
+from setuptools import setup
+
+
+def get_description():
+    with open("README.rst") as info:
+        return info.read()
+
+
+setup(
+    name="inapppy",
+    version="2.4.4",
+    packages=["inapppy", "inapppy.asyncio"],
+    install_requires=["aiohttp", "rsa", "requests", "google-api-python-client", "oauth2client==3.0.0"],
+    description="In-app purchase validation library for Apple AppStore and GooglePlay.",
+    keywords="in-app store purchase googleplay appstore validation",
+    author="Lukas Šalkauskas",
+    author_email="halfas.online@gmail.com",
+    url="https://github.com/dotpot/InAppPy",
+    long_description=get_description(),
+    license="MIT",
+)
