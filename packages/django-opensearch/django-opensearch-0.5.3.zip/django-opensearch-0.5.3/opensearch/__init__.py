d3e3cb48a5afd99780cc1a6f147a00c2f118c13d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:23 +0000

    Add package django-opensearch/0.5.3/django-opensearch-0.5.3.zip

---
+++ packages/django-opensearch/django-opensearch-0.5.3.zip/django-opensearch-0.5.3/opensearch/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# django-opensearch
+# opensearch/__init__.py
+
+
+__all__ = ["default_app_config"]  # type: list
+
+
+default_app_config = "opensearch.apps.DjangoOpensearchConfig"  # type: str
