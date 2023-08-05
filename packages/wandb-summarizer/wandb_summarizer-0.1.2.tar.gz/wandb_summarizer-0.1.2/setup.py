f283b4ed002e669294f1d272fc89f87d86463509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:31 +0000

    Add package wandb-summarizer/0.1.2/wandb_summarizer-0.1.2.tar.gz

---
+++ packages/wandb-summarizer/wandb_summarizer-0.1.2.tar.gz/wandb_summarizer-0.1.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+setuptools.setup(
+    name='wandb_summarizer',
+    version='0.1.2',
+    description='A tiny CSV export micro-library for Weights and Biases.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/sampottinger/wandb_summarizer',
+    author='A. Samuel Pottinger',
+    license='MIT',
+    packages=['wandb_summarizer'],
+    install_requires=[
+      'wandb',
+    ],
+    entry_points = {
+        'console_scripts': ['wandb-summarizer-to-csv=wandb_summarizer.export:main'],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Legal Industry',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Text Processing'
+    ],
+    download_url='https://github.com/sampottinger/wandb_summarizer/archive/master.zip'
+)
