c3c06c49e012487f1487ea343f85b111add35220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:17 +0000

    Add package rqopen-client/0.0.6/rqopen-client-0.0.6.tar.gz

---
+++ packages/rqopen-client/rqopen-client-0.0.6.tar.gz/rqopen-client-0.0.6/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+try:  # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+setup(
+    name='rqopen-client',
+    version='0.0.6',
+    description='rqopen-client',
+    packages=find_packages(exclude=[]),
+    author='ricequant',
+    author_email='public@ricequant.com',
+    url="https://github.com/ricequant/rqopen-client",
+    package_data={'': ['*.*']},
+    install_requires=[
+        str(ir.req)
+        for ir in parse_requirements("requirements.txt", session=False)
+    ],
+    zip_safe=False,
+)
