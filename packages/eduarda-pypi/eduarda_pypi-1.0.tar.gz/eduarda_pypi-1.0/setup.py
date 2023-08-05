61f56b80b1627a592dcf1bb05fb94455bbb63e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:39 +0000

    Add package eduarda-pypi/1.0/eduarda_pypi-1.0.tar.gz

---
+++ packages/eduarda-pypi/eduarda_pypi-1.0.tar.gz/eduarda_pypi-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='eduarda_pypi',
+	version='1.0',
+    author='eduarda Abaporojucaíba',
+    author_email='eduardacunha510@gmail.com',
+    description='Brincando com distribuição python',
+    py_modules = ['eduarda']
+)
