5650040958628d6d7b83b4c7c75ff65717159345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:44 +0000

    Add package ckipneutools/0.0.10/ckipneutools-0.0.10.tar.gz

---
+++ packages/ckipneutools/ckipneutools-0.0.10.tar.gz/ckipneutools-0.0.10/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name = "ckipneutools",
+    version = "0.0.10",
+    author = "Peng-Hsuan Li",
+    author_email = "jacobvsdanniel@gmail.com",
+    description = "Neural implementation of CKIP WS, POS, NER tools",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ckiplab/ckipneutools",
+    packages = ["ckipneutools"],
+    package_dir = {"ckipneutools": "src"},
+    extras_require = {
+        "tf": ["tensorflow"],
+        "tfgpu": ["tensorflow-gpu"],
+        "gdown": ["gdown"],
+    },
+    licence = "CC BY-NC-SA 4.0",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+    ],
+)
