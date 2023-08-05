3e0b8029db596c010d6ceade83b4053d1bf1042f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:06 +0000

    Add package followthemoney/1.17.2/followthemoney-1.17.2.tar.gz

---
+++ packages/followthemoney/followthemoney-1.17.2.tar.gz/followthemoney-1.17.2/followthemoney/types/entity.py
@@ -0,0 +1,19 @@
+from rdflib import URIRef
+
+from followthemoney.types.common import PropertyType
+from followthemoney.util import get_entity_id
+from followthemoney.util import defer as _
+
+
+class EntityType(PropertyType):
+    name = 'entity'
+    group = 'entities'
+    label = _('Entity')
+    plural = _('Entities')
+    matchable = True
+
+    def clean(self, text, **kwargs):
+        return get_entity_id(text)
+
+    def rdf(self, value):
+        return URIRef('entity:%s' % value)
