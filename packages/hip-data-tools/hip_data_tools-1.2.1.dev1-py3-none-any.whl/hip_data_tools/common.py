7591a12b0c4f75be0c31eba2c09a81b2f291e35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:55 +0000

    Add package hip-data-tools/1.2.1.dev1/hip_data_tools-1.2.1.dev1-py3-none-any.whl

---
+++ packages/hip-data-tools/hip_data_tools-1.2.1.dev1-py3-none-any.whl/hip_data_tools/common.py
@@ -0,0 +1,10 @@
+"""
+Module contains variables and methods used for common / shared operations throughput the package
+"""
+
+import logging
+
+LOG = logging.getLogger(__name__)
+"""
+logger object to handle logging in the entire package
+"""
