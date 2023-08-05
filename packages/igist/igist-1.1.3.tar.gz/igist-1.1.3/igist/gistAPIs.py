c15f88197eeb6475c80f690e67554c625ef2d83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:02 +0000

    Add package igist/1.1.3/igist-1.1.3.tar.gz

---
+++ packages/igist/igist-1.1.3.tar.gz/igist-1.1.3/igist/gistAPIs.py
@@ -0,0 +1,167 @@
+import json
+import requests
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    # filename='igist.log',
+                    datefmt='%Y/%m/%d %H:%M:%S  ',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+class Gist():
+    def __init__(self, usrname, password, token,
+                 is_public=True, description="This gist is created by python"):
+        # when creating a Gist class, a existing gist token should be specified
+        # ``token``
+        # - an existing gist token under this user, or a pulic one
+        # - '?' to chose from existing gists
+        # - '+' to create a new gist
+        self.usrname = usrname
+        self.password = password
+        self.gist_token = ''
+        if token == '?':
+            # chose from gist list
+            gists = self._list_gists()
+            tokens = list(gists.keys())
+            for index, g_token in enumerate(tokens):
+                print("【{}】. {} : {}".format(index, g_token, gists[g_token]))
+            usr_input = input(
+                "input a numeber to chose [ or '+' to create ] a gist: ")
+
+            if usr_input.isdigit() and 0 <= int(usr_input) < len(tokens):
+                self.gist_token = tokens[int(usr_input)]
+            elif usr_input == '+':
+                token = '+'
+
+        if token == '+':
+            self.gist_token = self._create_a_gist(
+                is_public=is_public, description=description)
+        elif self._is_gist_valid(token=token):
+            self.gist_token = token
+
+        if self.gist_token:
+            logging.info('gist class is bound with:{}'.format(self.gist_token))
+        else:
+            logging.critical('creating gist class error!')
+            raise Exception('creating gist class error!')
+
+    def _create_a_gist(self,
+                       is_public=True,
+                       description="This gist is created by python"):
+        # gist api allows to add a new gist with one or more files
+        logging.info('creating new gist...')
+        r = requests.post(
+            'https://api.github.com/gists',
+            json.dumps({
+                "description": description,
+                "public": is_public,
+                'files': {"InitialFile": {"content": 'Initial text'}},
+            }),
+            auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+        )
+        if r.status_code == 201:
+            token = r.json()['html_url'].split('/')[-1]
+            # delete InitialFile
+            self.delete('InitialFile', token=token)
+            logging.info('new gist token:{}'.format(token))
+            return token
+
+    def _is_gist_valid(self, token=''):
+        if not token:
+            token = self.gist_token
+        else:
+            r = requests.get(
+                'https://api.github.com/gists/{}'.format(token),
+                auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+            )
+        if r.status_code == 200:
+            return r.json()['id'] == token
+
+    def _list_gists(self):
+        # list all the gists of an authorized account, or public gists if no acount been authorized.
+        # gists' token and file names under each are returned
+        r = requests.get(
+            'https://api.github.com/gists',
+            auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+        )
+        if r.status_code == 200:
+            gists = {gist['id']: list(gist['files'].keys())
+                     for gist in r.json()}
+            return gists
+
+    def write(self, file_name, file_content, token=''):
+        if not token:
+            token = self.gist_token
+        r = requests.post(
+            'https://api.github.com/gists/{}'.format(self.gist_token),
+            json.dumps({'files': {file_name: {'content': file_content}}}),
+            auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+        )
+        if r.status_code == 200:
+            logging.info('write succeed: {}'.format(file_name))
+        else:
+            logging.error('write failed: {}'.format(file_name))
+
+    def delete(self, file_name, token=''):
+        if not token:
+            token = self.gist_token
+        r = requests.post(
+            'https://api.github.com/gists/{}'.format(token),
+            json.dumps({'files': {file_name: None}}),
+            auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+        )
+        if r.status_code == 200:
+            logging.info('{} deleted.'.format(file_name))
+        else:
+            logging.error('file {} deleting failed'.format(file_name))
+
+    def read(self, filename, token=''):
+        if not token:
+            token = self.gist_token
+        r = requests.get(
+            'https://api.github.com/gists/{}'.format(token),
+            auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+        )
+        if r.status_code == 200:
+            try:
+                files = r.json()['files']
+                content = files[filename]['content']
+                return content
+            except:
+                logging.error('file not found!')
+
+    def ls(self, token='', detail=False):
+        # list file of a gist
+        if not token:
+            token = self.gist_token
+        r = requests.get(
+            'https://api.github.com/gists/{}'.format(token),
+            auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+        )
+        if r.status_code == 200:
+            if detail:
+                return r.json()['files']
+            else:
+                files = r.json()['files']
+                names = [name for name in files]
+                return names
+
+    def remove_gist(self, token=''):
+        if not token:
+            token = self.gist_token
+        r = requests.delete(
+            'https://api.github.com/gists/{}'.format(token),
+            auth=requests.auth.HTTPBasicAuth(self.usrname, self.password)
+        )
+        if r.status_code == 204:
+            logging.info('gist removed succeed: {}'.format(token))
+        else:
+            logging.error('failed to remove: {}'.format(token))
+
+
+if __name__ == "__main__":
+    pass
+    # import os
+    # usr = os.getenv('github_username')
+    # psw = os.getenv('github_password')
+
---
+++ packages/igist/igist-1.1.3.tar.gz/igist-1.1.3/setup.py
@@ -0,0 +1,60 @@
+from setuptools import setup, find_packages
+import sys
+import os
+import time
+
+
+def publish():
+    """Publish to PyPi"""
+    print('publishing...')
+    os.system("rm -rf dist igist.egg-info build")
+    os.system("python3 setup.py sdist build")
+    os.system("twine upload dist/*")
+    os.system("rm -rf dist igist.egg-info build")
+
+def _auto_version(version=''):
+    import time
+    import json
+    # read version from file
+    with open('igist/__version__.py', 'rb') as f:
+        ver = json.loads(f.readline().decode('utf-8'))
+
+    ver['published time'] = time.strftime(
+        '%Y.%m.%d %H:%M:%S', time.localtime(time.time()))  
+    if version:
+        ver['version'] = list(map(int, version.split('.')))
+    else:
+        ver['version'][2] += 1
+
+    # storage version to file
+    with open('igist/__version__.py', 'wb') as f:
+        f.write(json.dumps(ver).encode('utf-8'))
+
+    return '.'.join(map(str, ver['version']))
+
+if sys.argv[-1] == "-p":
+    publish()
+    sys.exit()
+
+setup(
+    name='igist',
+    version=_auto_version(),
+    # version=_auto_version('1.1.2'),
+    description=('igist is a python package for Github gist'),
+    long_description=open('README.rst').read(),
+    author='vinct',
+    author_email='vt.y@qq.com',
+    maintainer='vincent',
+    maintainer_email='vt.y@qq.com',
+    license='MIT License',
+    install_requires=[],
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/vincent770/igist.git',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
+
+# python3 setpu.py -p
