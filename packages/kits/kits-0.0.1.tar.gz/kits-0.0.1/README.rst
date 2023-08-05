25bd41bd90e74beaf7d935fe69887ef0f82cf573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package kits/0.0.1/kits-0.0.1.tar.gz

---
+++ packages/kits/kits-0.0.1.tar.gz/kits-0.0.1/README.rst
@@ -0,0 +1,33 @@
+SDK
+===
+
+.. image:: https://img.shields.io/pypi/v/kits.svg
+    :target: https://pypi.python.org/pypi/kits/
+    :alt: Latest Version
+
+.. image:: https://img.shields.io/pypi/wheel/kits.svg
+    :target: https://pypi.python.org/pypi/kits/
+
+.. image:: https://img.shields.io/pypi/pyversions/kits.svg
+    :target: https://pypi.python.org/pypi/kits/
+
+.. image:: https://img.shields.io/pypi/l/kits.svg
+    :target: https://pypi.python.org/pypi/kits/
+
+
+
+SDK.
+
+
+Installing
+----------
+
+Install and update using `pip`_:
+
+.. code-block:: text
+
+    pip install -U kits
+
+
+
+.. _pip: https://pip.pypa.io/en/stable/quickstart/
---
+++ packages/kits/kits-0.0.1.tar.gz/kits-0.0.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import io
+from setuptools import setup, find_packages
+from collections import OrderedDict
+
+version = "0.0.1"
+
+readme = ''
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='kits',
+    version=version,
+    description='kits',
+    long_description=readme,
+    url='https://github.com/pipname/kits',
+    keywords='kits',
+    author='Longniao',
+    author_email='longniao@gmail.com',
+    maintainer='Longniao',
+    maintainer_email='longniao@gmail.com',
+    license='MIT',
+    zip_safe=False,
+    platforms='any',
+    python_requires='>=3.5',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=[]
+)
