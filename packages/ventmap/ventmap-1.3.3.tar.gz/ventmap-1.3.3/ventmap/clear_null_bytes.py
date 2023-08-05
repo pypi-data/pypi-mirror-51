1bab465cbfbaf97d982db07dfd07ab9376c0ba31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:52 +0000

    Add package ventmap/1.3.3/ventmap-1.3.3.tar.gz

---
+++ packages/ventmap/ventmap-1.3.3.tar.gz/ventmap-1.3.3/ventmap/clear_null_bytes.py
@@ -0,0 +1,39 @@
+"""
+clear_null_bytes
+~~~~~~~~~~~~~~~~
+
+Self explanatory; clears null bytes from files
+"""
+import argparse
+import os
+from io import open, StringIO
+
+
+def clear_descriptor_null_bytes(descriptor):
+    descriptor_text = descriptor.read()
+    stringio = StringIO()
+    try:
+        to_write = unicode(descriptor_text.replace("\x00", ""))
+    except NameError:  # python3
+        to_write = str(descriptor_text.replace("\x00", ""))
+    stringio.write(to_write)
+    stringio.seek(0)
+    return stringio
+
+
+def clear_null_bytes(input_file):
+    with open(input_file, "rU") as old:
+        return clear_descriptor_null_bytes(old)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="rel path to input file")
+    args = parser.parse_args()
+    stringio = clear_null_bytes(args.input_file)
+    with open(new_file, "w") as new:
+        new.write(stringio.read())
+
+
+if __name__ == "__main__":
+    main()
