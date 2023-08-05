f99c664a25cf6d58478abc3834c142bb8c4f34b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:44 +0000

    Add package pptop/0.3.3/pptop-0.3.3.tar.gz

---
+++ packages/pptop/pptop-0.3.3.tar.gz/pptop-0.3.3/pptop/plugins/open_files.py
@@ -0,0 +1,39 @@
+from pptop.plugin import GenericPlugin
+
+from collections import OrderedDict
+
+
+class Plugin(GenericPlugin):
+    '''
+    open_files plugin: files, open by process
+
+        path: full file path
+        fd: file descriptor
+        pos: position in file
+        mode: work mode
+    '''
+    default_interval = 0.5
+
+    def on_load(self):
+        self.description = 'Process open files'
+        self.title = 'Open files'
+        self.short_name = 'Files'
+        self.sorting_rev = False
+
+    def load_data(self):
+        try:
+            with self.data_lock:
+                self.data.clear()
+                for f in self.get_process().open_files():
+                    d = OrderedDict()
+                    d['path']= f.path
+                    d['fd']= f.fd
+                    d['pos.']= f.position
+                    d['mode']= f.mode
+                    self.data.append(d)
+                return True
+        except:
+            return False
+
+    async def run(self, *args, **kwargs):
+        super().run(*args, **kwargs)
---
+++ packages/pptop/pptop-0.3.3.tar.gz/pptop-0.3.3/pptop/plugins/plugin_selector.py
@@ -0,0 +1,45 @@
+from pptop.plugin import GenericPlugin
+
+from pptop.core import format_shortcut
+
+from atasker import background_task
+from collections import OrderedDict
+
+
+class Plugin(GenericPlugin):
+
+    default_interval = 60
+
+    def on_load(self):
+        self.title = 'Select plugin'
+        self.short_name = 'Plugns'
+        self.sorting_rev = False
+
+    def load_data(self):
+        self.data.clear()
+        for plugin_id, plugin in self.get_plugins().items():
+            p = plugin['p']
+            if p.name not in ['plugin_selector', 'help']:
+                sh = format_shortcut(plugin['shortcut'])
+                try:
+                    version = str(plugin['m'].__version__)
+                except:
+                    version = ''
+                d = OrderedDict()
+                d['id'] = p.name
+                d['name'] = p.title
+                d['description'] = p.description
+                d['shortcut'] = sh
+                d['version'] = version
+                self.data.append(d)
+
+    def handle_key_event(self, event, key, dtd):
+        if event == 'select':
+            background_task(self.switch_plugin)(
+                self.stdscr, self.get_plugin(self.get_selected_row()['id']))
+
+    async def run(self, *args, **kwargs):
+        super().run(*args, **kwargs)
+
+    def pause(self):
+        return
