4e33699adc85912aafb464beae97d9ff87a8df2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:51 +0000

    Add package onegov.swissvotes/1.2.4/onegov.swissvotes-1.2.4.tar.gz

---
+++ packages/onegov.swissvotes/onegov.swissvotes-1.2.4.tar.gz/onegov.swissvotes-1.2.4/onegov/swissvotes/fields/__init__.py
@@ -0,0 +1,8 @@
+from onegov.swissvotes.fields.dataset import SwissvoteDatasetField
+from onegov.swissvotes.fields.policy_area import PolicyAreaField
+
+
+__all__ = (
+    'PolicyAreaField',
+    'SwissvoteDatasetField',
+)
---
+++ packages/onegov.swissvotes/onegov.swissvotes-1.2.4.tar.gz/onegov.swissvotes-1.2.4/onegov/swissvotes/layouts/vote.py
@@ -0,0 +1,87 @@
+from cached_property import cached_property
+from onegov.core.elements import Link
+from onegov.swissvotes import _
+from onegov.swissvotes.layouts.default import DefaultLayout
+
+
+class VoteLayout(DefaultLayout):
+
+    @cached_property
+    def title(self):
+        return self.model.short_title
+
+    @cached_property
+    def editbar_links(self):
+        result = []
+        if self.request.has_role('admin', 'editor'):
+            result.append(
+                Link(
+                    text=_("Manage attachments"),
+                    url=self.request.link(self.model, name='upload'),
+                    attrs={'class': 'upload-icon'}
+                )
+            )
+            result.append(
+                Link(
+                    text=_("Delete vote"),
+                    url=self.request.link(self.model, name='delete'),
+                    attrs={'class': 'delete-icon'}
+                )
+            )
+        return result
+
+    @cached_property
+    def breadcrumbs(self):
+        return [
+            Link(_("Homepage"), self.homepage_url),
+            Link(_("Votes"), self.votes_url),
+            Link(self.title, '#'),
+        ]
+
+
+class VoteStrengthsLayout(DefaultLayout):
+
+    @cached_property
+    def title(self):
+        return _("Voter strengths")
+
+    @cached_property
+    def breadcrumbs(self):
+        return [
+            Link(_("Homepage"), self.homepage_url),
+            Link(_("Votes"), self.votes_url),
+            Link(self.model.short_title, self.request.link(self.model)),
+            Link(self.title, '#'),
+        ]
+
+
+class UploadVoteAttachemtsLayout(DefaultLayout):
+
+    @cached_property
+    def title(self):
+        return _("Manage attachments")
+
+    @cached_property
+    def breadcrumbs(self):
+        return [
+            Link(_("Homepage"), self.homepage_url),
+            Link(_("Votes"), self.votes_url),
+            Link(self.model.short_title, self.request.link(self.model)),
+            Link(self.title, '#'),
+        ]
+
+
+class DeleteVoteLayout(DefaultLayout):
+
+    @cached_property
+    def title(self):
+        return _("Delete vote")
+
+    @cached_property
+    def breadcrumbs(self):
+        return [
+            Link(_("Homepage"), self.homepage_url),
+            Link(_("Votes"), self.votes_url),
+            Link(self.model.short_title, self.request.link(self.model)),
+            Link(self.title, '#'),
+        ]
