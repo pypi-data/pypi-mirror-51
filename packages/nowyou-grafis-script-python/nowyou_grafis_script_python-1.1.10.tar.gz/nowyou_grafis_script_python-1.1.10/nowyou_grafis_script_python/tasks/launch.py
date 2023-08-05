45600ad9393a389b7a803a9fcc65f796520d0098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:01 +0000

    Add package nowyou-grafis-script-python/1.1.10/nowyou_grafis_script_python-1.1.10.tar.gz

---
+++ packages/nowyou-grafis-script-python/nowyou_grafis_script_python-1.1.10.tar.gz/nowyou_grafis_script_python-1.1.10/nowyou_grafis_script_python/tasks/launch.py
@@ -0,0 +1,38 @@
+from ..ui_tools.UI_tools import Ui_tools
+from ..utils.utils import resource_path
+
+import win32com.shell.shell as shell
+import win32event
+
+
+class Launch:
+
+    @staticmethod
+    def run():
+        Launch.launch_grafis()
+        Launch.close_new_version_modal()
+        Launch.close_temporary_files_modal()
+
+    # 1a
+    @staticmethod
+    def launch_grafis():
+        se_ret = shell.ShellExecuteEx(fMask=0x140, lpFile=r"P:\GRAFIS\Grafis.exe", nShow=1)
+        win32event.WaitForSingleObject(se_ret['hProcess'], -1)
+
+        while True:
+            position = Ui_tools.getPosition(resource_path('\static\1a.png'))
+            if position is not None:
+                break
+
+    # 1b
+    @staticmethod
+    def close_new_version_modal():
+        Ui_tools.hot_key('Esc')
+
+    # 1c
+    @staticmethod
+    def close_temporary_files_modal():
+        position = Ui_tools.getPosition(resource_path('\static\1b.png'))
+
+        if position is not None:
+            Ui_tools.hot_key('Esc')
