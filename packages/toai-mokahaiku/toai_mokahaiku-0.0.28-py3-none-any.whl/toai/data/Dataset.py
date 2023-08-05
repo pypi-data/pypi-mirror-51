d9c885154d9b0f9fd0e99c6ae13cd10cee0c594f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:10 +0000

    Add package toai-mokahaiku/0.0.28/toai_mokahaiku-0.0.28-py3-none-any.whl

---
+++ packages/toai-mokahaiku/toai_mokahaiku-0.0.28-py3-none-any.whl/toai/data/Dataset.py
@@ -0,0 +1,10 @@
+import math
+
+
+class Dataset:
+    def __init__(self, data, batch_size, length):
+        self.data = data
+        self.batch_size = batch_size
+        self.test = length
+        self.steps = math.ceil()
+
