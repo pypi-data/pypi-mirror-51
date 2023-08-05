7659612d30d7003d0f84c6838ca70d9ccbcf3bcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:08 +0000

    Add package pikselnetbox/0.0.6/pikselnetbox-0.0.6.tar.gz

---
+++ packages/pikselnetbox/pikselnetbox-0.0.6.tar.gz/pikselnetbox-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pikselnetbox',
+      version='0.0.6',
+      description='Module to automate Netbox',
+      url='http://gitlab.com/joshrchrds/piksel-netbox',
+      author='Josh Richards',
+      author_email='joshua.richards135@gmail.com',
+      license='MIT',
+      packages=['pikselnetbox'],
+      zip_safe=False)
+
