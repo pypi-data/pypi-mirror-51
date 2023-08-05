882dafd4512b9d5f2a0ac5bedc89ea350fb8d9ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:29 +0000

    Add package ruth/0.0.1/ruth-0.0.1.tar.gz

---
+++ packages/ruth/ruth-0.0.1.tar.gz/ruth-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import setuptools
+
+
+setup(
+  name = 'ruth',
+  packages = ['ruth'],
+  version = '0.0.1',
+  description = 'Ruth',
+  long_description = 'MIT',
+  author = '',
+  license = '',
+  package_data={},
+  url = '',
+  keywords = [],
+  classifiers = [],
+  install_requires = ['six', 'click', 'joblib', 'tqdm'],
+)
