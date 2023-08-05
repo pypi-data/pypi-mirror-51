57bba18e0b13454f4974e356cc4f4f55b533fb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:55 +0000

    Add package custom-html-validator/1.0.2/custom_html_validator-1.0.2-py3-none-any.whl

---
+++ packages/custom-html-validator/custom_html_validator-1.0.2-py3-none-any.whl/custom_html_validator/__init__.py
@@ -0,0 +1,10 @@
+from .custom_html_validator import *
+
+__copyright__    = 'Copyright (C) 2018 Fumihiko Umeta'
+__version__      = '1.0.2'
+__license__      = 'MIT license'
+__author__       = 'fumihiko umeta'
+__author_email__ = 'koan.u.f@gmail.com'
+__url__          = 'https://github.com/koan-u/custom_html_validator'
+
+__all__ = ['custom_html_validator']
