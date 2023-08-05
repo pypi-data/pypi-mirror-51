9a348f3cd09f1413cee9e326d1472295364f39a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:06 +0000

    Add package emp-ide-ext/1.20/emp-ide-ext-1.20.tar.gz

---
+++ packages/emp-ide-ext/emp-ide-ext-1.20.tar.gz/emp-ide-ext-1.20/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='emp-ide-ext',
+    version='1.20',
+    packages=['emp'],
+    author='singein',
+    author_email='singein@outlook.com',
+    url='http://emp.1zlab.com',
+    description='EMP(Easy MicroPython) is a upy module to make things Easy on MicroPython.'
+)
+
+# import setuptools
+# setuptools.setup(setup_requires=['pbr'], pbr=True)
