8f4901ac318c3e2b497463adff9131b7cdc0b544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:25 +0000

    Add package operacoes/1.0.0/operacoes-1.0.0.tar.gz

---
+++ packages/operacoes/operacoes-1.0.0.tar.gz/operacoes-1.0.0/operacoes.py
@@ -0,0 +1,11 @@
+def somar(a, b):
+    return a+b
+
+def subtrair(a, b):
+    return a-b
+
+def multiplicar(a, b):
+    return a*b
+
+def dividir(numerador, denominador):
+    return numerador/denominador
---
+++ packages/operacoes/operacoes-1.0.0.tar.gz/operacoes-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='operacoes',
+    version='1.0.0',
+    author='Lucas Emanual Nascimento Nóbrega Dias',
+    author_email='lucasiamtr@gmail.com',
+    description='As quatro principais operações matemáticas',
+    py_modules=['operacoes'],
+)
