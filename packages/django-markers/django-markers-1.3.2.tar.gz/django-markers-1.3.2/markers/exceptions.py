d8b36bab28a7806b16cfc4fb3ee87a8e4bb39f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package django-markers/1.3.2/django-markers-1.3.2.tar.gz

---
+++ packages/django-markers/django-markers-1.3.2.tar.gz/django-markers-1.3.2/markers/exceptions.py
@@ -0,0 +1,17 @@
+class InvalidTemplateError(Exception):
+    pass
+
+
+
+class InvalidColourError(Exception):
+    pass
+
+
+
+class InvalidOpacityError(Exception):
+    pass
+
+
+
+class InvalidHueError(Exception):
+    pass
