19d96f69aa083a8971fed78343fccd9cebbf4050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:07 +0000

    Add package modbus2websocket/0.0.4/modbus2websocket-0.0.4.tar.gz

---
+++ packages/modbus2websocket/modbus2websocket-0.0.4.tar.gz/modbus2websocket-0.0.4/modbus2websocket/async_modbus_server.py
@@ -0,0 +1,101 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Multiple Modbus Server using asyncio."""
+
+import asyncio
+from random import SystemRandom
+from pyModbusTCP.server import ModbusServer, DataBank
+import concurrent
+
+
+class ServerError(Exception):
+    """Server Exception class."""
+
+    pass
+
+
+class AsyncModbusServer(object):
+    """Asynchronous Modbus TCP Server."""
+
+    def __init__(
+        self,
+        *,
+        host='127.0.0.1',
+        bit_adr=0,
+        bit_num=1,
+        word_adr=0,
+        word_num=1,
+        timeout=1,
+        name='Modbus Server',
+    ):
+        """Init server."""
+        self.port = 502
+        self.host = host
+        self.bit_adr = bit_adr
+        self.bit_num = bit_num
+        self.word_adr = word_adr
+        self.word_num = word_num
+        self.timeout = timeout
+        self.name = name
+        self.connection = None
+        try:
+            self.connection = ModbusServer(
+                host=self.host,
+                port=self.port,
+                no_block=True,
+            )
+            self.connection.start()
+        except Exception as err:
+            raise ServerError('Error with server', err)
+
+    async def run_task(self):
+        """Run asyncio server."""
+        executor = concurrent.futures.ThreadPoolExecutor(
+            max_workers=3,
+        )
+        task = asyncio.create_task(self._awaitable(executor))
+        await task
+
+    def stop(self):
+        """Stop server."""
+        self.connection.stop()
+
+    def _setup(self):
+        cryptogen = SystemRandom()
+        while True:
+            bits_value = [
+                bool(cryptogen.randrange(2)) for
+                indx in range(self.bit_num)
+            ]
+            words_value = [
+                cryptogen.randrange(10) for
+                _ in range(self.word_num)
+            ]
+            try:
+                DataBank.set_bits(self.bit_adr, bits_value)
+                DataBank.set_words(self.word_adr, words_value)
+            except Exception as err:
+                raise ServerError('Error with writing', err)
+
+    async def _awaitable(self, executor):
+        loop = asyncio.get_event_loop()
+        while True:
+            await loop.run_in_executor(
+                executor,
+                self._setup(),
+                )
+            await asyncio.sleep(self.timeout)
+
+
+if __name__ == '__main__':
+    server1 = AsyncModbusServer(
+        host='127.0.0.1',
+        bit_adr=0,
+        bit_num=16,
+        word_adr=0,
+        word_num=5,
+        name='Server1',
+    )
+    asyncio.get_event_loop().run_until_complete(server1.run_task())
+    asyncio.get_event_loop().run_forever()
---
+++ packages/modbus2websocket/modbus2websocket-0.0.4.tar.gz/modbus2websocket-0.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+import sys
+if (sys.version_info.major < 3) and (sys.version_info.minor <= 6):
+    sys.exit('Sorry, Python 3.6 and 3.7 only supported')
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="modbus2websocket",
+    version='0.0.4',
+    description='Modbus/TCP to websocket router.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='modbus tcp websocket asyncio socket',
+    url='https://github.com/PakshNina/modbus2websocket',
+    author='Nina Pakshina',
+    author_email='ninucium@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'pyModbusTCP',
+        'websockets'
+    ],
+    include_package_data=True,
+    zip_safe=False,   
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    )
