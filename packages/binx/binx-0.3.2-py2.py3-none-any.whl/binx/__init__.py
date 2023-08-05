39a4069d4ed5cf3f8c569218fe46e6415cb82af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package binx/0.3.2/binx-0.3.2-py2.py3-none-any.whl

---
+++ packages/binx/binx-0.3.2-py2.py3-none-any.whl/binx/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for binx."""
+
+__author__ = """bsnacks000"""
+__email__ = 'bsnacks000@gmail.com'
+__version__ = '0.3.2'
+
+from .collection import BaseCollection, BaseSerializer, InternalObject
+
+__all__ = [ 'BaseCollection', 'InternalObject', 'BaseSerializer']
