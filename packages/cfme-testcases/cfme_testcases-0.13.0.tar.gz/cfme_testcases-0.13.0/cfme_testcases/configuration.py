9746ef685d99243f989d5f3b0e31f27198f5642a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:38 +0000

    Add package cfme-testcases/0.13.0/cfme_testcases-0.13.0.tar.gz

---
+++ packages/cfme-testcases/cfme_testcases-0.13.0.tar.gz/cfme_testcases-0.13.0/cfme_testcases/configuration.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Configuration loading.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+# pylint: disable=unused-import
+from dump2polarion.configuration import get_config  # noqa
