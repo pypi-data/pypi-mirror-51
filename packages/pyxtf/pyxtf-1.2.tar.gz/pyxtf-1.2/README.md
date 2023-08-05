3609cfe3e9634b493ae6ced21864118492bc693d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package pyxtf/1.2/pyxtf-1.2.tar.gz

---
+++ packages/pyxtf/pyxtf-1.2.tar.gz/pyxtf-1.2/README.md
@@ -0,0 +1,45 @@
+### pyxtf
+A python library for reading eXtended Triton Format (XTF) files (revision 44)
+
+##### Installation from pypi
+
+```bash
+pip3 install pyxtf
+```
+
+##### Installation from source
+Clone or download the repository and run the following command. This requires setuptools to be installed.
+
+```bash
+python3 setup.py install
+```
+
+###### Dependencies
+The project depends on setuptools and numpy. Matplotlib is used for plotting, but is not required for basic functionality.
+
+##### Usage
+
+```python
+import pyxtf
+
+input_file = 'yourfile.xtf'
+(file_header, packets) = pyxtf.xtf_read(input_file, verbose=True)
+```
+
+The file_header is of type XTFFileHeader, which is a c-structure that starts off every XTF file. The packets object is a dictionary of the packets that follow the file header. The key is of type XTFHeaderType, which is an enumerated class. The value is a list of objects that belong to that type of header type. E.g usage might look as the following.
+
+```python
+...
+# Retrieve a list of all sonar packets
+sonar_packets = packets[pyxtf.XTFHeaderType.sonar]
+
+# Print the first sonar packet (ping)
+print(sonar_packets[0])
+```
+
+Examples can be found in the [examples directory](https://github.com/oysstu/pyxtf/tree/master/examples) on github.
+
+##### Contribution
+ If you find an XTF-file that does not work, either submit a patch or new packet type, or be prepared to send an example XTF-file when submitting the bug-report.
+
+
---
+++ packages/pyxtf/pyxtf-1.2.tar.gz/pyxtf-1.2/setup.py
@@ -0,0 +1,45 @@
+from os import path
+from setuptools import setup
+from tools.generate_pyi import generate_pyi
+
+
+def main():
+    # Generate .pyi files
+    import pyxtf.xtf_ctypes
+    generate_pyi(pyxtf.xtf_ctypes)
+    import pyxtf.vendors.kongsberg
+    generate_pyi(pyxtf.vendors.kongsberg)
+
+    # read the contents of README file
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+
+    # Run setup script
+    setup(name='pyxtf',
+          version='1.2',
+          description='eXtended Triton Format (XTF) file interface',
+          long_description=long_description,
+          long_description_content_type='text/markdown',
+          author='Oystein Sture',
+          author_email='oysstu@gmail.com',
+          url='https://github.com/oysstu/pyxtf',
+          license='MIT',
+          setup_requires=['numpy>=1.11'],
+          install_requires=['numpy>=1.11', 'matplotlib>=1.5.1'],
+          packages=['pyxtf', 'pyxtf.vendors'],
+          package_data={'': ['*.pyi']},
+          use_2to3=False,
+          classifiers=[
+              'License :: OSI Approved :: MIT License',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Other Audience',
+              'Intended Audience :: Science/Research',
+              'Natural Language :: English',
+              'Topic :: Scientific/Engineering',
+              'Programming Language :: Python :: 3 :: Only'
+          ])
+
+
+if __name__ == '__main__':
+    main()
