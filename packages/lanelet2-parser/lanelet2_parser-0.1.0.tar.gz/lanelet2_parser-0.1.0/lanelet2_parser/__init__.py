3a140a9fad75f86d9d616fc563b2dfe9170b7c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:36:04 +0000

    Add package lanelet2-parser/0.1.0/lanelet2_parser-0.1.0.tar.gz

---
+++ packages/lanelet2-parser/lanelet2_parser-0.1.0.tar.gz/lanelet2_parser-0.1.0/lanelet2_parser/__init__.py
@@ -0,0 +1,8 @@
+from .parser import Lanelet2Parser
+
+__copyright__    = 'Copyright (C) 2019 Tier IV, Inc.'
+__version__      = '0.1.0'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Yuki Iida'
+__author_email__ = 'yuki.iida@tier4.jp'
+__url__          = 'https://github.com/tier4/lanelet2-parser'
