2038b76b9c818693954a647a0ec07ba8d718bb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:01 +0000

    Add package rtcbot/0.0.8/rtcbot-0.0.8-py3-none-any.whl

---
+++ packages/rtcbot/rtcbot-0.0.8-py3-none-any.whl/rtcbot/__init__.py
@@ -0,0 +1,10 @@
+from .base import SubscriptionClosed
+from .connection import RTCConnection
+from .websocket import Websocket
+from .camera import CVCamera, PiCamera, CVDisplay
+from .audio import Microphone, Speaker
+from .inputs import Gamepad, Mouse, Keyboard
+from .arduino import SerialConnection
+from .subscriptions import *
+from .javascript import getRTCBotJS
+from .devices import *
