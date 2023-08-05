c2da0b6f2ef64c4016a2a9603a720e15d926d977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:10 +0000

    Add package hallgrimJS/0.4.1/hallgrimJS-0.4.1.tar.gz

---
+++ packages/hallgrimJS/hallgrimJS-0.4.1.tar.gz/hallgrimJS-0.4.1/hallgrimJS/__init__.py
@@ -0,0 +1,9 @@
+
+__title__ = 'hallgrimJS'
+__version__ = '0.4.1'
+__author__ = 'Jan Maximilian Michal, Lukas Mayer, Toni Ringling'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Jan Maximilian Michal'
+
+# for the executable
+from .hallgrimJS import parseme
