e001fc04224d457fffd24b4ff9b4a1902de99eba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:08 +0000

    Add package backports.unittest-mock/1.5/backports.unittest_mock-1.5-py2.py3-none-any.whl

---
+++ packages/backports.unittest-mock/backports.unittest_mock-1.5-py2.py3-none-any.whl/backports/unittest_mock/__init__.py
@@ -0,0 +1,15 @@
+import sys
+import unittest
+
+
+def install():
+    "make mock appear from the future"
+    if sys.version_info > (3, 3):
+        return
+
+    mock = __import__('mock')
+    sys.modules['unittest.mock'] = unittest.mock = mock
+
+
+def pytest_configure():
+    install()
