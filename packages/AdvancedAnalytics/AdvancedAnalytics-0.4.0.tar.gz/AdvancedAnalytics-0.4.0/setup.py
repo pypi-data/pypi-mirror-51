180565aaa862e41d906ada79b8d21220e8ae6b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:41 +0000

    Add package AdvancedAnalytics/0.4.0/AdvancedAnalytics-0.4.0.tar.gz

---
+++ packages/AdvancedAnalytics/AdvancedAnalytics-0.4.0.tar.gz/AdvancedAnalytics-0.4.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 19:44:15 2019
+
+@author: EJones
+"""
+import setuptools
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="AdvancedAnalytics", 
+    version="0.4.0", 
+    author="Edward R Jones", 
+    author_email="ejones@tamu.edu", 
+    url="https://github.com/tandonneur/AdvancedAnalytics", 
+    description="Python support for 'The Art and Science of Data Analytics'",
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent"],
+    keywords=["Analytics", "data map", "preprocessing", "pre-processing", 
+              "postprocessing", "post-processing", "NLTK", "Sci-Learn", 
+              "sklearn", "StatsModels", "web scraping", "word cloud",
+              "regression", "decision trees", "random forest", 
+              "neural network", "cross validation", "topic analysis",
+              "sentiment analytic", "natural language processing", "NLP"],
+    zip_safe=True,
+    install_requires = [
+            "statsmodels>=0.9.0",
+            "nltk>=3.4",
+            "pydotplus>=2.0.2",
+            "graphviz>=2.40.1",
+            "wordcloud>=1.5.0",
+            "newspaper3k>=0.2.8",
+            "newsapi-python>=0.2.5"]
+)
