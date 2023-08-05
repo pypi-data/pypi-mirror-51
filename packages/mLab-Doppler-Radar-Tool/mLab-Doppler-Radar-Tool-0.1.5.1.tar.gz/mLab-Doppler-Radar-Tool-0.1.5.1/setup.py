cffe949832351a7d115b76810071a904202765e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:27 +0000

    Add package mLab-Doppler-Radar-Tool/0.1.5.1/mLab-Doppler-Radar-Tool-0.1.5.1.tar.gz

---
+++ packages/mLab-Doppler-Radar-Tool/mLab-Doppler-Radar-Tool-0.1.5.1.tar.gz/mLab-Doppler-Radar-Tool-0.1.5.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='mLab-Doppler-Radar-Tool',
+      version='0.1.5.1',
+      packages=['mLab_DopplerRadar'],
+      install_requires=[
+          'pyserial','pyqtgraph'
+      ],
+      include_package_data=True,
+      url='', license='MIT',
+      author='Alex Kuan',
+      author_email='agathakuannew@gmail.com',
+      description='mLab Doppler Radar UART tool and example code')
