7d770c8d8138f7928b655be94e65d562783a2e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:31 +0000

    Add package HABApp/0.7.1/HABApp-0.7.1.tar.gz

---
+++ packages/HABApp/HABApp-0.7.1.tar.gz/HABApp-0.7.1/HABApp/__init__.py
@@ -0,0 +1,12 @@
+import HABApp.util
+
+import HABApp.config
+import HABApp.core
+
+import HABApp.mqtt
+import HABApp.openhab
+import HABApp.rule
+import HABApp.runtime
+
+from HABApp.rule import Rule
+#from HABApp.runtime import Runtime
---
+++ packages/HABApp/HABApp-0.7.1.tar.gz/HABApp-0.7.1/HABApp/rule/rule_parameter.py
@@ -0,0 +1,50 @@
+import typing
+
+from ..rule_manager import RuleParameters
+
+
+class RuleParameter:
+    def __init__(self, rule_parameters: RuleParameters, filename: str, *keys, default_value: typing.Any = 'ToDo'):
+
+        assert isinstance(rule_parameters, RuleParameters), type(rule_parameters)
+        self.__parameters: RuleParameters = rule_parameters
+
+        assert isinstance(filename, str)
+        self.filename: str = filename
+        self.keys = keys
+
+        # as a convenience try to create the file and the file structure
+        self.__parameters.add_param(self.filename, *self.keys, default_value=default_value)
+
+    def get_value(self):
+        return self.__parameters.get_param(self.filename, *self.keys)
+
+    def __eq__(self, other):
+        return self.get_value() == other
+
+    def __lt__(self, other):
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+
+        return self.get_value() < other
+
+    def __le__(self, other):
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+
+        return self.get_value() <= other
+
+    def __ge__(self, other):
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+
+        return self.get_value() >= other
+
+    def __gt__(self, other):
+        if not isinstance(other, (int, float)):
+            return NotImplemented
+
+        return self.get_value() > other
+
+    def __repr__(self):
+        return f'<RuleParameter file: {self.filename}, keys: {self.keys}, value: {self.get_value()}'
