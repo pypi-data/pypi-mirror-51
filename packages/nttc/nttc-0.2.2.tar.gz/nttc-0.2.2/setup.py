4a04c206114be431b1baa3f6d65d93475b075ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:47 +0000

    Add package nttc/0.2.2/nttc-0.2.2.tar.gz

---
+++ packages/nttc/nttc-0.2.2.tar.gz/nttc-0.2.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'nttc',
+  packages = ['nttc'], # this must be the same as the name above
+  version = '0.2.2',
+  description = 'A set of functions that process and create topic models from a sample of community-detected Twitter networks\' tweets.',
+  author = 'Chris A. Lindgren',
+  author_email = 'chris.a.lindgren@gmail.com',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url = 'https://github.com/lingeringcode/nttc/', # use the URL to the github repo
+  download_url = 'https://github.com/lingeringcode/nttc/', 
+  install_requires = ['pandas', 'numpy', 'emoji', 'nltk', 'pprint', 'gensim', 'spacy'],
+  keywords = ['data processing', 'topic modeling', 'naming network communities'], # arbitrary keywords
+  classifiers = [],
+  include_package_data=True
+)
