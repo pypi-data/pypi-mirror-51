ac5902bd181febb1eb02063f0bb53aee0637d479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package ftw/1.2.0/ftw-1.2.0.tar.gz

---
+++ packages/ftw/ftw-1.2.0.tar.gz/ftw-1.2.0/ftw/__init__.py
@@ -0,0 +1,6 @@
+from . import http
+from . import logchecker
+from . import ruleset
+from . import testrunner
+from . import util
+from . import errors
---
+++ packages/ftw/ftw-1.2.0.tar.gz/ftw-1.2.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ftw',
+      version='1.2.0',
+      description='Framework for Testing WAFs',
+      author='Chaim Sanders, Zack Allen',
+      author_email='zma4580@gmail.com, chaim.sanders@gmail.com',
+      url='https://www.github.com/crs-support/ftw',
+      download_url='https://github.com/crs-support/ftw/tarball/1.2.0',
+      include_package_data=True,
+      package_data={
+        'ftw': ['util/public_suffix_list.dat']
+      },
+      entry_points = {
+        'pytest11': [
+            'ftw = ftw.pytest_plugin'
+        ]
+      },
+      packages=['ftw'],
+      keywords=['waf'],
+      install_requires=[
+          'IPy',
+          'pytest==2.9.1',
+          'PyYAML',
+          'python-dateutil==2.6.0'
+      ],
+     )
