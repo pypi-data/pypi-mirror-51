a681181a83173ebaab9da14fcbe8a197b7d09935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:04 +0000

    Add package pipgen/0.2.2/pipgen-0.2.2.tar.gz

---
+++ packages/pipgen/pipgen-0.2.2.tar.gz/pipgen-0.2.2/bin/pippush.sh
@@ -0,0 +1,38 @@
+if [ $# -lt 2 ]; then
+  printf "Usage: 'pippush.sh (path to package top-level) (local | test | prod)'\n\n"
+  exit
+fi
+WHERE=$2
+cd $1
+PKG_NAME=${PWD##*/}
+
+#install/update requirements for using pip
+printf "Installing and updating pip dependencies.\n"
+pip3 install -q --upgrade --no-cache-dir -r requirements.txt
+
+#build pip package
+printf "Building package from file hierarchy.\n"
+python3 setup.py -q sdist -q bdist_wheel
+
+#install new version on local only
+printf "Uninstalling and reinstalling '$PKG_NAME' with pip.\n"
+pip3 uninstall -q "$PKG_NAME" -y
+pip3 install -q --no-cache-dir dist/*.whl
+
+#install on local, then release on test.pypi
+if [ $WHERE = "test" ]; then
+  printf "Uploading '$PKG_NAME' to https://test.pypi.org/project/$PKG_NAME/.\n"
+  twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+fi
+
+#install on local, then release on production pypi
+if [ $WHERE = "prod" ]; then
+  printf "Uploading '$PKG_NAME' to https://pypi.org/project/$PKG_NAME/.\n"
+  twine upload dist/*
+fi
+
+#cleanup build files
+rm -rf dist build *.egg-info
+cd ..
+
+echo \'$PKG_NAME\' is now on version \'$(pip3 freeze | grep "$PKG_NAME" | sed "s/$PKG_NAME==//")\' && echo ''
---
+++ packages/pipgen/pipgen-0.2.2.tar.gz/pipgen-0.2.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("../README.md") as readmefile:
+    long_description = readmefile.read()
+
+setuptools.setup(
+  name = "pipgen",
+  version = "0.2.2",
+  author = "Casey Johnson",
+  author_email = "ctj0001@mix.wvu.edu",
+  description = "A package for easily creating and distributing pip packages.",
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  url = "https://github.com/caseyjohnsonwv/pip-package-generator",
+  packages = setuptools.find_packages(),
+  include_package_data = True,
+  scripts = ['./bin/pipify.sh', './bin/depipify.sh', './bin/pippush.sh'],
+  install_requires = ['twine', 'setuptools', 'wheel'],
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
