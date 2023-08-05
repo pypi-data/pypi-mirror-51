46a6e9763ddacf6f82d27f704dddbd30a94d7d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:50 +0000

    Add package abridge/0.1.4/abridge-0.1.4.tar.gz

---
+++ packages/abridge/abridge-0.1.4.tar.gz/abridge-0.1.4/abridge/cli.py
@@ -0,0 +1,113 @@
+"""
+CLI module for abridge
+"""
+
+import argparse
+import glob
+import os
+import sys
+from concurrent.futures import ThreadPoolExecutor, as_completed, thread
+from typing import List
+
+from . import __name__ as module_name
+from . import processor, ui
+
+
+def _shutdown(executor: ThreadPoolExecutor) -> None:
+    """
+    Force shutdown the given threadpool
+    """
+
+    executor._threads.clear()  # type: ignore # pylint: disable=protected-access
+    thread._threads_queues.clear()  # type: ignore # pylint: disable=protected-access
+
+
+def _runner(args: argparse.Namespace) -> None:
+    """
+    Run posh splice with the given input args
+    """
+
+    clips: List[str] = []
+    errors: List[str] = []
+
+    for clip_input in args.clips:
+        clip_files = glob.glob(clip_input)
+        clips += clip_files
+
+        if not clip_files:
+            errors.append(f"{clip_input}: No such file or directory")
+
+    if errors:
+        sys.exit("\n".join(errors))
+
+    if not os.path.isdir(args.out_dir):
+        os.mkdir(args.out_dir)
+
+    clips_progress = ui.get_manager().counter(
+        desc="Processed clips", unit="clips", total=len(clips)
+    )
+
+    with ThreadPoolExecutor(max_workers=args.workers) as executor:
+        futures = [
+            executor.submit(
+                processor.abridge_clip,
+                clip,
+                args.out_dir,
+                args.diff_threshold,
+                args.repetition_threshold,
+            )
+            for clip in clips
+        ]
+
+        try:
+            for future in as_completed(futures):
+                future.result()
+                clips_progress.update()
+
+        except KeyboardInterrupt:
+            _shutdown(executor)
+
+    clips_progress.close()
+    ui.get_manager().stop()
+
+
+def main() -> None:
+    """
+    CLI main, parse the input args and execute
+    """
+
+    parser = argparse.ArgumentParser(
+        module_name, description="Effortlessly shorten videos"
+    )
+    parser.add_argument(
+        "clips", metavar="clip", nargs="+", help="Clip to cut or glob group"
+    )
+    parser.add_argument(
+        "-w",
+        default=3,
+        type=int,
+        metavar="workers",
+        help="Number of clip processors",
+        dest="workers",
+    )
+    parser.add_argument(
+        "-o", metavar="outdir", type=str, default="processed", dest="out_dir"
+    )
+    parser.add_argument(
+        "-t",
+        metavar="diff-threshold",
+        type=float,
+        default=20,
+        dest="diff_threshold",
+        help="Difference threshold required between frames for a frames to be considered different",
+    )
+    parser.add_argument(
+        "-r",
+        metavar="repetition-threshold",
+        type=int,
+        default=5,
+        dest="repetition_threshold",
+        help="Number of frames in a row required to make a cut",
+    )
+
+    _runner(parser.parse_args())
---
+++ packages/abridge/abridge-0.1.4.tar.gz/abridge-0.1.4/abridge/processor.py
@@ -0,0 +1,92 @@
+"""
+abridge - an automated video splicing tool
+"""
+
+import ntpath
+import os
+from typing import List, Tuple
+
+import numpy as np
+from moviepy.editor import VideoClip, VideoFileClip
+
+from . import ui
+
+
+def _find_voids(
+    clip: VideoClip, diff_threshold: float, repetition_threshold: int
+) -> List[Tuple[float, float]]:
+    cuts: List[Tuple[float, float]] = []
+    last_used_frame = None
+    last_used_frame_t = 0
+
+    num_similar = 0
+
+    with ui.ProgressBar(f"{clip.filename} - Processing frames", 1) as progress:
+        for frame_t, frame in clip.iter_frames(with_times=True, logger=progress):
+            if last_used_frame is None:
+                last_used_frame = frame
+                last_used_frame_t = frame_t
+                continue
+
+            diff = np.mean((frame - last_used_frame) ** 2)
+
+            if diff < diff_threshold:
+                # there are no signficant differences between this frame and the
+                # last used frame
+
+                num_similar += (
+                    1  # Count the number of frames in a row which are similar
+                )
+
+            else:
+                # there was a significant difference between the frames
+                if num_similar > repetition_threshold:
+                    # there were enough differences in a row
+                    cuts.append((last_used_frame_t, frame_t))
+
+                # take note of this frame, and use it as reference for comparison
+                last_used_frame = frame
+                last_used_frame_t = frame_t
+                num_similar = 0
+
+    return cuts
+
+
+def _apply_cuts(clip: VideoClip, cuts: List[Tuple[float, float]]) -> VideoClip:
+    for start, end in reversed(cuts):
+        clip = clip.cutout(start, end)
+    return clip
+
+
+def abridge_clip(
+    path: str,
+    out_dir: str = "processed",
+    diff_threshold: float = 20,
+    repetition_threshold: int = 5,
+) -> None:
+    """
+    Splice and render out the given clip with the given processing attributes
+    """
+
+    clip = VideoFileClip(path)
+    clipname = ntpath.basename(path)
+
+    cuts: List[Tuple[float, float]] = _find_voids(
+        clip, diff_threshold, repetition_threshold
+    )
+
+    print(f"{clipname} - Removing empty sections")
+
+    before_time = clip.duration
+
+    clip = _apply_cuts(clip, cuts)
+
+    after_time = clip.duration
+    print(
+        f"{clipname} - Removed {before_time - after_time}s from footage. (before: {before_time}s, after: {after_time}s)"
+    )
+
+    with ui.ProgressBar(f"{clipname} - Rendering", 1) as progress:
+        clip.write_videofile(os.path.join(out_dir, clipname), logger=progress)
+
+    clip.close()
