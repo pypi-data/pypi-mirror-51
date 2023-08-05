791abb0f5ca381c1b2bb9495628c2053f688f958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:57 +0000

    Add package libapiclient/0.0.2/libapiclient-0.0.2.tar.gz

---
+++ packages/libapiclient/libapiclient-0.0.2.tar.gz/libapiclient-0.0.2/README.md
@@ -0,0 +1,6 @@
+# libapiclient
+API client common
+
+## v0.0.2
+
+* Log traceback when error
---
+++ packages/libapiclient/libapiclient-0.0.2.tar.gz/libapiclient-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+install_requires = ['requests', 'libenum']
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="libapiclient",
+    version="0.0.2",
+    author="jiao.xue",
+    author_email="jiao.xuejiao@gmail.com",
+    description="API client tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/caser789/libapiclient",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
