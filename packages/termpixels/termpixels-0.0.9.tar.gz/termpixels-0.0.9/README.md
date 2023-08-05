51509177facdd27325771fa4152d1de5b729b903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:49 +0000

    Add package termpixels/0.0.9/termpixels-0.0.9.tar.gz

---
+++ packages/termpixels/termpixels-0.0.9.tar.gz/termpixels-0.0.9/README.md
@@ -0,0 +1,78 @@
+# termpixels
+*the terminal as a character-cell matrix*
+
+## Get it
+[This project is on PyPI][pypi].
+
+Alternatively, clone this repository and `pip install -e .` in the root directory.
+
+Requires Python 3, and **no dependencies**\*!
+
+\* Requires [`pytest`](https://pypi.org/project/pytest/) to run tests
+
+## Purpose
+Creating programs that run inside of terminals seems convoluted. The goal of termpixels is to **abstract the terminal into a 2D array of "pixels"**, or character cells, which each contain a single text character, a foreground color, and a background color. termpixels allows you to modify the screen contents anywhere, at any time, and then handles updating the terminal automatically, as well as simplifying complicated terminal input processing.
+
+Ultimately, this project seeks to make the terminal *more accessible* and *more fun*.
+
+## Limitations
+There are lots of great libraries for coloring terminal output. This one is **designed for full-screen applications** that completely control the contents of the screen. That means that it, e.g., automatically saves and clears the screen, resets the cursor position, and accepts input in cbreak mode. 
+
+It's not the best solution for simply printing colored text, though [you can do that if you want][text coloring].
+
+## Demo
+![Demo gif](fun-text.gif)
+```python
+from termpixels import App, Color
+from time import time
+from math import sin
+
+class FunTextApp(App):
+    def on_frame(self):
+        self.screen.clear()                           # remove everything from the screen
+        text = "Hello world, from termpixels!"
+        
+        for i, c in enumerate(text):
+            f = i / len(text)
+            color = Color.hsl(f + time(), 1, 0.5)     # create a color from a hue value
+            x = self.screen.w // 2 - len(text) // 2   # horizontally center the text
+            offset = sin(time() * 3 + f * 5) * 2      # some arbitrary math
+            y = round(self.screen.h / 2 + offset)     # vertical center with an offset
+            self.screen.print(c, x + i, y, fg=color)  # draw the text to the screen buffer
+        
+        self.screen.update()                          # commit the changes to the screen
+
+if __name__ == "__main__":
+    FunTextApp().start()
+```
+
+### More demos
+* [DoomTerm](https://github.com/loganzartman/doomterm)
+* [tpmatrix](https://github.com/loganzartman/tpmatrix)
+
+## Features
+* **Unix** (and **Mac**) terminal feature detection with terminfo (via Python [curses][python-curses])
+* **Windows** support through Win32 Console API
+* Terminal (re)**size detection**
+* Asynchronous input
+	* **Keyboard** input with support for special keys like arrows, function keys, escape, etc.
+	* **Mouse** click, scroll and move input in terminals supporting xterm mouse
+* 16, 256, and true **color** output (with detection for best supported mode)
+* Display the **cursor** anywhere (or hide it!)
+* **Preserves** the state of the user's terminal using alternate screen buffer.
+* Rudimentary support for **fullwidth** characters.
+* No reliance on ncurses except for terminfo lookup
+* 100% Python
+* and more
+
+## Inspiration
+* [tcell][tcell]
+* [ncurses][ncurses]
+* [Build your own Command Line][byocl]
+
+[python-curses]: https://docs.python.org/3/howto/curses.html
+[tcell]: https://github.com/gdamore/tcell
+[ncurses]: https://www.gnu.org/software/ncurses/
+[pypi]: https://pypi.org/project/termpixels/
+[byocl]: http://www.lihaoyi.com/post/BuildyourownCommandLinewithANSIescapecodes.html
+[text coloring]: https://github.com/loganzartman/termpixels/blob/master/termpixels/examples/simple_text_coloring.py
---
+++ packages/termpixels/termpixels-0.0.9.tar.gz/termpixels-0.0.9/termpixels/app.py
@@ -0,0 +1,148 @@
+"""A utility for building complete terminal applications.
+
+The App class wires together various components of termpixels to provide a
+complete platform for building terminal applications. It provides the following
+pre-built instances as member variables:
+    - backend: provides output (rendering) capabilities
+    - input: provides input (keyboard, mouse, etc.) capabilities
+    - screen: provides higher-level interface for printing text, filling areas,
+              reading characters, etc.
+
+The App class is generally used by extending it and overriding special event
+handler methods to implement the application's business logic. An instance of
+the subclass is constructed, and then the start() method is called on it, e.g.
+
+Usage:
+    class MyApp(App):
+        def on_frame(self):
+            ...
+        ...
+
+    if __name__ == "__main__":
+        MyApp().start()
+"""
+
+from time import sleep, perf_counter
+from termpixels.screen import Screen
+from termpixels.detector import detect_backend, detect_input
+
+class App:
+    def __init__(self, *, mouse=False, framerate=30):
+        """Initialize the application.
+
+        The constructor may be overridden in subclasses to perform one-time
+        initialization routines, like creating event listeners. Most start-up 
+        code should be placed in the on_start() method, as it will be called
+        each time the app starts (if it starts more than once), and the 
+        terminal will be fully configured and ready for use.
+        """
+        self.backend = detect_backend()
+        self.input = detect_input()
+        self.input.listen("key", lambda d: self.on_key(d))
+        self.input.listen("mouse", lambda d: self.on_mouse(d))
+        def handle_resize(_):
+            self.backend.size_dirty = True
+            self.on_resize()
+        self.input.listen("resize", handle_resize)
+        self._framerate = framerate
+        self._mouse = mouse
+        self._stopping = False
+
+    def start(self, *args, **kwargs):
+        """Start collecting input and run the App's main loop.
+        
+        Starts the application and begins calling the on_frame() method at the
+        framerate specified in the constructor.
+
+        Forwards all arguments to the on_start() callback.
+        """
+        try:
+            self._stopping = False
+            self.backend.enter_alt_buffer()
+            # screen needs to be initialized with alternate buffer active or
+            # it will destroy user's screen contents
+            self.screen = Screen(self.backend, self.input)
+            self.backend.clear_screen()
+            self.backend.application_keypad = True
+            if self._mouse:
+                self.backend.mouse_tracking = True
+            self.input.start()
+            self.on_start(*args, **kwargs)
+            self.backend.flush()
+            while not self._stopping:
+                t0 = perf_counter()
+                self.on_frame()
+                dt = perf_counter() - t0
+                sleep(max(1/500, 1/self._framerate - dt))
+        except KeyboardInterrupt:
+            pass
+        finally:
+            self.on_stop()
+            self.backend.application_keypad = False
+            self.backend.mouse_tracking = False
+            self.input.stop()
+            self.screen.show_cursor = True
+            self.screen.update()
+            self.backend.exit_alt_buffer()
+
+    def stop(self):
+        """Tell the application to stop running gracefully."""
+        self._stopping = True
+    
+    def on_start(self):
+        """Handle the application starting.
+
+        This should be overridden in applications that want to implement logic
+        that occurs as soon as start() has been called and the terminal has 
+        been properly configured.
+        """
+        pass
+    
+    def on_stop(self):
+        """Handle the application stopping.
+
+        This should be overridden in applications that want to implement logic
+        that occurs when the application exits for any reason, before the 
+        terminal has been reset to its original state.
+        """
+        pass
+
+    def on_key(self, data):
+        """Handle a key press event.
+        
+        This should be overridden in applications that need to respond to 
+        keyboard input. It is invoked with a termpixels.keys.Key instance
+        asynchronously, whenever a key is pressed.
+        """
+        pass
+    
+    def on_mouse(self, data):
+        """Handle a mouse event.
+
+        This should be overridden in applications that need to respond to
+        mouse input. In addition to overriding this method, subclasses must 
+        invoke the App constructor with the `mouse` parameter set to True, or
+        no mouse events will be fired. This method is invoked with a 
+        termpixels.keys.Mouse instance asynchronously, whenever a mouse event
+        occurs.
+        """
+        pass
+    
+    def on_resize(self):
+        """Handle a terminal resize event.
+
+        This should be overridden in applications that need to respond to 
+        changes in the terminal size. It is not strictly necessary to override 
+        this method to do so, as the terminal size may be accessed within the
+        App instance at any time as `self.screen.w` and `self.screen.h`
+        """
+        pass
+    
+    def on_frame(self):
+        """Handle a frame event.
+
+        This should be overridden in applications that need to perform updates
+        at a regular interval. It is invoked automatically at the framerate
+        specified in the App constructor once start() has been called.
+        """
+        pass
