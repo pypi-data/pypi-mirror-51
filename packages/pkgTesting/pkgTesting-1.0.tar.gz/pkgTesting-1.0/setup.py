e5cc41856cce9cea7acbbafccd2ab896624618cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:24 +0000

    Add package pkgTesting/1.0/pkgTesting-1.0.tar.gz

---
+++ packages/pkgTesting/pkgTesting-1.0.tar.gz/pkgTesting-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pkgTesting',
+    version='1.0',
+    packages=['foo'],
+    url='',
+    license='MIT',
+    author='ramon',
+    author_email='ramon@abc.com',
+    description='something for testing'
+)
