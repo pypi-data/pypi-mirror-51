d54a82ca2bb15e138542aa55c021db9583abb0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:24 +0000

    Add package pydov/0.2.1/pydov-0.2.1-py2.py3-none-any.whl

---
+++ packages/pydov/pydov-0.2.1-py2.py3-none-any.whl/pydov/search/grondwaterfilter.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""Module containing the search classes to retrieve DOV borehole data."""
+import pandas as pd
+
+from owslib.fes import (
+    Not,
+    PropertyIsNull,
+    And,
+)
+from .abstract import AbstractSearch
+from ..types.grondwaterfilter import GrondwaterFilter
+from ..util import owsutil
+
+
+class GrondwaterFilterSearch(AbstractSearch):
+    """Search class to retrieve information about groundwater screens
+    (GrondwaterFilter).
+    """
+
+    __wfs_schema = None
+    __wfs_namespace = None
+    __md_metadata = None
+    __fc_featurecatalogue = None
+    __xsd_schemas = None
+
+    def __init__(self):
+        """Initialisation."""
+        super(GrondwaterFilterSearch,
+              self).__init__('gw_meetnetten:meetnetten', GrondwaterFilter)
+
+    def _init_namespace(self):
+        """Initialise the WFS namespace associated with the layer."""
+        if GrondwaterFilterSearch.__wfs_namespace is None:
+            GrondwaterFilterSearch.__wfs_namespace = self._get_namespace()
+
+    def _init_fields(self):
+        """Initialise the fields and their metadata available in this search
+        class."""
+        if self._fields is None:
+            if GrondwaterFilterSearch.__wfs_schema is None:
+                GrondwaterFilterSearch.__wfs_schema = self._get_schema()
+
+            if GrondwaterFilterSearch.__md_metadata is None:
+                GrondwaterFilterSearch.__md_metadata = \
+                    self._get_remote_metadata()
+
+            if GrondwaterFilterSearch.__fc_featurecatalogue is None:
+                csw_url = self._get_csw_base_url()
+                fc_uuid = owsutil.get_featurecatalogue_uuid(
+                    GrondwaterFilterSearch.__md_metadata)
+
+                GrondwaterFilterSearch.__fc_featurecatalogue = \
+                    owsutil.get_remote_featurecatalogue(csw_url, fc_uuid)
+
+            if GrondwaterFilterSearch.__xsd_schemas is None:
+                GrondwaterFilterSearch.__xsd_schemas = \
+                    self._get_remote_xsd_schemas()
+
+            fields = self._build_fields(
+                GrondwaterFilterSearch.__wfs_schema,
+                GrondwaterFilterSearch.__fc_featurecatalogue,
+                GrondwaterFilterSearch.__xsd_schemas)
+
+            for field in fields.values():
+                if field['name'] not in self._type.get_field_names(
+                        include_wfs_injected=True):
+                    self._type._fields.append({
+                        'name': field['name'],
+                        'source': 'wfs',
+                        'sourcefield': field['name'],
+                        'type': field['type'],
+                        'wfs_injected': True
+                    })
+
+            self._fields = self._build_fields(
+                GrondwaterFilterSearch.__wfs_schema,
+                GrondwaterFilterSearch.__fc_featurecatalogue,
+                GrondwaterFilterSearch.__xsd_schemas)
+
+    def search(self, location=None, query=None, return_fields=None):
+        """Search for groundwater screens (GrondwaterFilter). Provide
+        `location` and/or `query`. When `return_fields` is None,
+        all fields are returned.
+
+        Excludes 'empty' filters (i.e. Putten without Filters) by extending
+        the `query` with a not-null check on pkey_filter.
+
+        Parameters
+        ----------
+        location : pydov.util.location.AbstractLocationFilter or \
+                    owslib.fes.BinaryLogicOpType<AbstractLocationFilter> or \
+                    owslib.fes.UnaryLogicOpType<AbstractLocationFilter>
+            Location filter limiting the features to retrieve. Can either be a
+            single instance of a subclass of AbstractLocationFilter, or a
+            combination using And, Or, Not of AbstractLocationFilters.
+        query : owslib.fes.OgcExpression
+            OGC filter expression to use for searching. This can contain any
+            combination of filter elements defined in owslib.fes. The query
+            should use the fields provided in `get_fields()`. Note that not
+            all fields are currently supported as a search parameter.
+        return_fields : list<str> or tuple<str> or set<str>
+            A list of fields to be returned in the output data. This should
+            be a subset of the fields provided in `get_fields()`. Note that
+            not all fields are currently supported as return fields.
+
+        Returns
+        -------
+        pandas.core.frame.DataFrame
+            DataFrame containing the output of the search query.
+
+        Raises
+        ------
+        pydov.util.errors.InvalidSearchParameterError
+            When not one of `location` or `query` is provided.
+
+        pydov.util.errors.InvalidFieldError
+            When at least one of the fields in `return_fields` is unknown.
+
+            When a field that is only accessible as return field is used as
+            a query parameter.
+
+            When a field that can only be used as a query parameter is used as
+            a return field.
+
+        pydov.util.errors.FeatureOverflowError
+            When the number of features to be returned is equal to the
+            maxFeatures limit of the WFS server.
+
+        AttributeError
+            When the argument supplied as return_fields is not a list,
+            tuple or set.
+
+        """
+        self._pre_search_validation(location, query, return_fields)
+
+        exclude_empty_filters = Not([PropertyIsNull(
+                                     propertyname='pkey_filter')])
+
+        if query is not None:
+            query = And([query, exclude_empty_filters])
+        else:
+            query = exclude_empty_filters
+
+        fts = self._search(location=location, query=query,
+                           return_fields=return_fields)
+
+        gw_filters = GrondwaterFilter.from_wfs(fts, self.__wfs_namespace)
+
+        df = pd.DataFrame(data=GrondwaterFilter.to_df_array(gw_filters,
+                                                            return_fields),
+                          columns=GrondwaterFilter.get_field_names(
+                              return_fields))
+        return df
---
+++ packages/pydov/pydov-0.2.1-py2.py3-none-any.whl/pydov/types/grondwaterfilter.py
@@ -0,0 +1,215 @@
+# -*- coding: utf-8 -*-
+"""Module containing the DOV data type for screens (Filter), including
+subtypes."""
+
+from .abstract import (
+    AbstractDovType,
+    AbstractDovSubType,
+)
+
+
+class Peilmeting(AbstractDovSubType):
+
+    _name = 'peilmeting'
+    _rootpath = './/filtermeting/peilmeting'
+
+    _fields = [{
+        'name': 'datum',
+        'source': 'xml',
+        'sourcefield': '/datum',
+        'definition': 'Datum van opmeten.',
+        'type': 'date',
+        'notnull': True
+    }, {
+        'name': 'tijdstip',
+        'source': 'xml',
+        'sourcefield': '/tijdstip',
+        'definition': 'Tijdstip van opmeten (optioneel).',
+        'type': 'string',
+        'notnull': False
+    }, {
+        'name': 'peil_mtaw',
+        'source': 'xml',
+        'sourcefield': '/peil_mtaw',
+        'definition': 'Diepte van de peilmeting, uitgedrukt in mTAW.',
+        'type': 'float',
+        'notnull': False
+    }, {
+        'name': 'betrouwbaarheid',
+        'source': 'xml',
+        'sourcefield': '/betrouwbaarheid',
+        'definition': 'Lijst van betrouwbaarheden (goed, onbekend of'
+                      'twijfelachtig).',
+        'type': 'string',
+        'notnull': False
+    }, {
+        'name': 'methode',
+        'source': 'xml',
+        'sourcefield': '/methode',
+        'xsd_type': 'PeilmetingMethodeEnumType',
+        'definition': 'Methode waarop de peilmeting uitgevoerd werd.',
+        'type': 'string',
+        'notnull': False
+    }, {
+        'name': 'filterstatus',
+        'source': 'xml',
+        'sourcefield': '/filterstatus',
+        'xsd_type': 'FilterstatusEnumType',
+        'definition': 'Status van de filter tijdens de peilmeting (in rust - '
+                      'werking).',
+        'type': 'string',
+        'notnull': False
+    }, {
+        'name': 'filtertoestand',
+        'source': 'xml',
+        'sourcefield': '/filtertoestand',
+        'xsd_type': 'FiltertoestandEnumType',
+        'definition':  "Filtertoestand bij de peilmeting. Standaardwaarde is "
+                       "'1' = Normaal.",
+        'type': 'integer',
+        'notnull': False
+    }]
+
+
+class GrondwaterFilter(AbstractDovType):
+    """Class representing the DOV data type for Groundwater screens."""
+
+    _subtypes = [Peilmeting]
+
+    _xsd_schemas = [
+        'https://www.dov.vlaanderen.be/xdov/schema/latest/xsd/kern/'
+        'gwmeetnet/FilterDataCodes.xsd',
+        'https://www.dov.vlaanderen.be/xdov/schema/latest/xsd/kern/'
+        'interpretatie/HydrogeologischeStratigrafieDataCodes.xsd'
+    ]
+
+    _fields = [{
+        'name': 'pkey_filter',
+        'source': 'wfs',
+        'sourcefield': 'filterfiche',
+        'type': 'string'
+    }, {
+        'name': 'pkey_grondwaterlocatie',
+        'source': 'wfs',
+        'sourcefield': 'putfiche',
+        'type': 'string'
+    }, {
+        'name': 'gw_id',
+        'source': 'wfs',
+        'sourcefield': 'GW_ID',
+        'type': 'string'
+    }, {
+        'name': 'filternummer',
+        'source': 'wfs',
+        'sourcefield': 'filternummer',
+        'type': 'string'
+    }, {
+        'name': 'filtertype',
+        'source': 'wfs',
+        'sourcefield': 'filtertype',
+        'type': 'string'
+    }, {
+        'name': 'x',
+        'source': 'wfs',
+        'sourcefield': 'X_mL72',
+        'type': 'float'
+    }, {
+        'name': 'y',
+        'source': 'wfs',
+        'sourcefield': 'Y_mL72',
+        'type': 'float'
+    }, {
+        'name': 'mv_mtaw',
+        'source': 'wfs',
+        'sourcefield': 'Z_mTAW',
+        'type': 'float'
+    }, {
+        'name': 'gemeente',
+        'source': 'wfs',
+        'sourcefield': 'gemeente',
+        'type': 'string'
+    }, {
+        'name': 'meetnet_code',
+        'source': 'xml',
+        'sourcefield': '/filter/meetnet',
+        'xsd_type': 'MeetnetEnumType',
+        'definition': 'Tot welk meetnet behoort deze filter.',
+        'type': 'integer',
+        'notnull': False
+    }, {
+        'name': 'aquifer_code',
+        'source': 'xml',
+        'sourcefield': '/filter/ligging/aquifer',
+        'xsd_type': 'AquiferEnumType',
+        'definition': 'In welke watervoerende laag hangt de filter (code).',
+        'type': 'string',
+        'notnull': False
+    }, {
+        'name': 'grondwaterlichaam_code',
+        'source': 'xml',
+        'sourcefield': '/filter/ligging/grondwaterlichaam',
+        'xsd_type': 'GrondwaterlichaamEnumType',
+        'definition': '',
+        'type': 'string',
+        'notnull': False
+    }, {
+        'name': 'regime',
+        'source': 'xml',
+        'sourcefield': '/filter/ligging/regime',
+        'definition': '',
+        'type': 'string',
+        'notnull': False
+    }, {
+        'name': 'diepte_onderkant_filter',
+        'source': 'wfs',
+        'sourcefield': 'onderkant_filter_m',
+        'type': 'float'
+    }, {
+        'name': 'lengte_filter',
+        'source': 'wfs',
+        'sourcefield': 'lengte_filter_m',
+        'type': 'float'
+    }]
+
+    def __init__(self, pkey):
+        """Initialisation.
+
+        Parameters
+        ----------
+        pkey : str
+            Permanent key of the Filter (screen), being a URI of the form
+            `https://www.dov.vlaanderen.be/data/boring/<id>`.
+
+        """
+        super(GrondwaterFilter, self).__init__('filter', pkey)
+
+    @classmethod
+    def from_wfs_element(cls, feature, namespace):
+        """Build `GrondwaterFilter` instance from a WFS feature element.
+
+        Parameters
+        ----------
+        feature : etree.Element
+            XML element representing a single record of the WFS layer.
+        namespace : str
+            Namespace associated with this WFS featuretype.
+
+        Returns
+        -------
+        filter : GrondwaterFilter
+            An instance of this class populated with the data from the WFS
+            element.
+
+        """
+        gwfilter = GrondwaterFilter(
+            feature.findtext('./{{{}}}filterfiche'.format(namespace)))
+
+        for field in cls.get_fields(source=('wfs',)).values():
+            gwfilter.data[field['name']] = cls._parse(
+                func=feature.findtext,
+                xpath=field['sourcefield'],
+                namespace=namespace,
+                returntype=field.get('type', None)
+            )
+
+        return gwfilter
