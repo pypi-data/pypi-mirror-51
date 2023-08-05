b3ede738210c8b0d81909d67404083734550b8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:48 +0000

    Add package drdigit-brezniczky/0.0.3/drdigit-brezniczky-0.0.3.tar.gz

---
+++ packages/drdigit-brezniczky/drdigit-brezniczky-0.0.3.tar.gz/drdigit-brezniczky-0.0.3/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drdigit-brezniczky",
+    version="0.0.3",
+    author="Janos Brezniczky",
+    author_email="brezniczky@gmail.com",
+    description="A digit doctoring detection package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brezniczky/drdigit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: "
+        "GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning ",
+        "Topic :: Scientific/Engineering"
+    ],
+    install_requires=[
+        "joblib>=0.13.2",
+        "numpy>=1.17.0",
+        "pandas>=0.24.2",
+        "python-dateutil>=2.8.0",
+        "pytz>=2019.2",
+        "scipy>=1.3.1",
+        "six>=1.12.0",
+    ]
+)
