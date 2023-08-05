a301b782c2df0ae09cbef4665e2ee443b7dca707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:28 +0000

    Add package hub/0.2/hub-0.2-py3-none-any.whl

---
+++ packages/hub/hub-0.2-py3-none-any.whl/hub/exceptions.py
@@ -0,0 +1,39 @@
+
+class OutOfBoundsError(Exception):
+    """Raised upon finding a missing chunk."""
+    pass
+
+
+class AlignmentError(Exception):
+    """Raised when there is an Alignment error."""
+    pass
+
+
+class IncompatibleShapes(Exception):
+    """Shapes do not match"""
+    pass
+
+
+class IncompatibleBroadcasting(Exception):
+    """Broadcasting issue"""
+    pass
+
+
+class IncompatibleTypes(Exception):
+    """Types can not cast"""
+    pass
+
+
+class WrongTypeError(Exception):
+    """Types is not supported"""
+    pass
+
+
+class NotAuthorized(Exception):
+    """Types is not supported"""
+    pass
+
+
+class ArrayNotFound(Exception):
+    """When Info could not be found for array"""
+    pass
