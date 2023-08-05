d830385441b270bf20cca107c5cabc56e96dc198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:56 +0000

    Add package meta1testnet/0.2.5/meta1testnet-0.2.5.tar.gz

---
+++ packages/meta1testnet/meta1testnet-0.2.5.tar.gz/meta1testnet-0.2.5/meta1testnet/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from meta1testnet import Meta1
+
+
+__all__ = [
+    "meta1testnet",
+    "account",
+    "amount",
+    "asset",
+    "block",
+    "blockchain",
+    "dex",
+    "market",
+    "storage",
+    "price",
+    "utils",
+    "wallet",
+    "committee",
+    "vesting",
+    "proposal",
+    "message",
+]
---
+++ packages/meta1testnet/meta1testnet-0.2.5.tar.gz/meta1testnet-0.2.5/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+# Work around mbcs bug in distutils.
+# http://bugs.python.org/issue10945
+import codecs
+
+try:
+    codecs.lookup("mbcs")
+except LookupError:
+    ascii = codecs.lookup("ascii")
+    codecs.register(lambda name, enc=ascii: {True: enc}.get(name == "mbcs"))
+
+VERSION = "0.2.5"
+URL = "https://github.com/meta1-blockchain/meta1-python-testnet"
+
+setup(
+    name="meta1testnet",
+    version=VERSION,
+    description="Python library for meta1testnet",
+    long_description=open("README.md").read(),
+    download_url="{}/tarball/{}".format(URL, VERSION),
+    author="Rostislav Gogolauri",
+    author_email="invest@lampgram.com",
+    maintainer="Rostislav Gogolauri",
+    maintainer_email="invest@lampgram.com",
+    url=URL,
+    keywords=["meta1testnet", "library", "api", "rpc"],
+    packages=["meta1testnet", "meta1api", "meta1base"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Office/Business :: Financial",
+    ],
+    install_requires=open("requirements.txt").readlines(),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    include_package_data=True,
+)
