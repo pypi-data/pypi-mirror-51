9e9036d43491c9e8f074648af3f5b30a0a8d4e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:45 +0000

    Add package Flask-JSONLocale/1.7/Flask-JSONLocale-1.7.tar.gz

---
+++ packages/Flask-JSONLocale/Flask-JSONLocale-1.7.tar.gz/Flask-JSONLocale-1.7/setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-JSONlocale
+-------------
+
+This is the description for that library
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-JSONLocale',
+    version='1.7',
+    url='http://example.com/flask-jsonlocale/',
+    license='GNU',
+    author='Martin Urbanec',
+    author_email='martin@urbanec.cz',
+    description='An extension for Flask allowing users to easily localize their Flask app',
+    long_description="Lorem ipsum dolor sit amet",
+    py_modules=['flask_jsonlocale'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+	    'simplejson'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
