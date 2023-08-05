af1421e6d3f8bd0dee518cfdd0be6793e57b930a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:22 +0000

    Add package uitable/0.0.1/uitable-0.0.1-py2-none-any.whl

---
+++ packages/uitable/uitable-0.0.1-py2-none-any.whl/uitable/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding: utf-8
+from .main import UiTable
+from .utils.exceptions import HeaderAndRowLenNotMatch, HeaderNotDefine
---
+++ packages/uitable/uitable-0.0.1-py2-none-any.whl/uitable/utils/exceptions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+# define custom exceptions
+
+
+class HeaderAndRowLenNotMatch(Exception):
+    pass
+
+
+class HeaderNotDefine(Exception):
+    pass
