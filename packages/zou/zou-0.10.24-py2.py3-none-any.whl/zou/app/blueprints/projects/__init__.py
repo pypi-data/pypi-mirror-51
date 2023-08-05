f94c73b3be2b6f92791faa3e49acb74f67b15207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:10 +0000

    Add package zou/0.10.24/zou-0.10.24-py2.py3-none-any.whl

---
+++ packages/zou/zou-0.10.24-py2.py3-none-any.whl/zou/app/blueprints/projects/__init__.py
@@ -0,0 +1,58 @@
+from flask import Blueprint
+from zou.app.utils.api import configure_api_from_blueprint
+
+from .resources import (
+    AllProjectsResource,
+    OpenProjectsResource,
+    ProductionTeamResource,
+    ProductionTeamRemoveResource,
+    ProductionMetadataDescriptorResource,
+    ProductionMetadataDescriptorsResource,
+    ProductionMilestonesResource,
+    ProductionScheduleItemsResource,
+    ProductionAssetTypesScheduleItemsResource,
+    ProductionEpisodesScheduleItemsResource,
+    ProductionSequencesScheduleItemsResource
+)
+
+routes = [
+    ("/data/projects/open", OpenProjectsResource),
+    ("/data/projects/all", AllProjectsResource),
+
+    ("/data/projects/<project_id>/team", ProductionTeamResource),
+    (
+        "/data/projects/<project_id>/team/<person_id>",
+        ProductionTeamRemoveResource
+    ),
+    (
+        "/data/projects/<project_id>/metadata-descriptors",
+        ProductionMetadataDescriptorsResource
+    ),
+    (
+        "/data/projects/<project_id>/metadata-descriptors/<descriptor_id>",
+        ProductionMetadataDescriptorResource
+    ),
+    (
+        "/data/projects/<project_id>/milestones",
+        ProductionMilestonesResource
+    ),
+    (
+        "/data/projects/<project_id>/schedule-items",
+        ProductionScheduleItemsResource
+    ),
+    (
+        "/data/projects/<project_id>/schedule-items/<task_type_id>/asset-types",
+        ProductionAssetTypesScheduleItemsResource,
+    ),
+    (
+        "/data/projects/<project_id>/schedule-items/<task_type_id>/episodes",
+        ProductionEpisodesScheduleItemsResource,
+    ),
+    (
+        "/data/projects/<project_id>/schedule-items/<task_type_id>/sequences",
+        ProductionSequencesScheduleItemsResource,
+    )
+]
+
+blueprint = Blueprint("projects", "projects")
+api = configure_api_from_blueprint(blueprint, routes)
