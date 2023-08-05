4f41f2aab4ae02f8bd27946cdde52fa6ff37a099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:33 +0000

    Add package ktrain/0.2.3/ktrain-0.2.3.tar.gz

---
+++ packages/ktrain/ktrain-0.2.3.tar.gz/ktrain-0.2.3/ktrain/predictor.py
@@ -0,0 +1,22 @@
+from .imports import *
+class Predictor(ABC):
+    """
+    Abstract class to preprocess data
+    """
+    @abstractmethod
+    def predict(self, data):
+        pass
+
+    @abstractmethod
+    def get_classes(self, filename):
+        pass
+
+    def save(self, fname):
+        self.model.save(fname)
+        fname_preproc = fname+'.preproc'
+        with open(fname_preproc, 'wb') as f:
+            pickle.dump(self.preproc, f)
+        return
+
+
+
