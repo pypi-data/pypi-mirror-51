8280a33ec4b88faf1c3cff644111fdf44a44fe49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:46:14 +0000

    Add package dogage/0.0.3/DogAge-0.0.3.tar.gz

---
+++ packages/dogage/DogAge-0.0.3.tar.gz/DogAge-0.0.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['DogAge']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['argparse', 'matplotlib', 'numpy', 'pandas', 'sklearn']
+
+entry_points = \
+{'console_scripts': ['dogage_validate = DogAge:dogage_validate.main']}
+
+setup_kwargs = {
+    'name': 'dogage',
+    'version': '0.0.3',
+    'description': 'Programms for DogAge Challenge',
+    'long_description': None,
+    'author': 'Aleksandr Sinitca',
+    'author_email': 'siniza.s.94@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
