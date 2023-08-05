23627662d62525faa207ea88d1d5ff958323ec15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:34 +0000

    Add package polarishub-flask/0.1.1/polarishub_flask-0.1.1.tar.gz

---
+++ packages/polarishub-flask/polarishub_flask-0.1.1.tar.gz/polarishub_flask-0.1.1/polarishub_flask/app.py
@@ -0,0 +1,21 @@
+from polarishub_flask import server
+import os
+from multiprocessing import Process
+
+os.chdir("polarishub_flask")
+app = server.create_app()
+def open_browser():
+    if os.name == 'nt':
+        os.system("start http://localhost:5000/")
+    else:
+        os.system("python -m webbrowser \"http://localhost:5000/\"")
+
+def start_app():
+    app.run(host="0.0.0.0", port=5000)
+
+def main():
+    open_browser()
+    start_app()
+
+if __name__ == "__main__":
+    main()
