da60be42262c47d5a1129f45f05331ce249c7d7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:26 +0000

    Add package dzo/0.0.3/dzo-0.0.3-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/dzo/dzo-0.0.3-cp37-cp37m-macosx_10_14_x86_64.whl/dzo/tokenizer/token.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Token schema module.
+"""
+from typing import NamedTuple, Tuple
+
+
+class NGramToken(NamedTuple):
+    """Result schema for NGramTokenizer.tokeinze method.
+    """
+    surface: str
+
+    def get_normalized(self) -> str:  # pylint: disable=missing-docstring
+        return self.surface
+
+
+class MeCabToken(NamedTuple):
+    """Result schema for MeCabTokenizer.tokenize method.
+    """
+    surface: str
+    pos: Tuple[str, ...]
+    infl_type: str
+    infl_form: str
+    base_form: str
+    reading: str
+    phonetic: str
+
+    def get_normalized(self) -> str:  # pylint: disable=missing-docstring
+        return self.base_form
