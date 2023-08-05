25214087066e301e5e8ca6a4a55bd19ca6fa7e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:28 +0000

    Add package skydl/0.0.15/skydl-0.0.15.tar.gz

---
+++ packages/skydl/skydl-0.0.15.tar.gz/skydl-0.0.15/skydl/ray/experimental/ray_streaming_util.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from ray.experimental.streaming.communication import QueueConfig
+
+
+class DefaultStreamingEnvConfig(object):
+    """
+    default ray streaming env config
+    """
+    def __init__(self, max_size=999999,
+                max_batch_size=99999,
+                max_batch_time=0.01,
+                prefetch_depth=10,
+                background_flush=False,
+                parallelism=1):
+        self.queue_config = QueueConfig(
+            max_size=max_size,
+            max_batch_size=max_batch_size,
+            max_batch_time=max_batch_time,
+            prefetch_depth=prefetch_depth,
+            background_flush=background_flush)
+        self.parallelism = parallelism
+
+
+class DefaultStreamingSource(object):
+    """default streaming source implement"""
+    def get_next(self):
+        # get current record
+        return None
+
