6dae174eb0b10979c516b64dc63a11fb28dfd1b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:44:48 +0000

    Add package first-package/0.0.2/first_package-0.0.2.tar.gz

---
+++ packages/first-package/first_package-0.0.2.tar.gz/first_package-0.0.2/README.md
@@ -0,0 +1,4 @@
+Description
+===========
+
+Some description
---
+++ packages/first-package/first_package-0.0.2.tar.gz/first_package-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="first_package",
+    version="0.0.2",
+    author="Urev Oleg",
+    author_email="urevolegg@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
