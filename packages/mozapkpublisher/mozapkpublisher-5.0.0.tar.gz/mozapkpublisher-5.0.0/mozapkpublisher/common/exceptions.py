63a40c1db51a966aed958cb02fec1032d7e120b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:09 +0000

    Add package mozapkpublisher/5.0.0/mozapkpublisher-5.0.0.tar.gz

---
+++ packages/mozapkpublisher/mozapkpublisher-5.0.0.tar.gz/mozapkpublisher-5.0.0/mozapkpublisher/common/exceptions.py
@@ -0,0 +1,57 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class LoggedError(Exception):
+    def __init__(self, msg):
+        logger.fatal(msg)
+        super(LoggedError, self).__init__(msg)
+
+
+class WrongArgumentGiven(LoggedError):
+    pass
+
+
+class CheckSumMismatch(LoggedError):
+    def __init__(self, checked_file, expected, actual):
+        super(CheckSumMismatch, self).__init__(
+            msg='Downloading "{}" failed!. Checksum "{}" was expected, but actually got "{}"'
+                .format(checked_file, expected, actual)
+        )
+
+
+class NotMultiLocaleApk(LoggedError):
+    def __init__(self, apk_path, unique_locales):
+        super(NotMultiLocaleApk, self).__init__(
+            'Not a multilocale APK. "{}" contains only: {}'.format(apk_path, unique_locales)
+        )
+
+
+class NoLocaleFound(LoggedError):
+    def __init__(self, apk_path, omni_ja_location, chrome_manifest_location):
+        super(NoLocaleFound, self).__init__(
+            'No locale detected in {}:{}:{}'.format(apk_path, omni_ja_location, chrome_manifest_location)
+        )
+
+
+class BadApk(LoggedError):
+    pass
+
+
+class BadSetOfApks(LoggedError):
+    pass
+
+
+class NoTranslationGiven(LoggedError):
+    def __init__(self, given_translations):
+        super(NoTranslationGiven, self).__init__(
+            msg='"{}" doesn\'t contain any item to work with'.format(given_translations)
+        )
+
+
+class TranslationMissingData(LoggedError):
+    def __init__(self, locale_name, additional_message):
+        super(TranslationMissingData, self).__init__(
+            msg='Locale "{}" misses some data: {}'.format(locale_name, additional_message)
+        )
