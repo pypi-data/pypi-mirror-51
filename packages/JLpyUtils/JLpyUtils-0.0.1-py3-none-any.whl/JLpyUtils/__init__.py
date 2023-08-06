5bbfc0a145af39cb07cd173ef87ca280baef3c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:38 +0000

    Add package JLpyUtils/0.0.1/JLpyUtils-0.0.1-py3-none-any.whl

---
+++ packages/JLpyUtils/JLpyUtils-0.0.1-py3-none-any.whl/JLpyUtils/__init__.py
@@ -0,0 +1,15 @@
+"""
+Custom modules/classes/methods for various data science, computer vision, and machine learning operations in python
+"""
+
+__version__ = '0.0.1'
+__repo__ = "https://github.com/jlnerd/JLpyUtils.git",
+
+    
+import JLpyUtils.plot
+import JLpyUtils.summary_tables
+import JLpyUtils.img
+import JLpyUtils.video
+import JLpyUtils.ML_models
+import JLpyUtils.kaggle
+import JLpyUtils.file_utils
