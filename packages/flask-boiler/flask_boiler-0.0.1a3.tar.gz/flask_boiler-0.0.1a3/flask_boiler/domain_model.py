758c449651e4b483174465400338379e6696baac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:18 +0000

    Add package flask-boiler/0.0.1a3/flask_boiler-0.0.1a3.tar.gz

---
+++ packages/flask-boiler/flask_boiler-0.0.1a3.tar.gz/flask_boiler-0.0.1a3/flask_boiler/domain_model.py
@@ -0,0 +1,10 @@
+from typing import Callable
+
+from .firestore_object import PrimaryObject
+
+
+class DomainModel(PrimaryObject):
+    """
+    Domain model is intended for handling business logic.
+    """
+    pass
---
+++ packages/flask-boiler/flask_boiler-0.0.1a3.tar.gz/flask_boiler-0.0.1a3/tests/test_context.py
@@ -0,0 +1,16 @@
+from flask_boiler import config
+from flask_boiler import context
+
+
+Config = config.Config
+
+def test_firebase_app_context():
+    config = Config(
+        app_name="gravitate-dive-testing",
+        debug=True,
+        testing=True,
+        certificate_filename="gravitate-dive-testing-firebase-adminsdk-g1ybn-2dde9daeb0.json"
+    )
+    CTX = context.Context
+    CTX.read(config)
+    assert CTX.firebase_app.project_id == "gravitate-dive-testing"
