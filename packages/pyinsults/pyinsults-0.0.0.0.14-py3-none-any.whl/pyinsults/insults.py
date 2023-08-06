9b68377777a70cc9a7be484ecf9ed950d798ccf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:56 +0000

    Add package pyinsults/0.0.0.0.14/pyinsults-0.0.0.0.14-py3-none-any.whl

---
+++ packages/pyinsults/pyinsults-0.0.0.0.14-py3-none-any.whl/pyinsults/insults.py
@@ -0,0 +1,108 @@
+import random
+
+adjectives = [
+
+    "stinking",
+    "whining",
+    "pathetic",
+    "ugly",
+    "obnoxious",
+    "low-life",
+    "nasty-ass",
+    "utter",
+    "clueless",
+    "weapons-grade",
+    "weaselheaded",
+    "toupéd",
+    "witless",
+    "backwards",
+    "undercooked",
+    "absolute",
+    "monumental",
+    "foolish",
+    "ignorant",
+    "inbred",
+    "half-assed",
+    "Trump voting",
+    "Boris Johnson",
+    "ass leaking",
+    "unwanted",
+    "pointless",
+    "corporate",
+    "weaponized",
+    "freaky"
+
+
+
+
+
+]
+
+insults = [
+    "asshole",
+    "moron",
+    "cumstain",
+    "foolio",
+    "dickwad",
+    "Trump voter",
+    "Justin Bieber fan",
+    "retard",
+    "fuckface",
+    "cocksmoker",
+    "shitgoblin",
+    "jizztrumpet",
+    "goatfucker",
+    "motherfucker",
+    "faglord",
+    "shitbag",
+    "dickbag",
+    "cockwomble",
+    "fucknugget",
+    "thundercunt",
+    "numpty",
+    "fleshbag",
+    "cocksplat",
+    "cabbage",
+    "peasant",
+    "farmer",
+    "crayon chewer",
+    "tosser",
+    "git",
+    "muppet",
+    "plonker",
+    "knob head",
+    "bell end",
+    "arsemonger",
+    "maggot",
+    "butthole",
+    "blobfish",
+    "pug",
+    "soap eater",
+    "windowlicker",
+    "fart",
+    "monkey",
+    "fuckface",
+    "mongoose",
+    "twat",
+    "cunt",
+    "inbred",
+    "worm",
+    "square",
+    "shitface",
+    "skank",
+    "dickhead",
+    "piece of fuck",
+    "dumbass",
+
+
+
+
+]
+
+
+def random_insult():
+    return random.choice(insults)
+
+
+def long_insult():
+    return ' '.join([random.choice(adjectives), random.choice(insults)])
