836a9c866ed128eceb1a5eacf95d1539c3d7c4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:24 +0000

    Add package rapido/0.0.1/rapido-0.0.1.tar.gz

---
+++ packages/rapido/rapido-0.0.1.tar.gz/rapido-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from os import path
+
+with open("rapido/__version__.py", "r") as f:
+    exec(f.read())
+
+
+def read_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="rapido",
+    version=__version__,
+    author=__author__,
+    packages=find_packages(exclude=("tests")),
+    install_requires=[],
+    classifiers=["Programming Language :: Python :: 3.7"],
+)
