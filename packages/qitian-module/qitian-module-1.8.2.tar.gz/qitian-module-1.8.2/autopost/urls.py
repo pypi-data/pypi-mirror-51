0b14ef3612a3ae4ab58e34add94cfc86187e257e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:27 +0000

    Add package qitian-module/1.8.2/qitian-module-1.8.2.tar.gz

---
+++ packages/qitian-module/qitian-module-1.8.2.tar.gz/qitian-module-1.8.2/autopost/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import IndexView, PreviewDetail, PreviewList, CrawNewsView, CopyTaskView, ConvertDocxView
+
+app_name = 'autopost'
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('preview_list/', PreviewList.as_view(), name='preview_list'),
+    path('preview_detail/', PreviewDetail.as_view(), name='preview_detail'),
+    path('craw_news/<int:task_id>/', CrawNewsView.as_view(), name='craw_news'),
+    path('copy_task/<int:task_id>/', CopyTaskView.as_view(), name='copy_task'),
+    path('convert_docx/', ConvertDocxView.as_view(), name='convert_docx'),
+]
