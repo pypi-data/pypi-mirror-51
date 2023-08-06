696aa91d23fb36c6020096682a34dc2e3d5e45bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:53 +0000

    Add package afterglowpy/0.5.0/afterglowpy-0.5.0-cp37-cp37m-macosx_10_13_x86_64.whl

---
+++ packages/afterglowpy/afterglowpy-0.5.0-cp37-cp37m-macosx_10_13_x86_64.whl/afterglowpy/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Afterglowpy"""
+from . import shock
+from . import cocoon
+from . import jet
+from .flux import fluxDensity, intensity
+from .cocoon import (Hz2eV, Msun, c, cgs2mJy, day2sec, eV2Hz, ee, h, hbar,
+                     mJy2cgs, me, mp, parsec, sec2day, sigmaT)
+
+__all__ = [shock, cocoon, jet, fluxCalc, fluxDensity, intensity, Hz2eV, Msun,
+           c, cgs2mJy, day2sec, eV2Hz, ee, h, hbar, mJy2cgs, me, mp, parsec,
+           sec2day, sigmaT]
