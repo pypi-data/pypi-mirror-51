0a601c0c7a1eb23aa6cb434d5c252db527edf809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:45 +0000

    Add package social-ethosa/0.0.3/social-ethosa-0.0.3.tar.gz

---
+++ packages/social-ethosa/social-ethosa-0.0.3.tar.gz/social-ethosa-0.0.3/social_ethosa/utils.py
@@ -0,0 +1,8 @@
+def get_val(obj, string, returned=None):
+    return obj[string] if string in obj.keys() else returned
+
+def upl(file, name):
+    return { name : open(file, 'rb') }
+
+def upload_files(upload_url, file):
+    return requests.post(upload_url, files=file, verify=False).json()
