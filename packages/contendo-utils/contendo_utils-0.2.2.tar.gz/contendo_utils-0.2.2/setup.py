b1d99ff26a6ac5e8ab6ba06f4ccb32234ce76b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:20 +0000

    Add package contendo-utils/0.2.2/contendo_utils-0.2.2.tar.gz

---
+++ packages/contendo-utils/contendo_utils-0.2.2.tar.gz/contendo_utils-0.2.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='contendo_utils',
+      version='0.2.2',
+      description='General python and google-cloud related utilities',
+      url='https://github.com/sportsight-pro/sportsight-core/tree/master/contendo_utils',
+      author='Yahali Sherman, contendo.ai',
+      author_email='yahali@contendo.ai',
+      license='MIT',
+      packages=['contendo_utils'],
+      install_requires=[
+          'google-cloud-bigquery',
+          'google-cloud-storage',
+      ],
+      zip_safe=False)
