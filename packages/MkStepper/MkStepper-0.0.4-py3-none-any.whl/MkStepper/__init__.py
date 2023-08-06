4f3aa2162dd65b8381a72d923507b9f83facf1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:11 +0000

    Add package MkStepper/0.0.4/MkStepper-0.0.4-py3-none-any.whl

---
+++ packages/MkStepper/MkStepper-0.0.4-py3-none-any.whl/MkStepper/__init__.py
@@ -0,0 +1,106 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+class ULN2003:
+	def __init__(self, pins, speed):
+		"""
+		Stepper Motor Python Driver for ULN 2003 Driver
+		Raspberry Pi
+		Note: GPIO BCM Will Be Set
+		Note: add GPIO.cleanup() at the end of your scripts
+
+		ULN2003([10,12,13,14], 5)
+
+		pins:list - Location of pins according to your wiring
+
+		speed:int - 0-5 Speed of the stepper motor. 1 being the fastest
+
+		"""                
+		self.IN1=6 # IN1
+		self.IN2=13 # IN2
+		self.IN3=19 # IN3
+		self.IN4=26 # IN4
+		self.time = speed*0.001
+		GPIO.setup(self.IN1,GPIO.OUT)
+		GPIO.setup(self.IN2,GPIO.OUT)
+		GPIO.setup(self.IN3,GPIO.OUT)
+		GPIO.setup(self.IN4,GPIO.OUT)
+
+		GPIO.output(self.IN1, False)
+		GPIO.output(self.IN2, False)
+		GPIO.output(self.IN3, False)
+		GPIO.output(self.IN4, False)
+
+	def Step1(self):
+		GPIO.output(self.IN4, True)
+		sleep(self.time)
+		GPIO.output(self.IN4, False)
+
+	def Step2(self):
+		GPIO.output(self.IN4, True)
+		GPIO.output(self.IN3, True)
+		sleep(self.time)
+		GPIO.output(self.IN4, False)
+		GPIO.output(self.IN3, False)
+
+	def Step3(self):
+		GPIO.output(self.IN3, True)
+		sleep(self.time)
+		GPIO.output(self.IN3, False)
+
+	def Step4(self):
+		GPIO.output(self.IN2, True)
+		GPIO.output(self.IN3, True)
+		sleep(self.time)
+		GPIO.output(self.IN2, False)
+		GPIO.output(self.IN3, False)
+
+	def Step5(self):
+		GPIO.output(self.IN2, True)
+		sleep(self.time)
+		GPIO.output(self.IN2, False)
+
+	def Step6(self):
+		GPIO.output(self.IN1, True)
+		GPIO.output(self.IN2, True)
+		sleep(self.time)
+		GPIO.output(self.IN1, False)
+		GPIO.output(self.IN2, False)
+
+	def Step7(self):
+		GPIO.output(self.IN1, True)
+		sleep(self.time)
+		GPIO.output(self.IN1, False)
+
+	def Step8(self):
+		GPIO.output(self.IN4, True)
+		GPIO.output(self.IN1, True)
+		sleep(self.time)
+		GPIO.output(self.IN4, False)
+		GPIO.output(self.IN1, False)
+
+
+	def Left(self, steps): 
+		for i in range(steps):
+			self.Step1()
+			self.Step2()
+			self.Step3()
+			self.Step4()
+			self.Step5()
+			self.Step6()
+			self.Step7()
+			self.Step8()
+
+	def Right(self, steps):
+		for i in range(steps):
+			self.Step8()
+			self.Step7()
+			self.Step6()
+			self.Step5()
+			self.Step4()
+			self.Step3()
+			self.Step2()
+			self.Step1()
+
