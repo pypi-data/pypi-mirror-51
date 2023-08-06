c2f1268b98345b720e430fb3a3be3f08deb45acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:36 +0000

    Add package azure-kusto-ingest/0.0.33/azure_kusto_ingest-0.0.33-py2.py3-none-any.whl

---
+++ packages/azure-kusto-ingest/azure_kusto_ingest-0.0.33-py2.py3-none-any.whl/azure/kusto/ingest/exceptions.py
@@ -0,0 +1,25 @@
+"""All exceptions can be raised by KustoIngestClient."""
+
+from azure.kusto.data.exceptions import KustoClientError
+
+
+class KustoDuplicateMappingError(KustoClientError):
+    """
+    Error to be raised when ingestion properties has both
+    ingestion mappings and ingestion mapping reference.
+    """
+
+    def __init__(self):
+        message = "Ingestion properties contains ingestion mapping and ingestion mapping reference."
+        super(KustoDuplicateMappingError, self).__init__(message)
+
+
+class KustoMissingMappingReferenceError(KustoClientError):
+    """
+    Error to be raised when ingestion properties has data format of Json, SingleJson, MultiJson or Avro
+    but ingestion mappings reference was not defined.
+    """
+
+    def __init__(self):
+        message = "When stream format is json, mapping name must be provided."
+        super(KustoMissingMappingReferenceError, self).__init__(message)
