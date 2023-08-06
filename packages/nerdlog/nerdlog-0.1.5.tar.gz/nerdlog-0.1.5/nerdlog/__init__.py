0c7c74b2f7395880ba3b4d848d083156294e883e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:11 +0000

    Add package nerdlog/0.1.5/nerdlog-0.1.5.tar.gz

---
+++ packages/nerdlog/nerdlog-0.1.5.tar.gz/nerdlog-0.1.5/nerdlog/__init__.py
@@ -0,0 +1,10 @@
+__all__ = ['Logger']
+
+__version__ = '0.1.5'
+__copyright__ = 'Copyright 2019 Magnus Hirth'
+__author__ = 'Magnus Hirth'
+__author_email__ = 'mhmorgan42@gmail.com'
+__description__ = 'Simple logging library.'
+__license__ = 'Licensed under the BSD 3-clause "New" or "Revised" License'
+
+from nerdlog.logger import Logger
---
+++ packages/nerdlog/nerdlog-0.1.5.tar.gz/nerdlog-0.1.5/setup.py
@@ -0,0 +1,19 @@
+import nerdlog
+from setuptools import setup
+
+long_description = '''NerdLog is a simple logging library, see https://gitlab.com/mhmorgan/pynerdlog'''
+
+setup(
+    name='nerdlog',
+    version=nerdlog.__version__,
+    copyright=nerdlog.__copyright__,
+    author=nerdlog.__author__,
+    author_email=nerdlog.__author_email__,
+    description=nerdlog.__description__,
+    long_description=long_description,
+    license=nerdlog.__license__,
+    packages=['nerdlog'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ]
+)
