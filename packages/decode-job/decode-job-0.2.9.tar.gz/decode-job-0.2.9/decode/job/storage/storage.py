0833cafd4046a93acd6360f4d467e85a87ede07e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:13 +0000

    Add package decode-job/0.2.9/decode-job-0.2.9.tar.gz

---
+++ packages/decode-job/decode-job-0.2.9.tar.gz/decode-job-0.2.9/decode/job/storage/storage.py
@@ -0,0 +1,12 @@
+class Storage():
+    def delete(self, filepath, bucket_name=None):
+        raise NotImplementedError()
+
+    def read(self, filepath, bucket_name=None):
+        raise NotImplementedError()
+
+    def save_file(self, filename, key=None, bucket_name=None):
+        raise NotImplementedError()
+
+    def save(self, filename, contents, bucket_name=None):
+        raise NotImplementedError()
