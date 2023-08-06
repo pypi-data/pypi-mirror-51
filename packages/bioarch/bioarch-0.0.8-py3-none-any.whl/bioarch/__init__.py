a1147886c1929a409f051f940e3c98c4de1d9c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:02 +0000

    Add package bioarch/0.0.8/bioarch-0.0.8-py3-none-any.whl

---
+++ packages/bioarch/bioarch-0.0.8-py3-none-any.whl/bioarch/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+
+import sys
+
+
+from .age import AgeCategory, EstimatedAge
+from .individual import AgeSexStature, BurialInfo, Individual, LongBoneMeasurement, OsteologicalSex
+from .left_right import LeftRight
+from .mouth import Mouth, Tooth
+from .occupational_markers import EnthesialMarker, OccupationalMarkers
+from .sex import Sex
+
+
+if sys.version_info < (3, 7):
+    print('ERROR: Only Python 3.7 and above is supported')
+    sys.exit(-1)
+
+
+__title__ = 'bioarch'
+__version__ = '0.0.8'
+__author__ = 'Guy Taylor'
+
+__all__ = ['AgeCategory', 'EstimatedAge'] + ['AgeSexStature', 'BurialInfo', 'Individual', 'LongBoneMeasurement', 'OsteologicalSex'] + \
+          ['EnthesialMarker', 'OccupationalMarkers'] + ['LeftRight'] + ['Mouth', 'Tooth'] + ['Sex']
