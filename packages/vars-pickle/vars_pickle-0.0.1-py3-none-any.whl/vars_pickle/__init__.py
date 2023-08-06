c48ff6b4bbe41999f222f057e1e22645721e84d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:45 +0000

    Add package vars-pickle/0.0.1/vars_pickle-0.0.1-py3-none-any.whl

---
+++ packages/vars-pickle/vars_pickle-0.0.1-py3-none-any.whl/vars_pickle/__init__.py
@@ -0,0 +1,65 @@
+name = "vars_pickle"
+__version__ = "0.0.1"
+import __main__ as _main_module
+import pickle
+import datetime
+import os
+import numpy as np
+import pandas as pd
+type_list = ['str','function','pandas.core.frame.DataFrame','pandas.core.series.Series','list','dict','int','float','set','tuple']
+drop_list = ['In','Out','var_dic_list','get_ipython','']
+
+def dumps(name = '',path = 'data_pickle'):
+    """
+    默认保存到：./data_pickle/%Y-%m-%d/%H-%M-%S.pkl
+    name:自定义变量保存位置
+    path:保存数据的文件夹位置
+    """
+    if name == '':
+        path = os.path.join(path,datetime.datetime.now().strftime('%Y-%m-%d'))
+        if not os.path.exists(path):
+            os.makedirs(path)
+            print('创建'+path)
+        path = os.path.join(path,datetime.datetime.now().strftime('%H-%M-%S'))+'.pkl'
+    else:
+        path = name
+    pickl = []
+    for i in _main_module.__dict__.copy().items():
+        if i[0][0] != '_' and str(type(i[1])).split("'")[1] in type_list and i[0] not in drop_list:
+            pickl.append(i)
+    try:
+        pickl = tuple( pickl )
+        f = open(path,'wb')
+        pickle.dump(pickl,f)
+        f.close()
+        print("pickle:"+path)
+    except:
+          print('pickle ERROR')
+def loads(name = '',replace = False,path = 'data_pickle'):
+    """
+    默认读取：./data_pickle/%Y-%m-%d/%H-%M-%S.pkl中最新的文件
+    name:自定义变量读取位置
+    replace:是否替换已经定义的变量
+    path:读取数据的文件夹位置
+    """
+    try:
+        if name == '':
+            dir_list = [datetime.datetime.strptime(i,'%Y-%m-%d') for i in os.listdir(path)]
+            path = os.path.join(path,max(dir_list).strftime('%Y-%m-%d'))
+            dir_list = [datetime.datetime.strptime(i.split('.')[0],'%H-%M-%S') for i in os.listdir(path)]
+            path = os.path.join(path,max(dir_list).strftime('%H-%M-%S'))+'.pkl'
+        else:
+            path = name
+        setattr(_main_module,'np',np)
+        setattr(_main_module,'pd',pd)
+        f = open(path,'rb')
+        pickl = pickle.load(f)
+        for i in pickl:
+            if hasattr(_main_module,i[0]) and replace == False:
+                pass
+            else:
+                print(i[0])
+                setattr(_main_module,i[0],i[1])
+        print('laod:'+path)
+    except:
+        print('load ERROR')
