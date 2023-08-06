0b386091cad561f24a36e5fbe9ba33c849dff4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:36 +0000

    Add package markdown-timesheet/0.3/markdown_timesheet-0.3.tar.gz

---
+++ packages/markdown-timesheet/markdown_timesheet-0.3.tar.gz/markdown_timesheet-0.3/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+test_requirements = ["pytest"]
+
+setup(
+    author="Zev Averbach",
+    author_email="zev@averba.ch",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="A quick way to add up time entries in a markdown document.",
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="markdown timesheet",
+    name="markdown_timesheet",
+    packages=find_packages(include=["app"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    entry_points="""
+        [console_scripts]
+        add=app.cli:cli
+    """,
+    url="https://github.com/zevaverbach/markdown_timesheet",
+    version="0.03",
+    zip_safe=False,
+)
