cbb12b0c90e00d45746d62074507b8d2591e9cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:39 +0000

    Add package toai-mokahaiku/0.0.34/toai_mokahaiku-0.0.34-py3-none-any.whl

---
+++ packages/toai-mokahaiku/toai_mokahaiku-0.0.34-py3-none-any.whl/toai/image/__init__.py
@@ -0,0 +1,7 @@
+# pylama:ignore=W0611
+
+from .ImageAugmentor import ImageAugmentor
+from .ImageDataset import ImageDataset
+from .ImageLearner import ImageLearner
+from .ImageParser import ImageParser
+from .ImageResizer import ImageResizer
