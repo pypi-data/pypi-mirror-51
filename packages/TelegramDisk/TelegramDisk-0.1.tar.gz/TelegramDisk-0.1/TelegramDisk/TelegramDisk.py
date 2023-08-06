299477ca617e14a7e467f304ccfbbb5546ceb90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:10 +0000

    Add package TelegramDisk/0.1/TelegramDisk-0.1.tar.gz

---
+++ packages/TelegramDisk/TelegramDisk-0.1.tar.gz/TelegramDisk-0.1/TelegramDisk/TelegramDisk.py
@@ -0,0 +1,7 @@
+
+
+def hello(nonsense=False):
+    if not nonsense:
+        print('Hello! Module in development stage. Please check github: bit.ly/TelegramDisk')
+    else:
+        print('t.me/not_statilko')
---
+++ packages/TelegramDisk/TelegramDisk-0.1.tar.gz/TelegramDisk-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'TelegramDisk',
+  packages = ["TelegramDisk"],
+  version = '0.1',
+  license='MIT License',
+  description = 'asyncio Telegram disk based on telethon. [IN DEVELOPMENT]',
+  # long_description=open('README.md','rt').read(),
+  # long_description_content_type='text/markdown',
+  author = 'NonSense',
+  author_email = 'valerastatilko@gmail.com',
+  url = 'https://github.com/NotStatilko/TelegramDisk',
+  download_url = '',
+  keywords = ['Python', 'Telegram', 'Drive', 'Disk', 'Telethon', 'asyncio'],
+
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Other/Nonlisted Topic',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ]
+)
