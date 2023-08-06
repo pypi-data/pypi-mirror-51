fe27f8e4f750e9341d6db00fc7d99b872a7e54b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:06 +0000

    Add package python-ranges/0.1.2/python_ranges-0.1.2-py3-none-any.whl

---
+++ packages/python-ranges/python_ranges-0.1.2-py3-none-any.whl/ranges/__init__.py
@@ -0,0 +1,13 @@
+"""
+The `ranges` module contains the data structures Range, RangeSet,
+and RangeDict.
+
+See http://pypi.org/project/python-ranges for information.
+
+See http://github.com/superbird11/ranges for source code.
+"""
+
+from .ranges import Range, RangeSet, RangeDict
+
+__all__ = ["Range", "RangeSet", "RangeDict"]
+name = "ranges"
