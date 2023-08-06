05b6d0d49723f1ab10266405493fb58c17de96cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:23 +0000

    Add package mlvis/0.0.6/mlvis-0.0.6.tar.gz

---
+++ packages/mlvis/mlvis-0.0.6.tar.gz/mlvis-0.0.6/mlvis/__init__.py
@@ -0,0 +1,11 @@
+from .widget import Widget
+from .widget_builder import *
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'mlvis',
+        'require': 'mlvis/extension'
+    }]
