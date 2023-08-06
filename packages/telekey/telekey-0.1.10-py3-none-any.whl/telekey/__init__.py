0240fa6b227e997b65fd08f69333345f91510069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:57 +0000

    Add package telekey/0.1.10/telekey-0.1.10-py3-none-any.whl

---
+++ packages/telekey/telekey-0.1.10-py3-none-any.whl/telekey/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.10'
+
+from .decorators import *
+from .tools import *
+from .page import *
+from . import telekeyproject
---
+++ packages/telekey/telekey-0.1.10-py3-none-any.whl/telekey/telekeyproject/app/hanlders.py
@@ -0,0 +1,8 @@
+# you can read this docs for writing bot
+# handler reference : https://python-telegram-bot.readthedocs.io/en/stable/telegram.ext.html
+
+from telegram import ext
+from . import views
+
+HANDLERS = [
+]
