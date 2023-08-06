9d8da6c9d5c0be3d0321d351996cc17e59019abd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:57 +0000

    Add package django-better-admin-arrayfield/1.0.3/django-better-admin-arrayfield-1.0.3.tar.gz

---
+++ packages/django-better-admin-arrayfield/django-better-admin-arrayfield-1.0.3.tar.gz/django-better-admin-arrayfield-1.0.3/django_better_admin_arrayfield/static/js/django_better_admin_arrayfield.js
@@ -0,0 +1,32 @@
+window.addEventListener('load', function () {
+
+    function addRemoveEventListener(widgetElement) {
+        widgetElement.querySelectorAll('.remove').forEach(function (element) {
+            element.addEventListener('click', function () {
+                this.parentNode.remove();
+            });
+        });
+    }
+
+    document.querySelectorAll('.dynamic-array-widget').forEach(function (widgetElement) {
+
+        var inititalElement = widgetElement.querySelector('.array-item');
+        var elementTemplate = inititalElement.cloneNode(true);
+        var parentElement = inititalElement.parentElement;
+
+        addRemoveEventListener(widgetElement);
+
+        widgetElement.querySelector('.add-array-item').addEventListener('click', function () {
+            var newElement = elementTemplate.cloneNode(true);
+            var id_parts = newElement.querySelector('input').getAttribute('id').split('_');
+            var id = id_parts.slice(0, -1).join('_') + '_' + String(parseInt(id_parts.slice(-1)[0]) + 1);
+            newElement.querySelector('input').setAttribute('id', id);
+            newElement.querySelector('input').value = '';
+
+            addRemoveEventListener(newElement);
+            parentElement.appendChild(newElement);
+        });
+
+    });
+
+});
