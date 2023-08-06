18bf55b8494a0c53e848a45e1f9c1706c8bfa56f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:42 +0000

    Add package fsm_eigenvalue/1.0.1/fsm_eigenvalue-1.0.1.tar.gz

---
+++ packages/fsm_eigenvalue/fsm_eigenvalue-1.0.1.tar.gz/fsm_eigenvalue-1.0.1/fsm_eigenvalue/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+
+__version__ = '1.0.1'
+
+
+ASTIFF_BLOCK_SIZE = 4
+BASE_CACHE_DIR = os.path.expanduser('~/.cache/fsm_eigenvalue')
+DEFAULT_PAGINATE_BY = 50
---
+++ packages/fsm_eigenvalue/fsm_eigenvalue-1.0.1.tar.gz/fsm_eigenvalue-1.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fsm_eigenvalue',
+    version='1.0.1',
+    url='https://github.com/petarmaric/fsm_eigenvalue',
+    license='BSD',
+    author='Petar Maric',
+    author_email='petarmaric@uns.ac.rs',
+    description='Console app and Python API implementing a generalization of '\
+                'eigenvalue problem within the harmonic coupled finite strip '\
+                'method, used for parametric modeling of static and dynamic '\
+                'inelastic buckling, free vibration, damage and failure in '\
+                'prismatic shell structures.',
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': ['fsm_eigenvalue=fsm_eigenvalue.shell:main'],
+    },
+    install_requires=open('requirements.txt').read().splitlines(),
+)
