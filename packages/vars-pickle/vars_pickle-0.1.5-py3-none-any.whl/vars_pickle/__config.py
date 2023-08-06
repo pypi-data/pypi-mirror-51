93164242a4ffe7448a60db5a6375d38cc78bb0f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:59 +0000

    Add package vars-pickle/0.1.5/vars_pickle-0.1.5-py3-none-any.whl

---
+++ packages/vars-pickle/vars_pickle-0.1.5-py3-none-any.whl/vars_pickle/__config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 08:27:35 2019
+
+@author: 10167232
+"""
+path = 'data_pickle'
+type_list = ['str','function','pandas.core.frame.DataFrame','pandas.core.series.Series','list','dict','int','float','set','tuple']
+drop_list = ['In','Out','var_dic_list','get_ipython','']
