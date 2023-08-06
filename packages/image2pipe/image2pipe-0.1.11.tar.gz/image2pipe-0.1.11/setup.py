8797305e385b9783313add786aefd260cac28aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:22 +0000

    Add package image2pipe/0.1.11/image2pipe-0.1.11.tar.gz

---
+++ packages/image2pipe/image2pipe-0.1.11.tar.gz/image2pipe-0.1.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='image2pipe',
+    version='0.1.11',
+    author='Anton P. Linevich',
+    author_email='anton@linevich.com',
+    keywords="ffmpeg yuv image2pipe",
+    packages=['image2pipe', ],
+    scripts=[],
+    url='https://github.com/chexov/image2pipe',
+    license='LICENSE.txt',
+    description='Simple ffmpeg wrapper for image2pipe which yields rawvideo frames from input video URL',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['tblib', 'numpy', 'websocket'],
+    python_requires='>=3.2',
+)
