a80e6457963cee895c43f85f98f2546051deed28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:45 +0000

    Add package fststr/0.1/fststr-0.1.tar.gz

---
+++ packages/fststr/fststr-0.1.tar.gz/fststr-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='fststr',
+    version='0.1',
+    packages=['fststr',],
+    license='MIT license',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
