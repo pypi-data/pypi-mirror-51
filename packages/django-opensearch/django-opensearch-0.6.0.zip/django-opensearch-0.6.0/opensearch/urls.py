d844aa3a2ff12b3792d6e87c027ae2f1dfa1d035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:17 +0000

    Add package django-opensearch/0.6.0/django-opensearch-0.6.0.zip

---
+++ packages/django-opensearch/django-opensearch-0.6.0.zip/django-opensearch-0.6.0/opensearch/urls.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# django-opensearch
+# opensearch/urls.py
+
+
+from typing import List, Union
+
+from django.conf.urls import url
+from django.urls.resolvers import URLPattern, URLResolver
+
+from opensearch.views import opensearch
+
+
+__all__ = ["urlpatterns"]  # type: List[str]
+
+
+# opensearch urls
+urlpatterns = [
+    url(r"^opensearch\.xml$", opensearch, name="opensearch")  # opensearch
+]  # type: List[Union[URLPattern, URLResolver]]
