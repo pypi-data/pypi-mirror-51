6bf673c34aebfceeb2bcfdd105a36311336dc3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:48 +0000

    Add package inscribe/1.0.7/inscribe-1.0.7.tar.gz

---
+++ packages/inscribe/inscribe-1.0.7.tar.gz/inscribe-1.0.7/inscribe/config.py
@@ -0,0 +1,26 @@
+config = {
+  'production': {
+    'api': {
+      'url': 'https://app.inscribe.ai',
+      'pusher_key': '34737e9c65723ff9a1c4'
+    }
+  },
+  'staging': {
+    'api': {
+      'url': 'https://staging.inscribe.ai',
+      'pusher_key': 'c05efca39516e9defad3'
+    }
+  },
+  'development': {
+    'api': {
+      'url': 'http://127.0.0.1:8001',
+      'pusher_key': '4fbd8ececbefd9b717b1'
+    }
+  },
+  'test': {
+    'api': {
+      'url': 'http://127.0.0.1:8001',
+      'pusher_key': '9ea2a8905b7f1a63f8bc'
+    }
+  }
+}
