6e6560ca776d8e420e466a79f050e8c3b99b0692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:14 +0000

    Add package pltCIreg/0.0.2/pltCIreg-0.0.2.tar.gz

---
+++ packages/pltCIreg/pltCIreg-0.0.2.tar.gz/pltCIreg-0.0.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+setup(
+    name='pltCIreg',
+    version='0.0.2',
+    author='Aldrich_1',
+    author_email='zhengz3000@gmail.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'吃枣药丸',
+    packages=['pltCIreg'],
+    install_requires=[],
+)
