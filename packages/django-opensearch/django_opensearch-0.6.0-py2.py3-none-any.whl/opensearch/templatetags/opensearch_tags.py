7efd5e84da18f540942bcd0a768fa91142f07edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:17 +0000

    Add package django-opensearch/0.6.0/django_opensearch-0.6.0-py2.py3-none-any.whl

---
+++ packages/django-opensearch/django_opensearch-0.6.0-py2.py3-none-any.whl/opensearch/templatetags/opensearch_tags.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# django-opensearch
+# opensearch/templatetags/opensearch_tags.py
+
+
+from typing import Dict, List
+
+from django import template
+
+from opensearch.conf import settings
+
+
+__all__ = ["opensearch_meta"]  # type: List[str]
+
+
+register = template.Library()
+
+
+@register.inclusion_tag("opensearch/templatetags/opensearch_meta.html")
+def opensearch_meta() -> Dict[str, str]:
+    """
+    Return meta rel opensearch tag.
+
+    :return: opensearch description.
+    :rtype: dDict[str, str].
+    """
+
+    return {"OPENSEARCH_DESCRIPTION": settings.OPENSEARCH_DESCRIPTION}
