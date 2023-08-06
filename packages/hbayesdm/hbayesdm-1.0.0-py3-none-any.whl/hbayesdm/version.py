bf22f2856b83abc1edf7b5cc977a20feaeae548b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:10 +0000

    Add package hbayesdm/1.0.0/hbayesdm-1.0.0-py3-none-any.whl

---
+++ packages/hbayesdm/hbayesdm-1.0.0-py3-none-any.whl/hbayesdm/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM HBAYESDM SETUP.PY
+short_version = '0.7.2'
+version = '0.7.2'
+full_version = '0.7.2.dev0+Unknown'
+git_revision = 'Unknown'
+release = False
+if not release:
+    version = full_version
