94eceea206ac929ee1864a8519b127e0a311c86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:29 +0000

    Add package viaenv/0.2.1/viaenv-0.2.1.tar.gz

---
+++ packages/viaenv/viaenv-0.2.1.tar.gz/viaenv-0.2.1/README.md
@@ -0,0 +1,47 @@
+# viaenv - Configuration via Environment for Python
+
+[![CircleCI](https://circleci.com/gh/tebeka/viaenv.svg?style=svg)](https://circleci.com/gh/tebeka/viaenv)
+[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+
+
+`viaenv` uses [variable annotation](https://www.python.org/dev/peps/pep-0526/)
+to populate values from environment variables.
+
+## Example
+
+```python
+from datetime import timedelta
+from viaenv import populate_from_env
+
+
+class config:
+    port: int = 8080
+    log_file: str = '/var/log/server.log'
+    timeout: timedelta = timedelta(milliseconds=100)
+
+
+populate_from_env(
+    config, prefix='SRV',
+    # If you don't pass env, populate_from_env will use os.environ
+    env={'SRV_PORT': '9000', 'SRV_TIMEOUT': '300ms'})
+
+print(config.port)  # 9000
+print(cfg.timeout == timedelta(milliseconds=300)  # True
+```
+
+## Supported Types
+
+- `bool`: `y`, `yes`, `t`, `true`, `on`, `1` -> `True`, `n`, `no`, `f`, `false`, `off`, `0` -> `False`
+- `date`: `2019-05-18`
+- `datetime`: `2019-05-18T13:43:12`
+- `dict`: `{"x": 1, "y": 2}` (JSON format)
+- `float`: `1.3`, `1e7`
+- `int`: `12`, `0x12`, `0o12`, `0b12`
+- `list`: `[1, 2, 3]` (JSON format)
+- `str`: Anything goes
+- `time`: `13:43:12`
+- `timedelta`: `10us`, `20ms`, `30s`, `17m`, `2h`, `7d`, `1h20m`
+
+You can add your types by calling `register_type_parser(typ, parser)` where
+`typ` is a type (e.g. `float`) and `parser` is a one argument function that
+will get the value as a string.
---
+++ packages/viaenv/viaenv-0.2.1.tar.gz/viaenv-0.2.1/viaenv.py
@@ -0,0 +1,147 @@
+"""Configuration via environment"""
+import json
+import re
+from datetime import date, datetime, time, timedelta
+from distutils.util import strtobool
+from os import environ
+
+__version__ = '0.2.1'
+__all__ = ['populate_from_env', 'register_type_parser']
+
+
+def populate_from_env(obj, prefix='', env=None):
+    """Populate values in obj from environment. obj should have
+    __annotations__ attribute.
+
+    >>> from datetime import timedelta
+    >>> from viaenv import populate_from_env
+    >>> class config:
+    ...     port: int = 8080
+    ...     log_file: str = '/var/log/server.log'
+    ...     timeout: timedelta = timedelta(milliseconds=100)
+    ...
+    >>> populate_from_env(
+    ...     config, prefix='SRV',
+    ...     # If you don't pass env, populate_from_env will use os.environ
+    ...     env={'SRV_PORT': '9000', 'SRV_TIMEOUT': '300ms'})
+    >>> config.port
+    9000
+    >>> config.timeout == timedelta(milliseconds=300)
+    True
+    """
+    if not hasattr(obj, '__annotations__'):
+        raise ValueError(f'no type annotation in {obj!r} of type')
+
+    env = environ if env is None else env
+    cfg = {}  # We first populate cfg, and only if all succeed update obj
+
+    if prefix:
+        prefix = prefix + '_'
+
+    for name, typ in obj.__annotations__.items():
+        env_name = prefix + name.upper()
+        value = env.get(env_name)
+        if value is None:
+            continue
+
+        parser = find_parser(typ)
+        cfg[name] = parser(value)
+
+    for name, value in cfg.items():
+        setattr(obj, name, value)
+
+
+_type_parsers = []
+
+
+def register_type_parser(typ, parser):
+    """Register a parser for type "typ", "parser" should be a function that
+    gets one string value and returned parsed type.
+    """
+    # TODO: warn on duplicates
+    _type_parsers.insert(0, (typ, parser))
+
+
+def find_parser(typ):
+    # Try exact type first
+    for cls, func in _type_parsers:
+        if cls == typ:
+            return func
+
+    # Try subclass
+    for cls, func in _type_parsers:
+        if issubclass(typ, cls):
+            return func
+    return typ
+
+
+def type_parser(typ):
+    def wrapper(func):
+        register_type_parser(typ, func)
+        return func
+    return wrapper
+
+
+@type_parser(int)
+def parse_int(value):
+    return int(value, 0)  # Allow 10, 0x10, 0o10, 0b10
+
+
+_second = int(1e6)
+_time_units = {
+    'us': 1,
+    'ms': 1_000,
+    's': _second,
+    'm': 60 * _second,
+    'h': 60 * 60 * _second,
+    'd': 24 * 60 * 60 * _second,
+}
+
+
+@type_parser(timedelta)
+def parse_timedelta(value):
+    us = 0
+    # 10m30s
+    for amount, unit in re.findall(r'([0-9]+)([a-zA-Z]+)', value):
+        n = _time_units.get(unit.lower())
+        if n is None:
+            raise ValueError(f'unknown time unit {unit!r} in {value!r}')
+        us += n * int(amount)
+    return timedelta(microseconds=us)
+
+
+@type_parser(list)
+def parse_list(value):
+    lval = json.loads(value)
+    if not isinstance(lval, list):
+        raise TypeError(f'{value!r} is not a list')
+    return lval
+
+
+@type_parser(dict)
+def parse_dict(value):
+    dval = json.loads(value)
+    if not isinstance(dval, dict):
+        raise TypeError(f'{value!r} is not a dict')
+    return dval
+
+
+@type_parser(bool)
+def parse_bool(value):
+    return bool(strtobool(value))
+
+
+# TODO: Allow adding/changing time formats
+@type_parser(date)
+def parse_date(value):
+    return datetime.strptime(value, '%Y-%m-%d').date()
+
+
+@type_parser(time)
+def parse_time(value):
+    return datetime.strptime(value, '%H:%M:%S').time()
+
+
+@type_parser(datetime)
+def parse_datetime(value):
+    return datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
