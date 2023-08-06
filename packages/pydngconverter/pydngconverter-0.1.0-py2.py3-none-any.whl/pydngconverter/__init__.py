708ace90c6e421e4fa525213875b380a624309fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:10 +0000

    Add package pydngconverter/0.1.0/pydngconverter-0.1.0-py2.py3-none-any.whl

---
+++ packages/pydngconverter/pydngconverter-0.1.0-py2.py3-none-any.whl/pydngconverter/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+PyDngConverter
+====================================
+A Python Interface to Adobe's DNG Converter.
+
+Uses multiprocessing and Image manipulation to significantly
+speed up the process of convering RAW images to DNG.
+"""
+
+__author__ = """Braden Mars"""
+__version__ = '0.1.0'
+
+from pydngconverter.main import (Compression, CRawCompat, DNGConverter,
+                                 DNGVersion, JPEGPreview)
+
+__all__ = ['DNGConverter', 'CRawCompat',
+           'DNGVersion', 'JPEGPreview', 'Compression']
