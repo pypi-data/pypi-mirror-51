04046f09353be553a1ad54016bc4d8db20ea3361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:14 +0000

    Add package zesty-poker/1.10.0/zesty_poker-1.10.0.tar.gz

---
+++ packages/zesty-poker/zesty_poker-1.10.0.tar.gz/zesty_poker-1.10.0/setup.py
@@ -0,0 +1,25 @@
+"""
+zesty_poker: A pure Python poker hand evaluation library
+"""
+
+from setuptools import setup
+
+setup(
+    name='zesty_poker',
+    version='1.10.0',
+    description='zesty_poker is a pure Python poker hand evaluation library',
+    long_description='none',
+    author='Kenny Chau (forked from treys)',
+    url='https://github.com/kennychau9/zesty_poker',
+    license='MIT',
+    packages=['zesty_poker'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Games/Entertainment'
+    ]
+)
