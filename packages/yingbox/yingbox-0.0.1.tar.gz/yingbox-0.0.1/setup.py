9cce581cce93baddad4ba51003ea0f00c5c4ddcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:04 +0000

    Add package yingbox/0.0.1/yingbox-0.0.1.tar.gz

---
+++ packages/yingbox/yingbox-0.0.1.tar.gz/yingbox-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yingbox",
+    version="0.0.1",
+    author="Ying Zhou",
+    author_email="yingzhou474@gmail.com",
+    description="Ying's data science helper functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mathyingzhou/yingbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
