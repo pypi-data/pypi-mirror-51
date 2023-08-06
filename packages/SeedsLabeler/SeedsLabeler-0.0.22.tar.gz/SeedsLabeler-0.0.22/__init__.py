86ecc8f91041c66512bca8d64a62fcf86b4cf76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:47 +0000

    Add package SeedsLabeler/0.0.22/SeedsLabeler-0.0.22.tar.gz

---
+++ packages/SeedsLabeler/SeedsLabeler-0.0.22.tar.gz/SeedsLabeler-0.0.22/__init__.py
@@ -0,0 +1,4 @@
+__authors__ = "Merey Ramazanova, Silvio Giancola"
+__authors_username__ = "ramazam, giancos"
+__author_email__ = "merey.ramazanova@kaust.edu.sa"
+__github__ = 'https://github.com/meryusha/seeds_labeler/'
