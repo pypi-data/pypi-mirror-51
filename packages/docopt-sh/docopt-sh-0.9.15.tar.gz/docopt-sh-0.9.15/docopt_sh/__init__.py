ef466c352b0122119efa4381c09f510aec8ec9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:52 +0000

    Add package docopt-sh/0.9.15/docopt-sh-0.9.15.tar.gz

---
+++ packages/docopt-sh/docopt-sh-0.9.15.tar.gz/docopt-sh-0.9.15/docopt_sh/__init__.py
@@ -0,0 +1,16 @@
+"""
+docopt.sh - Bash argument parser generator.
+  This program looks for `DOC="... Usage​: ..."`  in a script
+  and appends a matching parser to it.
+"""
+# The "... Usage: ..." above contains a zero-width space between `Usage` and `:`
+# in order to prevent docopt from parsing it as a `usage:`` section
+
+__all__ = ['docopt_sh']
+__version__ = '0.9.15'
+
+
+class DocoptError(Exception):
+  def __init__(self, message, exit_code=1):
+    self.message = message
+    self.exit_code = exit_code
