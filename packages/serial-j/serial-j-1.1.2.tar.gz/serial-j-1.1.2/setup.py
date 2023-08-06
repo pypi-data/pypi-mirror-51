19701012e12bb05a7c4d5bfede1b5320c2289d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:10 +0000

    Add package serial-j/1.1.2/serial-j-1.1.2.tar.gz

---
+++ packages/serial-j/serial-j-1.1.2.tar.gz/serial-j-1.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("Pypi.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='serial-j',
+    version='1.1.2',
+    author='Junpu Fan',
+    author_email='junpufan@me.com ',
+    description='Validating and Serializing JSON data into Python object with minimal effort.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/JunpuFan/serial-j',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
