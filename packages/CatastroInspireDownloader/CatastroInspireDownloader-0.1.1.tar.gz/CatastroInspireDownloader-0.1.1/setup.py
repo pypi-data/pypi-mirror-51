4b29d96d8419a1301b9979a6dd301394138b04e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:22 +0000

    Add package CatastroInspireDownloader/0.1.1/CatastroInspireDownloader-0.1.1.tar.gz

---
+++ packages/CatastroInspireDownloader/CatastroInspireDownloader-0.1.1.tar.gz/CatastroInspireDownloader-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CatastroInspireDownloader',
+    version='0.1.1',
+    install_requires=[
+        'Click==7.0',
+        'atoma',
+        'requests==2.22.0',
+    ],
+    author="Francisco P. Sampayo",
+    author_email="francisco.perez@geomati.co",
+    description="Herramienta para descargar datos Inspire del Catastro de España a través de Atom.",
+    packages=find_packages(include=['cidownloader']),
+    entry_points='''
+        [console_scripts]
+        cidownloader=cidownloader.cli:cli
+    ''',
+    license='GPLv3',
+)
