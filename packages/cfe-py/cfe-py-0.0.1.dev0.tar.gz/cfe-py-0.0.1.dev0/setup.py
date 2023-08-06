16767197654449dd146a9f0eb3c76d4aa0b129da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:59 +0000

    Add package cfe-py/0.0.1.dev0/cfe-py-0.0.1.dev0.tar.gz

---
+++ packages/cfe-py/cfe-py-0.0.1.dev0.tar.gz/cfe-py-0.0.1.dev0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="cfe-py",
+    version="0.0.1.dev0",
+    author="Diogo Souza da Silva",
+    author_email="diogo@diogok.net",
+    description="A teste",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="https://github.com/diogok/cfe",
+    packages=["cfe-py"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
