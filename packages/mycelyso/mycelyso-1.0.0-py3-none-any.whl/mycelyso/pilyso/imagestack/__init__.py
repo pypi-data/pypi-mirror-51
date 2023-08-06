fe5efa4cb83d039e64a997f4a1e848d1f30a7d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:45 +0000

    Add package mycelyso/1.0.0/mycelyso-1.0.0-py3-none-any.whl

---
+++ packages/mycelyso/mycelyso-1.0.0-py3-none-any.whl/mycelyso/pilyso/imagestack/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+The imagestack module contains the base functionality for nD bio image opening with metadata support.
+"""
+
+from .imagestack import ImageStack, Dimensions, FloatFilter, MinMaxFilter, UnwrapFilter
+
