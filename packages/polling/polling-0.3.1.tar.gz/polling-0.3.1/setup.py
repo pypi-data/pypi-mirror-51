aa32c25f323331bf63d3f071f3e059671394be3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:24 +0000

    Add package polling/0.3.1/polling-0.3.1.tar.gz

---
+++ packages/polling/polling-0.3.1.tar.gz/polling-0.3.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from polling import __version__
+
+
+setup(
+    name='polling',
+    description='Powerful polling utility with many configurable options',
+    version=__version__,
+    author='Justin Iso',
+    author_email='justin+polling@justiniso.com',
+    url='http://github.com/justiniso/polling',
+    license='MIT',
+    download_url='',
+    py_modules=['polling'],
+    tests_require=[
+        'mock==3.0.5',
+        'pytest==5.1.1'
+    ],
+    test_suite='tests'
+)
