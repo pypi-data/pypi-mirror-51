a3fafa69e8503b6b0ba487acc65a56a3ee2d9efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:30 +0000

    Add package docker-hub/2.1.0/docker_hub-2.1.0-py2.py3-none-any.whl

---
+++ packages/docker-hub/docker_hub-2.1.0-py2.py3-none-any.whl/src/libs/docker_client.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+import json
+from os import path
+from ..consts import *
+
+
+class DockerClient:
+    """ A wrapper for communication with docker engine """
+    def __init__(self):
+        """ We load the docker engine config file when intialized """
+        self.docker_config_data = {}
+        config_json_file = path.expanduser(DOCKER_AUTH_FILE)
+        if path.isfile(config_json_file):
+            with open(config_json_file) as data_file:
+                self.docker_config_data = json.load(data_file)
+
+    def get_auth_token(self):
+        """ Just get the auth token from docker config file """
+        if 'auths' in self.docker_config_data:
+            for auth in self.docker_config_data['auths']:
+                if 'auth' in self.docker_config_data['auths'][auth]:
+                    return self.docker_config_data['auths'][auth]['auth']
+
+        return False
