43d226f8a1e9e136693ac8ffa48618d9b4824e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:46 +0000

    Add package sspo-client/1.0.0/sspo_client-1.0.0-py3-none-any.whl

---
+++ packages/sspo-client/sspo_client-1.0.0-py3-none-any.whl/sspo_client/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from sspo_client.api.scrum_atomic_projects_api import ScrumAtomicProjectsApi
+from sspo_client.api.scrum_complex_projects_api import ScrumComplexProjectsApi
+from sspo_client.api.scrum_projects_api import ScrumProjectsApi
