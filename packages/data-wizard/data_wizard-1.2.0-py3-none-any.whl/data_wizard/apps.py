771b7181b1cc7b1d5d0a237656c96cf7f39324a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:09 +0000

    Add package data-wizard/1.2.0/data_wizard-1.2.0-py3-none-any.whl

---
+++ packages/data-wizard/data_wizard-1.2.0-py3-none-any.whl/data_wizard/apps.py
@@ -0,0 +1,28 @@
+from django.apps import AppConfig
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+
+
+class WizardConfig(AppConfig):
+    name = 'data_wizard'
+    verbose_name = 'Data Wizard'
+
+    def ready(self):
+        from .compat import reverse
+
+        self.module.autodiscover()
+        self.module.init_backend()
+
+        if 'wq.db.rest' in settings.INSTALLED_APPS:
+            return
+
+        # FIXME: Drop this check in 2.0
+        try:
+            base_url = reverse('data_wizard:run-list')
+        except Exception:
+            pass
+        else:
+            if base_url == '/':
+                raise ImproperlyConfigured(
+                    "data_wizard.urls at /, add 'datawizard/' prefix"
+                )
