b197170e2d598b31c1c2b2e65bba1cfc02551caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:20 +0000

    Add package rftoc/0.0.3/rftoc-0.0.3.tar.gz

---
+++ packages/rftoc/rftoc-0.0.3.tar.gz/rftoc-0.0.3/test_cont.py
@@ -0,0 +1,50 @@
+import pytest
+from rftoc import rftoc
+
+
+@pytest.mark.parametrize(
+    'arg',
+    [
+        ('name: someslide\n# Foo'.split('\n'), '* [Foo](#someslide)\n'),
+        ('name: someslide\n# Foo-Bar'.split('\n'), '* [Foo-Bar](#someslide)\n'),
+        ('name: someslide\n# Foo-Bar `Baz`'.split('\n'), '* [Foo-Bar `Baz`](#someslide)\n'),
+        ('name: someslide\n## Foo'.split('\n'), '    + [Foo](#someslide)\n'),
+        ('name: someslide\n### Foo'.split('\n'), '        - [Foo](#someslide)\n'),
+        ('name: someslide\n#### Foo'.split('\n'), '            * [Foo](#someslide)\n'),
+        (
+
+'''
+name: first
+# foo
+
+name: second
+## bar
+'''.split('\n')
+,
+
+'''\
+* [foo](#first)
+    + [bar](#second)
+'''
+        ),
+    ],
+)
+def test_foo(arg):
+    src, expected = arg
+
+    toc = rftoc(src)
+    assert toc == expected
+
+
+@pytest.mark.parametrize(
+    'arg',
+    [
+        ('name: someslide\n### Foo'.split('\n'), '        - [Foo](#someslide)\n'),
+        ('name: someslide\n#### Foo'.split('\n'), ''),
+    ]
+)
+def test_bar(arg):
+    src, expected = arg
+
+    toc = rftoc(src, max_level=3)
+    assert toc == expected
