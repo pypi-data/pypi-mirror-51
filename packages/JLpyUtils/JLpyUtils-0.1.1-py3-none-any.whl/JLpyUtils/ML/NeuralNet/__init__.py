b4f45c51796707766ba0ed1c543a2383c712016a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:12 +0000

    Add package JLpyUtils/0.1.1/JLpyUtils-0.1.1-py3-none-any.whl

---
+++ packages/JLpyUtils/JLpyUtils-0.1.1-py3-none-any.whl/JLpyUtils/ML/NeuralNet/__init__.py
@@ -0,0 +1,12 @@
+
+import warnings as __warnings__
+
+__warnings__.filterwarnings('ignore')
+import tensorflow as __tf__
+__warnings__.filterwarnings('default')
+
+from . import Conv2D
+from . import plot
+from . import search
+from . import DenseNet
+from . import utils
