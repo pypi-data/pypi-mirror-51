5aaa68b34575069a0e7a4cfd801cd55bc5a87607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:55 +0000

    Add package bee-django-coin/0.1.40/bee-django-coin-0.1.40.tar.gz

---
+++ packages/bee-django-coin/bee-django-coin-0.1.40.tar.gz/bee-django-coin-0.1.40/bee_django_coin/forms.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'bee'
+
+from django import forms
+from django.conf import settings
+from .models import CoinType, UserCoinRecord, Item
+
+
+# ===== course contract======
+class CoinTypeForm(forms.ModelForm):
+    class Meta:
+        model = CoinType
+        fields = ['name', "identity", "coin", "info"]
+
+
+class UserCoinRecordForm(forms.ModelForm):
+    class Meta:
+        model = UserCoinRecord
+        fields = ["coin", "reason", 'coin_type']
+
+
+class OtherCoinCreateForm(forms.ModelForm):
+    user = forms.ModelChoiceField(label='选择学生', required=True, queryset=None)
+    coin = forms.IntegerField(label='数量', required=True, min_value=1)
+    reason = forms.CharField(label='原因', required=False)
+
+    class Meta:
+        model = UserCoinRecord
+        fields = ["user", "coin", "reason"]
+
+    def __init__(self, users, *args, **kwargs):
+        super(OtherCoinCreateForm, self).__init__(*args, **kwargs)
+        self.fields["user"] = forms.ModelChoiceField(queryset=users, label='选择学生', required=True)
+
+#
+class ItemForm(forms.ModelForm):
+    ITEM_TYPE_CHOICES = ((2, "抵用券"),)
+    coin = forms.IntegerField(label='商品价格',help_text='购买所需'+settings.COIN_NAME,required=True,min_value=1)
+    item_type = forms.ChoiceField(choices=ITEM_TYPE_CHOICES, label='商品类型')
+    # stauts=forms.ChoiceField(label='商品状态',choices=ITEM_STATUS)
+
+    class Meta:
+        model = Item
+        fields = ["name","url", "coin", 'pic', 'item_type', 'status', "info"]
