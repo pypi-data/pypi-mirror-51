4fbdad5056c12e7ef2ebed37d14ec28bff13917b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:45 +0000

    Add package accept-types/0.4.1/accept_types-0.4.1-py3-none-any.whl

---
+++ packages/accept-types/accept_types-0.4.1-py3-none-any.whl/accept_types/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import, unicode_literals
+
+from .accept_types import get_best_match, parse_header, AcceptableType
+
+__all__ = ['get_best_match', 'parse_header', 'AcceptableType']
