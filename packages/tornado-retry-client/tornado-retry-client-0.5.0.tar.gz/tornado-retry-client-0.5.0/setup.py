93da6a1695d27e8b8c1c67ddd02f5cd3d4b3a6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:35 +0000

    Add package tornado-retry-client/0.5.0/tornado-retry-client-0.5.0.tar.gz

---
+++ packages/tornado-retry-client/tornado-retry-client-0.5.0.tar.gz/tornado-retry-client-0.5.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='tornado-retry-client',
+    version='0.5.0',
+    description='Simple retry tornado http client',
+    long_description='',
+    classifiers=[],
+    keywords='tornado retry client',
+    author='Wilson Júnior',
+    author_email='wilsonpjunior@gmail.com',
+    url='https://github.com/wpjunior/tornado-retry-client',
+    license='MIT',
+    include_package_data=True,
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    platforms=['any'],
+    install_requires=[
+        'tornado==5.1.1',
+    ],
+    test_suite='nose.collector',
+    extras_require={
+        'tests': [
+            'mock',
+            'nose',
+            'coverage',
+            'flake8',
+            'yanc',
+            'bumpversion',
+        ]
+    }
+)
