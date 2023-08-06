b6cf714bbb3e967bc9b0f6c01d38df1c222c1c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:32 +0000

    Add package weather-application5/0.3/weather_application5-0.3.tar.gz

---
+++ packages/weather-application5/weather_application5-0.3.tar.gz/weather_application5-0.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+     name='weather_application5',
+     version='0.3',
+     scripts=['weather_info_nitr'] ,
+     author="Biswajeet_Sahoo",
+     author_email="biswajeetsahoo54@gmail.com",
+ )
+
+
