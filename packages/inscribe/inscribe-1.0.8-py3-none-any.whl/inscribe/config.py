ecf9be45d2cc30ceee38dd736cc192d27f30807e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:06 +0000

    Add package inscribe/1.0.8/inscribe-1.0.8-py3-none-any.whl

---
+++ packages/inscribe/inscribe-1.0.8-py3-none-any.whl/inscribe/config.py
@@ -0,0 +1,26 @@
+config = {
+  'production': {
+    'api': {
+      'url': 'https://app.inscribe.ai',
+      'pusher_key': '49b7f1ae16f046aa189e'
+    }
+  },
+  'staging': {
+    'api': {
+      'url': 'https://staging.inscribe.ai',
+      'pusher_key': '607749bd12253359acb4'
+    }
+  },
+  'development': {
+    'api': {
+      'url': 'http://127.0.0.1:8001',
+      'pusher_key': '4fbd8ececbefd9b717b1'
+    }
+  },
+  'test': {
+    'api': {
+      'url': 'http://127.0.0.1:8001',
+      'pusher_key': '9ea2a8905b7f1a63f8bc'
+    }
+  }
+}
