a78823481df73f5b3c45a01bcecdc0434bae7143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:29 +0000

    Add package gaiaclient/0.0.15/gaiaclient-0.0.15.tar.gz

---
+++ packages/gaiaclient/gaiaclient-0.0.15.tar.gz/gaiaclient-0.0.15/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gaiaclient",
+    version="0.0.15",
+    license='MIT License',
+    author="JOT Automation Ltd.",
+    author_email="rami.rahikkala@jotautomation.com",
+    description="Client for JOT Automation gaia machines",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jotautomation/gaiapythonclient",
+    py_modules=['gaiaclient'],
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
