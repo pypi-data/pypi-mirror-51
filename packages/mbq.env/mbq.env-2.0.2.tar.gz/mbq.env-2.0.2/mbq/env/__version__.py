6882738683b521a39b5c065f4869cdbdb4d5ce2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:17 +0000

    Add package mbq.env/2.0.2/mbq.env-2.0.2.tar.gz

---
+++ packages/mbq.env/mbq.env-2.0.2.tar.gz/mbq.env-2.0.2/mbq/env/__version__.py
@@ -0,0 +1,7 @@
+__author__ = 'Managed by Q, Inc.'
+__author_email__ = 'open-source@managedbyq.com'
+__description__ = 'MBQ Env'
+__license__ = 'Apache 2.0'
+__title__ = 'mbq.env'
+__url__ = 'https://github.com/managedbyq/mbq.env'
+__version__ = '2.0.2'
---
+++ packages/mbq.env/mbq.env-2.0.2.tar.gz/mbq.env-2.0.2/setup.py
@@ -0,0 +1,42 @@
+import codecs
+import os
+
+import setuptools
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+about = {}
+with codecs.open(os.path.join(here, 'mbq', 'env', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+setuptools.setup(
+    name=about['__title__'],
+    description=about['__description__'],
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    version=about['__version__'],
+    license=about['__license__'],
+    url=about['__url__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    maintainer=about['__author__'],
+    maintainer_email=about['__author_email__'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+    ],
+    keywords='env environment',
+    package_data={"mbq.env": ["py.typed"]},
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
