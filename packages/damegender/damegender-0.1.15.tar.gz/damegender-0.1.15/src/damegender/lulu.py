7bb0232382aba435c7379bcab18b855f0b8e589b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:36 +0000

    Add package damegender/0.1.15/damegender-0.1.15.tar.gz

---
+++ packages/damegender/damegender-0.1.15.tar.gz/damegender-0.1.15/src/damegender/lulu.py
@@ -0,0 +1,14 @@
+
+import json
+from app.dame_gender import Gender
+
+g = Gender()
+g.ukfile()
+
+# with open("files/names/uk/jsonuk.json") as jsondata:
+#     d = json.load(jsondata)
+#     print(d)
+
+
+# g = Gender()
+# print(g.ukfile())
