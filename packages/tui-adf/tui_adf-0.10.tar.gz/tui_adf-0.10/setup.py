f7854a0e07deeb93b8f7b0d8ad0ecabef63d69c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:46 +0000

    Add package tui-adf/0.10/tui_adf-0.10.tar.gz

---
+++ packages/tui-adf/tui_adf-0.10.tar.gz/tui_adf-0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tui_adf",
+    version="0.10",
+    author="Michael Riesmeyer",
+    author_email="Michael.Riesmeyer@tu-ilmenau.de",
+    description="Funktionen für Lehrveranstaltung Analoge Digitale Filter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/Kyrioris/",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy',
+                      'scipy',
+                      'PyDynamic'
+            ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
