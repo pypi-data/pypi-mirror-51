05f4a5633a820e57b5072cda5f6b2a1599dcb69f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:09 +0000

    Add package pycopy-cpython-uzlib/0.2/pycopy-cpython-uzlib-0.2.tar.gz

---
+++ packages/pycopy-cpython-uzlib/pycopy-cpython-uzlib-0.2.tar.gz/pycopy-cpython-uzlib-0.2/setup.py
@@ -0,0 +1,19 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system's.
+sys.path.pop(0)
+from setuptools import setup
+sys.path.append("..")
+
+setup(name='pycopy-cpython-uzlib',
+      version='0.2',
+      description='Pycopy module uzlib ported to CPython',
+      long_description='This is a compatibility module for the standard library of Pycopy project\n(https://github.com/pfalcon/pycopy). It allows applications using\nPycopy APIs to run on CPython.\n',
+      url='https://github.com/pfalcon/pycopy-lib',
+      author='Paul Sokolovsky',
+      author_email='pycopy-dev@googlegroups.com',
+      maintainer='Paul Sokolovsky',
+      maintainer_email='pycopy-dev@googlegroups.com',
+      license='Python',
+      py_modules=['uzlib'],
+      install_requires=['pycopy-cpython-micropython'])
