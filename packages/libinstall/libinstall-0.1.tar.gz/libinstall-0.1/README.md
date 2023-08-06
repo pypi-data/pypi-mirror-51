4ba8188964d40226ec1d238b87eee68b2484aff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package libinstall/0.1/libinstall-0.1.tar.gz

---
+++ packages/libinstall/libinstall-0.1.tar.gz/libinstall-0.1/README.md
@@ -0,0 +1,12 @@
+# Libinstall
+Tired of python repository without requirement ? libinstall will install automatically all libraries needed for the projet.
+
+# Todo:
+
+  * Handle "all" options and clarify what's the most effective.
+  * Add timeout for api files.
+
+# Description
+
+  This project is under construction
+  
---
+++ packages/libinstall/libinstall-0.1.tar.gz/libinstall-0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libinstall',
+    version='0.1',
+    packages=find_packages(),
+    author="Raphael Krief",
+    author_email="raphaelkriefbm@gmail.com",
+    description="Installe les librairies python necessaire d'un projet",
+    long_description=open('README.md').read(),
+    url="https://github.com/Raphencoder/libinstall",
+    classifiers=[
+    "Programming Language :: Python",
+    "Development Status :: 1 - Planning",
+    "License :: OSI Approved",
+    "Natural Language :: French",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.7",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+],
+    py_modules=['libinstall'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': ['libinstall=libinstall.script:cli'],
+    },
+    license="BSD 3-Clause License"
+    )
