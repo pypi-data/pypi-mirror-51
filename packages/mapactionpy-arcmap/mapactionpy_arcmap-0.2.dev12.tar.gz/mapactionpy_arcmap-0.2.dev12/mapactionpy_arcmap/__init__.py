02d07aa3aa0b70582cf3da3e5194d22a25ca2d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:33 +0000

    Add package mapactionpy-arcmap/0.2.dev12/mapactionpy_arcmap-0.2.dev12.tar.gz

---
+++ packages/mapactionpy-arcmap/mapactionpy_arcmap-0.2.dev12.tar.gz/mapactionpy_arcmap-0.2.dev12/mapactionpy_arcmap/__init__.py
@@ -0,0 +1,7 @@
+# from .LayerProperty import LayerProperty
+# from .LayerProperties import LayerProperties
+# from .MapRecipe import MapRecipe
+# from .MapChef import MapChef
+# from .MapCookbook import MapCookbook
+# from .MapReport import MapReport
+# from .MapResult import MapResult
