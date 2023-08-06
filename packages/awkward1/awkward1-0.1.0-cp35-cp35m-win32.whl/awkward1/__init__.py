54ba38176654fdba54afb8d0c5624febec11bc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package awkward1/0.1.0/awkward1-0.1.0-cp35-cp35m-win32.whl

---
+++ packages/awkward1/awkward1-0.1.0-cp35-cp35m-win32.whl/awkward1/__init__.py
@@ -0,0 +1,6 @@
+# BSD 3-Clause License; see https://github.com/jpivarski/awkward-1.0/blob/master/LICENSE
+
+import awkward1.layout
+import awkward1._numba
+
+__version__ = awkward1.layout.__version__
