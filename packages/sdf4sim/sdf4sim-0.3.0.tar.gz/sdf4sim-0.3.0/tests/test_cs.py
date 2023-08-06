aba02447fbc386b84373dd0ac07a28f07ef05adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:42 +0000

    Add package sdf4sim/0.3.0/sdf4sim-0.3.0.tar.gz

---
+++ packages/sdf4sim/sdf4sim-0.3.0.tar.gz/sdf4sim-0.3.0/tests/test_cs.py
@@ -0,0 +1,47 @@
+"""Tests for sdf4sim.cs"""
+
+import logging
+
+from os import path
+from sdf4sim import example, sdf, cs
+
+
+logging.basicConfig(
+    format='%(asctime)-15s %(message)s',
+    filename='test_cs.log',
+    filemode='a'
+)
+
+
+def test_control_cs_valid():
+    """Check if conversion to cs gives a valid graph"""
+    sdf_graph = cs.convert_to_sdf(example.control.gauss_seidel(1., 5., 1.))
+    assert sdf.validate_graph(sdf_graph)
+
+
+def test_control_loop_example():
+    """A test whether the control loop example runs"""
+    example.control.print_error_measurement()
+    import matplotlib
+    matplotlib.use('agg')
+    example.control.visualise_error_measurement(fig_file='cs_compare.pdf')
+    assert path.isfile('cs_compare.pdf')
+    example.control.sil_comparison()
+
+
+def test_repetition_vector():
+    """Test to see check the cs repetition vector"""
+    cosimulations = [
+        example.control.gauss_seidel(1., 5., 1.),
+        example.control.gauss_seidel(1., 5., 1., True),
+        example.control.gauss_seidel(1., 5., 1., True, True),
+        example.control.gauss_seidel(1., 5., 1., False, True),
+        example.control.gauss_jacobi(1., 5., 1.),
+        example.control.multi_rate(1., 5., 1.),
+    ]
+    for cosimulation in cosimulations:
+        sdfg = cs.convert_to_sdf(cosimulation)
+        schedule = sdf.calculate_schedule(sdfg)
+        repetitions = cs.repetition_vector(cosimulation)
+        for agent in sdfg[0]:
+            assert sum(agent == executed for executed in schedule) == repetitions[agent]
