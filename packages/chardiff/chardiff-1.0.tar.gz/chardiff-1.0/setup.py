6ab4cd1a880ecaca44acd0a8476a5c7729ddb764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:39 +0000

    Add package chardiff/1.0/chardiff-1.0.tar.gz

---
+++ packages/chardiff/chardiff-1.0.tar.gz/chardiff-1.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='chardiff',
+    version='1.0',
+    description='Compare 2 strings and highlight differences',
+    long_description=open("README.md").read(),
+    author='Matt Copperwaite',
+    author_email='matt@copperwaite.net',
+    url='https://github.com/yamatt/python3-chardiff',
+    packages=['chardiff'],
+    scripts=['scripts/chardiff']
+)
