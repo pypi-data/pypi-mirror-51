cbae78395980cc151f19a57ead045840cd0a0ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:05 +0000

    Add package weather-application1/0.1/weather_application1-0.1.tar.gz

---
+++ packages/weather-application1/weather_application1-0.1.tar.gz/weather_application1-0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+     name='weather_application1',
+     version='0.1',
+     scripts=['weather_info_nitr'] ,
+     author="Biswajeet_Sahoo",
+     author_email="biswajeetsahoo54@gmail.com",
+ )
+
+
