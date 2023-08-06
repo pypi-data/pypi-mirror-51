955d04f462839e1866a10f95b15a201bf72bc856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:03 +0000

    Add package chainer-mask-rcnn/0.5.23/chainer-mask-rcnn-0.5.23.tar.gz

---
+++ packages/chainer-mask-rcnn/chainer-mask-rcnn-0.5.23.tar.gz/chainer-mask-rcnn-0.5.23/setup.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+
+import distutils.spawn
+import shlex
+import subprocess
+import sys
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = '0.5.23'
+
+
+if sys.argv[-1] == 'release':
+    if not distutils.spawn.find_executable('twine'):
+        print(
+            'Please install twine:\n\n\tpip install twine\n',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    commands = [
+        'git tag v{:s}'.format(version),
+        'git push origin master --tag',
+        'python setup.py sdist',
+        'twine upload dist/chainer-mask-rcnn-{:s}.tar.gz'.format(version),
+    ]
+    for cmd in commands:
+        print('+ {}'.format(cmd))
+        subprocess.check_call(shlex.split(cmd))
+    sys.exit(0)
+
+
+def get_long_description():
+    with open('README.md') as f:
+        long_description = f.read()
+
+    try:
+        import github2pypi
+
+        return github2pypi.replace_url(
+            slug='wkentaro/chainer-mask-rcnn', content=long_description
+        )
+    except Exception:
+        return long_description
+
+
+setup(
+    name='chainer-mask-rcnn',
+    version=version,
+    packages=find_packages(exclude=['github2pypi']),
+    include_package_data=True,
+    install_requires=open('requirements.txt').readlines(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    description='Chainer Implementation of Mask R-CNN.',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/wkentaro/chainer-mask-rcnn',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+)
