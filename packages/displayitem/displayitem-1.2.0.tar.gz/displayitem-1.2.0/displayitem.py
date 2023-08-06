9db9a113cb571d1f208df98ac769fa76b1237cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:08 +0000

    Add package displayitem/1.2.0/displayitem-1.2.0.tar.gz

---
+++ packages/displayitem/displayitem-1.2.0.tar.gz/displayitem-1.2.0/displayitem.py
@@ -0,0 +1,20 @@
+"""This is the "displayitem.py" module, and it provides one function called
+print_lol() which prints lists that may or may not include nested lists."""
+def print_lol(the_list, level=0):
+    """This function takes in two argument "the_list" and "level". "the_list"
+prints each data item on a new line whether it is nested or not while "level"
+inserts tab-stops whenever it finds a nested list."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
+
+
+'''
+movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman",
+           ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+'''
