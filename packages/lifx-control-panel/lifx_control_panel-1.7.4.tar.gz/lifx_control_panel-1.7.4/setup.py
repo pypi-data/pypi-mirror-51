00e05b6979feb5c58cabf8304f56fd4ed7721ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:54 +0000

    Add package lifx-control-panel/1.7.4/lifx_control_panel-1.7.4.tar.gz

---
+++ packages/lifx-control-panel/lifx_control_panel-1.7.4.tar.gz/lifx_control_panel-1.7.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name='lifx_control_panel',
+      version='1.7.4',
+      description='An open source application for controlling your LIFX brand lights',
+      url='http://github.com/samclane/LIFX-Control-Panel',
+      author='Sawyer McLane',
+      author_email='samclane@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      scripts=['lifx_control_panel/__main__.pyw'],
+      include_package_data=True,
+      keywords=['lifx', 'iot', 'smartbulb', 'smartlight', 'lan', 'application'],
+      install_requires=[
+            'PyHook3',
+            'desktopmagic',
+            'win32gui',
+            'pyaudio',
+            'Pillow',
+            'lifxlan',
+            'pywin32',
+            'numexpr',
+            'numpy'
+      ],
+      download_url="https://github.com/samclane/LIFX-Control-Panel/archive/1.7.4.tar.gz")
