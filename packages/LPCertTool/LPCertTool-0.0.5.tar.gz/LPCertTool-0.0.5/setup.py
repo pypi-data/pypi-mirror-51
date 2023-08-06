2315b855a1bdd5b7467d0dcd3529766207846fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:53 +0000

    Add package LPCertTool/0.0.5/LPCertTool-0.0.5.tar.gz

---
+++ packages/LPCertTool/LPCertTool-0.0.5.tar.gz/LPCertTool-0.0.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+path = "./Readme.txt"
+with open(path, "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "LPCertTool",
+    version = "0.0.5",
+    author = "Ning.Liu",
+    author_email = "ning.liu@linkplay.com",
+    description = "Linkplay MSP Certificate Tool",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liuning0930",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+    install_requires=[
+        "PyQt5",
+        "XlsxWriter",
+        "xlrd"
+    ],
+    entry_points={
+              'console_scripts': [
+                  'lpcerttool=LPCertLog.LPCertReportUI:main',
+              ],
+          },
+)
