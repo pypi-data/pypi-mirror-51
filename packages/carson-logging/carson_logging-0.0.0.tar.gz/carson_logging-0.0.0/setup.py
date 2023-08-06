0b6726aa147df1ea6932f46d5da669457955dc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:03 +0000

    Add package carson-logging/0.0.0/carson_logging-0.0.0.tar.gz

---
+++ packages/carson-logging/carson_logging-0.0.0.tar.gz/carson_logging-0.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name = 'carson_logging',
+    packages = ['packages'],
+    scripts = ['runner'],
+    version = '0.0.0',
+    description = 'easier to use original python provides a module of logging',
+    author = 'Carson',
+    author_email = 'jackparadise520a@gmail.com',
+    url = 'https://github.com/CarsonSlovoka/carson-logging',
+    download_url = 'https://github.com/CarsonSlovoka/carson-logging/tarball/v0.0.0',
+    keywords = ['logging', 'log'],
+    license="Apache-2.0",
+    classifiers = [],
+)
