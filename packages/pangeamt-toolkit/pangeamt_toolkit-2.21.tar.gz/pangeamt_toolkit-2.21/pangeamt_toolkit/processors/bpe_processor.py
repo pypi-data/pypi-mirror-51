c1ffe51aeeb42e710d48426101bda7a3cfcbedb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:31 +0000

    Add package pangeamt-toolkit/2.21/pangeamt_toolkit-2.21.tar.gz

---
+++ packages/pangeamt-toolkit/pangeamt_toolkit-2.21.tar.gz/pangeamt_toolkit-2.21/pangeamt_toolkit/processors/bpe_processor.py
@@ -0,0 +1,40 @@
+from subword_nmt.apply_bpe import BPE as _BPE
+from subword_nmt.apply_bpe import read_vocabulary as _rv
+from re import sub as _sub
+import codecs as _codecs
+
+
+class BPEProcessor:
+
+    def __init__(self, bpe_codes, bpe_vocab=None, bpe_threshold=None,\
+            bpe_glossaries=None):
+        self._mod = 'bpe'
+        if bpe_glossaries == None:
+            bpe_glossaries = []
+        if bpe_vocab:
+            _vocab = \
+                _rv(_codecs.open(bpe_vocab, encoding='utf-8'), bpe_threshold)
+            self._bpe = _BPE(_codecs.open(bpe_codes, encoding='utf-8'),\
+                vocab=_vocab, glossaries=bpe_glossaries)
+        else:
+            self._bpe = _BPE(_codecs.open(bpe_codes, encoding='utf-8'))
+
+    def get_mod(self):
+        return self._mod
+    mod = property(get_mod)
+
+    def preprocess(self, seg):
+        '''Apply BPE to seg.src
+        '''
+        seg.src = self._bpe.segment(seg.src)
+
+    def preprocess_str(self, str):
+        return self._bpe.segment(str)
+
+    def postprocess(self, seg):
+        '''Remove BPE from seg.tgt
+        '''
+        seg.tgt = _sub("(@@ )|(@@ ?$)", '', seg.tgt)
+
+    def postprocess_str(self, str):
+        return _sub("(@@ )|(@@ ?$)", '', str)
