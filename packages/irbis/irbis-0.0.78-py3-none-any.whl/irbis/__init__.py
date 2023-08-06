afae5f6805efe90fadbb37d4c1034b563d3b8602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:27 +0000

    Add package irbis/0.0.78/irbis-0.0.78-py3-none-any.whl

---
+++ packages/irbis/irbis-0.0.78-py3-none-any.whl/irbis/__init__.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+
+# ManagedIrbis ported from C# to Python.
+# Python supported: 3.6.x
+# IRBIS64 supported: 2015 and higher
+
+__version__ = '0.0.78'
+__author__ = 'Alexey Mironov'
+__email__ = 'amironov73@gmail.com'
+
+__title__ = 'pyiribis'
+__summary__ = 'ManagedIrbis ported from C# to Python'
+__uri__ = 'http://arsmagna.ru'
+
+
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2018 Alexey Mironov'
