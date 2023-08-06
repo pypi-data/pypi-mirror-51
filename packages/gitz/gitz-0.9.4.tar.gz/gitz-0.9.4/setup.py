2201c429b29486441a19053c8079a457db85c200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package gitz/0.9.4/gitz-0.9.4.tar.gz

---
+++ packages/gitz/gitz-0.9.4.tar.gz/gitz-0.9.4/setup.py
@@ -0,0 +1,32 @@
+import os
+
+VERSION = '0.9.4'
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+COMMANDS = sorted(f for f in os.listdir(ROOT_DIR) if f.startswith('git-'))
+
+
+if __name__ == '__main__':
+    import setuptools
+
+    setuptools.setup(
+        name='gitz',
+        version=VERSION,
+        author='Tom Ritchford',
+        author_email='tom@swirly.com',
+        url='https://github.com/rec/gitz',
+        description='🗜 gitz - tiny useful git commands, some dangerous 🗜',
+        long_description=open('README.rst').read(),
+        license='MIT',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Utilities',
+        ],
+        keywords=['git'],
+        scripts=COMMANDS,
+        py_modules=['gitz'],
+    )
