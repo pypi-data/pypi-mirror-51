635e2024dd1ea4798d9ab2026867f1773764ccc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:26 +0000

    Add package developers-chamber/0.0.5/developers-chamber-0.0.5.tar.gz

---
+++ packages/developers-chamber/developers-chamber-0.0.5.tar.gz/developers-chamber-0.0.5/developers_chamber/scripts/ecs.py
@@ -0,0 +1,35 @@
+import os
+
+import click
+
+from developers_chamber.ecs_utils import deploy_new_task_definition as deploy_new_task_func
+from developers_chamber.ecs_utils import stop_service as stop_service_func
+from developers_chamber.scripts import cli
+
+
+default_region = os.environ.get('AWS_REGION')
+
+
+@cli.group()
+def ecs():
+    """ECS commands"""
+
+
+@ecs.command()
+@click.option('--cluster', help='ecs cluster name', type=str, required=True)
+@click.option('--service', help='ecs service names', type=str, required=True)
+@click.option('--task-definition', help='ecs task definition name', type=str, required=True)
+@click.option('--image', help='docker image tag to use', type=str, required=True)
+@click.option('--region', help='aws region', type=str, default=default_region, required=True)
+def deploy_new_task_definition(cluster, service, task_definition, image, region):
+    """Deploy new task definition in AWS ECS."""
+    click.echo(deploy_new_task_func(cluster, service, task_definition, image, region))
+
+
+@ecs.command()
+@click.option('--cluster', help='ecs cluster name', type=str, required=True)
+@click.option('--service', help='ecs service name', type=str, required=True)
+@click.option('--region', help='aws region', type=str, default=default_region, required=True)
+def stop_service(cluster, service, region):
+    """Stop an AWS ECS service by updating its size and setting it to 0."""
+    click.echo(stop_service_func(cluster, service, region))
