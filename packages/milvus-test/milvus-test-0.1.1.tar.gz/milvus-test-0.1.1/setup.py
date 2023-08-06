105e7fef38617534f02350a3fcf8697832a88d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package milvus-test/0.1.1/milvus-test-0.1.1.tar.gz

---
+++ packages/milvus-test/milvus-test-0.1.1.tar.gz/milvus-test-0.1.1/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / 'README.md').read_text()
+
+setuptools.setup(
+    name="milvus-test",
+    version="0.1.1",
+    description="Python Sdk for Milvus; Test version",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/milvus-io/pymilvus',
+    license="Apache-2.0",
+    packages=["milvus.client", 'milvus.thrift', 'milvus'],
+    include_package_data=True,
+    install_requires=["thrift"],
+    classifiers=[
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+
+
+    python_requires='>=3.4'
+)
