f9dcda290f056f1352d143b63774b8fba0162d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:20 +0000

    Add package linuk/1.0.0/linuk-1.0.0-py3-none-any.whl

---
+++ packages/linuk/linuk-1.0.0-py3-none-any.whl/linuk/basic.py
@@ -0,0 +1,26 @@
+
+def listmaker(object,include_space=False,data_type=str):
+	'''Makes List from space separated strings 
+	Example: listmaker('object1 object2 object3')
+
+PARAMETERS:
+	include_space : bool, default=False
+		listmaker('object_1 object_2 object_3')
+		'_' is interpreted as space
+
+	data_type : Datatype, default=str
+		change Datatype of Objects
+		available datatypes{int,str,float}
+
+	
+
+	'''
+	madelist = object.split(' ')
+	if include_space==True:
+		for i in range(len(madelist)):
+			madelist[i]  =  madelist[i].replace('_',' ')
+
+	for i in range(len(madelist)):
+			madelist[i]  =  data_type(madelist[i])
+
+	return madelist
