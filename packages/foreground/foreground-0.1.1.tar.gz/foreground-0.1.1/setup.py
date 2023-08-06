12e3fe82fcddcf5a04bbca757c3fcfed36d33fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:16 +0000

    Add package foreground/0.1.1/foreground-0.1.1.tar.gz

---
+++ packages/foreground/foreground-0.1.1.tar.gz/foreground-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from foreground import __version__, __author__
+
+setup(
+    name='foreground',
+    packages=['foreground'],
+    version=__version__,
+    description='Select foreground text color based on background color to get better contrast.',
+    author=__author__,
+    author_email='azaitsev@gmail.com',
+    url='https://github.com/azaitsev/foreground',
+    keywords=['colors', 'foreground', 'background', 'text', 'contrast', 'web', 'font'],
+    classifiers=[]
+)
