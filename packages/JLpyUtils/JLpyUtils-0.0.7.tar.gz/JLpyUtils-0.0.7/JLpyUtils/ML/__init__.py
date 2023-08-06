3e1bf88e96cf5a4b72596987f79c9355d53d3501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:12 +0000

    Add package JLpyUtils/0.0.7/JLpyUtils-0.0.7.tar.gz

---
+++ packages/JLpyUtils/JLpyUtils-0.0.7.tar.gz/JLpyUtils-0.0.7/JLpyUtils/ML/__init__.py
@@ -0,0 +1,9 @@
+"""
+Custom machine learning module for python focusing on streamlining and wrapping sklearn & tensorflow/keras functions
+====================================================================================================================
+"""
+    
+import JLpyUtils.ML.model_selection
+import JLpyUtils.ML.NeuralNet
+import JLpyUtils.ML.preprocessing
+import JLpyUtils.ML.inspection
