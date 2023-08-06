d8a081ef4d85fbda77ff22bcd02e8b20fcf537ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:10 +0000

    Add package scheduled-task/1.0.0/scheduled_task-1.0.0.tar.gz

---
+++ packages/scheduled-task/scheduled_task-1.0.0.tar.gz/scheduled_task-1.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='scheduled_task',
+    version='1.0.0',
+    author='Artem Kryvonis',
+    license='MIT',
+    author_email='me@kryvonis.com',
+    description='scheduled tasks for Python 3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Kryvonis/scheduled_task',
+    packages=['scheduled_task'],
+    classifiers=[
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
