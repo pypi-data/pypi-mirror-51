1eabe9925b2326fb5939a143b738b38e16038e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:12 +0000

    Add package motivate-lizixi/0.1.2/motivate_lizixi-0.1.2.tar.gz

---
+++ packages/motivate-lizixi/motivate_lizixi-0.1.2.tar.gz/motivate_lizixi-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+__project__='motivate_lizixi'
+__version__='0.1.2'
+__description__='a Python moudle to motivate you'
+__packages__=['motivate']
+setup(
+        name=__project__,
+        version=__version__,
+        description=__description__,
+        packages=__packages__,
+)
