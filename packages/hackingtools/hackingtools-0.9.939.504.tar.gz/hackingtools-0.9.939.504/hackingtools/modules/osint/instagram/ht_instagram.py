af4ead8bbb29f2c5c83492e7067238d21ceaf2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:16 +0000

    Add package hackingtools/0.9.939.504/hackingtools-0.9.939.504.tar.gz

---
+++ packages/hackingtools/hackingtools-0.9.939.504.tar.gz/hackingtools-0.9.939.504/hackingtools/modules/osint/instagram/ht_instagram.py
@@ -0,0 +1,20 @@
+from hackingtools.core import Logger, Config
+import hackingtools as ht
+import os
+
+config = Config.getConfig(parentKey='modules', key='ht_instagram')
+output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'output'))
+
+class StartModule():
+    
+    def __init__(self):
+        pass
+        
+    def help(self):
+        functions = ht.getFunctionsNamesFromModule('ht_instagram')
+        Logger.printMessage(message=functions)
+        return functions
+
+    def getEdad(self):
+        Logger.printMessage(message='{methodName}'.format(methodName='getEdad'), debug_module=True)
+        return 23
