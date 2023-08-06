9b9cd27bfce529fb738977b125e103c72484910f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:52 +0000

    Add package pdp-lgtm/0.1.3/pdp-lgtm-0.1.3.tar.gz

---
+++ packages/pdp-lgtm/pdp-lgtm-0.1.3.tar.gz/pdp-lgtm-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+version = '0.1.3'
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='pdp-lgtm',
+    version=version,
+    description="party of dope pajamas looks good to me.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Shaoqing Tan',
+    author_email='tansq7@gmail.com',
+    url='https://github.com/timlyrics/pdp-lgtm',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': ['pdp=pdp_lgtm.pdp:main'],
+    },
+)
