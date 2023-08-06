9dac0b43416718395307660b90e4a9f3e63c4fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:37 +0000

    Add package PySSL/0.0.4/PySSL-0.0.4.tar.gz

---
+++ packages/PySSL/PySSL-0.0.4.tar.gz/PySSL-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name = 'PySSL',
+	version = '0.0.4',
+	description = 'Basic libraries for SSL',
+	packages = ['cmdIf', 'visionIf'],
+	package_data = {
+		'cmdIf' : ['cmdIf.pyd', 'boost_python37-vc140-mt-x64-1_67.dll'],
+		'visionIf' : ['visionIf.pyd', 'boost_python37-vc140-mt-x64-1_67.dll']
+	}
+)
