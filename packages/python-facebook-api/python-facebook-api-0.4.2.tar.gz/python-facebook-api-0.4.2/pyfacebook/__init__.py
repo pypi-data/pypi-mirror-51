662ec9e56c889dd7b1c939c1848a8a73243d1145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:03 +0000

    Add package python-facebook-api/0.4.2/python-facebook-api-0.4.2.tar.gz

---
+++ packages/python-facebook-api/python-facebook-api-0.4.2.tar.gz/python-facebook-api-0.4.2/pyfacebook/__init__.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+
+from .api import Api, InstagramApi  # noqa
+from .error import PyFacebookError  # noqa
+from .models import (  # noqa
+    AccessToken,
+    Attachment,
+    Comment,
+    CommentSummary,
+    Cover,
+    InstagramMedia,
+    InstagramUser,
+    Page,
+    PageCategory,
+    PageEngagement,
+    PagePicture,
+    Post,
+    ReactionSummary,
+    ShareSummary
+)
