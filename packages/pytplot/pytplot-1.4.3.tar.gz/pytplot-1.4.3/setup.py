61d577a1ac345ceecd2f97cfb41fd9afe7575795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:43 +0000

    Add package pytplot/1.4.3/pytplot-1.4.3.tar.gz

---
+++ packages/pytplot/pytplot-1.4.3.tar.gz/pytplot-1.4.3/setup.py
@@ -0,0 +1,11 @@
+#
+#To upload the latest version, change "version=0.X.X+1" and type:
+#	python setup.py sdist
+#   twine upload dist/*
+#
+#
+
+from setuptools import setup
+
+from setuptools import setup
+setup()
