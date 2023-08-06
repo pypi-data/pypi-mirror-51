7230633e759aefbb9a82dd3d9484068bafc00f4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:28 +0000

    Add package hestia/0.3.3/hestia-0.3.3.tar.gz

---
+++ packages/hestia/hestia-0.3.3.tar.gz/hestia-0.3.3/hestia/services.py
@@ -0,0 +1,22 @@
+HEADERS_CLI_VERSION = 'X_POLYAXON_CLI_VERSION'
+HEADERS_CLIENT_VERSION = 'X_POLYAXON_CLIENT_VERSION'
+HEADERS_INTERNAL = 'X_POLYAXON_INTERNAL'
+HEADERS_OPERATOR = 'X_POLYAXON_OPERATOR'
+
+
+class PolyaxonServices(object):  # noqa
+    DOCKERIZER = 'dockerizer'
+    INITIALIZER = 'initializer'
+    SIDECAR = 'sidecar'
+    HELPER = 'helper'
+    RUNNER = 'runner'
+    CONTROLLER = 'controller'
+
+    VALUES = {
+        DOCKERIZER,
+        INITIALIZER,
+        SIDECAR,
+        HELPER,
+        RUNNER,
+        CONTROLLER
+    }
