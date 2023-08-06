d48186b55da562153124c0e91cc26df18cf545c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:01 +0000

    Add package contendo-utils/0.2.1/contendo_utils-0.2.1.tar.gz

---
+++ packages/contendo-utils/contendo_utils-0.2.1.tar.gz/contendo_utils-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='contendo_utils',
+      version='0.2.1',
+      description='General python and google-cloud related utilities',
+      url='https://github.com/sportsight-pro/sportsight-core/tree/master/contendo_utils',
+      author='Yahali Sherman, contendo.ai',
+      author_email='yahali@contendo.ai',
+      license='MIT',
+      packages=['contendo_utils'],
+      zip_safe=False)
