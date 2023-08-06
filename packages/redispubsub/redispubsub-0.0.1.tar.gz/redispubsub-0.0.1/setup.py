7ac38f7a7c8c4d0e38051395a932923836ebcbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:31 +0000

    Add package redispubsub/0.0.1/redispubsub-0.0.1.tar.gz

---
+++ packages/redispubsub/redispubsub-0.0.1.tar.gz/redispubsub-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="redispubsub",
+    version=__import__("redisrpc").VERSION,
+    author="Munis Isazade",
+    author_email="munisisazade@gmail.com",
+    description="Redis RPC server for microservices",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    license='MIT',
+    url="https://github.com/munisisazade/redispubsub",
+    install_requires=["redis"],
+    platforms=['any'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
