b71defc3bf42a62a696298eab0d41236444166e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:11 +0000

    Add package aiozk/0.17.0/aiozk-0.17.0.tar.gz

---
+++ packages/aiozk/aiozk-0.17.0.tar.gz/aiozk-0.17.0/aiozk/recipes/election.py
@@ -0,0 +1,45 @@
+import asyncio
+import time
+
+from .sequential import SequentialRecipe
+
+
+class LeaderElection(SequentialRecipe):
+
+    def __init__(self, base_path):
+        super(LeaderElection, self).__init__(base_path)
+        self.has_leadership = False
+
+        self.leadership_future = self.client.loop.create_future()
+
+    async def join(self):
+        await self.create_unique_znode("candidate")
+        await self.check_position()
+
+    async def check_position(self, _=None):
+        owned_positions, candidates = await self.analyze_siblings()
+        if "candidate" not in owned_positions:
+            return
+
+        position = owned_positions["candidate"]
+
+        self.has_leadership = bool(position == 0)
+
+        if self.has_leadership:
+            self.leadership_future.set_result(None)
+            return
+
+        await self.wait_on_sibling(candidates[position - 1])
+        asyncio.ensure_future(self.check_position(), loop=self.client.loop)
+
+    async def wait_for_leadership(self, timeout=None):
+        if self.has_leadership:
+            return
+
+        if timeout:
+            await asyncio.wait_for(self.leadership_future, timeout, loop=self.client.loop)
+        else:
+            await self.leadership_future
+
+    async def resign(self):
+        await self.delete_unique_znode("candidate")
