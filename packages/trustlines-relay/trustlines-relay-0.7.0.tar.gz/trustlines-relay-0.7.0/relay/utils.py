892d0d50388d9de4bc13a760985caaaa9b55794f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:15 +0000

    Add package trustlines-relay/0.7.0/trustlines-relay-0.7.0.tar.gz

---
+++ packages/trustlines-relay/trustlines-relay-0.7.0.tar.gz/trustlines-relay-0.7.0/relay/utils.py
@@ -0,0 +1,19 @@
+import web3
+
+
+def merge_two_dicts(x, y):
+    """Given two dicts, merge them into a new dict as a shallow copy."""
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+def trim_args(args):
+    trimmed_args = {}
+    for key in args:
+        trimmed_args[key[1 : len(key)]] = args[key]
+    return trimmed_args
+
+
+def sha3(text: str) -> str:
+    return web3.Web3.keccak(text=text).hex()
