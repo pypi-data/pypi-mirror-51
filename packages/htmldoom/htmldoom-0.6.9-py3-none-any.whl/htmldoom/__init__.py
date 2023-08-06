d91f510b0b973d4911afcb3693332dcadf6c0988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:15 +0000

    Add package htmldoom/0.6.9/htmldoom-0.6.9-py3-none-any.whl

---
+++ packages/htmldoom/htmldoom-0.6.9-py3-none-any.whl/htmldoom/__init__.py
@@ -0,0 +1,29 @@
+__author__ = "Arijit Basu"
+__email__ = "sayanarijit@gmail.com"
+__homepage__ = "https://github.com/sayanarijit/htmldoom"
+__description__ = "An intuitive, high performance HTML rendering framework"
+
+__version__ = "v0.6.9"
+__license__ = "MIT"
+__all__ = [
+    "__author__",
+    "__email__",
+    "__homepage__",
+    "__description__",
+    "__version__",
+    "__license__",
+    "elements",
+    "functions",
+    "doctype",
+    "render",
+    "renders",
+    "conf",
+    "raw",
+    "txt",
+    "comment",
+    "CacheConfig",
+]
+
+from htmldoom.base import comment, doctype, raw, txt
+from htmldoom.conf import CacheConfig
+from htmldoom.util import render, renders
