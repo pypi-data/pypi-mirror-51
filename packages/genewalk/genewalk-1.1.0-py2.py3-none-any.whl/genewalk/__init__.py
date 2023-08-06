e07fedad15ccca0b10547fc736f211a717b705cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:45 +0000

    Add package genewalk/1.1.0/genewalk-1.1.0-py2.py3-none-any.whl

---
+++ packages/genewalk/genewalk-1.1.0-py2.py3-none-any.whl/genewalk/__init__.py
@@ -0,0 +1,13 @@
+import logging
+default_logger_format = ('%(levelname)s: [%(asctime)s] %(name)s'
+                         ' - %(message)s')
+default_date_format = '%Y-%m-%d %H:%M:%S'
+
+logging.basicConfig(format=default_logger_format, level=logging.INFO,
+                    datefmt=default_date_format)
+
+
+logger = logging.getLogger('genewalk')
+
+
+__version__ = '1.1.0'
