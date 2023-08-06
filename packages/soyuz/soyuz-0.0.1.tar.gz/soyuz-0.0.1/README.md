787538acc282d3a9af43e56f2ad1021802319a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:08 +0000

    Add package soyuz/0.0.1/soyuz-0.0.1.tar.gz

---
+++ packages/soyuz/soyuz-0.0.1.tar.gz/soyuz-0.0.1/README.md
@@ -0,0 +1,2 @@
+# astronaut
+NLTK API with SpaCy models
---
+++ packages/soyuz/soyuz-0.0.1.tar.gz/soyuz-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'soyuz',
+  packages = ['soyuz'],
+  version = '0.0.1',
+  description = 'NLTK API with SpaCy models.',
+  long_description = '',
+  author = '',
+  license = '',
+  url = 'https://github.com/alvations/soyuz',
+  keywords = [],
+  classifiers = [],
+)
