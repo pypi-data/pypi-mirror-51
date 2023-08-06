64ef06feca5af896a7ba3e182c4d56d12c8f60ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:45 +0000

    Add package CloudSEK-Web-crawling-Task/1.0/CloudSEK%20Web%20crawling%20Task-1.0.tar.gz

---
+++ packages/CloudSEK-Web-crawling-Task/CloudSEK%20Web%20crawling%20Task-1.0.tar.gz/CloudSEK Web crawling Task-1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+     name='CloudSEK Web crawling Task',
+     version='1.0',
+     scripts=['App'] ,
+     author="Nishant prajapati",
+     author_email="knishant147@gmail.com",
+     )
