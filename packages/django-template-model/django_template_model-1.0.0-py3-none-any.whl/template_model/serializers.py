1899f4b015cfef77ddcc135a7ad2c82b69aced82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:23 +0000

    Add package django-template-model/1.0.0/django_template_model-1.0.0-py3-none-any.whl

---
+++ packages/django-template-model/django_template_model-1.0.0-py3-none-any.whl/template_model/serializers.py
@@ -0,0 +1,28 @@
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+from rest_framework import serializers
+
+from .models import Template
+
+
+class TemplateSerializer(serializers.ModelSerializer):
+    content = serializers.CharField(required=False, write_only=True)
+    template_file = serializers.FileField(required=False, write_only=True)
+
+    class Meta:
+        model = Template
+        fields = ('pk', 'name', 'mime_type', 'template_file', 'content', 'added', 'updated')
+        extra_kwargs = {
+            'mime_type': {'read_only': True},
+            'added': {'read_only': True},
+            'updated': {'read_only': True},
+        }
+
+    def validate(self, attrs):
+        if 'content' in attrs and 'template_file' in attrs:
+            raise serializers.ValidationError('You can not specify a text and upload a file.')
+        elif 'content' not in attrs and 'template_file' not in attrs:
+            raise serializers.ValidationError('Please specify a text or upload a file.')
+        elif 'content' in attrs:
+            attrs['template_file'] = ContentFile(attrs.pop('content'), name=slugify(attrs['name']))
+        return attrs
