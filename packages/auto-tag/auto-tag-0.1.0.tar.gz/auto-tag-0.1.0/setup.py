c4acd8324d1eb08ca9d3fe6e33dd58d26a8ee126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:26 +0000

    Add package auto-tag/0.1.0/auto-tag-0.1.0.tar.gz

---
+++ packages/auto-tag/auto-tag-0.1.0.tar.gz/auto-tag-0.1.0/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Setup file for the package.
+"""
+
+import os
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = os.path.abspath(pathlib.Path(__file__).parent)
+
+# The text of the README file
+README = open(
+    os.path.join(HERE, 'README.md'),
+    'r').read()
+
+# This call to setup() does all the work
+setup(
+    name='auto-tag',
+    version='0.1.0',
+    description='Automatically tag a branch based on commit message',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='Matei-Marius Micu',
+    author_email='contact@mateimicu.com	',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'auto-tag=auto_tag.__main__:main',
+        ]
+    },
+    install_reqs=[
+        'gitpython',
+        'semantic_version',
+    ]
+)
