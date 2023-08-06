9b72fb28f20773c4a208691d43cc0860be91d2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:51 +0000

    Add package ucms/1.0.0/ucms-1.0.0.tar.gz

---
+++ packages/ucms/ucms-1.0.0.tar.gz/ucms-1.0.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: mage
+# Mail: mage@woodcol.com
+# Created Time: 2018-1-23 19:17:34
+#############################################
+
+
+import setuptools
+
+with open('README.md','r',encoding='UTF-8') as f:
+    long_description = f.read()
+    
+setuptools.setup(
+    name="ucms",
+    version="1.0.0",
+    keywords=("ucms"),
+    description="UCMS采集接口python推送库,用于python将采集内容推送到UCMS上",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT Licence",
+    url="https://github.com/dayudada/ucms_caiji_python",
+    author="dayudada",
+    author_email="dayudada@foxmail.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        'requests'
+    ]
+)
