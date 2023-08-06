5f4e3bd70aeff2305dcd6c887a4c4c02f1958d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:20 +0000

    Add package dotscience/0.5.0/dotscience-0.5.0.tar.gz

---
+++ packages/dotscience/dotscience-0.5.0.tar.gz/dotscience-0.5.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dotscience",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Subtree, Inc",
+    author_email="support@dotscience.com",
+    description="Tools for writing Dotscience workloads",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dotmesh-io/dotscience-python",
+    packages=setuptools.find_packages(),
+    tests_require=['pytest', 'hypothesis'],
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
