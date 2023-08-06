66272b0eb7cb402307bb1794b9efd256f46ff334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:19 +0000

    Add package django-project-info/1.0.2/django-project-info-1.0.2.tar.gz

---
+++ packages/django-project-info/django-project-info-1.0.2.tar.gz/django-project-info-1.0.2/project_info/__init__.py
@@ -0,0 +1,19 @@
+import os.path
+import json
+
+
+def get_project_info_dict(version_config=None):
+    if version_config is None:
+        from django.conf import settings
+
+        version_config = settings.VERSION_CONFIG
+
+    if os.path.isfile(version_config):
+        with open(version_config) as f:
+            return json.load(f)
+    else:
+        return {}
+
+
+def get_version(version_config=None):
+    return get_project_info_dict(version_config).get('version')
---
+++ packages/django-project-info/django-project-info-1.0.2.tar.gz/django-project-info-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-project-info',
+    version='1.0.2',
+    description='A small plugin which takes a configuration (like Bower or npm) and it provides data via context'
+    'processors.',
+    keywords='django, project-info',
+    author='Lukas Rychtecky',
+    author_email='lukas.rychtecky@gmail.com',
+    url='https://github.com/druids/django-project-info',
+    license='MIT',
+    package_dir={'project_info': 'project_info'},
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'django>=1.11',
+    ],
+    zip_safe=False
+)
