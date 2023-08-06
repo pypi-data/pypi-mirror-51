32d0d8437d931060324cea470c49e6eb04d7a399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:47 +0000

    Add package aiostream/0.3.3/aiostream-0.3.3.tar.gz

---
+++ packages/aiostream/aiostream-0.3.3.tar.gz/aiostream-0.3.3/aiostream/stream/aggregate.py
@@ -0,0 +1,62 @@
+"""Aggregation operators."""
+
+import asyncio
+import operator as op
+
+from . import select
+from ..aiter_utils import anext
+from ..core import operator, streamcontext
+
+__all__ = ['accumulate', 'reduce', 'list']
+
+
+@operator(pipable=True)
+async def accumulate(source, func=op.add, initializer=None):
+    """Generate a series of accumulated sums (or other binary function)
+    from an asynchronous sequence.
+
+    If ``initializer`` is present, it is placed before the items
+    of the sequence in the calculation, and serves as a default
+    when the sequence is empty.
+    """
+    iscorofunc = asyncio.iscoroutinefunction(func)
+    async with streamcontext(source) as streamer:
+        # Initialize
+        if initializer is None:
+            try:
+                value = await anext(streamer)
+            except StopAsyncIteration:
+                return
+        else:
+            value = initializer
+        # First value
+        yield value
+        # Iterate streamer
+        async for item in streamer:
+            value = func(value, item)
+            if iscorofunc:
+                value = await value
+            yield value
+
+
+@operator(pipable=True)
+def reduce(source, func, initializer=None):
+    """Apply a function of two arguments cumulatively to the items
+    of an asynchronous sequence, reducing the sequence to a single value.
+
+    If ``initializer`` is present, it is placed before the items
+    of the sequence in the calculation, and serves as a default when the
+    sequence is empty.
+    """
+    acc = accumulate.raw(source, func, initializer)
+    return select.item.raw(acc, -1)
+
+
+@operator(pipable=True)
+async def list(source):
+    """Generate a single list from an asynchronous sequence."""
+    result = []
+    async with streamcontext(source) as streamer:
+        async for item in streamer:
+            result.append(item)
+    yield result
