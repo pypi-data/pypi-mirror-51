75e45996a535426f1be2329925c37b5bbc6118f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:21 +0000

    Add package ibm-watson/4.0.0rc1/ibm-watson-4.0.0rc1.tar.gz

---
+++ packages/ibm-watson/ibm-watson-4.0.0rc1.tar.gz/ibm-watson-4.0.0rc1/examples/natural_language_understanding_v1.py
@@ -0,0 +1,19 @@
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+authenticator = IAMAuthenticator('your_api_key')
+service = NaturalLanguageUnderstandingV1(
+    version='2018-03-16',
+    ## url is optional, and defaults to the URL below. Use the correct URL for your region.
+    url='https://gateway.watsonplatform.net/natural-language-understanding/api',
+    authenticator=authenticator)
+
+response = service.analyze(
+    text='Bruce Banner is the Hulk and Bruce Wayne is BATMAN! '
+    'Superman fears not Banner, but Wayne.',
+    features=Features(entities=EntitiesOptions(),
+                      keywords=KeywordsOptions())).get_result()
+
+print(json.dumps(response, indent=2))
