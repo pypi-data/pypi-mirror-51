a1bc4315eaf92e18fc878bda7684fb59792ad478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:21 +0000

    Add package quantzbrapi/0.0.8/quantzbrapi-0.0.8.tar.gz

---
+++ packages/quantzbrapi/quantzbrapi-0.0.8.tar.gz/quantzbrapi-0.0.8/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    author="Sergio Pelissari",
+    author_email="sonared@gmail.com",
+    name="quantzbrapi",
+    version="0.0.8",
+    long_description_content_type="text/markdown",
+    url="https://github.com/primehaxor/quantzbrapi",
+    packages=['quantzbrapi'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+    install_requires=["requests", "pandas"],
+)   
