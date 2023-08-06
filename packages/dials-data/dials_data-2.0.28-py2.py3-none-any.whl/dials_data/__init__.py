2bef577a8957ed9826294444d6b346f54874e608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:39 +0000

    Add package dials-data/2.0.28/dials_data-2.0.28-py2.py3-none-any.whl

---
+++ packages/dials-data/dials_data-2.0.28-py2.py3-none-any.whl/dials_data/__init__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+DIALS Regression Data Manager
+https://github.com/dials/data
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+import warnings
+
+__all__ = ["pytest_addoption", "dials_data"]
+__author__ = """Markus Gerstel"""
+__email__ = "dials-support@lists.sourceforge.net"
+__version__ = "2.0.28"
+__commit__ = "a8a397aae7fe13fbd805382e2cddf2584fe97f2c"
+__version_tuple__ = tuple(int(x) for x in __version__.split("."))
+
+
+def pytest_addoption(parser):
+    warnings.warn(
+        "The dials_data import instructions have changed. Please check "
+        "https://dials-data.readthedocs.io/en/latest/installation.html#as-a-developer-to-write-tests-with-dials-data"
+        " for updated instructions",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+
+
+@pytest.fixture
+def dials_data():
+    warnings.warn(
+        "The dials_data import instructions have changed. Please check "
+        "https://dials-data.readthedocs.io/en/latest/installation.html#as-a-developer-to-write-tests-with-dials-data"
+        " for updated instructions",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    pytest.skip("dials_data import instructions need updating")
