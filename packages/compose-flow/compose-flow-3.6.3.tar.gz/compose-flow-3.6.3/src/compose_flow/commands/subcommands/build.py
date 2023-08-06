f43526fbf3fd0df169323c1fb6a6e18ffb2432e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:07 +0000

    Add package compose-flow/3.6.3/compose-flow-3.6.3.tar.gz

---
+++ packages/compose-flow/compose-flow-3.6.3.tar.gz/compose-flow-3.6.3/src/compose_flow/commands/subcommands/build.py
@@ -0,0 +1,13 @@
+
+from .base import BaseBuildSubcommand
+
+
+class Build(BaseBuildSubcommand):
+    """
+    Subcommand for building Docker images
+    """
+
+    dirty_working_copy_okay = True
+
+    def handle(self):
+        self.build()
