542e7f1858b717c5c3eca23a968b421b12702e4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:35 +0000

    Add package algorithm-x/0.0.2/algorithm_x-0.0.2.tar.gz

---
+++ packages/algorithm-x/algorithm_x-0.0.2.tar.gz/algorithm_x-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="algorithm_x",
+    version="0.0.2",
+    author="Bjarki Ágúst Guðmundsson",
+    author_email="suprdewd@gmail.com",
+    description="An efficient implementation of Algorithm X",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SuprDewd/algorithm_x_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
