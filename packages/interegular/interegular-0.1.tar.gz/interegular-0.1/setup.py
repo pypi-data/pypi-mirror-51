00aced38e9606ecb98ee7d1a0114819afd7374f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:50 +0000

    Add package interegular/0.1/interegular-0.1.tar.gz

---
+++ packages/interegular/interegular-0.1.tar.gz/interegular-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import re
+
+__version__, = re.findall('__version__ = "(.*)"', open('interegular/__init__.py').read())
+
+setup(
+    name='interegular',
+    version=__version__,
+    packages=['interegular', 'interegular.utils'],
+    install_requires=['dataclasses; python_version<"3.7"'],
+    author='MegaIng',
+    author_email='MegaIng <trampchamp@hotmail.de>',
+    description="a regex intersection checker",
+    license="MIT",
+    url='https://github.com/MegaIng/regex_intersections',
+    download_url='https://github.com/MegaIng/interegular/archive/v0.1.tar.gz',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
