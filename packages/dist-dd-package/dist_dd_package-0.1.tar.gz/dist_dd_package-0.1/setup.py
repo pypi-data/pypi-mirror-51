d2f7ff4d02ed382793db4affaecffa861fbd357f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:09 +0000

    Add package dist-dd-package/0.1/dist_dd_package-0.1.tar.gz

---
+++ packages/dist-dd-package/dist_dd_package-0.1.tar.gz/dist_dd_package-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_dd_package',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_dd_package'],
+      zip_safe=False)
