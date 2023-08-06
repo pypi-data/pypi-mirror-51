7f9b6d2674570cd51a63c87b369d76043b18f2f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:00 +0000

    Add package pythran/0.9.3.post1/pythran-0.9.3.post1.tar.gz

---
+++ packages/pythran/pythran-0.9.3.post1.tar.gz/pythran-0.9.3.post1/pythran/pythonic/numpy/linspace.hpp
@@ -0,0 +1,21 @@
+#ifndef PYTHONIC_NUMPY_LINSPACE_HPP
+#define PYTHONIC_NUMPY_LINSPACE_HPP
+
+#include "pythonic/include/numpy/linspace.hpp"
+
+#include "pythonic/numpy/arange.hpp"
+
+PYTHONIC_NS_BEGIN
+
+namespace numpy
+{
+  types::ndarray<double, types::pshape<long>>
+  linspace(double start, double stop, long num, bool endpoint)
+  {
+    double step = (stop - start) / (num - (endpoint ? 1 : 0));
+    return arange(start, stop + (endpoint ? step * .5 : 0), step);
+  }
+}
+PYTHONIC_NS_END
+
+#endif
