5f89dcb73880b2402c64ecaf0c1e12def97908d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:28 +0000

    Add package example-pkg-siddharth1995/0.0.2/example-pkg-siddharth1995-0.0.2.tar.gz

---
+++ packages/example-pkg-siddharth1995/example-pkg-siddharth1995-0.0.2.tar.gz/example-pkg-siddharth1995-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-siddharth1995",
+    version="0.0.2",
+    author="Siddharth Bhonge",
+    author_email="bsiddharth4@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)  
