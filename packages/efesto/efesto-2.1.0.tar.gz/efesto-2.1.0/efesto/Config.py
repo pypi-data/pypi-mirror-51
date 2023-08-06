19f88a534c76a62de9b9103fae7ad113100c6ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:38 +0000

    Add package efesto/2.1.0/efesto-2.1.0.tar.gz

---
+++ packages/efesto/efesto-2.1.0.tar.gz/efesto-2.1.0/efesto/Config.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from bassoon import Bassoon
+
+
+class Config(Bassoon):
+
+    defaults = {
+        'ADMIN_ENDPOINTS': '0',
+        'APP_NAME': 'efesto',
+        'BATCH_ENDPOINTS': '1',
+        'DB_URL': 'sqlite:///efesto.db',
+        'HATEOAS_ENCODER': 'siren',
+        'JWT_SECRET': 'secret',
+        'JWT_LEEWAY': '5',
+        'JWT_AUDIENCE': 'efesto',
+        'LOG_LEVEL': 'info',
+        'LOG_FORMAT': '[{time:YYYY-MM-DD HH:mm:ss}] [{level}] {message}',
+        'MIDDLEWARES': 'authentication:json:log',
+        'PUBLIC_ENDPOINTS': 'index,version',
+        'SWAGGER': '1',
+        'XCLACKS': '1'
+    }
