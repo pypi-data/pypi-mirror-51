b7974f9d09661d51ed075326e06f8bc4cab2ead8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:39 +0000

    Add package pymarktex/1.2.1/pymarktex-1.2.1.tar.gz

---
+++ packages/pymarktex/pymarktex-1.2.1.tar.gz/pymarktex-1.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'pymarktex',
+    version          = '1.2.1',
+    description      = 'Will convert a markdown text file to a fancy PDF document',
+    license          = 'MIT',
+    url              = 'http://github.com/xapple/pymarktex/',
+    author           = 'Lucas Sinclair',
+    author_email     = 'lucas.sinclair@me.com',
+    packages         = find_packages(),
+    scripts          = ['scripts/pymarktex'],
+    install_requires = ['autopaths', 'plumbing', 'sh', 'pystache'],
+    long_description = open('README.md').read(),
+)
