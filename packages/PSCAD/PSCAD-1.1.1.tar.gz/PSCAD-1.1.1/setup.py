287d4ec13fe6c40c5d2797cc0905538b1dc8e6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:56 +0000

    Add package PSCAD/1.1.1/PSCAD-1.1.1.tar.gz

---
+++ packages/PSCAD/PSCAD-1.1.1.tar.gz/PSCAD-1.1.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages, setup
+from PSCAD.lib.core.__version__ import __version__
+
+
+def get_requirements(requirements):
+    """ Reads the module requirements from the given file.
+
+        :param requirements: Requirements .txt file
+        :type requirements: str
+        :returns: List of requirements
+        :rtype: lsit
+    """
+
+    with open(requirements, 'rt') as f:
+        return f.read().splitlines()
+
+
+# Open README.md for long description
+with open('README.md', 'r') as readme:
+    setup(
+        name="PSCAD",
+        license="GPLv3",
+        description="A network toolkit that can be used "
+                    "to scan, sniff and discover hosts.",
+        long_description=readme.read(),
+        long_description_content_type="text/markdown",
+        author="cblopez",
+        version=__version__,
+        url="https://github.com/cblopez/pscad",
+        packages=find_packages(),
+        package_data={'': ['requirements.txt']},
+        entry_points={
+            'console_scripts': [
+                'pscad = PSCAD.pscad:main'
+            ]
+        },
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'Topic :: Security',
+            'Topic :: System :: Networking :: Monitoring',
+            'Operating System :: Unix',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        ],
+        install_requires=get_requirements('requirements.txt'),
+        include_package_data=True
+    )
