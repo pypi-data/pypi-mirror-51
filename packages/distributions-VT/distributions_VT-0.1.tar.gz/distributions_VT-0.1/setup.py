cfd1a3484dddcdf88d44cf88e50e2a523524db7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:05 +0000

    Add package distributions-VT/0.1/distributions_VT-0.1.tar.gz

---
+++ packages/distributions-VT/distributions_VT-0.1.tar.gz/distributions_VT-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_VT',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_VT'],
+      zip_safe=False)
