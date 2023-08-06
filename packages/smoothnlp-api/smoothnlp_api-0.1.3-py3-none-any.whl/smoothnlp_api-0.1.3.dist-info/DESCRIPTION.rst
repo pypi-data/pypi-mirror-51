d9f034305251d3afd69c92cc473d500e76b3924d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:21 +0000

    Add package smoothnlp-api/0.1.3/smoothnlp_api-0.1.3-py3-none-any.whl

---
+++ packages/smoothnlp-api/smoothnlp_api-0.1.3-py3-none-any.whl/smoothnlp_api-0.1.3.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+SmoothNLP的数据接口的Python SDK
+
+
---
+++ packages/smoothnlp-api/smoothnlp_api-0.1.3-py3-none-any.whl/smoothnlp_api/api/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from smoothnlp_api.api.investment_api import InvestmentApi
+from smoothnlp_api.api.news_api import NewsApi
