974bc6b7c917a0ac8060fbe9889bcce3ebe1d489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:25 +0000

    Add package fetchDBconn/0.0.3/fetchDBconn-0.0.3.tar.gz

---
+++ packages/fetchDBconn/fetchDBconn-0.0.3.tar.gz/fetchDBconn-0.0.3/fetchDBconn/engine.py
@@ -0,0 +1,19 @@
+def get_secret(secret_name,region_name,aws_key,aws_secret):
+    secret_name = secret_name 
+    region_name = region_name
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+        aws_access_key_id = aws_key, 
+        aws_secret_access_key = aws_secret #
+    )
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except:
+        raise "Error"
+        
+    return ast.literal_eval(get_secret_value_response['SecretString'])
+
