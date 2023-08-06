57f9693101787197cf98ffd087f57b9886780ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:41 +0000

    Add package telekey/0.1.7/telekey-0.1.7.tar.gz

---
+++ packages/telekey/telekey-0.1.7.tar.gz/telekey-0.1.7/setup.py
@@ -0,0 +1,34 @@
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+print(HERE)
+
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+with open(os.path.join(HERE, "requirements.txt")) as fid:
+    REQUIREMENTS = fid.read().split()
+
+# This call to setup() does all the work
+setup(
+    name="telekey",
+    version="0.1.7",
+    description="An Easy way to develop telegram bots",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/danialkeimasi/telekey",
+    author="Danial Keimasi",
+    author_email="danialkeimasi@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["telekey"],
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    entry_points={"console_scripts": ["telekey=telekey.__main__:main"]},
+)
