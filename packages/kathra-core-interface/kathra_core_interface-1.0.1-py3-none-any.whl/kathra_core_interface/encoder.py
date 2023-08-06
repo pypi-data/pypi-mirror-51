6e752ada711ed40d7eeb8641c3199162c7c53984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:17 +0000

    Add package kathra-core-interface/1.0.1/kathra_core_interface-1.0.1-py3-none-any.whl

---
+++ packages/kathra-core-interface/kathra_core_interface-1.0.1-py3-none-any.whl/kathra_core_interface/encoder.py
@@ -0,0 +1,40 @@
+"""
+ Copyright 2019 The Kathra Authors.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ Contributors:
+
+    IRT SystemX (https://www.kathra.org/)    
+
+"""
+ 
+from six import iteritems
+from connexion.apps.flask_app import FlaskJSONEncoder
+from kathra_core_model.model import Model
+
+
+class JSONEncoder(FlaskJSONEncoder):
+    include_nulls = False
+
+    def default(self, o):
+        if isinstance(o, Model):
+            dikt = {}
+            for attr, _ in iteritems(o.swagger_types):
+                value = getattr(o, attr)
+                if value is None and not self.include_nulls:
+                    continue
+                attr = o.attribute_map[attr]
+                dikt[attr] = value
+            return dikt
+        return FlaskJSONEncoder.default(self, o)
