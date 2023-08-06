4b609f69b205f74eda6c81ccbb38014b26c5dc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:09 +0000

    Add package mastercard-crossborder/2.0.1/mastercard_crossborder-2.0.1-py2-none-any.whl

---
+++ packages/mastercard-crossborder/mastercard_crossborder-2.0.1-py2-none-any.whl/mastercardcrossborder/__init__.py
@@ -0,0 +1,12 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardcrossborder.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardcrossborder.cancelpayment import *
+from mastercardcrossborder.cardedrate import *
+from mastercardcrossborder.payment import *
+from mastercardcrossborder.quotes import *
+from mastercardcrossborder.retrievepayment import *
