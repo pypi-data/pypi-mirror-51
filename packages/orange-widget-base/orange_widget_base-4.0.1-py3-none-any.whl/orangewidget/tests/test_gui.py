45db1a15d264ce0308baa327bd03d0736426c276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:27 +0000

    Add package orange-widget-base/4.0.1/orange_widget_base-4.0.1-py3-none-any.whl

---
+++ packages/orange-widget-base/orange_widget_base-4.0.1-py3-none-any.whl/orangewidget/tests/test_gui.py
@@ -0,0 +1,30 @@
+from AnyQt.QtCore import Qt
+
+from orangewidget import gui
+from orangewidget.tests.base import GuiTest
+from orangewidget.widget import OWBaseWidget
+
+
+class TestDoubleSpin(GuiTest):
+    # make sure that the gui element does not crash when
+    # 'checked' parameter is forwarded, ie. is not None
+    def test_checked_extension(self):
+        widget = OWBaseWidget()
+        widget.some_param = 0
+        widget.some_option = False
+        gui.doubleSpin(widget=widget, master=widget, value="some_param",
+                       minv=1, maxv=10, checked="some_option")
+
+
+class TestFloatSlider(GuiTest):
+
+    def test_set_value(self):
+        w = gui.FloatSlider(Qt.Horizontal, 0., 1., 0.5)
+        w.setValue(1)
+        # Float slider returns value divided by step
+        # 1/0.5 = 2
+        self.assertEqual(w.value(), 2)
+        w = gui.FloatSlider(Qt.Horizontal, 0., 1., 0.05)
+        w.setValue(1)
+        # 1/0.05 = 20
+        self.assertEqual(w.value(), 20)
---
+++ packages/orange-widget-base/orange_widget_base-4.0.1-py3-none-any.whl/orangewidget/version.py
@@ -0,0 +1,10 @@
+# THIS FILE IS GENERATED FROM ORANGE-WIDGET-BASE SETUP.PY
+short_version = '4.0.1'
+version = '4.0.1'
+full_version = '4.0.1'
+git_revision = 'ebbcf58661f9cea9c63a7397aab05a2459e7d0a4'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
