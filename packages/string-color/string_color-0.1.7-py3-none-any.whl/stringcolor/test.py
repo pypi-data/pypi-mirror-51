b5762cd00cf78af4689b77f55e865f57303b7fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:38 +0000

    Add package string-color/0.1.7/string_color-0.1.7-py3-none-any.whl

---
+++ packages/string-color/string_color-0.1.7-py3-none-any.whl/stringcolor/test.py
@@ -0,0 +1,8 @@
+from main import cs, underline, bold
+print(cs("warning!", "yellow", "#ff0000"))
+print(cs("hello world", "red"))
+print(cs("here we go", "orchid"))
+print(cs("away to space!", "DeepPink3"))
+print(cs("final fantasy", "#ffff87"))
+print(bold(cs("purple number 4, bold", "purple4")))
+print(underline(cs("blue, underlined", "blue")))
