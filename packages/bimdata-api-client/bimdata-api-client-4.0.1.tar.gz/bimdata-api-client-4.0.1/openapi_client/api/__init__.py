d7a798b24ec6916285c221aeb60c2da4031f3422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:21 +0000

    Add package bimdata-api-client/4.0.1/bimdata-api-client-4.0.1.tar.gz

---
+++ packages/bimdata-api-client/bimdata-api-client-4.0.1.tar.gz/bimdata-api-client-4.0.1/openapi_client/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.application_api import ApplicationApi
+from openapi_client.api.bcf_api import BcfApi
+from openapi_client.api.checkplan_api import CheckplanApi
+from openapi_client.api.cloud_api import CloudApi
+from openapi_client.api.ifc_api import IfcApi
+from openapi_client.api.project_api import ProjectApi
+from openapi_client.api.user_api import UserApi
