63a1dac6e4f3e02d31aed10bb389857b4e9e6fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:07 +0000

    Add package newsworthycharts/1.6.4/newsworthycharts-1.6.4.tar.gz

---
+++ packages/newsworthycharts/newsworthycharts-1.6.4.tar.gz/newsworthycharts-1.6.4/newsworthycharts/lib/color_fn.py
@@ -0,0 +1,33 @@
+""" Functions for deteriming colors accoriding to some rule.
+"""
+
+def color_fn(func):
+    """ Wrapper that validates respones
+    """
+    def validate_response(*args, **kwargs):
+        """ Check that returned value is valid
+        """
+        resp = func(*args, **kwargs)
+        named_colors = ["neutral", "strong", "positive", "negative"]
+        if resp not in named_colors:
+            # TODO: Also allow valid HEX colors
+            raise Exception("{} is not a valid color name".format(resp))
+        return resp
+
+    return validate_response
+
+@color_fn
+def positive_negative(value):
+    """ Return positive/negative color based on a value being
+    above/below 0.
+    """
+    if value is None:
+        color_name = "neutral"
+    elif value < 0:
+        color_name = "negative"
+    elif value > 0:
+        color_name = "positive"
+    else:
+        color_name = "neutral"
+
+    return color_name
