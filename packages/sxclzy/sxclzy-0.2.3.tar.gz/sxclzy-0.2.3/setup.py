ac84be5677c3b31d7f197773c8a4162884e738d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package sxclzy/0.2.3/sxclzy-0.2.3.tar.gz

---
+++ packages/sxclzy/sxclzy-0.2.3.tar.gz/sxclzy-0.2.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sxclzy",
+    version="0.2.3",
+    author="GuardianAngel",
+    author_email="zhling2012@live.com",
+    description="a tool to schedule your functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GuardianGH/sxclzy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'psutil>=5.6.2',
+        'prettytable>=0.7.2',
+        'SQLAlchemy>=1.3.3',
+        'dill>=0.3.0',
+    ]
+)
