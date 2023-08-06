93084102915803b535869e1e5701b36417c01112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:31 +0000

    Add package pyBabyMaker/0.2.2/pyBabyMaker-0.2.2-py3.7-linux-x86_64.egg

---
+++ packages/pyBabyMaker/pyBabyMaker-0.2.2-py3.7-linux-x86_64.egg/pyBabyMaker/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#
+# Author: Yipeng Sun <syp at umd dot edu>
+# License: BSD 2-clause
+# Last Change: Wed Sep 04, 2019 at 06:30 PM -0400
+
+name = 'pyBabyMaker'
+version = '0.2.2'
