0e2760ff50cfe8f3625663d9e230cc63b50bea97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package runjob/1.2.0/runjob-1.2.0.tar.gz

---
+++ packages/runjob/runjob-1.2.0.tar.gz/runjob-1.2.0/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup
+from src.version import __version__
+
+def getdes():
+    des = ""
+    with open(os.path.join(os.getcwd(),"README.md")) as fi:
+        des = fi.read()
+    return des
+
+setup(
+    name="runjob",
+    version=__version__,
+    packages=["runjob"],
+    package_dir={"runjob": "src"},
+    data_files=[("doc", ["doc/jobA.log", "doc/jobB.log", "doc/example.job"]), ],
+    license="BSD",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    long_description = getdes(),
+    entry_points={
+        'console_scripts': [
+            'runjob = runjob.main:main'
+        ]
+    }
+)
