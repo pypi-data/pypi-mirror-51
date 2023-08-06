671ebbedae4c269ab239257197925e3e459dba79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:25 +0000

    Add package darksky_weather/1.5.0/darksky_weather-1.5.0.tar.gz

---
+++ packages/darksky_weather/darksky_weather-1.5.0.tar.gz/darksky_weather-1.5.0/darksky/base.py
@@ -0,0 +1,41 @@
+import pytz
+from datetime import datetime
+
+from .utils import undo_snake_case_key, get_datetime_from_unix
+
+
+class BaseWeather(object):
+    summary: str
+    icon: str
+    data_class: object
+
+    def __init__(self, summary=None, icon=None, data=None, timezone=None):
+        self.summary = summary
+        self.icon = icon
+
+        assert self.data_class is not None
+        self.data = []
+        for item in (data or []):
+            item['timezone'] = timezone
+            self.data.append(
+                self.data_class(**item)
+            )
+
+
+class AutoInit(object):
+    def __init__(self, **params):
+        try:
+            timezone = pytz.timezone(params.pop('timezone', None))
+        except (pytz.UnknownTimeZoneError, AttributeError):
+            timezone = pytz.UTC
+
+        fields = [key for key in self.__annotations__]
+        for field in fields:
+            api_field = undo_snake_case_key(field)
+            if self.__annotations__[field] == datetime:
+                params[api_field] = get_datetime_from_unix(params.get(api_field), timezone)
+
+            if api_field in params:
+                setattr(self, field, params.get(api_field))
+            else:
+                setattr(self, field, None)
