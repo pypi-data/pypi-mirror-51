9e769b4a811d46d86ac5b20cf8a910f08c12fbb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:48 +0000

    Add package weatherr-appp/0.1/weatherr_appp-0.1.tar.gz

---
+++ packages/weatherr-appp/weatherr_appp-0.1.tar.gz/weatherr_appp-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+     name='weatherr_appp',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['hiii']                  # The name of your scipt, and also the command you'll be using for calling it
+ )
