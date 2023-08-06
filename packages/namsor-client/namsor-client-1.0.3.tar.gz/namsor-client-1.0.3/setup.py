ae0a5523fd865cafefd63b4cac8fbd47ca82fa10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:33 +0000

    Add package namsor-client/1.0.3/namsor-client-1.0.3.tar.gz

---
+++ packages/namsor-client/namsor-client-1.0.3.tar.gz/namsor-client-1.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="namsor-client",
+    version="1.0.3",
+    author="Joseph Pallipadan",
+    author_email="pallipadanjoseph@gmail.com",
+    description="A Wrapper for the Namsor Classification API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['Gender', 'Classify', 'Ethnicity', 'Origin',
+              'Country', 'Classifier', 'Namsor', 'Wrapper', 'Name to Gender', 'Name to Ethnicity', 'Name to Origin', 'Name'],
+    url="https://github.com/JosephPallipadan/Namsor-API-Wrapper-Python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests', 'XlsxWriter'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
