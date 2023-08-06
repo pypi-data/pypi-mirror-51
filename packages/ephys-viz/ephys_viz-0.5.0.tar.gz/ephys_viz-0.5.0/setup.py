93d56da8adefcb195bc5b677dcc803224f7234e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:22 +0000

    Add package ephys-viz/0.5.0/ephys_viz-0.5.0.tar.gz

---
+++ packages/ephys-viz/ephys_viz-0.5.0.tar.gz/ephys_viz-0.5.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+pkg_name = "ephys_viz"
+
+setuptools.setup(
+    name=pkg_name,
+    version="0.5.0",
+    author="Jeremy Magland",
+    description="Neurophysiology visualization widgets",
+    packages=setuptools.find_packages(),
+    scripts=[],
+    include_package_data=True,
+    install_requires=[
+        'reactopya',
+        'simplejson',
+        'jupyter',
+        'numpy',
+        'mountaintools',
+        'spikeforest',
+        'scipy'
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
