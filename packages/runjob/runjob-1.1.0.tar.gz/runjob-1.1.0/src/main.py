309f8331e32f8c0673df9bed06ae4960a1de012a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package runjob/1.1.0/runjob-1.1.0.tar.gz

---
+++ packages/runjob/runjob-1.1.0.tar.gz/runjob-1.1.0/src/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+# coding:utf-8
+
+import os
+import sys
+import time
+import signal
+import argparse
+
+from subprocess import call, PIPE
+from threading import Thread
+from datetime import datetime
+
+from qsub import qsub
+from version import __version__
+
+
+class ParseSingal(Thread):
+    def __init__(self):
+        super(ParseSingal, self).__init__()
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+    def run(self):
+        time.sleep(1)
+
+    def signal_handler(self, signum, frame):
+        call('qdel "*_%d"' % os.getpid(), shell=True, stderr=PIPE, stdout=PIPE)
+        sys.exit(signum)
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(
+        description="For manger submit your jobs in a job file.")
+    parser.add_argument("-n", "--num", type=int,
+                        help="the max job number runing at the same time, default: 1000", default=1000, metavar="<int>")
+    parser.add_argument("-j", "--jobfile", type=str, required = True,
+                        help="the input jobfile", metavar="<jobfile>")
+    parser.add_argument('-v', '--version',
+                        action='version', version="%(prog)s v" + __version__)
+    parser.add_argument('-i', '--injname', help="job names you need to run, default: all jobnames of you job file",
+                        nargs="*", type=str, metavar="<str>")
+    parser.add_argument('-s', '--start', help="job beginning with the number you given, 1 by default",
+                        type=int, default=1, metavar="<int>")
+    parser.add_argument('-e', '--end', help="job ending with the number you given, last job by default",
+                        type=int, metavar="<int>")
+    return parser.parse_args()
+
+
+def main():
+    args = parseArgs()
+    h = ParseSingal()
+    h.start()
+    qjobs = qsub(args.jobfile, args.num, args.injname, args.start, args.end)
+    qjobs.run()
+    if len(qjobs.error) == 0:
+        print("[%s] All tesks(%d, %d) in file (%s) finished successfully." %
+              (datetime.today().isoformat(), len(qjobs.success), len(qjobs.thisjobs), os.path.abspath(qjobs.jfile)))
+    else:
+        print "[%s] All tesks(%d, %d) in file (%s) finished, But there are ERROR tesks." % (
+            datetime.today().isoformat(), len(qjobs.success), len(qjobs.thisjobs), os.path.abspath(qjobs.jfile))
+        print "Success jobs: %d" % len(qjobs.success)
+        print "Error jobs: %d" % len(qjobs.error)
+
+
+if __name__ == "__main__":
+    main()
