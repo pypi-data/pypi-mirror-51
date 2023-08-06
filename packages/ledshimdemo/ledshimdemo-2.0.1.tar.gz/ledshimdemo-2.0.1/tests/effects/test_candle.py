dc0f75e49dbaa1f12b4d531eee831679b4533421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:55 +0000

    Add package ledshimdemo/2.0.1/ledshimdemo-2.0.1.tar.gz

---
+++ packages/ledshimdemo/ledshimdemo-2.0.1.tar.gz/ledshimdemo-2.0.1/tests/effects/test_candle.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import mock
+import sys
+
+sys.modules['smbus'] = mock.Mock()  # Mock the hardware layer to avoid errors.
+
+from ledshimdemo.canvas import Canvas
+from ledshimdemo.effects.candle import CandleEffect
+
+
+class TestCandle(TestCase):
+
+    TEST_CANVAS_SIZE = 28  # type: int
+
+    def test_effect(self):
+        canvas = Canvas(self.TEST_CANVAS_SIZE)
+        effect = CandleEffect(canvas)
+        effect.compose()
+        self.assertEqual(repr(effect), "Candle(Candle Size:14, Flame Size:14)")
---
+++ packages/ledshimdemo/ledshimdemo-2.0.1.tar.gz/ledshimdemo-2.0.1/tests/effects/test_solid_colours.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+import mock
+import sys
+
+sys.modules['smbus'] = mock.Mock()  # Mock the hardware layer to avoid errors.
+
+from ledshimdemo.canvas import Canvas
+from ledshimdemo.effects.solid_colours import SolidColoursEffect
+
+
+class TestSolidColours(TestCase):
+
+    TEST_CANVAS_SIZE = 3  # type: int
+
+    def test_effect(self):
+        canvas = Canvas(self.TEST_CANVAS_SIZE)
+        effect = SolidColoursEffect(canvas)
+        effect.compose()
+        self.assertEqual(repr(effect), "SolidColours(Step:1)")
