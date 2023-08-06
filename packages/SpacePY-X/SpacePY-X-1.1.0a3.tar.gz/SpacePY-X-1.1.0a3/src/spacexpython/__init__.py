e8de6de419d8103de80baa949d250f7a479fe3a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:05 +0000

    Add package SpacePY-X/1.1.0a3/SpacePY-X-1.1.0a3.tar.gz

---
+++ packages/SpacePY-X/SpacePY-X-1.1.0a3.tar.gz/SpacePY-X-1.1.0a3/src/spacexpython/__init__.py
@@ -0,0 +1,15 @@
+# Console entry point
+
+from . import capsules
+from . import cores
+from . import dragons
+from . import history
+from . import info
+from . import landingpads
+from . import launches
+from . import launchpads
+from . import missions
+from . import payloads
+from . import rockets
+from . import urldata
+from . import utils
