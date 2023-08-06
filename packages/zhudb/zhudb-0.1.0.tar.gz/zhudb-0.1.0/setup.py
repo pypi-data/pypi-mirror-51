b4cb4def80b800c6a70461c3ac58c9d4594df249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:28 +0000

    Add package zhudb/0.1.0/zhudb-0.1.0.tar.gz

---
+++ packages/zhudb/zhudb-0.1.0.tar.gz/zhudb-0.1.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='zhudb',
+    version='0.1.0',
+    author='bamboo',
+    author_email='wenzhu_art@hotmail.com',
+    url='https://github.com/wenzhuart/zhudb',
+    description=u'A sql tool in python3',
+    packages=['zhudb'],
+    install_requires=['sqlite3', 'pymysql']
+)
---
+++ packages/zhudb/zhudb-0.1.0.tar.gz/zhudb-0.1.0/zhudb/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# 20160119 - 04 Sep 2018 (Tue)
+from zhudb.mysql import ZhuMysql
+from zhudb.sqlite import ZhuSqlite
