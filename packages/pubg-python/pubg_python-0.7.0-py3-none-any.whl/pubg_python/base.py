d5dd8d768cd26934da1428a7b6863fae292c3b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:11 +0000

    Add package pubg-python/0.7.0/pubg_python-0.7.0-py3-none-any.whl

---
+++ packages/pubg-python/pubg_python-0.7.0-py3-none-any.whl/pubg_python/base.py
@@ -0,0 +1,64 @@
+from . import exceptions
+from .clients import (
+    APIClient,
+    TelemetryClient,
+)
+from .domain.base import Shard
+from .domain.telemetry.base import Telemetry
+from .querysets import QuerySet
+
+
+def shardful_endpoint(f):
+    def wrapper(self, *args, **kwargs):
+        return QuerySet(self.api_client, self.shard_url.join(f.__name__))
+    return wrapper
+
+
+def endpoint(f):
+    def wrapper(self, *args, **kwargs):
+        return QuerySet(
+            self.api_client, self.api_client.url.copy().join(f.__name__))
+    return wrapper
+
+
+class PUBG:
+
+    def __init__(self, api_key, shard=None):
+        self.shard = shard
+        self.api_client = APIClient(api_key)
+        self.telemetry_client = TelemetryClient()
+
+    @property
+    def shard(self):
+        return self._shard
+
+    @shard.setter
+    def shard(self, value):
+        if not isinstance(value, Shard):
+            raise exceptions.InvalidShardError('Invalid Shard')
+        self._shard = value
+
+    @property
+    def shard_url(self):
+        return self.api_client.url.copy().join(
+            'shards/{}/'.format(self.shard.value))
+
+    @shardful_endpoint
+    def matches(self):
+        pass
+
+    @shardful_endpoint
+    def players(self):
+        pass
+
+    @shardful_endpoint
+    def samples(self):
+        pass
+
+    @endpoint
+    def tournaments(self):
+        pass
+
+    def telemetry(self, url):
+        data = self.telemetry_client.request(url)
+        return Telemetry(data, url)
