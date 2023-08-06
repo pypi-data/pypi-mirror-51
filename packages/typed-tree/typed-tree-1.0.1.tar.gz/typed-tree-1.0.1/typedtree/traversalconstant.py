8df7792ce2881aa9a3368c0e2506eb0baf688d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:27 +0000

    Add package typed-tree/1.0.1/typed-tree-1.0.1.tar.gz

---
+++ packages/typed-tree/typed-tree-1.0.1.tar.gz/typed-tree-1.0.1/typedtree/traversalconstant.py
@@ -0,0 +1,13 @@
+"""
+TraversalConstant enumeration.
+
+May 01, 2019
+Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
+"""
+
+from enum import Enum
+
+
+class TraversalConstant(Enum):
+    DEPTH = 1
+    BREADTH = 2
---
+++ packages/typed-tree/typed-tree-1.0.1.tar.gz/typed-tree-1.0.1/typedtree/tree.py
@@ -0,0 +1,79 @@
+"""
+Tree class.
+
+May 01, 2019
+Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
+"""
+
+from typing import Dict, Optional, Generator, Any
+
+from typedtree.node import Node, Edge
+from typedtree.traversalconstant import TraversalConstant
+
+
+class Tree:
+    def __init__(self) -> None:
+        self.__nodes: Dict[str, Node] = {}
+
+    @property
+    def nodes(self) -> Dict[str, Node]:
+        return self.__nodes
+
+    def add_node(
+        self,
+        identifier: str,
+        parent_pointer: Optional[str] = None,
+        node_type: Optional[str] = None,
+        edge_type: Optional[str] = None,
+        payload: Optional[Any] = None,
+    ) -> Node:
+
+        parent = None
+        if parent_pointer is not None:
+            parent = Edge(parent_pointer, edge_type)
+            self[parent_pointer].add_child(Edge(identifier, edge_type))
+
+        node = Node(identifier, parent=parent, type=node_type, payload=payload)
+        self[identifier] = node
+
+        return node
+
+    def display(self, identifier: str, depth: int = 0) -> None:
+        node = self[identifier]
+
+        node_type = node.type if node.type else "*Undefined*"
+        edge_type = node.parent.type or "*Undefined*" if node.parent else "*Undefined*"
+
+        if depth == 0:
+            print(f"{node.identifier} [{node_type}] - ({edge_type})")
+        else:
+            print("\t" * depth, f"{node.identifier} [{node_type}] - ({edge_type})")
+
+        depth += 1
+        for child in node.children:
+            self.display(child.pointer, depth)  # Recursive call
+
+    def traverse(
+        self, identifier: str, mode: TraversalConstant = TraversalConstant.DEPTH
+    ) -> Generator:
+        # Loosely based on an algorithm from 'Essential LISP' by John R. Anderson, Albert T. Corbett
+        # and Brian J. Reiser, page 239-241
+
+        yield identifier
+        queue = self[identifier].children
+        while queue:
+            yield queue[0].pointer
+            expansion = self[queue[0].pointer].children
+            if mode is TraversalConstant.DEPTH:
+                queue = expansion + queue[1:]  # Depth-first traversal
+            elif mode is TraversalConstant.BREADTH:
+                queue = queue[1:] + expansion  # Breadth-first traversal
+
+    def __getitem__(self, key: str) -> Node:
+        return self.__nodes[key]
+
+    def __setitem__(self, key: str, item: Node) -> None:
+        self.__nodes[key] = item
+
+    def __len__(self) -> int:
+        return len(self.__nodes)
