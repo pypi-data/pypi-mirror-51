2a0a951e9a2616d33bf9a3e2508984da66e91e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:20 +0000

    Add package antiafk/1.0.0/antiafk-1.0.0.tar.gz

---
+++ packages/antiafk/antiafk-1.0.0.tar.gz/antiafk-1.0.0/antiafk/keyboard.py
@@ -0,0 +1,18 @@
+from pynput.mouse import Key, Controller
+
+
+class Keyboard(object):
+    def __init__(self, button):
+        self.button = button
+
+    def __repr__(self):
+        return f''
+
+    def __str__(self):
+        return f''
+
+    def trigger(self):
+        try:
+            pass
+        except Exception as e:
+            raise e
---
+++ packages/antiafk/antiafk-1.0.0.tar.gz/antiafk-1.0.0/antiafk/mouse.py
@@ -0,0 +1,18 @@
+from pynput.mouse import Mouse, Controller
+
+
+class Mouse(object):
+    def __init__(self, button):
+        self.button = button
+
+    def __repr__(self):
+        return f''
+
+    def __str__(self):
+        return f''
+
+    def trigger(self):
+        try:
+            pass
+        except Exception as e:
+            raise e
