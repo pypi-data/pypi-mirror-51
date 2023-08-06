8270594af6744b8ac08af330587c4317e4c82132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package access-modifiers/0.3.1/access_modifiers-0.3.1-py3-none-any.whl

---
+++ packages/access-modifiers/access_modifiers-0.3.1-py3-none-any.whl/access_modifiers/access_modifiers.py
@@ -0,0 +1,70 @@
+"""Access modifiers for Python."""
+
+from functools import wraps
+from sys import _getframe as getframe
+from typing import Callable, TypeVar
+
+
+class AccessException(Exception):
+    """Exception raised when a private or protected method is called from outside the class."""
+
+
+ReturnType = TypeVar('ReturnType')
+
+
+_CHECK_ACCESS = True
+
+
+def disable() -> None:
+    """Disable all access checks. Needs to be invoked before the decorators are evaluated."""
+    global _CHECK_ACCESS  # pylint: disable=global-statement
+    _CHECK_ACCESS = False
+
+
+def enable() -> None:
+    """Enable all access checks. For testing purposes."""
+    global _CHECK_ACCESS  # pylint: disable=global-statement
+    _CHECK_ACCESS = True
+
+
+def privatemethod(method: Callable[..., ReturnType]) -> Callable[..., ReturnType]:
+    """Decorator that creates a private method."""
+    if not _CHECK_ACCESS:
+        return method
+    method_class_qualname = getframe(1).f_locals.get("__qualname__")
+    @wraps(method)
+    def private_method_wrapper(*args, **kwargs) -> ReturnType:
+        """Wrap the original method to make it private."""
+        caller_frame = getframe(1)
+        caller_code = caller_frame.f_code
+        caller_name = caller_code.co_name
+        while caller_name.startswith("<") and caller_frame.f_back:
+            # Code is a <lambda>, <dictcomp>, <listcomp>, or other non-method code block
+            caller_frame = caller_frame.f_back
+            caller_code = caller_frame.f_code
+            caller_name = caller_code.co_name
+        caller_class = caller_frame.f_locals.get("self").__class__
+        # Look up the caller method to see if it's defined in the same class as the wrapped method
+        classes = [cls for cls in caller_class.mro() if caller_name in cls.__dict__]
+        for cls in classes:
+            caller = cls.__dict__[caller_name]
+            caller = caller.__dict__["__wrapped__"] if "__wrapped__" in caller.__dict__ else caller
+            if caller.__code__ == caller_code and method_class_qualname == cls.__qualname__:
+                return method(*args, **kwargs)
+        raise AccessException(f"Attempted call to private method {method} from outside its class")
+    return private_method_wrapper
+
+
+def protectedmethod(method: Callable[..., ReturnType]) -> Callable[..., ReturnType]:
+    """Decorator that creates a protected method."""
+    if not _CHECK_ACCESS:
+        return method
+    @wraps(method)
+    def protected_method_wrapper(*args, **kwargs) -> ReturnType:
+        """Wrap the original method to make it protected."""
+        caller_frame = getframe(1)
+        caller_instance = caller_frame.f_locals.get("self")
+        if caller_instance is not args[0]:
+            raise AccessException(f"Attempted call to protected method {method} from another object")
+        return method(*args, **kwargs)
+    return protected_method_wrapper
