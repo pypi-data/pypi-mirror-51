a66f1c43395c492bc4baeeec697914d5389b82c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:25 +0000

    Add package forex-types/0.0.5/forex-types-0.0.5.tar.gz

---
+++ packages/forex-types/forex-types-0.0.5.tar.gz/forex-types-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="forex-types",
+    version="0.0.5",
+    author="Andrew Allaire",
+    author_email="andrew.allaire@gmail.com",
+    description="Basic Forex classes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aallaire/python_forex_types",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
