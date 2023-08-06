37ce60c7db401bfc548794bdb71562723351f4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:58 +0000

    Add package pyopencell/0.2.0/pyopencell-0.2.0-py2.py3-none-any.whl

---
+++ packages/pyopencell/pyopencell-0.2.0-py2.py3-none-any.whl/pyopencell/responses/paging.py
@@ -0,0 +1,20 @@
+class Paging:
+    fullTextFilter = ""
+    filters = {}
+    fields = ""
+    offset = 0
+    limit = 0
+    sortBy = "id"
+    sortOrder = "ASCENDING"  # "ASCENDING" or "DESCENDING"
+    totalNumberOfRecords = 0
+
+    def __init__(self, fullTextFilter="", filters={}, fields="", offset=0, limit=0,
+                 sortBy="id", sortOrder="ASCENDING", totalNumberOfRecords=0):
+        self.fullTextFilter = fullTextFilter
+        self.filters = filters
+        self.fields = fields
+        self.offset = offset
+        self.limit = limit
+        self.sortBy = sortBy
+        self.sortOrder = sortOrder
+        self.totalNumberOfRecords = totalNumberOfRecords
