151e3b9c30ebf62cc51c91887de05990726f2cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:34 +0000

    Add package modin/0.6.0/modin-0.6.0.tar.gz

---
+++ packages/modin/modin-0.6.0.tar.gz/modin-0.6.0/modin/engines/ray/generic/frame/partition_manager.py
@@ -0,0 +1,28 @@
+import ray
+import numpy as np
+
+from modin.engines.base.frame.partition_manager import BaseFrameManager
+
+
+class RayFrameManager(BaseFrameManager):
+    """This method implements the interface in `BaseFrameManager`."""
+
+    @classmethod
+    def to_numpy(cls, partitions):
+        """Convert this object into a NumPy Array from the partitions.
+
+        Returns:
+            A NumPy Array
+        """
+        parts = ray.get(
+            [
+                obj.apply(lambda df: df.to_numpy()).oid
+                for row in partitions
+                for obj in row
+            ]
+        )
+        n = partitions.shape[1]
+        parts = [parts[i * n : (i + 1) * n] for i in list(range(partitions.shape[0]))]
+
+        arr = np.block(parts)
+        return arr
---
+++ packages/modin/modin-0.6.0.tar.gz/modin-0.6.0/setup.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="modin",
+    version="0.6.0",
+    description="Modin: Make your pandas code run faster by changing one line of code.",
+    packages=find_packages(),
+    url="https://github.com/modin-project/modin",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["pandas==0.25.1", "ray==0.7.3"],
+    extras_require={
+        # can be installed by pip install modin[dask]
+        "dask": ["dask>=2.1.0", "distributed>=2.3.2"]
+    },
+)
