525dec9c98b42a1b0ae72b89ebb4714e5909316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:16 +0000

    Add package python-ranges/0.1.3/python-ranges-0.1.3.tar.gz

---
+++ packages/python-ranges/python-ranges-0.1.3.tar.gz/python-ranges-0.1.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-ranges",
+    version="0.1.3",
+    author="Louis Jacobowitz",
+    author_email="ldjacobowitzer@gmail.com",
+    description="Continuous Range, RangeSet, and RangeDict data structures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/superbird11/ranges",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        'Documentation': 'https://python-ranges.readthedocs.io/en/latest/',
+        'GitHub': 'https://github.com/Superbird11/ranges',
+    },
+    python_requires='>=3.6',
+)
