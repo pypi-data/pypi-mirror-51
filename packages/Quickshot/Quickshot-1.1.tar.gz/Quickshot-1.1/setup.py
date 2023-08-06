beec7dbb34dcb1b88c2e80e9c43a83bcb4073f31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:39 +0000

    Add package Quickshot/1.1/Quickshot-1.1.tar.gz

---
+++ packages/Quickshot/Quickshot-1.1.tar.gz/Quickshot-1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='Quickshot',
+    version='1.1',
+    author="Shawn Eisenach",
+    author_email="shawn@wholewheattoast.com",
+    packages=setuptools.find_packages(),
+    description="A simple tool to visdiff two web pages.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+)
