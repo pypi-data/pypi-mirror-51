6ee6c91fb2e42ca578f5daf1f67d25c64743a5ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:49 +0000

    Add package kedro-viz/2.1.1/kedro-viz-2.1.1.tar.gz

---
+++ packages/kedro-viz/kedro-viz-2.1.1.tar.gz/kedro-viz-2.1.1/kedro_viz/html/precache-manifest.13b0e4c51da9853ac72bf4de7c2d55de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d062ab0ffb6d29defad1e71f7693d53d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9c4a222019e8850e3642",
+    "url": "./static/css/2.d438ea0b.chunk.css"
+  },
+  {
+    "revision": "28dc83facd0aeeeef1a2",
+    "url": "./static/css/main.43fae2c5.chunk.css"
+  },
+  {
+    "revision": "9c4a222019e8850e3642",
+    "url": "./static/js/2.108e1c0c.chunk.js"
+  },
+  {
+    "revision": "28dc83facd0aeeeef1a2",
+    "url": "./static/js/main.14c53502.chunk.js"
+  },
+  {
+    "revision": "5e453e62ba3764ff3b95",
+    "url": "./static/js/runtime~main.55ceb434.js"
+  },
+  {
+    "revision": "6259cc84bb236512f69bc1d20397254c",
+    "url": "./static/media/brodyquest.6259cc84.mp3"
+  },
+  {
+    "revision": "7498697619bed8bcf479fc4d8648b5b4",
+    "url": "./static/media/delete.74986976.svg"
+  },
+  {
+    "revision": "e702bd5fc3663eda7c4459232f5a76c0",
+    "url": "./static/media/easter-egg.e702bd5f.png"
+  }
+]);
