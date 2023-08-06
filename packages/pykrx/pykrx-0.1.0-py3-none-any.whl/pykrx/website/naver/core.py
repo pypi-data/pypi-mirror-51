e6fd219b94e006e91746b09404d4bfe41cb173e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:27 +0000

    Add package pykrx/0.1.0/pykrx-0.1.0-py3-none-any.whl

---
+++ packages/pykrx/pykrx-0.1.0-py3-none-any.whl/pykrx/website/naver/core.py
@@ -0,0 +1,33 @@
+from pykrx.website.comm.webio import Webio
+from pandas import DataFrame
+
+
+class NaverWebIo(Webio):
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def base_url(self):
+        return "https://fchart.stock.naver.com"
+
+
+class Sise(NaverWebIo):
+    @property
+    def uri(self):
+        return "/sise.nhn"
+
+    def read(self, ticker, count, timeframe='day'):
+        """
+        :param ticker:
+        :param count:
+        :param timeframe: day/week/month
+        :return:
+        """
+        result = self.get(symbol=ticker, timeframe=timeframe, count=count,
+                          requestType="0")
+        return result.text
+
+
+if __name__ == "__main__":
+    r = Sise().read("006800", 10, "week")
+    print(r)
