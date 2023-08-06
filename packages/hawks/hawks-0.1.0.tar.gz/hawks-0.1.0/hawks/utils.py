4513fce7b60cab3517fc17f320617b9472b93fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:00 +0000

    Add package hawks/0.1.0/hawks-0.1.0.tar.gz

---
+++ packages/hawks/hawks-0.1.0.tar.gz/hawks-0.1.0/hawks/utils.py
@@ -0,0 +1,66 @@
+"""
+Functions to help with error handling and generally support everything else. Could be integrated into the Generator, but sometimes can be used outside that.
+"""
+import json
+from datetime import datetime
+from pathlib import Path
+from functools import reduce
+
+def load_json(fname, subdict=None):
+    try:
+        with open(fname) as json_file:
+            params = json.load(json_file)
+    except ValueError as e:
+        raise Exception(f"Unable to load file {fname}") from e
+    if subdict is None:
+        return params
+    else:
+        return params[subdict]
+
+def get_key_paths(d, key_paths=None, param_lists=None, acc=None):
+    """Used to traverse a config and identify where multiple parameters are given
+    """
+    # Avoid mutable default argument issue for first call
+    if key_paths is None:
+        key_paths = []
+    if param_lists is None:
+        param_lists = []
+    if acc is None:
+        acc = []
+    # Loop over the items
+    for k, v in d.items():
+        if isinstance(v, dict):
+            get_key_paths(v, key_paths, param_lists, acc=acc + [k])
+        elif isinstance(v, list):
+            key_paths.append(acc + [k])
+            param_lists.append(v)
+    return key_paths, param_lists
+
+def set_key_path(d, key_path, v):
+    """Used to set the parameter of a multiconfig to a single, given value
+    """
+    d1 = reduce(dict.get, key_path[:-1], d)
+    d1[key_path[-1]] = v
+
+def df_to_csv(df, path, filename):
+    # Check that the folder provided is a path
+    if isinstance(path, Path):
+        # Make the directory if needed
+        path.mkdir(parents=True, exist_ok=True)
+    else:
+        # Make it a path
+        path = Path(path)
+        # Make it a directory if it's not
+        if not path.is_dir():
+            path.mkdir(parents=True)
+    # Save to csv via pandas
+    df.to_csv(
+        path / f"{filename}.csv",
+        sep=",",
+        index=False
+    )
+
+def get_date():
+    """Used to get get and format current date, to name folders when no name is given
+    """
+    return datetime.today().strftime('%Y_%m_%d-%H%M%S')
---
+++ packages/hawks/hawks-0.1.0.tar.gz/hawks-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        name='hawks',
+        version='0.1.0',
+        author='Cameron Shand',
+        author_email='cameron.shand@manchester.ac.uk',
+        packages=['hawks'],
+        url='https://github.com/sea-shunned/hawks',
+        license='MIT License',
+        description='A package for generating synthetic clusters, with parameters to customize different aspects of the complexity of the cluster structure',
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        include_package_data=True,
+        python_requires='>=3.6',
+        install_requires=[
+            "deap == 1.2.2",
+            "matplotlib >= 3.0",
+            "numpy >= 1.15",
+            "pandas >= 0.23",
+            "scikit-learn >= 0.20",
+            "scipy >= 1.1",
+            "seaborn >= 0.9.0",
+            "tqdm >= 4.15",
+        ],
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 3.6",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence"
+        ]
+    )
