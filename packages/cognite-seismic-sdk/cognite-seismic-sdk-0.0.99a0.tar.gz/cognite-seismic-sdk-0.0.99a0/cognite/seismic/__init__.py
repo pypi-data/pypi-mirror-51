1afc9e1ba0736a96f1e8ee40db66f5a2175c1c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:12 +0000

    Add package cognite-seismic-sdk/0.0.99a0/cognite-seismic-sdk-0.0.99a0.tar.gz

---
+++ packages/cognite-seismic-sdk/cognite-seismic-sdk-0.0.99a0.tar.gz/cognite-seismic-sdk-0.0.99a0/cognite/seismic/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+from cognite.seismic._api_client import CogniteSeismicClient
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "protocompiled")))
+
+
+__version__ = "0.0.99a"
