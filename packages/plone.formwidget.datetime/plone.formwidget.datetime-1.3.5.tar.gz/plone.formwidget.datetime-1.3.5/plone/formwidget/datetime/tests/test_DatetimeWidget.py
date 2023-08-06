ee60fa14141a2e33e647de84bf091e2c4044578b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:30 +0000

    Add package plone.formwidget.datetime/1.3.5/plone.formwidget.datetime-1.3.5.tar.gz

---
+++ packages/plone.formwidget.datetime/plone.formwidget.datetime-1.3.5.tar.gz/plone.formwidget.datetime-1.3.5/plone/formwidget/datetime/tests/test_DatetimeWidget.py
@@ -0,0 +1,165 @@
+from datetime import datetime
+from zope import schema
+
+import mock
+import unittest2 as unittest
+
+
+class TestDatetimeWidget(unittest.TestCase):
+
+    def createInstance(self):
+        from plone.formwidget.datetime.z3cform.widget import DatetimeWidget
+        instance = DatetimeWidget(mock.Mock())
+        instance.name = 'field'
+        instance.field = schema.Datetime(title=u'field')
+        return instance
+
+    def test_subclass(self):
+        from plone.formwidget.datetime.z3cform.widget import DatetimeWidget
+        from plone.formwidget.datetime.base import AbstractDatetimeWidget
+        from plone.formwidget.datetime.z3cform.widget import DateWidget
+        self.assertTrue(
+            issubclass(
+                DatetimeWidget,
+                (
+                    AbstractDatetimeWidget,
+                    DateWidget,
+                )
+            )
+        )
+
+    def test_instance_provides__default_not_in_(self):
+        instance = self.createInstance()
+        from plone.formwidget.datetime.z3cform.interfaces import IDatetimeWidget
+        self.assertTrue(IDatetimeWidget.providedBy(instance))
+
+    def test_extract(self):
+        instance = self.createInstance()
+        instance.request = {
+            'field-day': '21',
+            'field-month': '11',
+            'field-year': '2011',
+            'field-hour': '5',
+            'field-minute': '30',
+        }
+        self.assertEqual(
+            instance.extract(),
+            ('2011', '11', '21', '5', '30')
+        )
+
+    def test_extract__default_in_(self):
+        instance = self.createInstance()
+        instance.request = {
+            'field-day': '21',
+            'field-month': '11',
+            'field-year': '2011',
+            'field-hour': '5',
+            'field-minute': '30',
+        }
+        default = mock.Mock()
+        self.assertEqual(
+            instance.extract(default),
+            ('2011', '11', '21', '5', '30')
+        )
+
+    def test_extract__ampm_am(self):
+        instance = self.createInstance()
+        instance.ampm = True
+        instance.request = {
+            'field-day': '21',
+            'field-month': '11',
+            'field-year': '2011',
+            'field-hour': '5',
+            'field-minute': '30',
+            'field-ampm': 'AM'
+        }
+        self.assertEqual(
+            instance.extract(),
+            ('2011', '11', '21', '5', '30')
+        )
+
+    def test_extract__ampm_pm(self):
+        instance = self.createInstance()
+        instance.ampm = True
+        instance.request = {
+            'field-day': '21',
+            'field-month': '11',
+            'field-year': '2011',
+            'field-hour': '5',
+            'field-minute': '30',
+            'field-ampm': 'PM'
+        }
+        self.assertEqual(
+            instance.extract(),
+            ('2011', '11', '21', '17', '30')
+        )
+
+    def test_extract__ampm_ampm(self):
+        instance = self.createInstance()
+        instance.ampm = True
+        instance.request = {
+            'field-day': '21',
+            'field-month': '11',
+            'field-year': '2011',
+            'field-hour': '5',
+            'field-minute': '30',
+            'field-ampm': 'AMPM'
+        }
+        self.assertEqual(
+            instance.extract('default'),
+            'default'
+        )
+
+    @mock.patch('plone.formwidget.datetime.z3cform.widget.DatetimeWidget._dtformatter')
+    def test_extract__default_in_with_error(self, _dtformatter):
+        instance = self.createInstance()
+        instance.request = mock.MagicMock()
+        default = instance.request.get('field-day')
+        from zope.i18n.format import DateTimeParseError
+        instance._dtformatter.parse = mock.Mock(side_effect=DateTimeParseError)
+        self.assertEqual(instance.extract(default), default)
+
+    def test_extract__default_in_without_error(self):
+        instance = self.createInstance()
+        instance.request = {
+            'field-day': '21',
+            'field-month': '11',
+            'field-year': '2011',
+            'field-hour': '5',
+            'field-minute': '30',
+        }
+        self.assertEqual(
+            len(instance.extract()),
+            5
+        )
+
+    def test_extract__no_time(self):
+        instance = self.createInstance()
+        instance.request = {
+            'field-day': '21',
+            'field-month': '11',
+            'field-year': '2011',
+            'field-hour': '',
+            'field-minute': '',
+        }
+        default = mock.Mock()
+        self.assertEqual(
+            instance.extract(default),
+            ('2011', '11', '21', '00', '00')
+        )
+
+    def test_years_range_default(self):
+        instance = self.createInstance()
+        instance.update()
+        self.assertEqual((-10, 10), instance.years_range)
+
+    def test_years_range_min_max(self):
+        instance = self.createInstance()
+        now = datetime.now()
+        instance.field = schema.Datetime(
+            title=u'field',
+            min=datetime(now.year - 6, 1, 1),
+            max=datetime(now.year, 1, 1),
+        )
+        instance.update()
+        self.assertEqual((-6, 1), instance.years_range)
---
+++ packages/plone.formwidget.datetime/plone.formwidget.datetime-1.3.5.tar.gz/plone.formwidget.datetime-1.3.5/setup.py
@@ -0,0 +1,75 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='plone.formwidget.datetime',
+    version='1.3.5',
+    description="Datetime widgets for Plone",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 6 - Mature",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='plone date time datetime event widget archetypes z3c.form',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://github.com/plone/plone.formwidget.datetime',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.formwidget'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'DateTime',
+        'Products.CMFPlone',
+        'plone.app.jquerytools',
+        'pytz',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'plone.app.locales >= 4.3.9',
+    ],
+    extras_require=dict(
+        z3cform=[
+            'z3c.form',
+            'zope.component',
+            'zope.schema',
+        ],
+        archetypes=[
+            'Products.Archetypes',
+            'Products.CMFCore',
+            'Zope2',
+        ],
+        test=[
+            'plone.formwidget.datetime[archetypes, z3cform]',
+            'Products.ATContentTypes',
+            'Products.GenericSetup',
+            'lxml',
+            'mock',
+            'plone.app.testing',
+            'plone.testing',
+            'unittest2',
+            'zc.buildout',
+            'zope.app.testing',
+            'zope.configuration',
+            'zope.security',
+            'zope.testing',
+        ],
+      ),
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
