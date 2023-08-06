d943aa091ce9fb4a7d8f545914db8d968b8fce0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:15 +0000

    Add package ursekar-distributions/1.1/ursekar-distributions-1.1.tar.gz

---
+++ packages/ursekar-distributions/ursekar-distributions-1.1.tar.gz/ursekar-distributions-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ursekar-distributions',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['ursekar-distributions'],
+      zip_safe=False)
