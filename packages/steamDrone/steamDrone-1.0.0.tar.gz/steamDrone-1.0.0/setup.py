e9d1db6e69d67f5d89ecf8366a0109c5d576eddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:53 +0000

    Add package steamDrone/1.0.0/steamDrone-1.0.0.tar.gz

---
+++ packages/steamDrone/steamDrone-1.0.0.tar.gz/steamDrone-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='steamDrone',
+    version='1.0.0',
+    py_modules=['steamDrone'],
+    author='yingxc',
+    author_email='a245387976@gmail.com',
+    description='用于MR100无人机编程',
+    license = "MIT Licence",
+    
+    python_requires='>=3',
+)
