d8826460496f47035caca89dca8d98a7ee8e38b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:32 +0000

    Add package embedding-as-service/0.8.0/embedding_as_service-0.8.0.tar.gz

---
+++ packages/embedding-as-service/embedding_as_service-0.8.0.tar.gz/embedding_as_service-0.8.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    require_packages = [line[:-1] if line[-1] == '\n' else line for line in f]
+
+setuptools.setup(
+    name="embedding_as_service",
+    version="0.8.0",
+    author="Aman Srivastava",
+    author_email="amans.rlx@gmail.com",
+    description="embedding-as-service: one-stop solution to encode sentence to vectors using various embedding methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/amansrivastava17/embedding-as-service",
+    packages=setuptools.find_packages(),
+    install_requires=require_packages,
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
