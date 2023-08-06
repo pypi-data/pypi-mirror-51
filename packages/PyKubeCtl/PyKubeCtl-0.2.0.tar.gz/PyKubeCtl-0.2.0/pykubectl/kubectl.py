7eee3e2db8d4d4b1c8830529b5e48858fbdab594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:36 +0000

    Add package PyKubeCtl/0.2.0/PyKubeCtl-0.2.0.tar.gz

---
+++ packages/PyKubeCtl/PyKubeCtl-0.2.0.tar.gz/PyKubeCtl-0.2.0/pykubectl/kubectl.py
@@ -0,0 +1,38 @@
+import json
+import logging
+from subprocess import CalledProcessError, check_output
+
+
+class KubeCtl(object):
+    def __init__(self, bin='kubectl', global_flags=''):
+        super(KubeCtl, self).__init__()
+        self.kubectl = '{} {}'.format(bin, global_flags)
+
+    def execute(self, command, definition=None, safe=False):
+        cmd = '{} {}'.format(self.kubectl, command)
+
+        if definition:
+            pre = 'echo \'{}\''.format(definition)
+            cmd = '{} | {} -f -'.format(pre, cmd)
+
+        logging.debug('executing {}'.format(cmd))
+
+        try:
+            return check_output(cmd, shell=True)
+        except CalledProcessError as e:
+            if not safe:
+                raise e
+            logging.warn('Command {} failed, swallowing'.format(command))
+
+    def apply(self, *args, **kwargs):
+        return self.execute('apply', *args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        return self.execute('delete', *args, **kwargs)
+
+    def get(self, *args, **kwargs):
+        result = self.execute('get -o json', *args, **kwargs).decode()
+        return json.loads(result)['items']
+
+    def describe(self, *args, **kwargs):
+        return self.execute('describe', *args, **kwargs)
