ec3ba06f47493be56a0bb261c70ad808ea160ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:56 +0000

    Add package string-color/0.0.5/string-color-0.0.5.tar.gz

---
+++ packages/string-color/string-color-0.0.5.tar.gz/string-color-0.0.5/stringcolor/test.py
@@ -0,0 +1,7 @@
+from stringcolor import cs
+cstr = '\033[38;5;196;48;5;46mballon knot\033[0m'
+print(cstr)
+print(cs("hello world", "red"))
+print(cs("here we go", "orchid"))
+print(cs("away to space!", "DeepPink3"))
+print(cs("final fantasy", "#ffff87"))
