29bb510e2dac1ab7ec637d8d048f17e4de30899a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:04 +0000

    Add package qaviton-package-manager/2019.9.3.19.26.55.655506/qaviton_package_manager-2019.9.3.19.26.55.655506.tar.gz

---
+++ packages/qaviton-package-manager/qaviton_package_manager-2019.9.3.19.26.55.655506.tar.gz/qaviton_package_manager-2019.9.3.19.26.55.655506/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+package_name = "qaviton_package_manager"
+
+
+if __name__ == "__main__":
+    from sys import version_info as v
+    from qaviton_package_manager import __author__, __version__, __author_email__, __description__, __url__, __license__
+    from setuptools import setup, find_packages
+    with open("requirements.txt") as f: requirements = f.read().splitlines()
+    with open("README.md", encoding="utf8") as f: long_description = f.read()
+    setup(
+        name=package_name,
+        version=__version__,
+        author=__author__,
+        author_email=__author_email__,
+        description=__description__,
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url=__url__,
+        packages=[pkg for pkg in find_packages() if pkg.startswith("qaviton_package_manager")],
+        license=__license__,
+        classifiers=[
+            f"Programming Language :: Python :: {v[0]}.{v[1]}",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=requirements
+    )
