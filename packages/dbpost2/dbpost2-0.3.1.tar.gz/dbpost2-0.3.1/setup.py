2c704d9ce6a7a5b2049db3cfa4bb3c93176753fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:57 +0000

    Add package dbpost2/0.3.1/dbpost2-0.3.1.tar.gz

---
+++ packages/dbpost2/dbpost2-0.3.1.tar.gz/dbpost2-0.3.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+setup(
+    name="dbpost2",
+    version="0.3.1",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['pymongo', 'dataset'],
+    url="https://github.com/dantezhu/dbpost",
+    license="BSD",
+    author="grey",
+    author_email="greyeee@gmail.com",
+    description="proxy for save db data. support mysql、sqlite、mongodb",
+)
