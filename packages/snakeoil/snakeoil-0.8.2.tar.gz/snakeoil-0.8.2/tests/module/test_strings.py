8b6fd6785795fda2e812d80445d5f570dd0d7f83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:30 +0000

    Add package snakeoil/0.8.2/snakeoil-0.8.2.tar.gz

---
+++ packages/snakeoil/snakeoil-0.8.2.tar.gz/snakeoil-0.8.2/tests/module/test_strings.py
@@ -0,0 +1,28 @@
+from snakeoil.strings import pluralism
+
+
+def test_none():
+    # default
+    assert pluralism([]) == 's'
+
+    # different suffix for nonexistence
+    assert pluralism([], none='') == ''
+
+def test_singular():
+    # default
+    assert pluralism([1]) == ''
+
+    # different suffix for singular existence
+    assert pluralism([1], singular='o') == 'o'
+
+def test_plural():
+    # default
+    assert pluralism([1, 2]) == 's'
+
+    # different suffix for plural existence
+    assert pluralism([1, 2], plural='ies') == 'ies'
+
+def test_int():
+    assert pluralism(0) == 's'
+    assert pluralism(1) == ''
+    assert pluralism(2) == 's'
