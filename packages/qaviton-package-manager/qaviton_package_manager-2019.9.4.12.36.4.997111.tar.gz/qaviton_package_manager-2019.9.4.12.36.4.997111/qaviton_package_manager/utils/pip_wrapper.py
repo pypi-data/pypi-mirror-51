e00f3651055b47fa7b83bcd91f9c75ef3a0099b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:35 +0000

    Add package qaviton-package-manager/2019.9.4.12.36.4.997111/qaviton_package_manager-2019.9.4.12.36.4.997111.tar.gz

---
+++ packages/qaviton-package-manager/qaviton_package_manager-2019.9.4.12.36.4.997111.tar.gz/qaviton_package_manager-2019.9.4.12.36.4.997111/qaviton_package_manager/utils/pip_wrapper.py
@@ -0,0 +1,18 @@
+from qaviton_package_manager.utils.system import run
+from sys import executable as python
+
+
+class PIP:
+    """wrapper around pip"""
+    def __init__(pip): pip.version = pip('--version')
+    def __call__(pip, *args): return run(python, '-m', 'pip', *args)
+    def install(pip, *args): return pip('install', *args)
+    def uninstall(pip, *args): return pip('uninstall', *args, '-y')
+    def freeze(pip, filename, *args): return pip('freeze', '>', filename, *args)
+    def exist(pip, package):
+        try: pip(f'show {package}'); return True
+        except: return
+
+
+pip = PIP()
+__all__ = ['pip']
