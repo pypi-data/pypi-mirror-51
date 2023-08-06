c8b9f511ecddb61262060cca0327abeb799816cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:59 +0000

    Add package python-libnessus/1.0.0.10/python-libnessus-1.0.0.10.tar.gz

---
+++ packages/python-libnessus/python-libnessus-1.0.0.10.tar.gz/python-libnessus-1.0.0.10/libnessus/parser.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+import xml.etree.ElementTree as ET
+from libnessus import exceptions as NessusExceptions
+from libnessus.objects import NessusReportHost, NessusReportItem, NessusReport
+from libnessus.objects import reportlogger
+
+log = reportlogger.ReportLogger('PARSER')
+
+
+class NessusParser(object):
+    @classmethod
+    def parse(cls, nessus_data, data_type='XML', strict=False):
+        """
+        Parses a Nessus data file
+        :param nessus_data The datafile to be parsed
+        :param data_type File format (XML by default)
+        :param strict Forces strict parsing of report objects which may be invalid
+        """
+        nessusobj = None
+
+        if not isinstance(nessus_data, str):
+            raise Exception("Wrong nessus_data type given as argument: "
+                            "Nessus data should be provided as strings")
+
+        if nessus_data and data_type == "XML":
+            nessusobj = cls._parse_xml(nessus_data, strict)
+        else:
+            raise Exception("No or unknown data type provided. Please check "
+                            "documentation for supported data types.")
+        return nessusobj
+
+    @classmethod
+    def _parse_xml(cls, nessus_data=None, strict=False):
+        try:
+            root = ET.fromstring(nessus_data)
+        except:
+            raise Exception("Wrong XML structure: cannot parse data")
+
+        nessusobj = None
+        if root.tag == 'NessusClientData':
+            nessusobj = cls._parse_xmlv1(root)
+        elif root.tag == 'NessusClientData_v2':
+            nessusobj = cls._parse_xmlv2(root, strict)
+        else:
+            raise Exception("Unexpected data structure for XML root node")
+        return nessusobj
+
+    @classmethod
+    def _parse_xmlv1(cls, root=None):
+        raise Exception("Nessus XML v1 parsing is not supported yet.")
+
+    @classmethod
+    def _parse_xmlv2(cls, root=None, strict=False):
+        """
+            This private method will return 0 or one report
+            (as described in the nessus documentation)
+            :param root: a string representing a part or a complete nessus scan
+            :return: NessusReport or None
+        """
+        nrp = None
+
+        for nessus_report in root.findall("Report"):
+            nessus_hosts = []
+            for nessus_host in nessus_report.findall("ReportHost"):
+                _nhost = cls.parse_host(nessus_host, strict)
+                nessus_hosts.append(_nhost)
+
+            if 'name' in nessus_report.attrib:
+                report_name = nessus_report.attrib['name']
+            else:
+                report_name = 'none'
+                print("warning: Failed to get report name from report")
+            nrp = NessusReport(name=report_name, hosts=nessus_hosts)
+
+        return nrp
+
+    @classmethod
+    def parse_host(cls, root=None, strict=False):
+        _host_name = root.attrib['name'] if 'name' in root.attrib else 'none'
+        _host_prop_elt = root.find("HostProperties")
+        _dhp = dict([(e.attrib['name'], e.text) for e in list(_host_prop_elt)])
+        _dhp.update({'name': _host_name})
+
+        _vuln_list = []
+        for report_item in root.findall("ReportItem"):
+            try:
+                _new_item = cls.parse_reportitem(report_item)
+            except NessusExceptions.MissingAttribute:
+                if strict:
+                    log.error("Strict parsing enforced: Invalid report item encountered!")
+                    raise
+                else:
+                    log.warning("Invalid report item encountered... Skipping")
+                    continue
+            _vuln_list.append(_new_item)
+
+        return NessusReportHost(_dhp, _vuln_list)
+
+    @classmethod
+    def parse_reportitem(cls, root=None):
+        """
+        This function parses the xml and returns a ReportItem object
+        This object contains everything from the Nessus XML
+        see http://static.tenable.com/documentation/nessus_v2_file_format.pdf
+        if an element can be represented more than once it will become a list
+        """
+        _vuln_data = {}
+        # add all attrib in the dicts
+        _vuln_data.update(root.attrib)
+        # parse each elem and add it to the dict
+        # + create a list as value if needed
+        for elt in root:
+            if elt.tag in ['cve', 'bid', 'osvdb', 'iava', 'iavb', 'xref']:
+                try:
+                    _vuln_data[elt.tag].append(elt.text)
+                except:
+                    _vuln_data[elt.tag] = []
+                    _vuln_data[elt.tag].append(elt.text)
+            else:
+                    _vuln_data.update({elt.tag: elt.text})
+
+        return NessusReportItem(_vuln_data)
+
+    @classmethod
+    def parse_fromstring(cls, nessus_data, data_type="XML", strict=False):
+        if not isinstance(nessus_data, str):
+            raise Exception("bad argument type : should be a string")
+        return cls.parse(nessus_data, data_type, strict)
+
+    @classmethod
+    def parse_fromfile(cls, nessus_report_path, data_type="XML", strict=False):
+        try:
+            with open(nessus_report_path, 'r') as fileobj:
+                fdata = fileobj.read()
+                rval = cls.parse(fdata, data_type, strict)
+        except IOError:
+            raise
+        return rval
+# I comment the following method
+# not used up until now
+# we'll see if really needed
+#    @classmethod
+#    def parse_fromdict(cls, rdict):
+#        nreport = {}
+#
+#        rdict = rdict.pop()
+#        try:
+#            if rdict.keys()[0] == '__NessusReport__':
+#                r = rdict['__NessusReport__']
+#                rname = r['name']
+#
+#                hlist = []
+#                for _host in r['_NessusReport__hosts']:
+#                    _vlist = []
+#                    for _vulns in _host['__NessusReportHost']['report_items']:
+#                        _vdictdata =\
+#                            _vulns['__NessusVuln__']['_NessusVuln__vuln_info']
+#                        _vlist.append(NessusVuln(_vdictdata))
+#
+#                    _dhp =\
+#                       _host['__NessusHost__']['_NessusHost__host_properties']
+#                    nh = NessusReportHost(_dhp, _vlist)
+#                    hlist.append(nh)
+#                nreport = NessusReport(name=rname, hosts=hlist)
+#        except KeyError:
+#            raise
+#        return nreport
---
+++ packages/python-libnessus/python-libnessus-1.0.0.10.tar.gz/python-libnessus-1.0.0.10/libnessus/plugins/es.py
@@ -0,0 +1,124 @@
+from elasticsearch import Elasticsearch
+import jsonpickle
+import datetime
+import base64
+import sys
+
+from libnessus.plugins.backendplugin import NessusBackendPlugin
+
+# returns python version (3, 2 or 1)
+python_version = sys.version_info[0]
+
+
+class NessusEsPlugin(NessusBackendPlugin):
+    """
+      This class handle the persistence of NessusReport object in ElasticSearc
+      Implementation is made using ElasticSearch python module
+      Object of this class must be create via the
+      BackendPluginFactory.create(**url) where url is a named dict like
+      {'plugin_name': "es"} this dict may receive all the param
+      Elasticsearch() support
+    """
+    def __init__(self, dbname=None, store=None, **kwargs):
+        NessusBackendPlugin.__init__(self)
+        try:
+            self.dbname = 'nessus'
+            self.store = 'reports'
+            self.index = "{db}_{store}".format(
+                db=self.dbname,
+                store=self.store)
+            self.es = Elasticsearch(**kwargs)
+            self.es.indices.create(index=self.index, ignore=400)
+        except:
+            raise
+
+    def insert(self, report):
+        """
+            insert NessusReport in the backend
+            :param NessusReport:
+            :return: str the ident of the object in the backend for
+            future usage
+            or None
+        """
+        j = jsonpickle.encode(report, unpicklable=False)
+        if python_version == 3:
+            # python3
+            j2 = jsonpickle.encode(report).encode('utf-8')
+            b64 = base64.b64encode(j2).decode(encoding='UTF-8')
+        else:
+            # python 2
+            j2 = jsonpickle.encode(report)
+            b64 = base64.b64encode(j2)
+        docid = hash(report.name)
+        docu = {"hash": docid,
+                "json": j,
+                "json_base64": b64,
+                "date": datetime.datetime.utcnow(),
+                "name": report.name,
+                "endtime": report.endtime,
+                "ipaddress": [host.address for host in report.hosts]}
+        try:
+            self.es.index(
+                index=self.index,
+                doc_type=self.store,
+                body=docu,
+                id=docid
+                )
+            return docid
+        except:
+            raise
+
+    def delete(self, myid):
+        """
+            delete NessusReport if the backend
+            :param id: str
+            :return: bool
+        """
+        rc = self.es.delete(
+            index=self.index,
+            doc_type=self.store,
+            id=myid)
+        return rc['found']
+
+    def get(self, id):
+        """
+            retreive a NessusReport from the backend
+            :param id: str
+            :return: NessusReport or None
+        """
+        report = None
+        filter = {"match": {"_id": id}}
+        ret = self.getall(filter=filter)
+        if len(ret) == 1:
+            id, report = ret[0]
+        return report
+
+    def getall(self, filter={"match_all": {}}, limit=30, qfrom=0):
+        """
+            :return: collection of tuple (id,NessusReport)
+            :param filter: a dict that will be added to match_all
+            ie : {"match": {"_id": id}}
+            :type dict_filter: dict
+            :param limit: max number of result allowed
+            :type limit: int
+            :param qfrom: offset into results
+            :type qfrom: int
+        """
+        nessusreportlist = []
+        rsearch = self.es.search(
+            index=self.index,
+            body={
+                "from": qfrom,
+                "size": limit,
+                "query": filter,
+                "_source": ["json_base64"]})
+        if rsearch['hits']['total'] > 0:
+            for hit in rsearch['hits']['hits']:
+                srcdict = hit['_source']
+                b64 = srcdict['json_base64']
+                b64 = b64.encode('utf-8')
+                pickle = base64.b64decode(b64).decode('utf-8')
+                id = hit['_id']
+                nessusreport = jsonpickle.decode(pickle)
+                nessusreportlist.append((id, nessusreport))
+        return nessusreportlist
