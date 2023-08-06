d0337ddf3edb85c55f77f33d3d88d3b1c1295d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:18 +0000

    Add package ip-geolocation-api/1.0.0/ip-geolocation-api-1.0.0.tar.gz

---
+++ packages/ip-geolocation-api/ip-geolocation-api-1.0.0.tar.gz/ip-geolocation-api-1.0.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['pack']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['pyipwhois = entry:main']}
+
+setup_kwargs = {
+    'name': 'ip-geolocation-api',
+    'version': '1.0.0',
+    'description': 'Free IP Geolocation API and IP Location Lookup Database',
+    'long_description': None,
+    'author': 'ipwhois.io',
+    'author_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '==2.7.5',
+}
+
+
+setup(**setup_kwargs)
