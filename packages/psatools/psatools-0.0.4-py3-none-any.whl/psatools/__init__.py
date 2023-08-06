68b6e8521ba79171d1356078e16f38e14825e833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:56 +0000

    Add package psatools/0.0.4/psatools-0.0.4-py3-none-any.whl

---
+++ packages/psatools/psatools-0.0.4-py3-none-any.whl/psatools/__init__.py
@@ -0,0 +1,13 @@
+"""psatools for electrical engineers
+"""
+
+__version__ = "0.0.4"
+from .psatools import(
+    calci,
+    calcpf,
+    calcp,
+    calcs,
+    calcir,
+    calcq,
+    calcshcs,
+)
