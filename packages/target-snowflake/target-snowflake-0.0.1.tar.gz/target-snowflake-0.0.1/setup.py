3b958b45446203a9dcabd38dd32866c644b6887a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:35 +0000

    Add package target-snowflake/0.0.1/target-snowflake-0.0.1.tar.gz

---
+++ packages/target-snowflake/target-snowflake-0.0.1.tar.gz/target-snowflake-0.0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from os import path
+
+from setuptools import setup, find_packages
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='target-snowflake',
+    url='https://github.com/datamill-co/target-snowflake',
+    author='datamill',
+    version="0.0.1",
+    description='Singer.io target for loading data into Snowflake',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['target_snowflake'],
+    install_requires=[
+        'boto3==1.9.205', # Dependency conflict with snowflake and target-redshift
+        'singer-python==5.6.1', # Dependency conflict with snowflake
+        'singer-target-postgres==0.1.10',
+        'snowflake-connector-python==1.9.0',
+        'target-redshift==0.0.9'
+    ],
+    setup_requires=[
+        "pytest-runner"
+    ],
+    extras_require={
+        'tests': [
+            "chance==0.110",
+            "Faker==1.0.8",
+            "pytest==4.5.0"
+        ]},
+    entry_points='''
+      [console_scripts]
+      target-snowflake=target_snowflake:cli
+    ''',
+    packages=find_packages()
+)
