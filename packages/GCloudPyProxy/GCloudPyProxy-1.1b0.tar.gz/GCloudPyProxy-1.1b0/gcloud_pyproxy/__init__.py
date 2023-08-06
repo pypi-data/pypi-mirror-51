4c608bde7e1dca8f01d1caa70b1c35cba6596931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:20 +0000

    Add package GCloudPyProxy/1.1b0/GCloudPyProxy-1.1b0.tar.gz

---
+++ packages/GCloudPyProxy/GCloudPyProxy-1.1b0.tar.gz/GCloudPyProxy-1.1b0/gcloud_pyproxy/__init__.py
@@ -0,0 +1,54 @@
+from subprocess import Popen, PIPE
+import json
+from shutil import which
+
+class GCloudError(Exception):
+    pass
+
+def gcloud_proxy(command, *args, **kwargs):
+    if "replace_underscore" in kwargs.keys():
+        replace_underscore = kwargs["replace_underscore"]
+        del kwargs["replace_underscore"]
+    else:
+        replace_underscore = True
+    flags = [
+        "--{key}=\"{value}\"".format(
+            key=(key.replace("_", "-") if replace_underscore else key),
+            value=val) for key, val in kwargs.items()
+        ]
+    f = Popen(" ".join([
+        "gcloud",
+        command,
+        *["\"{}\"".format(arg) for arg in args],
+        *flags,
+        "--format=json"
+    ]),
+    stdout=PIPE,
+    stderr=PIPE,
+    shell=True
+    )
+    out, err = f.communicate()
+    if f.returncode == 0:
+        return json.loads(out.decode("utf-8"))
+    else:
+        raise GCloudError(err.decode("utf-8"))
+
+class GCloud:
+    def __init__(self):
+        pass
+    def __getattribute__(self, attr):
+        attributes = attr.split("_")
+        if attributes[-1] == "":
+            attributes.pop(-1)
+        def run_gcloud(*args, **kwargs):
+            return gcloud_proxy(*attributes, *args, **kwargs)
+        return run_gcloud
+
+if which("gcloud") == None:
+    raise FileNotFoundError("\'gcloud\' command was not found in your environment. Ensure it is installed and set up properly.")
+
+if __name__ == '__main__':
+    gc = GCloud()
+    print(gc.projects_list())
+
+__all__ = [GCloud, GCloudError]
---
+++ packages/GCloudPyProxy/GCloudPyProxy-1.1b0.tar.gz/GCloudPyProxy-1.1b0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+def long_description():
+    with open("README.md") as infile:
+        return infile.read()
+
+def license():
+    return "MIT License"
+
+long_description_content_type="text/markdown"
+
+setup(
+    name="GCloudPyProxy",
+    version="1.1b",
+    packages=find_packages(),
+    author="Tomas Votava",
+    author_email="info@tomasvotava.eu",
+    description="This package provides proxy class for calling gcloud commands directly from Python.",
+    keywords="gcp google cloud platform gcloud python proxy",
+    url="https://github.com/tomasvotava/gcloud-pyproxy",
+    project_urls={
+        "Bug Tracker": "https://github.com/tomasvotava/gcloud-pyproxy/issues",
+        "Documentation": "https://github.com/tomasvotava/gcloud-pyproxy/blob/master/README.md",
+        "Source Code": "https://github.com/tomasvotava/gcloud-pyproxy"
+    },
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Build Tools",
+        "Development Status :: 4 - Beta"
+    ],
+    long_description=long_description(),
+    download_url="https://github.com/tomasvotava/gcloud-pyproxy/archive/master.zip",
+    license=license(),
+    long_description_content_type="text/markdown"
+)
+
