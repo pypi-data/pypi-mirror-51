cf5db5db71fafa6e2c6621e10938f49a3f3e5543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:30 +0000

    Add package datasetsanirudh/0.0.2/datasetsanirudh-0.0.2.tar.gz

---
+++ packages/datasetsanirudh/datasetsanirudh-0.0.2.tar.gz/datasetsanirudh-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datasetsanirudh",
+    version="0.0.2",
+    author="Anirudh Mathad",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/asthinasthi/datasets-anirudh",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
