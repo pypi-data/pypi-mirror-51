8c0347ae5ec6cd87b3775e1e89bf9488390f04c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:24 +0000

    Add package dircolors/0.0.3/dircolors-0.0.3-py3-none-any.whl

---
+++ packages/dircolors/dircolors-0.0.3-py3-none-any.whl/dircolors/__init__.py
@@ -0,0 +1,10 @@
+# dircolors module, just import the main Dircolors class
+#
+# Copyright 2019 Allen Wild <allenwild93@gmail.com>
+# SPDX-License-Identifier: Apache-2.0
+
+""" Python library to colorize filenames in a terminal based on their type """
+
+__version__ = '0.0.3'
+
+from .dircolors import Dircolors
