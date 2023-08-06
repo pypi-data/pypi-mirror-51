8f9b8b553c23acc4872e3b2c992b83f53c70d316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:39 +0000

    Add package mLab-Doppler-Radar-Tool/0.1.5.4/mLab-Doppler-Radar-Tool-0.1.5.4.tar.gz

---
+++ packages/mLab-Doppler-Radar-Tool/mLab-Doppler-Radar-Tool-0.1.5.4.tar.gz/mLab-Doppler-Radar-Tool-0.1.5.4/README.md
@@ -0,0 +1,6 @@
+**mLab Doppler Radar Tool: UART serial & data plot**
+
+[![Latest PyPI version](https://img.shields.io/badge/pypi-v0.1-green.svg)](https://pypi.org/project/qt-ledwidget/)
+[![License: MIT](https://img.shields.io/dub/l/vibe-d.svg)](https://opensource.org/licenses/MIT)
+
+for Raspberry Pi,please install pyqt5-dev-tools, pyqt5-dev first. 
---
+++ packages/mLab-Doppler-Radar-Tool/mLab-Doppler-Radar-Tool-0.1.5.4.tar.gz/mLab-Doppler-Radar-Tool-0.1.5.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='mLab-Doppler-Radar-Tool',
+      version='0.1.5.4',
+      packages=['mLab_DopplerRadar'],
+      install_requires=[
+          'pyserial','pyqtgraph'
+      ],
+      include_package_data=True,
+      url='', license='MIT',
+      author='Alex Kuan',
+      author_email='agathakuannew@gmail.com',
+      description='mLab Doppler Radar UART tool and example code')
