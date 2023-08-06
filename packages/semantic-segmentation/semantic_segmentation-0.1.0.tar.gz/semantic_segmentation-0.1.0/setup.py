38dd78a4db0567f537a13431fd209227799d1b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:14 +0000

    Add package semantic-segmentation/0.1.0/semantic_segmentation-0.1.0.tar.gz

---
+++ packages/semantic-segmentation/semantic_segmentation-0.1.0.tar.gz/semantic_segmentation-0.1.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='semantic_segmentation',
+    version='0.1.0',
+    description=(
+        'Semantic Segmentation on Tensorflow && Keras'
+    ),
+    long_description=open('README.MD').read(),
+    long_description_content_type='text/markdown',
+    author='Yang Lu',
+    author_email='luyanger1799@outlook.com',
+    license='Apache 2,0 License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/luyanger1799/Amazing-Semantic-Segmentation.git',
+    python_requires='>=3.6',
+    )
