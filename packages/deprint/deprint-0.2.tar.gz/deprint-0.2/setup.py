35fc4f9d9a6e8bed339ace7a71ea0fe67f2cadac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:10 +0000

    Add package deprint/0.2/deprint-0.2.tar.gz

---
+++ packages/deprint/deprint-0.2.tar.gz/deprint-0.2/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+import setuptools
+
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+  name = 'deprint',
+  packages = ['deprint'],
+  version = '0.2',
+  license='MIT',
+  description = 'Declarative Python fancy print statement',
+  long_description = long_description,
+  long_description_content_type = "text/markdown",
+  author = 'Wattanit Hotrakool',
+  author_email = 'wattanit@protonmail.com',
+  url = 'https://github.com/rorasa/deprint',
+  download_url = 'https://github.com/rorasa/deprint/archive/v_02.tar.gz',
+  keywords = ['declarative', 'print', 'fancy', 'color'],
+  python_requires='>=3',
+#   install_requires=[ 
+#       ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
