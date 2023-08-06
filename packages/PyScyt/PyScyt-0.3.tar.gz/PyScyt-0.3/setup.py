e7246759eb7e74396d848224a7e925a5e9a54c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:00 +0000

    Add package PyScyt/0.3/PyScyt-0.3.tar.gz

---
+++ packages/PyScyt/PyScyt-0.3.tar.gz/PyScyt-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyScyt',
+    version='v0.3',
+    packages=find_packages(),
+    url='https://github.com/Porter97/PyScyt',
+    license='',
+    author='Spencer Porter',
+    author_email='spencer.porter3@gmail.com',
+    description='A simple Python framework for generating project citations and metadata',
+    keywords=['medium', 'sdk', 'api'],
+)
