eab52cf72ba89cd373f5294b82042d4edef884b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:16 +0000

    Add package tuyadatac/0.0.4b2/tuyadatac-0.0.4b2-py3-none-any.whl

---
+++ packages/tuyadatac/tuyadatac-0.0.4b2-py3-none-any.whl/tuyadatac/client_workspace_test.py
@@ -0,0 +1,13 @@
+class DefaultConnect:
+    def address(self):
+        return '0.0.0.0'
+    def port(self):
+        return 9091
+
+class WorkspaceFlickr:
+    def workspace(self):
+        return '/flickr'
+    def username(self):
+        return 'user'
+    def password(self):
+        return '12345'
