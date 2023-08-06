430f4fdf3035e8130da0298cc374d6447e9e22d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:05 +0000

    Add package richie/1.8.3/richie-1.8.3-py2.py3-none-any.whl

---
+++ packages/richie/richie-1.8.3-py2.py3-none-any.whl/richie/apps/courses/models/__init__.py
@@ -0,0 +1,10 @@
+"""Make models.py a module containing one file per model as it was getting too long."""
+# flake8: noqa
+# pylint: disable=wildcard-import
+
+from .blog import *
+from .category import *
+from .course import *
+from .organization import *
+from .person import *
+from .role import *
