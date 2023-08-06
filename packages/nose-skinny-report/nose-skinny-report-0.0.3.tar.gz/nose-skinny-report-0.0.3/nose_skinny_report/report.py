ecb524460e48576e306991d61ee454d4f4a94f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:48 +0000

    Add package nose-skinny-report/0.0.3/nose-skinny-report-0.0.3.tar.gz

---
+++ packages/nose-skinny-report/nose-skinny-report-0.0.3.tar.gz/nose-skinny-report-0.0.3/nose_skinny_report/report.py
@@ -0,0 +1,20 @@
+import nose2.events
+
+__unittest = True
+
+class ResultReporter(nose2.events.Plugin):
+    """Result plugin that just prints the test ids"""
+    alwaysOn = True
+
+    def __init__(self):
+        self.tests = []
+
+    def startTest(self, event):
+        event.handled = True
+        if not issubclass(event.test.__class__, Exception):
+            self.tests.append(event.test.id().split()[0])
+
+    def afterTestRun(self, event):
+        self.tests.sort()
+        for t in self.tests:
+            print(t)
