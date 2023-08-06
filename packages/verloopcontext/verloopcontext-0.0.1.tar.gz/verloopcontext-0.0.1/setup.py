49a9881c86c676b9962aa5cf420a9cdda667bea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:47 +0000

    Add package verloopcontext/0.0.1/verloopcontext-0.0.1.tar.gz

---
+++ packages/verloopcontext/verloopcontext-0.0.1.tar.gz/verloopcontext-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name="verloopcontext",
+      version="0.0.1",
+      description="Verloop context to pass headers and metadata around twirp",
+      author="Piyush",
+      author_email="piyush@verloop.io",
+      licesnse='unlicense',
+      packages=['verloopcontext'],
+      install_requires=[
+            'structlog'
+      ],
+      test_requires=[
+      ],
+      zip_safe=False)
