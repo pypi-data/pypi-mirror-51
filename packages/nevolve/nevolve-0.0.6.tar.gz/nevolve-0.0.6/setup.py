fc4c405dfd79e29acc0cfc11e48f3c8ad3ebfd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:35 +0000

    Add package nevolve/0.0.6/nevolve-0.0.6.tar.gz

---
+++ packages/nevolve/nevolve-0.0.6.tar.gz/nevolve-0.0.6/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="nevolve",
+    packages=setuptools.find_packages(),
+    version="0.0.6",
+    author="Kaustubh Olpadkar",
+    description=(
+        'Neuro-Evolution Library for Reinforcement Learning.'
+    ),
+    url='https://github.com/kaustubholpadkar/nevolve',
+    keywords='neuroevolution machine-learning neural-networks'
+)
