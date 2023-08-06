a38cf3cd6ff895d1fb752b51b4ee7ffb00448d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:04 +0000

    Add package lara-mg3/0.1/lara_mg3-0.1.tar.gz

---
+++ packages/lara-mg3/lara_mg3-0.1.tar.gz/lara_mg3-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+ 
+setup(
+     name='lara_mg3',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['hi']                  # The name of your scipt, and also the command you'll be using for calling it
+ )
