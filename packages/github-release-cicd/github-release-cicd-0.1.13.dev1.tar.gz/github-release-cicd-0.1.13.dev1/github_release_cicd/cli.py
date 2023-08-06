7afd636bcda70ed04c695a52a43c1cfa71caa362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:25 +0000

    Add package github-release-cicd/0.1.13.dev1/github-release-cicd-0.1.13.dev1.tar.gz

---
+++ packages/github-release-cicd/github-release-cicd-0.1.13.dev1.tar.gz/github-release-cicd-0.1.13.dev1/github_release_cicd/cli.py
@@ -0,0 +1,106 @@
+import glob
+
+import click
+
+from github import Github
+
+
+class GithubRelease(object):
+    def __init__(self, github):
+        self.github = github
+
+
+@click.group()
+@click.option(
+    '--token',
+    help='GitHub API token.',
+    envvar='TOKEN',
+    show_envvar=True,
+    required=True,
+)
+@click.option(
+    '--repo',
+    help='GitHub repository name.',
+    envvar='REPO',
+    show_envvar=True,
+    required=True,
+)
+@click.version_option()
+@click.pass_context
+def cli(ctx, token, repo):
+    """Manage GitHub releases."""
+
+    ctx.obj = Github(token).get_user().get_repo(repo)
+
+
+@cli.command()
+@click.option(
+    '--tag',
+    help='Release tag.',
+    envvar='TAG',
+    show_envvar=True,
+    required=True,
+)
+@click.option(
+    '--name',
+    help='Release name.',
+    envvar='NAME',
+    show_envvar=True,
+)
+@click.option(
+    '--message',
+    help='Release message.',
+    envvar='MESSAGE',
+    show_envvar=True,
+    required=True,
+)
+@click.option(
+    '--draft/--no-draft',
+    help='Release is a draft.',
+    envvar='DRAFT',
+    show_envvar=True,
+    default=False,
+)
+@click.option(
+    '--prerelease/--no-prelease',
+    help='Release is a prerelease.',
+    envvar='PRERELEASE',
+    show_envvar=True,
+    default=False,
+)
+@click.option(
+    '--target',
+    help='Release commit target.',
+    envvar='TARGET',
+    show_envvar=True,
+)
+@click.option(
+    '--assets',
+    help='Release assets to upload.',
+    envvar='ASSETS',
+    show_envvar=True,
+)
+@click.pass_obj
+def create(repo, tag, name, message, draft, prerelease, target, assets):
+    """Create release."""
+
+    if not name:
+        name = tag
+
+    release = repo.create_git_release(
+        tag=tag,
+        name=name,
+        message=message,
+        target_commitish=target,
+        prerelease=prerelease,
+    )
+
+    for item in glob.glob(assets):
+        release.upload_asset(item)
+
+
+def main():
+    cli(
+        auto_envvar_prefix='GITHUB_RELEASE_CICD',
+        prog_name='github_release_cicd',
+    )
