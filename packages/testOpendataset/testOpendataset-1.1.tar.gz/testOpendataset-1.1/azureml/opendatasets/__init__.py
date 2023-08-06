dbff7773baa2d379e2ecceb5f2e11c1d3af9cf14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:35 +0000

    Add package testOpendataset/1.1/testOpendataset-1.1.tar.gz

---
+++ packages/testOpendataset/testOpendataset-1.1.tar.gz/testOpendataset-1.1/azureml/opendatasets/__init__.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Enable consuming Azure open datasets into dataframes and enrich customer data."""
+
+from ._open_image import OpenImage
+
+__all__ = [
+    'OpenImage',
+]
