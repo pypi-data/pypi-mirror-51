350f21dca239a398d4f7b39e422ad29524a768a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:15 +0000

    Add package egproject/0.0.23/egproject-0.0.23.zip

---
+++ packages/egproject/egproject-0.0.23.zip/egproject-0.0.23/egproject/_main.py
@@ -0,0 +1,47 @@
+import numpy
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import codecs
+from ast import literal_eval
+import itertools
+import re
+import xlrd
+import time as wtime
+from datetime import datetime,time
+from openpyxl.styles import Font,Fill
+from openpyxl.styles.colors import RED
+from openpyxl.styles import PatternFill,Border,Side,Alignment,Protection,Font
+from openpyxl.styles import colors
+from openpyxl.styles.colors import BLUE
+#import pygame
+from Tkinter import *
+import Tkinter
+import tkMessageBox
+from PIL import ImageTk, Image
+import random
+import numpy as np
+#from pygame.locals import *
+import xlsxwriter
+import pandas as pd
+from shutil import copyfile
+#import delny
+
+
+modules_req=['xlsxwriter', 'numpy','openpyxl','PIL','delny']
+for mm in modules_req:
+	try: 
+		import mm
+		print("vxv",mm)
+	except:
+		print("Please install the moudle named :" ,mm)
+
+
+
+
+def main():
+	for i in range(10):
+		print(i)
+		
+if __name__=="__main__":
+
+		main()
