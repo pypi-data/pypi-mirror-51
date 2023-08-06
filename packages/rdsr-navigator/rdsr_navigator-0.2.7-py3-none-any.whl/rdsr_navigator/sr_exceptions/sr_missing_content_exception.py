2bb18969d1713b96343218200962a1bf0ed5641f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:54 +0000

    Add package rdsr-navigator/0.2.7/rdsr_navigator-0.2.7-py3-none-any.whl

---
+++ packages/rdsr-navigator/rdsr_navigator-0.2.7-py3-none-any.whl/rdsr_navigator/sr_exceptions/sr_missing_content_exception.py
@@ -0,0 +1,5 @@
+from rdsr_navigator.sr_exceptions.sr_exception_base import SrExceptionBase
+
+class SrMissingContentException(SrExceptionBase):
+    def __init__(self, message=''):
+        super().__init__(message)
