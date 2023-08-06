f33db48f21c20b8a0c178a1c5bd4127f8b333115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:02 +0000

    Add package vars-pickle/0.1.8/vars_pickle-0.1.8-py3-none-any.whl

---
+++ packages/vars-pickle/vars_pickle-0.1.8-py3-none-any.whl/vars_pickle/__config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  6 08:27:35 2019
+
+@author: 10167232
+"""
+path = 'data_pickle'
+type_list = ['str','function','pandas.core.frame.DataFrame',
+             'pandas.core.series.Series','list','dict','int',
+             'float','set','tuple','bool','complex','numpy.ndarray']
+drop_list = ['In','Out','var_dic_list','get_ipython','']
---
+++ packages/vars-pickle/vars_pickle-0.1.8-py3-none-any.whl/vars_pickle/__init__.py
@@ -0,0 +1,20 @@
+"""
+dumps:保存内存中的变量
+loads:读取已保存的变量到主命名空间（顺便引入numpy、pandas）
+type_list:需要保存的变量类型
+drop_list:不想保存的变量列表（主要针对jupyter）
+
+
+Created on Thu Sep  5 14:11:55 2019
+
+@author: karond
+"""
+
+__version__ = "0.1.8"
+__author__ = 'karond'
+
+
+from .__vars_pickle import dumps
+from .__vars_pickle import loads
+from .__config import drop_list
+from .__config import type_list
