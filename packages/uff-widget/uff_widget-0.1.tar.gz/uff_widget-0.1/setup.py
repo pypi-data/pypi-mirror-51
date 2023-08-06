40f24c85aa859c07b5ffe52445d6bc0e85c1ee92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:07 +0000

    Add package uff-widget/0.1/uff_widget-0.1.tar.gz

---
+++ packages/uff-widget/uff_widget-0.1.tar.gz/uff_widget-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='uff_widget',  
+     version='0.1',
+     author='Lovro Trilar,Janko Slavič',
+     author_email='Lovro.3lar@gmail.com, janko.slavic@fs.uni-lj.si',
+     description='vizualization of uff file',
+     url='https://github.com/ladisk/uff_widget',
+     packages=setuptools.find_packages(),
+     classifiers=[
+         'Programming Language :: Python :: 3',
+         'License :: OSI Approved :: MIT License',
+         'Operating System :: OS Independent',
+     ],
+ )
