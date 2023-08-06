15d9d7b23ad4d5f716ed076047026054b6437ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:50 +0000

    Add package ast-tools/0.0.5/ast_tools-0.0.5.tar.gz

---
+++ packages/ast-tools/ast_tools-0.0.5.tar.gz/ast_tools-0.0.5/README.md
@@ -0,0 +1,11 @@
+[![Build Status](https://travis-ci.com/leonardt/ast_tools.svg?branch=master)](https://travis-ci.com/leonardt/ast_tools)
+[![Coverage Status](https://coveralls.io/repos/github/leonardt/ast_tools/badge.svg?branch=master)](https://coveralls.io/github/leonardt/ast_tools?branch=master)
+
+Toolbox for working with the Python AST
+
+```
+pip install ast_tools
+```
+
+# Useful References
+* [Green Tree Snakes - the missing Python AST docs](greentreesnakes.readthedocs.io/)
---
+++ packages/ast-tools/ast_tools-0.0.5.tar.gz/ast_tools-0.0.5/setup.py
@@ -0,0 +1,27 @@
+"""
+setup script for package
+"""
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name='ast_tools',
+    url='https://github.com/leonardt/ast_tools',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu',
+    version='0.0.5',
+    description='Toolbox for working with the Python AST',
+    scripts=[],
+    packages=[
+        "ast_tools",
+        "ast_tools.visitors",
+        "ast_tools.transformers",
+        "ast_tools.passes"
+    ],
+    install_requires=['astor'],
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown"
+)
