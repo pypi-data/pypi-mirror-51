152ca76db4d6114a45e8034a742e587666a27a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:54 +0000

    Add package pulp-rpm-client/3.0.0b5.dev1567529079/pulp_rpm-client-3.0.0b5.dev1567529079.tar.gz

---
+++ packages/pulp-rpm-client/pulp_rpm-client-3.0.0b5.dev1567529079.tar.gz/pulp_rpm-client-3.0.0b5.dev1567529079/pulpcore/client/pulp_rpm/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_rpm.api.content_advisories_api import ContentAdvisoriesApi
+from pulpcore.client.pulp_rpm.api.content_packages_api import ContentPackagesApi
+from pulpcore.client.pulp_rpm.api.distributions_rpm_api import DistributionsRpmApi
+from pulpcore.client.pulp_rpm.api.publications_rpm_api import PublicationsRpmApi
+from pulpcore.client.pulp_rpm.api.remotes_rpm_api import RemotesRpmApi
+from pulpcore.client.pulp_rpm.api.rpm_copy_api import RpmCopyApi
+from pulpcore.client.pulp_rpm.api.rpm_upload_api import RpmUploadApi
