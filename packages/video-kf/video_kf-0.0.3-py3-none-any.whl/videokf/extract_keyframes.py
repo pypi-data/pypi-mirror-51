bda35bd9ecff32aae4896f27c2c7c78c7e2d4d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:11 +0000

    Add package video-kf/0.0.3/video_kf-0.0.3-py3-none-any.whl

---
+++ packages/video-kf/video_kf-0.0.3-py3-none-any.whl/videokf/extract_keyframes.py
@@ -0,0 +1,34 @@
+from videokf.ffmpeg_manager.check_ffmpeg import get_ff
+from videokf.keyframe_manager.keyframe_extractor import get_keyframes
+
+
+def extract_keyframes(video_file, method="iframes", output_dir_keyframes="keyframes", dir_exe=None, ffmpeg_exe=None,
+                      ffprobe_exe=None, remove_frames_dir=True):
+    """
+
+    Args:
+        video_file (str): Path to the video file.
+        method (str): Flag to choose between the different methods to select the keyframes. The possible flags are
+                      "iframes", "color" and "flow".
+        output_dir_keyframes (str): It can be either a full directory path where the keyframes will be stored, or a
+                                    string, in which case, a folder with this name will be created in the same
+                                    directory of the video and the keyframes will be saved there.
+        dir_exe (str): Directory from where to read the executables or where to download them. By default, it is a
+                       folder called 'FFmpeg' in the home directory. It won't be used if both ffmpeg_exe and
+                       ffprobe_exe are given.
+        ffmpeg_exe (str): Path to the ffmpeg executable.
+        ffprobe_exe (str): Path to the ffprobe executable.
+        remove_frames_dir (bool): If True, it removes the folder containing all the frames, after it has been used.
+
+    Returns:
+
+    """
+    # Get paths to ffmpeg and ffprobe executables
+    if ffmpeg_exe is None:
+        ffmpeg_exe = get_ff("ffmpeg", dir_exe)
+
+    if ffprobe_exe is None:
+        ffprobe_exe = get_ff("ffprobe", dir_exe)
+
+    # Extract frames
+    get_keyframes(ffmpeg_exe, ffprobe_exe, video_file, method, output_dir_keyframes, remove_frames_dir)
---
+++ packages/video-kf/video_kf-0.0.3-py3-none-any.whl/videokf/ffmpeg_manager/check_ffmpeg.py
@@ -0,0 +1,199 @@
+import os
+import stat
+import re
+from pathlib import Path
+import requests
+import zipfile
+import platform
+
+from videokf.utils.all_utils import make_dir, url_retrieve
+
+
+def get_ff(type, dir=None):
+    """Gets ffmpeg or ffprobe executables, either from an env var, from dir or downloading it.
+
+    Args:
+        type (str): Choose what url file to return. Either 'ffmpeg' or 'ffprobe'.
+        dir(str): Directory from where to read the executable or where to download it.
+
+    Returns:
+        tuple(str): Tuple with ffmpeg path file.
+
+    """
+    save_dir = get_default_dir(dir)
+
+    # Get urls
+    url = choose_url(type)
+
+    # Get files or download them
+    ff = get_executable(type, save_dir, url)
+
+    # Allow executing program for Ubuntu and OSX
+    allow_executing_as_program(str(ff))
+
+    return str(ff)
+
+
+def get_executable(file_name, dir, url):
+    """Gets an executable file after checking if it's an env variable and if it's in dir.
+
+    First, it will be checked if file_name is an environmental variable.
+    If that it False, it will be checked if the file is in dir.
+    If this fails, the file will be downloaded and saved in dir.
+
+    Args:
+        file_name (str): File to check.
+        dir (str): Directory to check and where to save the file in case of download.
+        url (str): Url from where to download the file.
+
+    Returns:
+        str: Full path of the file.
+
+    """
+    return check_environ_var(file_name.upper()) or \
+           check_if_file_exists(file_name, dir) or \
+           download_and_unzip(url, dir, file_name)
+
+
+def check_environ_var(var):
+    """Checks if an environmental variable is present or not and returns it.
+
+    Args:
+        var (str): Environmental variable to check.
+
+    Returns:
+        str or None: Full path of the environmental variable, it it exists. It returns None, if the variable doesn't
+                     exist.
+
+    """
+    return os.environ.get(var)
+
+
+def check_if_file_exists(file, dir):
+    """Checks if a file exists in a directory and returns its full path if it does exist.
+
+    Args:
+        file (str): Full path of the file to check.
+        dir (str): Directory to check for the existence of the file.
+
+    Returns:
+        str: Full path of the file if it exists. It returns None, if the file doesn't exist.
+
+    """
+    my_file = dir / Path(file)
+    if my_file.is_file():
+        return my_file.absolute()
+
+    # Check windows case with .exe extension
+    my_file = my_file.with_suffix(".exe")
+    if my_file.is_file():
+        return my_file.absolute()
+
+
+def download_and_unzip(url, dir, file_name, remove_zip=True):
+    """Downloads a zip file, unzips it and removes the zip file if the flag is true.
+
+    Args:
+        url (str): Url of the zip file to download.
+        dir (str): Directory to extract the zip file to.
+        file_name (str): File name to extract from inside the zip file.
+        remove_zip (bool): If True, it removes the zip file after unzipping it.
+
+    """
+    file_name = Path(file_name)
+
+    print(f"Downloading {file_name} ...")
+    zip_file = Path(dir) / "temp_file.zip"
+    url_retrieve(url, zip_file)
+    print("Download complete")
+
+    with zipfile.ZipFile(zip_file) as f:
+        # List all files inside zip file
+        file_list = f.namelist()
+
+        # Match files with 'file_name' pattern
+        match_list = [re.match(f"{file_name.name}(.exe)*", x) for x in file_list]
+
+        # Get file that matched the pattern.
+        # If file not matched, return None, which will prompt an error during extraction
+        save_file = next((item.string for item in match_list if item is not None), None)
+
+        # Extract the file
+        f.extract(save_file, dir)
+
+    if remove_zip:
+        Path(zip_file).unlink()
+
+    return Path(dir) / save_file
+
+
+def choose_url(type):
+    """Chooses which url to download ffmpeg and ffprobe from based on the operating system.
+
+    Args:
+        type (str): Choose what url file to return. Either 'ffmpeg' or 'ffprobe'.
+
+    Returns:
+        tuple(str): Tuple of two elements: the first one is the url of ffmpeg and the second one of ffprobe.
+
+    """
+    # Download urls information
+    url_api = "https://ffbinaries.com/api/v1/version/latest"
+    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) "
+                             "Chrome/50.0.2661.102 Safari/537.36"}
+    result = requests.get(url_api, headers=headers).json()["bin"]
+
+    # Find operating system and architecture
+    system = platform.system()
+    architecture, _= platform.architecture()
+
+    if system == "Windows" and architecture == "64bit":
+        url = result["windows-64"]
+    elif system == "Windows" and architecture == "32bit":
+        url = result["windows-32"]
+    elif system == "Linux" and architecture == "64bit":
+        url = result["linux-64"]
+    elif system == "Linux" and architecture == "32bit":
+        url = result["linux-32"]
+    elif system == "Darwin":
+        url = result["osx-64"]
+    else:
+        print("There was an error while detecting your operating system! Please, download ffmpeg and ffprobe manually.")
+
+    return url[type]
+
+
+def set_default_dir():
+    """Sets the default directory where ffmpeg and ffprobe will be searched and download, if not found.
+
+    Args:
+          dir(str): Directory from where to read the executable or where to download it.
+
+    """
+    return Path.home()
+
+
+def get_default_dir(dir=None):
+    """Gets the path of the default directory where ffmpeg and ffprobe will be searched and donwload, if not found.
+
+    It will create the directory, if it doesn't exist.
+
+    Args:
+          dir(str): Directory from where to read the executable or where to download it.
+
+    """
+    if dir is None:
+        dir = set_default_dir()
+
+    return make_dir("Ffmpeg", dir)
+
+
+def allow_executing_as_program(file):
+    """Turns on the option 'allow executing file as program' in Ubuntu and OSX.
+
+    Args:
+        file (str): Path of the file.
+
+    """
+    st = os.stat(file)
+    os.chmod(file, st.st_mode | stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
