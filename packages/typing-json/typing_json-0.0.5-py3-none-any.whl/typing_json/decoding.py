dcf1a93c68548555e40d113fe25b699ef8ef1ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:16 +0000

    Add package typing-json/0.0.5/typing_json-0.0.5-py3-none-any.whl

---
+++ packages/typing-json/typing_json-0.0.5-py3-none-any.whl/typing_json/decoding.py
@@ -0,0 +1,128 @@
+""" Decoding utilities """
+
+from typing import Any, Union
+from collections import deque, OrderedDict
+from collections.abc import Mapping
+from typing_extensions import Literal
+from typing_json.typechecking import is_instance, is_namedtuple
+from typing_json.encoding import JSON_BASE_TYPES, is_json_encodable
+
+_UNREACHABLE_ERROR_MSG = "Should never reach this point, please open an issue on GitHub."
+
+def from_json_obj(obj: Any, t: Any) -> Any:
+    """ Converts an object of json standard type to json encodable type. """
+    # pylint:disable=invalid-name,too-many-branches,too-many-statements,too-many-return-statements
+    if not is_json_encodable(t):
+        raise TypeError("Type %s is not json-encodable."%str(t))
+    if t in JSON_BASE_TYPES:
+        if not isinstance(obj, t):
+            raise TypeError("Object %s is not %s."%(str(obj), str(t)))
+        return obj
+    if t in (None, type(None)):
+        if obj is not None:
+            raise TypeError("Object %s is not null (t=%s)."%(str(obj), str(t)))
+        return None
+    if t is ...:
+        if obj is not None:
+            raise TypeError("Object %s is not null (t=%s)."%(str(obj), str(t)))
+        return ...
+    if is_namedtuple(t):
+        if not isinstance(obj, (dict, OrderedDict, list)):
+            raise TypeError("Object %s is not (ordered) dictionary or list (t=%s)."%(str(obj), str(t))) # pylint:disable=line-too-long
+        fields = getattr(t, "_fields")
+        field_types = getattr(t, "_field_types")
+        field_defaults = getattr(t, "_field_defaults")
+        if isinstance(obj, list):
+            if len(fields) != len(obj):
+                raise TypeError("Object %s does not provide the right number of values for a namedtuple.")
+            return_val = t(*tuple(from_json_obj(obj[i] if i < len(obj) else field_defaults[field], field_types[field]) for i, field in enumerate(fields))) # pylint:disable=line-too-long
+            assert is_instance(return_val, t)
+            return return_val
+        converted_dict: OrderedDict() = {} # type:ignore
+        if set(obj.keys()).union(set(field_defaults.keys())) != set(field_types.keys()):
+            key_diff = set(obj.keys()).union(set(field_defaults.keys())) - set(field_types.keys())
+            if key_diff:
+                raise TypeError("Object %s does not have the required keys: t=%s, extra keys %s."%(str(obj), str(t), str(key_diff))) # pylint:disable=line-too-long
+            key_diff = set(field_types.keys()) - set(obj.keys()).union(set(field_defaults.keys()))
+            raise TypeError("Object %s does not have the required keys: t=%s, missing keys %s."%(str(obj), str(t), str(key_diff))) # pylint:disable=line-too-long
+        for field in fields:
+            field_type = field_types[field]
+            if not field in obj:
+                converted_dict[field] = field_defaults[field]
+            else:
+                converted_dict[field] = from_json_obj(obj[field], field_type)
+        return_val = t(**converted_dict)
+        assert is_instance(return_val, t)
+        return return_val
+    if hasattr(t, "__origin__") and hasattr(t, "__args__"): # generics
+        if t.__origin__ is Union:
+            for s in t.__args__:
+                try:
+                    return_val = from_json_obj(obj, s)
+                    assert is_instance(return_val, t)
+                    return return_val
+                except TypeError:
+                    continue
+            raise TypeError("Object %s is not convertible to any of %s."%(str(obj), str(t)))
+        if t.__origin__ is Literal:
+            if not is_instance(obj, t):
+                raise TypeError("Object %s is not allowed (t=%s)."%(str(obj), str(t)))
+            return obj
+        if t.__origin__ is list:
+            if not isinstance(obj, list):
+                raise TypeError("Object %s is not list (t=%s)."%(str(obj), str(t)))
+            return_val = list(from_json_obj(x, t.__args__[0]) for x in obj)
+            assert is_instance(return_val, t)
+            return return_val
+        if t.__origin__ is deque:
+            if not isinstance(obj, list):
+                raise TypeError("Object %s is not list (t=%s)."%(str(obj), str(t)))
+            return_val = deque(from_json_obj(x, t.__args__[0]) for x in obj)
+            assert is_instance(return_val, t)
+            return return_val
+        if t.__origin__ is set:
+            if not isinstance(obj, list):
+                raise TypeError("Object %s is not list (t=%s)."%(str(obj), str(t)))
+            return_val = set(from_json_obj(x, t.__args__[0]) for x in obj)
+            assert is_instance(return_val, t)
+            return return_val
+        if t.__origin__ is frozenset:
+            if not isinstance(obj, list):
+                raise TypeError("Object %s is not list (t=%s)."%(str(obj), str(t)))
+            return_val = frozenset(from_json_obj(x, t.__args__[0]) for x in obj)
+            assert is_instance(return_val, t)
+            return return_val
+        if t.__origin__ is tuple:
+            if not isinstance(obj, list):
+                raise TypeError("Object %s is not list (t=%s)."%(str(obj), str(t)))
+            if len(t.__args__) == 2 and t.__args__[1] is ...: # pylint:disable=no-else-return
+                return_val = tuple(from_json_obj(x, t.__args__[0]) for x in obj)
+                assert is_instance(return_val, t)
+                return return_val
+            else:
+                if len(obj) != len(t.__args__):
+                    raise TypeError("List %s is of incorrect length (t=%s)."%(str(obj), str(t)))
+                return_val = tuple(from_json_obj(x, t.__args__[i]) for i, x in enumerate(obj))
+                assert is_instance(return_val, t)
+                return return_val
+        if t.__origin__ in (dict, Mapping):
+            if not isinstance(obj, (dict, OrderedDict)):
+                raise TypeError("Object %s is not dict or OrderedDict (t=%s)."%(str(obj), str(t)))
+            converted_dict = dict() # type:ignore
+            for field in obj:
+                if not isinstance(field, str):
+                    raise TypeError("Object key %s is string (t=%s)."%(field, str(t)))
+                converted_dict[field] = from_json_obj(obj[field], t.__args__[1])
+            assert is_instance(converted_dict, t)
+            return converted_dict
+        if t.__origin__ is OrderedDict:
+            if not isinstance(obj, OrderedDict):
+                raise TypeError("Object %s is not dict or OrderedDict (t=%s)."%(str(obj), str(t)))
+            converted_dict = OrderedDict() # type:ignore
+            for field in obj:
+                if not isinstance(field, str):
+                    raise TypeError("Object key %s is string (t=%s)."%(field, str(t)))
+                converted_dict[field] = from_json_obj(obj[field], t.__args__[1])
+            assert is_instance(converted_dict, t)
+            return converted_dict
+    raise AssertionError(_UNREACHABLE_ERROR_MSG) # pragma: no cover
---
+++ packages/typing-json/typing_json-0.0.5-py3-none-any.whl/typing_json/typechecking.py
@@ -0,0 +1,140 @@
+""" Dynamic typechecking utilities. """
+
+from typing import Any, Union, Optional
+from collections import deque, OrderedDict
+from collections.abc import Mapping
+from typing_extensions import Literal
+
+TYPECHECKABLE_BASE_TYPES = (bool, int, float, complex, str, bytes, bytearray, memoryview,
+                            list, tuple, range, slice, set, frozenset, dict, type,
+                            deque, OrderedDict, object)
+
+def is_typecheckable(t: Any) -> bool:
+    """ Returns True if is_instance is guaranteed to work with type t. """
+    # pylint:disable=invalid-name
+    if t in TYPECHECKABLE_BASE_TYPES:
+        return True
+    if t in (None, type(None), ..., NotImplemented, Any):
+        return True
+    if hasattr(t, "__origin__") and hasattr(t, "__args__"):
+        if t.__origin__ in (list, tuple, set, frozenset, dict,
+                            deque, OrderedDict, Union, Optional, Mapping):
+            return all(is_typecheckable(s) for s in t.__args__)
+        if t.__origin__ is Literal:
+            return all(isinstance(s, TYPECHECKABLE_BASE_TYPES) or s in (None, ..., NotImplemented, Any) for s in t.__args__) # pylint:disable=line-too-long
+    if is_namedtuple(t):
+        return True
+    return False
+
+def is_instance(obj: Any, t: Any) -> bool:
+    """ Extends isinstance to support the typing package. """
+    # pylint:disable=invalid-name,too-many-return-statements,too-many-branches
+    if t in TYPECHECKABLE_BASE_TYPES:
+        return isinstance(obj, t)
+    if t in (None, type(None)):
+        return obj is None
+    if t is ...:
+        return obj is ...
+    if t is NotImplemented:
+        return obj is NotImplemented
+    if t == Any:
+        return True
+    if is_namedtuple(t):
+        field_types = getattr(t, "_field_types")
+        for field in field_types:
+            if not hasattr(obj, field):
+                return False
+            field_val = getattr(obj, field)
+            if not is_instance(field_val, field_types[field]):
+                return False
+        return True
+    if hasattr(t, "__origin__") and hasattr(t, "__args__"): # generics
+        if t.__origin__ is Union: # includes Optional
+            return any(is_instance(obj, s) for s in t.__args__)
+        if t.__origin__ is Literal:
+            return any(obj is s for s in t.__args__)
+        if t.__origin__ is list: # List[_T]
+            if not isinstance(obj, list):
+                return False
+            return all(is_instance(x, t.__args__[0]) for x in obj)
+        if t.__origin__ is tuple: # Tuple[_T1, _T2, ... ,_TN] or Tuple[_T, Ellipsis]
+            if not isinstance(obj, tuple):
+                return False
+            if len(t.__args__) == 2 and t.__args__[1] is ...: # Tuple[_T, Ellipsis] # pylint:disable=no-else-return
+                return all(is_instance(x, t.__args__[0]) for x in obj)
+            else: # Tuple[_T1, _T2, ... ,_TN]
+                if len(obj) != len(t.__args__):
+                    return False
+                return all(is_instance(x, t.__args__[i]) for i, x in enumerate(obj))
+        if t.__origin__ is set: # Set[_T]
+            if not isinstance(obj, set):
+                return False
+            return all(is_instance(x, t.__args__[0]) for x in obj)
+        if t.__origin__ is frozenset: # FrozenSet[_T]
+            if not isinstance(obj, frozenset):
+                return False
+            return all(is_instance(x, t.__args__[0]) for x in obj)
+        if t.__origin__ is dict: # Dict[_T, _S]
+            if not isinstance(obj, (dict)):
+                return False
+            if not all(is_instance(x, t.__args__[0]) for x in obj):
+                return False
+            if not all(is_instance(obj[x], t.__args__[1]) for x in obj):
+                return False
+            return True
+        if t.__origin__ is deque: # Deque[_T]
+            if not isinstance(obj, deque):
+                return False
+            return all(is_instance(x, t.__args__[0]) for x in obj)
+        if t.__origin__ is OrderedDict: # OrderedDict[_T, _S]
+            if not isinstance(obj, (OrderedDict)):
+                return False
+            if not all(is_instance(x, t.__args__[0]) for x in obj):
+                return False
+            if not all(is_instance(obj[x], t.__args__[1]) for x in obj):
+                return False
+            return True
+        if t.__origin__ is Mapping: # Mapping[_T, _S]
+            if not isinstance(obj, (dict, OrderedDict)):
+                return False
+            if not all(is_instance(x, t.__args__[0]) for x in obj):
+                return False
+            if not all(is_instance(obj[x], t.__args__[1]) for x in obj):
+                return False
+            return True
+    raise TypeError("Type %s is not supported."%str(t))
+
+def is_namedtuple(t: Any) -> bool:
+    """
+        Checks whether a given type is a NamedTuple.
+        Code modified from this stackoverflow question:
+        https://stackoverflow.com/questions/2166818/how-to-check-if-an-object-is-an-instance-of-a-namedtuple
+    """
+    # pylint:disable=invalid-name,too-many-return-statements,too-many-branches,protected-access
+    if not hasattr(t, "__bases__"):
+        return False
+    base_classes = t.__bases__
+    if len(base_classes) != 1 or base_classes[0] != tuple:
+        return False
+    fields = getattr(t, "_fields", None)
+    if not isinstance(fields, tuple):
+        return False
+    if not all(isinstance(n, str) for n in fields):
+        return False
+    field_types = getattr(t, "_field_types", None)
+    if not isinstance(field_types, dict):
+        return False
+    if not all((n in field_types) for n in fields):
+        return False
+    if not all((n in fields) for n in field_types):
+        return False
+    if not all(is_typecheckable(field_types[n]) for n in field_types):
+        return False
+    field_defaults = getattr(t, "_field_defaults", None)
+    if not isinstance(field_defaults, dict):
+        return False
+    if not all((n in fields) for n in field_defaults):
+        return False
+    if not all(is_instance(field_defaults[n], field_types[n]) for n in field_defaults):
+        return False
+    return True
