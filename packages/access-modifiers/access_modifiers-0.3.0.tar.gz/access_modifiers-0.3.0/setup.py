dbc073b1a69eefae9e8c3f49bc3f229b69e947e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:01 +0000

    Add package access-modifiers/0.3.0/access_modifiers-0.3.0.tar.gz

---
+++ packages/access-modifiers/access_modifiers-0.3.0.tar.gz/access_modifiers-0.3.0/setup.py
@@ -0,0 +1,26 @@
+"""Setup access_modifiers."""
+
+import setuptools
+
+
+setuptools.setup(
+    name="access_modifiers",
+    version="0.3.0",
+    author="Frank Niessink",
+    author_email="frank@niessink.com",
+    description="Private and protected access modifiers for Python",
+    license="Apache 2.0",
+    keywords="access modifier,protected,private,oop",
+    url="https://github.com/fniessink/access-modifiers",
+    packages=setuptools.find_packages(),
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development"])
