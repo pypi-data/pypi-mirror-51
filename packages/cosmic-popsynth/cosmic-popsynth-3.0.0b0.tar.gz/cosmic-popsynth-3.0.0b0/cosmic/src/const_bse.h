e8f4984650832bb30cb80845b59971f611a8ebe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:57 +0000

    Add package cosmic-popsynth/3.0.0b0/cosmic-popsynth-3.0.0b0.tar.gz

---
+++ packages/cosmic-popsynth/cosmic-popsynth-3.0.0b0.tar.gz/cosmic-popsynth-3.0.0b0/cosmic/src/const_bse.h
@@ -0,0 +1,42 @@
+*
+* const_bse.h
+*
+      INTEGER idum1
+      COMMON /RAND1/ idum1
+      INTEGER idum2,iy,ir(32)
+      COMMON /RAND2/ idum2,iy,ir
+      INTEGER ktype(0:14,0:14)
+      COMMON /TYPES/ ktype
+      INTEGER tflag,ifflag,nsflag,wdflag,bhflag,windflag,qcflag
+      COMMON /FLAGS/ tflag,ifflag,nsflag,wdflag,bhflag,windflag,qcflag
+      INTEGER ceflag,cekickflag,cemergeflag,cehestarflag,ussn
+      COMMON /CEFLAGS/ ceflag,cekickflag,cemergeflag,cehestarflag,ussn
+      INTEGER pisn_track(2)
+      COMMON /TRACKERS/ pisn_track
+*
+      INTEGER eddlimflag
+      REAL*8 neta,bwind,hewind,mxns,beta,xi,acc2,epsnov
+      REAL*8 eddfac,gamma
+      COMMON /WINDVARS/ neta,bwind,hewind,mxns,beta,xi,acc2,epsnov
+      COMMON /WINDVARS/ eddfac,gamma,eddlimflag
+      REAL*8 alpha1,lambda
+      COMMON /CEVARS/ alpha1,lambda
+      REAL*8 bconst,CK
+      COMMON /MAGVARS/ bconst,CK
+      REAL*8 sigma,sigmadiv,bhsigmafrac,pisn
+      REAL*8 polar_kick_angle,mu_SN1,omega_SN1
+      COMMON /SNVARS/ sigma,sigmadiv,bhsigmafrac,pisn
+      COMMON /SNVARS/ polar_kick_angle,mu_SN1,omega_SN1
+*
+      INTEGER*8 id1_pass,id2_pass
+      REAL*8 merger
+      COMMON /CMCPASS/ merger, id1_pass,id2_pass
+      REAL*8 pts1,pts2,pts3
+      COMMON /POINTS/ pts1,pts2,pts3
+      REAL*8 dmmax,drmax
+      COMMON /TSTEPC/ dmmax,drmax
+      REAL*8 scm(50000,14),spp(20,3)
+      COMMON /SINGLE/ scm,spp
+      REAL*8 bcm(50000,42),bpp(1000,23)
+      COMMON /BINARY/ bcm,bpp
+*
