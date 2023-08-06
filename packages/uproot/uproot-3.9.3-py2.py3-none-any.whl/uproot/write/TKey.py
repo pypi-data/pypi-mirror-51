6142b4084fc2376a7e8518b4c9436453dd8d4f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:08 +0000

    Add package uproot/3.9.3/uproot-3.9.3-py2.py3-none-any.whl

---
+++ packages/uproot/uproot-3.9.3-py2.py3-none-any.whl/uproot/write/TKey.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import struct
+
+import uproot.write.sink.cursor
+import uproot.write.util
+
+class TKey(object):
+    def __init__(self, fClassName, fName, fTitle=b"", fObjlen=0, fSeekKey=100, fSeekPdir=0, fCycle=1):
+        self.fClassName = fClassName
+        self.fName = fName
+        self.fTitle = fTitle
+
+        self.fObjlen = fObjlen
+        self.fSeekKey = fSeekKey
+        self.fSeekPdir = fSeekPdir
+        self.fCycle = fCycle
+        self.fDatime = uproot.write.util.datime()
+        self.fNbytes = self.fObjlen + self.fKeylen
+
+    @property
+    def fKeylen(self):
+        return self._format1.size + uproot.write.sink.cursor.Cursor.length_strings([self.fClassName, self.fName, self.fTitle])
+
+    def update(self):
+        self.cursor.update_fields(self.sink, self._format1, self.fNbytes, self._version, self.fObjlen, self.fDatime, self.fKeylen, self.fCycle, self.fSeekKey, self.fSeekPdir)
+
+    def write(self, cursor, sink):
+        self.cursor = uproot.write.sink.cursor.Cursor(cursor.index)
+        self.sink = sink
+
+        self.update()
+
+        cursor.skip(self._format1.size)
+        cursor.write_string(sink, self.fClassName)
+        cursor.write_string(sink, self.fName)
+        cursor.write_string(sink, self.fTitle)
+
+    _version = 1004
+    _format1 = struct.Struct(">ihiIhhqq")
+
+class TKey32(TKey):
+    _version = 4
+    _format1 = struct.Struct(">ihiIhhii")
