7209e37ae15cd09dba0d39e273a33ac42705dd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:26 +0000

    Add package ckiptagger/0.0.16/ckiptagger-0.0.16.tar.gz

---
+++ packages/ckiptagger/ckiptagger-0.0.16.tar.gz/ckiptagger-0.0.16/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name = "ckiptagger",
+    version = "0.0.16",
+    author = "Peng-Hsuan Li",
+    author_email = "jacobvsdanniel@gmail.com",
+    description = "Neural implementation of CKIP WS, POS, NER tools",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ckiplab/ckiptagger",
+    python_requires = ">=3.6",
+    packages = ["ckiptagger"],
+    package_dir = {"ckiptagger": "src"},
+    extras_require = {
+        "tf": ["tensorflow"],
+        "tfgpu": ["tensorflow-gpu"],
+        "gdown": ["gdown"],
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
