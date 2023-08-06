ac3cdbd195ac7e41bd5c00ed1919f92279052182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:51 +0000

    Add package pyecs/0.2/pyecs-0.2.tar.gz

---
+++ packages/pyecs/pyecs-0.2.tar.gz/pyecs-0.2/pyecs/core.py
@@ -0,0 +1,398 @@
+from __future__ import annotations
+
+from collections import defaultdict
+from dataclasses import dataclass, field
+from time import time
+from typing import Any, Callable, Dict, List, Optional, Tuple, Type
+from uuid import UUID, uuid1
+
+__all__ = ("ECSController", "Component", "Quit", "register_system")
+
+
+class Quit(Exception):
+    ...
+
+
+@dataclass
+class ECSController:
+    """Controller that handles all the entity/component management and system dispatching."""
+
+    _entities: Dict[UUID, List[str]] = field(init=False, default_factory=dict)
+    _components: Dict[str, Dict[UUID, Component]] = field(
+        init=False, default_factory=dict
+    )
+    _systems: Dict[str, Tuple[Callable, List[str], List[UUID]]] = field(
+        init=False, default_factory=dict
+    )
+    _system_groups: Dict[int, List[str]] = field(
+        init=False, default_factory=lambda: defaultdict(list)
+    )
+    _to_be_delete: Tuple[List[UUID], List[Tuple[UUID, str]]] = field(  # type: ignore
+        init=False, default=(list(), list())
+    )
+    _last_update: float = field(init=False, default_factory=time)
+    data: Dict[str, Any] = field(init=False, default_factory=dict)
+    time_per_cycle: float = 1 / 60
+
+    def __post_init__(self):
+        for component_type in _COMPONENT_REGISTRY:
+            self._components[component_type.type()] = dict()
+
+    def __getitem__(self, key: str) -> Any:
+        return self.data[key]
+
+    def __setitem__(self, key: str, value: Any):
+        self.data[key] = value
+
+    def add_entity(self, *components: Component) -> UUID:
+        """Add a single entity to the controller.
+
+        *Public interface for `_add_entity`*
+
+        :param *components: components to add to the entity
+        :return: entity id
+        """
+        entity_id = self._add_entity()
+        self.add_components(entity_id, *components)
+        return entity_id
+
+    def _add_entity(self) -> UUID:
+        """Generate a unique entity id and setup component type cache for it.
+
+        :return: entity id
+        """
+        entity_id = uuid1()
+        self._entities[entity_id] = []
+        return entity_id
+
+    def get_entity(self, entity_id: UUID, *components: Type[Component]) -> EntityProxy:
+        """Get an entity proxy correlating to an entity and some of its components
+
+        :param entity_id: id of the target entity
+        :param *components: components to collect in the proxy
+        :raises KeyError: Unknown entity id
+        :return: an entity proxy populated with the given components
+        """
+        return EntityProxy(
+            uuid=entity_id,
+            components=dict(
+                zip(
+                    [component.type() for component in components],
+                    self.get_components(entity_id, *components),
+                )
+            ),
+            _controller=self,
+        )
+
+    def delete_entity(self, entity_id: UUID, *, instant: bool = False):
+        """Delete an entity from the controller.
+
+        :param entity_id: entity to delete
+        :param instant: delete immediately instead of queueing deletion, defaults to False
+        :raises KeyError: Unknown entity id
+        """
+        if entity_id not in self._entities:
+            raise KeyError(f"Unknown entity id {entity_id}")
+        if instant:
+            self._delete_entity(entity_id)
+        else:
+            self._to_be_delete[0].append(entity_id)
+
+    def _delete_entity(self, entity_id: UUID):
+        """Actually delete an entity from the controller
+
+        :param entity_id: entity to delete
+        """
+        for component_type in self._entities[entity_id]:
+            self._delete_component(entity_id, component_type)
+        del self._entities[entity_id]
+
+    def add_components(self, entity_id: UUID, *components: Component):
+        """Add components to an existing entity.
+
+        *Public interface for `_add_component`*
+
+        :param entity_id: id of the entity to add the components to
+        :param *components: components to add to the entity
+        """
+        for component in components:
+            self._add_component(entity_id, component)
+
+    def _add_component(self, entity_id: UUID, component: Component):
+        """Add a component to an existing entity.
+
+        :param entity_id: id of the entity to add the component to
+        :param component: component to add to the entity
+        :raises KeyError: Unknown entity id
+        :raises TypeError: Unknown component type
+        :raises KeyError: Entity already has component of that type
+        """
+        component_type = type(component).type()
+        if entity_id not in self._entities:
+            raise KeyError(f"Unknown entity id {entity_id}")
+        elif component_type not in self._components:
+            raise TypeError(f"Unknown component type {component_type}")
+        elif component_type in self._entities[entity_id]:
+            raise KeyError(
+                f"Entity {entity_id} already has a component of type {component_type}"
+            )
+        self._entities[entity_id].append(component_type)
+        self._components[component_type][entity_id] = component
+
+        # calculate which systems will access the entity based on the change
+        for _, target_components, entity_cache in self._systems.values():
+            if (
+                entity_id not in entity_cache
+                and component_type in target_components
+                and all(
+                    target_component in self._entities[entity_id]
+                    for target_component in target_components
+                )
+            ):
+                entity_cache.append(entity_id)
+
+    def get_components(
+        self, entity_id: UUID, *components: Type[Component]
+    ) -> List[Component]:
+        """Get a list of components correlating to an entity
+
+        :param entity_id: id of the target entity
+        :param *components: components to collect in the proxy
+        :raises KeyError: Unknown entity id
+        :return: a list filled with the request components
+        """
+        if entity_id not in self._entities:
+            raise KeyError(f"Unknown entity id {entity_id}")
+        return [self._get_component(entity_id, component) for component in components]
+
+    def _get_component(self, entity_id: UUID, component: Type[Component]) -> Component:
+        """Get a component from an entity
+
+        :param entity_id: id of the target entity
+        :param component: component type to get
+        :raises TypeError: Unknown component type
+        :raises KeyError: Entity doesn't have a component of that type
+        :return: the requested component
+        """
+        component_type = component.type()
+        if component_type not in self._components:
+            raise TypeError(f"Unknown component type {component_type}")
+        elif component_type not in self._entities[entity_id]:
+            raise KeyError(
+                f"Entity {entity_id} doesn't have a component of type {component_type}"
+            )
+        return self._components[component_type][entity_id]
+
+    def delete_components(
+        self, entity_id: UUID, *component_types: Type[Component], instant: bool = False
+    ):
+        """Delete components from an entity
+
+        :param entity_id: entity to delete component from
+        :param *components: components to delete from the entity
+        :param instant: delete immediately instead of queueing deletion, defaults to False
+        :raises KeyError: Unknown entity id
+        :raises TypeError: Unknown component type
+        :raises KeyError: Entity already has component of that type
+        """
+        if entity_id not in self._entities:
+            raise KeyError(f"Unknown entity id {entity_id}")
+        for component_type in component_types:
+            component_type_ = component_type.type()
+            if component_type_ not in self._components:
+                raise TypeError(f"Unknown component type {component_type_}")
+            elif component_type_ not in self._entities[entity_id]:
+                raise KeyError(
+                    f"Entity {entity_id} doesn't have a component of type {component_type_}"
+                )
+            if instant:
+                self._delete_component(entity_id, component_type_)
+            else:
+                self._to_be_delete[1].append((entity_id, component_type_))
+
+    def _delete_component(self, entity_id: UUID, component_type: str):
+        """Actually delete a component from an entity
+
+        :param entity_id: entity to delete component from
+        :param component: component to delete to the entity
+        """
+        del self._components[component_type][entity_id]
+        del self._entities[entity_id][self._entities[entity_id].index(component_type)]
+        for _, target_components, entity_cache in self._systems.values():
+            if component_type in target_components and entity_id in entity_cache:
+                del entity_cache[entity_cache.index(entity_id)]
+
+    def register_system(
+        self,
+        target_components: Tuple[Type[Component], ...] = tuple(),
+        *,
+        name: Optional[str] = None,
+        group: int = 0,
+    ) -> Callable[[Callable], Callable]:
+        """Register a system with the controller
+
+        :param target_components: components the system targets, defaults to empty tuple
+        :param name: name of the system, defaults to the name of the system function
+        :param group: group the system is part of, defaults to 0
+        :return: a decorator to wrap the system function
+        """
+
+        def inner_register_system(func: Callable) -> Callable:
+            name_ = name or func.__name__
+            self._systems[name_] = (
+                func,
+                [component.type() for component in target_components],
+                list(),
+            )
+            self._system_groups[group].append(name_)
+            self._precalculate_system(name_)
+            return func
+
+        return inner_register_system
+
+    def _precalculate_system(self, name: str):
+        """Calculate the entity cache of a system
+
+        :param name: system to calculate the cache for
+        """
+        _, target_components, entity_cache = self._systems[name]
+        for entity_id, component_types in self._entities.items():
+            if entity_id not in entity_cache and all(
+                target_component in component_types
+                for target_component in target_components
+            ):
+                entity_cache.append(entity_id)
+
+    def _do_cleanup(self):
+        """Do cleanup that was cached so far."""
+        entity_deletions, component_deletions = self._to_be_delete
+        for entity_id, component_type in component_deletions:
+            self._delete_component(entity_id, component_type)
+        for entity_id in entity_deletions:
+            self._delete_entity(entity_id)
+        self._to_be_delete = (list(), list())  # type: ignore
+
+    def run(self):
+        """Run the controller"""
+        start = time()
+        while True:
+            try:
+                delta_t = time() - start
+                start = time()
+                self._run_systems(delta_t)
+            except Quit:
+                break
+            else:
+                self._do_cleanup()
+
+    def _run_systems(self, delta_t: float):
+        """Run all systems.
+
+        :param delta_t: time difference since last execution
+        """
+        for group in sorted(self._system_groups.keys()):
+            for system in self._system_groups[group]:
+                self._run_system(system, delta_t)
+
+    def _run_system(self, name: str, delta_t: float):
+        """Run a single system
+
+        :param name: name of the system to run
+        :param delta_t: time difference since last execution
+        """
+        func, target_components, entity_cache = self._systems[name]
+        if target_components:
+            entities = [
+                self.get_entity(
+                    entity_id,
+                    *[
+                        _COMPONENT_REGISTRY_REV[target_component]
+                        for target_component in target_components
+                    ],
+                )
+                for entity_id in entity_cache
+            ]
+            func(delta_t, self.data, entities)
+        else:
+            func(delta_t, self.data)
+
+
+@dataclass(repr=False)
+class EntityProxy:
+    """Proxy object that represents a subset the component list that defines an entity."""
+
+    _controller: ECSController
+    uuid: UUID
+    components: Dict[str, Component]
+
+    def delete(self):
+        self._controller.delete_entity(self.uuid)
+
+    def __getattribute__(self, name: str) -> Any:
+        components = object.__getattribute__(self, "components")
+        if name in components:
+            return components[name]
+        else:
+            return object.__getattribute__(self, name)
+
+    def __delattr__(self, name: str):
+        if name in self.components:
+            self._controller.delete_components(self.uuid, _COMPONENT_REGISTRY_REV[name])
+            del self.components[name]
+        else:
+            super().__delattr__(name)
+
+
+_COMPONENT_REGISTRY: Dict[Type[Component], str] = dict()
+_COMPONENT_REGISTRY_REV: Dict[str, Type[Component]] = dict()
+
+
+class Component:
+    def __init_subclass__(cls, identifier: Optional[str] = None):
+        super().__init_subclass__()
+        name = identifier or cls.__name__
+        _COMPONENT_REGISTRY[cls] = name
+        _COMPONENT_REGISTRY_REV[name] = cls
+
+    @classmethod
+    def type(cls) -> str:
+        return _COMPONENT_REGISTRY[cls]
+
+
+_SYSTEM_PREREGISTRY: Dict[
+    str, Tuple[Tuple[Type[Component], ...], Optional[str]]
+] = dict()
+
+
+def preconfigure_system(
+    target_components: Tuple[Type[Component], ...] = tuple(),
+    *,
+    name: Optional[str] = None,
+):
+    """Preconfigure a system for later registrations
+
+    :param target_components: components the system targets, defaults to empty tuple
+    :param name: name of the system, defaults to the name of the system function
+    """
+
+    def inner(func: Callable) -> Callable:
+        _SYSTEM_PREREGISTRY[func.__name__] = (target_components, name)
+        return func
+
+    return inner
+
+
+def register_system(controller: ECSController, system: Callable, group: int = 0):
+    """Register a preconfigured system with a controller
+
+    :param controller: the controller to register the system with
+    :param system: the preconfigured system to register
+    :param group: the system-group to register the system in, defaults to 0
+    :raises KeyError: Not a preconfigured system
+    """
+    if system.__name__ not in _SYSTEM_PREREGISTRY:
+        raise KeyError(
+            "'register_system' is only to be used with preconfigured systems, please use 'ECSController.register_system' instead."
+        )
+    target_components, name = _SYSTEM_PREREGISTRY[system.__name__]
+    controller.register_system(target_components, name=name, group=group)(system)
---
+++ packages/pyecs/pyecs-0.2.tar.gz/pyecs-0.2/test/test_core.py
@@ -0,0 +1,274 @@
+from uuid import uuid1
+
+import pytest
+
+from pyecs import core
+
+
+class ComponentFullname(core.Component):
+    ...
+
+
+class ComponentA(core.Component, identifier="A"):
+    ...
+
+
+class ComponentB(core.Component, identifier="B"):
+    ...
+
+
+def test_components():
+    assert ComponentFullname in core._COMPONENT_REGISTRY
+    assert core._COMPONENT_REGISTRY[ComponentFullname] == "ComponentFullname"
+    assert "ComponentFullname" in core._COMPONENT_REGISTRY_REV
+    assert core._COMPONENT_REGISTRY_REV["ComponentFullname"] == ComponentFullname
+
+    assert ComponentA in core._COMPONENT_REGISTRY
+    assert core._COMPONENT_REGISTRY[ComponentA] == "A"
+    assert "A" in core._COMPONENT_REGISTRY_REV
+    assert core._COMPONENT_REGISTRY_REV["A"] == ComponentA
+
+    assert ComponentB in core._COMPONENT_REGISTRY
+    assert core._COMPONENT_REGISTRY[ComponentB] == "B"
+    assert "B" in core._COMPONENT_REGISTRY_REV
+    assert core._COMPONENT_REGISTRY_REV["B"] == ComponentB
+
+
+def test_controller():
+    controller = core.ECSController()
+
+    assert controller._components == {
+        component_type: dict() for component_type in core._COMPONENT_REGISTRY_REV
+    }
+
+    uuid_a = controller.add_entity()
+
+    assert uuid_a in controller._entities
+    assert controller._entities[uuid_a] == []
+
+    componentA_a = ComponentA()
+    controller.add_components(uuid_a, componentA_a)
+
+    assert controller._entities[uuid_a] == ["A"]
+    assert uuid_a in controller._components["A"]
+    assert controller._components["A"][uuid_a] == componentA_a
+
+    componentA_b = ComponentA()
+    componentB_b = ComponentB()
+    uuid_b = controller.add_entity(componentA_b, componentB_b)
+
+    assert uuid_b in controller._entities
+    assert len(controller._entities) == 2
+    assert controller._entities[uuid_b] == ["A", "B"]
+    assert uuid_b in controller._components["A"]
+    assert controller._components["A"][uuid_b] == componentA_b
+    assert uuid_b in controller._components["B"]
+    assert controller._components["B"][uuid_b] == componentB_b
+
+    controller.delete_components(uuid_b, ComponentB, instant=True)
+
+    assert controller._entities[uuid_b] == ["A"]
+    assert uuid_b not in controller._components["B"]
+
+    controller.delete_entity(uuid_b, instant=True)
+
+    assert uuid_b not in controller._entities
+    assert uuid_b not in controller._components["A"]
+
+    uuid_c = uuid1()
+
+    class UnregisteredComponent(core.Component):
+        ...
+
+    with pytest.raises(TypeError):
+        # generate entity with unknown component type
+        controller.add_entity(UnregisteredComponent())
+
+    with pytest.raises(KeyError):
+        # add component to unknown entity id
+        controller.add_components(uuid_c, ComponentA())
+
+    with pytest.raises(KeyError):
+        # add component to entity that already has a component of that type
+        controller.add_components(uuid_a, ComponentA())
+
+    with pytest.raises(KeyError):
+        # delete unknown entity
+        controller.get_components(uuid_c, ComponentA)
+
+    with pytest.raises(TypeError):
+        # get component of unknown type
+        controller.get_components(uuid_a, UnregisteredComponent)
+
+    with pytest.raises(KeyError):
+        # get component which doesn't exist in entity
+        controller.get_components(uuid_a, ComponentB)
+
+    with pytest.raises(KeyError):
+        # delete unknown entity
+        controller.delete_entity(uuid_c)
+
+    with pytest.raises(KeyError):
+        # delete component in unknown entity
+        controller.delete_components(uuid_c, ComponentA)
+
+    with pytest.raises(TypeError):
+        # delete component of unknown type
+        controller.delete_components(uuid_a, UnregisteredComponent)
+
+    with pytest.raises(KeyError):
+        # delete component which doesn't exist in entity
+        controller.delete_components(uuid_a, ComponentB)
+
+
+def test_persistant_data():
+    controller = core.ECSController()
+
+    # Test persistant data on the controller
+    @controller.register_system()
+    def system1(delta_t, data):
+        data["last_delta_t"] = delta_t
+
+    @controller.register_system()
+    def system2(delta_t, data):
+        assert data["last_delta_t"] == delta_t
+
+    controller._run_system("system1", 0)
+    assert "last_delta_t" in controller.data
+    assert controller.data["last_delta_t"] == 0
+    controller._run_system("system2", 0)
+
+
+def test_systems():
+    controller = core.ECSController()
+
+    @controller.register_system((ComponentA,))
+    def system_a(delta_t, data, entities):
+        # abuse delta_t to check if entities is of proper length
+        assert len(entities) == delta_t
+        assert all(isinstance(entity, core.EntityProxy) for entity in entities)
+        assert all(len(entity.components) == 1 for entity in entities)
+        assert all("A" in entity.components for entity in entities)
+        assert all(isinstance(entity.A, ComponentA) for entity in entities)
+
+    @controller.register_system((ComponentA, ComponentB))
+    def system_ab(delta_t, data, entities):
+        # abuse delta_t to check if entities is of proper length
+        assert len(entities) == delta_t
+        assert all(len(entity.components) == 2 for entity in entities)
+        assert all(
+            "A" in entity.components and "B" in entity.components for entity in entities
+        )
+        assert all(
+            isinstance(entity.A, ComponentA) and isinstance(entity.B, ComponentB)
+            for entity in entities
+        )
+
+    controller._run_system("system_a", 0)
+    controller._run_system("system_ab", 0)
+
+    uuid_a = controller.add_entity(ComponentA())
+    controller._run_system("system_a", 1)
+    controller._run_system("system_ab", 0)
+
+    uuid_b = controller.add_entity(ComponentA())
+    controller._run_system("system_a", 2)
+    controller._run_system("system_ab", 0)
+
+    controller.add_components(uuid_a, ComponentB())
+    controller._run_system("system_a", 2)
+    controller._run_system("system_ab", 1)
+
+    controller.add_components(uuid_b, ComponentB())
+    controller._run_system("system_a", 2)
+    controller._run_system("system_ab", 2)
+
+    _ = controller.add_entity(ComponentB())
+    controller._run_system("system_a", 2)
+    controller._run_system("system_ab", 2)
+
+
+@core.preconfigure_system()
+def empty_system(delta_t, data):
+    ...
+
+
+def test_prebuilt():
+    controller = core.ECSController()
+
+    core.register_system(controller, empty_system)
+
+    assert "empty_system" in controller._systems
+    assert 0 in controller._system_groups
+    assert "empty_system" in controller._system_groups[0]
+
+    def unregistered_system(delta_t, data, entities):
+        ...
+
+    with pytest.raises(KeyError):
+        core.register_system(controller, unregistered_system)
+
+
+def test_system_precalculation():
+    controller = core.ECSController()
+
+    uuid = controller.add_entity(ComponentA())
+
+    @controller.register_system((ComponentA,))
+    def system(delta_t, data, entities):
+        assert len(entities) == 1
+        assert entities[0].uuid == uuid
+
+    controller._run_system("system", 0)
+
+
+def test_quit_exception():
+    controller = core.ECSController()
+    controller["tick"] = 0
+
+    @controller.register_system()
+    def quit_timeout(delta_t, data):
+        data["tick"] += 1
+        if data["tick"] == 4:
+            raise core.Quit
+
+    controller.run()
+    assert controller["tick"] == 4
+
+
+def test_automatic_cleanup():
+    controller = core.ECSController()
+
+    uuid = controller.add_entity(ComponentA(), ComponentB())
+    controller.delete_components(uuid, ComponentB)
+
+    assert controller._entities[uuid] == ["A", "B"]
+    assert controller._to_be_delete == ([], [(uuid, "B")])
+
+    controller.delete_entity(uuid)
+
+    assert uuid in controller._entities
+    assert controller._to_be_delete == ([uuid], [(uuid, "B")])
+
+    controller._do_cleanup()
+
+    assert controller._to_be_delete == ([], [])
+    assert uuid not in controller._entities
+    assert uuid not in controller._components["A"]
+    assert uuid not in controller._components["B"]
+
+    @controller.register_system((ComponentA, ComponentB))
+    def delete_system(delta_t, data, entities):
+        if len(entities) > 0:
+            for entity in entities:
+                del entity.A
+                entity.delete()
+                del entity.uuid
+                assert not hasattr(entity, "uuid")
+
+        else:
+            raise core.Quit()
+
+    controller.add_entity(ComponentA(), ComponentB())
+    controller.run()
+    assert len(controller._entities) == 0
