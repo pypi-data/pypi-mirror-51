6a7fc1999388d933a6013d1649fe385eb4f1642a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:04 +0000

    Add package qaviton-package-manager/2019.9.1.21.20.58.634894/qaviton_package_manager-2019.9.1.21.20.58.634894.tar.gz

---
+++ packages/qaviton-package-manager/qaviton_package_manager-2019.9.1.21.20.58.634894.tar.gz/qaviton_package_manager-2019.9.1.21.20.58.634894/qaviton_package_manager/utils/functions.py
@@ -0,0 +1,72 @@
+import os
+import shutil
+import socket
+from contextlib import closing
+from qaviton_package_manager.conf import REQUIREMENTS
+from qaviton_package_manager.utils.pip_wrapper import pip
+from qaviton_package_manager.utils.system import python
+
+
+def get_requirements(root):
+    requirements = root + os.sep + REQUIREMENTS
+    if not os.path.exists(requirements):
+        print(
+            f'{REQUIREMENTS} not found\nP.S. you can change the default requirements filename with qaviton_package_manager.conf.REQUIREMENTS = "filename"')
+        name = input(f'select REQUIREMENTS filename({REQUIREMENTS} default):')
+        if not name: name = REQUIREMENTS
+        requirements = root + os.sep + name
+        if not os.path.exists(requirements):
+            pip.freeze(requirements)
+    return requirements
+
+
+def try_to(f, *args, **kwargs):
+    try:
+        return f(*args, **kwargs)
+    except Exception as e:
+        return e
+
+
+def try_or_none(f, *args, **kwargs):
+    try:
+        return f(*args, **kwargs)
+    except:
+        return
+
+
+def clean_distibution():
+    if os.path.exists('build'): shutil.rmtree('build')
+    if os.path.exists('dist'): shutil.rmtree('dist')
+
+
+def create_distibution_packages():
+    clean_distibution()
+    python('setup.py sdist bdist_wheel --universal')
+
+
+def upload_to_pypi(username, password):
+    create_distibution_packages()
+    python(f'-m twine upload -u "{username}" -p "{password}" dist/*')
+    clean_distibution()
+
+
+def get_package_name():
+    return python('-c "from setup import package_name;print(package_name)"').decode('utf-8').splitlines()[0]
+
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+
+
+def package_match(package, requirement):
+    """https://www.python.org/dev/peps/pep-0440/#version-specifiers"""
+    if package == requirement \
+    or requirement.startswith(package + '=') \
+    or requirement.startswith(package + '>') \
+    or requirement.startswith(package + '<') \
+    or requirement.startswith(package + '~') \
+    or requirement.startswith(package + '!'):
+        return True
