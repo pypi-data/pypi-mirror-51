e743643aa795c63f943a3056e4c66d52762663e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:46 +0000

    Add package meta-permissions/0.1.0/meta_permissions-0.1.0-py3-none-any.whl

---
+++ packages/meta-permissions/meta_permissions-0.1.0-py3-none-any.whl/meta_permissions/groups_updater.py
@@ -0,0 +1,14 @@
+from edc_permissions.groups_updater import GroupsUpdater as Base
+from edc_permissions.constants import LAB, AUDITOR
+
+from .group_names import RANDO, AE_REVIEW
+
+
+class GroupsUpdater(Base):
+
+    extra_group_names = [RANDO, AE_REVIEW]
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ensure_users_not_in_group(
+            RANDO, users_by_groups=[AUDITOR, LAB])
