572e1811314d00a378a3b949117487800e73128e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:31 +0000

    Add package linuk/1.0.1/linuk-1.0.1.tar.gz

---
+++ packages/linuk/linuk-1.0.1.tar.gz/linuk-1.0.1/README.md
@@ -0,0 +1,16 @@
+# linuk
+Python Utility library for faster coding.
+
+# Installation
+    
+    pip install linuk
+
+
+# Example
+    
+    from linuk.basic import listmaker
+    listmaker('ab cd ef gh')
+    
+## for more documentation use built in help() command.
+    
+    
---
+++ packages/linuk/linuk-1.0.1.tar.gz/linuk-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="linuk",
+    version="1.0.1",
+    author="Kazi_jaber",
+    author_email="jaforjaber@gmail.com",
+    description="Library to make coding easier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Psychewolf/linuk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
