94d6d68a9f9fb5a37582d6fab23808ed4e1a32ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:06 +0000

    Add package geomac-wildfires/0.0.6/geomac_wildfires-0.0.6-py2.py3-none-any.whl

---
+++ packages/geomac-wildfires/geomac_wildfires-0.0.6-py2.py3-none-any.whl/geomac_wildfires/__init__.py
@@ -0,0 +1,27 @@
+import fiona
+from geojson import Feature, FeatureCollection
+
+
+def get_active_fires():
+    """
+    Get GeoMAC data
+    """
+    url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/active_perimeters_dd83.zip'
+    with fiona.open(f'zip+{url}') as f:
+        return FeatureCollection([Feature(geometry=d['geometry'], properties=d) for d in f])
+
+def get_all_fires():
+    """
+    Get GeoMAC data for all fires
+    """
+    url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/perimeters_dd83.zip'
+    with fiona.open(f'zip+{url}') as f:
+        return FeatureCollection([Feature(geometry=d['geometry'], properties=d) for d in f])
+
+def get_nifc_sitrep():
+    """
+    Get NIFC Sit Rep data
+    """
+    url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/current_year_all_states/nifc_sit_rep_dd83.zip'
+    with fiona.open(f'zip+{url}') as f:
+        return FeatureCollection([Feature(geometry=d['geometry'], properties=d) for d in f])
