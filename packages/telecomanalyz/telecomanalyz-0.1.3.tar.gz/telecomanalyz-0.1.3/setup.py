8c21d6e2f7b28aafde5c8a405689cbf732b6f7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:10 +0000

    Add package telecomanalyz/0.1.3/telecomanalyz-0.1.3.tar.gz

---
+++ packages/telecomanalyz/telecomanalyz-0.1.3.tar.gz/telecomanalyz-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='telecomanalyz',
+      version='0.1.3',
+      description='analyz job for telecom',
+      url='https://github.com/cuisidong/Demon',
+      author='cuisidong',
+      author_email='18605629932@wo.com.cn',
+      license='MIT',
+      packages=['telecomanalyz'],
+      install_requires=['geographiclib','mplleaflet','matplotlib'],
+      zip_safe=False)
