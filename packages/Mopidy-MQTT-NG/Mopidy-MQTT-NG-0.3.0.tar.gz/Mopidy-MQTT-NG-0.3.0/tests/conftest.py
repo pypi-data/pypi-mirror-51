d559f6c80fa7cf738dfef70482746292c1b00135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:42 +0000

    Add package Mopidy-MQTT-NG/0.3.0/Mopidy-MQTT-NG-0.3.0.tar.gz

---
+++ packages/Mopidy-MQTT-NG/Mopidy-MQTT-NG-0.3.0.tar.gz/Mopidy-MQTT-NG-0.3.0/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from mopidy.core import Core
+
+
+@pytest.fixture
+def config():
+    return {
+        'core': {},
+        'mqtt': {
+            'host': 'localhost',
+            'port': 1883,
+            'topic': 'mopidy',
+        },
+    }
+
+
+@pytest.fixture
+def core(config):
+    actor = Core.start(
+        config=config, backends=[]).proxy()
+
+    yield actor
+    actor.stop()
