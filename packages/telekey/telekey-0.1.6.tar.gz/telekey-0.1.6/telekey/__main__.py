e924988dc7cc4eaedf8ef4a7e33ae0502e9988d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:25 +0000

    Add package telekey/0.1.6/telekey-0.1.6.tar.gz

---
+++ packages/telekey/telekey-0.1.6.tar.gz/telekey-0.1.6/telekey/__main__.py
@@ -0,0 +1,28 @@
+import os
+import click
+
+from distutils.dir_util import copy_tree
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('botname', type=str)
+def newbot(botname):
+    project_dir = os.path.join(os.getcwd(), botname)
+    if not os.path.isdir(project_dir):
+        copy_tree(
+            os.path.join(os.path.abspath(os.path.dirname(__file__)), 'project'),
+            os.path.join(os.getcwd(), botname)
+        )
+    else:
+        print(f'the {botname} directory is exist!')
+
+def main():
+    cli()
+
+if __name__ == "__main__":
+    main()
