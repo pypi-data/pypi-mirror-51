38cf9e81ecc8d5c2da848304b5202c814686318c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:19 +0000

    Add package shreqt/0.2.2/shreqt-0.2.2.tar.gz

---
+++ packages/shreqt/shreqt-0.2.2.tar.gz/shreqt-0.2.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['shreqt']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyexasol>=0.6.4,<0.7.0',
+ 'sqlalchemy-exasol>=2.0,<3.0',
+ 'sqlalchemy>=1.3,<2.0',
+ 'sqlparse>=0.3.0,<0.4.0']
+
+setup_kwargs = {
+    'name': 'shreqt',
+    'version': '0.2.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Zibi Rzepka',
+    'author_email': 'zibi.rzepka@revolut.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
