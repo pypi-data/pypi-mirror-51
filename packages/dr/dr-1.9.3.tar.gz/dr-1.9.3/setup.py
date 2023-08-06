f63774e3b21cb08264ecd8e438905908e001da76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:05 +0000

    Add package dr/1.9.3/dr-1.9.3.tar.gz

---
+++ packages/dr/dr-1.9.3.tar.gz/dr-1.9.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dr',
+    version='1.9.3',
+    packages=["dr"],
+    description='An ed-like devRant client',
+    url='https://github.com/Ewpratten/dr',
+    author='Evan Pratten',
+    author_email='ewpratten@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['devrantsimple','classrant'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+    ),
+    entry_points={
+        'console_scripts': [
+            'dr = dr.__main__:main'
+        ]
+    })
