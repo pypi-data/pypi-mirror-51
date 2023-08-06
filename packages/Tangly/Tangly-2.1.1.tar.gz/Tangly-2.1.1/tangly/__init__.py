828574c74abfd364393e7ca43c9d72f228741f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:21 +0000

    Add package Tangly/2.1.1/Tangly-2.1.1.tar.gz

---
+++ packages/Tangly/Tangly-2.1.1.tar.gz/Tangly-2.1.1/tangly/__init__.py
@@ -0,0 +1,15 @@
+""" Writen by Rafael Rayes, Tangly will help you transforming
+a list into a table.
+USAGE:
+
+
+import tangly
+
+my_list = [['this', 'that'],
+           ['these', 'those'],]
+
+tangly.table(my_list)
+"""
+from . import helping, table
+name = 'Tangly'
+version = '2.1.1'
