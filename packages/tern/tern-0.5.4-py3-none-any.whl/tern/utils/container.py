a9d4beaef334036fba0d40bf906adaf723787995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:31 +0000

    Add package tern/0.5.4/tern-0.5.4-py3-none-any.whl

---
+++ packages/tern/tern-0.5.4-py3-none-any.whl/tern/utils/container.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017-2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+"""
+Docker container operations
+"""
+
+import docker
+import grp
+import logging
+import os
+import pwd
+import tarfile
+import time
+from requests.exceptions import HTTPError
+
+
+from tern.utils.constants import container
+from tern.utils.constants import logger_name
+from tern.utils.constants import temp_folder
+from tern.utils.constants import temp_tarfile
+
+
+# timestamp tag
+tag = str(int(time.time()))
+
+# global logger
+logger = logging.getLogger(logger_name)
+
+# global docker client
+client = None
+try:
+    client = docker.from_env()
+except IOError:
+    logger.critical("Docker daemon not running")
+    raise Exception("Critical Error using Docker API. See logs for details")
+except OSError:  # pylint: disable=duplicate-except
+    logger.critical("User has no access to docker unix socket")
+    raise Exception("Critical Error using Docker API. See logs for details")
+
+
+def is_sudo():
+    '''Check if the user uses sudo for docker commands'''
+    sudo = True
+    try:
+        members = grp.getgrnam('docker').gr_mem
+        if pwd.getpwuid(os.getuid()).pw_name in members:
+            sudo = False
+    except KeyError:
+        pass
+    return sudo
+
+
+def check_container():
+    '''Check is the test container is running'''
+    try:
+        client.containers.get(container)
+        return True
+    except docker.errors.NotFound:
+        return False
+
+
+def check_image(image_tag_string):
+    '''Check if image exists'''
+    logger.debug("Checking if image \"%s\" is available on disk...",
+        image_tag_string)
+    try:
+        client.images.get(image_tag_string)
+        logger.debug("Image \"%s\" found", image_tag_string)
+        return True
+    except docker.errors.ImageNotFound:
+        return False
+
+
+def pull_image(image_tag_string):
+    '''Try to pull an image from Dockerhub'''
+    logger.debug("Attempting to pull image \"%s\"", image_tag_string)
+    try:
+        client.images.pull(image_tag_string)
+        logger.debug("Image \"%s\" downloaded", image_tag_string)
+        return True
+    except docker.errors.ImageNotFound:
+        logger.warning("No such image: \"%s\"", image_tag_string)
+        return False
+
+
+def build_container(dockerfile, image_tag_string):
+    '''Invoke docker command to build a docker image from the dockerfile
+    It is assumed that docker is installed and the docker daemon is running'''
+    path = os.path.dirname(dockerfile)
+    if not check_image(image_tag_string):
+        try:
+            client.images.build(path=path, tag=image_tag_string, nocache=True)
+        except (TypeError, docker.errors.APIError, docker.errors.BuildError):  # pylint: disable=try-except-raise
+            raise
+
+
+def start_container(image_tag_string):
+    '''Start the test container in detach state'''
+    try:
+        client.containers.run(image_tag_string, name=container, detach=True)
+    except HTTPError:
+        # container may already be running
+        pass
+    try:
+        remove_container()
+        client.containers.run(image_tag_string, name=container, detach=True)
+    except HTTPError:
+        # not sure what the error is now
+        raise Exception("Cannot remove running container")
+
+
+def remove_container():
+    '''Remove the running test container'''
+    active_c = client.containers.get(container)
+    active_c.stop()
+    active_c.remove()
+
+
+def remove_image(image_tag_string):
+    '''Remove an image'''
+    if check_image(image_tag_string):
+        client.images.remove(image_tag_string)
+
+
+def get_image_id(image_tag_string):
+    '''Get the image ID by inspecting image and tag'''
+    try:
+        image = client.images.get(image_tag_string)
+        return image.id.split(':').pop()
+    except docker.errors.ImageNotFound:
+        return ''
+
+
+def extract_image_metadata(image_tag_string):
+    '''Run docker save and extract the files in a temporary directory'''
+    temp_path = os.path.abspath(temp_folder)
+    try:
+        image = client.images.get(image_tag_string)
+        result = image.save(chunk_size=2097152, named=True)
+        # write all of the tar byte stream into temporary tar file
+        with open(temp_tarfile, 'wb') as f:
+            for chunk in result:
+                f.write(chunk)
+        # extract tarfile into folder
+        with tarfile.open(temp_tarfile) as tar:
+            tar.extractall(temp_path)
+        # remove temporary tar file
+        os.remove(temp_tarfile)
+        if not os.path.exists(temp_path):
+            raise IOError('Unable to untar Docker image')
+    except docker.errors.APIError:  # pylint: disable=try-except-raise
+        raise
---
+++ packages/tern/tern-0.5.4-py3-none-any.whl/tern/utils/general.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2017-2019 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+import os
+import random
+import re
+import subprocess  # nosec
+from contextlib import contextmanager
+
+from tern import Version
+from tern.utils import constants
+
+
+# regex strings
+cleaning = '[\t\\\\]'
+concat = '&&|;'
+
+
+# from https://stackoverflow.com/questions/6194499/pushd-through-os-system
+@contextmanager
+def pushd(path):
+    curr_path = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(curr_path)
+
+
+def initialize_names():
+    randint = random.randint(10000, 99999)  # nosec
+    constants.image = constants.image + "_" + str(randint)
+    constants.tag = constants.tag + "_" + str(randint)
+    constants.container = constants.container + "_" + str(randint)
+
+
+def clean_command(command):
+    '''Given a command string, clean out all whitespaces, tabs and line
+    indentations
+    Leave && alone'''
+    return re.sub(cleaning, '', command).strip()
+
+
+def split_command(command):
+    '''Given a string of concatenated commands, return a list of commands'''
+    return re.split(concat, command)
+
+
+def parse_command(command):
+    '''Parse a unix command of the form:
+        command (subcommand) [options] [arguments]
+        Caveats:
+            1. There is no way of knowing whether a command contains
+            subcommands or not so those tokens will be identified as 'words'
+            2. There is no way of knowing whether an option requires an
+            argument or not. The arguments will be identified as 'words'
+        For most cases involving package management this should be enough to
+        identify whether a package was meant to be installed or removed.
+    Convert a given command into a dictionary of the form:
+        {'name': command,
+         'options': [list of option tuples]
+         'words': [remaining words]}
+    An option tuple contains the option flag and the option argument
+    We look ahead to see if the token after the option flag does not match
+    the regex for an option flag and assumes that is the argument
+    The token still remains in the words list because we do not know for sure
+    if it is a command argument or an option argument'''
+    options = re.compile('^-')
+    option_list = []
+    word_list = []
+    command_dict = {}
+    command_tokens = command.split(' ')
+    # first token is the command name
+    command_dict.update({'name': command_tokens.pop(0).strip()})
+    # find options in the rest of the list
+    while command_tokens:
+        if options.match(command_tokens[0]):
+            option_flag = command_tokens.pop(0).strip()
+            # we have to check if this is the end of the command
+            if command_tokens and not options.match(command_tokens[0]):
+                option_arg = command_tokens[0].strip()
+            else:
+                option_arg = ''
+            option_list.append((option_flag, option_arg))
+        else:
+            word_list.append(command_tokens.pop(0).strip())
+    # now we have options and the remainder words
+    command_dict.update({'options': option_list,
+                         'words': word_list})
+    return command_dict
+
+
+def get_git_rev_or_version():
+    '''Assuming we are operating within a git repository, get the SHA
+    of the current commit'''
+    command = ['git', 'rev-parse', 'HEAD']
+    try:
+        output = subprocess.check_output(command)  # nosec
+        if isinstance(output, bytes):
+            output = output.decode('utf-8')
+        ver_type = 'commit'
+
+    except subprocess.CalledProcessError:
+        ver_type = 'package'
+        output = Version
+    return ver_type, output.split('\n').pop(0)
+
+
+def prop_names(obj):
+    '''Given an object, return a generator that will produce the object's
+    property key in its __dict__ representation and it's name'''
+    prop_decorators = r'^__|^_'
+    for key in obj.__dict__.keys():
+        # remove private and protected decorator characters if any
+        priv_name = '_' + obj.__class__.__name__
+        prop_name = re.sub(priv_name, '', key)
+        prop_name = re.sub(prop_decorators, '', prop_name, 1)
+        yield key, prop_name
