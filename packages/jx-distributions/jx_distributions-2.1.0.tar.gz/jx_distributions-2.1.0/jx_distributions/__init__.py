494a858d83de27eb79f744ca6be7a56030394c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:55 +0000

    Add package jx-distributions/2.1.0/jx_distributions-2.1.0.tar.gz

---
+++ packages/jx-distributions/jx_distributions-2.1.0.tar.gz/jx_distributions-2.1.0/jx_distributions/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 10:46:22 2019
+
+@author: Jing
+"""
+
+from .Gaussiandistribution import Gaussian
+
+from .Binomialdistribution import Binomial
---
+++ packages/jx-distributions/jx_distributions-2.1.0.tar.gz/jx_distributions-2.1.0/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 10:48:29 2019
+
+@author: Jing
+"""
+
+
+from setuptools import setup
+
+setup(name = 'jx_distributions',
+     version = '2.1.0',
+     description = "This code in this package uses the concept of Object-Oriented Programming to \
+     carry out some simple calculations related to Gaussian and Bionomial distributions. \
+     This is a course project from the Udacity Data Scientist Nanodegree program. ",
+     packages = ['jx_distributions'],
+     author = 'Jing Xie',
+     author_email = 'jing.xie@usask.ca',
+     zip_safe=False)
