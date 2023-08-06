231efb7e6dce864f2c94a145b99477a61790cc4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:53 +0000

    Add package tinyconf/0.1.1/tinyconf-0.1.1-py3-none-any.whl

---
+++ packages/tinyconf/tinyconf-0.1.1-py3-none-any.whl/tinyconf/deserializers.py
@@ -0,0 +1,73 @@
+import typing
+from io import IOBase
+
+import os
+from configparser import ConfigParser
+
+from tinyconf.fields import Field
+
+class Deserializer():
+    """Base class for deserializers
+
+    """
+    def _deserialize(self, data: dict):
+        for attrib in dir(self):
+            d = getattr(self, attrib)
+            if isinstance(d, Field):
+                # Get the attribute from the data
+                v: typing.Optional[str] = data.get(d.name or attrib, None)
+
+                d.value = v
+
+                # Check the value is valid
+                d.validate()
+
+                # Finally, rebind to the value of the field
+                self.__dict__[attrib] = d.value
+
+class IniDeserializer(Deserializer):
+    """Class representing a deserializer for an INI file.
+    
+    Parameters
+    ----------
+        *args:
+            All unnamed arguments are passed to the underlying :class:`ConfigParser`
+        filename: Optional[:class:`str`]
+            Load a file object by name as a config file
+        file: Optional[:class:`IOBase`]
+            Load a file object as a config file. Please note- the file will not be
+            automatically closed.
+        string: Optional[:class:`str`]
+            Load a string as a config file
+        section: Optional[:class:`str`]
+            Which section should be loaded. Default is ``"DEFAULT"``
+        **kwargs:
+            All other named arguments are passed to the underlying :class:`ConfigParser`
+
+    """
+
+    def __init__(self, *args,
+        file: typing.Optional[IOBase]=None,
+        filename: typing.Optional[str]=None,
+        string: str='',
+        section='DEFAULT',
+        **kwargs):
+
+        cp = ConfigParser(*args, **kwargs)
+        if file is not None:
+            cp.read_file(file)
+        elif filename is not None:
+            with open(filename, 'r') as f:
+                cp.read_file(f)
+        else:
+            cp.read_string(string)
+
+        data: dict = dict(cp[section])
+        self._deserialize(data)
+
+class EnvDeserializer(Deserializer):
+    """Deserializes from the operating system environment variables
+
+    """
+    def __init__(self, *args, **kwargs):
+        self._deserialize(dict(os.environ))
