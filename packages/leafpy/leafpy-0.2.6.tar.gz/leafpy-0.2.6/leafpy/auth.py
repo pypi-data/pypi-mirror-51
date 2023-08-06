a8f9ec66d8156ededab15d0216db915c7ac7c763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:29 +0000

    Add package leafpy/0.2.6/leafpy-0.2.6.tar.gz

---
+++ packages/leafpy/leafpy-0.2.6.tar.gz/leafpy-0.2.6/leafpy/auth.py
@@ -0,0 +1,38 @@
+"""
+Logging in basically means getting the custom_sessionid and VIN, which are used to make 
+every subsequent request.
+"""
+
+from Crypto.Cipher import Blowfish
+import requests
+import base64
+
+def login(username, password, region_code='NNA', initial_app_strings='9s5rfKVuMrT03RtzajWNcA'):
+	baseprm = b'88dSp7wWnV3bvv9Z88zEwg'
+	c1  = Blowfish.new(baseprm, Blowfish.MODE_ECB)
+	packingLength = 8 - len(password) % 8
+	packedPassword = password + chr(packingLength) * packingLength
+	encryptedPassword = c1.encrypt(packedPassword.encode('latin-1'))
+	encodedPassword = base64.standard_b64encode(encryptedPassword)
+
+	url = "https://gdcportalgw.its-mo.com/api_v190426_NE/gdc/UserLoginRequest.php"
+	data = {
+		"RegionCode": region_code,
+		"UserId": username,
+		"initial_app_str": initial_app_strings,
+		"Password": encodedPassword,
+	}
+	headers = {'User-Agent': 'Mozilla/5.0'}
+
+	r = requests.post(url,data=data, headers=headers)
+	r.raise_for_status()
+	if not r.json()['status'] == 200:
+		raise Exception('Cannot login.  Probably username & password are wrong. ' + r.text)
+
+	if region_code == 'NE':
+		custom_sessionid = r.json()['vehicleInfo'][0]['custom_sessionid']
+	else:
+		custom_sessionid = r.json()['VehicleInfoList']['vehicleInfo'][0]['custom_sessionid']
+	VIN = r.json()['CustomerInfo']['VehicleInfo']['VIN']
+
+	return custom_sessionid, VIN
