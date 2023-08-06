b4d40d4b97576c911251773bff90b37c8f6d1987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:44 +0000

    Add package quanter/0.2/quanter-0.2.tar.gz

---
+++ packages/quanter/quanter-0.2.tar.gz/quanter-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="quanter",
+      version="0.2",
+      description="add function",
+      url="",
+      author="hugo",
+      author_email="",
+      license="MIT",
+      packages=["quanter"],
+      zip_safe=False)
