f03f3bfb9a9a5654d7e0a90d93c0042dafcb8de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:20 +0000

    Add package rknn-test/0.0.1/rknn-test-0.0.1.tar.gz

---
+++ packages/rknn-test/rknn-test-0.0.1.tar.gz/rknn-test-0.0.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+	name="rknn-test",
+	version="0.0.1",
+	author="oliver peng",
+	author_email="oliver.peng@rock-chips.com",
+	description="for test",
+	long_description=open("README").read(),
+	license="MIT",
+	url="https://github.com/silyman1/microblog",
+	packages=['rknn'],
+	install_requires=[
+		"lxml"
+		],
+	classifiers=[
+		"Environment :: Web Environment",
+		"Intended Audience :: Developers",
+		"Operating System :: OS Independent",
+		"Topic :: Text Processing :: Indexing",
+		"Topic :: Utilities",
+		"Topic :: Internet",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+                "Programming Language :: Python :: 3.6",
+                "Programming Language :: Python :: 3.7",
+                "Programming Language :: Python :: 3.5",
+                "Programming Language :: Python :: 3",
+	],
+)
