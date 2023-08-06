8bfae2d7e9fdd2bf2b27c132bc452705b5bcb6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package 1OS/0.1/1OS-0.1.tar.gz

---
+++ packages/1OS/1OS-0.1.tar.gz/1OS-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name			=	'1OS',
+      version		=	'0.1',
+      description	=	'1OS',
+      url			=	'https://github.com/1deos/1OS',
+      author       	=	'Drew DeSantis',
+      author_email	=	'atd@deos.org',
+      license		=	'OpenBSV',
+      packages		=	['1OS'],
+      zip_safe		=	False)
