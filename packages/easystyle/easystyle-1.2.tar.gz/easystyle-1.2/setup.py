76dbd7e5bda126cd8ec144fcad92bcf98aa4e2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:21 +0000

    Add package easystyle/1.2/easystyle-1.2.tar.gz

---
+++ packages/easystyle/easystyle-1.2.tar.gz/easystyle-1.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easystyle",
+    version="1.2",
+    author="Ethan Robbins",
+    author_email="ethan.d.robbins@gmail.com",
+    description="A package to allow for basic visuals with pandas dataframes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ethandrobbins/easystyle",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
