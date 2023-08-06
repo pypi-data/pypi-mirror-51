b6db894c6ac04558ea8bcb0c3adaaae63aa82614
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package zhlite/1.6.2/zhlite-1.6.2-py3-none-any.whl

---
+++ packages/zhlite/zhlite-1.6.2-py3-none-any.whl/zhlite/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from zhlite.zhlite import *
+
+__author__ = "zhangbo"
+__email__ = "deplives@deplives.com"
+
+__license__ = "MIT"
+
+__version__ = "1.6.2"
+__date__ = "2019-08-27"
+__release__ = " `User` `Question` `Answer` `Article` 新增  `__eq__` 和 `__hash__` 方法"
+
+
+class Version(object):
+
+    def __init__(self):
+        self.info = {
+            "version": __version__,
+            "date": __date__,
+            "release": __release__
+        }
+
+    def __str__(self):
+        return " ".join([f"{k}: {v}" for k, v in self.info.items()])
+
+
+version = Version()
+
+__all__ = ["Auth", "User", "Question", "Answer", "Article"]
