c7b8484d8cd47bad34193f5b8027c01181bd01fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:20 +0000

    Add package ndl-api/0.3.3/ndl-api-0.3.3.tar.gz

---
+++ packages/ndl-api/ndl-api-0.3.3.tar.gz/ndl-api-0.3.3/ndlapi/api/recognition/recognition.py
@@ -0,0 +1,45 @@
+"""
+NeurodataLab LLC 12.04.2019
+Created by Andrey Belyaev
+"""
+import grpc
+import json
+import ndlapi.api.recognition.recognition_task as CTask
+
+
+class IRecognition:
+
+    def __init__(self, auth):
+        ssl_cred = grpc.ssl_channel_credentials(auth.ssl_credentials().ca(),
+                                                auth.ssl_credentials().key(),
+                                                auth.ssl_credentials().cert())
+
+        token_cred = grpc.access_token_call_credentials(auth.token())
+
+        channel_cred = grpc.composite_channel_credentials(ssl_cred, token_cred)
+        self.channel = grpc.secure_channel(auth.host(), channel_cred)
+
+        self.stub = None
+
+    def on_image(self, image):
+        result = self.stub.process_image(image.request())
+        return json.loads(result.data.decode()), result.status, result.message
+
+    def on_video(self, video):
+        task_info = self.stub.process_video(video.request())
+        return CTask.Task(task_info.id, self)
+
+    def progress(self, task_info):
+        return self.stub.process_progress(task_info.request())
+
+    def result(self, task_info):
+        return self.stub.process_result(task_info.request())
+
+    def on_stream(self, stream):
+        out_iterator = self.stub.process_stream(stream)
+        stream.output(out_iterator)
+        return stream
+
+    @staticmethod
+    def postprocess_result(result):
+        return result
