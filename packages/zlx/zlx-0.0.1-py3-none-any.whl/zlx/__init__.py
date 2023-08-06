bdb60c65bc8667ab1d5329dd8860b436db06535a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:35 +0000

    Add package zlx/0.0.1/zlx-0.0.1-py3-none-any.whl

---
+++ packages/zlx/zlx-0.0.1-py3-none-any.whl/zlx/__init__.py
@@ -0,0 +1,19 @@
+import sys
+
+VER_MAJOR = 0
+VER_MINOR = 0
+BUILD = 1
+
+def sfmt (fmt, *a, **b):
+    return fmt.format(*a, **b)
+
+
+def omsg (fmt, *a, **b):
+    sys.stdout.write(fmt.format(*a, **b) + '\n')
+
+def emsg (fmt, *a, **b):
+    sys.stdout.write(fmt.format(*a, **b) + '\n')
+
+def err (fmt, *a, **b):
+    raise RuntimeError(sfmt(fmt, *a, **b))
+
