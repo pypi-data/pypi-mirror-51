a477f84624e2a8ec8bac7da335ebb647a27a85c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:01 +0000

    Add package string-color/0.1.2/string_color-0.1.2-py3-none-any.whl

---
+++ packages/string-color/string_color-0.1.2-py3-none-any.whl/stringcolor/test.py
@@ -0,0 +1,6 @@
+from stringcolor import cs
+print(cs("warning!", "yellow", "#ff0000"))
+print(cs("hello world", "red"))
+print(cs("here we go", "orchid"))
+print(cs("away to space!", "DeepPink3"))
+print(cs("final fantasy", "#ffff87"))
