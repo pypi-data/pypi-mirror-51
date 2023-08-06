38f0c1a5f6847b2a04b0e8efcd50ca8e7b63f02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:35 +0000

    Add package Group-Buy-Organizer/1.0.0/Group%20Buy%20Organizer-1.0.0.tar.gz

---
+++ packages/Group-Buy-Organizer/Group%20Buy%20Organizer-1.0.0.tar.gz/Group Buy Organizer-1.0.0/groupbuyorganizer/general/forms.py
@@ -0,0 +1,55 @@
+from flask_login import current_user
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+
+from groupbuyorganizer.admin.models import User
+
+
+class RegistrationForm(FlaskForm):
+    '''This field is used to register new accounts'''
+
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    '''This form is used to log into the web app.'''
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
+
+
+class UserOptionsForm(FlaskForm):
+    '''This field is used to register new accounts'''
+
+    username = StringField('Username (displayed to other users)', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email (used for logging in)', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            print(f'{email.data} {current_user.username}')
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
