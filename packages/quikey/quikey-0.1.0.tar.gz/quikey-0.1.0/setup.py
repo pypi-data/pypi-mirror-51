720b3b6afa9ccf306c284eb15663fbf1a6d6560f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:32 +0000

    Add package quikey/0.1.0/quikey-0.1.0.tar.gz

---
+++ packages/quikey/quikey-0.1.0.tar.gz/quikey-0.1.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+VERSION='0.1.0'
+
+setup(
+    name='quikey',
+    version=VERSION,
+    packages=find_packages(),
+    author='bostrt',
+    entry_points = {
+        'console_scripts': [
+            'qk=quikey.quikey:cli',
+            'quikey-daemon=quikey.qkdaemon:cli'
+            ]
+    },
+    description='A keyboard macro tool.',
+    license='',
+    url='https://github.com/bostrt/quikey',
+    install_requires=[
+        'click', 
+        'colored',
+        'python-daemon',
+        'inotify-simple',
+        'tinydb',
+        'pynput',
+        'terminaltables',
+        'xdg',
+        'humanize'
+        ]
+)
