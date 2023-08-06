21baeda9f50f841c884532a1f7bbcdbd09a4e8f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:08 +0000

    Add package SG-funniest/0.1/SG-funniest-0.1.tar.gz

---
+++ packages/SG-funniest/SG-funniest-0.1.tar.gz/SG-funniest-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='SG-funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      long_description='Really, the funniest around.',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['SG-funniest'],
+      install_requires=[
+          'markdown',
+      ],
+      include_package_data=True,
+      zip_safe=False)
