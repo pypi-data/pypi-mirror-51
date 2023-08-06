1417b8203b99c5fb6beb696d5e99346b9c755a13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:41 +0000

    Add package typed-tree/1.0.3/typed-tree-1.0.3.tar.gz

---
+++ packages/typed-tree/typed-tree-1.0.3.tar.gz/typed-tree-1.0.3/typedtree/traversalmode.py
@@ -0,0 +1,13 @@
+"""
+TraversalMode enumeration.
+
+May 01, 2019
+Brett Alistair Kromkamp (brett.kromkamp@gmail.com)
+"""
+
+from enum import Enum
+
+
+class TraversalMode(Enum):
+    DEPTH = 1
+    BREADTH = 2
