1f14d341526ba633577b8d3c18073a73db08ee21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:29 +0000

    Add package vincepdf/1.2/vincepdf-1.2-py3-none-any.whl

---
+++ packages/vincepdf/vincepdf-1.2-py3-none-any.whl/vincepdf/pdf2text.py
@@ -0,0 +1,13 @@
+""" This module converts a pdf to text """
+
+def convert():
+    """ 
+    This module converts a pdf to text 
+    
+    Parameters:
+    path (str): The path to a PDF File.
+    
+    Returns:
+    str: the content of the PDF file as text.
+    """
+    print("print2text")
