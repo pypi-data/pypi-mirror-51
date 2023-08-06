7962a9713e76029e6d6c54368828dda7066e9414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:23 +0000

    Add package tuyaha/0.0.4/tuyaha-0.0.4-py3-none-any.whl

---
+++ packages/tuyaha/tuyaha-0.0.4-py3-none-any.whl/tuyaha/devices/lock.py
@@ -0,0 +1,13 @@
+from tuyaha.devices.base import TuyaDevice
+
+
+class TuyaLock(TuyaDevice):
+
+    def state(self):
+        state = self.data.get('state')
+        if state == 'true':
+            return True
+        elif state == 'false':
+            return False
+        else:
+            return None
