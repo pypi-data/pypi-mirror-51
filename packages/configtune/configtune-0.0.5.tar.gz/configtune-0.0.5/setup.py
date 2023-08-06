0ad287a6012a7c7d396c31d45479632b816e323e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:50 +0000

    Add package configtune/0.0.5/configtune-0.0.5.tar.gz

---
+++ packages/configtune/configtune-0.0.5.tar.gz/configtune-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="configtune",
+	version="0.0.5",
+	description="A package to tune parameters with config files: designed with machine learning hyperparameter tuning in mind.",
+	long_description=None,
+	url= "https://github.com/orionw/configtune",
+	author="Orion Weller and Brandon Schoenfeld",
+	author_email="orionw@byu.edu",
+	license="MIT",
+	classifiers=[
+		"Programming Language :: Python :: 3.5",
+	],
+    download_url="https://github.com/orionw/configtune/archive/v0.0.5.tar.gz",
+	keywords="tuning machinelearning genetic hyperparameters",
+	packages=find_packages(),
+	install_requires=["deap", "numpy", "pandas", "scikit-optimize"],
+	python_requires="~=3.5",
+)
