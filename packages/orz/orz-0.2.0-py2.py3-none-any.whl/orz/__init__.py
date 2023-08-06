7664805a3d9f032852db804cde34ce17d877b5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:01 +0000

    Add package orz/0.2.0/orz-0.2.0-py2.py3-none-any.whl

---
+++ packages/orz/orz-0.2.0-py2.py3-none-any.whl/orz/__init__.py
@@ -0,0 +1,11 @@
+from .result import Result, Ok, Err, ensure, catch, first_ok
+from .exceptions import CheckError
+
+__all__ = [
+    'Result',
+    'Ok',
+    'Err',
+    'ensure'
+    'catch',
+    'first_ok'
+]
