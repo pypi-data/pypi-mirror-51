6dd897de4b33fbdffda816e0edaa87e517ec840f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:14 +0000

    Add package python-redis/0.2.2/python_redis-0.2.2.tar.gz

---
+++ packages/python-redis/python_redis-0.2.2.tar.gz/python_redis-0.2.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='python_redis',
+    version='0.2.2',
+    description='Redis Client',
+    long_description="""
+Redis Client implementation for Python 3.
+
+Copyright (c) 2016, Stephan Schultchen.
+
+License: MIT (see LICENSE for details)
+    """,
+    packages=['pyredis'],
+    url='https://github.com/schlitzered/pyredis',
+    license='MIT',
+    author='schlitzer',
+    author_email='stephan.schultchen@gmail.com',
+    test_suite='test',
+    platforms='posix',
+    classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3'
+    ],
+    setup_requires=[
+        'crc16'
+    ],
+    install_requires=[
+        'crc16'
+    ],
+    keywords=[
+        'redis'
+    ]
+)
