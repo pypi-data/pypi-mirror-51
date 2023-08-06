9282df3bc8ef4fb3a7177cfebe44ceb7fc2e1480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:18 +0000

    Add package qh-probability/1.0/qh_probability-1.0.tar.gz

---
+++ packages/qh-probability/qh_probability-1.0.tar.gz/qh_probability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='qh_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['qh_probability'],
+      author = 'Qin Hu',
+      author_email = 'erinqhu08@gmail.com',
+      zip_safe=False)
