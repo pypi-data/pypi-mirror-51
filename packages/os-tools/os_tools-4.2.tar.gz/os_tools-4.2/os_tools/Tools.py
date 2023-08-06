184ca941c141cbbd6e48d6f1a319991eceaab1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:01 +0000

    Add package os-tools/4.2/os_tools-4.2.tar.gz

---
+++ packages/os-tools/os_tools-4.2.tar.gz/os_tools-4.2/os_tools/Tools.py
@@ -0,0 +1,22 @@
+###########################################################################
+#
+# this module meant to provide general handling to python files
+#
+###########################################################################
+
+
+# will print an array
+def print_arr(arr, divider='\n'):
+    print(divider.join(arr))
+
+
+# will ask the user for an input
+def ask_for_input(question):
+    return input(question + '\n')
+
+# will copy to the clipboard
+def copyToClipboard(text):
+    import pyperclip
+    pyperclip.copy(text)
+    pyperclip.paste()
+
