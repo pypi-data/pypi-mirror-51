75c132c5ddd9286b99574bb74b28a0bc1f49bef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:45 +0000

    Add package pupil-pthreads-win/0.dev2/pupil-pthreads-win-0.dev2.tar.gz

---
+++ packages/pupil-pthreads-win/pupil-pthreads-win-0.dev2.tar.gz/pupil-pthreads-win-0.dev2/README.md
@@ -0,0 +1,13 @@
+# pupil-pthreads-win
+
+This is a precompiled version of pthreads for windows wrapped in a python package.
+We use it as dependency for various modules for the pupil core software.
+
+## Installation
+
+Note that this library only provides pthreads for windows and thus you are not recommended to use it  on another operating system. When trying to use it on another operating system, you might receive a warning.
+
+Install from PyPI:
+```bash
+pip install pupil-pthreads-win
+```
---
+++ packages/pupil-pthreads-win/pupil-pthreads-win-0.dev2.tar.gz/pupil-pthreads-win-0.dev2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from pathlib import Path
+
+with open("README.md") as f:
+    readme_text = f.read()
+
+package_dir = "src"
+package = "pupil_pthreads_win"
+
+package_data = [
+    str(match.resolve())
+    for match in (Path() / package_dir / package / "data").rglob("*")
+    if match.is_file()
+]
+
+setup(
+    name="pupil-pthreads-win",
+    description="A precompiled version of pthreads-win.",
+    long_description=readme_text,
+    long_description_content_type="text/markdown",
+    version="0.dev2",
+    url="https://github.com/pupil-labs/pupil-pthreads-win",
+    license="MIT License",
+    author="Pupil Labs GmbH",
+    author_email="pypi@pupil-labs.com",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=[package],
+    package_dir={"": package_dir},
+    package_data={package: package_data},
+)
