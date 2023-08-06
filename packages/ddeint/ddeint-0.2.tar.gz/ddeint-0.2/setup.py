6fdc194e1b701cf0b432942d4c271a1ae9943827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:35 +0000

    Add package ddeint/0.2/ddeint-0.2.tar.gz

---
+++ packages/ddeint/ddeint-0.2.tar.gz/ddeint-0.2/setup.py
@@ -0,0 +1,20 @@
+import ez_setup
+
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+exec(open("ddeint/version.py").read())  # loads __version__
+
+setup(
+    name="ddeint",
+    version=__version__,
+    author="Zulko",
+    description="Scipy-based Delay Differential Equations solver",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="see LICENSE.txt",
+    keywords="delay differential equation DDE",
+    packages=find_packages(exclude="docs"),
+    install_requires=["numpy", "scipy"],
+)
