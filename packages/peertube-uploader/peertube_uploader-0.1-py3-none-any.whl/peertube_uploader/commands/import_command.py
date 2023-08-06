6e3135a42dcab7ba2222e67a5abd8bdf6275145b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:44 +0000

    Add package peertube-uploader/0.1/peertube_uploader-0.1-py3-none-any.whl

---
+++ packages/peertube-uploader/peertube_uploader-0.1-py3-none-any.whl/peertube_uploader/commands/import_command.py
@@ -0,0 +1,55 @@
+import sys
+from enum import auto
+
+from peertube_uploader.commands.upload_base import UploadBaseCommand
+from peertube_uploader.utils import StringListEnum
+
+
+class ImportTypes(StringListEnum):
+    HTTP = auto()
+    MAGNET = auto()
+    TORRENT_FILE = auto()
+
+
+class ImportCommand(UploadBaseCommand):
+    """
+    Trigger an import of a URI
+    """
+    API_ENDPOINT = "/api/v1/videos/imports"
+
+    def get_parser(self, prog_name):
+        parser = super(ImportCommand, self).get_parser(prog_name)
+        parser.add_argument(
+            '--type',
+            choices=list(ImportTypes),
+            default=ImportTypes.HTTP,
+            type=ImportTypes.get,
+            help='What type of import it will be. Http by default'
+        )
+        parser.add_argument('uri', help='magnet or http URI')
+        return parser
+
+    def _build_upload_data(self, parsed_args):
+        super(ImportCommand, self)._build_upload_data(parsed_args)
+
+        if parsed_args.name:
+            self.upload_data["name"] = parsed_args.name
+        if "name" not in self.upload_data:
+            raise ValueError("Use the --name arg or pass the name in the JSON of --json")
+
+    def _build_request_kwargs(self, parsed_args):
+        uri = parsed_args.uri
+
+        if parsed_args.type == ImportTypes.HTTP:
+            self.upload_data["targetUrl"] = uri
+        elif parsed_args.type == ImportTypes.MAGNET:
+            self.upload_data["magnetUri"] = uri
+        elif parsed_args.type == ImportTypes.TORRENT_FILE:
+            raise NotImplementedError("Cannot import torrent files yet")
+        else:
+            raise ValueError("Unknown import type")
+
+        return {
+            "data": self.upload_data
+        }
+
---
+++ packages/peertube-uploader/peertube_uploader-0.1-py3-none-any.whl/peertube_uploader/requests.py
@@ -0,0 +1,13 @@
+from urllib.parse import urljoin
+
+import requests
+
+
+class RelativeSession(requests.Session):
+    def __init__(self, base_url):
+        super(RelativeSession, self).__init__()
+        self.__base_url = base_url
+
+    def request(self, method, url, **kwargs):
+        url = urljoin(self.__base_url, url)
+        return super(RelativeSession, self).request(method, url, **kwargs)
