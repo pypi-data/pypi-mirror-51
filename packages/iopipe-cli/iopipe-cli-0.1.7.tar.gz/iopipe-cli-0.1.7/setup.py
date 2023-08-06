ffa673daa0c9f88b80550544fbdd111a698209d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:35 +0000

    Add package iopipe-cli/0.1.7/iopipe-cli-0.1.7.tar.gz

---
+++ packages/iopipe-cli/iopipe-cli-0.1.7.tar.gz/iopipe-cli-0.1.7/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+import os
+
+try:
+    from pypandoc import convert
+
+    README = convert("README.md", "rst")
+except (ImportError, OSError):
+    README = open(os.path.join(os.path.dirname(__file__), "README.md"), "r").read()
+
+setup(
+    name='iopipe-cli',
+    version='0.1.7',
+    python_requires='>=3.3',
+    description="cli utility for managing and instrumenting serverless applications",
+    long_description=README,
+    author="IOpipe",
+    author_email="dev@iopipe.com",
+    url="https://github.com/iopipe/iopipe-cli",
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'boto3',
+        'requests',
+        'pyjwt'
+    ],
+    tests_require=[
+        "coverage==5.0a2",
+        "mock",
+        "more-itertools<6.0.0",
+        "pytest==4.1.0",
+        "pytest-benchmark==3.2.0",
+        "pytest-cov==2.6.1",
+        "requests",
+    ],
+    entry_points={
+        'console_scripts': [
+            'iopipe = iopipe_cli.cli:main',
+        ],
+    }
+)
