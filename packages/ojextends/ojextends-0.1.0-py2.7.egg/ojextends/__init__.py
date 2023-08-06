6d0aa411c8be24cce8c8da87a1da4590c55e08a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:17 +0000

    Add package ojextends/0.1.0/ojextends-0.1.0-py2.7.egg

---
+++ packages/ojextends/ojextends-0.1.0-py2.7.egg/ojextends/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from .ojextends import (List, JsonSerializable, objectToDict,objectToStr,objectFromDict,objectFromStr,objectsFromStr,objectsFromList,objectsToList,objectsToStr)
+
+__name__ = 'ojextends'
+
+__version__ = '0.1.0'
+
+__all__ = ['List', 'JsonSerializable', 'objectToDict','objectToStr',
+		'objectFromDict','objectFromStr','objectsFromStr',
+		'objectsFromList','objectsToList','objectsToStr']
