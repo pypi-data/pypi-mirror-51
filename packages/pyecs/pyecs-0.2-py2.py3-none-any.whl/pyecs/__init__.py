23cb7790843e1fb6f711d8cbf3f783f0b944c210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:51 +0000

    Add package pyecs/0.2/pyecs-0.2-py2.py3-none-any.whl

---
+++ packages/pyecs/pyecs-0.2-py2.py3-none-any.whl/pyecs/__init__.py
@@ -0,0 +1,8 @@
+from pyecs.core import (
+    Component,
+    ECSController,
+    preconfigure_system,
+    register_system,
+)  # noqa: F401
+
+__all__ = ("ECSController", "Component", "preconfigure_system", "register_system")
