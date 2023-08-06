52981c9ec0390f129d2ec2c691a494b3dabe64f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:02 +0000

    Add package underthesea/1.1.17a0/underthesea-1.1.17a0.tar.gz

---
+++ packages/underthesea/underthesea-1.1.17a0.tar.gz/underthesea-1.1.17a0/underthesea/sentiment/bank/__init__.py
@@ -0,0 +1,33 @@
+import logging
+import os
+from os.path import dirname
+import sys
+from languageflow.data import Sentence
+from languageflow.models.text_classifier import TextClassifier
+from underthesea.model_fetcher import ModelFetcher, UTSModel
+
+
+FORMAT = '%(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('underthesea')
+
+sys.path.insert(0, dirname(__file__))
+model_path = ModelFetcher.get_model_path(UTSModel.sa_bank)
+classifier = None
+
+
+def sentiment(text):
+    global classifier
+
+    if not classifier:
+        if os.path.exists(model_path):
+            classifier = TextClassifier.load(model_path)
+        else:
+            logger.error(
+                f"Could not load model at {model_path}.\n"
+                f"Download model with \"underthesea download {UTSModel.sa_bank.value}\".")
+            sys.exit(1)
+    sentence = Sentence(text)
+    classifier.predict(sentence)
+    labels = sentence.labels
+    return [label.value for label in labels]
