b7f0f5fa18c770a0336d11cc553163918259b0ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:06 +0000

    Add package napi-py/0.1.4/napi-py-0.1.4.tar.gz

---
+++ packages/napi-py/napi-py-0.1.4.tar.gz/napi-py-0.1.4/test/test_acceptance.py
@@ -0,0 +1,25 @@
+import unittest
+
+from napi import NapiPy
+
+
+class NapiPyAcceptanceTest(unittest.TestCase):
+    def test_should_download_correctly_encoded_subs(self):
+        movie_hash = '25b1087d997bfbf8a4be462255e05a05'
+        napi = NapiPy()
+        src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
+        self.assertEqual(src_enc, "utf-8")
+
+        with open(tmp_file) as subs_file:
+            subs = subs_file.read()
+
+        expected_phrases = ['źródło', 'władzę', 'Dziękuję', 'zgłębiają', 'ZWYCIĘŻĄ!']
+        for expected_phrase in expected_phrases:
+            self.assertIn(expected_phrase, subs)
+
+    def test_should_return_none_when_no_subtitles_for_movie(self):
+        movie_hash = '00000000000000000000000000000000'
+        napi = NapiPy()
+        src_enc, tgt_enc, tmp_file = napi.download_subs(movie_hash)
+        self.assertIsNone(src_enc)
+        self.assertIsNone(tmp_file)
