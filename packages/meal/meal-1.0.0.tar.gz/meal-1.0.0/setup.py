d310e40b505084ad1f471f09694484134111d3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:28 +0000

    Add package meal/1.0.0/meal-1.0.0.tar.gz

---
+++ packages/meal/meal-1.0.0.tar.gz/meal-1.0.0/setup.py
@@ -0,0 +1,10 @@
+# coding:utf-8
+from distutils.core import setup
+
+setup(
+    name='meal',
+    version='1.0.0',
+    author='guoyali',
+    author_email='840431172@qq.com',
+    description='get a meal by random',
+)
