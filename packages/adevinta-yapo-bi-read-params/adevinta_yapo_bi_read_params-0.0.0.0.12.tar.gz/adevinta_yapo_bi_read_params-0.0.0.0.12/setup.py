be58845cedb7c23a5be0c9ee4f53c303a10c2fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:31 +0000

    Add package adevinta-yapo-bi-read-params/0.0.0.0.12/adevinta_yapo_bi_read_params-0.0.0.0.12.tar.gz

---
+++ packages/adevinta-yapo-bi-read-params/adevinta_yapo_bi_read_params-0.0.0.0.12.tar.gz/adevinta_yapo_bi_read_params-0.0.0.0.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='adevinta_yapo_bi_read_params',
+      version='0.0.0.0.12',
+      description='Class develop that enable read input params by command line.',
+      url='https://github.mpi-internal.com/Yapo/bi-insights',
+      author='Yapo BI',
+      author_email='bi.insights@yapo.cl',
+      license='MIT',
+      packages=['adevinta_yapo_bi_read_params'],
+      zip_safe=False)
