2aa79bbaf6858883791463963e6bcbeb462ccd56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:47 +0000

    Add package pyrainbird/0.3.1/pyrainbird-0.3.1.tar.gz

---
+++ packages/pyrainbird/pyrainbird-0.3.1.tar.gz/pyrainbird-0.3.1/setup.py
@@ -0,0 +1,29 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+
+    name='pyrainbird',
+    version='0.3.1',
+    description='Rain Bird Controller',
+
+    install_requires=['pycryptodome', 'requests'],
+
+    packages=['pyrainbird'],
+
+    # The project's main homepage.
+    url='https://github.com/jbarrancos/pyrainbird/',
+
+    # Author details
+    author='J.J.Barrancos',
+    author_email='jordy@fusion-ict.nl',
+
+    license='MIT',
+
+    keywords=['Rain Bird'],
+    classifiers=[],
+    zip_safe=True
+
+)
