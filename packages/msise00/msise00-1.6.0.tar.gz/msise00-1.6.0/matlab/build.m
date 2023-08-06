435d6e454e0dfc3809c801a6e6426904fb1230e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:51 +0000

    Add package msise00/1.6.0/msise00-1.6.0.tar.gz

---
+++ packages/msise00/msise00-1.6.0.tar.gz/msise00-1.6.0/matlab/build.m
@@ -0,0 +1,64 @@
+function build(fc)
+
+if nargin < 1
+  fc = 'gfortran';
+end
+validateattributes(fc, {'char'}, {'vector'})
+
+cwd = fileparts(mfilename('fullpath'));
+srcdir = [cwd, filesep,'..',filesep,'src'];
+builddir = [cwd,filesep,'..', filesep, 'msise00'];
+
+cmd = [fc,' ',...
+       srcdir,filesep,'nrlmsise00_sub.for', ' ', ...
+       srcdir,filesep,'msise00_driver.f90', ' ',...
+       '-o ',builddir,filesep,'msise00_driver'];
+
+disp(cmd)
+runcmd(cmd)
+
+end
+
+
+function build_cmake(srcdir, builddir)
+%% build using CMake
+
+validateattributes(srcdir,{'char'},{'vector'})
+validateattributes(builddir,{'char'},{'vector'})
+
+runcmd('cmake --version')
+
+if ispc
+  wopts = '-G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"';
+else
+  wopts = '';
+end
+
+ccmd = ['cmake ', wopts, ' -S ', srcdir, ' -B ', builddir];
+
+runcmd(ccmd)
+
+runcmd(['cmake --build ',builddir, ' --parallel'])
+
+end
+
+
+function build_meson(srcdir, builddir)
+%% build using Meson + Ninja
+
+validateattributes(srcdir,{'char'},{'vector'})
+validateattributes(builddir,{'char'},{'vector'})
+
+disp('Meson: ')
+runcmd('meson --version')
+
+disp('Ninja: ')
+runcmd('ninja --version')
+
+if ~exist([builddir,filesep,'build.ninja'], 'file')
+  runcmd(['meson setup ',builddir,' ',srcdir])
+end
+
+runcmd(['ninja -C ',builddir])
+
+end
---
+++ packages/msise00/msise00-1.6.0.tar.gz/msise00-1.6.0/matlab/msise00.m
@@ -0,0 +1,47 @@
+function iono = msise00(time, glat, glon, f107a, f107, Ap, altkm)
+%% run MSIS E00
+validateattributes(glat, {'numeric'}, {'scalar'}, mfilename, 'geodetic latitude',2)
+validateattributes(glon, {'numeric'}, {'scalar'}, mfilename, 'geodetic longitude',3)
+validateattributes(f107a, {'numeric'}, {'positive','scalar'},mfilename, '81 day AVERAGE OF F10.7 FLUX (centered on day)',4)
+validateattributes(f107, {'numeric'}, {'positive','scalar'},mfilename, 'DAILY F10.7 FLUX FOR PREVIOUS DAY', 5)
+validateattributes(Ap, {'numeric'}, {'positive','scalar'}, mfilename, 'MAGNETIC INDEX(DAILY)',6)
+validateattributes(altkm, {'numeric'}, {'positive', 'scalar'},mfilename, 'altitude [km]',7)
+%% binary MSISe00
+cwd = fileparts(mfilename('fullpath'));
+exe = [cwd,filesep,'..', filesep, 'msise00', filesep, 'msise00_driver'];
+if ispc, exe = [exe, '.exe']; end
+if ~exist(exe,'file'), build(), end
+
+doy = date2doy(time);
+v = datevec(time);
+hms = num2str(v(4:6));
+iyd = int2str(v(1));
+iyd = [iyd(3:4), num2str(floor(doy),'%03d')];
+
+
+cmd = [exe, ' ', iyd, ' ', hms,...
+       ' ',num2str(glat), ' ', num2str(glon),...
+       ' ',num2str(f107a), ' ', num2str(f107), ' ', num2str(Ap),...
+       ' ',num2str(altkm)];
+[status,dat] = system(cmd);
+if status ~= 0, error(dat), end
+
+
+D = cell2mat(textscan(dat, '%f %f %f %f %f %f %f %f %f', 1, 'ReturnOnError', false));
+
+iono.altkm = altkm;
+iono.nHe = D(1);
+iono.nO = D(2);
+iono.nN2 = D(3);
+iono.nO2 = D(4);
+iono.nAr = D(5);
+iono.nTotal = D(6);
+iono.nH = D(7);
+iono.nN = D(8);
+iono.nOanomalous = D(9);
+
+
+T = cell2mat(textscan(dat, '%f %f', 1, 'HeaderLines', 1, 'ReturnOnError', false));
+iono.Texospheric = T(1);
+iono.Tn = T(2);
+end
