83339659cba3bccded27b2f835a5b38b8ac6b36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:06 +0000

    Add package pychoropleth/0.2.3/pychoropleth-0.2.3.tar.gz

---
+++ packages/pychoropleth/pychoropleth-0.2.3.tar.gz/pychoropleth-0.2.3/README.md
@@ -0,0 +1,12 @@
+# pychoropleth
+A simple library to create choropleth maps from geopandas dataframes using mplleaflet
+
+[Here](https://nbviewer.jupyter.org/github/SebMilardo/pychoropleth/blob/master/tests/Tests.ipynb) is an example of a choropleth map.
+
+# Installation
+```Shell
+pip install pychoropleth
+```
+
+# Status
+Work in Progress
---
+++ packages/pychoropleth/pychoropleth-0.2.3.tar.gz/pychoropleth-0.2.3/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+import setuptools
+
+
+setup(name='pychoropleth',
+    version='0.2.3',
+    description='A simple library to create choropleth maps from geopandas dataframes using mplleaflet',
+    packages=setuptools.find_packages(),
+    author='Sebastiano Milardo',
+    author_email='milardo@mit.edu',
+    license = 'MIT',
+    install_requires = ['mplleaflet','matplotlib','pandas','numpy','geopandas','shapely','osmnx','descartes'],
+    url = 'https://github.com/SebMilardo/pychoropleth',
+    project_urls={
+        'source': 'https://github.com/SebMilardo/pychoropleth/',
+    },
+    zip_safe=False,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers = ['Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 3']
+    )
