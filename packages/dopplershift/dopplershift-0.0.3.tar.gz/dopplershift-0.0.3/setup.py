72e98c59f52ead4d8ebb5acbe7b4c9431090fbc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:01 +0000

    Add package dopplershift/0.0.3/dopplershift-0.0.3.tar.gz

---
+++ packages/dopplershift/dopplershift-0.0.3.tar.gz/dopplershift-0.0.3/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Pythonic SQL
+# Copyright (C) 2019 alvations
+
+from distutils.core import setup
+
+setup(
+    name='dopplershift',
+    version='0.0.3',
+    packages=['dopplershift'],
+    description='Pythonic SQL for mere mortals.',
+    long_description='',
+    install_requires=[
+        "numpy",
+        'psycopg2',
+    ],
+    url = 'https://github.com/alvations/dopplershift',
+    license="MIT"
+)
