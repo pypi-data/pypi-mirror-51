4eb5116195fcc6f85595adad9df9d2e406fdbecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:10 +0000

    Add package python-rdesktop-gui/0.5/python_rdesktop_gui-0.5.tar.gz

---
+++ packages/python-rdesktop-gui/python_rdesktop_gui-0.5.tar.gz/python_rdesktop_gui-0.5/README.md
@@ -0,0 +1,16 @@
+# python_rdesktop_gui
+
+Autor (script): [Spencer McIntyre](https://gist.github.com/zeroSteiner/6179745)
+
+**python_rdesktop_gui** is a basic and simple graphic interface for rdesktop.
+
+## Install
+
+```
+$ sudo pip install python_rdesktop_gui
+```
+
+## Run
+```
+$ python_rdesktop_gui
+```
---
+++ packages/python-rdesktop-gui/python_rdesktop_gui-0.5.tar.gz/python_rdesktop_gui-0.5/setup.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+import sys
+import shutil
+
+shutil.copyfile('python_rdesktop_gui.py', 'python_rdesktop_gui')
+
+setup(
+    name='python_rdesktop_gui',
+    version='0.5',
+    scripts=['python_rdesktop_gui'],
+    platforms='linux',
+    author='Spencer McIntyre',
+    author_email='',
+    maintainer='Jorge Hernández (lesthack)',
+    maintainer_email='lesthack@gmail.com',
+    description='A sample gui for rdesktop',
+    license='GPL3',
+    keywords=['rdesktop', 'rdp', 'remote desktop', 'terminal server'],
+    url='https://github.com/lesthack/python_rdesktop_gui',
+    data_files=[
+        ('/usr/share/applications', ['data/python_rdesktop_gui.desktop']),
+        ('/usr/local/share/icons/hicolor/16x16/apps', ['data/icons/16x16/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/22x22/apps', ['data/icons/22x22/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/24x24/apps', ['data/icons/24x24/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/32x32/apps', ['data/icons/32x32/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/48x48/apps', ['data/icons/48x48/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/64x64/apps', ['data/icons/64x64/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/128x128/apps', ['data/icons/128x128/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/256x256/apps', ['data/icons/256x256/apps/python_rdesktop_gui.png']),
+        ('/usr/local/share/icons/hicolor/scalable/apps', ['data/icons/scalable/apps/python_rdesktop_gui.svg']),
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+)
