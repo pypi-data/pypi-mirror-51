80f367e86f89ccdb8d9be7882be2663a18d40346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:57 +0000

    Add package evol/0.5.1/evol-0.5.1.tar.gz

---
+++ packages/evol/evol-0.5.1.tar.gz/evol-0.5.1/evol/step.py
@@ -0,0 +1,138 @@
+from typing import Callable, Optional, TYPE_CHECKING
+
+from .population import Population
+
+if TYPE_CHECKING:
+    from evol.evolution import Evolution
+
+
+class EvolutionStep:
+
+    def __init__(self, name: Optional[str], **kwargs):
+        self.name = name
+        self.kwargs = kwargs
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name or ''})"
+
+
+class EvaluationStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.evaluate(**self.kwargs)
+
+
+class ApplyStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.apply(**self.kwargs)
+
+
+class CheckpointStep(EvolutionStep):
+
+    def __init__(self, name, every=1, **kwargs):
+        EvolutionStep.__init__(self, name, **kwargs)
+        self.count = 0
+        self.every = every
+
+    def apply(self, population) -> Population:
+        self.count += 1
+        if self.count >= self.every:
+            self.count = 0
+            return population.checkpoint(**self.kwargs)
+        return population
+
+
+class MapStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.map(**self.kwargs)
+
+
+class FilterStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.filter(**self.kwargs)
+
+
+class UpdateStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.update(**self.kwargs)
+
+
+class SurviveStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.survive(**self.kwargs)
+
+
+class BreedStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.breed(**self.kwargs)
+
+
+class MutateStep(EvolutionStep):
+
+    def apply(self, population) -> Population:
+        return population.mutate(**self.kwargs)
+
+
+class LogStep(EvolutionStep):
+    def __init__(self, name, every=1, **kwargs):
+        EvolutionStep.__init__(self, name, **kwargs)
+        self.count = 0
+        self.every = every
+
+    def apply(self, population) -> Population:
+        self.count += 1
+        if self.count >= self.every:
+            self.count = 0
+            return population.log(**self.kwargs)
+        return population
+
+
+class RepeatStep(EvolutionStep):
+
+    def __init__(self, name: str, evolution: 'Evolution', n: int,
+                 grouping_function: Optional[Callable] = None, **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.evolution = evolution
+        self.n = n
+        self.grouping_function = grouping_function
+
+    def apply(self, population: Population) -> Population:
+        if self.grouping_function is None:
+            if len(self.kwargs) > 0:
+                raise ValueError(f'Unexpected argument(s) for non-grouped repeat step: {self.kwargs}')
+            return population.evolve(evolution=self.evolution, n=self.n)
+        else:
+            return self._apply_grouped(population=population)
+
+    def _apply_grouped(self, population: Population) -> Population:
+        groups = population.group(grouping_function=self.grouping_function, **self.kwargs)
+        if population.pool:
+            results = population.pool.map(lambda group: group.evolve(evolution=self.evolution, n=self.n), groups)
+        else:
+            results = [group.evolve(evolution=self.evolution, n=self.n) for group in groups]
+        return Population.combine(*results, intended_size=population.intended_size, pool=population.pool)
+
+    def __repr__(self):
+        result = f"{self.__class__.__name__}({self.name or ''}) with evolution ({self.n}x):\n  "
+        result += repr(self.evolution).replace('\n', '\n  ')
+        return result
+
+
+class CallbackStep(EvolutionStep):
+    def __init__(self, name, every: int = 1, **kwargs):
+        EvolutionStep.__init__(self, name, **kwargs)
+        self.count = 0
+        self.every = every
+
+    def apply(self, population) -> Population:
+        self.count += 1
+        if self.count >= self.every:
+            self.count = 0
+            return population.callback(**self.kwargs)
+        return population
---
+++ packages/evol/evol-0.5.1.tar.gz/evol-0.5.1/setup.py
@@ -0,0 +1,61 @@
+import codecs
+from os import path
+from re import search, M
+from setuptools import setup, find_packages
+
+
+def load_readme():
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
+
+here = path.abspath(path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='evol',
+    version=find_version('evol', '__init__.py'),
+    description='A Grammar for Evolutionary Algorithms and Heuristics',
+    long_description=load_readme(),
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    author=['Vincent D. Warmerdam', 'Rogier van der Geer'],
+    author_email='vincentwarmerdam@gmail.com',
+    url='https://github.com/godatadriven/evol',
+    packages=find_packages(),
+    keywords=['genetic', 'algorithms', 'heuristics'],
+    python_requires='>=3.6',
+    tests_require=[
+        "pytest==3.3.1",
+    ],
+    extras_require={
+        "dev": ["Sphinx>=2.0.0"]
+    },
+    setup_requires=[
+        "flake8==3.6.0", "pytest-runner"
+    ],
+    install_requires=[
+        "multiprocess>=0.70.6.1"
+    ],
+    classifiers=['Intended Audience :: Developers',
+                 'Intended Audience :: Science/Research',
+                 'Programming Language :: Python :: 3.6',
+                 'Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: MIT License',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence']
+)
