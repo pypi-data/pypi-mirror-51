8e1a184be2d5cd755a53768db99700a274264db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:32 +0000

    Add package jupyda/0.0.6/jupyda-0.0.6.tar.gz

---
+++ packages/jupyda/jupyda-0.0.6.tar.gz/jupyda-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jupyda",
+    version="0.0.6",
+    author="Sourabh Maity",
+    author_email="no-one@no-where.com",
+    description="IPython magic extension to run CUDA C code on jupyter notebooks.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sourabhXIII/jupyda",
+    py_modules=['jupyda', 'src.magic_jupyda'],
+    # packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
