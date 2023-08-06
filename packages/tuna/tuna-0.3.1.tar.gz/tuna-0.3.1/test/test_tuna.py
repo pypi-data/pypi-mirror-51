74217de848d67e5a2da3e55955912c49a4c19e8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:12 +0000

    Add package tuna/0.3.1/tuna-0.3.1.tar.gz

---
+++ packages/tuna/tuna-0.3.1.tar.gz/tuna-0.3.1/test/test_tuna.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import time
+
+import tuna  # noqa
+
+
+def test_tuna():
+    this_dir = os.path.dirname(__file__)
+    filename = os.path.join(this_dir, "foo.prof")
+    cmd = ["tuna", filename, "--no-browser"]
+
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    # give server time to start up
+    time.sleep(3)
+    p.terminate()
+    return
+
+
+if __name__ == "__main__":
+    test_tuna()
---
+++ packages/tuna/tuna-0.3.1.tar.gz/tuna-0.3.1/tuna/__init__.py
@@ -0,0 +1,17 @@
+from . import cli
+from .__about__ import (
+    __author__,
+    __author_email__,
+    __license__,
+    __status__,
+    __version__,
+)
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__license__",
+    "__version__",
+    "__status__",
+    "cli",
+]
