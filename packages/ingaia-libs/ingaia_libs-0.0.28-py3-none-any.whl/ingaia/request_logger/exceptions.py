ba82f6677a7af83f8d13072d1d9ce402fbf70db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:19 +0000

    Add package ingaia-libs/0.0.28/ingaia_libs-0.0.28-py3-none-any.whl

---
+++ packages/ingaia-libs/ingaia_libs-0.0.28-py3-none-any.whl/ingaia/request_logger/exceptions.py
@@ -0,0 +1,8 @@
+from ingaia.commons.exceptions import GenericException
+
+
+class IngaiaRLTokenNotDefinedException(GenericException):
+    message = 'Define the environment variable "INGAIA_RL_TOKEN" in "app.yaml".'
+
+
+print(IngaiaRLTokenNotDefinedException())
