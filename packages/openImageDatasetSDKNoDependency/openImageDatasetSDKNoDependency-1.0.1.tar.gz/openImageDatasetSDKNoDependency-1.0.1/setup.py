ab35bb922e2cb2ef9353f4a9731d158d8becb34f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:22 +0000

    Add package openImageDatasetSDKNoDependency/1.0.1/openImageDatasetSDKNoDependency-1.0.1.tar.gz

---
+++ packages/openImageDatasetSDKNoDependency/openImageDatasetSDKNoDependency-1.0.1.tar.gz/openImageDatasetSDKNoDependency-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+ 
+setup(
+    name='openImageDatasetSDKNoDependency',
+    version='1.0.1',
+    description='Python SDK for the Open Image Dataset.',
+    packages=find_packages(),
+    author='zhenwan,t-zhiwan,t-tozho',
+    author_email='zhenwan@microsoft.com,t-zhiwan@microsoft.com,t-tozho@microsoft.com',
+    url='https://dev.azure.com/zhenwan/zhenwan_default/_git/AMLProjects',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
