1ee9d989ea312deea1622f6150b6d8e04abe2908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:44 +0000

    Add package ray/0.7.4/ray-0.7.4-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/ray/ray-0.7.4-cp37-cp37m-manylinux1_x86_64.whl/ray-0.7.4.data/purelib/ray/modin/modin/engines/python/pandas_on_python/frame/axis_partition.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import pandas
+
+from modin.engines.base.frame.axis_partition import PandasFrameAxisPartition
+from .partition import PandasOnPythonFramePartition
+
+
+class PandasOnPythonFrameAxisPartition(PandasFrameAxisPartition):
+    def __init__(self, list_of_blocks):
+        # Unwrap from BaseFramePartition object for ease of use
+        self.list_of_blocks = [obj.data for obj in list_of_blocks]
+
+    partition_type = PandasOnPythonFramePartition
+    instance_type = pandas.DataFrame
+
+
+class PandasOnPythonFrameColumnPartition(PandasOnPythonFrameAxisPartition):
+    """The column partition implementation for Ray. All of the implementation
+        for this class is in the parent class, and this class defines the axis
+        to perform the computation over.
+    """
+
+    axis = 0
+
+
+class PandasOnPythonFrameRowPartition(PandasOnPythonFrameAxisPartition):
+    """The row partition implementation for Ray. All of the implementation
+        for this class is in the parent class, and this class defines the axis
+        to perform the computation over.
+    """
+
+    axis = 1
