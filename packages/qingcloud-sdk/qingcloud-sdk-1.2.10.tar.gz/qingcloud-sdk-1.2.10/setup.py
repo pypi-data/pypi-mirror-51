c4f7486b87bd6afde65ae432cd6d251cd6317e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:03 +0000

    Add package qingcloud-sdk/1.2.10/qingcloud-sdk-1.2.10.tar.gz

---
+++ packages/qingcloud-sdk/qingcloud-sdk-1.2.10.tar.gz/qingcloud-sdk-1.2.10/setup.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+
+import sys
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.version_info < (2, 6):
+    error = 'ERROR: qingcloud-sdk requires Python Version 2.6 or above.'
+    print >> sys.stderr, error
+    sys.exit(1)
+
+
+setup(
+    name='qingcloud-sdk',
+    version='1.2.10',
+    description='Software Development Kit for QingCloud.',
+    long_description=open('README.rst', 'rb').read().decode('utf-8'),
+    keywords='qingcloud iaas qingstor sdk',
+    author='Yunify Team',
+    author_email='simon@yunify.com',
+    url='https://docs.qingcloud.com/sdk/',
+    packages=['qingcloud', 'qingcloud.conn', 'qingcloud.iaas', 'qingcloud.iaas.actions',
+              'qingcloud.misc', 'qingcloud.qingstor'],
+    package_dir={'qingcloud-sdk': 'qingcloud'},
+    namespace_packages=['qingcloud'],
+    include_package_data=True,
+    install_requires=['future']
+)
