1726d4daa436bb7d140e3812e53907103fa0d480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:13 +0000

    Add package synapse/0.1.24/synapse-0.1.24-py37-none-any.whl

---
+++ packages/synapse/synapse-0.1.24-py37-none-any.whl/synapse/lib/stormhttp.py
@@ -0,0 +1,42 @@
+import json
+import asyncio
+import gzip
+
+import aiohttp
+
+import synapse.common as s_common
+import synapse.lib.stormtypes as s_stormtypes
+
+class LibHttp(s_stormtypes.Lib):
+    '''
+    HTTP client API for STORM
+    '''
+
+    def addLibFuncs(self):
+        self.locls.update({
+            'get': self._httpEasyGet,
+            #'post':
+            #'session':
+        })
+
+    async def _httpEasyGet(self, url, headers=None):
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(url, headers=headers) as resp:
+                info = {
+                    'code': resp.status,
+                    'body': await resp.content.read(),
+                }
+                return HttpResp(info)
+
+class HttpResp(s_stormtypes.StormType):
+
+    def __init__(self, locls):
+        s_stormtypes.StormType.__init__(self)
+        self.locls.update(locls)
+        self.locls.update({
+            'json': self._httpRespJson,
+        })
+
+    async def _httpRespJson(self):
+        body = self.locls.get('body')
+        return json.loads(body)
---
+++ packages/synapse/synapse-0.1.24-py37-none-any.whl/synapse/tests/test_lib_stormhttp.py
@@ -0,0 +1,22 @@
+import synapse.tests.utils as s_test
+
+class StormHttpTest(s_test.SynTest):
+
+    async def test_storm_http_get(self):
+
+        async with self.getTestCore() as core:
+            addr, port = await core.addHttpPort(0)
+            core.insecure = True
+            text = '''
+                $hdr = (
+                    ("User-Agent", "Storm HTTP Stuff"),
+                )
+                $url = $lib.str.format("http://127.0.0.1:{port}/api/v1/model", port=$port)
+
+                for $name in $lib.inet.http.get($url, headers=$hdr).json().result.forms {
+                    [ test:str=$name ]
+                }
+            '''
+            opts = {'vars': {'port': port}}
+            nodes = await core.nodes(text, opts=opts)
+            self.len(1, await core.nodes('test:str=inet:ipv4'))
