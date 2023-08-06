7f17ac0ac0db5985139e92bb546101a6ab495c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:36 +0000

    Add package role-pattern-nlp/0.2.0/role_pattern_nlp-0.2.0-py3-none-any.whl

---
+++ packages/role-pattern-nlp/role_pattern_nlp-0.2.0-py3-none-any.whl/role_pattern_nlp/role_pattern.py
@@ -0,0 +1,26 @@
+from role_pattern_nlp import util
+from role_pattern_nlp import match
+from role_pattern_nlp import role_pattern_vis
+
+
+class RolePattern():
+
+    def __init__(self, spacy_dep_pattern, token_labels):
+        self.spacy_dep_pattern = spacy_dep_pattern
+        self.token_labels = token_labels
+        self.token_labels_depth_order = None  # Corresponds one-to-one with spacy_dep_pattern
+        self.name = 'pattern'
+        self.builder = None
+        self.label2colour = {}  # For visualisation
+        self.training_match = None
+
+    def match(self, doc):
+        pattern = {'spacy_dep_pattern': self.spacy_dep_pattern, 'token_labels': self.token_labels}
+        matches = match.find_matches(doc, pattern, 'pattern')
+        return matches
+
+    def to_pydot(self, **kwargs):
+        return role_pattern_vis.pattern_to_pydot(self, **kwargs)
+
+    def match_to_pydot(self, match, **kwargs):
+        return role_pattern_vis.match_to_pydot(self, match, **kwargs)
---
+++ packages/role-pattern-nlp/role_pattern_nlp-0.2.0-py3-none-any.whl/role_pattern_nlp/validate.py
@@ -0,0 +1,25 @@
+from role_pattern_nlp import util
+
+
+def features_are_in_dependency_pattern(features, pattern):
+    for pattern_element in pattern:
+        for feature in features:
+            if feature not in pattern_element['PATTERN']:
+                return False
+    return True
+
+
+def features_are_in_role_pattern(features, role_pattern):
+    spacy_dependency_pattern = role_pattern.spacy_dep_pattern
+    return features_are_in_dependency_pattern(features, spacy_dependency_pattern)
+
+
+def pattern_matches_example(role_pattern, match_example):
+    doc = util.doc_from_match(match_example)
+    matches = role_pattern.match(doc)
+    match_example = {slot: util.token_idxs(tokens) for slot, tokens in match_example.items()}  # Compare using token.i to protect against false inequality caused by state changes
+    for match in matches:
+        match = {slot: util.token_idxs(tokens) for slot, tokens in match.items()}
+        if match == match_example:
+            return True, matches
+    return False, matches
