bc14e97697450eee2273f29c93c462f521db56b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:12 +0000

    Add package vtorch/0.1.27/vtorch-0.1.27.tar.gz

---
+++ packages/vtorch/vtorch-0.1.27.tar.gz/vtorch-0.1.27/vtorch/data/fields/__init__.py
@@ -0,0 +1,5 @@
+from .field import Field
+from .text_field import TextField
+from .label_field import LabelField
+from .metadata_field import MetadataField
+from .multilabel_field import MultiLabelField
