46017b77ca078598430749deb42f77b633440232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:35 +0000

    Add package graaf-tools/0.3.19/graaf_tools-0.3.19.tar.gz

---
+++ packages/graaf-tools/graaf_tools-0.3.19.tar.gz/graaf_tools-0.3.19/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'graaf_tools',
+  packages = find_packages(),
+  version = '0.3.19',
+  license='MIT',
+  description = 'tools and code for research',
+  author = 'Rafael Van Belle',
+  author_email = 'rafael.vanbelle@gmail.com',
+  url = 'https://github.com/raftie/graaf_tools',
+  download_url = 'https://github.com/Raftie/graaf_tools/archive/v0.3.19.tar.gz',
+  keywords = ['network'],
+  install_requires=[
+          'tqdm',
+          'gensim',
+          'networkx',
+          'matplotlib',
+          'scikit-plot',
+          'xgboost',
+          'imblearn'
+      ],
+  classifiers=[
+  ],
+)
