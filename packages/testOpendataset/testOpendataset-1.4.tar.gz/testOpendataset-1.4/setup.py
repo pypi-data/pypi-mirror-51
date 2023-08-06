6c0a0144fdd7adb69629d5a97807073b999405c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:59 +0000

    Add package testOpendataset/1.4/testOpendataset-1.4.tar.gz

---
+++ packages/testOpendataset/testOpendataset-1.4.tar.gz/testOpendataset-1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from setuptools import find_packages
+ 
+setup(
+    name='testOpendataset',
+    version='1.4',
+    description='SDK for the Open Image Dataset.',
+    packages= find_packages(),
+    author='zhenwan,t-tozho,t-zhiwan',
+    author_email= "zhenwan@microsoft.com,t-tozho@microsoft.com,t-zhiwan@microsoft.com",
+    url='https://dev.azure.com/zhenwan/zhenwan_default/_git/AMLProjects',
+    install_requires=[
+        'azureml-telemetry',
+        'azure-storage-blob',
+        'multimethods>=1.0.0',
+        'matplotlib >= 3.0.3',
+        'opencv-contrib-python >= 4.0.25'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
