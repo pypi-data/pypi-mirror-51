e32dbf20a00b319f503bca7ceb65564810c50a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:31 +0000

    Add package opustools-pkg/0.0.42/opustools_pkg-0.0.42-py3-none-any.whl

---
+++ packages/opustools-pkg/opustools_pkg-0.0.42-py3-none-any.whl/opustools_pkg/parse/exhaustive_sentence_parser.py
@@ -0,0 +1,62 @@
+from .sentence_parser import SentenceParser
+
+class ExhaustiveSentenceParser(SentenceParser):
+
+    def __init__(self, document, preprocessing, direction, wmode, 
+            language, annotations, anno_attrs, delimiter):
+        super().__init__(document, direction, preprocessing, wmode, 
+            language, annotations, anno_attrs, delimiter)
+        self.sentences = {}
+        self.done = False
+
+    def storeSentences(self):
+        self.document.readline()
+        while not self.done:
+            sentence = ''
+            while True:
+                line = self.document.readline()
+                if not line:
+                    self.done = True
+                    break
+                self.parseLine(line)
+                if self.pre == 'xml' or self.pre == 'parsed':
+                    sentence = self.addToken(sentence)
+                elif self.pre == 'raw':
+                    if self.sfound and self.start == 's':
+                        sentence = self.chara
+                        self.chara = ''
+                elif self.pre == 'rawos':
+                    if (self.sfound and self.start == 'time' 
+                            or self.start == 's'):
+                        sentence = self.chara
+                if self.efound:
+                    self.sfound = False
+                    self.efound = False
+                    self.chara = ''
+                    break
+            if self.sid != '':
+                self.sentences[self.sid] = (sentence.strip(), self.attrs)
+                self.sid = ''
+        self.document.close()
+
+    def getSentence(self, sid):
+        if sid in self.sentences.keys():
+            return self.sentences[sid]
+        else:
+            return '', {}
+
+    def readSentence(self, ids):
+        if len(ids) == 0 or ids[0] == '':
+            return '', []
+        sentence = ''
+        attrsList = []
+        if self.wmode == 'tmx':
+            sentence = self.addTuBeginning()
+        for sid in ids:
+            newSentence, attrs = self.getSentence(sid)
+            sentence = self.addSentence(sentence, newSentence, sid)
+            attrsList.append(attrs)
+        if self.wmode == 'tmx':
+            sentence = self.addTuEnding(sentence)
+
+        return sentence[1:], attrsList
