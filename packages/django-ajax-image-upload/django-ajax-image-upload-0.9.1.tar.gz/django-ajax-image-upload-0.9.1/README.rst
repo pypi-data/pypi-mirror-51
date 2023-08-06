63cd2a795a8aac3ff9b99a46ba37a1898dbdddba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:10 +0000

    Add package django-ajax-image-upload/0.9.1/django-ajax-image-upload-0.9.1.tar.gz

---
+++ packages/django-ajax-image-upload/django-ajax-image-upload-0.9.1.tar.gz/django-ajax-image-upload-0.9.1/README.rst
@@ -0,0 +1,98 @@
+=============================
+Django Ajax Image Upload
+=============================
+
+.. image:: https://badge.fury.io/py/django-ajax-image-upload.svg
+    :target: https://badge.fury.io/py/django-ajax-image-upload
+
+.. image:: https://travis-ci.org/idaproject/django-ajax-image-upload.svg?branch=master
+    :target: https://travis-ci.org/idaproject/django-ajax-image-upload
+
+.. image:: https://codecov.io/gh/idaproject/django-ajax-image-upload/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/idaproject/django-ajax-image-upload
+
+Upload images via ajax.
+
+Documentation
+-------------
+
+The full documentation is at https://django-ajax-image-upload.readthedocs.io.
+
+Quickstart
+----------
+
+Install Django Ajax Image Upload::
+
+    pip install django-ajax-image-upload
+
+Add it to your `INSTALLED_APPS`:
+
+.. code-block:: python
+
+    INSTALLED_APPS = (
+        ...
+        'ajaximage',
+        ...
+    )
+
+Add Django Ajax Image Upload's URL patterns:
+
+.. code-block:: python
+
+    from ajaximage import urls as ajaximage_urls
+
+
+    urlpatterns = [
+        ...
+        url(r'^', include(ajaximage_urls)),
+        ...
+    ]
+
+Define your Image model:
+.. code-block:: python
+
+    from ajaximage.models import AbstractImage
+
+
+    class MyImage(AbstractImage):
+        # define additional fields here
+
+        class Meta:
+            ...
+
+
+Define generic tabular inline
+.. code-block::python
+
+    from ajaximage.admin import ImageInline
+
+    from .models import MyImage
+
+
+    class MyImageInline(ImageInline):
+        models = MyImage
+
+Example usage of AjaxImageUploadMixin:
+.. code-block::python
+
+    models.py
+
+
+Features
+--------
+
+* TODO
+
+Running Tests
+-------------
+
+Does the code actually work?
+
+::
+
+    source <YOURVIRTUALENV>/bin/activate
+    (myenv) $ pip install tox
+    (myenv) $ tox
+
+Credits
+-------
---
+++ packages/django-ajax-image-upload/django-ajax-image-upload-0.9.1.tar.gz/django-ajax-image-upload-0.9.1/ajaximage/widgets.py
@@ -0,0 +1,60 @@
+import os
+
+from django.forms import widgets
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+from django.core.files.storage import default_storage
+
+
+class AjaxImageWidget(widgets.TextInput):
+
+    html = """
+    <div class="ajaximage">
+        <a class="file-link" target="_blank" href="{file_url}">
+            <img class="file-img" src="{file_url}">
+        </a>
+        <a class="file-remove" href="#remove">Remove</a>
+        <input class="file-path" type="hidden" value="{file_path}" id="{element_id}" name="{name}"/>
+        <input type="file" class="file-input" />
+        <input class="file-dest" type="hidden" value="{upload_url}">
+        <div class="progress progress-striped active">
+            <div class="bar"></div>
+        </div>
+    </div>
+    """
+
+    class Media:
+        js = (
+            'ajaximage/js/ajaximage.js',
+        )
+        css = {
+            'all': (
+                'ajaximage/css/bootstrap-progress.min.css',
+                'ajaximage/css/styles.css',
+            )
+        }
+
+    def __init__(self, *args, **kwargs):
+        self.upload_to = kwargs.pop('upload_to', '')
+        super(AjaxImageWidget, self).__init__(*args, **kwargs)
+
+    # noinspection PyMethodOverriding
+    def render(self, name, value, attrs=None, renderer=None):
+        final_attrs = self.build_attrs(attrs)
+        element_id = final_attrs.get('id')
+        upload_to = self.upload_to
+        if callable(self.upload_to):
+            upload_to = self.upload_to(None, '')
+        kwargs = {'upload_to': upload_to}
+        upload_url = reverse('ajaximage', kwargs=kwargs)
+        file_path = str(value) if value else ''
+        file_url = default_storage.url(file_path) if value else ''
+        file_name = os.path.basename(file_url)
+        output = self.html.format(upload_url=upload_url,
+                                  file_url=file_url,
+                                  file_name=file_name,
+                                  file_path=file_path,
+                                  element_id=element_id,
+                                  name=name)
+
+        return mark_safe(output)
