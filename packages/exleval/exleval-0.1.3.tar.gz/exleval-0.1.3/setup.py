7f2d6595e453f34b7d69415c1621e681ebcf2bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:43 +0000

    Add package exleval/0.1.3/exleval-0.1.3.tar.gz

---
+++ packages/exleval/exleval-0.1.3.tar.gz/exleval-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='exleval',
+    author='Vlad Stepanov',
+    author_email='8uk.8ak@gmail.com',
+    version='0.1.3',
+    package_dir={'': 'src'},
+    packages=['exleval'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
+)
