df5e51adfd00fe343d3033b89d818acf6da7a98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:23 +0000

    Add package acrolib/0.0.3/acrolib-0.0.3.tar.gz

---
+++ packages/acrolib/acrolib-0.0.3.tar.gz/acrolib-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+
+extensions = [Extension("acrolib.cost_functions", ["src/acrolib/cost_functions.pyx"])]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="acrolib",
+    version="0.0.3",
+    author="Jeroen De Maeyer",
+    author_email="jeroen.demaeyer@kuleuven.be",
+    description="General utilities for my robotics research at ACRO.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JeroenDM/acrolib",
+    packages=find_packages(where="src"),
+    install_requires=["cython", "numpy", "pyquaternion"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    ext_modules=cythonize(extensions),
+    python_requires=">=3.6",
+)
