fa0d84e1388d4daab673f70cd86cbe2e55546e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:33 +0000

    Add package gcgman/1.4/gcgman-1.4.tar.gz

---
+++ packages/gcgman/gcgman-1.4.tar.gz/gcgman-1.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gcgman",
+    version="1.4",
+    author="N.Ahmet BASTUG",
+    author_email="bastugn@itu.edu.tr",
+    description="A package to write any letter you want in the github graph",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kosantosbik/gcgman",
+    scripts=['bin/gcgman'],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'tqdm',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Unix",
+    ],
+)
