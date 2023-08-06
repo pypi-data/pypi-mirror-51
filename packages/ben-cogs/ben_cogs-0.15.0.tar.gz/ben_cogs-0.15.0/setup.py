a0eafef069ce99c2dd30ce4bf3326f51edff5f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:06 +0000

    Add package ben-cogs/0.15.0/ben_cogs-0.15.0.tar.gz

---
+++ packages/ben-cogs/ben_cogs-0.15.0.tar.gz/ben_cogs-0.15.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import re
+from setuptools import setup
+
+with open('ben_cogs/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+setup(
+	name='ben_cogs',
+	author='bmintz',
+	author_email='bmintz@protonmail.com',
+	description='Shared cogs for use in Discord bots',
+	url='https://github.com/bmintz/cogs',
+	download_url='https://github.com/bmintz/cogs/archive/{}.tar.gz'.format(version),
+	version=version,
+	packages=['ben_cogs'],
+	install_requires=[
+		'discord.py>=1.2.3,<2.0.0',
+		'humanize',
+		'objgraph'],
+	extras_require={
+		'sql': [
+			'asyncpg',
+			'prettytable']},
+	python_requires='>=3.6.0',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Framework :: AsyncIO',
+		'License :: OSI Approved :: MIT License',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Topic :: Internet',
+		'Topic :: Utilities'])
