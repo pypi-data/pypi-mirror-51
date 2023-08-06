60479882c2787d9428b366d3da49911ef99cf87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:00 +0000

    Add package peakina/0.2.5/peakina-0.2.5-py3-none-any.whl

---
+++ packages/peakina/peakina-0.2.5-py3-none-any.whl/peakina/io/http/http_fetcher.py
@@ -0,0 +1,34 @@
+import tempfile
+from email.utils import parsedate_to_datetime
+from typing import IO, List, Optional
+
+from urllib3 import PoolManager
+
+from ..fetcher import Fetcher, register
+
+
+@register(schemes=['http', 'https'])
+class HttpFetcher(Fetcher):
+    def __init__(self, *args, **kwargs):
+        self.pool_manager = PoolManager()
+        super().__init__(*args, **kwargs)
+
+    def open(self, filepath) -> IO:
+        r = self.pool_manager.request('GET', filepath, preload_content=False)
+        ret = tempfile.NamedTemporaryFile(suffix='.httptmp')
+        for chunk in r.stream():
+            ret.write(chunk)
+        ret.seek(0)
+        return ret
+
+    def listdir(self, dirpath) -> List[str]:
+        raise NotImplementedError
+
+    def mtime(self, filepath) -> Optional[int]:
+        try:
+            r = self.pool_manager.request('HEAD', filepath)
+        except Exception:
+            return None
+        else:
+            dt = parsedate_to_datetime(r.headers['last-modified'])
+            return int(dt.timestamp())
