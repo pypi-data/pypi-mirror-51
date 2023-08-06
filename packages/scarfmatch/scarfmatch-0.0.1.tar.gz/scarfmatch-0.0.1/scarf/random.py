6b249fa70d70bccc3bf6fbb2b5ac971b0dccb47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:00 +0000

    Add package scarfmatch/0.0.1/scarfmatch-0.0.1.tar.gz

---
+++ packages/scarfmatch/scarfmatch-0.0.1.tar.gz/scarfmatch-0.0.1/scarf/random.py
@@ -0,0 +1,105 @@
+"""Random Instance Generators"""
+
+import numpy as np
+
+import scarf.instance
+
+__all__ = ["gen_random_instance"]
+
+
+def _id_2_tuple(num_single, idx):
+  if idx < num_single:
+    return idx
+  else:
+    couple_idx = idx - num_single
+    return (couple_idx // 2, couple_idx % 2)
+
+
+def gen_random_instance(num_single, num_couple, num_hospital,
+                        num_additional_seat=0,
+                        single_pref_len=0, couple_pref_len=0, ihp=True):
+  """Generate a uniform random instance.
+
+  Generate a stable matching instance where single doctor's preference lists are
+  chosen uniformly at random, each hospital's ranking of doctors are also chosen
+  at random. The couple's preference lists are chosen at random from all lists
+  that are unemployment averse.
+
+  Args:
+    num_single: int
+      Number of single doctors.
+    num_couple: int
+      Number of coupled doctors.
+    num_hospital: int
+      Number of hospitals.
+    num_additional_seat: int, optional
+      Number of total seats will be number of applicants plus num_additional_seat,
+      provided that each hospital has at least one seat (Otherwise the hospital
+      capacity is 1 for every hospital). Default is 0.
+    single_pref_len: int, optional
+      Length of single's preference list. Any hospital outside 
+      preference list are worse than unemployment option. Default: generate full
+      preference list.
+    couple_pref_len: int, optional
+      Length of couple's preference list. Any hospital pair outside 
+      preference list are worse than unemployment option. Default: generate full
+      preference list.
+    ihp: bool, optional
+      If True (default), hospitals will share the same preference on individual
+      doctors. Otherwise hospitals will have independent preference lists.
+
+  Returns:
+    A `ScarfInstance` object.
+  """
+  num_applicant = num_single + 2 * num_couple
+  single_pref_list = np.argsort(
+      np.random.rand(num_single, num_hospital)
+  ).tolist()
+  if single_pref_len:
+    for s in range(num_single):
+      single_pref_list[s] = single_pref_list[s][:single_pref_len]
+  num_hospital_pair = (num_hospital + 1) ** 2 - 1
+  couple_pref_seed = np.random.rand(num_couple, num_hospital_pair)
+  couple_pref_seed += np.array(
+      [(pid // (num_hospital + 1) == num_hospital) or 
+       (pid % (num_hospital + 1) == num_hospital)
+       for pid in range(num_hospital_pair)], dtype=np.float64)
+       # plans with unemployment is are ranked lower
+  couple_pref_list = np.argsort(couple_pref_seed).tolist()
+  wrap = lambda x: x if x < num_hospital else -1
+  cpl = min(couple_pref_len,
+            num_hospital_pair) if couple_pref_len > 0 else num_hospital_pair
+  couple_pref_list = [
+      [(wrap(pid // (num_hospital + 1)), 
+      	wrap(pid % (num_hospital + 1))) for pid in li[:cpl]]
+      for li in couple_pref_list
+  ]
+  if ihp:
+    hospital_pref_list = np.argsort(
+        np.random.rand(num_applicant)
+    ).tolist()
+    hospital_pref_list = [
+        _id_2_tuple(num_single, i) for i in hospital_pref_list
+    ]
+  else:
+    hospital_pref_list = np.argsort(
+        np.random.rand(num_hospital, num_applicant)
+    ).tolist()
+    hospital_pref_list = [
+        [_id_2_tuple(num_single, i) for i in li] for li in hospital_pref_list
+    ]
+  hosp_seat = np.random.randint(
+      num_hospital,
+      size=max(num_applicant - num_hospital + num_additional_seat, 0)
+  )  # assign each seat randomly to a hospital
+  hospital_cap = []
+  for h in range(num_hospital):
+    hospital_cap.append(int(sum(hosp_seat == h) + 1))
+  
+  return scarf.instance.ScarfInstance(
+      single_pref_list=single_pref_list,
+      couple_pref_list=couple_pref_list,
+      hospital_pref_list=hospital_pref_list,
+      hospital_cap=hospital_cap
+  )
+
---
+++ packages/scarfmatch/scarfmatch-0.0.1.tar.gz/scarfmatch-0.0.1/scarf/utils.py
@@ -0,0 +1,110 @@
+"""Scarf Algotihm util functions."""
+
+import numpy as np
+
+
+def check_single_preflist(s, pair_list):
+  assert(np.all([len(p) == 2 and p[0] == s for p in pair_list]))
+  assert(pair_list[-1] == (s, -1))
+
+
+def check_couple_preflist(c, pair_list):
+  assert(np.all([len(p) == 3 and p[0] == c for p in pair_list]))
+  assert(pair_list[-1] == (c, -1, -1))
+
+
+def check_hospital_preflist(h, pair_list):
+  assert(np.all(h in p for p in pair_list))
+  assert(pair_list[-1] == (-1, h))
+
+
+def recover_pref_lists(num_single, num_couple, num_hospital, U,
+                       pair_list):
+  """Recover preference list from utility matrix."""
+  assert(U.shape[0] == num_single + num_couple + num_hospital)
+  num_hospital_pair = (num_hospital + 1) ** 2 - 1
+  assert(U.shape[1] == len(pair_list))
+  # Sort decend by negating the array
+  orders_U = np.argsort(-U)
+  single_pref_list, couple_pref_list, hospital_pref_list = [], [], []
+  for s in range(num_single):
+    cols = orders_U[s]
+    single_s_pair_list = [pair_list[col] for col in cols if U[s][col] < 0]
+    check_single_preflist(s, single_s_pair_list)
+    # The last one is (s, num_hospital), remove
+    single_pref_list.append([p[1] for p in single_s_pair_list[:-1]])
+  for c in range(num_couple):
+    cols = orders_U[num_single + c]
+    couple_c_pair_list = [pair_list[col] for col in cols 
+                          if U[num_single + c][col] < 0]
+    check_couple_preflist(c, couple_c_pair_list)
+    # The last one is (c, num_hospital, num_hospital), remove
+    couple_pref_list.append([p[1:] for p in couple_c_pair_list[:-1]])
+  for h in range(num_hospital):
+    cols = orders_U[num_single + num_couple + h]
+    hospital_h_pref_list = [pair_list[col] for col in cols
+                            if U[num_single + num_couple + h][col] < 0]
+    check_hospital_preflist(h, hospital_h_pref_list)
+    # The last one is (s, num_hospital), remove
+    hospital_pref_list.append(hospital_h_pref_list[:-1])
+  return single_pref_list, couple_pref_list, hospital_pref_list
+
+
+def create_hospital_pref_on_pairs(num_hospital, h, one_hospital_pref_list, 
+                                  single_pref_list, couple_pref_list):
+  """Create hospital's preference on pairs given preference on individuals."""
+  pair_pref_list = []
+  for i in one_hospital_pref_list:
+    if isinstance(i, int):
+      if h in single_pref_list[i]:
+        pair_pref_list += [(i, h)]
+    else:
+      c, j = i  # couple c, member j
+      # find out if this member is the better one
+      member_j_position = one_hospital_pref_list.index((c, j))
+      other_member_position = one_hospital_pref_list.index((c, 1 - j))
+      is_better_member = member_j_position < other_member_position
+      # filter out the pairs related to hospital h and (c, j)
+      # where (c, j) is the worst member assigned to couple h in this pair
+      def is_relavent_pair(p):
+        return p[j] == h and not (p == (h, h) and is_better_member)
+      couple_c_pref_list = list(filter(
+          is_relavent_pair,
+          couple_pref_list[c]
+      ))
+      pair_pref_list += [(c,) + p for p in couple_c_pref_list]
+
+  return pair_pref_list
+
+
+def check_stable(U, basis):
+  """Check if a basis is ordinal basis for a utility matrix.
+
+  Args:
+    U: (m, n) utility matrix.
+    basis: a list of m column indices
+
+  Returns:
+    True if `basis` is an ordinal basis of `U`
+  """
+  U_pt = U + np.tile(
+    np.linspace(start=0.5, stop=0.0, num=U.shape[1]), 
+    (U.shape[0], 1))
+  rowmins = np.min(U[:, basis], axis=1, keepdims=True)
+  U_dom = U <= rowmins
+  return np.all(np.any(U_dom, axis=0))
+
+
+def check_feasible(A, basis, alloc, b, tol=1e-6):
+  """Check if a basis is a feasible basis for a polytope.
+
+  Args:
+    A: (m, n) constraint matrix.
+    basis: a list of m column indices
+    alloc: allocation vector
+    b: the right hand side vector of size (m,)
+
+  Returns:
+    True if `basis` is a feasible basis of the polytope `Ax=b, x>=0`.
+  """
+  return np.all(np.dot(A[:, basis], alloc) <= np.array(b) + tol)
