47f8a74dcfec1192cfe1c98730e066de9ba187a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:17 +0000

    Add package diagram/0.2.28/diagram-0.2.28.tar.gz

---
+++ packages/diagram/diagram-0.2.28.tar.gz/diagram-0.2.28/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import io
+import os
+
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+
+setup(
+    name='diagram',
+    version='0.2.28',
+    description='Text mode diagrams using UTF-8 characters and fancy colors',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tehmaze/diagram',
+    author='Wijnand Modderman-Lenstra',
+    author_email='maze@pyth0n.org',
+    keywords=['diagram', 'graph', 'ascii', 'ansi', 'text'],
+    py_modules=['diagram'],
+    scripts=[os.path.join('bin', 'diagram')],
+)
