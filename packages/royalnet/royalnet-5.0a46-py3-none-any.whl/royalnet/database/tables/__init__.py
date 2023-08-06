8bdd07524fd79ada84eed8eae97b3f430705ee68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:12 +0000

    Add package royalnet/5.0a46/royalnet-5.0a46-py3-none-any.whl

---
+++ packages/royalnet/royalnet-5.0a46-py3-none-any.whl/royalnet/database/tables/__init__.py
@@ -0,0 +1,18 @@
+from .royals import Royal
+from .telegram import Telegram
+from .diario import Diario
+from .aliases import Alias
+from .activekvgroups import ActiveKvGroup
+from .keyvalues import Keyvalue
+from .keygroups import Keygroup
+from .discord import Discord
+from .wikipages import WikiPage
+from .wikirevisions import WikiRevision
+from .medals import Medal
+from .medalawards import MedalAward
+from .bios import Bio
+from .reminders import Reminder
+from .triviascores import TriviaScore
+
+__all__ = ["Royal", "Telegram", "Diario", "Alias", "ActiveKvGroup", "Keyvalue", "Keygroup", "Discord", "WikiPage",
+           "WikiRevision", "Medal", "MedalAward", "Bio", "Reminder", "TriviaScore"]
