4e7181f700222ee66f29b19db74d543da779b738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:00 +0000

    Add package vtorch/0.1.38/vtorch-0.1.38-py3-none-any.whl

---
+++ packages/vtorch/vtorch-0.1.38-py3-none-any.whl/vtorch/data/fields/__init__.py
@@ -0,0 +1,6 @@
+from .field import Field
+from .text_field import TextField
+from .label_field import LabelField
+from .metadata_field import MetadataField
+from .text_field_bert import TextFieldBert
+from .multilabel_field import MultiLabelField
