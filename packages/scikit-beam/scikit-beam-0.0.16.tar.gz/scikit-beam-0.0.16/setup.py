29f97f671b5c94500a9c1e61b1dda0b4655b52aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:40 +0000

    Add package scikit-beam/0.0.16/scikit-beam-0.0.16.tar.gz

---
+++ packages/scikit-beam/scikit-beam-0.0.16.tar.gz/scikit-beam-0.0.16/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup, Extension
+import versioneer
+import os
+import sys
+
+import numpy as np
+from Cython.Build import cythonize
+
+
+def c_ext():
+    if os.name == 'nt':
+        # we are on windows. Do not compile the extension. Tons of errors are
+        # spit out when we compile on AppVeyor.
+        # https://gist.github.com/ericdill/bdc86eb81e338ca4624b
+        return []
+
+    # compile for MacOS without openmp
+    if sys.platform == 'darwin':
+        return [Extension('skbeam.ext.ctrans', ['src/ctrans.c'])]
+    # compile the extension on Linux.
+    return [Extension('skbeam.ext.ctrans', ['src/ctrans.c'],
+                      extra_compile_args=['-fopenmp'],
+                      extra_link_args=['-lgomp'])]
+
+
+def cython_ext():
+    return cythonize("**/*.pyx")
+
+
+setup(
+    name='scikit-beam',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author='Brookhaven National Lab',
+    description="Data analysis tools for X-ray science",
+    packages=setuptools.find_packages(exclude=['doc']),
+    include_dirs=[np.get_include()],
+    package_data={'skbeam.core.constants': ['data/*.dat']},
+    install_requires=['six', 'numpy'],  # essential deps only
+    ext_modules=c_ext() + cython_ext(),
+    url='http://github.com/scikit-beam/scikit-beam',
+    keywords='Xray Analysis',
+    license='BSD',
+    classifiers=['Development Status :: 3 - Alpha',
+                 "License :: OSI Approved :: BSD License",
+                 "Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.4",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Topic :: Scientific/Engineering :: Chemistry",
+                 "Topic :: Software Development :: Libraries",
+                 "Intended Audience :: Science/Research",
+                 "Intended Audience :: Developers",
+                 ],
+    )
