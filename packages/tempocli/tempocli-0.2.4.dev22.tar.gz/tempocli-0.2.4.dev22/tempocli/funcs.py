438ad687b06d4245431f49faf2536dfe1f06fd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:31 +0000

    Add package tempocli/0.2.4.dev22/tempocli-0.2.4.dev22.tar.gz

---
+++ packages/tempocli/tempocli-0.2.4.dev22.tar.gz/tempocli-0.2.4.dev22/tempocli/funcs.py
@@ -0,0 +1,51 @@
+import os
+import re
+
+import click
+import yaml
+
+
+TIME_UNITS = {
+    's': 1,
+    'm': 60,
+    'h': 3600,
+}
+
+# NOTE(awiddersheim): Builds out a regex to match a string that starts
+# with any number of decimals followed by a time unit.
+TIME_REGEX = re.compile(
+    r'^(?P<time>\d+)(?P<unit>({}))$'.format(
+        '|'.join(TIME_UNITS.keys()),
+    ),
+)
+
+
+def load_yaml(path):
+    with click.open_file(path) as f:
+        return yaml.safe_load(f.read())
+
+
+def parse_short_time(time):
+    match = TIME_REGEX.match(time)
+
+    if not match:
+        raise ValueError(
+            'Could not parse time, allowed units ({})'.format(
+                ', '.join(TIME_UNITS),
+            ),
+        )
+
+    data = match.groupdict()
+
+    return int(data['time']) * TIME_UNITS[data['unit']]
+
+
+def get_env_opt(var, *args, **kwargs):
+    return os.getenv(
+        '{}_{}'.format(
+            click.get_current_context().auto_envvar_prefix,
+            var,
+        ),
+        *args,
+        **kwargs,
+    )
