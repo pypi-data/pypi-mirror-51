0927036651b997bc6d5f518fb96fedc655017d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:20 +0000

    Add package decompress/0.0.2/decompress-0.0.2.tar.gz

---
+++ packages/decompress/decompress-0.0.2.tar.gz/decompress-0.0.2/README.md
@@ -0,0 +1,5 @@
+# Decompress
+
+Decompress makes unzipping, untarring, and uncompressing all types of files easy.  Just run your file through decompress and your compressed file will automatically be decompressed in the destination that you provide.  
+
+Decompress was heavily influenced from [fastai v2](https://github.com/fastai/fastai_dev/blob/master/dev/04_data_external.ipynb).
---
+++ packages/decompress/decompress-0.0.2.tar.gz/decompress-0.0.2/decompress/util.py
@@ -0,0 +1,25 @@
+import tarfile
+import zipfile
+from pathlib import Path
+
+# development of this library was heavily influenced by code in fastai https://github.com/fastai/fastai_dev/blob/master/dev/04_data_external.ipynb
+
+__all__ = ["decompress"]
+
+def decompress(fname=None, dest=None):
+    "decompress `fname` to folder `dest`."
+    assert(fname), "Please specify a fname to decompress"
+    fname = Path(fname)
+    dest = fname.parent if dest is None else Path(dest)/fname.stem
+    fname_suffix = fname.suffix
+    if fname_suffix==".tar":
+        tarfile.open(fname, 'r').extractall(dest)
+    elif fname_suffix==".tgz":
+        tarfile.open(fname, 'r:gz').extractall(dest)
+    elif fname_suffix==".bz2":
+        tarfile.open(fname, 'r:bz').extractall(dest)
+    elif zipfile.is_zipfile(fname):
+        zipfile.ZipFile(fname, 'r').extractall(dest)
+    else:
+        print(f'{fname.stem} is not yet supported for decompressing')
+    return dest
