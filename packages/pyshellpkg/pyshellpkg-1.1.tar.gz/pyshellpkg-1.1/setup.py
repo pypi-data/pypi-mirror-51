f8cdbb84cf733062b993fe582e1de332059ca5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:02 +0000

    Add package pyshellpkg/1.1/pyshellpkg-1.1.tar.gz

---
+++ packages/pyshellpkg/pyshellpkg-1.1.tar.gz/pyshellpkg-1.1/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup
+
+setup(
+    name = 'pyshellpkg',
+    packages = ['pyshellpkg'],
+    version = '1.1',  
+    description = 'A simple modular UNIX-like shell using Python',
+    author = 'priyanshi',
+    author_email = 'priyanss@adobe.com',
+    url = 'https://git.corp.adobe.com/priyanss/python-mini-project',
+    download_url = 'https://git.corp.adobe.com/priyanss/python-mini-project/archive/1.2.tar.gz',
+    keywords = ['PyShell'],
+    classifiers = [
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
