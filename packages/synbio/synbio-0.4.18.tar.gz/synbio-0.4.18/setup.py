f77795109a1de3bfa7561992b11c37e0bc8c75a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:09 +0000

    Add package synbio/0.4.18/synbio-0.4.18.tar.gz

---
+++ packages/synbio/synbio-0.4.18.tar.gz/synbio-0.4.18/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+
+setup(
+    name="synbio",
+    version="0.4.18",
+    author="JJTimmons",
+    author_email="jtimmons@latticeautomation.com",
+    description="Synbio design and build library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    url="https://github.com/Lattice-Automation/synbio",
+    test_suite="tests.suite",
+)
