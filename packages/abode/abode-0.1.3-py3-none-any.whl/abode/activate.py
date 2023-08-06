b6eb15d5bdad4d644eacd1cc495f5ab8d2605bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:33 +0000

    Add package abode/0.1.3/abode-0.1.3-py3-none-any.whl

---
+++ packages/abode/abode-0.1.3-py3-none-any.whl/abode/activate.py
@@ -0,0 +1,16 @@
+import argparse
+import subprocess
+import sys
+
+def activate():
+    # TODO
+    pass
+    # The code below doesn't work.
+
+    # parser = argparse.ArgumentParser(
+    #             description='Enter an environment')
+
+    # parser.add_argument('name', help='Name of environment')
+    # args = parser.parse_args(sys.argv[2:])
+    # print(args.name)
+    # subprocess.call("conda activate {}".format(args.name), shell=True)
