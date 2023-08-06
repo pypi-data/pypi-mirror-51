4dced9eef601c74c920543e62227592053561e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:30 +0000

    Add package snakeoil/0.8.2/snakeoil-0.8.2-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/snakeoil/snakeoil-0.8.2-cp36-cp36m-manylinux1_x86_64.whl/snakeoil/strings.py
@@ -0,0 +1,20 @@
+"""string related methods"""
+
+
+def pluralism(obj, none=None, singular='', plural='s'):
+    """Return singular or plural suffix depending on object's length or value."""
+    # default to plural for empty objects, e.g. there are 0 repos
+    if none is None:
+        none = plural
+
+    try:
+        value = len(obj)
+    except TypeError:
+        # value is probably an int
+        value = obj
+
+    if value == 0:
+        return none
+    elif value == 1:
+        return singular
+    return plural
