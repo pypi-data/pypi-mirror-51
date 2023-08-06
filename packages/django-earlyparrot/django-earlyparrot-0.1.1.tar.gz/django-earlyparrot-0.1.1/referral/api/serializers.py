3b816894a8edacd7ee4c311fc24f536bd0b86dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:37 +0000

    Add package django-earlyparrot/0.1.1/django-earlyparrot-0.1.1.tar.gz

---
+++ packages/django-earlyparrot/django-earlyparrot-0.1.1.tar.gz/django-earlyparrot-0.1.1/referral/api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from ..models import Campaign
+
+
+class CampaignSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Campaign
+        fields = ['name', 'campaign_id']
---
+++ packages/django-earlyparrot/django-earlyparrot-0.1.1.tar.gz/django-earlyparrot-0.1.1/referral/api/views/webhooks.py
@@ -0,0 +1,41 @@
+import logging
+
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+
+from ...models import Campaign
+from ...signals_define import referral_reward_acquired
+
+logger = logging.getLogger('referral')
+
+
+class RewardAPIView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        status_code = status.HTTP_404_NOT_FOUND
+        webhook_data = request.data
+
+        try:
+            campaign = Campaign.objects.get(campaign_id=webhook_data.get('campaignId'))
+            user = campaign.users.get(email=webhook_data.get('email'))
+
+            referral_reward_acquired.send(
+                sender=campaign,
+                user=user,
+                reward_data=webhook_data,
+            )
+            status_code = status.HTTP_200_OK
+            logger.info('RewardAPIView OK')
+
+        except Campaign.DoesNotExist:
+            logger.error('Campaign.DoesNotExist')
+
+        except get_user_model().DoesNotExist:
+            logger.error('User.DoesNotExist')
+
+        return HttpResponse(status=status_code)
