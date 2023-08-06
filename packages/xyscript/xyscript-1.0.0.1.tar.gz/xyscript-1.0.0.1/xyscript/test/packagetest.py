e7321f5aa6b115741d036d406333707f902037ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:20 +0000

    Add package xyscript/1.0.0.1/xyscript-1.0.0.1.tar.gz

---
+++ packages/xyscript/xyscript-1.0.0.1.tar.gz/xyscript-1.0.0.1/xyscript/test/packagetest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# AUTHOR: XYCoder
+# FILE: ~/Desktop/saic/xyscript/xyscript/test/packagetest.py
+# DATE: 2019/09/03 Tue
+# TIME: 18:43:37
+
+# DESCRIPTION:
+# from xyscript.common.xylog import faillog
+import time
+
+def main():
+    # faillog('test')
+    time.sleep(10)
+    print(u'test')
+
+
+if __name__ == '__main__':
+    main()
+    
