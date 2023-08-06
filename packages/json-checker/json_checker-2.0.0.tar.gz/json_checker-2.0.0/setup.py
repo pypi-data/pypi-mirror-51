4374350acddb0093cc7a54bdc32ef77e920b1873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:39 +0000

    Add package json-checker/2.0.0/json_checker-2.0.0.tar.gz

---
+++ packages/json-checker/json_checker-2.0.0.tar.gz/json_checker-2.0.0/setup.py
@@ -0,0 +1,41 @@
+import codecs
+from setuptools import setup, find_packages
+
+
+setup(
+    name="json_checker",
+    version='2.0.0',
+    author='Denis Korytkin',
+    author_email='dkorytkin@gmail.com',
+    description='Simple schema validation library',
+    keywords=[
+        'Json checker',
+        'API testing',
+        'requests testing',
+        'json schema validation'
+    ],
+    url='https://github.com/DKorytkin/json_checker',
+    platforms=['linux'],
+    packages=find_packages(),
+    license='MIT license',
+    py_modules=[
+        'json_checker.app',
+        'json_checker.core.base',
+        'json_checker.core.checkers',
+        'json_checker.core.exceptions',
+        'json_checker.core.reports',
+    ],
+    python_requires='>=3.6',
+    long_description=codecs.open('README.rst', 'r', 'utf-8').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
