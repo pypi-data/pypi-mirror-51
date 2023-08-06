5ff931ab206f9585186e2d836f4a302be125b8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:05 +0000

    Add package adevinta-yapo-bi-postgresql/0.0.0.0.3/adevinta_yapo_bi_postgresql-0.0.0.0.3.tar.gz

---
+++ packages/adevinta-yapo-bi-postgresql/adevinta_yapo_bi_postgresql-0.0.0.0.3.tar.gz/adevinta_yapo_bi_postgresql-0.0.0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='adevinta_yapo_bi_postgresql',
+      version='0.0.0.0.3',
+      description='Class develop to improving access to postgresql functions.',
+      url='https://github.mpi-internal.com/Yapo/bi-insights',
+      author='Yapo BI',
+      author_email='bi.insights@yapo.cl',
+      license='MIT',
+      packages=['adevinta_yapo_bi_postgresql'],
+      zip_safe=False)
