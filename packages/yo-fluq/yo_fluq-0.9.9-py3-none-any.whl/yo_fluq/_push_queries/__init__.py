b9ec473b94aa10d188b1fe518a015ec35b0e53e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:55:32 +0000

    Add package yo-fluq/0.9.9/yo_fluq-0.9.9-py3-none-any.whl

---
+++ packages/yo-fluq/yo_fluq-0.9.9-py3-none-any.whl/yo_fluq/_push_queries/__init__.py
@@ -0,0 +1,4 @@
+from .arch import PushQueryElement, CommonPQE
+from . import aggregation as agg
+from .aggregation_code_factory import AggregationCodeFactory
+from .push_query import PushQuery
---
+++ packages/yo-fluq/yo_fluq-0.9.9-py3-none-any.whl/yo_fluq/_push_queries/aggregation/__init__.py
@@ -0,0 +1,2 @@
+from .collections import *
+from .one_liners import *
