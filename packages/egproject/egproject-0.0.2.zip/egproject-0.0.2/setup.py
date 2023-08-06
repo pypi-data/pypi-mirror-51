e9aae90dd171cafc105771c839470467513728aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:51 +0000

    Add package egproject/0.0.2/egproject-0.0.2.zip

---
+++ packages/egproject/egproject-0.0.2.zip/egproject-0.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import os
+import sys
+def find_packages(where='.'):
+        # os.walk -> list[(dirname, list[subdirs], list[files])]
+        return [folder.replace("/", ".").lstrip(".")
+                for (folder, _, fils) in os.walk(where)
+                if "__init__.py" in fils]
+
+from io import open as io_open
+src_dir = os.path.abspath(os.path.dirname(__file__))
+
+def readme():
+	with open('README.md') as f:
+		README=f.read()
+	return README
+
+setup(
+	name="egproject",
+	version="0.0.2",
+	description="A Python package to print",
+	long_description=readme(),
+	long_description_content_type=["text/markdown"],
+	author="ANOOP SINGH RANA",
+	author_email="apours986@gmail.com",
+	license="MIT",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+	],
+	#packages="test_run", thisw was for python3 but u need each nd every directory separately
+	#package_dir={'':'test_run'},
+	packages=["egproject"],
+	include_package_data=True,
+	entry_points={
+		"console_scripts":  [
+			"egproject=egproject._main:main",
+		]
+	},
+)
