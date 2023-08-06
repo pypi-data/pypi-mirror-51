55f74e14b6a8689d9eff5cb62c65f70ef5e31bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:44 +0000

    Add package dtwr/0.3.6/dtwr-0.3.6.tar.gz

---
+++ packages/dtwr/dtwr-0.3.6.tar.gz/dtwr-0.3.6/dtwr/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python port of R's Comprehensive Dynamic Time Warp algorithm package."""
+
+__author__ = """Toni Giorgino"""
+__email__ = 'toni.giorgino@gmail.com'
+__version__ = '0.3.6'
+
+
+from .dtw import *
+from .stepPattern import *
+from .data import *
+from .countPaths import *
+from .dtwPlot import *
+from .mvm import *
+from .warp import *
+from .warpArea import *
+from .window import *
+
+import __main__ as main
+if not hasattr(main, '__file__'):
+    print("""Importing the dtwr module. When using in academic works please cite:
+  T. Giorgino. Computing and Visualizing Dynamic Time Warping Alignments in R: The dtw Package.
+  J. Stat. Soft., doi:10.18637/jss.v031.i07.\n""")
+
+
