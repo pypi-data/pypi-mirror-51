eccbc9a6a57dbff44cb5158d634edc6b3a967a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:10 +0000

    Add package methylpy/1.3.9/methylpy-1.3.9.tar.gz

---
+++ packages/methylpy/methylpy-1.3.9.tar.gz/methylpy-1.3.9/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+methylpy_version = '1.3.9'
+
+setup(
+    name='methylpy',
+    version=methylpy_version,
+    author='Yupeng He',
+    author_email='yupeng.he.bioinfo@gmail.com',
+    packages=['methylpy'], 
+    license='LICENSE.txt',
+    description='Bisulfite sequencing data processing and differential methylation analysis',
+    long_description=open('docs/README.rst').read(),
+    install_requires=[
+        "numpy >= 1.6.1",
+	"scipy >= 0.10.0",
+    ],
+    package_data = {
+        'methylpy.test':['test/*'],
+	'methylpy':['rms.cpp', 'run_rms_tests.out']
+    },
+    keywords = ["Bioinformatics pipeline",
+                "DNA methylation",
+                "Bisulfite sequencing data",
+                "Nome-seq data",
+                "Differential methylation",
+                "Calling DMRs",
+                "Epigenetics",
+                "Functional genomics"],
+    scripts = ["bin/methylpy"]
+)
+
+if __name__ == '__main__':
+    f = open("methylpy/__init__.py",'w')
+    f.write("__version__ = \'"+methylpy_version+"\'"+"\n")
+    f.close()
