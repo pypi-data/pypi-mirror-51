3fd0493ce422bffb2bb1b3764ae92090cee2c693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:42 +0000

    Add package edc-screening/0.1.4/edc_screening-0.1.4-py3-none-any.whl

---
+++ packages/edc-screening/edc_screening-0.1.4-py3-none-any.whl/edc_screening/tests/test_evaluators.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, tag
+from edc_reportable.evaluator import ValueBoundryError
+
+from ..age_evaluator import AgeEvaluator
+
+
+class TestEvaluators(TestCase):
+    def test_age(self):
+        evaluator = AgeEvaluator(age_lower=18, age_lower_inclusive=True)
+
+        print(dir(evaluator))
+
+        try:
+            evaluator.in_bounds_or_raise(age=17)
+        except ValueBoundryError:
+            print(evaluator.description(17))
