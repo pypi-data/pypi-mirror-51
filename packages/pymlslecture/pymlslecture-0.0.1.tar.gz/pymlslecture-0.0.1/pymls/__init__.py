6c9cfefef1ebbf1e5b0d25a100569474852fcb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:07 +0000

    Add package pymlslecture/0.0.1/pymlslecture-0.0.1.tar.gz

---
+++ packages/pymlslecture/pymlslecture-0.0.1.tar.gz/pymlslecture-0.0.1/pymls/__init__.py
@@ -0,0 +1,7 @@
+__VERSION__ = '1.5'
+
+from pymls.solver import Solver
+from pymls.layers import Layer, StochasticLayer
+from pymls.media import from_yaml
+
+import pymls.backing as backing
---
+++ packages/pymlslecture/pymlslecture-0.0.1.tar.gz/pymlslecture-0.0.1/tests/test_media_loading.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+# -*- coding:utf8 -*-
+#
+# media_loading.py
+#
+# This file is part of pymls, a software distributed under the MIT license.
+# For any question, please contact one of the authors cited below.
+#
+# Copyright (c) 2017
+# 	Olivier Dazel <olivier.dazel@univ-lemans.fr>
+# 	Mathieu Gaborit <gaborit@kth.se>
+# 	Peter Göransson <pege@kth.se>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+
+import pytest
+import tempfile
+import os
+
+from pymls.media import from_yaml, Air, EqFluidJCA
+from pymls.media.medium import Medium
+
+
+class TestMediaLoading:
+
+    @pytest.fixture
+    def yaml_fn(self, tmpdir):
+        path =  tmpdir.join('medium.yaml')
+        path.ensure()
+        return path.strpath
+
+    def test_file_not_found(self, tmpdir):
+
+        with pytest.raises(IOError):
+            from_yaml(tmpdir.join('inexistent.yaml').strpath)
+
+    def test_bad_medium_type(self, yaml_fn):
+        with open(yaml_fn, 'w') as fh:
+            fh.write('medium_type: phony_medium\n')
+
+        with pytest.raises(ValueError):
+            from_yaml(yaml_fn)
+
+    def test_missing_parameter(self, yaml_fn):
+        params = ['phi', 'sigma', 'alpha', 'Lambda_prime']
+
+        with open(yaml_fn, 'w') as fh:
+            fh.write('medium_type: eqf\n')
+            for p in params:
+                fh.write('{}: 42\n'.format(p))
+
+        with pytest.raises(LookupError):
+            from_yaml(yaml_fn)
+
+    def test_ok_loading_eqf(self, yaml_fn):
+        params = ['phi', 'sigma', 'alpha', 'Lambda_prime', 'Lambda',
+                  'rho_1', 'nu', 'E', 'eta']
+
+        with open(yaml_fn, 'w') as fh:
+            fh.write('medium_type: eqf\n')
+            for ii, p in enumerate(params):
+                fh.write('{}: {}\n'.format(p, ii))
+
+        medium = from_yaml(yaml_fn)
+        for ii, p in enumerate(params):
+            assert getattr(medium, p) == ii
