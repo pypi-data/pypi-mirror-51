cd25045e62c80e276e9a7413fe01606d660c1fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:57 +0000

    Add package django-earlyparrot/0.1.10/django-earlyparrot-0.1.10.tar.gz

---
+++ packages/django-earlyparrot/django-earlyparrot-0.1.10.tar.gz/django-earlyparrot-0.1.10/referral/tasks/campaign.py
@@ -0,0 +1,34 @@
+import requests
+import logging
+
+from celery import Task
+
+logger = logging.getLogger('referral')
+
+
+class CampaignSubscribeTask(Task):
+    ignore_result = True
+    name = 'CampaignSubscribeTask'
+
+    def run(self, *args, **kwargs):
+        campaign_id = kwargs.get('campaign_id')
+        email = kwargs.get('email')
+        rh = kwargs.get('rh')
+        url = 'https://admin.earlyparrot.com/api/campaigns/{}/subscribe'.format(campaign_id)
+
+        data = {
+            'firstName': kwargs.get('firstName'),
+            'lastName': kwargs.get('lastName'),
+            'email': email,
+            'rh': rh,
+        }
+
+        if kwargs.get('conversionName'):
+            data['conversionName'] = kwargs.get('conversionName')
+
+        try:
+            requests.post(url, data=data)
+            logger.info('CampaignSubscribeTask: {}-{}-{}'.format(campaign_id, email, rh))
+        except Exception as exc:
+            logger.error('CampaignSubscribeTask.Exception: {}-{}-{}'.format(campaign_id, email, rh))
+            logger.error('CampaignSubscribeTask.Exception: {}'.format(exc))
