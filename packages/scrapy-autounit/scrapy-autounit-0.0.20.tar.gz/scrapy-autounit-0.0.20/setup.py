ac3c8a06969f8051407e1ed1c7479e94de5204ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:12 +0000

    Add package scrapy-autounit/0.0.20/scrapy-autounit-0.0.20.tar.gz

---
+++ packages/scrapy-autounit/scrapy-autounit-0.0.20.tar.gz/scrapy-autounit-0.0.20/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='scrapy-autounit',
+    version='0.0.20',
+    author='',
+    author_email='',
+    description='Automatic unit test generation for Scrapy.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fcanobrash/scrapy-autounit',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'pathlib',
+    ],
+)
